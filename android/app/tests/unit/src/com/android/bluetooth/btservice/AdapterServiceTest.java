4610a31f97ade8f82661e32283a2df49f3f03420
Animesh Fatehpuria <fatehpuria@google.com>
2021-07-13 16:21:49 +0800

    Fix BT Query Routing Issue
    During BREDR_TIMEOUT, we accidentally skip the BLE_ON state which is
    important for broadcasting intent to external apps. This patch fixes
    this issue.
    
    Bug: 191828729
    Tag: #refactor
    Test: Manual
    Change-Id: I8fd6c2f54b2bdae9b8a587c144424a976b6201d5

--- android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -581,8 +581,13 @@ public class AdapterServiceTest {
         mAdapterService.onProfileServiceStateChanged(mMockService, BluetoothAdapter.STATE_OFF);
 
         verifyStateChange(BluetoothAdapter.STATE_TURNING_OFF,
-                BluetoothAdapter.STATE_BLE_TURNING_OFF, 1,
+                BluetoothAdapter.STATE_BLE_ON, 1,
                 AdapterState.BREDR_STOP_TIMEOUT_DELAY + CONTEXT_SWITCH_MS);
+
+        mServiceBinder.onBrEdrDown(mAttributionSource);
+
+        verifyStateChange(BluetoothAdapter.STATE_BLE_ON, BluetoothAdapter.STATE_BLE_TURNING_OFF,
+                1, CONTEXT_SWITCH_MS);
 
         // Stop GATT
         if (hearingAidSupported) {
