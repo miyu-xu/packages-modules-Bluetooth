593369cc3cfb4262cc7667e97ccf4228c37315d2
Chris Manton <cmanton@google.com>
2021-09-13 20:01:59 -0700

    sdp_test: Add sdpu_compare_uuid
    Bug: 191332474
    Test: atest net_test_stack_sdp
    Flag: EXEMPT, Test Infrastructure
    
    Change-Id: Id7d687ae19a091c7e0d28ce83f2d289d29f8b042

--- system/stack/test/sdp/stack_sdp_utils_test.cc
+++ system/stack/test/sdp/stack_sdp_utils_test.cc
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <frameworks/proto_logging/stats/enums/bluetooth/enums.pb.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <stdlib.h>
 
 #include <cstddef>
 
@@ -23,14 +25,23 @@
 #include "common/init_flags.h"
 #include "device/include/interop.h"
 #include "mock_btif_config.h"
+#include "osi/include/allocator.h"
 #include "profile/avrcp/avrcp_config.h"
 #include "stack/include/avrc_api.h"
 #include "stack/include/avrc_defs.h"
 #include "stack/include/bt_types.h"
 #include "stack/include/bt_uuid16.h"
+#include "stack/sdp/internal/sdp_api.h"
 #include "stack/sdp/sdpint.h"
+#include "test/fake/fake_osi.h"
 #include "test/mock/mock_btif_config.h"
+#include "test/mock/mock_osi_allocator.h"
 #include "test/mock/mock_osi_properties.h"
+#include "test/mock/mock_stack_l2cap_api.h"
+
+#ifndef BT_DEFAULT_BUFFER_SIZE
+#define BT_DEFAULT_BUFFER_SIZE (4096 + 16)
+#endif
 
 #define INVALID_LENGTH 5
 #define INVALID_UUID 0X1F
@@ -41,6 +52,10 @@
 #define HFP_PROFILE_MINOR_VERSION_6 0x06
 #define HFP_PROFILE_MINOR_VERSION_7 0x07
 
+static int L2CA_ConnectReq2_cid = 0x42;
+static RawAddress addr = RawAddress({0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6});
+static tSDP_DISCOVERY_DB* sdp_db = nullptr;
+
 using testing::_;
 using testing::DoAll;
 using testing::Return;
@@ -49,6 +64,9 @@ using testing::SetArrayArgument;
 bool sdp_dynamic_change_hfp_version(const tSDP_ATTRIBUTE* p_attr,
                                     const RawAddress& remote_address);
 void hfp_fallback(bool& is_hfp_fallback, const tSDP_ATTRIBUTE* p_attr);
+
+void sdp_callback(const RawAddress& bd_addr, tSDP_RESULT result);
+tCONN_CB* find_ccb(uint16_t cid, uint8_t state);
 
 const char* test_flags_feature_disabled[] = {
     "INIT_dynamic_avrcp_version_enhancement=false",
@@ -233,9 +251,57 @@ uint16_t get_avrc_target_feature(tSDP_ATTRIBUTE* p_attr) {
   return feature;
 }
 
-class StackSdpUtilsTest : public ::testing::Test {
+class StackSdpMockAndFakeTest : public ::testing::Test {
  protected:
   void SetUp() override {
+    fake_osi_ = std::make_unique<test::fake::FakeOsi>();
+    test::mock::stack_l2cap_api::L2CA_ConnectReq2.body =
+        [](uint16_t psm, const RawAddress& p_bd_addr, uint16_t sec_level) {
+          return ++L2CA_ConnectReq2_cid;
+        };
+    test::mock::stack_l2cap_api::L2CA_DataWrite.body = [](uint16_t cid,
+                                                          BT_HDR* p_data) {
+      osi_free_and_reset((void**)&p_data);
+      return 0;
+    };
+    test::mock::stack_l2cap_api::L2CA_DisconnectReq.body = [](uint16_t cid) {
+      return true;
+    };
+    test::mock::stack_l2cap_api::L2CA_Register2.body =
+        [](uint16_t psm, const tL2CAP_APPL_INFO& p_cb_info, bool enable_snoop,
+           tL2CAP_ERTM_INFO* p_ertm_info, uint16_t my_mtu,
+           uint16_t required_remote_mtu, uint16_t sec_level) {
+          return 42;  // return non zero
+        };
+  }
+
+  void TearDown() override {
+    test::mock::stack_l2cap_api::L2CA_ConnectReq2 = {};
+    test::mock::stack_l2cap_api::L2CA_Register2 = {};
+    test::mock::stack_l2cap_api::L2CA_DataWrite = {};
+    test::mock::stack_l2cap_api::L2CA_DisconnectReq = {};
+  }
+  std::unique_ptr<test::fake::FakeOsi> fake_osi_;
+};
+
+class StackSdpInitTest : public StackSdpMockAndFakeTest {
+ protected:
+  void SetUp() override {
+    StackSdpMockAndFakeTest::SetUp();
+    sdp_init();
+    sdp_db = (tSDP_DISCOVERY_DB*)osi_malloc(BT_DEFAULT_BUFFER_SIZE);
+  }
+
+  void TearDown() override {
+    osi_free(sdp_db);
+    StackSdpMockAndFakeTest::TearDown();
+  }
+};
+
+class StackSdpUtilsTest : public StackSdpInitTest {
+ protected:
+  void SetUp() override {
+    StackSdpInitTest::SetUp();
     bluetooth::common::InitFlags::Load(hfp_test_flags_feature_disabled);
     bluetooth::common::InitFlags::Load(test_flags_feature_disabled);
     GetInterfaceToProfiles()->profileSpecific_HACK->AVRC_GetProfileVersion =
@@ -270,6 +336,7 @@ class StackSdpUtilsTest : public ::testing::Test {
 
     localIopMock.reset();
     localAvrcpVersionMock.reset();
+    StackSdpInitTest::TearDown();
   }
   bluetooth::manager::MockBtifConfigInterface btif_config_interface_;
 };
@@ -628,4 +695,239 @@ TEST_F(StackSdpUtilsTest, check_HFP_version_fallback_success) {
   hfp_fallback(is_hfp_fallback, &hfp_attr);
   ASSERT_EQ(hfp_attr.value_ptr[PROFILE_VERSION_POSITION],
             HFP_PROFILE_MINOR_VERSION_6);
+}
+
+TEST_F(StackSdpInitTest, sdp_service_search_request) {
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, nullptr));
+  int cid = L2CA_ConnectReq2_cid;
+  tCONN_CB* p_ccb = sdpu_find_ccb_by_cid(cid);
+  ASSERT_NE(p_ccb, nullptr);
+  ASSERT_EQ(p_ccb->con_state, SDP_STATE_CONN_SETUP);
+
+  tL2CAP_CFG_INFO cfg;
+  sdp_cb.reg_info.pL2CA_ConfigCfm_Cb(p_ccb->connection_id, 0, &cfg);
+
+  ASSERT_EQ(p_ccb->con_state, SDP_STATE_CONNECTED);
+
+  sdp_disconnect(p_ccb, SDP_SUCCESS);
+  sdp_cb.reg_info.pL2CA_DisconnectCfm_Cb(p_ccb->connection_id, 0);
+
+  ASSERT_EQ(p_ccb->con_state, SDP_STATE_IDLE);
+}
+
+TEST_F(StackSdpInitTest, sdp_service_search_request_queuing) {
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, nullptr));
+  const int cid = L2CA_ConnectReq2_cid;
+  tCONN_CB* p_ccb1 = find_ccb(cid, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb1, nullptr);
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONN_SETUP);
+
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, nullptr));
+  tCONN_CB* p_ccb2 = find_ccb(cid, SDP_STATE_CONN_PEND);
+  ASSERT_NE(p_ccb2, nullptr);
+  ASSERT_NE(p_ccb2, p_ccb1);
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONN_PEND);
+
+  tL2CAP_CFG_INFO cfg;
+  sdp_cb.reg_info.pL2CA_ConfigCfm_Cb(p_ccb1->connection_id, 0, &cfg);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONNECTED);
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONN_PEND);
+
+  p_ccb1->disconnect_reason = SDP_SUCCESS;
+  sdp_disconnect(p_ccb1, SDP_SUCCESS);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_IDLE);
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONNECTED);
+
+  sdp_disconnect(p_ccb2, SDP_SUCCESS);
+  sdp_cb.reg_info.pL2CA_DisconnectCfm_Cb(p_ccb2->connection_id, 0);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_IDLE);
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_IDLE);
+}
+
+TEST_F(StackSdpInitTest, sdp_service_search_request_queuing_race_condition) {
+  // start first request
+  ASSERT_TRUE(SDP_ServiceSearchRequest(addr, sdp_db, sdp_callback));
+  const int cid1 = L2CA_ConnectReq2_cid;
+  tCONN_CB* p_ccb1 = find_ccb(cid1, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb1, nullptr);
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONN_SETUP);
+
+  tL2CAP_CFG_INFO cfg;
+  sdp_cb.reg_info.pL2CA_ConfigCfm_Cb(p_ccb1->connection_id, 0, &cfg);
+
+  ASSERT_EQ(p_ccb1->con_state, SDP_STATE_CONNECTED);
+
+  sdp_disconnect(p_ccb1, SDP_SUCCESS);
+  sdp_cb.reg_info.pL2CA_DisconnectCfm_Cb(p_ccb1->connection_id, 0);
+
+  const int cid2 = L2CA_ConnectReq2_cid;
+  ASSERT_NE(cid1, cid2);  // The callback a queued a new request
+  tCONN_CB* p_ccb2 = find_ccb(cid2, SDP_STATE_CONN_SETUP);
+  ASSERT_NE(p_ccb2, nullptr);
+  // If race condition, this will be stuck in PEND
+  ASSERT_EQ(p_ccb2->con_state, SDP_STATE_CONN_SETUP);
+
+  sdp_disconnect(p_ccb2, SDP_SUCCESS);
+}
+
+TEST_F(StackSdpInitTest, sdp_disc_wait_text) {
+  std::vector<std::pair<tSDP_DISC_WAIT, std::string>> states = {
+      std::make_pair(SDP_DISC_WAIT_CONN, "SDP_DISC_WAIT_CONN"),
+      std::make_pair(SDP_DISC_WAIT_HANDLES, "SDP_DISC_WAIT_HANDLES"),
+      std::make_pair(SDP_DISC_WAIT_ATTR, "SDP_DISC_WAIT_ATTR"),
+      std::make_pair(SDP_DISC_WAIT_SEARCH_ATTR, "SDP_DISC_WAIT_SEARCH_ATTR"),
+      std::make_pair(SDP_DISC_WAIT_CANCEL, "SDP_DISC_WAIT_CANCEL"),
+  };
+  for (const auto& state : states) {
+    ASSERT_STREQ(state.second.c_str(), sdp_disc_wait_text(state.first).c_str());
+  }
+  auto unknown =
+      base::StringPrintf("UNKNOWN[%d]", std::numeric_limits<uint8_t>::max());
+  ASSERT_STREQ(unknown.c_str(),
+               sdp_disc_wait_text(static_cast<tSDP_DISC_WAIT>(
+                                      std::numeric_limits<uint8_t>::max()))
+                   .c_str());
+}
+
+TEST_F(StackSdpInitTest, sdp_state_text) {
+  std::vector<std::pair<tSDP_STATE, std::string>> states = {
+      std::make_pair(SDP_STATE_IDLE, "SDP_STATE_IDLE"),
+      std::make_pair(SDP_STATE_CONN_SETUP, "SDP_STATE_CONN_SETUP"),
+      std::make_pair(SDP_STATE_CFG_SETUP, "SDP_STATE_CFG_SETUP"),
+      std::make_pair(SDP_STATE_CONNECTED, "SDP_STATE_CONNECTED"),
+      std::make_pair(SDP_STATE_CONN_PEND, "SDP_STATE_CONN_PEND"),
+  };
+  for (const auto& state : states) {
+    ASSERT_STREQ(state.second.c_str(), sdp_state_text(state.first).c_str());
+  }
+  auto unknown = base::StringPrintf("UNKNOWN[%hhu]",
+                                    std::numeric_limits<std::uint8_t>::max());
+  ASSERT_STREQ(unknown.c_str(),
+               sdp_state_text(static_cast<tSDP_STATE>(
+                                  std::numeric_limits<std::uint8_t>::max()))
+                   .c_str());
+}
+
+TEST_F(StackSdpInitTest, sdp_flags_text) {
+  std::vector<std::pair<tSDP_DISC_WAIT, std::string>> flags = {
+      std::make_pair(SDP_FLAGS_IS_ORIG, "SDP_FLAGS_IS_ORIG"),
+      std::make_pair(SDP_FLAGS_HIS_CFG_DONE, "SDP_FLAGS_HIS_CFG_DONE"),
+      std::make_pair(SDP_FLAGS_MY_CFG_DONE, "SDP_FLAGS_MY_CFG_DONE"),
+  };
+  for (const auto& flag : flags) {
+    ASSERT_STREQ(flag.second.c_str(), sdp_flags_text(flag.first).c_str());
+  }
+  auto unknown =
+      base::StringPrintf("UNKNOWN[%hhu]", std::numeric_limits<uint8_t>::max());
+  ASSERT_STREQ(unknown.c_str(),
+               sdp_flags_text(static_cast<tSDP_DISC_WAIT>(
+                                  std::numeric_limits<uint8_t>::max()))
+                   .c_str());
+}
+
+TEST_F(StackSdpInitTest, sdp_status_text) {
+  std::vector<std::pair<tSDP_STATUS, std::string>> status = {
+      std::make_pair(SDP_SUCCESS, "SDP_SUCCESS"),
+      std::make_pair(SDP_INVALID_VERSION, "SDP_INVALID_VERSION"),
+      std::make_pair(SDP_INVALID_SERV_REC_HDL, "SDP_INVALID_SERV_REC_HDL"),
+      std::make_pair(SDP_INVALID_REQ_SYNTAX, "SDP_INVALID_REQ_SYNTAX"),
+      std::make_pair(SDP_INVALID_PDU_SIZE, "SDP_INVALID_PDU_SIZE"),
+      std::make_pair(SDP_INVALID_CONT_STATE, "SDP_INVALID_CONT_STATE"),
+      std::make_pair(SDP_NO_RESOURCES, "SDP_NO_RESOURCES"),
+      std::make_pair(SDP_DI_REG_FAILED, "SDP_DI_REG_FAILED"),
+      std::make_pair(SDP_DI_DISC_FAILED, "SDP_DI_DISC_FAILED"),
+      std::make_pair(SDP_NO_DI_RECORD_FOUND, "SDP_NO_DI_RECORD_FOUND"),
+      std::make_pair(SDP_ERR_ATTR_NOT_PRESENT, "SDP_ERR_ATTR_NOT_PRESENT"),
+      std::make_pair(SDP_ILLEGAL_PARAMETER, "SDP_ILLEGAL_PARAMETER"),
+      std::make_pair(HID_SDP_NO_SERV_UUID, "HID_SDP_NO_SERV_UUID"),
+      std::make_pair(HID_SDP_MANDATORY_MISSING, "HID_SDP_MANDATORY_MISSING"),
+      std::make_pair(SDP_NO_RECS_MATCH, "SDP_NO_RECS_MATCH"),
+      std::make_pair(SDP_CONN_FAILED, "SDP_CONN_FAILED"),
+      std::make_pair(SDP_CFG_FAILED, "SDP_CFG_FAILED"),
+      std::make_pair(SDP_GENERIC_ERROR, "SDP_GENERIC_ERROR"),
+      std::make_pair(SDP_DB_FULL, "SDP_DB_FULL"),
+      std::make_pair(SDP_CANCEL, "SDP_CANCEL"),
+  };
+  for (const auto& stat : status) {
+    ASSERT_STREQ(stat.second.c_str(), sdp_status_text(stat.first).c_str());
+  }
+  auto unknown =
+      base::StringPrintf("UNKNOWN[%hu]", std::numeric_limits<uint16_t>::max());
+  ASSERT_STREQ(unknown.c_str(),
+               sdp_status_text(static_cast<tSDP_STATUS>(
+                                   std::numeric_limits<uint16_t>::max()))
+                   .c_str());
+}
+
+TEST_F(StackSdpInitTest, sdpu_compare_uuid_with_attr_u16) {
+  tSDP_DISC_ATTR attr = {
+      .p_next_attr = nullptr,
+      .attr_id = 0,
+      .attr_len_type = 0,
+      .attr_value =
+          {
+              .v =
+                  {
+                      .u16 = 0x1234,
+                  },
+          },
+  };
+
+  bool is_valid{false};
+  bluetooth::Uuid uuid = bluetooth::Uuid::FromString("1234", &is_valid);
+
+  ASSERT_TRUE(is_valid);
+  ASSERT_TRUE(sdpu_compare_uuid_with_attr(uuid, &attr));
+}
+
+TEST_F(StackSdpInitTest, sdpu_compare_uuid_with_attr_u32) {
+  tSDP_DISC_ATTR attr = {
+      .p_next_attr = nullptr,
+      .attr_id = 0,
+      .attr_len_type = 0,
+      .attr_value =
+          {
+              .v =
+                  {
+                      .u32 = 0x12345678,
+                  },
+          },
+  };
+
+  bool is_valid{false};
+  bluetooth::Uuid uuid = bluetooth::Uuid::FromString("12345678", &is_valid);
+
+  ASSERT_TRUE(is_valid);
+  ASSERT_TRUE(sdpu_compare_uuid_with_attr(uuid, &attr));
+}
+
+TEST_F(StackSdpInitTest, sdpu_compare_uuid_with_attr_u128) {
+  tSDP_DISC_ATTR* p_attr =
+      (tSDP_DISC_ATTR*)calloc(1, sizeof(tSDP_DISC_ATTR) + 16);
+  tSDP_DISC_ATTR attr = {
+      .p_next_attr = nullptr,
+      .attr_id = 0,
+      .attr_len_type = 0,
+      .attr_value = {},
+  };
+
+  uint8_t data[] = {
+      0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0,
+      0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0,
+  };
+
+  memcpy(p_attr, &attr, sizeof(tSDP_DISC_ATTR));
+  memcpy(p_attr->attr_value.v.array, data, 16);
+
+  bool is_valid{false};
+  bluetooth::Uuid uuid = bluetooth::Uuid::FromString(
+      "12345678-9abc-def0-1234-56789abcdef0", &is_valid);
+
+  ASSERT_TRUE(is_valid);
+  ASSERT_TRUE(sdpu_compare_uuid_with_attr(uuid, p_attr));
+
+  free(p_attr);
 }
