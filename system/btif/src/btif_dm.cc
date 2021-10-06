71580845ceb9bb8a4c5b67b4665bd7b897e71f58
Chris Manton <cmanton@google.com>
2021-10-06 16:41:43 -0700

    legacy: Use get_btm_client_interface().eir.BTM_GetEirUuidList
    Bug: 193716447
    Tag: #refactor
    Test: gd/cert/run
    
    Change-Id: I2aea5e972a68a60493948cae39cc5bc57f155907

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -80,6 +80,7 @@
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/include/bt_octets.h"
+#include "stack/include/btm_client_interface.h"
 #include "stack_config.h"
 #include "types/raw_address.h"
 
@@ -1210,9 +1211,9 @@ static void btif_dm_search_devices_evt(tBTA_DM_SEARCH_EVT event,
 
       /* Check EIR for services */
       if (p_search_data->inq_res.p_eir) {
-        BTM_GetEirUuidList(p_search_data->inq_res.p_eir,
-                           p_search_data->inq_res.eir_len, Uuid::kNumBytes16,
-                           &num_uuids, uuid_list, max_num_uuid);
+        get_btm_client_interface().eir.BTM_GetEirUuidList(
+            p_search_data->inq_res.p_eir, p_search_data->inq_res.eir_len,
+            Uuid::kNumBytes16, &num_uuids, uuid_list, max_num_uuid);
       }
 
       {
