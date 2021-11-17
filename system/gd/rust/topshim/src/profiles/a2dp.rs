2985874f16d6e6f9873681f0c5f8a921a16bab18
Charlie Boutier <charliebout@google.com>
2021-11-17 14:11:20 +0000

    topshim: Add generic dispatcher and callback trait to avoid redundancy.
    Test: m bt_topshim_facade
    Change-Id: I033e829ca03e5e1a838ad2e21c344b918e37e97a

--- system/gd/rust/topshim/src/profiles/a2dp.rs
+++ system/gd/rust/topshim/src/profiles/a2dp.rs
@@ -5,6 +5,8 @@ use num_traits::cast::FromPrimitive;
 use std::sync::{Arc, Mutex};
 use topshim_macros::cb_variant;
 
+use crate::profiles::Dispatcher;
+
 #[derive(Debug, FromPrimitive, PartialEq, PartialOrd)]
 #[repr(u32)]
 pub enum BtavConnectionState {
@@ -241,9 +243,7 @@ pub enum A2dpCallbacks {
     MandatoryCodecPreferred(RawAddress),
 }
 
-pub struct A2dpCallbacksDispatcher {
-    pub dispatch: Box<dyn Fn(A2dpCallbacks) + Send>,
-}
+pub type A2dpCallbacksDispatcher = Dispatcher<A2dpCallbacks>;
 
 type A2dpCb = Arc<Mutex<A2dpCallbacksDispatcher>>;
 
@@ -328,9 +328,7 @@ pub enum A2dpSinkCallbacks {
     ConnectionState(RawAddress, BtavConnectionState),
 }
 
-pub struct A2dpSinkCallbacksDispatcher {
-    pub dispatch: Box<dyn Fn(A2dpSinkCallbacks) + Send>,
-}
+type A2dpSinkCallbacksDispatcher = Dispatcher<A2dpSinkCallbacks>;
 
 type A2dpSinkCb = Arc<Mutex<A2dpSinkCallbacksDispatcher>>;
 
