333b0461c277676051be6a786e78c573aa0a7633
xiaoshun.xu <xiaoshun.xu@mediatek.com>
2021-11-23 11:10:24 +0800

    Fix reconnect RC, but RC key missing
    [Description]
    Fix is designed to fix reconnect with RC, but RC key missing
    issue, if RC key missing, Host will remove bond RC
    
    [Test Report]
    RC press combo keys to forget device, device reconnect
    with RC and remove bond if RC key missing, repair can use
    
    Test: manual, the RC reconnects after forget device
    Bug: 133389433
    
    Change-Id: I5739a7537070c07d82fd5a0205610308f55ffcf2

--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -1530,6 +1530,11 @@ void btm_ble_link_encrypted(const RawAddress& bd_addr, uint8_t encr_enable) {
       btm_sec_dev_rec_cback_event(p_dev_rec, BTM_SUCCESS, true);
     else if (p_dev_rec->sec_flags & ~BTM_SEC_LE_LINK_KEY_KNOWN) {
       btm_sec_dev_rec_cback_event(p_dev_rec, BTM_FAILED_ON_SECURITY, true);
+    }
+    /* Add logic handle for KEY_MISSING */
+    else if (p_dev_rec->role_central &&
+             (p_dev_rec->sec_status == HCI_ERR_KEY_MISSING)) {
+      btm_sec_dev_rec_cback_event(p_dev_rec, BTM_ERR_KEY_MISSING, true);
     } else if (p_dev_rec->role_central)
       btm_sec_dev_rec_cback_event(p_dev_rec, BTM_ERR_PROCESSING, true);
   }
