789885063d9e19a4b5227844de5654fedbaf0c8f
En-Shuo Hsu <enshuo@google.com>
2021-12-02 00:01:08 +0800

    floss: Change A2dp to accept RawAddress
    Match the interface APIs as much as possible.
    
    Tag: #floss
    Bug: 20448828
    Test: m bt_topshim_facade & emerge build
    
    Change-Id: I06bbca2b74a0eaf34ac00c58e52309e26774b40a

--- system/gd/rust/topshim/facade/src/media_service.rs
+++ system/gd/rust/topshim/facade/src/media_service.rs
@@ -1,6 +1,6 @@
 //! Media service facade
 
-use bt_topshim::btif::BluetoothInterface;
+use bt_topshim::btif::{BluetoothInterface, RawAddress};
 use bt_topshim::profiles::a2dp::{
     A2dp, A2dpCallbacksDispatcher, A2dpSink, A2dpSinkCallbacksDispatcher,
 };
@@ -80,10 +80,17 @@ impl MediaService for MediaServiceImpl {
         sink: UnarySink<A2dpSourceConnectResponse>,
     ) {
         let a2dp = self.btif_a2dp.clone();
-        ctx.spawn(async move {
-            a2dp.lock().unwrap().connect(req.address.clone());
-            a2dp.lock().unwrap().set_active_device(req.address.clone());
-            sink.success(A2dpSourceConnectResponse::default()).await.unwrap();
-        })
+        if let Some(addr) = RawAddress::from_string(req.address.clone()) {
+            ctx.spawn(async move {
+                a2dp.lock().unwrap().connect(addr);
+                a2dp.lock().unwrap().set_active_device(addr);
+                sink.success(A2dpSourceConnectResponse::default()).await.unwrap();
+            })
+        } else {
+            sink.fail(RpcStatus::with_message(
+                RpcStatusCode::INVALID_ARGUMENT,
+                format!("Invalid Request Address: {}", req.address),
+            ));
+        }
     }
 }
