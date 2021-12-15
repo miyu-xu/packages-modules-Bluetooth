79e720cc220607ae91e9bc8eee364fcd1b4e4486
David Duarte <licorne@google.com>
2021-12-15 16:23:40 +0000

    RootCanal: Partially implement authentication procedure
    Bug: 209803517
    Test: m root-canal
    Change-Id: Id3417082c212d247d13c54971612ac34e04bf4a5

--- system/vendor_libs/test_vendor_lib/model/controller/link_layer_controller.h
+++ system/vendor_libs/test_vendor_lib/model/controller/link_layer_controller.h
@@ -441,6 +441,10 @@ class LinkLayerController {
   void IncomingRemoteNameRequest(model::packets::LinkLayerPacketView packet);
   void IncomingRemoteNameRequestResponse(
       model::packets::LinkLayerPacketView packet);
+  void IncomingAuthenticationRequest(
+      model::packets::LinkLayerPacketView packet);
+  void IncomingAuthenticationResponse(
+      model::packets::LinkLayerPacketView packet);
 
   void IncomingEScoConnectionRequest(
       model::packets::LinkLayerPacketView packet);
