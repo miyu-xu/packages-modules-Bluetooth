64c6c47406976ecb4fb1014b697b6a78ef981467
Pixel and Ecosystems Test <noreply@google.com>
2021-12-16 00:29:27 -0800

    Add Type annotations and fix import statements (Part 8)
    go/python-style#type-annotations
    
    BUILD:
    http://sponge2/1f83deef-b333-43ca-8556-b76eb9d98c2f (three_button_dongle.py)
    http://sponge2/190fa64c-375e-4aea-bb7e-290b34843082 (voyager_8200_uc.py)
    http://sponge2/b8649302-9a53-4f0f-a0f8-1837eab4abd9 (wearable_uart_device.py)
    http://sponge2/9c599918-ba9c-4da9-bc51-ff0ac25b9f72 (android_bluetooth_client_decorator.py)
    http://sponge2/79963bb8-1a41-4a3d-8b5a-3a94cb83f229 (android_bluetooth_client_test_decorator.py)
    PiperOrigin-RevId: 416741434
    Change-Id: I399e236385c9f74e075c7f73202c9363fd246dae

--- system/blueberry/decorators/android_bluetooth_client_test_decorator.py
+++ system/blueberry/decorators/android_bluetooth_client_test_decorator.py
@@ -5,20 +5,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from mobly.controllers.android_device import AndroidDevice
+from typing import Any
+
+from mobly.controllers import android_device
 
 
-class AndroidBluetoothClientTestDecorator(AndroidDevice):
+class AndroidBluetoothClientTestDecorator(android_device.AndroidDevice):
   """A class used to test Blueberry's BT Client Profile decoration."""
 
-  def __init__(self, ad):
+  def __init__(self, ad: android_device.AndroidDevice) -> None:
     self._ad = ad
-    if not isinstance(self._ad, AndroidDevice):
+    if not isinstance(self._ad, android_device.AndroidDevice):
       raise TypeError('Must apply AndroidBluetoothClientTestDecorator to an '
                       'AndroidDevice')
 
-  def __getattr__(self, name):
+  def __getattr__(self, name: str) -> Any:
     return getattr(self._ad, name)
 
-  def test_decoration(self):
+  def test_decoration(self) -> str:
     return 'I make this device fancy!'
