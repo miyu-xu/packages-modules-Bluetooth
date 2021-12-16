abac40f2a7226cae2bad859edcefac34368b57ec
Pixel and Ecosystems Test <noreply@google.com>
2021-12-16 08:36:12 -0800

    Let Watch play media to avoid triggering connection switching in `setup_test` stage
    Test:
    http://sponge2/3e530c82-8418-409a-a203-e594bdbb8b0e
    PiperOrigin-RevId: 416818671
    Change-Id: I9507a667f07eb7eb23a7fc9d337d680900f47bf4

--- system/blueberry/tests/triangle/connection_switching_test.py
+++ system/blueberry/tests/triangle/connection_switching_test.py
@@ -4,6 +4,7 @@ import logging
 import time
 
 from mobly import test_runner
+from blueberry.utils import bt_audio_utils
 from blueberry.utils import triangle_base_test as base_test
 from blueberry.utils import triangle_constants
 
@@ -22,14 +23,21 @@ class ConnectionSwitchingTest(base_test.TriangleBaseTest):
     self.pair_and_connect_phone_to_headset()
     self.pair_and_connect_phone_to_watch()
     self.pair_and_connect_watch_to_headset()
+    sine_wave_file = bt_audio_utils.generate_sine_wave_to_device(self.watch)[0]
+    self.audio_file_on_watch = f'file://{sine_wave_file}'
 
   def setup_test(self):
     """Makes sure that Headset is connected to Watch instead of Phone."""
     super().setup_test()
     self.phone.disconnect_bluetooth(self.headset.mac_address)
+    # Play media to avoid Headset connection is switched.
+    self.watch.sl4a.mediaPlayOpen(self.audio_file_on_watch)
+    self.watch.sl4a.mediaPlayStart()
     self.watch.connect_bluetooth(self.headset.mac_address)
     self.assert_headset_a2dp_connection(connected=False, device=self.phone)
     self.assert_headset_hsp_connection(connected=False, device=self.phone)
+    self.watch.sl4a.mediaPlayStop()
+    self.watch.sl4a.mediaPlayClose()
 
   def test_trigger_connection_switching_when_headset_powered_on(self):
     """Test for triggering connection switching when Headset is powered on.
