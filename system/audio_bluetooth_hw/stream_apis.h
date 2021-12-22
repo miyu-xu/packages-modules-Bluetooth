9a91f9a1aa4dba8858b6bcb5072543ca715c8902
Josh Wu <joshwu@google.com>
2021-12-22 01:25:59 -0800

    Audio: Migrate to AIDL API
    Tag: #refactor
    Bug: 203490261
    Test: atest BluetoothInstrumentationTests
    Change-Id: I606fe51ce2e2c070646ce81966e69e82893f36a4

--- system/audio_bluetooth_hw/stream_apis.h
+++ system/audio_bluetooth_hw/stream_apis.h
@@ -18,9 +18,11 @@
 
 #include <hardware/audio.h>
 #include <system/audio.h>
+
 #include <list>
 
 #include "device_port_proxy.h"
+#include "device_port_proxy_hidl.h"
 
 constexpr unsigned int kBluetoothDefaultSampleRate = 44100;
 constexpr audio_format_t kBluetoothDefaultAudioFormatBitsPerSample =
@@ -50,7 +52,8 @@ struct BluetoothStreamOut {
   // Must be the first member so it can be cast from audio_stream
   // or audio_stream_out pointer
   audio_stream_out stream_out_{};
-  ::android::bluetooth::audio::BluetoothAudioPortOut bluetooth_output_;
+  std::unique_ptr<::android::bluetooth::audio::BluetoothAudioPort>
+      bluetooth_output_;
   int64_t last_write_time_us_;
   // Audio PCM Configs
   uint32_t sample_rate_;
@@ -81,7 +84,8 @@ struct BluetoothStreamIn {
   // Must be the first member so it can be cast from audio_stream
   // or audio_stream_in pointer
   audio_stream_in stream_in_;
-  ::android::bluetooth::audio::BluetoothAudioPortIn bluetooth_input_;
+  std::unique_ptr<::android::bluetooth::audio::BluetoothAudioPort>
+      bluetooth_input_;
   int64_t last_read_time_us_;
   // Audio PCM Configs
   uint32_t sample_rate_;
