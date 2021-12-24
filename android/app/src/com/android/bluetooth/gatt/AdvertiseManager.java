d954d0fff9975d22aeb7e0a84c80ab815d30983f
Vance Yu <vanceyu@google.com>
2021-12-24 05:37:26 +0000

    Add Bluetooth LE advertising logger
    There is no useful information for a BLE advertiser in bugreports. We faced some related issues so add a logger to record infomation for investigating issues.
    
    Tag: #stability
    Bug: 207808421
    Test: adb shell dumpsys bluetooth_manager
    Change-Id: I383b24b9fe18966915f0716a97f1675cbd608790

--- android/app/src/com/android/bluetooth/gatt/AdvertiseManager.java
+++ android/app/src/com/android/bluetooth/gatt/AdvertiseManager.java
@@ -157,10 +157,18 @@ class AdvertiseManager {
         if (status == 0) {
             entry.setValue(
                     new AdvertiserInfo(advertiserId, entry.getValue().deathRecipient, callback));
+
+            mService.mAdvertiserMap.setAdvertiserIdByRegId(regId, advertiserId);
         } else {
             IBinder binder = entry.getKey();
             binder.unlinkToDeath(entry.getValue().deathRecipient, 0);
             mAdvertisers.remove(binder);
+
+            AppAdvertiseStats stats = mService.mAdvertiserMap.getAppAdvertiseStatsById(regId);
+            if (stats != null) {
+                stats.recordAdvertiseStop();
+            }
+            mService.mAdvertiserMap.removeAppAdvertiseStats(regId);
         }
 
         callback.onAdvertisingSetStarted(advertiserId, txPower, status);
@@ -203,14 +211,19 @@ class AdvertiseManager {
             byte[] periodicDataBytes =
                     AdvertiseHelper.advertiseDataToBytes(periodicData, deviceName);
 
-        int cbId = --sTempRegistrationId;
-        mAdvertisers.put(binder, new AdvertiserInfo(cbId, deathRecipient, callback));
+            int cbId = --sTempRegistrationId;
+            mAdvertisers.put(binder, new AdvertiserInfo(cbId, deathRecipient, callback));
 
-        if (DBG) {
-            Log.d(TAG, "startAdvertisingSet() - reg_id=" + cbId + ", callback: " + binder);
-        }
-        startAdvertisingSetNative(parameters, advDataBytes, scanResponseBytes, periodicParameters,
-                periodicDataBytes, duration, maxExtAdvEvents, cbId);
+            if (DBG) {
+                Log.d(TAG, "startAdvertisingSet() - reg_id=" + cbId + ", callback: " + binder);
+            }
+
+            mService.mAdvertiserMap.add(cbId, callback, mService);
+            mService.mAdvertiserMap.recordAdvertiseStart(cbId, parameters, advertiseData,
+                    scanResponse, periodicParameters, periodicData, duration, maxExtAdvEvents);
+
+            startAdvertisingSetNative(parameters, advDataBytes, scanResponseBytes,
+                    periodicParameters, periodicDataBytes, duration, maxExtAdvEvents, cbId);
 
         } catch (IllegalArgumentException e) {
             try {
@@ -276,6 +289,8 @@ class AdvertiseManager {
         } catch (RemoteException e) {
             Log.i(TAG, "error sending onAdvertisingSetStopped callback", e);
         }
+
+        mService.mAdvertiserMap.recordAdvertiseStop(advertiserId);
     }
 
     void enableAdvertisingSet(int advertiserId, boolean enable, int duration, int maxExtAdvEvents) {
@@ -285,6 +300,9 @@ class AdvertiseManager {
             return;
         }
         enableAdvertisingSetNative(advertiserId, enable, duration, maxExtAdvEvents);
+
+        mService.mAdvertiserMap.enableAdvertisingSet(advertiserId,
+                enable, duration, maxExtAdvEvents);
     }
 
     void setAdvertisingData(int advertiserId, AdvertiseData data) {
@@ -305,6 +323,8 @@ class AdvertiseManager {
                 Log.e(TAG, "Failed to callback:" + Log.getStackTraceString(exception));
             }
         }
+
+        mService.mAdvertiserMap.setAdvertisingData(advertiserId, data);
     }
 
     void setScanResponseData(int advertiserId, AdvertiseData data) {
@@ -325,6 +345,8 @@ class AdvertiseManager {
                 Log.e(TAG, "Failed to callback:" + Log.getStackTraceString(exception));
             }
         }
+
+        mService.mAdvertiserMap.setScanResponseData(advertiserId, data);
     }
 
     void setAdvertisingParameters(int advertiserId, AdvertisingSetParameters parameters) {
@@ -334,6 +356,8 @@ class AdvertiseManager {
             return;
         }
         setAdvertisingParametersNative(advertiserId, parameters);
+
+        mService.mAdvertiserMap.setAdvertisingParameters(advertiserId, parameters);
     }
 
     void setPeriodicAdvertisingParameters(int advertiserId,
@@ -344,6 +368,8 @@ class AdvertiseManager {
             return;
         }
         setPeriodicAdvertisingParametersNative(advertiserId, parameters);
+
+        mService.mAdvertiserMap.setPeriodicAdvertisingParameters(advertiserId, parameters);
     }
 
     void setPeriodicAdvertisingData(int advertiserId, AdvertiseData data) {
@@ -364,6 +390,8 @@ class AdvertiseManager {
                 Log.e(TAG, "Failed to callback:" + Log.getStackTraceString(exception));
             }
         }
+
+        mService.mAdvertiserMap.setPeriodicAdvertisingData(advertiserId, data);
     }
 
     void setPeriodicAdvertisingEnable(int advertiserId, boolean enable) {
@@ -473,6 +501,11 @@ class AdvertiseManager {
 
         IAdvertisingSetCallback callback = entry.getValue().callback;
         callback.onPeriodicAdvertisingEnabled(advertiserId, enable, status);
+
+        AppAdvertiseStats stats = mService.mAdvertiserMap.getAppAdvertiseStatsById(advertiserId);
+        if (stats != null) {
+            stats.onPeriodicAdvertiseEnabled(enable);
+        }
     }
 
     static {
