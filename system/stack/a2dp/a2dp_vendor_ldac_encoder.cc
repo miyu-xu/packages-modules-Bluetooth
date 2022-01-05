65c93c41f87cadeaa63b94d8ea42c022d8323abf
Dorin Drimus <dorindrimus@google.com>
2022-01-05 17:02:33 +0000

    Limit the number of frames for LDAC
    Similar with how it's done for SBC, limit the number of frames sent from the LDAC encoder for Bluetooth A2DP. This will prevent underruns after play media resume.
    
    Change-Id: I3c22549e9c1d1ad97147679d6dcba5e3f560221f
    Test: N/A, manual
    Bug: 204161080

--- system/stack/a2dp/a2dp_vendor_ldac_encoder.cc
+++ system/stack/a2dp/a2dp_vendor_ldac_encoder.cc
@@ -540,8 +540,7 @@ void a2dp_vendor_ldac_send_frames(uint64_t timestamp_us) {
 static void a2dp_ldac_get_num_frame_iteration(uint8_t* num_of_iterations,
                                               uint8_t* num_of_frames,
                                               uint64_t timestamp_us) {
-  uint32_t result = 0;
-  uint8_t nof = 0;
+  uint32_t nof = 0;
   uint8_t noi = 1;
 
   uint32_t pcm_bytes_per_frame =
@@ -561,14 +560,22 @@ static void a2dp_ldac_get_num_frame_iteration(uint8_t* num_of_iterations,
       (float)a2dp_ldac_encoder_cb.ldac_feeding_state.bytes_per_tick * us_this_tick /
       (A2DP_LDAC_ENCODER_INTERVAL_MS * 1000);
 
-  result =
-      a2dp_ldac_encoder_cb.ldac_feeding_state.counter / pcm_bytes_per_frame;
-  a2dp_ldac_encoder_cb.ldac_feeding_state.counter -=
-      result * pcm_bytes_per_frame;
-  nof = result;
+  nof = a2dp_ldac_encoder_cb.ldac_feeding_state.counter / pcm_bytes_per_frame;
 
-  LOG_VERBOSE("%s: effective num of frames %u, iterations %u", __func__, nof,
-              noi);
+  if (nof > MAX_PCM_FRAME_NUM_PER_TICK) {
+    LOG_WARN("%s: limiting frames to be sent from %d to %d", __func__,
+            nof, MAX_PCM_FRAME_NUM_PER_TICK);
+
+    // Update the stats
+    size_t delta = nof - MAX_PCM_FRAME_NUM_PER_TICK;
+    a2dp_ldac_encoder_cb.stats.media_read_total_dropped_frames += delta;
+
+    nof = MAX_PCM_FRAME_NUM_PER_TICK;
+  }
+
+  a2dp_ldac_encoder_cb.ldac_feeding_state.counter -= nof * pcm_bytes_per_frame;
+
+  LOG_VERBOSE("%s: effective num of frames %u, iterations %u", __func__, nof, noi);
 
   *num_of_frames = nof;
   *num_of_iterations = noi;
