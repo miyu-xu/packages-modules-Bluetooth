ebd2a4b38367ccb48e1bb545507dbec07849ca1f
Chienyuan <chienyuanhuang@google.com>
2022-01-10 20:24:33 +0800

    [Test] enable address consolidation
    Tag: #refactor
    Bug: 197044261
    Test: manual
    Change-Id: I1797edb1bdb1afe61125a7eb9088e9fa46bcbb61

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -95,7 +95,7 @@ const Uuid UUID_VC = Uuid::FromString("1844");
 const Uuid UUID_CSIS = Uuid::FromString("1846");
 const Uuid UUID_LE_AUDIO = Uuid::FromString("184E");
 const Uuid UUID_LE_MIDI = Uuid::FromString("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
-const bool enable_address_consolidate = false;  // TODO remove
+const bool enable_address_consolidate = true;  // TODO remove
 
 #define COD_MASK 0x07FF
 
