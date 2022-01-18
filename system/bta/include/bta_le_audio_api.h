b29dcef9840c97758f38d0958ec1324fa8e54e95
Jakub Tyszkowski <jakub.tyszkowski@codecoup.pl>
2022-01-18 18:38:06 +0000

    le_audio: Move set configurations
    This moves the set configurations out of the
    le_audio_types.h, and hides them behind the set configuration
    provider.
    
    This abstraction will allow for the next step, which is moving
    from the hard-codded configurations to loading them from the
    external sources, eg. XML/JSON files.
    
    Bug: 150670922
    Tag: #feature
    Sponsor: jpawlowski@
    Test: atest --host bluetooth_le_audio_test
    Change-Id: I6435f6259d4d098da8c298a358c2b41dfc38ac10

--- system/bta/include/bta_le_audio_api.h
+++ system/bta/include/bta_le_audio_api.h
@@ -40,7 +40,6 @@ class LeAudioClient {
       base::Closure initCb, base::Callback<bool()> hal_2_1_verifier,
       const std::vector<bluetooth::le_audio::btle_audio_codec_config_t>&
           offloading_preference);
-  static void Cleanup(void);
   static LeAudioClient* Get(void);
   static void DebugDump(int fd);
 
@@ -57,4 +56,7 @@ class LeAudioClient {
   virtual std::vector<RawAddress> GetGroupDevices(const int group_id) = 0;
   static void AddFromStorage(const RawAddress& addr, bool autoconnect);
   static bool IsLeAudioClientRunning();
+
+  static void InitializeAudioSetConfigurationProvider(void);
+  static void CleanupAudioSetConfigurationProvider(void);
 };
