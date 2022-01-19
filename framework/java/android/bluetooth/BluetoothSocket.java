5c48274ae9a69abea5f252284d4511450f9a74e7
William Escande <wescande@google.com>
2022-01-19 19:11:16 +0100

    Use the new systemApi createConnectedLocalSocket
    Test: atest BluetoothInstrumentationTests
    Test: atest frameworks/base/core/tests/coretests/src/android/net/LocalSocketTest.java
    Bug: 200200870
    Tag: #refactor
    Change-Id: I7a7280b54bac5cf01098556a70c5662269f3bb15

--- framework/java/android/bluetooth/BluetoothSocket.java
+++ framework/java/android/bluetooth/BluetoothSocket.java
@@ -266,7 +266,7 @@ public final class BluetoothSocket implements Closeable {
         }
 
         as.mPfd = ParcelFileDescriptor.dup(fds[0]);
-        as.mSocket = LocalSocket.createConnectedLocalSocket(fds[0]);
+        as.mSocket = LocalSocket.createConnectedLocalSocket(as.mPfd);
         as.mSocketIS = as.mSocket.getInputStream();
         as.mSocketOS = as.mSocket.getOutputStream();
         as.mAddress = remoteAddr;
@@ -406,8 +406,7 @@ public final class BluetoothSocket implements Closeable {
                 if (DBG) Log.d(TAG, "connect(), SocketState: " + mSocketState + ", mPfd: " + mPfd);
                 if (mSocketState == SocketState.CLOSED) throw new IOException("socket closed");
                 if (mPfd == null) throw new IOException("bt socket connect failed");
-                FileDescriptor fd = mPfd.getFileDescriptor();
-                mSocket = LocalSocket.createConnectedLocalSocket(fd);
+                mSocket = LocalSocket.createConnectedLocalSocket(mPfd);
                 mSocketIS = mSocket.getInputStream();
                 mSocketOS = mSocket.getOutputStream();
             }
@@ -466,7 +465,7 @@ public final class BluetoothSocket implements Closeable {
                 }
 
                 if (DBG) Log.d(TAG, "bindListen(), Create LocalSocket");
-                mSocket = LocalSocket.createConnectedLocalSocket(fd);
+                mSocket = LocalSocket.createConnectedLocalSocket(mPfd);
                 if (DBG) Log.d(TAG, "bindListen(), new LocalSocket.getInputStream()");
                 mSocketIS = mSocket.getInputStream();
                 mSocketOS = mSocket.getOutputStream();
