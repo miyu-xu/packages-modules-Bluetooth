cd1bcceb1f4235b88ead0b5005e2330ddd13f3b0
Pixel and Ecosystems Test <noreply@google.com>
2022-01-25 16:48:59 -0800

    Blueberry project import generated by Copybara.
    Test: Manual test
    PiperOrigin-RevId: 424211739
    Change-Id: I7445254e702f8aa8227230fde3dd3724a30f8a19

--- system/blueberry/utils/ui_pages/fitbit_companion/__init__.py
+++ system/blueberry/utils/ui_pages/fitbit_companion/__init__.py
@@ -1,16 +1,33 @@
 """Gets context of Fitbit Companion App."""
 
+import time
+from typing import Any, Callable
+
+import immutabledict
+from mobly import asserts
+from mobly import signals
 from mobly.controllers import android_device
 
+# Internal import
+# Internal import
+from blueberry.controllers import fitbit_tracker_device
 from blueberry.utils.ui_pages import errors
 from blueberry.utils.ui_pages import ui_core
+from blueberry.utils.ui_pages import ui_node
 from blueberry.utils.ui_pages.fitbit_companion import account_pages
 from blueberry.utils.ui_pages.fitbit_companion import constants
 from blueberry.utils.ui_pages.fitbit_companion import context
 from blueberry.utils.ui_pages.fitbit_companion import other_pages
 from blueberry.utils.ui_pages.fitbit_companion import pairing_pages
 
-# Internal import
+MODEL_TO_PRODUCT_NAME_MAPPING = immutabledict.immutabledict({
+    'Buzz': 'Luxe',
+    'Luxe': 'Luxe',
+    'Morgan': 'Charge 5',
+    'Charge_5': 'Charge 5',
+})
+
+_INVALID_PAIRING_CODE_MESSAGE = "Sorry, this code isn't valid."
 
 
 def get_context(ad: android_device.AndroidDevice,
@@ -103,18 +120,18 @@ def go_google_play_page(ctx: context.Context) -> None:
     retry_intervals=retry.FuzzedExponentialIntervals(
         initial_delay_sec=1, num_retries=5, factor=1.1))
 def _click_unpair_button_on_device(ctx: context.Context,
-                                   device_name: str) -> None:
+                                   device_node: ui_node.UINode) -> None:
   """Unpairs given device.
 
   Args:
     ctx: Context object of Fitbit Companion App.
-    device_name: Name of Fitbit device to be unpaired.
+    device_node: Node of Fitbit device to be unpaired.
   """
-  ctx.page.click(device_name)
+  ctx.page.click(device_node)
   ctx.expect_pages([
-      pairing_pages.PairAndLinkPage,
-      account_pages.PairedDeviceDetailPage,
-      other_pages.ConfirmLocationPermissionPopup])
+      pairing_pages.PairAndLinkPage, account_pages.PairedDeviceDetailPage,
+      other_pages.ConfirmLocationPermissionPopup
+  ])
   if ctx.is_page(pairing_pages.PairAndLinkPage):
     ctx.page.cancel()
   elif ctx.is_page(other_pages.ConfirmLocationPermissionPopup):
@@ -142,10 +159,223 @@ def remove_all_paired_devices(ctx: context.Context) -> int:
   """
   removed_count = 0
   ctx.go_page(account_pages.AccountPage)
-  paired_devices = ctx.page.get_paired_devices()
-  while paired_devices:
-    _click_unpair_button_on_device(ctx, paired_devices[0])
+  paired_device_nodes = ctx.page.get_paired_devices()
+  while paired_device_nodes:
+    _click_unpair_button_on_device(ctx, paired_device_nodes[0])
     removed_count += 1
-    paired_devices = ctx.page.get_paired_devices()
+    paired_device_nodes = ctx.page.get_paired_devices()
 
   return removed_count
+
+
+def pair_pin_dec(
+    func: Callable[[context.Context, fitbit_tracker_device.FitbitTracker], None]
+) -> Callable[[context.Context, fitbit_tracker_device.FitbitTracker], None]:
+
+  def wrapper(*args, **kw):
+    device = args[1]
+    device.log.debug('Starting the pair-pin subscription...')
+    try:
+      device._device.bt.pair_pin_start()
+    except fitbit_tracker_cli.CliError as err:
+      if err and 'Already subscribed on pubsub' in err.output[0]:
+        device.log.warning('Fitbit device already subscribed on pubsub!')
+      else:
+        raise err
+
+    try:
+      func(*args, **kw)
+    finally:
+      device.log.debug('Stopping the pair-pin subscription...')
+      device._device.bt.pair_pin_stop()
+
+  return wrapper
+
+
+def _trigger_pairing(ctx: context.Context, fitbit_prod_name: str) -> None:
+  """Triggers the pairing process by Fitbit production name.
+
+  Args:
+    ctx: Context object of Fitbit Companion App.
+    fitbit_prod_name: Fitbit product name to select.
+  """
+  ctx.go_page(account_pages.AccountPage)
+
+  def _eval_existence_of_fitbit_product_name(node, name=fitbit_prod_name):
+    return name in node.text
+
+  ctx.log.info('Selecting device=%s...', fitbit_prod_name)
+  ctx.page.add_device()
+  ctx.expect_page(
+      pairing_pages.ChooseTrackerPage,
+      node_eval=_eval_existence_of_fitbit_product_name)
+  ctx.page.select_device(fitbit_prod_name)
+  ctx.page.confirm()
+
+  ctx.log.info('Accept pairing privacy requirement...')
+  ctx.expect_page(pairing_pages.PairPrivacyConfirmPage)
+  ctx.page.accept()
+  ctx.expect_page(pairing_pages.ConfirmChargePage)
+  ctx.page.next()
+  if ctx.is_page(other_pages.LocationDisabledPage):
+    # Optional page when you are required to enable location
+    # permission for Fitbit device.
+    ctx.log.info('Enabling location permission...')
+    ctx.page.enable()
+    ctx.expect_page(other_pages.SettingLocation)
+    ctx.page.set(True)
+    ctx.page.back()
+
+
+def _handle_pairing_4digit_page(ctx: context.Context,
+                                device: fitbit_tracker_device.FitbitTracker,
+                                max_pairing_retries: int = 10) -> None:
+  """Handles the input of 4-digit pairing pins.
+
+  Args:
+    ctx: Context object of Fitbit Companion App.
+    device: Fitbit tracker device.
+    max_pairing_retries: Number of retry in pairing step.
+
+  Raises:
+    signals.TestError: Fail to input 4-digit pairing pins.
+  """
+  ctx.expect_page(pairing_pages.Pairing4DigitPage, wait_sec=300)
+  pins = device.pair_pin_show()
+  ctx.log.info('Pairing pins=%s...', pins)
+  ctx.page.input_pins(pins)
+  pair_retry = 0
+  while (ctx.is_page(pairing_pages.Pairing4DigitPage) and
+         ctx.page.get_node_by_func(
+             lambda n: _INVALID_PAIRING_CODE_MESSAGE in n.text) is not None):
+    pair_retry += 1
+    if pair_retry >= max_pairing_retries:
+      raise signals.TestError(
+          f'Failed in pairing pins matching after {pair_retry} tries!')
+    pins = device.pair_pin_show()
+    ctx.log.warning('Retrying on pairing pins=%s...', pins)
+    ctx.page.input_pins(pins)
+    time.sleep(1)
+
+
+def _handle_post_pairing_flow(ctx: context.Context,
+                              max_pairing_retries: int = 10) -> None:
+  """Handles the pages after pairing process.
+
+  Args:
+    ctx: Context object of Fitbit Companion App.
+    max_pairing_retries: Number of retry in pairing step.
+
+  Raises:
+    signals.TestError: Fail complete the post pairing flow.
+  """
+  ctx.log.info('Entering post pairing flow...')
+  pair_retry = 0
+  while True:
+    ctx.expect_pages([
+        pairing_pages.PairRetryPage,
+        pairing_pages.PairAndLinkPage,
+        pairing_pages.PairingIntroPage,
+        pairing_pages.PairingConfirmPage,
+        pairing_pages.CancelPairPage,
+        pairing_pages.CancelPair2Page,
+        other_pages.AllowNotification,
+        other_pages.LinkConfirmPage,
+    ],
+                     wait_sec=90)
+    if ctx.is_page(pairing_pages.PairingConfirmPage):
+      ctx.log.info('Accept pairing confirm page...')
+      ctx.page.confirm()
+    elif ctx.is_page(pairing_pages.PairRetryPage):
+      ctx.log.warning('Skip pair retry page...')
+      ctx.back()
+    elif ctx.is_page(pairing_pages.PairAndLinkPage):
+      ctx.log.warning('Skip pair and link page...')
+      ctx.page.cancel()
+    elif (ctx.is_page(pairing_pages.CancelPairPage) or
+          ctx.is_page(pairing_pages.CancelPair2Page)):
+      ctx.log.warning('Skip pair-cancel page...')
+      ctx.page.yes()
+    elif ctx.is_page(other_pages.AllowNotification):
+      ctx.log.warning('Allow notification page...')
+      ctx.page.allow()
+    elif ctx.is_page(other_pages.LinkConfirmPage):
+      ctx.log.warning('Allow Fitbit to manage device page...')
+      ctx.page.ok()
+    elif ctx.is_page(pairing_pages.PairingIntroPage):
+      ctx.log.info('Passing through Fitbit introduction pages...')
+      break
+
+    pair_retry += 1
+    if pair_retry >= max_pairing_retries:
+      raise signals.TestError(
+          f'Failed in pairing process after {pair_retry} tries!')
+
+  ctx.expect_page(pairing_pages.PairingIntroPage)
+  while ctx.is_page(pairing_pages.PairingIntroPage):
+    ctx.page.next()
+
+  ctx.expect_pages([
+      pairing_pages.PremiumPage, other_pages.PurchaseFail,
+      account_pages.AccountPage
+  ])
+
+  if ctx.is_page(pairing_pages.PremiumPage):
+    # Preminum page is optional.
+    ctx.page.done()
+  elif ctx.is_page(other_pages.PurchaseFail):
+    # Optional page observed during manual pairing experiment.
+    ctx.page.ok()
+
+  ctx.expect_page(account_pages.AccountPage)
+  ctx.log.info('Completing pairing process and start evaluation process...')
+
+
+@pair_pin_dec
+def pair_device(ctx: context.Context,
+                device: fitbit_tracker_device.FitbitTracker,
+                max_pairing_retries: int = 10) -> None:
+  """Pairs with given Fitbit tracker device.
+
+  Args:
+    ctx: Context object of Fitbit Companion App.
+    device: Fitbit tracker device.
+    max_pairing_retries: Number of retry in pairing step.
+
+  Raises:
+    fitbit_tracker_cli.CliError: Fail in device's CLI operation.
+    signals.TestError: Fail in pairing process.
+  """
+  ctx.regr_page_call(pairing_pages.BTPermissionRequestPopup, 'ok')
+  ctx.regr_page_call(pairing_pages.CancelPairPage, 'yes')
+  ctx.regr_page_call(other_pages.FitbitLocationPermissionPopup, 'back')
+  ctx.regr_page_call(ui_core.NonePage, 'swipe_left')
+  ctx.regr_page_call(pairing_pages.PairRetryPage, 'retry')
+  ctx.regr_page_call(other_pages.NetworkOpFailPage, 'cancel')
+  ctx.regr_page_call(other_pages.PlayfulPage, 'skip')
+  ctx.regr_page_call(other_pages.LinkConfirmPage, 'ok')
+  ctx.regr_page_call(other_pages.PurchaseFail, 'ok')
+  ctx.regr_page_call(pairing_pages.PremiumPage, 'done')
+  ctx.regr_page_call(other_pages.PurchaseFail, 'ok')
+  ctx.regr_page_call(pairing_pages.UpdateDevicePage, 'update_later')
+
+  ctx.go_page(account_pages.AccountPage)
+  fitbit_prod_name = MODEL_TO_PRODUCT_NAME_MAPPING[device.model]
+  paired_device_names = [node.text for node in ctx.page.get_paired_devices()]
+  if fitbit_prod_name in paired_device_names:
+    ctx.log.info('Device %s is already paired!', device)
+    return
+
+  # Select Fitbit device to trigger pairing process.
+  _trigger_pairing(ctx, fitbit_prod_name)
+
+  # Pairing process will begin with input of 4 digit pins.
+  _handle_pairing_4digit_page(ctx, device, max_pairing_retries)
+
+  # Handle post flow after pairing process.
+  _handle_post_pairing_flow(ctx, max_pairing_retries)
+
+  paired_device_nodes = ctx.page.get_paired_devices()
+  asserts.assert_true(
+      fitbit_prod_name in [node.text for node in paired_device_nodes],
+      f'Unexpected paired device nodes={paired_device_nodes}')
