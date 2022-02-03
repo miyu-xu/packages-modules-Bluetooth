f32004b06289bfe90db9db95af5c746d56001977
Charlie Boutier <charliebout@google.com>
2022-02-03 14:21:36 +0000

    Blueberry A2DP: Add close
    Test: m BlueberryServer + manual
    
    Change-Id: Icf20dc9bc0d2e1ec77d19a7b6d7b43feb4750f64

--- android/blueberry/server/src/com/android/blueberry/A2dp.kt
+++ android/blueberry/server/src/com/android/blueberry/A2dp.kt
@@ -1,11 +1,24 @@
 package com.android.blueberry
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothA2dp
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.media.*
 import android.util.Log
 import blueberry.A2DPGrpc.A2DPImplBase
 import blueberry.A2dpProto.*
 import io.grpc.stub.StreamObserver
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 class A2dp(val mContext: Context, val host: Host) : A2DPImplBase() {
   private val TAG = "BlueberryA2dp"
@@ -13,6 +26,27 @@ class A2dp(val mContext: Context, val host: Host) : A2DPImplBase() {
   private var audioManager: AudioManager =
     mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
   private val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
+  private val bluetoothManager =
+    mContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+  private val bluetoothAdapter = bluetoothManager.adapter
+  private var bluetoothA2dp: BluetoothA2dp? = null
+
+  inner class A2dpServiceListener : BluetoothProfile.ServiceListener {
+    override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+      Log.d(TAG, "bluetoothA2dpConnected")
+      bluetoothA2dp = proxy as BluetoothA2dp
+    }
+
+    override fun onServiceDisconnected(profile: Int) {
+      Log.d(TAG, "bluetoothA2dpDisconnected")
+      bluetoothA2dp = null
+    }
+  }
+
+  init {
+    bluetoothAdapter.getProfileProxy(mContext, A2dpServiceListener(), BluetoothProfile.A2DP)
+  }
 
   private var audioTrack: AudioTrack =
     AudioTrack.Builder()
@@ -49,6 +83,39 @@ class A2dp(val mContext: Context, val host: Host) : A2DPImplBase() {
     audioTrack.play()
     responseObserver.onNext(StartResponse.getDefaultInstance())
     responseObserver.onCompleted()
+  }
+
+  fun BluetoothA2dp.disconnect(device: BluetoothDevice): Boolean =
+    this.javaClass.getMethod("disconnect", BluetoothDevice::class.java).invoke(this, device) as
+      Boolean
+
+  override fun close(request: CloseRequest, responseObserver: StreamObserver<CloseResponse>) {
+    Log.d(TAG, "close")
+
+    runBlocking {
+      val flow = callbackFlow {
+        val connectionStateBroadcastReceiver: BroadcastReceiver =
+          object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+              if (intent.getIntExtra(BluetoothA2dp.EXTRA_STATE, BluetoothAdapter.ERROR) ==
+                  BluetoothA2dp.STATE_DISCONNECTED
+              ) {
+                trySendBlocking(null)
+              }
+            }
+          }
+        val intentFilter = IntentFilter(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED)
+        mContext.registerReceiver(connectionStateBroadcastReceiver, intentFilter)
+        bluetoothA2dp!!.disconnect(host.getConnectedBluetoothDevice())
+
+        awaitClose { mContext.unregisterReceiver(connectionStateBroadcastReceiver) }
+      }
+
+      flow.first()
+
+      responseObserver.onNext(CloseResponse.getDefaultInstance())
+      responseObserver.onCompleted()
+    }
   }
 
   override fun playbackAudio(
