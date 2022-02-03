faa210d2ac989677cecd092c51189f63920a2b22
Charlie Boutier <charliebout@google.com>
2022-02-03 14:21:36 +0000

    Blueberry A2DP: Add close
    Test: m Blueberry + manual
    
    Change-Id: Icf20dc9bc0d2e1ec77d19a7b6d7b43feb4750f64

--- android/blueberry/server/src/com/android/blueberry/A2dp.kt
+++ android/blueberry/server/src/com/android/blueberry/A2dp.kt
@@ -1,5 +1,9 @@
 package com.android.blueberry
 
+import android.bluetooth.BluetoothA2dp
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.content.Context
 import android.media.*
 import android.util.Log
@@ -13,6 +17,27 @@ class A2dp(val mContext: Context, val host: Host) : A2DPImplBase() {
   private var audioManager: AudioManager =
     mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
   private val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
+  private val bluetoothManager =
+    mContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+  private val bluetoothAdapter = bluetoothManager.adapter
+  private var bluetoothA2dp: BluetoothA2dp? = null
+
+  inner class A2dpServiceListener : BluetoothProfile.ServiceListener {
+    override fun onServiceConnected(profile: Int, proxy: BluetoothProfile) {
+      Log.d(TAG, "bluetoothA2dpConnected")
+      bluetoothA2dp = proxy as BluetoothA2dp
+    }
+
+    override fun onServiceDisconnected(profile: Int) {
+      Log.d(TAG, "bluetoothA2dpDisconnected")
+      bluetoothA2dp = null
+    }
+  }
+
+  init {
+    bluetoothAdapter.getProfileProxy(mContext, A2dpServiceListener(), BluetoothProfile.A2DP)
+  }
 
   private var audioTrack: AudioTrack =
     AudioTrack.Builder()
@@ -48,6 +73,17 @@ class A2dp(val mContext: Context, val host: Host) : A2DPImplBase() {
     }
     audioTrack.play()
     responseObserver.onNext(StartResponse.getDefaultInstance())
+    responseObserver.onCompleted()
+  }
+
+  fun BluetoothA2dp.disconnect(device: BluetoothDevice): Boolean =
+    this.javaClass.getMethod("disconnect", BluetoothDevice::class.java).invoke(this, device) as
+      Boolean
+
+  override fun close(request: CloseRequest, responseObserver: StreamObserver<CloseResponse>) {
+    Log.d(TAG, "close")
+    bluetoothA2dp!!.disconnect(host.getConnectedBluetoothDevice())
+    responseObserver.onNext(CloseResponse.getDefaultInstance())
     responseObserver.onCompleted()
   }
 
