138320c75d44bbecea427969be3c7227347b244b
Charlie Boutier <charliebout@google.com>
2022-02-07 16:24:50 +0000

    Blueberry Host: Add disconnect
    Change-Id: I5335fd9235e9477b574a5f9793ce06f0c9cd5541

--- blueberry/src/com/android/blueberry/Host.kt
+++ blueberry/src/com/android/blueberry/Host.kt
@@ -167,5 +167,23 @@ class Host(private val context: Context) : HostImplBase() {
     responseObserver.onError(Status.UNKNOWN.asException())
   }
 
+  override fun disconnect(
+    request: DisconnectRequest,
+    responseObserver: StreamObserver<DisconnectResponse>
+  ) {
+    val addr = request.connection.cookie.toByteArray().decodeToString()
+    Log.d(TAG, "disconnect: $addr")
+    if (addr != bluetoothDevice.address ||
+        bluetoothDevice.getBondState() == BluetoothDevice.BOND_NONE
+    ) {
+      responseObserver.onError(Status.UNKNOWN.asException())
+    } else {
+      bluetoothDevice.removeBond()
+      responseObserver.onNext(DisconnectResponse.getDefaultInstance())
+      responseObserver.onCompleted()
+    }
+  }
+
+  fun BluetoothDevice.removeBond() = this.javaClass.getMethod("removeBond").invoke(this)
   fun getConnectedBluetoothDevice() = bluetoothDevice
 }
