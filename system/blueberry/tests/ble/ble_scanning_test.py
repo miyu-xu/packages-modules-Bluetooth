c2d4c85c856e5aa7b326816145c9341582449273
Pixel and Ecosystems Test <noreply@google.com>
2022-02-14 16:11:31 -0800

    Blueberry project import generated by Copybara.
    Test: Manual test
    PiperOrigin-RevId: 428632740
    Change-Id: I5d12a14a757c9e950d1b69f5d6e3073372dd5efe

---
+++ system/blueberry/tests/ble/ble_scanning_test.py
@@ -0,0 +1,80 @@
+"""Module of Blueberry BLE scanning test cases."""
+
+import logging
+
+from mobly import asserts as mobly_asserts
+from mobly import test_runner
+from mobly import signals
+from blueberry.utils import asserts
+from blueberry.utils import blueberry_base_test
+
+
+class BleScanningTest(blueberry_base_test.BlueberryBaseTest):
+  """Test Class for BLE Scanning Test.
+
+  Test will reset the bluetooth settings of the phone and attempt to examine
+  the advertisement from scanning of phone to look for the `derived_bt_device`
+  specified in the configuration file.
+  """
+
+  def setup_class(self):
+    """Conducts testing setup in class level."""
+    super().setup_class()
+
+    self.primary_device = self.android_devices[0]
+    self.primary_device.init_setup()
+    self.primary_device.sl4a_setup()
+
+    self.derived_bt_device = self.derived_bt_devices[0]
+    self.derived_bt_device.factory_reset_bluetooth()
+    self.bt_device_name = self.derived_bt_device.get_device_name()
+    self.bt_device_mac_address = (
+        self.derived_bt_device.get_bluetooth_mac_address())
+
+  def setup_test(self):
+    """Conducts testing setup before running each test cases."""
+    super().setup_test()
+    self.primary_device.factory_reset_bluetooth()
+
+  def assert_scanning(self,
+                      bt_device_name: str,
+                      expected_mac_address: str,
+                      timeout_sec: int = 90) -> None:
+    """Asserts that the primary device's scanning can find given BT device name.
+
+    Args:
+      bt_device_name: The name of BT device.
+      expected_mac_address: The expected MAC address of given BT device name.
+      timeout_sec: Number of seconds to wait for the advertisement with desired
+        device name.
+    """
+    with asserts.assert_not_raises(signals.ControllerError):
+      ble_mac_address = self.primary_device.scan_and_get_ble_device_address(
+          bt_device_name, timeout_sec=timeout_sec)
+
+      mobly_asserts.assert_equal(ble_mac_address, expected_mac_address)
+
+  def test_scanning_after_power_reset_bt_target(self):
+    """Tests BLE scanning after rebooting target BT device."""
+    logging.info('Rebooting target bt device...')
+    self.derived_bt_device.reboot()
+    self.assert_scanning(self.bt_device_name, self.bt_device_mac_address)
+
+  def test_scanning_after_power_reset_phone(self):
+    """Tests BLE scanning after rebooting primary device."""
+    logging.info('Rebooting primary device...')
+    self.primary_device.reboot()
+    self.primary_device.wait_for_bluetooth_toggle_state(True)
+    self.assert_scanning(self.bt_device_name, self.bt_device_mac_address)
+
+  def test_scanning_after_reset_airplane_mode_of_phone(self):
+    """Tests BLE scanning after flipping on/off airplane mode."""
+    logging.info('Flipping on/off airplane mode...')
+    self.primary_device.enable_airplane_mode()
+    self.primary_device.disable_airplane_mode()
+    self.primary_device.wait_for_bluetooth_toggle_state(True)
+    self.assert_scanning(self.bt_device_name, self.bt_device_mac_address)
+
+
+if __name__ == '__main__':
+  test_runner.main()
