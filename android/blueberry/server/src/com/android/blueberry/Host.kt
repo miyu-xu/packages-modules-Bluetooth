e56a34aa7da6ef76beb5ac84e5a0dcd2745f7cdc
Charlie Boutier <charliebout@google.com>
2022-02-16 09:16:24 +0000

    Blueberry: Clean reset
    Test: m BlueberryServer + manual
    
    Change-Id: I9f1e08eaacf22f295e6a19b07dded18cedb1557f

--- android/blueberry/server/src/com/android/blueberry/Host.kt
+++ android/blueberry/server/src/com/android/blueberry/Host.kt
@@ -37,7 +37,7 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
-class Host(private val context: Context) : HostImplBase() {
+class Host(private val context: Context, private val server: Server) : HostImplBase() {
   private val TAG = "BlueberryHost"
 
   private val bluetoothManager =
@@ -86,6 +86,9 @@ class Host(private val context: Context) : HostImplBase() {
 
       responseObserver.onNext(Empty.getDefaultInstance())
       responseObserver.onCompleted()
+
+      Log.i(TAG, "shutdownNow the gRPC Server")
+      server.shutdownNow()
     }
   }
 
@@ -134,7 +137,6 @@ class Host(private val context: Context) : HostImplBase() {
                     Log.d(TAG, "resp false")
                     trySendBlocking(false)
                   }
-                  Log.e(TAG, "deviceState: $deviceState")
                   if (deviceState == BluetoothAdapter.STATE_CONNECTED ||
                       deviceState == BluetoothAdapter.STATE_CONNECTING
                   ) {
