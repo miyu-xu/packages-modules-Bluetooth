739b35b8f31c181d4b23ca0e4365c88e33d7e138
Partha Sarathi Saha <partha-sarathi.saha@broadcom.com>
2022-02-18 00:33:18 +0530

    leaudio: Use QoS Configuration settings values set by the BAP Client
    Currently remote BAP Server provided, preferred QoS Configuration
    settings values like Retransmission Number and Max_Transport_Latency
    are used, obtained during Config Codec operation, rather than BAP Client
    preferred QoS Configuration settings, based on the intended use case.
    
    BAP Server may not be able to set meaningful preferred QoS Configuration
    settings values like Retransmission Number and Max_Transport_Latency
    during Config Codec operation, as may not have exact idea regarding
    BAP Client's intended use case.
    
    Hence, use QoS Configuration settings value, as preferred by BAP Client,
    based on the intended use case, rather than directly provided BAP server
    preferred QoS Configuration settings value during Config Codec
    operation.
    
    Test: Verified BAP Client QoS Configuration settings values are used
    Change-Id: I350f196ed9a9e7e2716d8fa477c2f4bd22afaada

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -1141,12 +1141,13 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         }
         ase->framing = rsp.framing;
         ase->preferred_phy = rsp.preferred_phy;
-        ase->max_transport_latency = rsp.max_transport_latency;
+        if (ase->max_transport_latency > rsp.max_transport_latency) {
+          ase->max_transport_latency = rsp.max_transport_latency;
+        }
         ase->pres_delay_min = rsp.pres_delay_min;
         ase->pres_delay_max = rsp.pres_delay_max;
         ase->preferred_pres_delay_min = rsp.preferred_pres_delay_min;
         ase->preferred_pres_delay_max = rsp.preferred_pres_delay_max;
-        ase->retrans_nb = rsp.preferred_retrans_nb;
 
         ase->state = AseState::BTA_LE_AUDIO_ASE_STATE_CODEC_CONFIGURED;
 
@@ -1210,12 +1211,13 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
 
         ase->framing = rsp.framing;
         ase->preferred_phy = rsp.preferred_phy;
-        ase->max_transport_latency = rsp.max_transport_latency;
+        if (ase->max_transport_latency > rsp.max_transport_latency) {
+          ase->max_transport_latency = rsp.max_transport_latency;
+        }
         ase->pres_delay_min = rsp.pres_delay_min;
         ase->pres_delay_max = rsp.pres_delay_max;
         ase->preferred_pres_delay_min = rsp.preferred_pres_delay_min;
         ase->preferred_pres_delay_max = rsp.preferred_pres_delay_max;
-        ase->retrans_nb = rsp.preferred_retrans_nb;
 
         /* This may be a notification from a re-configured ASE */
         ase->reconfigure = false;
