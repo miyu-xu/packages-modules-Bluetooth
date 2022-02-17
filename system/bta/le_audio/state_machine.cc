d3f63ff58cc4d0b6c9ef053a319fe2be1f74ebb1
Partha Sarathi Saha <partha-sarathi.saha@broadcom.com>
2022-02-18 00:33:18 +0530

    leaudio: Apply QoS settings based on use scenarios
    Use scenarios (voice, media, etc..) can be classified into low latency or high reliability type. Values defined in BAP spec can be applied to those use scenarios accordingly. In future when more use scenarios are added, QoS settings can be made per-direction basis for those use scenarios.
    
    Test: atest --host bluetooth_le_audio_test
    
    Change-Id: Ic69a1bace71bb05b5b04373b0ccc646ab121df54

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -1155,12 +1155,19 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
         }
         ase->framing = rsp.framing;
         ase->preferred_phy = rsp.preferred_phy;
-        ase->max_transport_latency = rsp.max_transport_latency;
+        if (!ase->max_transport_latency ||
+            ase->max_transport_latency > rsp.max_transport_latency) {
+          ase->max_transport_latency = rsp.max_transport_latency;
+          ase->retrans_nb = rsp.preferred_retrans_nb;
+          LOG(INFO) << __func__ << " Using remote preferred QoS settings";
+        }
         ase->pres_delay_min = rsp.pres_delay_min;
         ase->pres_delay_max = rsp.pres_delay_max;
         ase->preferred_pres_delay_min = rsp.preferred_pres_delay_min;
         ase->preferred_pres_delay_max = rsp.preferred_pres_delay_max;
-        ase->retrans_nb = rsp.preferred_retrans_nb;
+        if (!ase->retrans_nb) {
+          ase->retrans_nb = rsp.preferred_retrans_nb;
+        }
 
         ase->state = AseState::BTA_LE_AUDIO_ASE_STATE_CODEC_CONFIGURED;
 
@@ -1224,12 +1231,19 @@ class LeAudioGroupStateMachineImpl : public LeAudioGroupStateMachine {
 
         ase->framing = rsp.framing;
         ase->preferred_phy = rsp.preferred_phy;
-        ase->max_transport_latency = rsp.max_transport_latency;
+        if (!ase->max_transport_latency ||
+            ase->max_transport_latency > rsp.max_transport_latency) {
+          ase->max_transport_latency = rsp.max_transport_latency;
+          ase->retrans_nb = rsp.preferred_retrans_nb;
+          LOG(INFO) << __func__ << " Using remote preferred QoS settings";
+        }
         ase->pres_delay_min = rsp.pres_delay_min;
         ase->pres_delay_max = rsp.pres_delay_max;
         ase->preferred_pres_delay_min = rsp.preferred_pres_delay_min;
         ase->preferred_pres_delay_max = rsp.preferred_pres_delay_max;
-        ase->retrans_nb = rsp.preferred_retrans_nb;
+        if (!ase->retrans_nb) {
+          ase->retrans_nb = rsp.preferred_retrans_nb;
+        }
 
         /* This may be a notification from a re-configured ASE */
         ase->reconfigure = false;
