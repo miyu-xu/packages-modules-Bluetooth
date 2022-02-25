f270e29d148cdeb3cf36e1c66826142041081dec
Martin Brabham <optedoblivion@google.com>
2022-02-25 07:22:38 +0000

    Floss: LeRand controller plumbing
    Add ability to call LeRand and provide a callback function.
    
    Bug: 219982689
    Test: mma -j $(nproc)
    Test: ./build.py
    Test: atest bluetooth_test_gd_unit --test-filter="*controller*"
    Tag: #floss
    Change-Id: Id501efa2073f3b8887f7ca9a3894050becebc5cd

--- system/device/include/controller.h
+++ system/device/include/controller.h
@@ -21,8 +21,11 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "base/callback.h"
 #include "btcore/include/version.h"
 #include "types/raw_address.h"
+
+using LeRandCallback = base::Callback<void()>;
 
 typedef struct controller_t {
   bool (*get_is_ready)(void);
@@ -115,6 +118,7 @@ typedef struct controller_t {
   uint8_t* (*get_local_supported_codecs)(uint8_t* number_of_codecs);
   uint8_t (*get_le_all_initiating_phys)(void);
   uint8_t (*clear_event_filter)(void);
+  uint8_t (*le_rand)(LeRandCallback);
 
 } controller_t;
 
--- system/gd/hci/controller.cc
+++ system/gd/hci/controller.cc
@@ -511,6 +511,19 @@ struct Controller::impl {
                          module_.GetHandler()->BindOnceOn(this, &Controller::impl::check_status<ResetCompleteView>));
   }
 
+  void le_rand(LeRandCallback cb) {
+    std::unique_ptr<LeRandBuilder> packet = LeRandBuilder::Create();
+    hci_->EnqueueCommand(
+        std::move(packet),
+        module_.GetHandler()->BindOnceOn(this, &Controller::impl::le_rand_cb<LeRandCompleteView>, cb));
+  }
+
+  template <class T>
+  void le_rand_cb(LeRandCallback cb, CommandCompleteView packet) {
+    ASSERT(packet.IsValid());
+    cb.Run();
+  }
+
   void set_event_filter(std::unique_ptr<SetEventFilterBuilder> packet) {
     hci_->EnqueueCommand(std::move(packet), module_.GetHandler()->BindOnceOn(
                                                 this, &Controller::impl::check_status<SetEventFilterCompleteView>));
@@ -1018,6 +1031,10 @@ void Controller::SetEventMask(uint64_t event_mask) {
 
 void Controller::Reset() {
   CallOn(impl_.get(), &impl::reset);
+}
+
+void Controller::LeRand(LeRandCallback cb) {
+  CallOn(impl_.get(), &impl::le_rand, cb);
 }
 
 void Controller::SetEventFilterClearAll() {
