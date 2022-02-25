dbdc28a0d127c8a5a122cb2ab4ec8fe42e2d28c0
En-Shuo Hsu <enshuo@chromium.org>
2022-02-25 23:47:12 +0000

    Fill in more SLC implementation
    - Enhanced Call status
    - CLCC
    - CIEV
    
    Change-Id: I05acf735d6a7fc170a74fa2bde545090893fb4f7

--- system/gd/rust/topshim/hfp/hfp_shim.cc
+++ system/gd/rust/topshim/hfp/hfp_shim.cc
@@ -14,11 +14,10 @@
  * limitations under the License.
  */
 
-#include "gd/rust/topshim/hfp/hfp_shim.h"
-
 #include "btif/include/btif_hf.h"
 #include "gd/os/log.h"
 #include "gd/rust/topshim/common/utils.h"
+#include "gd/rust/topshim/hfp/hfp_shim.h"
 #include "include/hardware/bt_hf.h"
 #include "src/profiles/hfp.rs.h"
 #include "types/raw_address.h"
@@ -52,8 +51,30 @@ class DBusHeadsetCallbacks : public headset::Callbacks {
     topshim::rust::internal::connection_state_cb(state, bd_addr);
   }
 
-  void AudioStateCallback(
-      [[maybe_unused]] headset::bthf_audio_state_t state, [[maybe_unused]] RawAddress* bd_addr) override {}
+  void AudioStateCallback(headset::bthf_audio_state_t state, RawAddress* bd_addr) override {
+    LOG_WARN("AudioStateCallback %u from %s", state, bd_addr->ToString().c_str());
+    switch (state) {
+      case headset::bthf_audio_state_t::BTHF_AUDIO_STATE_CONNECTED:
+        headset_->PhoneStateChange(
+            1, 0, headset::bthf_call_state_t::BTHF_CALL_STATE_IDLE, "", (headset::bthf_call_addrtype_t)0, "", bd_addr);
+        headset_->VolumeControl(headset::bthf_volume_type_t::BTHF_VOLUME_TYPE_SPK, 5, bd_addr);
+        return;
+      case headset::bthf_audio_state_t::BTHF_AUDIO_STATE_DISCONNECTED:
+        headset_->PhoneStateChange(
+            0,
+            0,
+            headset::bthf_call_state_t::BTHF_CALL_STATE_DISCONNECTED,
+            "",
+            (headset::bthf_call_addrtype_t)0,
+            "",
+            bd_addr);
+        headset_->PhoneStateChange(
+            0, 0, headset::bthf_call_state_t::BTHF_CALL_STATE_IDLE, "", (headset::bthf_call_addrtype_t)0, "", bd_addr);
+        return;
+      default:
+        return;
+    }
+  }
 
   void VoiceRecognitionCallback(
       [[maybe_unused]] headset::bthf_vr_state_t state, [[maybe_unused]] RawAddress* bd_addr) override {}
@@ -84,7 +105,7 @@ class DBusHeadsetCallbacks : public headset::Callbacks {
     /* This is required to setup the SLC, the format of the response should be
      * +CIND: <call>,<callsetup>,<service>,<signal>,<roam>,<battery>,<callheld>
      */
-    LOG_WARN("Respond +CIND: 0,0,0,0,0,0,0 to AT+CIND? from %s", bd_addr->ToString().c_str());
+    LOG_WARN("Respond +CIND: 0,0,1,5,0,5,0 to AT+CIND? from %s", bd_addr->ToString().c_str());
 
     /* headset::Interface::CindResponse's parameters are similar but different
      * from the actual CIND response. It will construct the final response for
@@ -93,7 +114,7 @@ class DBusHeadsetCallbacks : public headset::Callbacks {
      *              held_call_num, callsetup_state, signal_strength,
      *              roam_state, battery_level, bd_addr);
      */
-    headset_->CindResponse(0, 0, 0, headset::BTHF_CALL_STATE_IDLE, 0, 0, 0, bd_addr);
+    headset_->CindResponse(1, 0, 0, headset::BTHF_CALL_STATE_IDLE, 5, 0, 5, bd_addr);
   }
 
   void AtCopsCallback(RawAddress* bd_addr) override {
@@ -116,6 +137,7 @@ class DBusHeadsetCallbacks : public headset::Callbacks {
         headset::BTHF_CALL_ADDRTYPE_UNKNOWN,
         bd_addr);
     */
+    headset_->AtResponse(headset::BTHF_AT_RESPONSE_OK, 0, bd_addr);
   }
 
   void UnknownAtCallback(char* at_string, RawAddress* bd_addr) override {
