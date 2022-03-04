bac6d08c55874909cc158f2196338c70252d3544
Pixel and Ecosystems Test <noreply@google.com>
2022-03-04 03:05:30 -0800

    Blueberry project import generated by Copybara.
    Test: Manual test
    PiperOrigin-RevId: 432403182
    Change-Id: I23d1f129baee72a80064d7df99a953c99a8885c0

--- system/blueberry/utils/ui_pages/utils.py
+++ system/blueberry/utils/ui_pages/utils.py
@@ -1,5 +1,52 @@
 """Utilities used by ui_pages module."""
+import abc
+import re
+
 from typing import Any, Callable, Dict
+
+
+class TextMatchStrategy(abc.ABC):
+  """Strategy to match text."""
+
+  @abc.abstractmethod
+  def match(self, text: str) -> bool:
+    """Matches input text.
+
+    Args:
+      text: The text to match.
+
+    Returns:
+      True iff the input text is wanted.
+    """
+    raise NotImplementedError
+
+
+class PlainTextMatchStrategy(TextMatchStrategy):
+  """Strategy to match by plain text.
+
+  Attributes:
+    expected_text: Text to match.
+  """
+
+  def __init__(self, expected_text: str):
+    self.expected_text = expected_text
+
+  def match(self, text: str) -> bool:
+    return self.expected_text == text
+
+
+class RETextMatchStrategy(TextMatchStrategy):
+  """Strategy to match by regular expression.
+
+  Attributes:
+    pattern_object: Regular expression to search text.
+  """
+
+  def __init__(self, text_re: str):
+    self.pattern_object = re.compile(text_re)
+
+  def match(self, text: str) -> bool:
+    return self.pattern_object.search(text) is not None
 
 
 def dr_wakeup_before_op(op: Callable[..., Any]) -> Callable[..., Any]:
