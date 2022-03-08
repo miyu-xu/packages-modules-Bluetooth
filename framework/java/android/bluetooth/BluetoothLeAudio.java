92a1113ef7ecc7921ed784229b233d40e0ff58e5
Łukasz Rymanowski <rlukasz@google.com>
2022-03-08 13:41:53 +0000

    BluetoothLeAudio: Fix intents permissions
    Bug: 218799288
    Test: Compile
    Change-Id: Ic188b893ee209f0e68e08faeb8f759971541feb1

--- framework/java/android/bluetooth/BluetoothLeAudio.java
+++ framework/java/android/bluetooth/BluetoothLeAudio.java
@@ -138,10 +138,7 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
     @SystemApi
     @RequiresLegacyBluetoothPermission
     @RequiresBluetoothConnectPermission
-    @RequiresPermission(allOf = {
-            android.Manifest.permission.BLUETOOTH_CONNECT,
-            android.Manifest.permission.BLUETOOTH_PRIVILEGED,
-    })
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
     @SdkConstant(SdkConstantType.BROADCAST_INTENT_ACTION)
     public static final String ACTION_LE_AUDIO_ACTIVE_DEVICE_CHANGED =
             "android.bluetooth.action.LE_AUDIO_ACTIVE_DEVICE_CHANGED";
@@ -160,10 +157,7 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
      * @hide
      */
     @SystemApi
-    @RequiresPermission(allOf = {
-            android.Manifest.permission.BLUETOOTH_CONNECT,
-            android.Manifest.permission.BLUETOOTH_PRIVILEGED,
-    })
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_PRIVILEGED)
     @SdkConstant(SdkConstantType.BROADCAST_INTENT_ACTION)
     public static final String ACTION_LE_AUDIO_GROUP_NODE_STATUS_CHANGED =
             "android.bluetooth.action.LE_AUDIO_GROUP_NODE_STATUS_CHANGED";
