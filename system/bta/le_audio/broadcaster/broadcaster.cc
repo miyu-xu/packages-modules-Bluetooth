0808466237bfa900d9bbdf891a6054747e778a76
Grzegorz Kołodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2022-03-08 14:53:55 +0000

    le_audio: Add support for Broadcaster session for Bluetoot stack
    Patch extends possibility for stack, Bluetooth Audio Hal part to use
    Broadcaster session for broadcasting.
    
    Tag: #feature
    Test: atest bluetooth_le_audio_client_test bluetooth_test_broadcaster
    Sponsor: jpawlowski@
    Bug:
    Change-Id: I4d5402f060a853456751e348072b941173848e04

--- system/bta/le_audio/broadcaster/broadcaster.cc
+++ system/bta/le_audio/broadcaster/broadcaster.cc
@@ -46,6 +46,7 @@ using le_audio::types::kLeAudioCodingFormatLC3;
 namespace {
 class LeAudioBroadcasterImpl;
 LeAudioBroadcasterImpl* instance;
+LeAudioBroadcastClientAudioSource* leAudioClientAudioSource;
 
 /* Class definitions */
 
@@ -106,8 +107,8 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
     callbacks_ = nullptr;
 
     if (audio_instance_) {
-      LeAudioClientAudioSource::Stop();
-      LeAudioClientAudioSource::Release(audio_instance_);
+      leAudioClientAudioSource->Stop();
+      leAudioClientAudioSource->Release(audio_instance_);
       audio_instance_ = nullptr;
     }
   }
@@ -224,7 +225,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     if (broadcasts_.count(broadcast_id) != 0) {
       LOG_INFO("Stopping LeAudioClientAudioSource");
-      LeAudioClientAudioSource::Stop();
+      leAudioClientAudioSource->Stop();
       broadcasts_[broadcast_id]->SetMuted(true);
       broadcasts_[broadcast_id]->ProcessMessage(
           BroadcastStateMachine::Message::SUSPEND, nullptr);
@@ -255,7 +256,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     if (broadcasts_.count(broadcast_id) != 0) {
       if (!audio_instance_) {
-        audio_instance_ = LeAudioClientAudioSource::Acquire();
+        audio_instance_ = leAudioClientAudioSource->Acquire();
         if (!audio_instance_) {
           LOG_ERROR("Could not acquire le audio");
           return;
@@ -277,7 +278,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     LOG_INFO("Stopping LeAudioClientAudioSource, broadcast_id=%d",
              broadcast_id);
-    LeAudioClientAudioSource::Stop();
+    leAudioClientAudioSource->Stop();
     broadcasts_[broadcast_id]->SetMuted(true);
     broadcasts_[broadcast_id]->ProcessMessage(
         BroadcastStateMachine::Message::STOP, nullptr);
@@ -374,7 +375,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
         CHECK(broadcasts_.count(broadcast_id) != 0);
         broadcasts_[broadcast_id]->HandleHciEvent(HCI_BLE_TERM_BIG_CPL_EVT,
                                                   evt);
-        LeAudioClientAudioSource::Release(audio_instance_);
+        leAudioClientAudioSource->Release(audio_instance_);
         audio_instance_ = nullptr;
       } break;
       default:
@@ -491,7 +492,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
               broadcast->SetMuted(false);
               auto cfg = static_cast<const LeAudioCodecConfiguration*>(data);
               auto is_started =
-                  LeAudioClientAudioSource::Start(*cfg, &audio_receiver_);
+                  leAudioClientAudioSource->Start(*cfg, &audio_receiver_);
               if (!is_started) {
                 /* Audio Source setup failed - stop the broadcast */
                 instance->StopAudioBroadcast(broadcast_id);
@@ -657,11 +658,11 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
         instance->audio_data_path_state_ = AudioDataPathState::ACTIVE;
 
       if (!IsAnyoneStreaming()) {
-        LeAudioClientAudioSource::CancelStreamingRequest();
+        leAudioClientAudioSource->CancelStreamingRequest();
         return;
       }
 
-      LeAudioClientAudioSource::ConfirmStreamingRequest();
+      leAudioClientAudioSource->ConfirmStreamingRequest();
     }
 
     virtual void OnAudioMetadataUpdate(
@@ -722,6 +723,8 @@ void LeAudioBroadcaster::Initialize(
     LOG_ALWAYS_FATAL("HAL requirements not met. Init aborted.");
   }
 
+  /* Create new client audio broadcast instance */
+  InitializeAudioClient(nullptr);
   IsoManager::GetInstance()->Start();
 
   instance = new LeAudioBroadcasterImpl(callbacks);
@@ -754,6 +757,10 @@ void LeAudioBroadcaster::Cleanup(void) {
   instance = nullptr;
 
   ptr->CleanUp();
+  if (leAudioClientAudioSource) {
+    delete leAudioClientAudioSource;
+    leAudioClientAudioSource = nullptr;
+  }
   delete ptr;
 }
 
@@ -761,4 +768,20 @@ void LeAudioBroadcaster::DebugDump(int fd) {
   dprintf(fd, "Le Audio Broadcaster:\n");
   if (instance) instance->Dump(fd);
   dprintf(fd, "\n");
+}
+
+void LeAudioBroadcaster::InitializeAudioClient(
+    LeAudioBroadcastClientAudioSource* clientAudioSource) {
+  if (leAudioClientAudioSource) {
+    LOG(WARNING) << __func__ << ", audio clients already initialized";
+    return;
+  }
+
+  if (!clientAudioSource) {
+    /* Create new instance if no pre-created is delivered */
+    leAudioClientAudioSource = new LeAudioBroadcastClientAudioSource();
+  } else {
+    /* Use pre-created instance e.g. from test suit */
+    leAudioClientAudioSource = clientAudioSource;
+  }
 }
