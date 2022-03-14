5d08c0ad900028a78e0996169c980dcd541280af
Chris Manton <cmanton@google.com>
2022-03-14 07:31:44 -0700

    Abstract gd/os/reactor from eventfd
    Bug: 224543807
    Tag: #refactor
    Test: gd/cert/run
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I6721411e402e6272fa999f5cc66442789d27b112

---
+++ system/gd/os/internal/reactor_api.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <base/callback.h>
+
+#include <atomic>
+#include <future>
+#include <list>
+#include <mutex>
+
+#include "common/callback.h"
+#include "os/reactor.h"
+
+namespace bluetooth {
+namespace os {
+
+class ReactorApi {
+ public:
+  virtual ~ReactorApi() = default;
+  virtual void Run() = 0;
+  virtual void Stop() = 0;
+  virtual Reactor::Reactable* Register(int fd, common::Closure on_read_ready, common::Closure on_write_ready) = 0;
+  virtual void Unregister(Reactor::Reactable* reactable) = 0;
+  virtual bool WaitForUnregisteredReactable(std::chrono::milliseconds timeout) = 0;
+  virtual bool WaitForIdle(std::chrono::milliseconds timeout) = 0;
+  virtual void ModifyRegistration(
+      Reactor::Reactable* reactable, common::Closure on_read_ready, common::Closure on_write_ready) = 0;
+
+ protected:
+  std::mutex mutex_;
+  std::atomic<bool> is_running_{false};
+  std::list<Reactor::Reactable*> invalidation_list_;
+  std::shared_ptr<std::promise<void>> idle_promise_;
+  std::shared_ptr<std::future<void>> executing_reactable_finished_;
+};
+
+}  // namespace os
+}  // namespace bluetooth
