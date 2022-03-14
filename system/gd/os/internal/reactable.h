077ec295f98a61a4dd6b32645d02e983d84c0ea1
Chris Manton <cmanton@google.com>
2022-03-14 07:31:44 -0700

    Abstract gd/os/reactor from eventfd
    Bug: 224543807
    Tag: #refactor
    Test: gd/cert/run
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I6721411e402e6272fa999f5cc66442789d27b112

---
+++ system/gd/os/internal/reactable.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <future>
+#include <memory>
+#include <mutex>
+
+#include "common/callback.h"
+#include "os/reactor.h"
+
+namespace bluetooth {
+namespace os {
+
+class Reactor::Reactable {
+ public:
+  Reactable(int fd, base::Closure on_read_ready, base::Closure on_write_ready)
+      : fd_(fd),
+        on_read_ready_(std::move(on_read_ready)),
+        on_write_ready_(std::move(on_write_ready)),
+        is_executing_(false),
+        removed_(false) {}
+  const int fd_;
+  base::Closure on_read_ready_;
+  base::Closure on_write_ready_;
+  bool is_executing_;
+  bool removed_;
+  std::mutex mutex_;
+  std::unique_ptr<std::promise<void>> finished_promise_;
+};
+
+}  // namespace os
+}  // namespace bluetooth
