9670e75921ccb2bf10fb096c3c3cadef72e45196
kuanyuhuang <kuanyuhuang@google.com>
2022-03-16 03:55:10 +0000

    Fix HID BTA_HH_GET_RPT_EVT callback data format
    When HID host send BTA_HH_GET_RPT_EVT packet but facing error, host will
    use tBTA_HH_CBDATA as response packet. But, callback function
    btif_hh_upstreams_evt expects tBTA_HH_HSDATA format for
    BTA_HH_GET_RPT_EVT.
    
    Bug: 216257125
    Test: atest BluetoothInstrumentationTests, manually reproduce
    Change-Id: I2170c12ad0e42807e3b7bbbe8bf04c2026488321

--- system/bta/hh/bta_hh_act.cc
+++ system/bta/hh/bta_hh_act.cc
@@ -1017,17 +1017,12 @@ static uint8_t convert_api_sndcmd_param(const tBTA_HH_CMD_DATA& api_sndcmd) {
 }
 
 void bta_hh_write_dev_act(tBTA_HH_DEV_CB* p_cb, const tBTA_HH_DATA* p_data) {
-  tBTA_HH_CBDATA cbdata = {BTA_HH_OK, 0};
   uint16_t event =
       (p_data->api_sndcmd.t_type - HID_TRANS_GET_REPORT) + BTA_HH_GET_RPT_EVT;
 
   if (p_cb->is_le_device)
     bta_hh_le_write_dev_act(p_cb, p_data);
-  else
-  {
-
-    cbdata.handle = p_cb->hid_handle;
-
+  else {
     /* match up BTE/BTA report/boot mode def */
     const uint8_t api_sndcmd_param =
         convert_api_sndcmd_param(p_data->api_sndcmd);
@@ -1037,13 +1032,36 @@ void bta_hh_write_dev_act(tBTA_HH_DEV_CB* p_cb, const tBTA_HH_DATA* p_data) {
                          p_data->api_sndcmd.rpt_id,
                          p_data->api_sndcmd.p_data) != HID_SUCCESS) {
       APPL_TRACE_ERROR("HID_HostWriteDev Error ");
-      cbdata.status = BTA_HH_ERR;
 
       if (p_data->api_sndcmd.t_type != HID_TRANS_CONTROL &&
-          p_data->api_sndcmd.t_type != HID_TRANS_DATA)
-        (*bta_hh_cb.p_cback)(event, (tBTA_HH*)&cbdata);
-      else if (api_sndcmd_param == BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG)
-        (*bta_hh_cb.p_cback)(BTA_HH_VC_UNPLUG_EVT, (tBTA_HH*)&cbdata);
+          p_data->api_sndcmd.t_type != HID_TRANS_DATA) {
+        BT_HDR cbhdr = {
+          .event = BTA_HH_GET_RPT_EVT,
+          .len = 0,
+          .offset = 0,
+          .layer_specific = 0,
+        };
+        tBTA_HH cbdata = {
+          .hs_data = {
+            .status = BTA_HH_ERR,
+            .handle = p_cb->hid_handle,
+            .rsp_data = {
+              .p_rpt_data = &cbhdr,
+            },
+          },
+        };
+        (*bta_hh_cb.p_cback)(event, &cbdata);
+      } else if (api_sndcmd_param == BTA_HH_CTRL_VIRTUAL_CABLE_UNPLUG) {
+        tBTA_HH cbdata = {
+          .dev_status = {
+            .status = BTA_HH_ERR,
+            .handle = p_cb->hid_handle,
+          },
+        };
+        (*bta_hh_cb.p_cback)(BTA_HH_VC_UNPLUG_EVT, &cbdata);
+      } else {
+        LOG_ERROR("HID_HostWriteDev Error in unknown state");
+      }
     } else {
       switch (p_data->api_sndcmd.t_type) {
         case HID_TRANS_SET_PROTOCOL:
