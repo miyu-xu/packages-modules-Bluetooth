fa9fd40aacca26bec182e5d54bfcbd602cb33a6e
Jakub Tyszkowski <jakub.tyszkowski@codecoup.pl>
2022-03-16 14:47:46 +0000

    broadcaster: Fix suppressing noisy intent
    Fixes the reversed logic to normal, which is to not supress
    the intent when Boradcast gets disabled and phone speaker can
    become noisy.
    
    Bug: 150670922
    Tag: #feature
    Test: atest BluetoothInstrumentationTests
    Sponsor: jpawlowski@
    Change-Id: Ib1715e1f6080a8746fe2f9283aa2a88af0f33947

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1160,7 +1160,7 @@ public class LeAudioService extends ProfileService {
                         mAudioManager.handleBluetoothActiveDeviceChanged(mActiveAudioOutDevice,
                                 previousDevice,
                                 // TODO: implement createLeAudioBroadcastInfo()
-                                BluetoothProfileConnectionInfo.createLeAudioInfo(true, true));
+                                BluetoothProfileConnectionInfo.createLeAudioInfo(false, true));
                     }
                 }
 
@@ -1187,7 +1187,7 @@ public class LeAudioService extends ProfileService {
                         mAudioManager.handleBluetoothActiveDeviceChanged(mActiveAudioOutDevice,
                                 previousDevice,
                                 // TODO: implement createLeAudioBroadcastInfo()
-                                BluetoothProfileConnectionInfo.createLeAudioInfo(false, true));
+                                BluetoothProfileConnectionInfo.createLeAudioInfo(true, true));
                     }
                 }
             }
