5234018fd674a3f2d1f6fa63998f7278a3388eaf
Martin Brabham <optedoblivion@google.com>
2022-03-19 01:56:41 +0000

    WIP OobPairingSl4aTest: Create bond out of band
    UNIMPLEMENTED RPC error on SetPrivacyPolicyForInitiatorAddress
    
    Bug: 201087902
    Test: gd/cert/run --clean --device --sl4a OobPairingSl4aTest.test_sl4a_create_bond_out_of_band
    Tag: #stability
    Change-Id: I5d2437d1960b0129fe96e5f227cb9531b4db9637

--- system/blueberry/tests/gd_sl4a/security/oob_pairing_sl4a_test.py
+++ system/blueberry/tests/gd_sl4a/security/oob_pairing_sl4a_test.py
@@ -19,20 +19,44 @@ import logging
 
 from google.protobuf import empty_pb2 as empty_proto
 
+from bluetooth_packets_python3 import hci_packets
+
+from blueberry.tests.gd_sl4a.lib.bt_constants import ble_scan_settings_phys
 from blueberry.tests.gd_sl4a.lib.gd_sl4a_base_test import GdSl4aBaseTestClass
+from blueberry.tests.gd.cert.py_le_security import PyLeSecurity
 from blueberry.tests.gd.cert.truth import assertThat
+
+from blueberry.facade import common_pb2 as common
+from blueberry.facade.hci import le_advertising_manager_facade_pb2 as le_advertising_facade
+from blueberry.facade.hci import le_initiator_address_facade_pb2 as le_initiator_address_facade
+from blueberry.facade.security.facade_pb2 import LeIoCapabilityMessage
+from blueberry.facade.security.facade_pb2 import LeOobDataPresentMessage
+
+LeIoCapabilities = LeIoCapabilityMessage.LeIoCapabilities
+LeOobDataFlag = LeOobDataPresentMessage.LeOobDataFlag
+
+DISPLAY_ONLY = LeIoCapabilityMessage(capabilities=LeIoCapabilities.DISPLAY_ONLY)
+
+OOB_NOT_PRESENT = LeOobDataPresentMessage(data_present=LeOobDataFlag.NOT_PRESENT)
 
 
 class OobData:
 
-    def __init__(self):
-        pass
+    address = None
+    confirmation = None
+    randomizer = None
+
+    def __init__(self, address, confirmation, randomizer):
+        self.address = address
+        self.confirmation = confirmation
+        self.randomizer = randomizer
 
 
 class OobPairingSl4aTest(GdSl4aBaseTestClass):
     # Events sent from SL4A
     SL4A_EVENT_GENERATED = "GeneratedOobData"
     SL4A_EVENT_ERROR = "ErrorOobData"
+    SL4A_EVENT_BONDED = "Bonded"
 
     # Matches tBT_TRANSPORT
     # Used Strings because ints were causing gRPC problems
@@ -59,15 +83,38 @@ class OobPairingSl4aTest(GdSl4aBaseTestClass):
             logging.error("Failed to generate OOB data!")
             return None
         logging.info("Data received!")
-        return OobData()
+        logging.info(event_info["data"])
+        return OobData(event_info["data"]["address_with_type"], event_info["data"]["confirmation"],
+                       event_info["data"]["randomizer"])
 
     def _generate_cert_oob_data(self, transport):
         if transport == self.TRANSPORT_LE:
-            return self.cert.security.GetLeOutOfBandData(empty_proto.Empty())
+            oob_data = self.cert.security.GetLeOutOfBandData(empty_proto.Empty())
+            # GetLeOutOfBandData adds null terminator to string in C code
+            # (length 17) before passing back to python via gRPC where it is
+            # converted back to bytes. Remove the null terminator for handling
+            # in python test, since length is known to be 16 for
+            # confirmation_value and random_value
+            oob_data.confirmation_value = oob_data.confirmation_value[:-1]
+            oob_data.random_value = oob_data.random_value[:-1]
+            return oob_data
         return None
+
+    def set_cert_privacy_policy_with_random_address_but_advertise_resolvable(self, irk):
+        random_address_bytes = "DD:34:02:05:5C:EE".encode()
+        private_policy = le_initiator_address_facade.PrivacyPolicy(
+            address_policy=le_initiator_address_facade.AddressPolicy.USE_RESOLVABLE_ADDRESS,
+            address_with_type=common.BluetoothAddressWithType(
+                address=common.BluetoothAddress(address=random_address_bytes), type=common.RANDOM_DEVICE_ADDRESS),
+            rotation_irk=irk)
+        self.cert.hci_le_initiator_address.SetPrivacyPolicyForInitiatorAddress(private_policy)
+        # Bluetooth MAC address must be upper case
+        return random_address_bytes.decode('utf-8').upper()
 
     def test_sl4a_classic_generate_oob_data(self):
         oob_data = self._generate_sl4a_oob_data(self.TRANSPORT_BREDR)
+        logging.info("OOB data received")
+        logging.info(oob_data)
         assertThat(oob_data).isNotNone()
 
     def test_sl4a_classic_generate_oob_data_twice(self):
@@ -81,3 +128,55 @@ class OobPairingSl4aTest(GdSl4aBaseTestClass):
     def test_cert_ble_generate_oob_data(self):
         oob_data = self._generate_cert_oob_data(self.TRANSPORT_LE)
         assertThat(oob_data).isNotNone()
+
+    def test_sl4a_create_bond_out_of_band(self):
+        self.cert.security.SetLeIoCapability(DISPLAY_ONLY)
+        self.cert.security.SetLeOobDataPresent(OOB_NOT_PRESENT)
+        self.cert.security.SetLeAuthRequirements(empty_proto.Empty())
+
+        data = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10]
+        byteArrayObject = bytearray(data)
+        irk = bytes(byteArrayObject)
+
+        DEVICE_NAME = 'Im_The_CERT!'
+        logging.info("Getting public address")
+        RANDOM_ADDRESS = self.set_cert_privacy_policy_with_random_address_but_advertise_resolvable(irk)
+        logging.info("Done %s" % RANDOM_ADDRESS)
+
+        gap_name = hci_packets.GapData()
+        gap_name.data_type = hci_packets.GapDataType.COMPLETE_LOCAL_NAME
+        gap_name.data = list(bytes(DEVICE_NAME, encoding='utf8'))
+        gap_data = le_advertising_facade.GapDataMsg(data=bytes(gap_name.Serialize()))
+        config = le_advertising_facade.AdvertisingConfig(
+            advertisement=[gap_data],
+            interval_min=512,
+            interval_max=768,
+            advertising_type=le_advertising_facade.AdvertisingEventType.ADV_IND,
+            own_address_type=common.USE_RANDOM_DEVICE_ADDRESS,
+            channel_map=7,
+            filter_policy=le_advertising_facade.AdvertisingFilterPolicy.ALL_DEVICES)
+        extended_config = le_advertising_facade.ExtendedAdvertisingConfig(
+            include_tx_power=True,
+            connectable=True,
+            legacy_pdus=True,
+            advertising_config=config,
+            secondary_advertising_phy=ble_scan_settings_phys["1m"])
+        request = le_advertising_facade.ExtendedCreateAdvertiserRequest(config=extended_config)
+        create_response = self.cert.hci_le_advertising_manager.ExtendedCreateAdvertiser(request)
+
+        oob_data = self._generate_cert_oob_data(self.TRANSPORT_LE)
+        assertThat(oob_data).isNotNone()
+
+        byte_address = self.cert.hci_controller.GetMacAddress(empty_proto.Empty()).address
+        logging.info(byte_address)
+        logging.info(type(byte_address))
+        self.dut.sl4a.bluetoothCreateBondOutOfBand(
+            byte_address.decode("utf-8").upper(), self.TRANSPORT_LE, oob_data.confirmation_value.hex(),
+            oob_data.random_value.hex())
+        try:
+            bond_state = self.dut.ed.pop_event(self.SL4A_EVENT_BONDED, self.default_timeout)
+        except queue.Empty as error:
+            logging.error("Failed to generate OOB data!")
+
+        assertThat(bond_state).isNotNone()
+        assertThat(bond_state["data"]["bonded_state"]).isEqualTo("1")
