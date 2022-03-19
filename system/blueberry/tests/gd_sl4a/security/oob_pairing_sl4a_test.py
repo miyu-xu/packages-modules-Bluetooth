79e541f6838256e54dd7cbc8396ff1538d31a2e9
Martin Brabham <optedoblivion@google.com>
2022-03-19 01:56:41 +0000

    WIP OobPairingSl4aTest: Create bond out of band
    Need to mess with the C and R to be sent and see what other self.cert.security
    calls need to be made.
    
    Bug: 201087902
    Test: gd/cert/run --clean --device --sl4a OobPairingSl4aTest.test_sl4a_create_bond_out_of_band
    Tag: #stability
    Change-Id: I5d2437d1960b0129fe96e5f227cb9531b4db9637

--- system/blueberry/tests/gd_sl4a/security/oob_pairing_sl4a_test.py
+++ system/blueberry/tests/gd_sl4a/security/oob_pairing_sl4a_test.py
@@ -20,19 +20,37 @@ import logging
 from google.protobuf import empty_pb2 as empty_proto
 
 from blueberry.tests.gd_sl4a.lib.gd_sl4a_base_test import GdSl4aBaseTestClass
+from blueberry.tests.gd.cert.py_le_security import PyLeSecurity
 from blueberry.tests.gd.cert.truth import assertThat
+
+from blueberry.facade.security.facade_pb2 import LeIoCapabilityMessage
+from blueberry.facade.security.facade_pb2 import LeOobDataPresentMessage
+
+LeIoCapabilities = LeIoCapabilityMessage.LeIoCapabilities
+LeOobDataFlag = LeOobDataPresentMessage.LeOobDataFlag
+
+DISPLAY_ONLY = LeIoCapabilityMessage(capabilities=LeIoCapabilities.DISPLAY_ONLY)
+
+OOB_NOT_PRESENT = LeOobDataPresentMessage(data_present=LeOobDataFlag.NOT_PRESENT)
 
 
 class OobData:
 
-    def __init__(self):
-        pass
+    address = None
+    confirmation = None
+    randomizer = None
+
+    def __init__(self, address, confirmation, randomizer):
+        self.address = address
+        self.confirmation = confirmation
+        self.randomizer = randomizer
 
 
 class OobPairingSl4aTest(GdSl4aBaseTestClass):
     # Events sent from SL4A
     SL4A_EVENT_GENERATED = "GeneratedOobData"
     SL4A_EVENT_ERROR = "ErrorOobData"
+    SL4A_EVENT_BONDED = "Bonded"
 
     # Matches tBT_TRANSPORT
     # Used Strings because ints were causing gRPC problems
@@ -46,6 +64,7 @@ class OobPairingSl4aTest(GdSl4aBaseTestClass):
 
     def setup_test(self):
         super().setup_test()
+        self.cert_security = PyLeSecurity(self.cert)
 
     def teardown_test(self):
         super().teardown_test()
@@ -59,7 +78,8 @@ class OobPairingSl4aTest(GdSl4aBaseTestClass):
             logging.error("Failed to generate OOB data!")
             return None
         logging.info("Data received!")
-        return OobData()
+        return OobData(event_info["data"]["address"], event_info["data"]["confirmation"],
+                       event_info["data"]["randomizer"])
 
     def _generate_cert_oob_data(self, transport):
         if transport == self.TRANSPORT_LE:
@@ -81,3 +101,24 @@ class OobPairingSl4aTest(GdSl4aBaseTestClass):
     def test_cert_ble_generate_oob_data(self):
         oob_data = self._generate_cert_oob_data(self.TRANSPORT_LE)
         assertThat(oob_data).isNotNone()
+
+    def test_sl4a_create_bond_out_of_band(self):
+        oob_data = self._generate_cert_oob_data(self.TRANSPORT_LE)
+        assertThat(oob_data).isNotNone()
+        self.cert.security.SetLeIoCapability(DISPLAY_ONLY)
+        self.cert.security.SetLeOobDataPresent(OOB_NOT_PRESENT)
+        self.cert_security.SetLeAuthRequirements()
+
+        print(oob_data.confirmation_value)
+        print(type(oob_data.confirmation_value))
+        byte_address = self.cert.hci_controller.GetMacAddress(empty_proto.Empty()).address
+        self.dut.sl4a.bluetoothCreateBondOutOfBand(
+            byte_address.decode("utf-8").upper(), self.TRANSPORT_LE, oob_data.confirmation_value.hex(),
+            oob_data.random_value.hex())
+        try:
+            bond_state = self.dut.ed.pop_event(self.SL4A_EVENT_BONDED, self.default_timeout)
+        except queue.Empty as error:
+            logging.error("Failed to generate OOB data!")
+
+        assertThat(bond_state).isNotNone()
+        assertThat(bond_state["data"]["bonded_state"]).isEqualTo("1")
