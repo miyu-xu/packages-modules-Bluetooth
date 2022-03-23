258c4576d3f80601693a52950c62936d73293812
Josh Wu <joshwu@google.com>
2022-03-22 23:12:21 -0700

    BTM: Remove all role check in CTKD
    We found that we don't need to check role in CTKD because even old
    Android devices don't initiate CTKD if they are not the pair initiator.
    
    Tag: #stability
    Bug: 198755234
    Test: atest BluetoothInstrumentationTests
    Change-Id: Ic8182be493414db794bce7292c63569b261f884f

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -3148,22 +3148,8 @@ void btm_sec_auth_complete(uint16_t handle, tHCI_STATUS status) {
     } else {
       BTM_LogHistory(kBtmLogTag, p_dev_rec->bd_addr, "Bonding completed",
                      hci_error_code_text(status));
-
-      tHCI_ROLE role = HCI_ROLE_UNKNOWN;
-      BTM_GetRole(p_dev_rec->bd_addr, &role);
-      if (role == HCI_ROLE_CENTRAL) {
-        // Encryption is required to start SM over BR/EDR
-        // indicate that this is encryption after authentication
-        BTM_SetEncryption(p_dev_rec->bd_addr, BT_TRANSPORT_BR_EDR, NULL, NULL,
-                          BTM_BLE_SEC_NONE);
-      } else if (p_dev_rec->IsLocallyInitiated()) {
-        // Encryption will be set in role_changed callback
-        LOG_INFO(
-            "%s auth completed in role=peripheral, try to switch role and "
-            "encrypt",
-            __func__);
-        BTM_SwitchRoleToCentral(p_dev_rec->RemoteAddress());
-      }
+      BTM_SetEncryption(p_dev_rec->bd_addr, BT_TRANSPORT_BR_EDR, NULL, NULL,
+                        BTM_BLE_SEC_NONE);
 
       l2cu_start_post_bond_timer(p_dev_rec->hci_handle);
     }
@@ -3289,10 +3275,8 @@ void btm_sec_encrypt_change(uint16_t handle, tHCI_STATUS status,
       BTM_TRACE_DEBUG("%s: BR key is temporary, skip derivation of LE LTK",
                       __func__);
     }
-    tHCI_ROLE role = HCI_ROLE_UNKNOWN;
-    BTM_GetRole(p_dev_rec->bd_addr, &role);
     if (p_dev_rec->new_encryption_key_is_p256) {
-      if (btm_sec_use_smp_br_chnl(p_dev_rec) && role == HCI_ROLE_CENTRAL &&
+      if (btm_sec_use_smp_br_chnl(p_dev_rec) &&
           /* if LE key is not known, do deriving */
           (!(p_dev_rec->sec_flags & BTM_SEC_LE_LINK_KEY_KNOWN) ||
            /* or BR key is higher security than existing LE keys */
