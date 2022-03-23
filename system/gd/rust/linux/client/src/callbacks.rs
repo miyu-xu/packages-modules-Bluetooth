be46ff386d6a2f935e928806c0545c9c4fa5a8ce
Sonny Sasaka <sonnysasaka@google.com>
2022-03-23 14:40:46 -0700

    Floss: Add suspend API
    This defines the suspend API at btadapterd level. The Suspend API is
    supposed to be used in conjunction with another entity that actually
    detects suspend/resume. In Chrome OS `btmanagerd` will communicate with
    `powerd`, and in systemd-based Linux distros `btmanagerd` will
    communicate with systemd using Inhibitor interface. In other Linux
    systems suspend/resume script hooks can use this API as well.
    
    Follow ups:
    * Implementation of suspend() and resume()
    * btclient command handler for debugging purposes.
    
    Bug: 224606285
    Tag: #floss
    Test: Manual - Build floss
    
    Change-Id: I5e9d3d37445aceded1224a7eaa6dfb564c25a3a6

--- system/gd/rust/linux/client/src/callbacks.rs
+++ system/gd/rust/linux/client/src/callbacks.rs
@@ -1,6 +1,7 @@
 use crate::dbus_iface::{
     export_bluetooth_callback_dbus_obj, export_bluetooth_connection_callback_dbus_obj,
     export_bluetooth_gatt_callback_dbus_obj, export_bluetooth_manager_callback_dbus_obj,
+    export_suspend_callback_dbus_obj,
 };
 use crate::ClientContext;
 use crate::{console_yellow, print_info};
@@ -10,6 +11,7 @@ use btstack::bluetooth::{
     BluetoothDevice, IBluetooth, IBluetoothCallback, IBluetoothConnectionCallback,
 };
 use btstack::bluetooth_gatt::{BluetoothGattService, IBluetoothGattCallback, LePhy};
+use btstack::suspend::ISuspendCallback;
 use btstack::RPCProxy;
 use dbus::nonblock::SyncConnection;
 use dbus_crossroads::Crossroads;
@@ -443,6 +445,57 @@ impl RPCProxy for BtGattCallback {
     fn export_for_rpc(self: Box<Self>) {
         let cr = self.dbus_crossroads.clone();
         export_bluetooth_gatt_callback_dbus_obj(
+            self.get_object_id(),
+            self.dbus_connection.clone(),
+            &mut cr.lock().unwrap(),
+            Arc::new(Mutex::new(self)),
+            Arc::new(Mutex::new(DisconnectWatcher::new())),
+        );
+    }
+}
+
+/// Callback container for suspend interface callbacks.
+pub(crate) struct SuspendCallback {
+    objpath: String,
+    context: Arc<Mutex<ClientContext>>,
+
+    dbus_connection: Arc<SyncConnection>,
+    dbus_crossroads: Arc<Mutex<Crossroads>>,
+}
+
+impl SuspendCallback {
+    pub(crate) fn new(
+        objpath: String,
+        context: Arc<Mutex<ClientContext>>,
+        dbus_connection: Arc<SyncConnection>,
+        dbus_crossroads: Arc<Mutex<Crossroads>>,
+    ) -> Self {
+        Self { objpath, context, dbus_connection, dbus_crossroads }
+    }
+}
+
+impl ISuspendCallback for SuspendCallback {
+    fn on_callback_registered(&self, _callback_id: u32) {}
+    fn on_suspend_ready(&self, _suspend_id: u32) {}
+    fn on_resumed(&self, _suspend_id: u32) {}
+}
+
+impl RPCProxy for SuspendCallback {
+    fn register_disconnect(&mut self, _f: Box<dyn Fn(u32) + Send>) -> u32 {
+        0
+    }
+
+    fn get_object_id(&self) -> String {
+        self.objpath.clone()
+    }
+
+    fn unregister(&mut self, _id: u32) -> bool {
+        false
+    }
+
+    fn export_for_rpc(self: Box<Self>) {
+        let cr = self.dbus_crossroads.clone();
+        export_suspend_callback_dbus_obj(
             self.get_object_id(),
             self.dbus_connection.clone(),
             &mut cr.lock().unwrap(),
