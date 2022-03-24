f5409520cae309597a05af23e9634ce6e8e8b331
Chienyuan <chienyuanhuang@google.com>
2022-03-24 20:31:24 +0800

    GD Scan: Support IRK for Address filter
    Tag: #feature
    Bug: 223823523
    Test: gd/cert/run --device --clean --sl4a LeAdvancedScanningTest
    Change-Id: Ia0f392197610d8e505309be65d6c5b41bd94b98c

--- system/gd/hci/le_scanning_manager.cc
+++ system/gd/hci/le_scanning_manager.cc
@@ -712,7 +712,7 @@ struct LeScanningManager::impl : public bluetooth::hci::LeAddressManagerCallback
 
       switch (filter.filter_type) {
         case ApcfFilterType::BROADCASTER_ADDRESS: {
-          update_address_filter(apcf_action, filter_index, filter.address, filter.application_address_type);
+          update_address_filter(apcf_action, filter_index, filter.address, filter.application_address_type, filter.irk);
           break;
         }
         case ApcfFilterType::SERVICE_UUID:
@@ -741,16 +741,34 @@ struct LeScanningManager::impl : public bluetooth::hci::LeAddressManagerCallback
   }
 
   void update_address_filter(
-      ApcfAction action, uint8_t filter_index, Address address, ApcfApplicationAddressType address_type) {
+      ApcfAction action,
+      uint8_t filter_index,
+      Address address,
+      ApcfApplicationAddressType address_type,
+      std::array<uint8_t, 16> irk) {
     if (action != ApcfAction::CLEAR) {
       le_scanning_interface_->EnqueueCommand(
           LeAdvFilterBroadcasterAddressBuilder::Create(action, filter_index, address, address_type),
           module_handler_->BindOnceOn(this, &impl::on_advertising_filter_complete));
+      if (!is_empty_128bit(irk)) {
+        std::array<uint8_t, 16> empty_irk;
+        le_address_manager_->AddDeviceToResolvingList(
+            static_cast<PeerAddressType>(address_type), address, irk, empty_irk);
+      }
     } else {
       le_scanning_interface_->EnqueueCommand(
           LeAdvFilterClearBroadcasterAddressBuilder::Create(filter_index),
           module_handler_->BindOnceOn(this, &impl::on_advertising_filter_complete));
     }
+  }
+
+  bool is_empty_128bit(const std::array<uint8_t, 16> data) {
+    for (int i = 0; i < 16; i++) {
+      if (data[i] != (uint8_t)0) {
+        return false;
+      }
+    }
+    return true;
   }
 
   void update_uuid_filter(
