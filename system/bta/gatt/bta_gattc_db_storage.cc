fe136e700631d3efd4220854f9a680c7994b1283
HsingYuan Lo <hylo@google.com>
2022-04-01 16:29:46 +0000

    Add hash API to manage persistent data
    Three APIs added
    1. bta_gattc_hash_load
    2. bta_gattc_hash_write
    3. bta_gattc_cache_link
    
    Bug: 154056389
    Test: build
    Tag: #feature
    Change-Id: I60d0b82be278ec514c4aa929f24b7c663cf0f4a2

--- system/bta/gatt/bta_gattc_db_storage.cc
+++ system/bta/gatt/bta_gattc_db_storage.cc
@@ -16,18 +16,35 @@
  *
  ******************************************************************************/
 
-#include "bta/gatt/bta_gattc_int.h"
+#include <base/strings/string_number_conversions.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#include "bta_gattc_int.h"
 
 using gatt::StoredAttribute;
 
 #define GATT_CACHE_PREFIX "/data/misc/bluetooth/gatt_cache_"
 #define GATT_CACHE_VERSION 6
 
+#define GATT_HASH_MAX_SIZE 30
+#define GATT_HASH_PATH_PREFIX "/data/misc/bluetooth/gatt_hash_"
+#define GATT_HASH_PATH "/data/misc/bluetooth"
+#define GATT_HASH_FILE_PREFIX "gatt_hash_"
+
+static void bta_gattc_hash_remove_least_recently_used_if_possible();
+
 static void bta_gattc_generate_cache_file_name(char* buffer, size_t buffer_len,
                                                const RawAddress& bda) {
   snprintf(buffer, buffer_len, "%s%02x%02x%02x%02x%02x%02x", GATT_CACHE_PREFIX,
            bda.address[0], bda.address[1], bda.address[2], bda.address[3],
            bda.address[4], bda.address[5]);
+}
+
+static void bta_gattc_generate_hash_file_name(char* buffer, size_t buffer_len,
+                                              const Octet16& hash) {
+  snprintf(buffer, buffer_len, "%s%s", GATT_HASH_PATH_PREFIX,
+           base::HexEncode(hash.data(), 16).c_str());
 }
 
 static gatt::Database EMPTY_DB;
@@ -107,6 +124,25 @@ gatt::Database bta_gattc_cache_load(const RawAddress& server_bda) {
   return bta_gattc_load_db(fname);
 }
 
+
+/*******************************************************************************
+ *
+ * Function         bta_gattc_hash_load
+ *
+ * Description      Load GATT cache from storage for server.
+ *
+ * Parameter        hash: 16-byte value
+ *
+ * Returns          non-empty GATT database on success, empty GATT database
+ *                  otherwise
+ *
+ ******************************************************************************/
+gatt::Database  bta_gattc_hash_load(const Octet16& hash) {
+  char fname[255] = {0};
+  bta_gattc_generate_hash_file_name(fname, sizeof(fname), hash);
+  return bta_gattc_load_db(fname);
+}
+
 /*******************************************************************************
  *
  * Function         bta_gattc_store_db
@@ -174,6 +210,52 @@ void bta_gattc_cache_write(const RawAddress& server_bda,
 
 /*******************************************************************************
  *
+ * Function         bta_gattc_cache_link
+ *
+ * Description      Link address-database file to hash-database file
+ *
+ * Parameter        server_bda: server bd address of this cache belongs to
+ *                  hash: 16-byte value
+ *
+ * Returns          true on success, false otherwise
+ *
+ ******************************************************************************/
+void bta_gattc_cache_link(const RawAddress& server_bda, const Octet16& hash) {
+  char addr_file[255] = {0};
+  char hash_file[255] = {0};
+  bta_gattc_generate_cache_file_name(addr_file, sizeof(addr_file), server_bda);
+  bta_gattc_generate_hash_file_name(hash_file, sizeof(hash_file), hash);
+
+  unlink(addr_file);  // remove addr file first if the file exists
+  int result = link(hash_file, addr_file);
+
+  LOG(INFO) << __func__ << ": link " << addr_file << " to " << hash_file
+            << ", result=" << result;
+}
+
+/*******************************************************************************
+ *
+ * Function         bta_gattc_hash_write
+ *
+ * Description      This callout function is executed by GATT when a server
+ *                  cache is available to save for specific hash.
+ *
+ * Parameter        hash: 16-byte value
+ *                  database: gatt::Database instance.
+ *
+ * Returns          true on success, false otherwise
+ *
+ ******************************************************************************/
+bool bta_gattc_hash_write(const Octet16& hash,
+                          const gatt::Database& database) {
+  char fname[255] = {0};
+  bta_gattc_generate_hash_file_name(fname, sizeof(fname), hash);
+  bta_gattc_hash_remove_least_recently_used_if_possible();
+  return bta_gattc_store_db(fname, database.Serialize());
+}
+
+/*******************************************************************************
+ *
  * Function         bta_gattc_cache_reset
  *
  * Description      This callout function is executed by GATTC to reset cache in
@@ -189,4 +271,87 @@ void bta_gattc_cache_reset(const RawAddress& server_bda) {
   char fname[255] = {0};
   bta_gattc_generate_cache_file_name(fname, sizeof(fname), server_bda);
   unlink(fname);
+}
+
+/*******************************************************************************
+ *
+ * Function         bta_gattc_hash_remove_least_recently_used_if_possible
+ *
+ * Description      When the max size reaches, find the oldest item and remove
+ *                  it if possible
+ *
+ * Parameter
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+static void bta_gattc_hash_remove_least_recently_used_if_possible() {
+  std::unique_ptr<DIR, decltype(&closedir)> dirp(opendir(GATT_HASH_PATH),
+                                                 &closedir);
+  if (dirp == nullptr) {
+    LOG(ERROR) << __func__ << ": open dir error, " << GATT_HASH_PATH;
+    return;
+  }
+
+  time_t lru_time = time(NULL);
+  size_t count = 0;
+  char to_be_removed[255] = {0};
+
+#if (BTA_GATT_DEBUG == TRUE)
+  LOG(INFO) << "<-----------Start Local Hash Cache---------->";
+#endif
+
+  dirent* dp;
+  while ((dp = readdir(dirp.get())) != nullptr) {
+    if (strcmp(".", dp->d_name) == 0 || strcmp("..", dp->d_name) == 0) {
+      continue;
+    }
+
+    // pattern match: gatt_hash_
+    size_t fname_len = strlen(dp->d_name);
+    size_t pattern_len = strlen(GATT_HASH_FILE_PREFIX);
+    if (pattern_len > fname_len) {
+      continue;
+    }
+
+    char tmp[255] = {0};
+    strncpy(tmp, dp->d_name, pattern_len);
+    if (strcmp(tmp, GATT_HASH_FILE_PREFIX) != 0) {
+      continue;
+    }
+
+    // increase hash file count
+    count++;
+
+    // check hard link count of the file
+    struct stat buf;
+    snprintf(tmp, 255, "%s/%s", GATT_HASH_PATH, dp->d_name);
+
+#if (BTA_GATT_DEBUG == TRUE)
+    int result = lstat(tmp, &buf);
+    LOG(INFO) << "name=" << dp->d_name << ", result=" << result
+              << ", linknum=" << buf.st_nlink << ", mtime=" << buf.st_mtime;
+#else
+    lstat(tmp, &buf);
+#endif
+
+    // if hard link count of the file is 1, it means no trusted device links to
+    // the inode. It is safe to be a candidate to be removed
+    if (buf.st_nlink == 1 && buf.st_mtime < lru_time) {
+      lru_time = buf.st_mtime;
+      snprintf(to_be_removed, 255, "%s/%s", GATT_HASH_PATH, dp->d_name);
+    }
+  }
+
+#if (BTA_GATT_DEBUG == TRUE)
+  LOG(INFO) << "<-----------End Local Hash Cache------------>";
+#endif
+
+  // if the number of hash files exceeds the limit, remove the oldest item.
+  // There is one exception, that is, if all hash files are linked by trusted
+  // devices, then nothing can be removed
+  if (count > GATT_HASH_MAX_SIZE && strcmp(to_be_removed, "") != 0) {
+    unlink(to_be_removed);
+    LOG(INFO) << __func__ << ": remove " << to_be_removed;
+  }
 }
