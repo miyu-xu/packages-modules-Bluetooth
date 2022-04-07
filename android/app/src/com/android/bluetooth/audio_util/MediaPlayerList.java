d137e8b31dd6b9665b1d6562d102dc0bf3ea4c15
Ted Wang <tedwang@google.com>
2022-04-07 11:08:25 +0800

    Set active player to MediaKeyEventSession when init
    Active player of MediaPlayerList should be set to player get from
    MediaSessionManager instead of pending callback from Media framework
    when init MediaPlayerList.
    
    Bug: 209731732
    Test: Manual
    
    Change-Id: I4269f7b0bbde97358c80697f8bbe9e515a6f0148

--- android/app/src/com/android/bluetooth/audio_util/MediaPlayerList.java
+++ android/app/src/com/android/bluetooth/audio_util/MediaPlayerList.java
@@ -196,7 +196,14 @@ public class MediaPlayerList {
 
                 // If there were any active players and we don't already have one due to the Media
                 // Framework Callbacks then set the highest priority one to active
-                if (mActivePlayerId == 0 && mMediaPlayers.size() > 0) setActivePlayer(1);
+                if (mActivePlayerId == 0 && mMediaPlayers.size() > 0) {
+                    String packageName = mMediaSessionManager.getMediaKeyEventSessionPackageName();
+                    if (!TextUtils.isEmpty(packageName)) {
+                        setActivePlayer(mMediaPlayerIds.get(packageName));
+                    } else {
+                        setActivePlayer(1);
+                    }
+                }
             });
     }
 
