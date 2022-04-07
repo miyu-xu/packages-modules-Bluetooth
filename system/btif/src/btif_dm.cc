d3d5b4beb0eedc16f19091ae3afd13ff298ac0d6
Martin Brabham <optedoblivion@google.com>
2022-04-07 16:16:19 +0000

    Floss: ClearFilterAcceptList BTIF DM API
    Bug: 224603601
    Test: mma -j $(nproc)
    Test: ./build.py
    Tag: #floss
    Change-Id: I206102f246c881e92c7b7ddb02ce37ac420ee710

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -3234,3 +3234,8 @@ void btif_dm_clear_event_mask() {
   LOG_VERBOSE("%s: called", __func__);
   bta_dm_clear_event_mask();
 }
+
+void btif_dm_clear_filter_accept_list() {
+  LOG_VERBOSE("%s: called", __func__);
+  bta_dm_clear_filter_accept_list();
+}
