923e85232ea16d0de7daedcc926c08f3e9b36f5f
Martin Geisler <mgeisler@google.com>
2022-04-13 12:38:39 +0200

    PDL: add regression tests for `pdl` output
    These new tests compare the output of `pdl` with the output of
    `bluetooth_packetgen` and ensures that we match this precisely (after
    formatting with ‘rustfmt’).
    
    We will later decouple the two generators, but for now, it’s useful to
    generate identical output.
    
    Bug: 228307941
    Test: atest pdl_tests
    Change-Id: Ie45ddd9fceeeefa9ad25c649bfc4db903f9fe1c2

--- tools/pdl/src/generator.rs
+++ tools/pdl/src/generator.rs
@@ -343,6 +343,10 @@ fn generate_packet_decl(
     code.push_str(&quote_block! {
         impl #data_name {
             fn conforms(bytes: &[u8]) -> bool {
+                // TODO(mgeisler): return Boolean expression directly.
+                if bytes.len() < #total_field_size {
+                    return false;
+                }
                 true
             }
 
@@ -500,7 +504,19 @@ pub fn generate_rust(sources: &ast::SourceDatabase, grammar: &ast::Grammar) -> R
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_utils::{assert_eq_with_diff, parse_str, rustfmt};
+    use crate::ast;
+    use crate::parser::parse_inline;
+    use crate::test_utils::{assert_eq_with_diff, rustfmt};
+
+    /// Parse a string fragment as a PDL file.
+    ///
+    /// # Panics
+    ///
+    /// Panics on parse errors.
+    pub fn parse_str(text: &str) -> ast::Grammar {
+        let mut db = ast::SourceDatabase::new();
+        parse_inline(&mut db, String::from("stdin"), String::from(text)).expect("parse error")
+    }
 
     #[test]
     fn test_generate_preamble() {
