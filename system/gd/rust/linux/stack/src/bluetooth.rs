ec6ca56427d195ac604eb874063ac53c4513e599
Ying Hsu <yinghsu@google.com>
2022-04-16 16:52:03 +0000

    Floss: Fixing classic keyboard SDP and connection
    If service discovery is ongoing, the connect_all_enabled_profiles()
    might not have any profile to connect. In that case, we will retry
    again once the UUIDs from a remote device are available.
    Besides, we also remove the cancel_discovery() from
    connect_all_enabled_profiles() as it could interrupt the service discovery
    once a device is bonded.
    
    Bug: 219021367
    Tag: #floss
    Test: manual test on BR/EDR keyboards and mouse
    Change-Id: Ie0445520cfed9b5fa821a08f116df16ea6e45073

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -150,10 +150,10 @@ pub trait IBluetooth {
     fn sdp_search(&self, device: BluetoothDevice, uuid: Uuid128Bit) -> bool;
 
     /// Connect all profiles supported by device and enabled on adapter.
-    fn connect_all_enabled_profiles(&self, device: BluetoothDevice) -> bool;
+    fn connect_all_enabled_profiles(&mut self, device: BluetoothDevice) -> bool;
 
     /// Disconnect all profiles supported by device and enabled on adapter.
-    fn disconnect_all_enabled_profiles(&self, device: BluetoothDevice) -> bool;
+    fn disconnect_all_enabled_profiles(&mut self, device: BluetoothDevice) -> bool;
 }
 
 /// Serializable device used in various apis.
@@ -278,6 +278,7 @@ pub struct Bluetooth {
     tx: Sender<Message>,
     uuid_helper: UuidHelper,
     is_connectable: bool,
+    wait_to_connect: bool,
 }
 
 impl Bluetooth {
@@ -305,6 +306,7 @@ impl Bluetooth {
             tx,
             uuid_helper: UuidHelper::new(),
             is_connectable: false,
+            wait_to_connect: false,
         }
     }
 
@@ -699,9 +701,16 @@ impl BtifBluetoothCallbacks for Bluetooth {
             );
 
             self.found_devices.get_mut(&address)
-        };
+        }
+        .unwrap();
 
-        device.unwrap().update_properties(properties);
+        device.update_properties(properties);
+
+        let info = device.info.clone();
+        let uuids = self.get_remote_uuids(info.clone());
+        if self.wait_to_connect && uuids.len() > 0 {
+            self.connect_all_enabled_profiles(info);
+        }
     }
 
     fn acl_state(
@@ -1185,7 +1194,7 @@ impl IBluetooth for Bluetooth {
         self.sdp.as_ref().unwrap().sdp_search(&mut addr.unwrap(), &uu) == BtStatus::Success
     }
 
-    fn connect_all_enabled_profiles(&self, device: BluetoothDevice) -> bool {
+    fn connect_all_enabled_profiles(&mut self, device: BluetoothDevice) -> bool {
         // Profile init must be complete before this api is callable
         if !self.profiles_ready {
             return false;
@@ -1197,10 +1206,8 @@ impl IBluetooth for Bluetooth {
             return false;
         }
 
-        // BREDR connection won't work when Inquiry is in progress.
-        self.cancel_discovery();
-
         // Check all remote uuids to see if they match enabled profiles and connect them.
+        let mut has_enabled_uuids = false;
         let uuids = self.get_remote_uuids(device.clone());
         for uuid in uuids.iter() {
             match self.uuid_helper.is_known_profile(uuid) {
@@ -1224,15 +1231,21 @@ impl IBluetooth for Bluetooth {
                             _ => (),
                         }
                     }
+                    has_enabled_uuids = true;
                 }
                 _ => {}
             }
         }
 
+        if !self.wait_to_connect && !has_enabled_uuids {
+            self.wait_to_connect = true;
+        }
         return true;
     }
 
-    fn disconnect_all_enabled_profiles(&self, device: BluetoothDevice) -> bool {
+    fn disconnect_all_enabled_profiles(&mut self, device: BluetoothDevice) -> bool {
+        self.wait_to_connect = false;
+
         if !self.profiles_ready {
             return false;
         }
@@ -1271,7 +1284,6 @@ impl IBluetooth for Bluetooth {
                 _ => {}
             }
         }
-
         return true;
     }
 }
