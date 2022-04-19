817736480eec5b91591808ec7f663ef8967715f2
Josh Wu <joshwu@google.com>
2022-04-19 07:18:36 -0700

    Add LeAudioMetrics class
    Tag: #feature
    Bug: 207811438
    Test: atest BluetoothInstrumentationTests
    Change-Id: I65d4cb72efdb491f06ed2352b72b62a49b66b44d

---
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioMetrics.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Defines the native interface that is used by state machine/service to
+ * send or receive messages from the native stack. This file is registered
+ * for the native methods in the corresponding JNI C++ file.
+ */
+package com.android.bluetooth.le_audio;
+
+/**
+ * Collecting data for LeAudio Metrics, assembling them and send to Statsd.
+ * @hide
+ */
+public class LeAudioMetrics {
+
+}
