85b65d1958051c6ba0584b511d73d0d13f54d96b
Jakub Tyszkowski <jakub.tyszkowski@codecoup.pl>
2022-04-25 18:36:04 +0200

    BassClient: Add initial support for group operations
    This is the simplest group operation mechanism.
    
    More sofisticated mechanism should to be added on top of this,
    to support self-aligning in case of diverging group members states
    (connection and broadcast sync. states).
    
    Bug: 150670922
    Bug: 230559809
    Tag: #feature
    Sponsor: jpawlowski@
    Test: atest BluetoothInstrumentationTests
    Change-Id: Ia1d38777e08851c8c0ed49687d11dfbdd701b918

--- android/app/src/com/android/bluetooth/csip/CsipSetCoordinatorService.java
+++ android/app/src/com/android/bluetooth/csip/CsipSetCoordinatorService.java
@@ -51,6 +51,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.modules.utils.SynchronousResultReceiver;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -591,6 +592,23 @@ public class CsipSetCoordinatorService extends ProfileService {
                 .filter(e -> e.getValue().contains(groupId))
                 .map(e -> e.getKey())
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * Get grouped devices
+     * @param device group member device
+     * @param uuid
+     * @return related list of devices.
+     */
+    public @NonNull List<BluetoothDevice> getGroupDevices(BluetoothDevice device, ParcelUuid uuid) {
+        List<Integer> groupIds = getAllGroupIds(uuid);
+        for (Integer id : groupIds) {
+            List<BluetoothDevice> devices = getGroupDevices(id);
+            if (devices.contains(device)) {
+                return devices;
+            }
+        }
+        return Collections.emptyList();
     }
 
     /**
