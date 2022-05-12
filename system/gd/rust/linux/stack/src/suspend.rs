81fa5e3c27a92d38417316850fad3cb243d2eb14
Martin Brabham <optedoblivion@google.com>
2022-05-12 19:04:12 +0000

    Floss: Revise wakeful non wakeful flow.
    Bug: 232547719
    Test: ./build.py
    Test: mma -j $(nproc)
    Test: system/gd/cert/run --clean --topshim SuspendTest
    Tag: #floss
    Change-Id: I9ecea25a1b44bb446759cee6220916ef4f9b36f8

--- system/gd/rust/linux/stack/src/suspend.rs
+++ system/gd/rust/linux/stack/src/suspend.rs
@@ -175,12 +175,12 @@ impl ISuspend for Suspend {
 
     fn resume(&self) -> bool {
         let suspend_id = 1;
-        self.intf.lock().unwrap().set_event_filter_inquiry_result_all_devices();
         self.intf.lock().unwrap().set_default_event_mask();
+        self.intf.lock().unwrap().set_event_filter_inquiry_result_all_devices();
         if self.is_connected_suspend {
             if self.was_a2dp_connected {
                 // TODO(230604670): self.intf.lock().unwrap().restore_filter_accept_list();
-                // TODO(230604670): reconnect to a2dp device if connected before
+                // TODO(230604670): reconnect to a2dp device
             }
             // TODO(224603198): start all advertising again
         }
