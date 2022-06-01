1fcd97dded99a72c9154ff943959c022753d8892
Md Shahriar Hossain Sajib <sajibonly@google.com>
2022-06-01 16:01:43 +0800

    Stop btif queue to get stuck on the same node
    If something goes wrong during processing a hf client connect request,
    the request node is creating a deadlock for other requests, blocking the
    queue forever. Releasing the blocking node if something goes wrong.
    Cherry picked from wear OS
    
    Bug: 263323082
    Bug: 232734797
    Test: manual
    Change-Id: If24762367a3ba18f14b54c24b5cf7bbe71de62e3
    (cherry picked from commit 05d6df87ff6164273be2c0df2e25f2515663a356)

--- system/test/mock/mock_bta_hf_client_api.cc
+++ system/test/mock/mock_bta_hf_client_api.cc
@@ -51,7 +51,7 @@ void BTA_HfClientAudioOpen(uint16_t handle) {
 void BTA_HfClientClose(uint16_t handle) { mock_function_count_map[__func__]++; }
 void BTA_HfClientDisable(void) { mock_function_count_map[__func__]++; }
 void BTA_HfClientDumpStatistics(int fd) { mock_function_count_map[__func__]++; }
-void BTA_HfClientOpen(const RawAddress& bd_addr, uint16_t* p_handle) {
+bt_status_t BTA_HfClientOpen(const RawAddress& bd_addr, uint16_t* p_handle) {
   mock_function_count_map[__func__]++;
 }
 void BTA_HfClientSendAT(uint16_t handle, tBTA_HF_CLIENT_AT_CMD_TYPE at,
