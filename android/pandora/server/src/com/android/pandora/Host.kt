e83980589af3ad4f40c7caa689bb379b0f631263
Charlie Boutier <charliebout@google.com>
2022-06-17 21:19:42 +0000

    [Pandora] Host - Implement Readpasskey
    Add Readpasskey and WritePasskey to host.proto
    Implement Readpasskey
    
    Test: dut2ref
    
    Change-Id: I5d9f7e09c64b367ee3455d4ec504a25bca23af20

--- android/pandora/server/src/com/android/pandora/Host.kt
+++ android/pandora/server/src/com/android/pandora/Host.kt
@@ -210,4 +210,25 @@ class Host(private val context: Context, private val server: Server) : HostImplB
       DisconnectResponse.getDefaultInstance()
     }
   }
+
+  override fun readPasskey(
+    request: ReadPasskeyRequest,
+    responseObserver: StreamObserver<ReadPasskeyResponse>
+  ) {
+    grpcUnary<ReadPasskeyResponse>(scope, responseObserver) {
+      Log.i(TAG, "readPasskey")
+      val pairingRequestIntent =
+        flow
+          .filter { it.getAction() == BluetoothDevice.ACTION_PAIRING_REQUEST }
+          .first()
+      val bluetoothDevice = pairingRequestIntent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+      val pairingVariant = pairingRequestIntent.getIntExtra(BluetoothDevice.EXTRA_PAIRING_VARIANT, BluetoothDevice.ERROR)
+      if (pairingVariant == BluetoothDevice.PAIRING_VARIANT_CONSENT) {
+        bluetoothDevice.setPairingConfirmation(true)
+      }
+      val passkey = pairingRequestIntent.getIntExtra(BluetoothDevice.EXTRA_PAIRING_KEY, BluetoothDevice.ERROR)
+      Log.i(TAG, "passkey: $passkey")
+      ReadPasskeyResponse.newBuilder().setPasskey(passkey).build()
+    }
+  }
 }
