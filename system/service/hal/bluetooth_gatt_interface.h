85c7d15e05117cf3b7bb022a12e6522fbaa837bb
Łukasz Rymanowski <rlukasz@google.com>
2022-06-24 13:31:15 +0000

    gatt: Fix incorrect write to the descriptor.
    This patch fixes regression after
    https://android-review.googlesource.com/q/topic:gatt-racecondition-fix
    
    In case of GATT Write error to descriptor or characteristic,
    the value which was tried to be written is zeroed in the
    onWriteDescriptor/Characteristic callback.
    This cause an issue in following use case:
    
    1) Application writes descriptor on unencrypted link to characteristic
    which requires authentication
    2) Remote device response with an error insufficient authenthication
    3) Android repeats write descriptor with new authenthication
    requrements with the descriptor value taken from the onWriteDescriptor
    callbac - the one which was zeroed. This result in try to write
    invalid value to the descriptor
    
    This patch make sure that value in the callback is always the one which
    was tried to write, no matter of status.
    
    Bug: 235756799
    Test: atest BluetoothInstrumentationTests
    Tag: #stability
    Change-Id: Icd18b95a3c44c082117d035cdf25961938de829d

--- system/service/hal/bluetooth_gatt_interface.h
+++ system/service/hal/bluetooth_gatt_interface.h
@@ -90,13 +90,13 @@ class BluetoothGattInterface {
 
     virtual void WriteCharacteristicCallback(BluetoothGattInterface* gatt_iface,
                                              int conn_id, int status,
-                                             uint16_t handle, uint16_t len,
-                                             const uint8_t* value);
+                                             uint16_t handle,
+                                             const std::vector<uint8_t> value);
 
     virtual void WriteDescriptorCallback(BluetoothGattInterface* gatt_iface,
                                          int conn_id, int status,
-                                         uint16_t handle, uint16_t len,
-                                         const uint8_t* value);
+                                         uint16_t handle,
+                                         const std::vector<uint8_t> value);
 
     virtual void MtuChangedCallback(BluetoothGattInterface* gatt_iface,
                                     int conn_id, int status, int mtu);
