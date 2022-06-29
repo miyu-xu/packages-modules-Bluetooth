9eeb0f6f41d0cbb8c8c5d8692aa85ad7920b46b5
Łukasz Rymanowski <rlukasz@google.com>
2022-06-29 21:57:00 +0000

    eatt: Disconnect EATT channel on GATT timeout
    When there is an operation on the EATT channel and there is gatt
    timeout, lets disconnect this channel
    
    Bug: 236083555
    Test: atest BluetoothInstrumentationTests
    Test: GATT/CL/GAT/BV-03-C
    Tag: #stability
    Change-Id: I02d5d538af801a4118d293f8244445e8b34a2d7a

--- system/stack/gatt/gatt_utils.cc
+++ system/stack/gatt/gatt_utils.cc
@@ -664,8 +664,17 @@ void gatt_rsp_timeout(void* data) {
     }
   }
 
-  LOG(WARNING) << __func__ << " disconnecting...";
-  gatt_disconnect(p_clcb->p_tcb);
+  auto eatt_channel = EattExtension::GetInstance()->FindEattChannelByCid(
+      p_clcb->p_tcb->peer_bda, p_clcb->cid);
+  if (eatt_channel) {
+    LOG(WARNING) << __func__ << " disconnecting EATT cid: " << p_clcb->cid;
+    EattExtension::GetInstance()->Disconnect(p_clcb->p_tcb->peer_bda,
+                                             p_clcb->cid);
+  } else {
+    LOG(WARNING) << __func__ << " disconnecting GATT...";
+    gatt_disconnect(p_clcb->p_tcb);
+    return;
+  }
 }
 
 extern void gatts_proc_srv_chg_ind_ack(tGATT_TCB tcb);
