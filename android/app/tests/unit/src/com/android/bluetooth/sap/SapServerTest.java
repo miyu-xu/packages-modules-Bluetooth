9187f0fb6c7989112542f37c0e3443a69cca6701
Kyunglyul Hyun <klhyun@google.com>
2022-07-02 02:56:10 +0900

    Add test for SapServer
    Bug: 237708568
    Test: atest BluetoothInstrumentationTests
    
    Change-Id: I3d95be363d0b8b237dc2247c1326d9824ea3ef3c

---
+++ android/app/tests/unit/src/com/android/bluetooth/sap/SapServerTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.sap;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Handler;
+
+import androidx.test.InstrumentationRegistry;
+
+import com.android.bluetooth.TestUtils;
+import com.android.bluetooth.btservice.AdapterService;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@RunWith(JUnit4.class)
+public class SapServerTest {
+    @Rule
+    public final MockitoRule mockito = MockitoJUnit.rule();
+    private Context mTargetContext;
+    private Context mContext;
+    private BluetoothAdapter mAdapter;
+    private BluetoothDevice mTestDevice;
+    @Mock private AdapterService mAdapterService;
+    @Mock private SapService mSapService;
+    @Mock private Handler mHandler;
+    @Mock private InputStream mInputStream;
+    @Mock private OutputStream mOutputStream;
+    @Mock private NotificationManager mNotificationManager;
+    private SapServer mSapServer;
+
+    @Before
+    public void setUp() throws Exception {
+        mTargetContext = InstrumentationRegistry.getTargetContext();
+        TestUtils.setAdapterService(mAdapterService);
+
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mContext = spy(new ContextWrapper(mTargetContext));
+
+        doReturn(Context.NOTIFICATION_SERVICE).when(mContext).getSystemServiceName(NotificationManager.class);
+        doReturn(mNotificationManager).when(mContext).getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mSapServer = new SapServer(mHandler, mContext, mInputStream, mOutputStream);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        TestUtils.clearAdapterService(mAdapterService);
+    }
+
+    @Test
+    public void testSetTestMode() {
+        mSapServer.setTestMode(SapMessage.TEST_MODE_ENABLE);
+        mSapServer.setTestMode(SapMessage.TEST_MODE_DISABLE);
+    }
+
+    @Test
+    public void testClearNotification() {
+        mSapServer.clearNotification();
+
+        verify(mNotificationManager).cancel(SapServer.NOTIFICATION_ID);
+    }
+
+    @Test
+    public void testSetNotification() {
+        int type = SapMessage.DISC_GRACEFULL;
+        int flag = 0;
+
+        mSapServer.setNotification(type, flag);
+
+        verify(mNotificationManager).createNotificationChannel(any(NotificationChannel.class));
+        verify(mNotificationManager).notify(eq(SapServer.NOTIFICATION_ID), any(Notification.class));
+    }
+
+    @Test
+    public void testClosedInputStream() throws Exception{
+        doReturn(-1).when(mInputStream).read();
+        mSapServer.start();
+
+        mSapServer.join();
+        verify(mInputStream).close();
+    }
+}
