912d4689a5824cbb70a73e424af2297473b5c5cc
Josh Wu <joshwu@google.com>
2022-07-06 17:53:57 +0800

    RootCanal: Authentication challenge
    * Move link_key_notification to pairing because they are not a part of
      pairing and pairing info is required to determin key type later.
    * Generate Sres code with AES-128-ECB
    * Note that this is not totally matches spec
    
    Tag: #feature
    Test: atest liblmp_tests --host
    Bug: 235777894
    Change-Id: I67d0c5966965be785c6cbc87441648257d3b24d7

--- tools/rootcanal/lmp/src/procedure/authentication.rs
+++ tools/rootcanal/lmp/src/procedure/authentication.rs
@@ -1,6 +1,8 @@
 // Bluetooth Core, Vol 2, Part C, 4.2.1
 
 use num_traits::ToPrimitive;
+use openssl::symm::{encrypt, Cipher};
+use std::convert::TryInto;
 
 use crate::either::Either;
 use crate::num_hci_command_packets;
@@ -9,6 +11,11 @@ use crate::procedure::features;
 use crate::procedure::legacy_pairing;
 use crate::procedure::secure_simple_pairing;
 use crate::procedure::Context;
+
+pub fn generate_sres(random_number: [u8; 16], key: [u8; 16]) -> [u8; 4] {
+    let cipher = Cipher::aes_128_ecb();
+    encrypt(cipher, &key, None, &random_number).unwrap()[0..4].try_into().unwrap()
+}
 
 async fn secure_simple_pairing_supported(ctx: &impl Context) -> bool {
     let ssp_bit = hci::LMPFeaturesPage1Bits::SecureSimplePairingHostSupport.to_u64().unwrap();
@@ -25,9 +32,66 @@ async fn secure_simple_pairing_supported(ctx: &impl Context) -> bool {
     local_supported && peer_supported.await
 }
 
-pub async fn send_authentication_challenge(ctx: &impl Context, transaction_id: u8) {
-    ctx.send_lmp_packet(lmp::AuRandBuilder { transaction_id, random_number: [0; 16] }.build());
-    let _ = ctx.receive_lmp_packet::<lmp::SresPacket>().await;
+pub async fn send_challenge(
+    ctx: &impl Context,
+    transaction_id: u8,
+    link_key: [u8; 16],
+) -> Result<(), ()> {
+    let random_number = [0; 16];
+    ctx.send_lmp_packet(lmp::AuRandBuilder { transaction_id, random_number }.build());
+
+    match ctx.receive_lmp_packet::<Either<lmp::SresPacket, lmp::NotAcceptedPacket>>().await {
+        Either::Left(response) => {
+            if *response.get_authentication_rsp() == generate_sres(random_number, link_key) {
+                Ok(())
+            } else {
+                Err(())
+            }
+        }
+        Either::Right(_) => Err(()),
+    }
+}
+
+pub async fn receive_challenge(ctx: &impl Context, link_key: [u8; 16]) {
+    let random_number = *ctx.receive_lmp_packet::<lmp::AuRandPacket>().await.get_random_number();
+    ctx.send_lmp_packet(
+        lmp::SresBuilder {
+            transaction_id: 0,
+            authentication_rsp: generate_sres(random_number, link_key),
+        }
+        .build(),
+    );
+}
+
+pub async fn request_link_key_from_host(ctx: &impl Context) -> Option<[u8; 16]> {
+    ctx.send_hci_event(hci::LinkKeyRequestBuilder { bd_addr: ctx.peer_address() }.build());
+    match ctx.receive_hci_command::<Either<
+        hci::LinkKeyRequestReplyPacket,
+        hci::LinkKeyRequestNegativeReplyPacket,
+    >>().await {
+        Either::Left(reply) => {
+            ctx.send_hci_event(
+                hci::LinkKeyRequestReplyCompleteBuilder {
+                    num_hci_command_packets,
+                    status: hci::ErrorCode::Success,
+                    bd_addr: ctx.peer_address(),
+                }
+                .build(),
+            );
+            Some(*reply.get_link_key())
+        },
+        Either::Right(_) => {
+            ctx.send_hci_event(
+                hci::LinkKeyRequestNegativeReplyCompleteBuilder {
+                    num_hci_command_packets,
+                    status: hci::ErrorCode::Success,
+                    bd_addr: ctx.peer_address(),
+                }
+                .build(),
+            );
+            None
+        }
+    }
 }
 
 pub async fn initiate(ctx: &impl Context) {
@@ -40,80 +104,25 @@ pub async fn initiate(ctx: &impl Context) {
         .build(),
     );
 
-    ctx.send_hci_event(hci::LinkKeyRequestBuilder { bd_addr: ctx.peer_address() }.build());
-
-    let pairing = match ctx.receive_hci_command::<Either<
-        hci::LinkKeyRequestReplyPacket,
-        hci::LinkKeyRequestNegativeReplyPacket,
-    >>().await {
-        Either::Left(_reply) => {
-            ctx.send_hci_event(
-                hci::LinkKeyRequestReplyCompleteBuilder {
-                    num_hci_command_packets,
-                    status: hci::ErrorCode::Success,
-                    bd_addr: ctx.peer_address(),
-                }
-                .build(),
-            );
-            false
-        },
-        Either::Right(_) => {
-            ctx.send_hci_event(
-                hci::LinkKeyRequestNegativeReplyCompleteBuilder {
-                    num_hci_command_packets,
-                    status: hci::ErrorCode::Success,
-                    bd_addr: ctx.peer_address(),
-                }
-                .build(),
-            );
-
-            let result = if secure_simple_pairing_supported(ctx).await {
+    let result = match request_link_key_from_host(ctx).await {
+        Some(link_key) => send_challenge(ctx, 0, link_key).await,
+        None => {
+            // No link key, start pairing
+            if secure_simple_pairing_supported(ctx).await {
                 secure_simple_pairing::initiate(ctx).await
             } else {
                 legacy_pairing::initiate(ctx).await
-            };
-
-            if result.is_err() {
-                ctx.send_hci_event(
-                    hci::AuthenticationCompleteBuilder {
-                        status: hci::ErrorCode::AuthenticationFailure,
-                        connection_handle: ctx.peer_handle(),
-                    }
-                    .build(),
-                );
-                return;
             }
-            true
         }
     };
 
-    send_authentication_challenge(ctx, 0).await;
-
-    // Link Key Calculation
-    if pairing {
-        let _random_number = ctx.receive_lmp_packet::<lmp::AuRandPacket>().await;
-
-        // TODO: Resolve authentication challenge
-        ctx.send_lmp_packet(
-            lmp::SresBuilder { transaction_id: 0, authentication_rsp: [0; 4] }.build(),
-        );
-
-        ctx.send_hci_event(
-            hci::LinkKeyNotificationBuilder {
-                bd_addr: ctx.peer_address(),
-                key_type: hci::KeyType::AuthenticatedP192,
-                link_key: [0; 16],
-            }
-            .build(),
-        );
-    }
+    let status = match result {
+        Ok(_) => hci::ErrorCode::Success,
+        Err(_) => hci::ErrorCode::AuthenticationFailure,
+    };
 
     ctx.send_hci_event(
-        hci::AuthenticationCompleteBuilder {
-            status: hci::ErrorCode::Success,
-            connection_handle: ctx.peer_handle(),
-        }
-        .build(),
+        hci::AuthenticationCompleteBuilder { status, connection_handle: ctx.peer_handle() }.build(),
     );
 }
 
@@ -124,10 +133,16 @@ pub async fn respond(ctx: &impl Context) {
     >>()
     .await
     {
-        Either::Left(_random_number) => {
-            // TODO: Resolve authentication challenge
-            // TODO: Ask for link key
-            ctx.send_lmp_packet(lmp::SresBuilder { transaction_id: 0, authentication_rsp: [0; 4] }.build());
+        Either::Left(au_rand) => {
+            match request_link_key_from_host(ctx).await {
+                Some(link_key) => {
+                    let remote_random_number = *au_rand.get_random_number();
+                    ctx.send_lmp_packet(lmp::SresBuilder { transaction_id: 0, authentication_rsp: generate_sres(remote_random_number, link_key) }.build());
+                },
+                None => {
+                    ctx.send_lmp_packet(lmp::NotAcceptedBuilder { transaction_id: 0, not_accepted_opcode: lmp::Opcode::AuRand, error_code: hci::ErrorCode::PinOrKeyMissing.to_u8().unwrap() }.build());
+                }
+            }
         },
         Either::Right(pairing) => {
             let result = match pairing {
@@ -137,26 +152,12 @@ pub async fn respond(ctx: &impl Context) {
                     legacy_pairing::respond(ctx, in_rand).await,
             };
 
+            // Result must be used
             if result.is_err() {
-                return;
+                println!("Authentication failure!");
             }
 
-            // Link Key Calculation
-
-            let _random_number = ctx.receive_lmp_packet::<lmp::AuRandPacket>().await;
-            // TODO: Resolve authentication challenge
-            ctx.send_lmp_packet(lmp::SresBuilder { transaction_id: 0, authentication_rsp: [0; 4] }.build());
-
-            send_authentication_challenge(ctx, 0).await;
-
-            ctx.send_hci_event(
-                hci::LinkKeyNotificationBuilder {
-                    bd_addr: ctx.peer_address(),
-                    key_type: hci::KeyType::AuthenticatedP192,
-                    link_key: [0; 16],
-                }
-                .build(),
-            );
+            // TODO: Handle error
         }
     }
 }
