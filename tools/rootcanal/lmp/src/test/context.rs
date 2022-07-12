5d4b52929ed76ffa78ce73b3d07da3274a67ebb5
Josh Wu <joshwu@google.com>
2022-07-12 17:06:56 +0800

    RootCanal: Implement ECDH for BR/EDR
    Tag: #feature
    Bug: 235777894
    Test: atest liblmp_tests
    Change-Id: Iee36b24fd5ea0680e5767c3bbf1e92d1b2d852a1

--- tools/rootcanal/lmp/src/test/context.rs
+++ tools/rootcanal/lmp/src/test/context.rs
@@ -1,3 +1,6 @@
+use num_bigint::BigInt;
+use num_traits::One;
+
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::convert::{TryFrom, TryInto};
@@ -78,6 +81,16 @@ impl Context for TestContext {
         } else {
             0
         }
+    }
+
+    fn generate_random_bytes(&self, length: usize) -> Vec<u8> {
+        let mut buf = Vec::new();
+        buf.resize(length, 0);
+        buf
+    }
+
+    fn generate_private_key(&self, _key_size: usize) -> BigInt {
+        BigInt::one()
     }
 }
 
