08715207d850c554764ef2e0d8fea9d5350ef247
Jesse Melhuish <melhuishj@chromium.org>
2022-07-13 15:54:12 -0500

    DO NOT SUBMIT
    Change-Id: Ib4874815cc4736711d89d027eec09fc63a01c564

---
+++ system/gd/rust/linux/stack/src/battery_provider_manager.rs
@@ -0,0 +1,55 @@
+use log::debug;
+
+#[derive(Debug, Default, Clone)]
+pub struct BatteryProvider {
+    pub source_info: String,
+    pub remote_address: String,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Battery {
+    pub percentage: i32,
+    pub source_info: String,
+    pub variant: String,
+}
+
+pub trait IBatteryProviderCallback {
+    fn refresh_battery_info(&self);
+}
+
+pub trait IBatteryProviderManager {
+    fn register_battery_provider(
+        &mut self,
+        battery_provider: BatteryProvider,
+        battery_provider_callback: Box<dyn IBatteryProviderCallback + Send>,
+    ) -> i32;
+    fn unregister_battery_provider(&mut self, battery_id: i32);
+    fn set_battery_percentage(&mut self, battery_id: i32, battery: Battery);
+}
+
+pub struct BatteryProviderManager {}
+
+impl BatteryProviderManager {
+    pub fn new() -> BatteryProviderManager {
+        BatteryProviderManager {}
+    }
+}
+
+impl IBatteryProviderManager for BatteryProviderManager {
+    fn register_battery_provider(
+        &mut self,
+        battery_provider: BatteryProvider,
+        battery_provider_callback: Box<dyn IBatteryProviderCallback + Send>,
+    ) -> i32 {
+        debug!("Registered /org/chromium/bluetooth/hci");
+        213423
+    }
+
+    fn unregister_battery_provider(&mut self, battery_id: i32) {
+        todo!()
+    }
+
+    fn set_battery_percentage(&mut self, battery_id: i32, battery: Battery) {
+        todo!()
+    }
+}
--- system/gd/rust/linux/stack/src/lib.rs
+++ system/gd/rust/linux/stack/src/lib.rs
@@ -6,10 +6,12 @@
 #[macro_use]
 extern crate num_derive;
 
+pub mod battery_provider_manager;
 pub mod bluetooth;
 pub mod bluetooth_gatt;
 pub mod bluetooth_media;
 pub mod callbacks;
+pub mod dbus_arg;
 pub mod socket_manager;
 pub mod suspend;
 pub mod uuid;
