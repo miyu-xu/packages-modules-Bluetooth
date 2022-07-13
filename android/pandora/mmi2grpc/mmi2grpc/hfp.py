d3a09fae9314407855075ed53f29240ffcd771f3
William Escande <wescande@google.com>
2022-07-13 16:05:47 -0700

    [PANDORA_TEST] Implement HFP profile on pts-bot
    Tag: #feature
    Bug: 237447510
    Test: atest pts-bot:HFP/AG/SDP/BV-01-I -v
    Change-Id: I9fdc97060b42f47a517f51a361067c739dd3f46a

--- android/pandora/mmi2grpc/mmi2grpc/hfp.py
+++ android/pandora/mmi2grpc/mmi2grpc/hfp.py
@@ -21,14 +21,38 @@ from pandora.host_grpc import Host
 
 import sys
 import threading
+import os
+import socket
 
 
 class HFPProxy(ProfileProxy):
 
-    def __init__(self, channel):
+    def __init__(self, channel, test: str):
         super().__init__()
+        os.system('adb forward tcp:4242 vsock:2:9200')
         self.hfp = HFP(channel)
         self.host = Host(channel)
+        if test in [
+                'HFP/AG/ACC/BI-12-I',
+                'HFP/AG/ACC/BI-13-I',
+                'HFP/AG/ACC/BI-14-I',
+                'HFP/AG/ACC/BV-08-I',
+                'HFP/AG/ACC/BV-09-I',
+                'HFP/AG/ACC/BV-10-I',
+                'HFP/AG/ACC/BV-11-I',
+                'HFP/AG/ACC/BV-15-I',
+                'HFP/AG/ATH/BV-04-I',
+        ]:
+            self.asyncWaitConnection(3)
+
+    def asyncWaitConnection(self, delay, pts_addr=None):
+        print(f'HFP NOT A MMI: WaitConnection', file=sys.stderr)
+        args = ()
+        if pts_addr != None:
+            args = dict(address=pts_addr)
+
+        th = threading.Timer(interval=delay, function=self.host.WaitConnection, args=args)
+        th.start()
 
     @assert_description
     def TSC_delete_pairing_iut(self, pts_addr: bytes, **kwargs):
@@ -41,6 +65,48 @@ class HFPProxy(ProfileProxy):
         return "OK"
 
     @assert_description
+    def TSC_iut_connectable(self, test: str, pts_addr: bytes, **kwargs):
+        """
+        Make the Implementation Under Test (IUT) connectable, then click Ok.
+        """
+
+        if test in ["HFP/AG/SDP/BV-01-I"]:
+            self.asyncWaitConnection(2, pts_addr)
+
+        return "OK"
+
+    @assert_description
+    def TSC_INFO_slc_with_30_seconds_wait(self, **kwargs):
+        """
+        After clicking the OK button, PTS will connect to the IUT and then be
+        idle for 30 seconds as part of the test procedure.
+
+        Click OK to proceed.
+        """
+
+        return "OK"
+
+    def tryCall(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("127.0.0.1", 4242))
+        s.sendall(b'REM0\r\nAT+REMOTECALL=4,0,0,"42",129\r\n')
+        self.socket = s
+
+    @assert_description
+    def TSC_ag_iut_enable_call(self, pts_addr: bytes, **kwargs):
+        """
+        Click Ok, then place a call from an external line to the Implementation
+        Under Test (IUT). Do not answer the call unless prompted to do so.
+        """
+
+        # TODO b/238708237 Not Implemented
+        raise NotImplementedError(self.__class__.__name__ + '.TSC_ag_iut_enable_call See b/238708237       (╥﹏╥)')
+
+        th = threading.Timer(interval=3, function=self.tryCall)
+        th.start()
+        return "OK"
+
+    @assert_description
     def TSC_iut_enable_slc(self, pts_addr: bytes, **kwargs):
         """
         Click Ok, then initiate a service level connection from the
@@ -49,6 +115,17 @@ class HFPProxy(ProfileProxy):
 
         self.hfp.EnableSlc(address=pts_addr)
         return "OK"
+
+    @assert_description
+    def TSC_ag_iut_call_no_slc(self, **kwargs):
+        """
+        Place a call from an external line to the Implementation Under Test
+        (IUT).  When the call is active, click Ok.
+        """
+
+        # TODO b/238708237 Not Implemented
+        raise NotImplementedError(self.__class__.__name__ +
+                                  '.TSC_ag_iut_call_no_slc See b/238708237     .·´¯`(>▂<)´¯`·. ')
 
     @assert_description
     def TSC_iut_search(self, **kwargs):
