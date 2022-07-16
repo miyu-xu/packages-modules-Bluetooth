55e83dd8cdd581d5733c353819e671e544090343
Etienne Ruffieux <eruffieux@google.com>
2022-07-15 18:06:16 -0700

    PTS bot implementation for GATT profile tests
    Tests added:
    - GATT/CL/GAC/BV-01-C
    
    Tag: #feature
    Bug: 239103399
    Test: PTS
    Change-Id: I537feda919e61e7a7419e31d3ea2d2aadb4ac8db

---
+++ android/pandora/mmi2grpc/mmi2grpc/gatt.py
@@ -0,0 +1,89 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+
+from mmi2grpc._helpers import assert_description
+from mmi2grpc._proxy import ProfileProxy
+from pandora.gatt_grpc import GATT
+from pandora.host_grpc import Host
+from pandora.host_pb2 import Connection
+
+
+class GATTProxy(ProfileProxy):
+
+    def __init__(self, channel):
+        super().__init__()
+        self.gatt = GATT(channel)
+        self.host = Host(channel)
+
+    @assert_description
+    def MMI_IUT_INITIATE_CONNECTION(self, pts_addr: bytes, **kwargs):
+        """
+        Please initiate a GATT connection to the PTS.
+
+        Description: Verify that
+        the Implementation Under Test (IUT) can initiate GATT connect request to
+        PTS.
+        """
+
+        self.gatt.ConnectLe(address=pts_addr)
+
+        return "OK"
+
+    @assert_description
+    def MMI_IUT_MTU_EXCHANGE(self, **kwargs):
+        """
+        Please send exchange MTU command to the PTS.
+
+        Description: Verify that
+        the Implementation Under Test (IUT) can send Exchange MTU command to the
+        tester.
+        """
+
+        self.gatt.ExchangeMTU(mtu=512)
+
+        return "OK"
+
+    def MMI_IUT_SEND_PREPARE_WRITE_REQUEST_VALID_SIZE(self, description: str, **kwargs):
+        """
+        Please send prepare write request with handle = 'FFFF'O and size = 'XXX'
+        to the PTS.
+
+        Description: Verify that the Implementation Under Test
+        (IUT) can send data according to negotiate MTU size.
+        """
+
+        self.gatt.DiscoverServices()
+
+        matches = re.findall("'([a0-Z9]*)'O and size = '([a0-Z9]*)'", description)
+        handle = matches[0][0]
+        size = int(matches[0][1])
+        self.gatt.WriteCharacteristic(handle=handle, size=size)
+
+        return "OK"
+
+    @assert_description
+    def MMI_IUT_INITIATE_DISCONNECTION(self, **kwargs):
+        """
+        Please initiate a GATT disconnection to the PTS.
+
+        Description: Verify
+        that the Implementation Under Test (IUT) can initiate GATT disconnect
+        request to PTS.
+        """
+
+        self.gatt.Disconnect()
+
+        return "OK"
