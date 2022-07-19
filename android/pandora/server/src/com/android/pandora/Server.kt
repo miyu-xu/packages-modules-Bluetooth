ee8c8e93df70ce2ab28f4825d57802cdb6d47e02
Myles Watson <mylesgw@google.com>
2022-07-18 17:19:07 -0700

    Pandora: Implement RFCOMM/DEVA/
    Bug: 240483035
    Test: atest pts-bot:RFCOMM/DEVA/ -v
    Change-Id: I3b55dab8a14e05bdd595720d3c6383c6f2795b39

--- android/pandora/server/src/com/android/pandora/Server.kt
+++ android/pandora/server/src/com/android/pandora/Server.kt
@@ -32,6 +32,7 @@ class Server(context: Context) {
   private var avrcp: Avrcp
   private var gatt: Gatt
   private var hfp: Hfp
+  private var rfcomm: Rfcomm
   private var sm: Sm
   private var grpcServer: GrpcServer
 
@@ -41,6 +42,7 @@ class Server(context: Context) {
     avrcp = Avrcp(context)
     gatt = Gatt(context)
     hfp = Hfp(context)
+    rfcomm = Rfcomm(context)
     sm = Sm(context)
     grpcServer =
       NettyServerBuilder.forPort(GRPC_PORT)
@@ -49,6 +51,7 @@ class Server(context: Context) {
         .addService(avrcp)
         .addService(gatt)
         .addService(hfp)
+        .addService(rfcomm)
         .addService(sm)
         .build()
 
@@ -63,6 +66,7 @@ class Server(context: Context) {
     avrcp.deinit()
     gatt.deinit()
     hfp.deinit()
+    rfcomm.deinit()
     sm.deinit()
     grpcServer.shutdownNow()
   }
