a6cf800b86467150a5c216fc8cc99f5119a20551
Rahul Arya <aryarahul@google.com>
2022-07-23 00:03:04 +0000

    Boilerplate for HID tests
    Bug: 239986609
    Test: atest pts-bot:HID11/HOS/HRE/BV-01-I
    Change-Id: Id2d1be88bde602338328abb5a8859bc37cd0ec0a

--- android/pandora/mmi2grpc/mmi2grpc/__init__.py
+++ android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -24,6 +24,7 @@ import grpc
 from mmi2grpc.a2dp import A2DPProxy
 from mmi2grpc.gatt import GATTProxy
 from mmi2grpc.hfp import HFPProxy
+from mmi2grpc.hid11 import HID11Proxy
 from mmi2grpc.sdp import SDPProxy
 from mmi2grpc.sm import SMProxy
 from mmi2grpc._helpers import format_proxy
@@ -56,8 +57,6 @@ class IUT:
         self._a2dp = None
         self._gatt = None
         self._hfp = None
-        self._sdp = None
-        self._sm = None
 
     def __enter__(self):
         """Resets the IUT when starting a PTS test."""
@@ -70,8 +69,6 @@ class IUT:
         self._a2dp = None
         self._gatt = None
         self._hfp = None
-        self._sdp = None
-        self._sm = None
 
     def _retry(self, func):
 
@@ -125,16 +122,6 @@ class IUT:
             if not self._hfp:
                 self._hfp = HFPProxy(grpc.insecure_channel(f'localhost:{self.port}'))
             return self._hfp.interact(test, interaction, description, pts_address)
-        # Handles SDP MMIs.
-        if profile in ('SDP'):
-            if not self._sdp:
-                self._sdp = SDPProxy(grpc.insecure_channel(f'localhost:{self.port}'))
-            return self._sdp.interact(test, interaction, description, pts_address)
-        # Handles SM MMIs.
-        if profile in ('SM'):
-            if not self._sm:
-                self._sm = SMProxy(grpc.insecure_channel(f'localhost:{self.port}'))
-            return self._sm.interact(test, interaction, description, pts_address)
 
         # Handles unsupported profiles.
         code = format_proxy(profile, interaction, description)
