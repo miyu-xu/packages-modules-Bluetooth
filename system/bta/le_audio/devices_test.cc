718cd9ece4ad1e2679b8109504641211d5d09592
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2022-08-04 13:48:44 +0000

    le_audio: Use the same scenarios for Rintone and Conversational contexts
    Patch modifies scenarios used for Ringtone and Conversational contexts.
    
    Bug: 233961821
    Tag: #feature
    Sponsor: jpawlowski@
    Test: atest bluetooth_le_audio_test
    Test: atest bluetooth_le_audio_client_test
    Change-Id: Ie0c19a0a4d1958ea6e641e7a9c3297cf611a5851

--- system/bta/le_audio/devices_test.cc
+++ system/bta/le_audio/devices_test.cc
@@ -200,7 +200,7 @@ bool IsLc3SettingSupported(LeAudioContextType context_type, Lc3SettingId id) {
   switch (context_type) {
     case LeAudioContextType::RINGTONE:
       if (id == Lc3SettingId::LC3_16_2 || id == Lc3SettingId::LC3_16_1 ||
-          id == Lc3SettingId::LC3_32_2)
+          id == Lc3SettingId::LC3_24_2 || id == Lc3SettingId::LC3_32_2)
         return true;
 
       break;
