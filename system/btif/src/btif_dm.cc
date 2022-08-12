ef78f6f5ce61060e0410739413826a6194d66420
Qasim Javed <qasimj@google.com>
2022-08-12 12:27:34 -0700

    Cache EIR UUIDs per Bluetooth address outside the pairing_cb_t
    Currently, EIR UUIDs are cached inside the pairing control block. This
    means EIR UUIDs belonging to device A can overwrite the EIR UUIDs
    belonging to device B. Subsequently, if SDP fails for device A, we would
    happily fetch the EIR UUIDs we know of (Device B's) and report it to the
    Java layer. This is clearly incorrect.
    
    Fix this by storing the EIR UUIDs per bdaddr. Upon failure of SDP, send
    the EIR UUIDs belonging to the device for which SDP failed to Java and
    then clear those EIR UUIDs so that we do not store them in memory for
    longer than desired.
    
    Bug: 242350646
    Tag: #refactor
    Test: tested it manually by forcing only EIR UUIDs to propagate to the
    Java layer
    
    Change-Id: Id3c4f7efd44c724a9451f31388d96d8f6119c1ba

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -67,6 +67,7 @@
 #include "btif_sdp.h"
 #include "btif_storage.h"
 #include "btif_util.h"
+#include "common/lru_cache.h"
 #include "common/metrics.h"
 #include "device/include/controller.h"
 #include "device/include/interop.h"
@@ -156,8 +157,6 @@ typedef struct {
   bool is_le_nc; /* LE Numeric comparison */
   btif_dm_ble_cb_t ble;
   uint8_t fail_reason;
-  Uuid::UUID128Bit eir_uuids[32];
-  uint8_t num_eir_uuids;
   std::set<Uuid::UUID128Bit> uuids;
 } btif_dm_pairing_cb_t;
 
@@ -205,6 +204,9 @@ typedef struct {
 #define BTA_SERVICE_ID_TO_SERVICE_MASK(id) (1 << (id))
 
 #define MAX_BTIF_BOND_EVENT_ENTRIES 15
+
+static bluetooth::common::LruCache<RawAddress, std::set<Uuid>> eir_uuids_cache(
+    128);
 
 static skip_sdp_entry_t sdp_rejectlist[] = {{76}};  // Apple Mouse and Keyboard
 
@@ -1309,13 +1311,16 @@ static void btif_dm_search_devices_evt(tBTA_DM_SEARCH_EVT event,
         /* Cache EIR queried services */
         if (num_uuids > 0) {
           uint16_t* p_uuid16 = (uint16_t*)uuid_list;
-          pairing_cb.num_eir_uuids = 0;
-          LOG_INFO("EIR UUIDS:");
+          auto uuid_iter = eir_uuids_cache.find(bdaddr);
+          if (uuid_iter == eir_uuids_cache.end()) {
+            auto triple = eir_uuids_cache.try_emplace(bdaddr, std::set<Uuid>{});
+            uuid_iter = std::get<0>(triple);
+          }
+          LOG_INFO("EIR UUIDs for %s:", bdaddr.ToString().c_str());
           for (int i = 0; i < num_uuids; ++i) {
             Uuid uuid = Uuid::From16Bit(p_uuid16[i]);
             LOG_INFO("        %s", uuid.ToString().c_str());
-            pairing_cb.eir_uuids[i] = uuid.To128BitBE();
-            pairing_cb.num_eir_uuids++;
+            uuid_iter->second.insert(uuid);
           }
         }
 
@@ -1455,15 +1460,26 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
         // when SDP failed or no UUID is discovered
         if (p_data->disc_res.result != BTA_SUCCESS ||
             p_data->disc_res.num_uuids == 0) {
-          LOG_INFO("SDP failed, send %d EIR UUIDs to unblock bonding %s",
-                   pairing_cb.num_eir_uuids, bd_addr.ToString().c_str());
           bt_property_t prop_uuids;
-          Uuid uuid = {};
+          std::vector<uint8_t> property_value;
+          auto num_eir_uuids = 0;
+          auto uuids_iter = eir_uuids_cache.find(bd_addr);
+          if (uuids_iter != eir_uuids_cache.end()) {
+            num_eir_uuids = static_cast<int>(uuids_iter->second.size());
+            LOG_INFO("SDP failed, send %d EIR UUIDs to unblock bonding %s",
+                     num_eir_uuids, bd_addr.ToString().c_str());
+            for (auto& eir_uuid : uuids_iter->second) {
+              auto uuid_128bit = eir_uuid.To128BitBE();
+              property_value.insert(property_value.end(), uuid_128bit.begin(),
+                                    uuid_128bit.end());
+            }
+          }
           prop_uuids.type = BT_PROPERTY_UUIDS;
-          if (pairing_cb.num_eir_uuids > 0) {
-            prop_uuids.val = pairing_cb.eir_uuids;
-            prop_uuids.len = pairing_cb.num_eir_uuids * Uuid::kNumBytes128;
+          if (num_eir_uuids > 0) {
+            prop_uuids.val = (void*)property_value.data();
+            prop_uuids.len = num_eir_uuids * Uuid::kNumBytes128;
           } else {
+            Uuid uuid = {};
             prop_uuids.val = &uuid;
             prop_uuids.len = Uuid::kNumBytes128;
           }
@@ -1473,7 +1489,7 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
            */
           invoke_remote_device_properties_cb(BT_STATUS_SUCCESS, bd_addr, 1,
                                              &prop_uuids);
-          pairing_cb = {};
+          eir_uuids_cache.erase(uuids_iter);
           break;
         }
         // Both SDP and bonding are done, clear pairing control block in case
