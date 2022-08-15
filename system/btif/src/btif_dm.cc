78bf1666c2678b1d826506a9c9dbc9108f82f308
Qasim Javed <qasimj@google.com>
2022-08-15 14:07:24 -0700

    Ignore empty as well as base UUIDs.
    We ignore empty (all zeroes) UUIDs but we do not yet ignore base UUIDs
    which are then reported to Java. Base UUIDs should be ignored by the
    stack instead of passing them upwards.
    
    Bug: 242595392
    Tag: #refactor
    Test: Manually by looking at the SDP results reported to Java
    
    Change-Id: Ic06962161143693be21df25f4c35fef65637ee1e

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -1376,6 +1376,10 @@ static void btif_get_existing_uuids(RawAddress* bd_addr, Uuid* existing_uuids) {
   btif_storage_get_remote_device_property(bd_addr, &tmp_prop);
 }
 
+static bool btif_should_ignore_uuid(const Uuid& uuid) {
+  return uuid.IsEmpty() || uuid.IsBase();
+}
+
 /*******************************************************************************
  *
  * Function         btif_dm_search_services_evt
@@ -1418,7 +1422,7 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
         LOG_INFO("New UUIDs for %s:", bd_addr.ToString().c_str());
         for (i = 0; i < p_data->disc_res.num_uuids; i++) {
           auto uuid = p_data->disc_res.p_uuid_list + i;
-          if (uuid->IsEmpty()) {
+          if (btif_should_ignore_uuid(*uuid)) {
             continue;
           }
           LOG_INFO("index:%d uuid:%s", i, uuid->ToString().c_str());
@@ -1430,7 +1434,7 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
 
         for (int i = 0; i < BT_MAX_NUM_UUIDS; i++) {
           Uuid uuid = existing_uuids[i];
-          if (uuid.IsEmpty()) {
+          if (btif_should_ignore_uuid(uuid)) {
             continue;
           }
           if (btif_is_interesting_le_service(uuid)) {
@@ -1518,7 +1522,7 @@ static void btif_dm_search_services_evt(tBTA_DM_SEARCH_EVT event,
       LOG_INFO("New BLE UUIDs for %s:", bd_addr.ToString().c_str());
       for (Uuid uuid : *p_data->disc_ble_res.services) {
         if (btif_is_interesting_le_service(uuid)) {
-          if (uuid.IsEmpty()) {
+          if (btif_should_ignore_uuid(uuid)) {
             continue;
           }
           LOG_INFO("index:%d uuid:%s", static_cast<int>(uuids.size()),
