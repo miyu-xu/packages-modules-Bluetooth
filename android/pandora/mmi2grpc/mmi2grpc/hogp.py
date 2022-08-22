065a025eaeea33dab76c04dab41c5b7ac19fa438
Rahul Arya <aryarahul@google.com>
2022-08-22 18:49:07 +0000

    hogp
    Change-Id: I53c5d79ca78b3a2c93594f3d8cdcd0fb9b26735e

---
+++ android/pandora/mmi2grpc/mmi2grpc/hogp.py
@@ -0,0 +1,28 @@
+from mmi2grpc._helpers import assert_description
+from mmi2grpc._proxy import ProfileProxy
+
+from pandora.hid_grpc import HID
+from pandora.host_grpc import Host
+
+
+class HOGPProxy(ProfileProxy):
+
+    def __init__(self, channel):
+        super().__init__()
+        self.hid = HID(channel)
+        self.host = Host(channel)
+        self.connection = None
+
+    @assert_description
+    def IUT_INITIATE_CONNECTION(self, pts_addr: bytes, **kwargs):
+        """
+        Please initiate a GATT connection to the PTS.
+    
+        Description: Verify that
+        the Implementation Under Test (IUT) can initiate a GATT connect request
+        to the PTS.
+        """
+
+        self.connection = self.host.ConnectLE(address=pts_addr).connection
+    
+        return "OK"
