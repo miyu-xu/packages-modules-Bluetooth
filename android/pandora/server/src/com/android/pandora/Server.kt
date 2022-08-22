b7f9fc874e33bb4bc7dacbaee4199f4740ad8025
Rahul Arya <aryarahul@google.com>
2022-08-22 22:14:43 +0000

    [Pandora] Classic HID host-side tests
    Android supports acting as a HID device too, but those tests are not
    available with the ICSs we have enabled at the moment.
    
    Bug: 239986609
    Test: pts-bot
    Tag: #stability
    Change-Id: I3f250bf8071481ebe8cdb620cf6207b71007f882

--- android/pandora/server/src/com/android/pandora/Server.kt
+++ android/pandora/server/src/com/android/pandora/Server.kt
@@ -33,6 +33,7 @@ class Server(context: Context) {
   private var gatt: Gatt
   private var hfp: Hfp
   private var sm: Sm
+  private var hid: Hid
   private var grpcServer: GrpcServer
 
   init {
@@ -42,6 +43,7 @@ class Server(context: Context) {
     gatt = Gatt(context)
     hfp = Hfp(context)
     sm = Sm(context)
+    hid = Hid(context)
     grpcServer =
       NettyServerBuilder.forPort(GRPC_PORT)
         .addService(host)
@@ -50,6 +52,7 @@ class Server(context: Context) {
         .addService(gatt)
         .addService(hfp)
         .addService(sm)
+        .addService(hid)
         .build()
 
     Log.d(TAG, "Starting Pandora Server")
@@ -64,6 +67,7 @@ class Server(context: Context) {
     gatt.deinit()
     hfp.deinit()
     sm.deinit()
+    hid.deinit()
     grpcServer.shutdownNow()
   }
 
