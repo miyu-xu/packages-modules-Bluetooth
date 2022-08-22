8460b6dc33d0497f0eb3e50f5e821d565cc30e20
Rahul Arya <aryarahul@google.com>
2022-08-22 22:14:43 +0000

    [Pandora] Classic HID host-side tests
    Android supports acting as a HID device too, but those tests are not
    available with the ICSs we have enabled at the moment.
    
    Bug: 239986609
    Test: pts-bot
    Tag: #stability
    Change-Id: I3f250bf8071481ebe8cdb620cf6207b71007f882

--- android/pandora/mmi2grpc/mmi2grpc/__init__.py
+++ android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -24,6 +24,7 @@ import grpc
 from mmi2grpc.a2dp import A2DPProxy
 from mmi2grpc.avrcp import AVRCPProxy
 from mmi2grpc.gatt import GATTProxy
+from mmi2grpc.hid import HIDProxy
 from mmi2grpc.hfp import HFPProxy
 from mmi2grpc.sdp import SDPProxy
 from mmi2grpc.sm import SMProxy
@@ -61,6 +62,7 @@ class IUT:
         self._hfp = None
         self._sdp = None
         self._sm = None
+        self._hid = None
 
     def __enter__(self):
         """Resets the IUT when starting a PTS test."""
@@ -77,6 +79,7 @@ class IUT:
         self._hfp = None
         self._sdp = None
         self._sm = None
+        self._hid = None
 
     def _retry(self, func):
 
@@ -145,6 +148,11 @@ class IUT:
             if not self._sm:
                 self._sm = SMProxy(grpc.insecure_channel(f'localhost:{self.port}'))
             return self._sm.interact(test, interaction, description, pts_address)
+        # Handles HID MMIs.
+        if profile in ('HID'):
+            if not self._hid:
+                self._hid = HIDProxy(grpc.insecure_channel(f'localhost:{self.port}'))
+            return self._hid.interact(test, interaction, description, pts_address)
 
         # Handles unsupported profiles.
         code = format_proxy(profile, interaction, description)
