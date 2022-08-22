fd9f1e1fe71f9edf42e79123f32e16714257e1e5
Rahul Arya <aryarahul@google.com>
2022-08-22 18:41:13 +0000

    Utils to control rootcanal from PTSbot MMIs
    Bug: 239986609
    Test: PTS tests in next CL
    Tag: #feature
    Change-Id: Ieb06ea537636e03019e9465103a41719b24d1c52

--- android/pandora/server/src/com/android/pandora/Hid.kt
+++ android/pandora/server/src/com/android/pandora/Hid.kt
@@ -1,34 +1,44 @@
 package com.android.pandora
 
-import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothHidHost
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.content.Context
-import android.util.Log
-import com.google.protobuf.Empty
 import io.grpc.stub.StreamObserver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import pandora.HIDGrpc.HIDImplBase
-import pandora.HidProto.*
+import pandora.HidProto.SendHostReportRequest
+import pandora.HidProto.SendHostReportResponse
 
 @kotlinx.coroutines.ExperimentalCoroutinesApi
 class Hid(val context: Context) : HIDImplBase() {
   private val TAG = "PandoraHid"
 
-  private val scope: CoroutineScope
+  private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
   private val bluetoothManager =
       context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
   private val bluetoothAdapter = bluetoothManager.adapter
-  
-  init {
-    // Init the CoroutineScope
-    scope = CoroutineScope(Dispatchers.Default)
-  }
+  private val bluetoothHidHost = getProfileProxy<BluetoothHidHost>(context, BluetoothProfile.HID_HOST)
 
   fun deinit() {
     // Deinit the CoroutineScope
     scope.cancel()
   }
+
+  override fun sendHostReport(
+    request: SendHostReportRequest,
+    responseObserver: StreamObserver<SendHostReportResponse>,
+  ) {
+    grpcUnary(scope, responseObserver) {
+      bluetoothHidHost.setReport(
+        request.address.toBluetoothDevice(bluetoothAdapter),
+        request.reportType.number.toByte(),
+        request.report)
+      SendHostReportResponse.getDefaultInstance()
+    }
+  }
+
 }
