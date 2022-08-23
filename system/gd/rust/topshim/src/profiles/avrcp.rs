66b3992717ea87e53a677606501a7328a9c85418
howardchung <howardchung@google.com>
2022-08-23 12:03:13 +0800

    Floss: Implement APIs in topshim profiles to make them togglable
    Implements the following APIs for topshim profiles
    - enable
    - disable
    - is_enabled
    
    The original logic of |initialize| is splitted into setting the
    dispachers and enabling the internal interface. The new |initialize|
    do the former part and |enable| do the latter part.
    
    Bug: 239434117
    Test: test along with the top of this chain
    Tag: #floss
    
    Change-Id: Ie604d38fb7e961c2b2c86817336db6f6e78c0069

--- system/gd/rust/topshim/src/profiles/avrcp.rs
+++ system/gd/rust/topshim/src/profiles/avrcp.rs
@@ -2,7 +2,9 @@ use crate::btif::{BluetoothInterface, RawAddress};
 use crate::topstack::get_dispatchers;
 
 use std::sync::{Arc, Mutex};
-use topshim_macros::cb_variant;
+use topshim_macros::{cb_variant, profile_enabled_or};
+
+use log::warn;
 
 #[cxx::bridge(namespace = bluetooth::topshim::rust)]
 pub mod ffi {
@@ -107,6 +109,7 @@ cb_variant!(
 pub struct Avrcp {
     internal: cxx::UniquePtr<ffi::AvrcpIntf>,
     _is_init: bool,
+    _is_enabled: bool,
 }
 
 // For *const u8 opaque btif
@@ -119,31 +122,56 @@ impl Avrcp {
             avrcpif = ffi::GetAvrcpProfile(intf.as_raw_ptr());
         }
 
-        Avrcp { internal: avrcpif, _is_init: false }
+        Avrcp { internal: avrcpif, _is_init: false, _is_enabled: false }
+    }
+
+    pub fn is_initialized(&self) -> bool {
+        self._is_init
     }
 
     pub fn initialize(&mut self, callbacks: AvrcpCallbacksDispatcher) -> bool {
         if get_dispatchers().lock().unwrap().set::<AvrcpCb>(Arc::new(Mutex::new(callbacks))) {
             panic!("Tried to set dispatcher for Avrcp callbacks while it already exists");
         }
+        self._is_init = true;
+        true
+    }
+
+    pub fn is_enabled(&self) -> bool {
+        self._is_enabled
+    }
+
+    pub fn enable(&mut self) -> bool {
         self.internal.pin_mut().init();
+        self._is_enabled = true;
         true
     }
 
-    pub fn cleanup(&mut self) -> bool {
+    #[profile_enabled_or(false)]
+    pub fn disable(&mut self) -> bool {
         self.internal.pin_mut().cleanup();
+        self._is_enabled = false;
         true
     }
 
+    #[profile_enabled_or]
+    pub fn set_volume(&mut self, volume: i8) {
+        self.internal.pin_mut().set_volume(volume);
+    }
+
+    #[profile_enabled_or]
     pub fn connect(&mut self, addr: RawAddress) {
         self.internal.pin_mut().connect(addr.into());
     }
 
+    #[profile_enabled_or]
     pub fn disconnect(&mut self, addr: RawAddress) {
         self.internal.pin_mut().disconnect(addr.into());
     }
 
-    pub fn set_volume(&mut self, volume: i8) {
-        self.internal.pin_mut().set_volume(volume);
+    #[profile_enabled_or(false)]
+    pub fn cleanup(&mut self) -> bool {
+        self.internal.pin_mut().cleanup();
+        true
     }
 }
