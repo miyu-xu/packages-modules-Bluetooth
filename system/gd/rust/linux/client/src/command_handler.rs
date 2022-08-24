10d0a83ca32a4f377a4b363e129c7e33aa902ae4
howardchung <howardchung@google.com>
2022-08-24 18:56:46 +0800

    Floss: Implement set allowed services in BluetoothAdmin
    This API disables the services that is not in the allowed list. A
    profile is disabled by calling |disable| in topshim layer which should
    call |cleanup| to the libbluetooth interface. Similarly, a profile is
    re-enabled by calling |enable| in topshim and |init| of the libbluetooth
    interface will be called subsequently.
    
    However, the cleanup function of HFP and A2DP in topshim-cpp layer
    haven't been implemented. By testing locally, the consequence is that
    when a2dp and hfp are disabled by policy, the audio headset can still be
    connected and stay connected, but there is no audio stream to the
    headset nor the chromebook. Additionally, the headset could adjust the
    volumn via HFP.
    
    Bug: 239470589
    Test: test it along with the top of this chain
    Tag: #floss
    Change-Id: Id4eba16f298599dad8d469bf719bc5ef2211c639

--- system/gd/rust/linux/client/src/command_handler.rs
+++ system/gd/rust/linux/client/src/command_handler.rs
@@ -335,8 +335,8 @@ impl CommandHandler {
                     let le_ext_adv_supported = adapter_dbus.is_le_extended_advertising_supported();
                     let wbs_supported = adapter_dbus.is_wbs_supported();
                     let uuid_helper = UuidHelper::new();
-                    let enabled_profiles = uuid_helper.get_enabled_profiles();
-                    let connected_profiles: Vec<Profile> = enabled_profiles
+                    let supported_profiles = uuid_helper.get_supported_profiles();
+                    let connected_profiles: Vec<Profile> = supported_profiles
                         .iter()
                         .filter(|&&prof| adapter_dbus.get_profile_connection_state(prof) > 0)
                         .cloned()
