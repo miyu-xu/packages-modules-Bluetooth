fe8e026ca9dee6c5f2057f2938b693e90c128595
Jesse Melhuish <melhuishj@chromium.org>
2022-09-02 16:53:02 -0500

    floss: Add initial BAS implementation
    Bug: 242217799
    Tag: #floss
    Test: Not yet tested
    
    Change-Id: I1ec6b173f0aa45842b762ae450456db9ec3ba3e4

--- system/gd/rust/linux/stack/src/battery_manager.rs
+++ system/gd/rust/linux/stack/src/battery_manager.rs
@@ -1,22 +1,91 @@
+use crate::bluetooth_battery_service::{
+    BatteryService, BatteryServiceStatus, IBatteryService, IBatteryServiceCallback,
+};
+use crate::callbacks::Callbacks;
+use crate::Message;
+use crate::RPCProxy;
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::Sender;
+
 #[derive(Debug, Clone)]
 pub struct Battery {
-    pub percentage: i32,
+    pub percentage: u32,
     pub source_info: String,
     pub variant: String,
 }
 
-pub struct BatteryManager {}
+pub struct BatteryManager {
+    bas: Arc<Mutex<Box<BatteryService>>>,
+    tx: Sender<Message>,
+    callbacks: Arc<Mutex<Callbacks<dyn IBatteryManagerCallback + Send>>>,
+    notifications_enabled: Arc<Mutex<HashSet<u32>>>,
+}
 
 impl BatteryManager {
-    pub fn new() -> BatteryManager {
-        BatteryManager {}
+    pub fn new(bas: Arc<Mutex<Box<BatteryService>>>, tx: Sender<Message>) -> BatteryManager {
+        let callbacks = Arc::new(Mutex::new(Callbacks::new(
+            tx.clone(),
+            Message::BatteryManagerCallbackDisconnected,
+        )));
+        Self {
+            bas: bas,
+            tx: tx.clone(),
+            callbacks: callbacks,
+            notifications_enabled: Arc::new(Mutex::new(HashSet::new())),
+        }
+    }
+
+    pub fn init(&self) {
+        self.bas
+            .lock()
+            .unwrap()
+            .register_callback(Box::new(BasCallback::new(self.callbacks.clone())));
+    }
+}
+
+struct BasCallback {
+    callbacks: Arc<Mutex<Callbacks<dyn IBatteryManagerCallback + Send>>>,
+}
+
+impl BasCallback {
+    pub fn new(
+        callbacks: Arc<Mutex<Callbacks<dyn IBatteryManagerCallback + Send>>>,
+    ) -> BasCallback {
+        Self { callbacks: callbacks }
+    }
+}
+
+impl IBatteryServiceCallback for BasCallback {
+    fn on_battery_service_status_updated(
+        &self,
+        remote_address: String,
+        status: BatteryServiceStatus,
+    ) {
+        return;
+    }
+
+    fn on_battery_level_updated(&self, remote_address: String, battery_level: u32) {
+        return;
+    }
+
+    fn on_battery_level_read(&self, remote_address: String, battery_level: u32) {
+        return;
+    }
+}
+
+impl RPCProxy for BasCallback {
+    fn get_object_id(&self) -> String {
+        todo!()
     }
 }
 
 /// Callback for interacting with the BatteryManager.
-pub trait IBatteryManagerCallback {
+pub trait IBatteryManagerCallback: RPCProxy {
     /// Invoked whenever battery information associated with the given remote changes.
     fn on_battery_info_updated(&self, remote_address: String, battery: Battery);
+
+    fn on_battery_info_read(&self, remote_address: String, battery: Battery);
 }
 
 /// Central point for getting battery information that might be sourced from numerous systems.
@@ -25,31 +94,53 @@ pub trait IBatteryManager {
     /// callback_id for future calls.
     fn register_battery_callback(
         &mut self,
-        remote_address: String,
         battery_manager_callback: Box<dyn IBatteryManagerCallback + Send>,
-    ) -> i32;
+    ) -> Option<u32>;
 
     /// Unregister a callback.
-    fn unregister_battery_callback(&mut self, callback_id: i32);
+    fn unregister_battery_callback(&mut self, callback_id: u32);
+
+    fn enable_notifications(&mut self, callback_id: u32, enable: bool);
 
     /// Returns battery information for the remote, sourced from the highest priority origin.
-    fn get_battery_information(&self, remote_address: String) -> Battery;
+    fn get_battery_information(&self, remote_address: String) -> Option<Battery>;
+
+    fn refresh_battery_information(&self, remote_address: String);
 }
 
 impl IBatteryManager for BatteryManager {
     fn register_battery_callback(
         &mut self,
-        _remote_address: String,
-        _battery_manager_callback: Box<dyn IBatteryManagerCallback + Send>,
-    ) -> i32 {
-        todo!()
+        battery_manager_callback: Box<dyn IBatteryManagerCallback + Send>,
+    ) -> Option<u32> {
+        Some(self.callbacks.lock().unwrap().add_callback(battery_manager_callback))
     }
 
-    fn unregister_battery_callback(&mut self, _callback_id: i32) {
-        todo!()
+    fn unregister_battery_callback(&mut self, callback_id: u32) {
+        self.callbacks.lock().unwrap().remove_callback(callback_id);
     }
 
-    fn get_battery_information(&self, _remote_address: String) -> Battery {
-        todo!()
+    fn enable_notifications(&mut self, callback_id: u32, enable: bool) {
+        if let None = self.callbacks.lock().unwrap().get_by_id(callback_id) {
+            return;
+        }
+        self.notifications_enabled.lock().unwrap().remove(&callback_id);
+        if enable {
+            self.notifications_enabled.lock().unwrap().insert(callback_id);
+        }
+    }
+
+    fn get_battery_information(&self, remote_address: String) -> Option<Battery> {
+        log::info!("Attempting to return battery information for {}", remote_address.clone());
+        let battery_level = self.bas.lock().unwrap().get_battery_level(remote_address)?;
+        Some(Battery {
+            percentage: battery_level,
+            source_info: "BAS".to_string(),
+            variant: "".to_string(),
+        })
+    }
+
+    fn refresh_battery_information(&self, remote_address: String) {
+        self.bas.lock().unwrap().refresh_battery_level(remote_address);
     }
 }
---
+++ system/gd/rust/linux/stack/src/bluetooth_battery_service.rs
@@ -0,0 +1,342 @@
+use crate::bluetooth_gatt::{
+    BluetoothGatt, BluetoothGattService, IBluetoothGatt, IBluetoothGattCallback,
+};
+use crate::callbacks::Callbacks;
+use crate::uuid::parse_uuid_string;
+use crate::uuid::BAS;
+use crate::Message;
+use crate::RPCProxy;
+use bt_topshim::profiles::gatt::GattStatus;
+use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::Sender;
+
+pub const CHARACTERISTIC_BATTERY_LEVEL: &str = "00002A19-0000-1000-8000-00805F9B34FB";
+
+pub struct BatteryService {
+    gatt: Arc<Mutex<Box<BluetoothGatt>>>,
+    tx: Sender<Message>,
+    callbacks: Arc<Mutex<Callbacks<dyn IBatteryServiceCallback + Send>>>,
+    client_id: Arc<Mutex<Option<i32>>>,
+    battery_levels: Arc<Mutex<HashMap<String, u32>>>,
+    notifications_enabled: Arc<Mutex<HashSet<u32>>>,
+    handles: Arc<Mutex<HashMap<String, i32>>>,
+}
+
+impl BatteryService {
+    pub fn new(gatt: Arc<Mutex<Box<BluetoothGatt>>>, tx: Sender<Message>) -> BatteryService {
+        let client_id = Arc::new(Mutex::new(None));
+        let battery_levels = Arc::new(Mutex::new(HashMap::new()));
+        let callbacks = Arc::new(Mutex::new(Callbacks::new(
+            tx.clone(),
+            Message::BatteryServiceCallbackDisconnected,
+        )));
+        let notifications_enabled = Arc::new(Mutex::new(HashSet::new()));
+        let handles = Arc::new(Mutex::new(HashMap::new()));
+        Self {
+            gatt: gatt,
+            tx: tx.clone(),
+            callbacks: callbacks,
+            client_id: client_id,
+            battery_levels: battery_levels,
+            notifications_enabled: notifications_enabled,
+            handles: handles,
+        }
+    }
+
+    pub fn init(&self) {
+        self.gatt.lock().unwrap().register_client(
+            // TODO: make dynamic
+            String::from("e4d2acffcfaa42198f494606b7412117"),
+            Box::new(GattCallback::new(
+                self.client_id.clone(),
+                self.callbacks.clone(),
+                self.gatt.clone(),
+                self.battery_levels.clone(),
+                self.notifications_enabled.clone(),
+                self.handles.clone(),
+            )),
+            false,
+        );
+    }
+
+    fn init_device(&self, remote_address: String) {
+        log::info!("melhuishj: init_device for {}", remote_address.clone());
+        let client_id = match *self.client_id.lock().unwrap() {
+            Some(id) => id,
+            None => {
+                log::info!(
+                    "melhuishj: init_device failed for {} due to no client_id",
+                    remote_address
+                );
+                return;
+            }
+        };
+        let bas_uuid = match parse_uuid_string(BAS) {
+            Some(uuid) => uuid,
+            None => {
+                log::info!(
+                    "melhuishj: init_device failed for {} due to unable to parse BAS",
+                    remote_address
+                );
+                return;
+            }
+        };
+        log::info!("melhuishj: beginning client_connect for {}", remote_address.clone());
+        self.gatt.lock().unwrap().client_connect(client_id, remote_address, false, 2, false, 1);
+    }
+}
+
+pub trait IBatteryService {
+    fn register_callback(
+        &mut self,
+        callback: Box<dyn IBatteryServiceCallback + Send>,
+    ) -> Option<u32>;
+    fn unregister_callback(&mut self, callback_id: u32);
+    fn enable_notifications(&mut self, callback_id: u32, enable: bool);
+    fn get_battery_level(&self, remote_address: String) -> Option<u32>;
+    fn refresh_battery_level(&self, remote_address: String);
+}
+
+pub enum BatteryServiceStatus {
+    BatteryServiceNotReady,
+    BatteryServiceReady,
+    BatteryServiceNotSupported,
+}
+
+pub trait IBatteryServiceCallback: RPCProxy {
+    fn on_battery_service_status_updated(
+        &self,
+        remote_address: String,
+        status: BatteryServiceStatus,
+    );
+    fn on_battery_level_updated(&self, remote_address: String, battery_level: u32);
+    fn on_battery_level_read(&self, remote_address: String, battery_level: u32);
+}
+
+impl IBatteryService for BatteryService {
+    // Refactor to not use remote_address here
+    fn register_callback(
+        &mut self,
+        callback: Box<dyn IBatteryServiceCallback + Send>,
+    ) -> Option<u32> {
+        let callback_id = self.callbacks.lock().unwrap().add_callback(callback);
+        Some(callback_id)
+    }
+
+    fn unregister_callback(&mut self, callback_id: u32) {
+        self.callbacks.lock().unwrap().remove_callback(callback_id);
+    }
+
+    fn enable_notifications(&mut self, callback_id: u32, enable: bool) {
+        if let None = self.callbacks.lock().unwrap().get_by_id(callback_id) {
+            return;
+        }
+        self.notifications_enabled.lock().unwrap().remove(&callback_id);
+        if enable {
+            self.notifications_enabled.lock().unwrap().insert(callback_id);
+        }
+    }
+
+    fn get_battery_level(&self, remote_address: String) -> Option<u32> {
+        log::info!("Attempting to return battery level for {}", remote_address.clone());
+        self.battery_levels.lock().unwrap().get(&remote_address).cloned()
+    }
+
+    fn refresh_battery_level(&self, remote_address: String) {
+        let client_id = match *self.client_id.lock().unwrap() {
+            Some(id) => id,
+            None => {
+                log::info!(
+                    "melhuishj: refresh_battery_level failed for {} due to no client_id",
+                    remote_address
+                );
+                return;
+            }
+        };
+        let handle = match self.handles.lock().unwrap().get(&remote_address) {
+            Some(id) => *id,
+            None => {
+                self.init_device(remote_address);
+                return;
+            }
+        };
+        self.gatt.lock().unwrap().read_characteristic(client_id, remote_address.clone(), handle, 0);
+        self.gatt.lock().unwrap().register_for_notification(
+            client_id,
+            remote_address,
+            handle,
+            true,
+        );
+    }
+}
+
+struct GattCallback {
+    client_id: Arc<Mutex<Option<i32>>>,
+    callbacks: Arc<Mutex<Callbacks<dyn IBatteryServiceCallback + Send>>>,
+    gatt: Arc<Mutex<Box<BluetoothGatt>>>,
+    // TODO: support multiple batteries per device
+    battery_levels: Arc<Mutex<HashMap<String, u32>>>,
+    notifications_enabled: Arc<Mutex<HashSet<u32>>>,
+    handles: Arc<Mutex<HashMap<String, i32>>>,
+}
+
+impl GattCallback {
+    fn new(
+        client_id: Arc<Mutex<Option<i32>>>,
+        callbacks: Arc<Mutex<Callbacks<dyn IBatteryServiceCallback + Send>>>,
+        gatt: Arc<Mutex<Box<BluetoothGatt>>>,
+        battery_levels: Arc<Mutex<HashMap<String, u32>>>,
+        notifications_enabled: Arc<Mutex<HashSet<u32>>>,
+        handles: Arc<Mutex<HashMap<String, i32>>>,
+    ) -> Self {
+        Self {
+            client_id: client_id,
+            callbacks: callbacks,
+            gatt: gatt,
+            battery_levels: battery_levels,
+            notifications_enabled: notifications_enabled,
+            handles: handles,
+        }
+    }
+
+    fn set_battery_level(&self, remote_address: String, value: Vec<u8>) {
+        log::info!("Setting battery level for {}", remote_address.clone());
+        let mut level: [u8; 4] = [0, 0, 0, 0];
+        let copy_limit = if value.len() < 4 { value.len() } else { 4 };
+        for i in 0..copy_limit {
+            level[i] = value[i];
+        }
+        if let Some(battery_level) = self.battery_levels.lock().unwrap().get_mut(&remote_address) {
+            *battery_level = u32::from_le_bytes(level);
+        }
+    }
+}
+
+impl IBluetoothGattCallback for GattCallback {
+    fn on_client_registered(&self, status: GattStatus, client_id: i32) {
+        log::info!("melhuishj: on_client_registered with client_id {}", client_id);
+        *self.client_id.lock().unwrap() = Some(client_id);
+    }
+
+    fn on_client_connection_state(
+        &self,
+        status: GattStatus,
+        client_id: i32,
+        connected: bool,
+        addr: String,
+    ) {
+        log::info!(
+            "melhuishj: on_client_connection_state for {} with GattStatus {:?}",
+            addr.clone(),
+            status.clone()
+        );
+        if !connected {
+            log::info!("melhuishj: not connected to {} with GattStatus {:?}", addr, status);
+            return;
+        }
+        let client_id = match *self.client_id.lock().unwrap() {
+            Some(id) => id,
+            None => {
+                log::info!("melhuishj: init_device failed for {} due to no client_id", addr);
+                return;
+            }
+        };
+        log::info!("melhuishj: beginning to discover_services for {}", addr.clone());
+        self.gatt.lock().unwrap().discover_services(client_id, addr); //, BAS.to_string());
+    }
+
+    fn on_search_complete(
+        &self,
+        addr: String,
+        services: Vec<BluetoothGattService>,
+        status: GattStatus,
+    ) {
+        log::info!("melhuishj: finished searching for BAS on {}", addr.clone());
+        let bas_uuid = match parse_uuid_string(BAS) {
+            Some(uuid) => uuid,
+            None => return,
+        };
+        let battery_level_uuid = match parse_uuid_string(CHARACTERISTIC_BATTERY_LEVEL) {
+            Some(uuid) => uuid,
+            None => return,
+        };
+        // TODO: handle multiple instances of BAS
+        let bas = match services.iter().find(|service| service.uuid == bas_uuid.uu) {
+            Some(bas) => bas,
+            None => {
+                self.callbacks.lock().unwrap().for_all_callbacks(|callback| {
+                    callback.on_battery_service_status_updated(
+                        addr.clone(),
+                        BatteryServiceStatus::BatteryServiceNotSupported,
+                    )
+                });
+                return;
+            }
+        };
+        let battery_level = match bas
+            .characteristics
+            .iter()
+            .find(|characteristic| characteristic.uuid == battery_level_uuid.uu)
+        {
+            Some(battery_level) => battery_level,
+            None => return,
+        };
+        if status != GattStatus::Success {
+            return;
+        }
+        if let Some(id) = *self.client_id.lock().unwrap() {
+            let handle = battery_level.instance_id;
+            self.handles.lock().unwrap().insert(addr.clone(), handle.clone());
+            self.gatt.lock().unwrap().register_for_notification(id, addr.clone(), handle, true);
+            if let None = self.battery_levels.lock().unwrap().get(&addr) {
+                self.gatt.lock().unwrap().read_characteristic(
+                    id,
+                    addr,
+                    battery_level.instance_id,
+                    0,
+                );
+            }
+        }
+    }
+
+    fn on_characteristic_read(
+        &self,
+        addr: String,
+        status: GattStatus,
+        handle: i32,
+        value: Vec<u8>,
+    ) {
+        if status != GattStatus::Success {
+            return;
+        }
+        self.set_battery_level(addr.clone(), value);
+        self.callbacks.lock().unwrap().for_all_callbacks(|callback| {
+            callback.on_battery_level_read(
+                addr.clone(),
+                *self.battery_levels.lock().unwrap().get(&addr).unwrap(),
+            )
+        });
+    }
+
+    fn on_notify(&self, addr: String, handle: i32, value: Vec<u8>) {
+        self.set_battery_level(addr.clone(), value);
+        let level = match self.battery_levels.lock().unwrap().get(&addr) {
+            Some(level) => *level,
+            None => return,
+        };
+        // TODO: expand Callbacks to allow direct filtering/exposing the underlying iter
+        self.notifications_enabled.lock().unwrap().iter().for_each(|id| {
+            match self.callbacks.lock().unwrap().get_by_id(*id) {
+                Some(callback) => callback.on_battery_level_updated(addr.clone(), level),
+                None => (),
+            }
+        });
+    }
+}
+
+impl RPCProxy for GattCallback {
+    fn get_object_id(&self) -> String {
+        todo!()
+    }
+}
