62c11dd2e8ba09f85e1c1dd24b92ef5afd80bbce
Rahul Arya <aryarahul@google.com>
2022-09-03 01:59:55 +0000

    [Pandora] GAP MMIs
    Test: atest pts-bot:GAP/SEC
    Change-Id: I58bd8e1cfb83ac6c75fa0c6abd860a9bd128801c

---
+++ android/pandora/server/src/com/android/pandora/Gap.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.pandora
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+import pandora.GAPGrpc.GAPImplBase
+import pandora.GapProto.*
+
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+class Gap(private val context: Context) : GAPImplBase() {
+  private val TAG = "PandoraGap"
+
+  private val mScope: CoroutineScope
+  private val flow: Flow<Intent>
+
+  private val mBluetoothManager = context.getSystemService(BluetoothManager::class.java)!!
+  private val mBluetoothAdapter = mBluetoothManager.adapter
+
+  init {
+    mScope = CoroutineScope(Dispatchers.Default)
+
+    val intentFilter = IntentFilter()
+    intentFilter.addAction(BluetoothDevice.ACTION_UUID)
+
+    flow = intentFlow(context, intentFilter).shareIn(mScope, SharingStarted.Eagerly)
+  }
+
+  fun deinit() {
+    mScope.cancel()
+  }
+}
