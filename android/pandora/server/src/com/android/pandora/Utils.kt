9afff6626d74931241beacd204a54feed71be1f3
Rahul Arya <aryarahul@google.com>
2022-09-03 02:09:02 +0000

    [Pandora] Implementing advertising interfaces
    Based on discussion in aosp/2193570, moving to this CL because of merge
    conflicts + lazy.
    
    Test: GAP PTS tests in future CLs
    Bug: 245007199
    Tag: #feature
    Change-Id: I3ac8a0e2d735148ec5628e230149544cb25c2e8f

--- android/pandora/server/src/com/android/pandora/Utils.kt
+++ android/pandora/server/src/com/android/pandora/Utils.kt
@@ -233,3 +233,28 @@ fun Connection.toBluetoothDevice(adapter: BluetoothAdapter): BluetoothDevice =
 fun String.toByteArray(): ByteArray = MacAddress.fromString(this).toByteArray()
 
 fun BluetoothDevice.toByteArray(): ByteArray = this.address.toByteArray()
+
+class HandleManager<T> {
+  private var nextHandle = 1
+  private val handleMap = mutableMapOf<Int, T>()
+
+  private fun parseHandle(handle: ByteString) = Integer.parseInt(handle.toString())
+  private fun deparseHandle(handle: Int) = ByteString.copyFromUtf8(handle.toString())
+
+  fun register(x: T): ByteString = registerWithHandle { x }.first
+
+  fun registerWithHandle(f: (ByteString) -> T): Pair<ByteString, T> {
+    nextHandle += 1
+    val out = f(deparseHandle(nextHandle))
+    handleMap[nextHandle] = out
+    return Pair(deparseHandle(nextHandle), out)
+  }
+
+  fun get(handle: ByteString): T? {
+    return handleMap[parseHandle(handle)]
+  }
+
+  fun remove(handle: ByteString) {
+    handleMap.remove(parseHandle(handle))
+  }
+}
