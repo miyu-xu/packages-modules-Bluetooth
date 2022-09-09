c933269a76d9786fda4ad567e6f5f426b8c6d8ae
Chen Chen <cncn@google.com>
2022-09-09 18:41:39 +0000

    BluetoothMetrics: Bypass counter metrics caching for profile connection failures to get timestamp
    Change-Id: I0bdbf89ddda03f27acda1f7f6d3112eb40393996

--- system/gd/metrics/counter_metrics.cc
+++ system/gd/metrics/counter_metrics.cc
@@ -49,7 +49,7 @@ void CounterMetrics::Stop() {
   LOG_INFO("Counter metrics canceled");
 }
 
-bool CounterMetrics::Count(int32_t key, int64_t count) {
+bool CounterMetrics::CacheCount(int32_t key, int64_t count) {
   if (!IsInitialized()) {
     LOG_WARN("Counter metrics isn't initialized");
     return false;
@@ -73,8 +73,10 @@ bool CounterMetrics::Count(int32_t key, int64_t count) {
   return true;
 }
 
-void CounterMetrics::WriteCounter(int32_t key, int64_t count) {
+bool CounterMetrics::Count(int32_t key, int64_t count) {
+  LOG_INFO("Counter metrics cpp %d %s", key, std::to_string(count).c_str());
   os::LogMetricBluetoothCodePathCounterMetrics(key, count);
+  return true;
 }
 
 void CounterMetrics::DrainBufferedCounters() {
@@ -85,7 +87,7 @@ void CounterMetrics::DrainBufferedCounters() {
   std::lock_guard<std::mutex> lock(mutex_);
   LOG_INFO("Draining buffered counters");
   for (auto const& pair : counters_) {
-    WriteCounter(pair.first, pair.second);
+    Count(pair.first, pair.second);
   }
   counters_.clear();
 }
