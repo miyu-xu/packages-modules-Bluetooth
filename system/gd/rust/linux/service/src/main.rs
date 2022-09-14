c1aea4d3a18f336b4010955972288b67400b52b6
howardchung <howardchung@google.com>
2022-09-14 03:47:41 +0000

    Floss: Implement service allowlist persistentcy
    Admin policy will be stored in /var/lib/bluetooth/admin_policy.json
    
    It will be updated upon a successful SetServiceAllowedlist call, and it
    will be loaded upon btadapterd starts.
    
    Bug: 239470589
    Test: tested it with the following steps
    Set allowed list via gdbus
    check /var/lib/bluetooth/admin_policy.json is updated correctly
    restart btadapterd
    Get allowed list via gdbus and check if it's persistent
    Tag: #floss
    Change-Id: I141f60a618e905c8cfc4c58fd3966faa2c5de273
    
    Change-Id: I158e90e9ac2eff95eda0d19bd18e4355ce5bd144

--- system/gd/rust/linux/service/src/main.rs
+++ system/gd/rust/linux/service/src/main.rs
@@ -40,6 +40,7 @@ mod iface_bluetooth_gatt;
 mod iface_bluetooth_media;
 
 const DBUS_SERVICE_NAME: &str = "org.chromium.bluetooth";
+const ADMIN_SETTINGS_FILE_PATH: &str = "/var/lib/bluetooth/admin_policy.json";
 
 fn make_object_name(idx: i32, name: &str) -> String {
     String::from(format!("/org/chromium/bluetooth/hci{}/{}", idx, name))
@@ -121,7 +122,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         Arc::new(Mutex::new(Box::new(BatteryService::new(bluetooth_gatt.clone(), tx.clone()))));
     let battery_manager =
         Arc::new(Mutex::new(Box::new(BatteryManager::new(battery_service.clone(), tx.clone()))));
-    let bluetooth_admin = Arc::new(Mutex::new(Box::new(BluetoothAdmin::new())));
+    let bluetooth_admin =
+        Arc::new(Mutex::new(Box::new(BluetoothAdmin::new(String::from(ADMIN_SETTINGS_FILE_PATH)))));
     let bluetooth = Arc::new(Mutex::new(Box::new(Bluetooth::new(
         tx.clone(),
         intf.clone(),
