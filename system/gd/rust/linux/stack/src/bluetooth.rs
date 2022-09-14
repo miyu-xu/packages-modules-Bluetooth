a9cec3d1007b09565a5ee464cb32ecef66fee66d
howardchung <howardchung@google.com>
2022-09-14 08:54:55 +0000

    Floss: Implement admin policy callbacks
    Implement the following callbacks for client.
    
    - OnServiceAllowlistChanged:
    Get the updated service allowlist
    
    - OnDevicePolicyEffectChanged:
    Get the updated policy effect, which includes
    (1) list of services that are disallowed by the current policy
    (2) boolean value to indicate if the device has an adapter-supported
    profile that is blocked by the policy.
    
    Bug: 239470493
    Test: test it along with the next patch
    Tag: #floss
    Change-Id: Idb770f9a7a34401d2d58421fb18b07013330cb09

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -1,5 +1,4 @@
 //! Anything related to the adapter API (IBluetooth).
-
 use bt_topshim::btif::{
     BaseCallbacks, BaseCallbacksDispatcher, BluetoothInterface, BluetoothProperty, BtAclState,
     BtBondState, BtConnectionState, BtDeviceType, BtDiscoveryState, BtHciErrorCode, BtPinCode,
@@ -22,6 +21,7 @@ use log::{debug, error, warn};
 use num_traits::cast::ToPrimitive;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::hash::Hash;
 use std::sync::{Arc, Condvar, Mutex};
 use std::time::Duration;
 use std::time::Instant;
@@ -30,6 +30,7 @@ use tokio::sync::oneshot::Sender as OneShotSender;
 use tokio::task::JoinHandle;
 use tokio::time;
 
+use crate::bluetooth_admin::BluetoothAdmin;
 use crate::bluetooth_media::{BluetoothMedia, IBluetoothMedia, MediaActions};
 use crate::callbacks::Callbacks;
 use crate::uuid::{Profile, UuidHelper, HOGP};
@@ -200,7 +201,7 @@ pub trait IBluetoothQA {
 }
 
 /// Serializable device used in various apis.
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct BluetoothDevice {
     pub address: String,
     pub name: String,
@@ -255,11 +256,11 @@ impl BluetoothDeviceContext {
             last_seen,
             properties: HashMap::new(),
         };
-        device.update_properties(properties);
+        device.update_properties(&properties);
         device
     }
 
-    pub(crate) fn update_properties(&mut self, in_properties: Vec<BluetoothProperty>) {
+    pub(crate) fn update_properties(&mut self, in_properties: &Vec<BluetoothProperty>) {
         for prop in in_properties {
             match &prop {
                 BluetoothProperty::BdAddr(bdaddr) => {
@@ -271,7 +272,7 @@ impl BluetoothDeviceContext {
                 _ => {}
             }
 
-            self.properties.insert(prop.get_type(), prop);
+            self.properties.insert(prop.get_type(), prop.clone());
         }
     }
 
@@ -317,6 +318,22 @@ pub trait IBluetoothCallback: RPCProxy {
     fn on_bond_state_changed(&self, status: u32, device_address: String, state: u32);
 }
 
+/// An interface for other modules to track found remote devices.
+pub trait IBluetoothDeviceCallback {
+    /// When a device is found via discovery.
+    fn on_device_found(&mut self, remote_device: BluetoothDevice);
+
+    /// When a device is cleared from discovered devices cache.
+    fn on_device_cleared(&mut self, remote_device: BluetoothDevice);
+
+    /// When a device property is changed.
+    fn on_remote_device_properties_changed(
+        &mut self,
+        remote_device: BluetoothDevice,
+        properties: Vec<BluetoothProperty>,
+    );
+}
+
 pub trait IBluetoothConnectionCallback: RPCProxy {
     /// Notification sent when a remote device completes HCI connection.
     fn on_device_connected(&self, remote_device: BluetoothDevice);
@@ -331,6 +348,7 @@ pub struct Bluetooth {
 
     bonded_devices: HashMap<String, BluetoothDeviceContext>,
     bluetooth_media: Arc<Mutex<Box<BluetoothMedia>>>,
+    bluetooth_admin: Arc<Mutex<Box<BluetoothAdmin>>>,
     callbacks: Callbacks<dyn IBluetoothCallback + Send>,
     connection_callbacks: Callbacks<dyn IBluetoothConnectionCallback + Send>,
     discovering_started: Instant,
@@ -363,6 +381,7 @@ impl Bluetooth {
         intf: Arc<Mutex<BluetoothInterface>>,
         bluetooth_media: Arc<Mutex<Box<BluetoothMedia>>>,
         sig_notifier: Arc<(Mutex<bool>, Condvar)>,
+        bluetooth_admin: Arc<Mutex<Box<BluetoothAdmin>>>,
     ) -> Bluetooth {
         Bluetooth {
             bonded_devices: HashMap::new(),
@@ -373,6 +392,7 @@ impl Bluetooth {
             ),
             hh: None,
             bluetooth_media,
+            bluetooth_admin,
             discovering_started: Instant::now(),
             intf,
             is_connectable: false,
@@ -589,6 +609,8 @@ impl Bluetooth {
             self.callbacks.for_all_callbacks(|callback| {
                 callback.on_device_cleared(d.clone());
             });
+
+            self.bluetooth_admin.lock().unwrap().on_device_cleared(&d);
         }
 
         // If we have any fresh devices remaining, re-queue a freshness check.
@@ -855,7 +877,7 @@ impl BtifBluetoothCallbacks for Bluetooth {
         let address = device.address.clone();
 
         if let Some(existing) = self.found_devices.get_mut(&address) {
-            existing.update_properties(properties);
+            existing.update_properties(&properties);
             existing.seen();
         } else {
             let device_with_props = BluetoothDeviceContext::new(
@@ -873,6 +895,8 @@ impl BtifBluetoothCallbacks for Bluetooth {
         self.callbacks.for_all_callbacks(|callback| {
             callback.on_device_found(device.info.clone());
         });
+
+        self.bluetooth_admin.lock().unwrap().on_device_found(&device.info);
     }
 
     fn discovery_state(&mut self, state: BtDiscoveryState) {
@@ -1017,7 +1041,7 @@ impl BtifBluetoothCallbacks for Bluetooth {
 
         match device {
             Some(d) => {
-                d.update_properties(properties);
+                d.update_properties(&properties);
                 d.seen();
 
                 Bluetooth::send_metrics_remote_device_info(d);
@@ -1025,8 +1049,13 @@ impl BtifBluetoothCallbacks for Bluetooth {
                 let info = d.info.clone();
                 let uuids = self.get_remote_uuids(info.clone());
                 if self.wait_to_connect && uuids.len() > 0 {
-                    self.connect_all_enabled_profiles(info);
+                    self.connect_all_enabled_profiles(info.clone());
                 }
+
+                self.bluetooth_admin
+                    .lock()
+                    .unwrap()
+                    .on_remote_device_properties_changed(&info, &properties);
             }
             None => (),
         };
