1d65a111847ef331ef81c6033ac36770f2775aa0
Qasim Javed <qasimj@google.com>
2022-09-15 09:31:28 -0700

    Wait for a string to appear in logs.
    Currently, we have to wait for arbitrary amount of time before we check
    the log for a particular string. Instead of that, add functionality to
    wait for a provided string to appear in the logs.
    
    Bug: 246955322
    Tag: #gd-refactor
    Test: atest bluetooth_gd_test_unit
    
    Change-Id: Ib2fcfeef82d3419f4c02e1815b907b0b6ac499a9

--- system/gd/common/testing/log_capture.h
+++ system/gd/common/testing/log_capture.h
@@ -15,6 +15,7 @@
  */
 
 #include <cstddef>
+#include <future>
 #include <string>
 
 namespace bluetooth {
@@ -39,6 +40,8 @@ class LogCapture {
   size_t Size() const;
   // Truncates and resets the file pointer discarding all logs up to this point
   void Reset();
+  // Wait until the provided string shows up in the logs
+  void WaitUntilLogContains(std::promise<void>* promise, std::string text);
 
  private:
   int create_backing_store() const;
