eb998e5e66167e9f839e16f981bcd4e8c20ff7a9
Łukasz Rymanowski <rlukasz@google.com>
2022-09-15 10:33:29 +0000

    bta_dm: Don't clear scheduled service discovery on BTA_DmSearchCancel()
    This function suppose to clear scheduled devices discovery or inquire
    and not scheduled service discovery
    
    Problem can be visible when pairing coorinated set and could end up with
    one device being bonded but without any services connected.
    
    Bug: 247511422
    Test: atest BluetoothInstrumentationTests
    Tag: #feature
    
    Change-Id: I83f55a173a17544c1a736a8218b769b038b9eecc

--- system/bta/dm/bta_dm_act.cc
+++ system/bta/dm/bta_dm_act.cc
@@ -1564,7 +1564,6 @@ bool bta_dm_is_search_request_queued() {
  ******************************************************************************/
 void bta_dm_search_clear_queue() {
   osi_free_and_reset((void**)&bta_dm_search_cb.p_pending_search);
-  fixed_queue_flush(bta_dm_search_cb.pending_discovery_queue, osi_free);
 }
 
 /*******************************************************************************
