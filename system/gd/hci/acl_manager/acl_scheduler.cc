56592177d47751e92b59c532a08c048f0d3016d4
Rahul Arya <aryarahul@google.com>
2022-09-17 21:33:48 +0000

    Add Remote Name Request scheduling to ACL scheduler
    Remote name requests need to be queued along with ACL Create Connection
    requests to avoid the commands being rejected by the controller. This
    adds the relevant hooks into ACL scheduler to do so, analogous to those
    that exist for connection creation.
    
    Test: gd unit tests (TODO)
    Bug: 246640776
    Tag: #refactor
    Change-Id: Ia6ce5d598eddb771110df44375f9f8256e411132

--- system/gd/hci/acl_manager/acl_scheduler.cc
+++ system/gd/hci/acl_manager/acl_scheduler.cc
@@ -16,8 +16,10 @@
 
 #include "acl_scheduler.h"
 
+#include <optional>
 #include <queue>
 #include <unordered_set>
+#include <variant>
 
 namespace bluetooth {
 namespace hci {
@@ -29,10 +31,17 @@ struct AclCreateConnectionQueueEntry {
   common::ContextualOnceCallback<void()> callback;
 };
 
+struct RemoteNameRequestQueueEntry {
+  Address address;
+  common::ContextualOnceCallback<void()> callback;
+};
+
+using QueueEntry = std::variant<AclCreateConnectionQueueEntry, RemoteNameRequestQueueEntry>;
+
 struct AclScheduler::impl {
  public:
   void EnqueueOutgoingAclConnection(Address address, common::ContextualOnceCallback<void()> start_connection) {
-    pending_outgoing_connections_.push({address, std::move(start_connection)});
+    pending_outgoing_connections_.push(AclCreateConnectionQueueEntry{address, std::move(start_connection)});
     try_dequeue_next_connection();
   }
 
@@ -45,16 +54,25 @@ struct AclScheduler::impl {
       common::ContextualOnceCallback<void()> handle_outgoing_connection,
       common::ContextualOnceCallback<void()> handle_incoming_connection,
       common::ContextualOnceCallback<void(std::string)> handle_unknown_connection) {
-    if (outgoing_connecting_address_ == address) {
-      outgoing_connecting_address_ = Address::kEmpty;
-      handle_outgoing_connection.InvokeIfNotEmpty();
-    } else if (incoming_connecting_address_set_.find(address) != incoming_connecting_address_set_.end()) {
+    // Check if an outgoing request (a) exists, (b) is a Create Connection, (c) matches the received address
+    if (outgoing_entry_.has_value()) {
+      auto entry = std::get_if<AclCreateConnectionQueueEntry>(&outgoing_entry_.value());
+      if (entry != nullptr && entry->address == address) {
+        // If so, clear the current entry and advance the queue
+        outgoing_entry_.reset();
+        handle_outgoing_connection.InvokeIfNotEmpty();
+        try_dequeue_next_connection();
+        return;
+      }
+    }
+
+    // Otherwise check if it's an incoming request, and leave the queue the way it is
+    if (incoming_connecting_address_set_.find(address) != incoming_connecting_address_set_.end()) {
       incoming_connecting_address_set_.erase(address);
       handle_incoming_connection.InvokeIfNotEmpty();
     } else {
       handle_unknown_connection.InvokeIfNotEmpty(set_of_incoming_connecting_addresses());
     }
-    try_dequeue_next_connection();
   }
 
   void CancelAclConnection(
@@ -63,6 +81,22 @@ struct AclScheduler::impl {
       common::ContextualOnceCallback<void()> cancel_connection_completed) {
     cancel_connection.Invoke();
   }
+
+  void EnqueueRemoteNameRequest(Address address, common::ContextualOnceCallback<void()> start_request) {
+    pending_outgoing_connections_.push(RemoteNameRequestQueueEntry{address, std::move(start_request)});
+  }
+
+  void ReportRemoteNameRequestCompletion(Address address) {
+    outgoing_entry_.reset();
+    try_dequeue_next_connection();
+  }
+
+  void CancelRemoteNameRequest(
+      Address address,
+      common::ContextualOnceCallback<void()> cancel_request,
+      common::ContextualOnceCallback<void()> cancel_request_completed){
+      // TODO
+  };
 
   void Stop() {
     stopped_ = true;
@@ -73,13 +107,13 @@ struct AclScheduler::impl {
     if (stopped_) {
       return;
     }
-    if (incoming_connecting_address_set_.empty() && outgoing_connecting_address_.IsEmpty() &&
+    if (incoming_connecting_address_set_.empty() && !outgoing_entry_.has_value() &&
         !pending_outgoing_connections_.empty()) {
       LOG_INFO("Pending connections is not empty; so sending next connection");
       auto entry = std::move(pending_outgoing_connections_.front());
       pending_outgoing_connections_.pop();
-      outgoing_connecting_address_ = entry.address;
-      entry.callback.Invoke();
+      std::visit([](auto&& variant) { variant.callback.Invoke(); }, entry);
+      outgoing_entry_ = std::move(entry);
     }
   }
 
@@ -89,8 +123,8 @@ struct AclScheduler::impl {
     return buffer.str();
   }
 
-  Address outgoing_connecting_address_;
-  std::queue<AclCreateConnectionQueueEntry> pending_outgoing_connections_;
+  std::optional<QueueEntry> outgoing_entry_;
+  std::queue<QueueEntry> pending_outgoing_connections_;
   std::unordered_set<Address> incoming_connecting_address_set_;
   bool stopped_ = false;
 };
@@ -137,6 +171,28 @@ void AclScheduler::CancelAclConnection(
       address,
       std::move(cancel_connection),
       std::move(cancel_connection_completed)));
+}
+
+void AclScheduler::EnqueueRemoteNameRequest(Address address, common::ContextualOnceCallback<void()> start_request) {
+  GetHandler()->Post(common::BindOnce(
+      &impl::EnqueueRemoteNameRequest, common::Unretained(pimpl_.get()), address, std::move(start_request)));
+}
+
+void AclScheduler::ReportRemoteNameRequestCompletion(Address address) {
+  GetHandler()->Post(
+      common::BindOnce(&impl::ReportRemoteNameRequestCompletion, common::Unretained(pimpl_.get()), address));
+}
+
+void AclScheduler::CancelRemoteNameRequest(
+    Address address,
+    common::ContextualOnceCallback<void()> cancel_request,
+    common::ContextualOnceCallback<void()> cancel_request_completed) {
+  GetHandler()->Post(common::BindOnce(
+      &impl::CancelRemoteNameRequest,
+      common::Unretained(pimpl_.get()),
+      address,
+      std::move(cancel_request),
+      std::move(std::move(cancel_request_completed))));
 }
 
 void AclScheduler::ListDependencies(ModuleList* list) const {}
