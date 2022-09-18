24369bc530e880d70ad90495fc990936eac09cd8
Rahul Arya <aryarahul@google.com>
2022-09-18 19:47:11 +0000

    Add module to execute Remote Name Requests
    This module is intended to shim at the HCI layer, so from the POV of
    callers it's as if they are making the HCI call directly.
    
    It uses the ACL scheduler to avoid conflicting requests with itself or
    ACL Create Connection.
    
    Test: TODO cert + unit
    Bug: 246640776
    Tag: #feature
    BYPASS_LONG_LINES_REASON: Bluetooth
    
    Change-Id: I9e4e4e06f34b61b2b9579d0e415988f9ba2ee151

---
+++ system/gd/hci/remote_name_request.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "common/contextual_callback.h"
+#include "hci/hci_packets.h"
+#include "module.h"
+
+namespace bluetooth {
+namespace hci {
+
+// The RemoteNameRequestModule handles Remote Name Requests, which produce both Remote Name Request Completed events,
+// and Remote Host Supported Features Notification events.
+
+// Historical note: This class is intended to provide a shim at the *HCI* layer, so legacy Remote Name Requests can
+// interoperate with the GD ACL scheduler. Thus, we intentionally do not merge identical requests, cache responses, or
+// handle request timeouts - we leave this to our callers. When GD clients start to use this module, richer
+// functionality should be added.
+class RemoteNameRequestModule : public bluetooth::Module {
+ public:
+  // Dispatch a Remote Name Request
+  void StartRemoteNameRequest(
+      Address address,
+      std::unique_ptr<RemoteNameRequestBuilder> request,
+      common::ContextualOnceCallback<void(ErrorCode)> on_completion,
+      common::ContextualOnceCallback<void(RemoteHostSupportedFeaturesNotificationView)>
+          on_remote_host_supported_features_notification,
+      common::ContextualOnceCallback<void(RemoteNameRequestCompleteView)> on_remote_name_complete);
+
+  // Cancel a Remote Name Request
+  void CancelRemoteNameRequest(Address address);
+
+ private:
+  struct impl;
+  std::unique_ptr<impl> pimpl_;
+
+ protected:
+  void ListDependencies(ModuleList* list) const override;
+  void Start() override;
+  void Stop() override;
+  std::string ToString() const override {
+    return std::string("RemoteNameRequestModule");
+  }
+
+ public:
+  static const ModuleFactory Factory;
+  RemoteNameRequestModule();
+  ~RemoteNameRequestModule();
+};
+
+}  // namespace hci
+}  // namespace bluetooth
