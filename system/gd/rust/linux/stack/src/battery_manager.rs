eba7b26375155f3e3c9719f5c902a990f543542f
Jesse Melhuish <melhuishj@chromium.org>
2022-09-19 16:40:56 -0500

    floss: Add BatteryProviderManager implementation
    Register BAS as a BatteryProvider, switch BatteryManager to source from
    BatteryProviderManager.
    
    Bug: 242217799
    Tag: #floss
    Test: Tested manually using dbus-send
    Change-Id: Id49226a04b3092bd90fb09548b2d5564de768b55

--- system/gd/rust/linux/stack/src/battery_manager.rs
+++ system/gd/rust/linux/stack/src/battery_manager.rs
@@ -1,10 +1,7 @@
-use crate::battery_service::{
-    BatteryService, BatteryServiceStatus, IBatteryService, IBatteryServiceCallback,
-};
+use crate::battery_provider_manager::{BatteryProviderManager, IBatteryConsumerCallback};
 use crate::callbacks::Callbacks;
 use crate::Message;
 use crate::RPCProxy;
-use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc::Sender;
 
@@ -12,15 +9,22 @@ use tokio::sync::mpsc::Sender;
 /// passing and external calls.
 #[derive(Debug, Clone)]
 pub struct Battery {
+    pub address: String,
     pub percentage: u32,
     pub source_info: String,
     pub variant: String,
 }
 
+/// Helper representation of a collection of batteries to simplify
+/// passing around data internally.
+pub struct Batteries {
+    pub batteries: Vec<Battery>,
+}
+
 /// Callback for interacting with the BatteryManager.
 pub trait IBatteryManagerCallback: RPCProxy {
     /// Invoked whenever battery information associated with the given remote changes.
-    fn on_battery_info_updated(&self, remote_address: String, battery: Battery);
+    fn on_battery_info_updated(&self, remote_address: String, batteries: Vec<Battery>);
 }
 
 /// Central point for getting battery information that might be sourced from numerous systems.
@@ -35,69 +39,51 @@ pub trait IBatteryManager {
     /// Unregister a callback.
     fn unregister_battery_callback(&mut self, callback_id: u32);
 
-    /// Enables notifications for a given callback.
-    fn enable_notifications(&mut self, callback_id: u32, enable: bool);
-
     /// Returns battery information for the remote, sourced from the highest priority origin.
-    fn get_battery_information(&self, remote_address: String) -> Option<Battery>;
+    fn get_battery_information(&self, remote_address: String) -> Option<Vec<Battery>>;
 }
 
 /// Repesentation of the BatteryManager.
 pub struct BatteryManager {
-    bas: Arc<Mutex<Box<BatteryService>>>,
+    battery_provider_manager: Arc<Mutex<Box<BatteryProviderManager>>>,
     callbacks: Callbacks<dyn IBatteryManagerCallback + Send>,
-    /// List of callback IDs that have enabled notifications.
-    notifications_enabled: HashSet<u32>,
+}
+
+/// Enum for BatteryConsumerCallback to relay messages back to the main thread.
+pub enum BatteryConsumerCallbacks {
+    /// Params: remote_address, batteries
+    OnBatteryInfoUpdated(String, Option<Batteries>),
 }
 
 impl BatteryManager {
     /// Construct a new BatteryManager with callbacks communicating on tx.
-    pub fn new(bas: Arc<Mutex<Box<BatteryService>>>, tx: Sender<Message>) -> BatteryManager {
+    pub fn new(
+        battery_provider_manager: Arc<Mutex<Box<BatteryProviderManager>>>,
+        tx: Sender<Message>,
+    ) -> BatteryManager {
         let callbacks = Callbacks::new(tx.clone(), Message::BatteryManagerCallbackDisconnected);
-        let notifications_enabled = HashSet::new();
-        Self { bas, callbacks, notifications_enabled }
-    }
-
-    /// Invoked after BAS has been initialized.
-    pub fn init(&self) {
-        self.bas.lock().unwrap().register_callback(Box::new(BasCallback::new()));
+        battery_provider_manager
+            .lock()
+            .unwrap()
+            .register_battery_consumer_callback(Box::new(BatteryConsumerCallback::new(tx.clone())));
+        Self { battery_provider_manager, callbacks }
     }
 
     /// Remove a callback due to disconnection or unregistration.
     pub fn remove_callback(&mut self, callback_id: u32) {
         self.callbacks.remove_callback(callback_id);
     }
-}
 
-struct BasCallback {}
-
-impl BasCallback {
-    pub fn new() -> BasCallback {
-        Self {}
-    }
-}
-
-impl IBatteryServiceCallback for BasCallback {
-    fn on_battery_service_status_updated(
-        &self,
-        _remote_address: String,
-        _status: BatteryServiceStatus,
-    ) {
-        todo!()
-    }
-
-    fn on_battery_level_updated(&self, _remote_address: String, _battery_level: u32) {
-        todo!()
-    }
-
-    fn on_battery_level_read(&self, _remote_address: String, _battery_level: u32) {
-        todo!()
-    }
-}
-
-impl RPCProxy for BasCallback {
-    fn get_object_id(&self) -> String {
-        "BAS Callback".to_string()
+    /// Handles all callback messages to acoid deadlocks.
+    pub fn handle_callback(&mut self, callback: BatteryConsumerCallbacks) {
+        match callback {
+            BatteryConsumerCallbacks::OnBatteryInfoUpdated(remote_address, batteries) => {
+                let batteries = batteries.map_or(vec![], |battery| battery.batteries);
+                self.callbacks.for_all_callbacks(|callback| {
+                    callback.on_battery_info_updated(remote_address.clone(), batteries.clone())
+                });
+            }
+        }
     }
 }
 
@@ -113,24 +99,93 @@ impl IBatteryManager for BatteryManager {
         self.remove_callback(callback_id);
     }
 
-    fn enable_notifications(&mut self, callback_id: u32, enable: bool) {
-        if self.callbacks.get_by_id(callback_id).is_none() {
-            return;
-        }
-        self.notifications_enabled.remove(&callback_id);
-        if enable {
-            self.notifications_enabled.insert(callback_id);
+    fn get_battery_information(&self, remote_address: String) -> Option<Vec<Battery>> {
+        Some(
+            self.battery_provider_manager
+                .lock()
+                .unwrap()
+                .get_battery_info(remote_address)?
+                .batteries,
+        )
+    }
+}
+
+impl Batteries {
+    pub fn new() -> Self {
+        Self { batteries: vec![] }
+    }
+
+    pub fn from_vec(batteries: Vec<Battery>) -> Self {
+        Self { batteries: batteries }
+    }
+
+    /// Updates a battery matching all non-battery-level fields if
+    /// found, otherwise adds new_battery verbatim.
+    pub fn add_or_update_battery(&mut self, new_battery: Battery) {
+        match self.batteries.iter_mut().find(|battery| {
+            battery.address == new_battery.address
+                && battery.source_info == new_battery.source_info
+                && battery.variant == new_battery.variant
+        }) {
+            Some(battery) => *battery = new_battery,
+            None => self.batteries.push(new_battery),
         }
     }
 
-    // TODO(b/233101174): update to use all available sources once
-    // BatteryProviderManager is implemented.
-    fn get_battery_information(&self, remote_address: String) -> Option<Battery> {
-        let battery_level = self.bas.lock().unwrap().get_battery_level(remote_address)?;
-        Some(Battery {
-            percentage: battery_level,
-            source_info: "BAS".to_string(),
-            variant: "".to_string(),
-        })
+    /// Returns a Batteries object containing the highest quality pick
+    /// from all available data.
+    pub fn pick_best(&self) -> Batteries {
+        let batteries = self
+            .batteries
+            .iter()
+            .filter(|battery| battery.source_info == "BAS")
+            .cloned()
+            .collect::<Vec<Battery>>();
+        if batteries.len() > 0 {
+            return Batteries::from_vec(batteries);
+        }
+        let batteries = self
+            .batteries
+            .iter()
+            .filter(|battery| battery.source_info == "powerd")
+            .cloned()
+            .collect::<Vec<Battery>>();
+        if batteries.len() > 0 {
+            return Batteries::from_vec(batteries);
+        }
+        Batteries::from_vec(self.batteries.clone())
+    }
+}
+
+/// The callback used to receive updates from BatteryProviderManager.
+struct BatteryConsumerCallback {
+    tx: Sender<Message>,
+}
+
+impl BatteryConsumerCallback {
+    fn new(tx: Sender<Message>) -> Self {
+        Self { tx }
+    }
+}
+
+impl IBatteryConsumerCallback for BatteryConsumerCallback {
+    // All callback messages get relayed to the main thread for
+    // processing.
+
+    fn on_battery_info_updated(&self, remote_address: String, batteries: Option<Batteries>) {
+        let tx = self.tx.clone();
+        tokio::spawn(async move {
+            let _ = tx
+                .send(Message::BatteryManagerCallbacks(
+                    BatteryConsumerCallbacks::OnBatteryInfoUpdated(remote_address, batteries),
+                ))
+                .await;
+        });
+    }
+}
+
+impl RPCProxy for BatteryConsumerCallback {
+    fn get_object_id(&self) -> String {
+        "Battery Consumer Callback".to_string()
     }
 }
