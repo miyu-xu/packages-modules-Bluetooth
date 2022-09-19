21093dd181b88fca3a5f0b6fbb12e5bd2e2770b7
Rahul Arya <aryarahul@google.com>
2022-09-19 00:17:35 +0000

    Add Remote Name Request shim
    Add a shim at the HCI layer to redirect calls to Remote Name Request
    Create and Cancel into the GD scheduler.
    
    Bug: 246640776
    Test: PTS + manual pairing/unpairing
    Tag: #gd-refactor
    BYPASS_LONG_LINES_REASON: Bluetooth
    
    Change-Id: I5b8be66477c6fe7a09dcf46650bce7d48e6a11be

--- system/main/shim/shim.cc
+++ system/main/shim/shim.cc
@@ -81,3 +81,7 @@ bool bluetooth::shim::is_gd_dumpsys_module_started() {
 bool bluetooth::shim::is_gd_btaa_enabled() {
   return bluetooth::common::init_flags::btaa_hci_is_enabled();
 }
+
+bool bluetooth::shim::is_gd_remote_name_request_enabled() {
+  return bluetooth::common::init_flags::gd_remote_name_request_is_enabled();
+}
