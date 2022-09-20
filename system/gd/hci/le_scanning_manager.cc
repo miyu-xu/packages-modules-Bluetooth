0440fbf1013243897f3b4c379ed1df88feb8ff64
Sonny Sasaka <sonnysasaka@google.com>
2022-09-20 14:41:14 -0700

    gd/hci: add MsftExtensionManager in LeScanningManager
    This patch adds MsftExtensionManager to the ListDependencies
    of LeScanningManager so that LeScanningManager can use the
    functionality of MsftExtensionManager later.
    
    This also provides a minimal skeleton for parallel work of
    MSFT LE scanning and HAL layer.
    
    Bug: 217274009 246398494
    Tag: #floss
    Test: ./build.py --target test
    Test: Manual - Build on AOSP and Linux
    Test: Manual test on
          - brya/gimble (Intel controller) and
          - trogdor/lazor (Qualcomm controller).
          The supported features and varialbe-length vendor prefixes
          are displayed correctly in the logs when floss is enabled.
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I31d1e44935402a2547d156624db356020285ac5f

--- system/gd/hci/le_scanning_manager.cc
+++ system/gd/hci/le_scanning_manager.cc
@@ -24,6 +24,7 @@
 #include "hci/hci_packets.h"
 #include "hci/le_periodic_sync_manager.h"
 #include "hci/le_scanning_interface.h"
+#include "hci/msft.h"
 #include "hci/vendor_specific_event_manager.h"
 #include "module.h"
 #include "os/handler.h"
@@ -235,11 +236,13 @@ struct LeScanningManager::impl : public LeAddressManagerCallback {
       HciLayer* hci_layer,
       Controller* controller,
       AclManager* acl_manager,
+      MsftExtensionManager* msft_manager,
       VendorSpecificEventManager* vendor_specific_event_manager) {
     module_handler_ = handler;
     hci_layer_ = hci_layer;
     controller_ = controller;
     acl_manager_ = acl_manager;
+    msft_manager_ = msft_manager;
     vendor_specific_event_manager_ = vendor_specific_event_manager;
     le_address_manager_ = acl_manager->GetLeAddressManager();
     le_scanning_interface_ = hci_layer_->GetLeScanningInterface(
@@ -1518,6 +1521,7 @@ struct LeScanningManager::impl : public LeAddressManagerCallback {
   HciLayer* hci_layer_;
   Controller* controller_;
   AclManager* acl_manager_;
+  MsftExtensionManager* msft_manager_;
   VendorSpecificEventManager* vendor_specific_event_manager_;
   LeScanningInterface* le_scanning_interface_;
   LeAddressManager* le_address_manager_;
@@ -1578,6 +1582,7 @@ void LeScanningManager::ListDependencies(ModuleList* list) const {
   list->add<VendorSpecificEventManager>();
   list->add<Controller>();
   list->add<AclManager>();
+  list->add<MsftExtensionManager>();
 }
 
 void LeScanningManager::Start() {
@@ -1586,6 +1591,7 @@ void LeScanningManager::Start() {
       GetDependency<HciLayer>(),
       GetDependency<Controller>(),
       GetDependency<AclManager>(),
+      GetDependency<MsftExtensionManager>(),
       GetDependency<VendorSpecificEventManager>());
 }
 
