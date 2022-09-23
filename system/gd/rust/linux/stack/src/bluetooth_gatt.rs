a5ab110a93966772a6e88fe3cd29498c59df0a85
William Escande <wescande@google.com>
2022-09-23 15:44:37 -0700

    BT5.3: Add include_adi for Periodic Advertising
    Pre-convergence CL to add support for the parameter accross legacy and
    gd.
    Replace the `enable` type to be a bool to avoid confusion and add the
    new field `include_adi` for all methods
    
    Bug: 248126637
    Tag: #feature
    Test: Manual end-to-end testing (need 5.3 support in controller)
    Change-Id: Ie06f64126842258f8cb2f508814d1923d415ded8

--- system/gd/rust/linux/stack/src/bluetooth_gatt.rs
+++ system/gd/rust/linux/stack/src/bluetooth_gatt.rs
@@ -297,7 +297,12 @@ pub trait IBluetoothGatt {
     fn set_periodic_advertising_data(&mut self, advertiser_id: i32, data: AdvertiseData);
 
     /// Enables or disables periodic advertising.
-    fn set_periodic_advertising_enable(&mut self, advertiser_id: i32, enable: bool);
+    fn set_periodic_advertising_enable(
+        &mut self,
+        advertiser_id: i32,
+        enable: bool,
+        include_adi: bool,
+    );
 
     // GATT Client
 
@@ -1144,13 +1149,18 @@ impl IBluetoothGatt for BluetoothGatt {
         }
     }
 
-    fn set_periodic_advertising_enable(&mut self, advertiser_id: i32, enable: bool) {
+    fn set_periodic_advertising_enable(
+        &mut self,
+        advertiser_id: i32,
+        enable: bool,
+        include_adi: bool,
+    ) {
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
-            self.gatt
-                .as_mut()
-                .unwrap()
-                .advertiser
-                .set_periodic_advertising_enable(s.adv_id(), enable);
+            self.gatt.as_mut().unwrap().advertiser.set_periodic_advertising_enable(
+                s.adv_id(),
+                enable,
+                include_adi,
+            );
         }
     }
 
