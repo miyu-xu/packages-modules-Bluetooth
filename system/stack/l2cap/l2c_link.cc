374d221bbeb81c97bbb06ad95a5a9094f1f2d8a4
Huirong Liao <huirong.liao@mediatek.com>
2022-09-23 16:36:56 +0800

    [DTV03173493][] Fix L2CAP issue
    [Description]
    1. Fix send SMP_Pairing_Req simutaneously issue, cl4334563
    2. Fix when gatt disconnect too fast, RC pair fail, cl4372193
    3. Fix connect fail when bonding then connect, cl4941390
    4. Fix pts issue, cl 5250478
    5. Fix Fix mi rabbit speaker reconnect fail, cl 5430547
    
    [Test Report]
    a2dp, hogp connect successully
    
    CR-Id: DTV03173493
    Change-Id: I1b2aafb3df65026d060c208a935b7c217e399c00

--- system/stack/l2cap/l2c_link.cc
+++ system/stack/l2cap/l2c_link.cc
@@ -41,6 +41,9 @@
 #include "stack/l2cap/l2c_int.h"
 #include "types/bt_transport.h"
 #include "types/raw_address.h"
+/** for devices in IOT list, accept connection as specific role @{*/
+#include "device/include/interop.h"
+/** @} */
 
 extern tBTM_CB btm_cb;
 
@@ -105,6 +108,19 @@ void l2c_link_hci_conn_req(const RawAddress& bd_addr) {
         p_lcb->SetLinkRoleAsCentral();
     }
 
+    /** for devices in IOT list, accept connection as specific role @{*/
+    LOG_DEBUG("%s: p_lcb->LinkRole()=%d", __func__, p_lcb->LinkRole());
+    if ((HCI_ROLE_CENTRAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_SLAVE, &bd_addr)){
+      p_lcb->SetLinkRoleAsPeripheral();
+    } else if ((HCI_ROLE_PERIPHERAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_MASTER, &bd_addr)){
+      p_lcb->SetLinkRoleAsCentral();
+    } else {
+      LOG_ERROR("p_lcb->link_role must be HCI_ROLE_CENTRAL or HCI_ROLE_PERIPHERAL");
+    }
+    /** @} */
+
     /* Tell the other side we accept the connection */
     acl_accept_connection_request(bd_addr, p_lcb->LinkRole());
 
@@ -124,6 +140,19 @@ void l2c_link_hci_conn_req(const RawAddress& bd_addr) {
       p_lcb->SetLinkRoleAsPeripheral();
     else
       p_lcb->SetLinkRoleAsCentral();
+
+    /** for devices in IOT list, accept connection as specific role @{*/
+    LOG_DEBUG("%s: p_lcb->LinkRole()=%d", __func__, p_lcb->LinkRole());
+    if ((HCI_ROLE_CENTRAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_SLAVE, &bd_addr)){
+      p_lcb->SetLinkRoleAsPeripheral();
+    } else if ((HCI_ROLE_PERIPHERAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_MASTER, &bd_addr)){
+      p_lcb->SetLinkRoleAsCentral();
+    } else {
+      LOG_ERROR("p_lcb->link_role must be HCI_ROLE_CENTRAL or HCI_ROLE_PERIPHERAL");
+    }
+    /** @} */
 
     acl_accept_connection_request(bd_addr, p_lcb->LinkRole());
 
