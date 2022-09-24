f1287157e9d391ea40674871004ccca7e2e22feb
Rahul Arya <aryarahul@google.com>
2022-09-24 01:52:56 +0000

    [Pandora] Remove beacons from RootCanal PHY
    PTS-bot is *meant* to only look for advertisements from the known DUT
    address, but what is and what ought to be are not always the same thing.
    
    Test: GAP pts-bot tests
    Bug: 248814583
    
    Change-Id: Icef61d6339677927801a8da07d7cdb7277695538

--- android/pandora/mmi2grpc/mmi2grpc/__init__.py
+++ android/pandora/mmi2grpc/mmi2grpc/__init__.py
@@ -76,6 +76,7 @@ class IUT:
     def __enter__(self):
         """Resets the IUT when starting a PTS test."""
         self.rootcanal = RootCanal()
+        self.rootcanal.disconnect_phy()
         self.rootcanal.reconnect_phone()
 
         # Note: we don't keep a single gRPC channel instance in the IUT class
