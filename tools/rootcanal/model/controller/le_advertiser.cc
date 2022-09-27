c1b3165acad0b917165a905b8d2abfea5249af49
Henri Chataing <henrichataing@google.com>
2022-09-27 15:01:38 +0000

    RootCanal: Refactor LE scanning commands
    Implement HCI parameter validation as required by the specification.
    Resolve or generate private addresses when required in
    6.B.6.3 Privacy in scanning state.
    
    Test: atest --host --no-bazel rootcanal_hci_test
    Change-Id: Ice2e2e46b8ca157ca1b4131d0c7a642a2074ddc6

--- tools/rootcanal/model/controller/le_advertiser.cc
+++ tools/rootcanal/model/controller/le_advertiser.cc
@@ -50,6 +50,18 @@ ErrorCode LinkLayerController::LeSetAdvertisingParameters(
     PeerAddressType peer_address_type, Address peer_address,
     uint8_t advertising_channel_map,
     AdvertisingFilterPolicy advertising_filter_policy) {
+  // Legacy advertising commands are disallowed when extended advertising
+  // commands were used since the last reset.
+  if (extended_advertising_in_use_) {
+    LOG_INFO(
+        "legacy advertising command rejected because extended advertising"
+        " is being used");
+    return ErrorCode::COMMAND_DISALLOWED;
+  }
+
+  // Using legacy advertising now.
+  legacy_advertising_in_use_ = true;
+
   // Clear reserved bits.
   advertising_channel_map &= 0x7;
 
@@ -118,21 +130,56 @@ ErrorCode LinkLayerController::LeSetAdvertisingParameters(
 }
 
 // HCI command LE_Set_Advertising_Data (Vol 4, Part E § 7.8.7).
-void LinkLayerController::LeSetAdvertisingData(
+ErrorCode LinkLayerController::LeSetAdvertisingData(
     const std::vector<uint8_t>& advertising_data) {
+  // Legacy advertising commands are disallowed when extended advertising
+  // commands were used since the last reset.
+  if (extended_advertising_in_use_) {
+    LOG_INFO(
+        "legacy advertising command rejected because extended advertising"
+        " is being used");
+    return ErrorCode::COMMAND_DISALLOWED;
+  }
+
+  // Using legacy advertising now.
+  legacy_advertising_in_use_ = true;
+
   legacy_advertiser_.advertising_data = advertising_data;
+  return ErrorCode::SUCCESS;
 }
 
 // HCI command LE_Set_Scan_Response_Data (Vol 4, Part E § 7.8.8).
-void LinkLayerController::LeSetScanResponseData(
+ErrorCode LinkLayerController::LeSetScanResponseData(
     const std::vector<uint8_t>& scan_response_data) {
+  // Legacy advertising commands are disallowed when extended advertising
+  // commands were used since the last reset.
+  if (extended_advertising_in_use_) {
+    LOG_INFO(
+        "legacy advertising command rejected because extended advertising"
+        " is being used");
+    return ErrorCode::COMMAND_DISALLOWED;
+  }
+
+  // Using legacy advertising now.
+  legacy_advertising_in_use_ = true;
+
   legacy_advertiser_.scan_response_data = scan_response_data;
+  return ErrorCode::SUCCESS;
 }
 
 // HCI command LE_Advertising_Enable (Vol 4, Part E § 7.8.9).
 ErrorCode LinkLayerController::LeSetAdvertisingEnable(bool advertising_enable) {
-  // Note: additional checks would apply in the case of a LE only Controller
-  // with no configured public device address.
+  // Legacy advertising commands are disallowed when extended advertising
+  // commands were used since the last reset.
+  if (extended_advertising_in_use_) {
+    LOG_INFO(
+        "legacy advertising command rejected because extended advertising"
+        " is being used");
+    return ErrorCode::COMMAND_DISALLOWED;
+  }
+
+  // Using legacy advertising now.
+  legacy_advertising_in_use_ = true;
 
   if (!advertising_enable) {
     legacy_advertiser_.advertising_enable = false;
@@ -146,6 +193,9 @@ ErrorCode LinkLayerController::LeSetAdvertisingEnable(bool advertising_enable) {
                                  AddressType::RANDOM_DEVICE_ADDRESS};
   std::optional<AddressWithType> resolvable_address =
       GenerateResolvablePrivateAddress(peer_address, IrkSelection::Local);
+
+  // TODO: additional checks would apply in the case of a LE only Controller
+  // with no configured public device address.
 
   switch (legacy_advertiser_.own_address_type) {
     case OwnAddressType::PUBLIC_DEVICE_ADDRESS:
