7c6b7c4a1b4cb82bd17aeed3f4d5aa737e983fd1
Jakub Tyszkowski <tyszkowski@google.com>
2022-09-27 10:20:28 +0000

    LeAudioService: Fix failing unit test
    Now when the device is unbonded, we trigger the disconnection
    if device is not in a disconnected state. Tests need to catch this
    intent.
    
    Fixes the following:
    
    [31/39]
    com.android.bluetooth.le_audio.LeAudioServiceTest#testDeleteStateMachineUnbondEvents:
    FAILED (237ms)
    
    STACKTRACE:
    expected: 0
    but was : 2
            at
    	com.android.bluetooth.le_audio.LeAudioServiceTest.verifyConnectionStateIntent(LeAudioServiceTest.java:289)
            at
    	com.android.bluetooth.le_audio.LeAudioServiceTest.generateConnectionMessageFromNative(LeAudioServiceTest.java:846)
            at
    	com.android.bluetooth.le_audio.LeAudioServiceTest.testDeleteStateMachineUnbondEvents(LeAudioServiceTest.java:713)
    
    Test: atest com.android.bluetooth.le_audio --no-bazel-mode
    Bug: 249250843
    Tag: #feature
    Change-Id: I1f87ab52bb57af1fe1e55c9d7613b851c013c25c

--- android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -285,10 +285,9 @@ public class LeAudioServiceTest {
         assertThat(intent).isNotNull();
         assertThat(intent.getAction())
                 .isEqualTo(BluetoothLeAudio.ACTION_LE_AUDIO_CONNECTION_STATE_CHANGED);
-        assertThat(device).isEqualTo(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
-        assertThat(newState).isEqualTo(intent.getIntExtra(BluetoothProfile.EXTRA_STATE, -1));
-        assertThat(prevState).isEqualTo(intent.getIntExtra(BluetoothProfile.EXTRA_PREVIOUS_STATE,
-                -1));
+        assertThat((BluetoothDevice)intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)).isEqualTo(device);
+        assertThat(intent.getIntExtra(BluetoothProfile.EXTRA_STATE, -1)).isEqualTo(newState);
+        assertThat(intent.getIntExtra(BluetoothProfile.EXTRA_PREVIOUS_STATE, -1)).isEqualTo(prevState);
     }
 
     /**
@@ -707,25 +706,29 @@ public class LeAudioServiceTest {
         // Device unbond - state machine is not removed
         mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_NONE);
         assertThat(mService.getDevices().contains(mLeftDevice)).isTrue();
+        verifyConnectionStateIntent(TIMEOUT_MS, mLeftDevice, BluetoothProfile.STATE_DISCONNECTED,
+                BluetoothProfile.STATE_CONNECTING);
 
         // LeAudio stack event: CONNECTION_STATE_CONNECTED - state machine is not removed
         mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_BONDED);
         generateConnectionMessageFromNative(mLeftDevice, BluetoothProfile.STATE_CONNECTED,
-                BluetoothProfile.STATE_CONNECTING);
-        assertThat(BluetoothProfile.STATE_CONNECTED)
-                .isEqualTo(mService.getConnectionState(mLeftDevice));
+                BluetoothProfile.STATE_DISCONNECTED);
+        assertThat(mService.getConnectionState(mLeftDevice))
+                .isEqualTo(BluetoothProfile.STATE_CONNECTED);
         assertThat(mService.getDevices().contains(mLeftDevice)).isTrue();
         // Device unbond - state machine is not removed
         mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_NONE);
         assertThat(mService.getDevices().contains(mLeftDevice)).isTrue();
-
-        // LeAudio stack event: CONNECTION_STATE_DISCONNECTING - state machine is not removed
-        mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_BONDED);
-        generateConnectionMessageFromNative(mLeftDevice, BluetoothProfile.STATE_DISCONNECTING,
+        verifyConnectionStateIntent(TIMEOUT_MS, mLeftDevice, BluetoothProfile.STATE_DISCONNECTING,
                 BluetoothProfile.STATE_CONNECTED);
         assertThat(BluetoothProfile.STATE_DISCONNECTING)
                 .isEqualTo(mService.getConnectionState(mLeftDevice));
         assertThat(mService.getDevices().contains(mLeftDevice)).isTrue();
+
+        // LeAudio stack event: CONNECTION_STATE_DISCONNECTING - state machine is not removed
+        mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_BONDED);
+        assertThat(BluetoothProfile.STATE_DISCONNECTING)
+                .isEqualTo(mService.getConnectionState(mLeftDevice));
         // Device unbond - state machine is not removed
         mService.bondStateChanged(mLeftDevice, BluetoothDevice.BOND_NONE);
         assertThat(mService.getDevices().contains(mLeftDevice)).isTrue();
