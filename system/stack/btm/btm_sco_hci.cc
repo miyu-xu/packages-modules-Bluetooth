6459d0744edc2f4dc66768533b18eeb0de31c464
En-Shuo Hsu <enshuo@google.com>
2022-09-28 22:41:30 +0000

    WIP: Add unittest for SCO over HCI interface
    Change-Id: I2ab14ba04853f9b19336a33dc87d29e02c1f7fc1

--- system/stack/btm/btm_sco_hci.cc
+++ system/stack/btm/btm_sco_hci.cc
@@ -88,7 +88,13 @@ void open() {
   if (sco_uipc != nullptr) {
     LOG_WARN("Re-opening UIPC that is already running");
   }
+
   sco_uipc = UIPC_Init();
+  if (sco_uipc == nullptr) {
+    LOG_ERROR("%s failed to init UIPC", __func__);
+    return;
+  }
+
   UIPC_Open(*sco_uipc, UIPC_CH_ID_AV_AUDIO, sco_data_cb, SCO_HOST_DATA_PATH);
   struct group* grp = getgrnam(SCO_HOST_DATA_GROUP);
   chmod(SCO_HOST_DATA_PATH, 0770);
@@ -413,7 +419,7 @@ struct tBTM_MSBC_INFO {
   }
 
  public:
-  void init(size_t pkt_size) {
+  size_t init(size_t pkt_size) {
     decode_buf_wo = 0;
     decode_buf_ro = 0;
     encode_buf_wo = 0;
@@ -421,7 +427,7 @@ struct tBTM_MSBC_INFO {
 
     pkt_size = get_supported_packet_size(pkt_size, &buf_size);
     if (pkt_size != BTM_MSBC_PKT_LEN) check_alignment = true;
-    if (pkt_size == packet_size) return;
+    if (pkt_size == packet_size) return packet_size;
     packet_size = pkt_size;
 
     if (msbc_decode_buf) osi_free(msbc_decode_buf);
@@ -436,6 +442,7 @@ struct tBTM_MSBC_INFO {
     }
     plc = (tBTM_MSBC_PLC*)osi_calloc(sizeof(*plc));
     plc->init();
+    return packet_size;
   }
 
   void deinit() {
@@ -536,7 +543,7 @@ struct tBTM_MSBC_INFO {
 
 static tBTM_MSBC_INFO* msbc_info = nullptr;
 
-void init(size_t pkt_size) {
+size_t init(size_t pkt_size) {
   hfp_msbc_decoder_init();
   hfp_msbc_encoder_init();
 
@@ -547,7 +554,7 @@ void init(size_t pkt_size) {
   }
 
   msbc_info = (tBTM_MSBC_INFO*)osi_calloc(sizeof(*msbc_info));
-  msbc_info->init(pkt_size);
+  return msbc_info->init(pkt_size);
 }
 
 void cleanup() {
@@ -575,6 +582,11 @@ size_t enqueue_packet(const uint8_t* data, size_t pkt_size) {
     return 0;
   }
 
+  if (data == nullptr) {
+    LOG_WARN("Invalid data to enqueue");
+    return 0;
+  }
+
   if (msbc_info->check_alignment) {
     if (data[0] != BTM_MSBC_H2_HEADER_0 || data[2] != BTM_MSBC_SYNC_WORD) {
       LOG_DEBUG("Waiting for valid mSBC frame head");
@@ -597,6 +609,11 @@ size_t decode(const uint8_t** out_data) {
 
   if (msbc_info == nullptr) {
     LOG_WARN("mSBC buffer uninitialized or cleaned");
+    return 0;
+  }
+
+  if (out_data == nullptr) {
+    LOG_WARN("Invalid output pointer");
     return 0;
   }
 
@@ -640,6 +657,11 @@ size_t encode(int16_t* data, size_t len) {
     return 0;
   }
 
+  if (data == nullptr) {
+    LOG_WARN("Invalid data to encode");
+    return 0;
+  }
+
   if (len < BTM_MSBC_CODE_SIZE) {
     LOG_DEBUG(
         "PCM frames with size %lu is insufficient to be encoded into a mSBC "
@@ -667,6 +689,11 @@ size_t encode(int16_t* data, size_t len) {
 size_t dequeue_packet(const uint8_t** output) {
   if (msbc_info == nullptr) {
     LOG_WARN("mSBC buffer uninitialized or cleaned");
+    return 0;
+  }
+
+  if (output == nullptr) {
+    LOG_WARN("Invalid output pointer");
     return 0;
   }
 
---
+++ system/stack/test/btm/sco_hci_test.cc
@@ -0,0 +1,328 @@
+/*
+ *
+ *  Copyright 2022 The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <map>
+#include <memory>
+
+#include "stack/btm/btm_sco.h"
+#include "udrv/include/uipc.h"
+
+using testing::AllOf;
+using testing::Ge;
+using testing::Le;
+
+extern std::map<std::string, int> mock_function_count_map;
+std::unique_ptr<tUIPC_STATE> mock_uipc_init_ret;
+uint32_t mock_uipc_read_ret;
+bool mock_uipc_send_ret;
+
+namespace {
+
+using testing::Test;
+
+class ScoHciTest : public Test {
+ public:
+ protected:
+  void SetUp() override {
+    mock_function_count_map.clear();
+    mock_uipc_init_ret = nullptr;
+    mock_uipc_read_ret = 0;
+    mock_uipc_send_ret = true;
+  }
+  void TearDown() override {}
+};
+
+class ScoHciWithOpenCleanTest : public ScoHciTest {
+ public:
+ protected:
+  void SetUp() override {
+    mock_uipc_init_ret = std::make_unique<tUIPC_STATE>();
+    bluetooth::audio::sco::open();
+  }
+  void TearDown() override { bluetooth::audio::sco::cleanup(); }
+};
+
+class ScoHciWbsWithInitCleanTest : public Test {
+ public:
+ protected:
+  void SetUp() override { bluetooth::audio::sco::wbs::init(60); }
+  void TearDown() override { bluetooth::audio::sco::wbs::cleanup(); }
+};
+
+/*
+TEST_F(ScoHciTest, ScoOverHciOpenFail) {
+  bluetooth::audio::sco::open();
+  ASSERT_EQ(mock_function_count_map["UIPC_Init"], 1);
+  ASSERT_EQ(mock_function_count_map["UIPC_Open"], 0);
+}
+
+TEST_F(ScoHciTest, ScoOverHciOpenClean) {
+  mock_uipc_init_ret = std::make_unique<tUIPC_STATE>();
+  bluetooth::audio::sco::open();
+  ASSERT_EQ(mock_function_count_map["UIPC_Init"], 1);
+  ASSERT_EQ(mock_function_count_map["UIPC_Open"], 1);
+
+  mock_uipc_init_ret = std::make_unique<tUIPC_STATE>();
+  // Double open will override uipc
+  bluetooth::audio::sco::open();
+  ASSERT_EQ(mock_function_count_map["UIPC_Init"], 2);
+  ASSERT_EQ(mock_function_count_map["UIPC_Open"], 2);
+
+  bluetooth::audio::sco::cleanup();
+  ASSERT_EQ(mock_function_count_map["UIPC_Close"], 1);
+
+  // Double clean shouldn't fail
+  bluetooth::audio::sco::cleanup();
+  ASSERT_EQ(mock_function_count_map["UIPC_Close"], 1);
+}
+
+TEST_F(ScoHciTest, ScoOverHciReadNoOpen) {
+  uint8_t buf[100];
+  ASSERT_EQ(bluetooth::audio::sco::read(buf, sizeof(buf)), size_t(0));
+  ASSERT_EQ(mock_function_count_map["UIPC_Read"], 0);
+}
+
+TEST_F(ScoHciWithOpenCleanTest, ScoOverHciRead) {
+  uint8_t buf[100];
+  mock_uipc_read_ret = sizeof(buf);
+  ASSERT_EQ(bluetooth::audio::sco::read(buf, sizeof(buf)), mock_uipc_read_ret);
+  ASSERT_EQ(mock_function_count_map["UIPC_Read"], 1);
+}
+
+TEST_F(ScoHciTest, ScoOverHciWriteNoOpen) {
+  uint8_t buf[100];
+  bluetooth::audio::sco::write(buf, sizeof(buf));
+  ASSERT_EQ(mock_function_count_map["UIPC_Send"], 0);
+}
+
+TEST_F(ScoHciWithOpenCleanTest, ScoOverHciWrite) {
+  uint8_t buf[100];
+  ASSERT_EQ(bluetooth::audio::sco::write(buf, sizeof(buf)), sizeof(buf));
+  ASSERT_EQ(mock_function_count_map["UIPC_Send"], 1);
+
+  // Send fails
+  mock_uipc_send_ret = false;
+  ASSERT_EQ(bluetooth::audio::sco::write(buf, sizeof(buf)), size_t(0));
+  ASSERT_EQ(mock_function_count_map["UIPC_Send"], 2);
+}
+
+TEST(ScoHciWbsTest, WbsInit) {
+  ASSERT_EQ(bluetooth::audio::sco::wbs::init(60), size_t(60));
+  ASSERT_EQ(bluetooth::audio::sco::wbs::init(72), size_t(72));
+  // Fallback to 60 if the packet size is not supported
+  ASSERT_EQ(bluetooth::audio::sco::wbs::init(48), size_t(60));
+  bluetooth::audio::sco::wbs::cleanup();
+}
+
+TEST(ScoHciWbsTest, WbsEnqueuePacketWithoutInit) {
+  uint8_t payload[60];
+  // Return 0 if buffer is uninitialized
+  ASSERT_EQ(
+      bluetooth::audio::sco::wbs::enqueue_packet(payload, sizeof(payload)),
+      size_t(0));
+}
+
+TEST_F(ScoHciWbsWithInitCleanTest, WbsEnqueuePacket) {
+  uint8_t payload[60];
+  // Return 0 if payload is invalid
+  ASSERT_EQ(
+      bluetooth::audio::sco::wbs::enqueue_packet(nullptr, sizeof(payload)),
+      size_t(0));
+  // Return 0 if packet size is consistent
+  ASSERT_EQ(bluetooth::audio::sco::wbs::enqueue_packet(payload, 72), size_t(0));
+  ASSERT_EQ(
+      bluetooth::audio::sco::wbs::enqueue_packet(payload, sizeof(payload)),
+      size_t(60));
+  // Return 0 if buffer is full
+  ASSERT_EQ(
+      bluetooth::audio::sco::wbs::enqueue_packet(payload, sizeof(payload)),
+      size_t(0));
+}
+
+TEST(ScoHciWbsTest, WbsDecodeWithoutInit) {
+  const uint8_t* decoded = nullptr;
+  // Return 0 if buffer is uninitialized
+  ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded), size_t(0));
+  ASSERT_EQ(decoded, nullptr);
+}
+
+TEST_F(ScoHciWbsWithInitCleanTest, WbsDecode) {
+  const uint8_t* decoded = nullptr;
+  uint8_t payload[60] = {0};
+
+  // No data to decode
+  ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded), size_t(0));
+  ASSERT_EQ(decoded, nullptr);
+  // Fill in invalid packet, all zeros.
+  ASSERT_EQ(
+      bluetooth::audio::sco::wbs::enqueue_packet(payload, sizeof(payload)),
+      sizeof(payload));
+  // PLC should be triggered and return valid frames
+  ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded),
+            size_t(BTM_MSBC_CODE_SIZE));
+  ASSERT_NE(decoded, nullptr);
+
+  decoded = nullptr;
+  uint8_t msbc_zero_packet[] = {
+      0x01, 0x08, 0xad, 0x00, 0x00, 0xc5, 0x00, 0x00, 0x00, 0x00, 0x77, 0x6d,
+      0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6, 0xdb, 0x77,
+      0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6, 0xdb,
+      0x77, 0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6,
+      0xdb, 0x77, 0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6c, 0x00};
+
+  ASSERT_EQ(bluetooth::audio::sco::wbs::enqueue_packet(msbc_zero_packet,
+                                                       sizeof(payload)),
+            sizeof(msbc_zero_packet));
+  ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded),
+            size_t(BTM_MSBC_CODE_SIZE));
+  ASSERT_NE(decoded, nullptr);
+  for (size_t i = 0; i < BTM_MSBC_CODE_SIZE; i++) ASSERT_EQ(decoded[i], 0);
+
+  decoded = nullptr;
+  // No remaining data to decode
+  ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded), size_t(0));
+  ASSERT_EQ(decoded, nullptr);
+}
+
+TEST(ScoHciWbsTest, WbsEncodeWithoutInit) {
+  int16_t data[120] = {0};
+  // Return 0 if buffer is uninitialized
+  ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)), size_t(0));
+}
+
+TEST_F(ScoHciWbsWithInitCleanTest, WbsEncode) {
+  int16_t data[120] = {0};
+
+  // Return 0 if data is invalid
+  ASSERT_EQ(bluetooth::audio::sco::wbs::encode(nullptr, sizeof(data)),
+            size_t(0));
+  // Return 0 if data length is insufficient
+  ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data) - 1),
+            size_t(0));
+  ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)),
+            sizeof(data));
+
+  // Return 0 if the packet buffer is full
+  ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)), size_t(0));
+}
+
+TEST(ScoHciWbsTest, WbsDequeuePacketWithoutInit) {
+  const uint8_t* encoded = nullptr;
+  // Return 0 if buffer is uninitialized
+  ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(&encoded), size_t(0));
+  ASSERT_EQ(encoded, nullptr);
+}
+
+TEST_F(ScoHciWbsWithInitCleanTest, WbsDequeuePacket) {
+  const uint8_t* encoded = nullptr;
+  // Return 0 if output pointer is invalid
+  ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(nullptr), size_t(0));
+  ASSERT_EQ(encoded, nullptr);
+
+  // Return 0 if there is insufficient data to dequeue
+  ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(&encoded), size_t(0));
+  ASSERT_EQ(encoded, nullptr);
+}
+
+TEST_F(ScoHciWbsWithInitCleanTest, WbsEncodeDequeuePackets) {
+  uint8_t h2_header_frames_count[] = {0x08, 0x38, 0xc8, 0xf8};
+  int16_t data[120] = {0};
+  const uint8_t* encoded = nullptr;
+  uint8_t msbc_zero_packet[] = {
+      0x01, 0x08, 0xad, 0x00, 0x00, 0xc5, 0x00, 0x00, 0x00, 0x00, 0x77, 0x6d,
+      0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6, 0xdb, 0x77,
+      0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6, 0xdb,
+      0x77, 0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6d, 0xdd, 0xb6,
+      0xdb, 0x77, 0x6d, 0xb6, 0xdd, 0xdb, 0x6d, 0xb7, 0x76, 0xdb, 0x6c, 0x00};
+
+  for (size_t i = 0; i < 5; i++) {
+    msbc_zero_packet[1] = h2_header_frames_count[i % 4];
+    ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)),
+              sizeof(data));
+    ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(&encoded), size_t(60));
+    ASSERT_NE(encoded, nullptr);
+    for (size_t j = 0; j < 60; j++) {
+      ASSERT_EQ(encoded[j], msbc_zero_packet[j]);
+    }
+  }
+}
+*/
+TEST_F(ScoHciWbsWithInitCleanTest, WbsPlc) {
+  int16_t sine[16] = {0, 100,  200,  300,  400,  300,  200,  100,
+                      0, -100, -200, -300, -400, -300, -200, -100};
+  int16_t data[120];
+  int16_t expect_data[120];
+  const uint8_t* encoded = nullptr;
+  const uint8_t* decoded = nullptr;
+  uint8_t invalid_pkt[60] = {0};
+  size_t plc_idx = 45;
+
+  // Simulate a run without packet loss
+  for (size_t i = 0, sample_idx = 0; i < 50; i++) {
+    // Data is a 1000Hz sine wave
+    for (size_t j = 0; j < 120; j++, sample_idx++)
+      data[j] = sine[sample_idx % 16];
+    ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)),
+              sizeof(data));
+    ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(&encoded), size_t(60));
+    ASSERT_NE(encoded, nullptr);
+
+    // Receive the packet
+    ASSERT_EQ(bluetooth::audio::sco::wbs::enqueue_packet(encoded, 60),
+              size_t(60));
+    ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded),
+              size_t(BTM_MSBC_CODE_SIZE));
+    ASSERT_NE(decoded, nullptr);
+    if (i == plc_idx)
+      std::copy((const int16_t*)decoded,
+                (const int16_t*)(decoded + BTM_MSBC_CODE_SIZE), expect_data);
+  }
+  // Start with fresh buffer
+  bluetooth::audio::sco::wbs::cleanup();
+  bluetooth::audio::sco::wbs::init(60);
+  // Simulate a run with packet loss
+  for (size_t i = 0, sample_idx = 0; i <= plc_idx; i++) {
+    // Data is a 1000Hz sine wave
+    for (size_t j = 0; j < 120; j++, sample_idx++)
+      data[j] = sine[sample_idx % 16];
+    ASSERT_EQ(bluetooth::audio::sco::wbs::encode(data, sizeof(data)),
+              sizeof(data));
+    ASSERT_EQ(bluetooth::audio::sco::wbs::dequeue_packet(&encoded), size_t(60));
+    ASSERT_NE(encoded, nullptr);
+
+    // Receive the packet. Substitute to invalid when packet loss.
+    ASSERT_EQ(bluetooth::audio::sco::wbs::enqueue_packet(
+                  i != plc_idx ? encoded : invalid_pkt, 60),
+              size_t(60));
+    ASSERT_EQ(bluetooth::audio::sco::wbs::decode(&decoded),
+              size_t(BTM_MSBC_CODE_SIZE));
+    ASSERT_NE(decoded, nullptr);
+  }
+  int16_t* ptr = (int16_t*)decoded;
+  for (size_t i = 0; i < 120; i++)
+    EXPECT_THAT(ptr[i] - expect_data[i], AllOf(Ge(-3), Le(3)))
+        << "PLC data " << ptr[i] << " deviates from expected "
+        << expect_data[i] << " at index " << i;
+  //        ASSERT_EQ(*((int16_t*)(decoded + 2 * i)), expect_data[i]);
+}
+
+}  // namespace
