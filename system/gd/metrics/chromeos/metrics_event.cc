952426d729b2e504fd0ca5653e9fe00aaaa979ed
Michael Sun <michaelfsun@google.com>
2022-09-28 12:52:32 -0700

    floss: metrics: convert device type into BlueZ compatible type
    When reporting device type for paring/bonding state changes, BlueZ uses
    connection types (LE v.s., BREDR) instead of device type (LE, BREDR, DUAL).
     Update the Floss metrics to use the same conventions.
    
    However, minimal information is available regarding the paring/bonding over
     which connection type at the Floss. Even the bond initiator can use the
    "auto" type without specifying LE v.s., BREDR. So the type matching is
    through best effort. A previously recorded connection type will apply to
    the following metrics event, and an "auto" connection or DUAL type device
    will be categorized as  UNKNOWN before submission.
    
    BUG: 240781725
    Tag: #floss
    Test: emerge-${BOARD} floss
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I9063ed5039d671d3ad8c79a0167eb180edac4367

--- system/gd/metrics/chromeos/metrics_event.cc
+++ system/gd/metrics/chromeos/metrics_event.cc
@@ -27,6 +27,8 @@ namespace metrics {
 
 // topshim::btif::BtBondState is a copy of hardware/bluetooth.h:bt_bond_state_t
 typedef bt_bond_state_t BtBondState;
+// topshim::btif::BtDeviceType is a copy of hardware/bluetooth.h:bt_device_type_t
+typedef bt_device_type_t BtDeviceType;
 // topshim::btif::BtStatus is a copy of hardware/bluetooth.h:bt_status_t
 typedef bt_status_t BtStatus;
 // topshim::profile::hid_host::BthhConnectionState is a copy of hardware/bluetooth.h:bthh_connection_state_t
@@ -197,6 +199,27 @@ static PairingState FailReasonToPairingState(int32_t fail_reason) {
 
 AdapterState ToAdapterState(uint32_t state) {
   return state == 1 ? AdapterState::ON : AdapterState::OFF;
+}
+
+ConnectionType ToPairingDeviceType(std::string addr, uint32_t device_type) {
+  // A map stores the pending ConnectionType used to match a pairing event with unknown type.
+  // map<address, type>
+  static std::map<std::string, ConnectionType> pending_type;
+
+  switch ((BtDeviceType)device_type) {
+    case BtDeviceType::BT_DEVICE_DEVTYPE_BLE:
+      pending_type[addr] = ConnectionType::CONN_TYPE_LE;
+      return ConnectionType::CONN_TYPE_LE;
+    case BtDeviceType::BT_DEVICE_DEVTYPE_BREDR:
+      pending_type[addr] = ConnectionType::CONN_TYPE_BREDR;
+      return ConnectionType::CONN_TYPE_BREDR;
+    case BtDeviceType::BT_DEVICE_DEVTYPE_DUAL:
+      if (pending_type.find(addr) != pending_type.end()) {
+        return pending_type[addr];
+      } else {
+        return ConnectionType::CONN_TYPE_UNKNOWN;
+      }
+  }
 }
 
 PairingState ToPairingState(uint32_t status, uint32_t bond_state, int32_t fail_reason) {
