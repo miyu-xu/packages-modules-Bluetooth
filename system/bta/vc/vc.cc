4d19d13fe148b71fa716dc43fa082550d8dab5de
Jakub Tyszkowski <tyszkowski@google.com>
2022-10-06 12:50:05 +0000

    vc: Send control point request only if device is ready
    When trying to do control point operations, being connected
    is not enough as we also need to know the volume change counter.
    
    If a group API is used to set volume bofore the individual
    devices are reported as connected, we should not set it
    as we dont know the volume change counter yet. The group volume
    which could not be applied because of this, will be applied from
    the cache once we report the device as connected.
    
    Test: atest --host bluetooth_vc_test --no-bazel-mode
    Bug: 248475583
    Tag: #feature
    Change-Id: I27b462f7f55122cc7c38fb18908d52e86ea4b5d8

--- system/bta/vc/vc.cc
+++ system/bta/vc/vc.cc
@@ -389,7 +389,7 @@ class VolumeControlImpl : public VolumeControl {
               << loghex(device->mute) << " change_counter "
               << loghex(device->change_counter);
 
-    if (!device->device_ready) return;
+    if (!device->IsReady()) return;
 
     /* This is just a read, send single notification */
     if (!is_notification) {
@@ -462,7 +462,7 @@ class VolumeControlImpl : public VolumeControl {
               << " offset: " << loghex(offset->offset)
               << " counter: " << loghex(offset->change_counter);
 
-    if (!device->device_ready) return;
+    if (!device->IsReady()) return;
 
     callbacks_->OnExtAudioOutVolumeOffsetChanged(device->address, offset->id,
                                                  offset->offset);
@@ -483,7 +483,7 @@ class VolumeControlImpl : public VolumeControl {
     LOG(INFO) << __func__ << "id " << loghex(offset->id) << "location "
               << loghex(offset->location);
 
-    if (!device->device_ready) return;
+    if (!device->IsReady()) return;
 
     callbacks_->OnExtAudioOutLocationChanged(device->address, offset->id,
                                              offset->location);
@@ -514,7 +514,7 @@ class VolumeControlImpl : public VolumeControl {
 
     LOG(INFO) << __func__ << " " << description;
 
-    if (!device->device_ready) return;
+    if (!device->IsReady()) return;
 
     callbacks_->OnExtAudioOutDescriptionChanged(device->address, offset->id,
                                                 std::move(description));
@@ -584,7 +584,7 @@ class VolumeControlImpl : public VolumeControl {
     }
 
     // If we get here, it means, device has not been exlicitly disconnected.
-    bool device_ready = device->device_ready;
+    bool device_ready = device->IsReady();
 
     device_cleanup_helper(device, device->connecting_actively);
 
@@ -772,7 +772,7 @@ class VolumeControlImpl : public VolumeControl {
                 (std::get<RawAddress>(addr_or_group_id)).ToString().c_str());
       VolumeControlDevice* dev = volume_control_devices_.FindByAddress(
           std::get<RawAddress>(addr_or_group_id));
-      if (dev && dev->IsConnected()) {
+      if (dev && dev->IsReady()) {
         std::vector<RawAddress> devices = {dev->address};
         PrepareVolumeControlOperation(devices, bluetooth::groups::kGroupUnknown,
                                       false, opcode, arg);
@@ -790,7 +790,7 @@ class VolumeControlImpl : public VolumeControl {
       auto devices = csis_api->GetDeviceList(group_id);
       for (auto it = devices.begin(); it != devices.end();) {
         auto dev = volume_control_devices_.FindByAddress(*it);
-        if (!dev || !dev->IsConnected()) {
+        if (!dev || !dev->IsReady()) {
           it = devices.erase(it);
         } else {
           it++;
@@ -831,7 +831,7 @@ class VolumeControlImpl : public VolumeControl {
                 std::get<RawAddress>(addr_or_group_id).ToString().c_str());
       VolumeControlDevice* dev = volume_control_devices_.FindByAddress(
           std::get<RawAddress>(addr_or_group_id));
-      if (dev && dev->IsConnected() && (dev->volume != volume)) {
+      if (dev && dev->IsReady() && (dev->volume != volume)) {
         std::vector<RawAddress> devices = {dev->address};
         RemovePendingVolumeControlOperations(devices,
                                              bluetooth::groups::kGroupUnknown);
@@ -851,7 +851,7 @@ class VolumeControlImpl : public VolumeControl {
       auto devices = csis_api->GetDeviceList(group_id);
       for (auto it = devices.begin(); it != devices.end();) {
         auto dev = volume_control_devices_.FindByAddress(*it);
-        if (!dev || !dev->IsConnected()) {
+        if (!dev || !dev->IsReady()) {
           it = devices.erase(it);
         } else {
           it++;
@@ -964,7 +964,7 @@ class VolumeControlImpl : public VolumeControl {
   int latest_operation_id_;
 
   void verify_device_ready(VolumeControlDevice* device, uint16_t handle) {
-    if (device->device_ready) return;
+    if (device->IsReady()) return;
 
     // VerifyReady sets the device_ready flag if all remaining GATT operations
     // are completed
