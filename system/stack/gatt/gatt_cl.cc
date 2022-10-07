899fb1c45e1e9a0353f5d3cd39328f4965bc5568
Łukasz Rymanowski <rlukasz@google.com>
2022-10-07 16:27:08 +0000

    gatt: Improving sending queued gatt request from the tGATT_TCB
    This patch imidiatelly check if there is any outstanding, ready to send
    message on the ATT CID 4 and if not, EATT channels are search for such
    messages.
    
    Withouth this patch code could end checking, if there is oustanding
    request waiting for response.
    
    Bug: 249431973
    Test: atest BluetoothInstrumentationTest
    Test: manual testes with device supporting EATT
    Tag: #feature
    Change-Id: I1d3351ebc82c4f1c0bf7bf0941c50c55bd4ec3e8

--- system/stack/gatt/gatt_cl.cc
+++ system/stack/gatt/gatt_cl.cc
@@ -1130,11 +1130,12 @@ uint8_t gatt_cmd_to_rsp_code(uint8_t cmd_code) {
 
 /** Find next command in queue and sent to server */
 bool gatt_cl_send_next_cmd_inq(tGATT_TCB& tcb) {
-  std::queue<tGATT_CMD_Q>* cl_cmd_q;
+  std::queue<tGATT_CMD_Q>* cl_cmd_q = nullptr;
 
-  while (!tcb.cl_cmd_q.empty() ||
-         EattExtension::GetInstance()->IsOutstandingMsgInSendQueue(tcb.peer_bda)) {
-    if (!tcb.cl_cmd_q.empty()) {
+  while (
+      gatt_is_outstanding_msg_in_att_send_queue(tcb) ||
+      EattExtension::GetInstance()->IsOutstandingMsgInSendQueue(tcb.peer_bda)) {
+    if (gatt_is_outstanding_msg_in_att_send_queue(tcb)) {
       cl_cmd_q = &tcb.cl_cmd_q;
     } else {
       EattChannel* channel =
