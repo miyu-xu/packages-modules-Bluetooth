c3856c91d4aa8002f50ce395558613df76c558f3
Jakub Tyszkowski <tyszkowski@google.com>
2022-10-10 14:25:24 +0000

    vc: Fix doing initial reads and writes twice
    We already handle encryption complete event which is more
    reliable than passing callback to the enryption request.
    We should have a single source of true for the encryption
    result. For a reconnecting device having both was the cause
    of duplicate GATT operations for the initial reads and writes
    (change counter reads etc.)
    
    Bug: 248475583
    Tag: #feature
    Test: tested manually
    Change-Id: Ia4f40a6bead331883a381205cc150e009ec18070

--- system/test/mock/mock_bta_vc_device.cc
+++ system/test/mock/mock_bta_vc_device.cc
@@ -28,7 +28,6 @@ extern std::map<std::string, int> mock_function_count_map;
 #include <vector>
 
 #include "bta/vc/devices.h"
-#include "stack/btm/btm_sec.h"
 
 using namespace bluetooth::vc::internal;
 
@@ -36,7 +35,7 @@ using namespace bluetooth::vc::internal;
 #define UNUSED_ATTR
 #endif
 
-bool VolumeControlDevice::EnableEncryption(tBTM_SEC_CALLBACK* callback) {
+bool VolumeControlDevice::EnableEncryption() {
   mock_function_count_map[__func__]++;
   return false;
 }
