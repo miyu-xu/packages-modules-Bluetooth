a9d278cde8c250326e9c06ccc8a83b68431f5c61
Md Shahriar Hossain Sajib <sajibonly@google.com>
2022-10-13 15:21:15 +0800

    Hfp Audio Policy Client impl
    Bug: 235541516
    Test: manual
    Change-Id: I5fd6c7959a85104f2bb5e31809a0e250c2dd13d0

--- android/app/src/com/android/bluetooth/btservice/ActiveDeviceManager.java
+++ android/app/src/com/android/bluetooth/btservice/ActiveDeviceManager.java
@@ -20,6 +20,7 @@ import android.annotation.RequiresPermission;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothAudioPolicy;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHapClient;
 import android.bluetooth.BluetoothHeadset;
@@ -794,11 +795,14 @@ class ActiveDeviceManager {
         if (headsetService == null) {
             return;
         }
-        if (!headsetService.setActiveDevice(device)) {
-            return;
+        if (headsetService.getHfpCallAudioPolicy(device).getConnectingPolicy()
+                == BluetoothAudioPolicy.CALL_AUDIO_ALLOWED) {
+            if (!headsetService.setActiveDevice(device)) {
+                return;
+            }
+            mHfpActiveDevice = device;
+            mPendingHfpActiveDevice = null;
         }
-        mHfpActiveDevice = device;
-        mPendingHfpActiveDevice = null;
         if (mPendingA2dpActiveDevice != null) {
             setA2dpActiveDevice(mPendingA2dpActiveDevice);
         }
