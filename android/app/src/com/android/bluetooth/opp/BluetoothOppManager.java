f14f283504fdb0d39c7464e6d6f7358a4bb60ab1
Chienyuan <chienyuanhuang@google.com>
2022-10-14 17:48:19 +0800

    OPP: Use identity address
    Tag: #refactor
    Bug: 250516491
    Test: manual
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I4769b33a5d0e5af9b60ff29006d062fcf6c51583

--- android/app/src/com/android/bluetooth/opp/BluetoothOppManager.java
+++ android/app/src/com/android/bluetooth/opp/BluetoothOppManager.java
@@ -470,7 +470,7 @@ public class BluetoothOppManager {
                 }
 
                 values.put(BluetoothShare.MIMETYPE, contentType);
-                values.put(BluetoothShare.DESTINATION, mRemoteDevice.getAddress());
+                values.put(BluetoothShare.DESTINATION, mRemoteDevice.getIdentityAddress());
                 values.put(BluetoothShare.TIMESTAMP, ts);
                 if (mIsHandoverInitiated) {
                     values.put(BluetoothShare.USER_CONFIRMATION,
@@ -480,7 +480,7 @@ public class BluetoothOppManager {
                         mContext.getContentResolver().insert(BluetoothShare.CONTENT_URI, values);
                 if (V) {
                     Log.v(TAG, "Insert contentUri: " + contentUri + "  to device: " + getDeviceName(
-                            mRemoteDevice));
+                            mRemoteDevice.getIdentityAddress()));
                 }
             }
         }
@@ -492,7 +492,7 @@ public class BluetoothOppManager {
             ContentValues values = new ContentValues();
             values.put(BluetoothShare.URI, mUri);
             values.put(BluetoothShare.MIMETYPE, mTypeOfSingleFile);
-            values.put(BluetoothShare.DESTINATION, mRemoteDevice.getAddress());
+            values.put(BluetoothShare.DESTINATION, mRemoteDevice.getIdentityAddress());
             if (mIsHandoverInitiated) {
                 values.put(BluetoothShare.USER_CONFIRMATION,
                         BluetoothShare.USER_CONFIRMATION_HANDOVER_CONFIRMED);
@@ -501,7 +501,7 @@ public class BluetoothOppManager {
                     mContext.getContentResolver().insert(BluetoothShare.CONTENT_URI, values);
             if (V) {
                 Log.v(TAG, "Insert contentUri: " + contentUri + "  to device: " + getDeviceName(
-                        mRemoteDevice));
+                        mRemoteDevice.getIdentityAddress()));
             }
         }
     }
