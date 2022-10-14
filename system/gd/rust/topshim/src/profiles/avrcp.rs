530638071a8fd755de76e510ca93b8644a9fef48
Michael Sun <michaelfsun@google.com>
2022-10-14 11:27:33 -0700

    floss: avrcp: propagate media information through topshim
    Propagate the parsed media information through the rust/c++ barrier.
    Avoided using custom types and used all native cxx-supported ones.
    
    Bug: 240780471
    Tag: #floss
    Test: ./build.py
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: Idd156aa3941870dcb69d5832acdfb0114ecf4979

--- system/gd/rust/topshim/src/profiles/avrcp.rs
+++ system/gd/rust/topshim/src/profiles/avrcp.rs
@@ -9,7 +9,7 @@ pub struct PlayerMetadata {
     pub title: String,
     pub artist: String,
     pub album: String,
-    pub length: i64,
+    pub length_us: i64,
 }
 
 #[cxx::bridge(namespace = bluetooth::topshim::rust)]
@@ -31,6 +31,15 @@ pub mod ffi {
         fn connect(self: Pin<&mut AvrcpIntf>, bt_addr: RustRawAddress) -> u32;
         fn disconnect(self: Pin<&mut AvrcpIntf>, bt_addr: RustRawAddress) -> u32;
         fn set_volume(self: Pin<&mut AvrcpIntf>, volume: i8);
+        fn set_playback_status(self: Pin<&mut AvrcpIntf>, status: &String);
+        fn set_position(self: Pin<&mut AvrcpIntf>, position_us: i64);
+        fn set_metadata(
+            self: Pin<&mut AvrcpIntf>,
+            title: &String,
+            artist: &String,
+            album: &String,
+            length_us: i64,
+        );
 
     }
     extern "Rust" {
@@ -153,5 +162,22 @@ impl Avrcp {
 
     pub fn set_volume(&mut self, volume: i8) {
         self.internal.pin_mut().set_volume(volume);
+    }
+
+    pub fn set_playback_status(&mut self, status: &String) {
+        self.internal.pin_mut().set_playback_status(status);
+    }
+
+    pub fn set_position(&mut self, position_us: i64) {
+        self.internal.pin_mut().set_position(position_us);
+    }
+
+    pub fn set_metadata(&mut self, metadata: &PlayerMetadata) {
+        self.internal.pin_mut().set_metadata(
+            &metadata.title,
+            &metadata.artist,
+            &metadata.album,
+            metadata.length_us,
+        );
     }
 }
