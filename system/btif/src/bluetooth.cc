1d04ec716d861aef566be8e105690e4c5804118b
Rahul Arya <aryarahul@google.com>
2022-10-17 17:26:23 +0000

    [Invisalign] Move profile-specific log levels out of core
    Test: compiles, prints log messages on startup
    Bug: 254063018
    Change-Id: Ice83e58e3342e04f641c1435f4d9676b0a5ea195

--- system/btif/src/bluetooth.cc
+++ system/btif/src/bluetooth.cc
@@ -85,6 +85,7 @@
 #include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "osi/include/wakelock.h"
+#include "profile_log_levels.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
 #include "stack/gatt/connection_manager.h"
 #include "stack/include/avdt_api.h"
@@ -428,6 +429,7 @@ static void start_profiles() {
   // initialize profile-specific logging levels
   const auto stack_config = stack_config_get_interface();
   if (stack_config->get_trace_config_enabled()) {
+    // REMINDER: move this function call up to this CL!!!!
     load_levels_from_config(stack_config->get_all());
   }
 }
