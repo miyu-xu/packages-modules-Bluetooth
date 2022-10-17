1559468ffcbb1b2f2c9c8300c4e6bb8b7e65baa5
Rahul Arya <aryarahul@google.com>
2022-10-17 22:54:31 +0000

    ???
    Change-Id: I8a8584eb1331f51b60382ad3da8aa081d8ff2b0d

--- system/btif/src/bluetooth.cc
+++ system/btif/src/bluetooth.cc
@@ -762,8 +762,6 @@ static void dump(int fd, const char** arguments) {
   connection_manager::dump(fd);
   bluetooth::bqr::DebugDump(fd);
   bluetooth::shim::Dump(fd, arguments);
-  PAN_Dumpsys(fd);
-  DumpsysHid(fd);
 }
 
 static void dumpMetrics(std::string* output) {
