68076169e3b99b0907fcb77bcc57f66f90f57563
Martin Geisler <mgeisler@google.com>
2022-10-16 18:21:16 -0700

    pdl: Add support for ‘enum’ declarations
    This only allows us to parse the enums and to generate Rust code for
    the enum declaration itself. Support for enums inside packets will
    come next.
    
    Tests have been added to the canonical PDL files: they demonstrate
    that we can generate valid Rust code. A test was added to show that we
    need “#[repr(u64)]” for an enum with a maximum discriminant.
    
    Test: atest pdl_rust_generator_tests_{le,be} pdl_tests
    Change-Id: I4f8777c17bc0d7ddeddf214c66db03f070d3794e

--- tools/pdl/src/backends/rust.rs
+++ tools/pdl/src/backends/rust.rs
@@ -270,12 +270,34 @@ fn generate_packet_decl(
     code
 }
 
+fn generate_enum_decl(id: &str, tags: &[ast::Tag]) -> String {
+    let variants = tags.iter().map(|t| {
+        let variant = format_ident!("{}", t.id);
+        let value = syn::parse_str::<syn::LitInt>(&format!("{:#x}", t.value)).unwrap();
+        quote! {
+            #variant = #value
+        }
+    });
+
+    let name = format_ident!("{}", id);
+    let code = quote! {
+        #[derive(FromPrimitive, ToPrimitive, Debug, Hash, Eq, PartialEq, Clone, Copy)]
+        #[repr(u64)]
+        pub enum #name {
+            #(#variants),*
+        }
+    };
+
+    code.to_string()
+}
+
 fn generate_decl(ctx: &Context<'_>, file: &ast::File, decl: &ast::Decl) -> String {
     match decl {
         ast::Decl::Packet { id, fields, parent_id, .. } => {
             let fields = fields.iter().map(Field::from).collect::<Vec<_>>();
             generate_packet_decl(ctx, file, id, &fields, parent_id)
         }
+        ast::Decl::Enum { id, tags, .. } => generate_enum_decl(id, tags),
         _ => todo!("unsupported Decl::{:?}", decl),
     }
 }
@@ -449,6 +471,68 @@ mod tests {
         let actual_code = generate_decl(&ctx, &file, decl);
         assert_snapshot_eq(
             "tests/generated/packet_decl_complex_big_endian.rs",
+            &rustfmt(&actual_code),
+        );
+    }
+
+    #[test]
+    fn test_generate_enum_decl_little_endian() {
+        let file = parse_str(
+            r#"
+              little_endian_packets
+
+              enum Enum7 : 7 {
+                  A = 1,
+                  B = 2,
+              }
+            "#,
+        );
+        let ctx = Context::new(&file);
+        let decl = &file.declarations[0];
+        let actual_code = generate_decl(&ctx, &file, decl);
+        assert_snapshot_eq(
+            "tests/generated/enum_decl_simple_little_endian.rs",
+            &rustfmt(&actual_code),
+        );
+    }
+
+    #[test]
+    fn test_generate_enum_decl_big_endian() {
+        let file = parse_str(
+            r#"
+              big_endian_packets
+
+              enum Enum7 : 7 {
+                  A = 1,
+                  B = 2,
+              }
+            "#,
+        );
+        let ctx = Context::new(&file);
+        let decl = &file.declarations[0];
+        let actual_code = generate_decl(&ctx, &file, decl);
+        assert_snapshot_eq(
+            "tests/generated/enum_decl_simple_big_endian.rs",
+            &rustfmt(&actual_code),
+        );
+    }
+
+    #[test]
+    fn test_generate_enum_max_discriminant_little_endian() {
+        let file = parse_str(
+            r#"
+              little_endian_packets
+
+              enum MaxDiscriminantEnum : 64 {
+                  Max = 0xffffffffffffffff,
+              }
+            "#,
+        );
+        let ctx = Context::new(&file);
+        let decl = &file.declarations[0];
+        let actual_code = generate_decl(&ctx, &file, decl);
+        assert_snapshot_eq(
+            "tests/generated/enum_max_discriminant_little_endian.rs",
             &rustfmt(&actual_code),
         );
     }
