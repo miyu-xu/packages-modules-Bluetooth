edc722db008ca6dd06cc0e52d45ddffd37ca2324
Rahul Arya <aryarahul@google.com>
2022-10-18 17:33:54 +0000

    [Invisalign] Add HACK_* interface for ad-hoc callbacks out of the core
    We shouldn't have these, but they exist, and I don't want to touch what
    isn't obviously broken.
    
    Bug: 254063018
    Test: all existing tests
    Change-Id: I069bb911da4639adc83268e641718b1fbb57090c

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -677,7 +677,9 @@ static void btif_dm_cb_create_bond(const RawAddress bd_addr,
   }
 
   if (is_hid && (device_type & BT_DEVICE_TYPE_BLE) == 0) {
-    const bt_status_t status = btif_hh_connect(&bd_addr);
+    const bt_status_t status =
+        GetInterfaceToProfiles()->profileSpecific_HACK->btif_hh_connect(
+            &bd_addr);
     if (status != BT_STATUS_SUCCESS)
       bond_state_changed(status, bd_addr, BT_BOND_STATE_NONE);
   } else {
@@ -2221,7 +2223,8 @@ void btif_dm_remove_bond(const RawAddress bd_addr) {
   // there is a valid hid connection with this bd_addr. If yes VUP will be
   // issued.
 #if (BTA_HH_INCLUDED == TRUE)
-  if (btif_hh_virtual_unplug(&bd_addr) != BT_STATUS_SUCCESS)
+  if (GetInterfaceToProfiles()->profileSpecific_HACK->btif_hh_virtual_unplug(
+          &bd_addr) != BT_STATUS_SUCCESS)
 #endif
   {
     BTIF_TRACE_DEBUG("%s: Removing HH device", __func__);
