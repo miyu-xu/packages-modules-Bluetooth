830f7296ee117f4614ee843f33a9d43f4f4b675d
Rahul Arya <aryarahul@google.com>
2022-10-20 01:46:46 +0000

    [Invisalign] Add CodecInterface
    Not sure if this should belong in the core, or as an injected interface. We only
    inject this one codec since A2DP lives outside the core, but SCO is (currently)
    inside.
    
    Bug: 254063018
    Test: all existing tests
    
    Change-Id: I0470a82f639595e33e82257f4031fc629706c97d

--- system/btif/include/core_callbacks.h
+++ system/btif/include/core_callbacks.h
@@ -81,11 +81,29 @@ struct ConfigInterface {
   virtual ~ConfigInterface() = default;
 };
 
+// This interface lets us communicate with encoders used in profiles
+struct CodecInterface {
+  virtual void initialize();
+  virtual void cleanup();
+
+  virtual uint32_t encodePacket(int16_t* input, uint8_t* output);
+  virtual bool decodePacket(const uint8_t* i_buf, int16_t* o_buf,
+                            size_t out_len);
+
+  explicit CodecInterface() = default;
+  CodecInterface(const CodecInterface&) = delete;
+  CodecInterface& operator=(const CodecInterface&) = delete;
+  virtual ~CodecInterface() = default;
+};
+
 // This class defines the overall interface expected by bluetooth::core.
 struct CoreInterface {
   // generic interface
   EventCallbacks* events;
   ConfigInterface* config;
+
+  // codecs
+  CodecInterface* msbcCodec;
 
   virtual void onBluetoothEnabled() = 0;
   virtual bt_status_t toggleProfile(tBTA_SERVICE_ID service_id,
@@ -94,8 +112,10 @@ struct CoreInterface {
   virtual void onLinkDown(const RawAddress& bd_addr) = 0;
 
   CoreInterface(EventCallbacks* eventCallbacks,
-                ConfigInterface* configInterface)
-      : events{eventCallbacks}, config{configInterface} {};
+                ConfigInterface* configInterface, CodecInterface* msbcCodec)
+      : events{eventCallbacks},
+        config{configInterface},
+        msbcCodec{msbcCodec} {};
 
   CoreInterface(const CoreInterface&) = delete;
   CoreInterface& operator=(const CoreInterface&) = delete;
