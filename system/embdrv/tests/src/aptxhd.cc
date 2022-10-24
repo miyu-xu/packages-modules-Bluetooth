ed95cbd78bbece63af7759782d75e9dd7b5d1dec
William Escande <wescande@google.com>
2022-10-24 16:52:41 -0700

    Add regression test for aptx
    Test: atest --host libaptx_enc_tests
    Test: atest --host libaptxhd_enc_tests
    Bug: 226572369
    Change-Id: Ib717faa54ca87ecec46c33a3333f52f87d9f40c3

---
+++ system/embdrv/tests/src/aptxhd.cc
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <gtest/gtest.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+
+#include "aptXHDbtenc.h"
+
+#define BYTES_PER_CODEWORD 24
+
+class LibAptxHdEncTest : public ::testing::Test {
+ private:
+ protected:
+  void* aptxhdbtenc = nullptr;
+  void SetUp() override {
+    aptxhdbtenc = malloc(SizeofAptxhdbtenc());
+    ASSERT_NE(aptxhdbtenc, nullptr);
+    ASSERT_EQ(aptxhdbtenc_init(aptxhdbtenc, 0), 0);
+  }
+
+  void TearDown() override { free(aptxhdbtenc); }
+
+  void codeword_cmp(const uint8_t p[BYTES_PER_CODEWORD],
+                    const uint32_t codeword[2]) {
+    uint32_t pcmL[4];
+    uint32_t pcmR[4];
+    for (size_t i = 0; i < 4; i++) {
+      pcmL[i] = ((p[0] << 0) | (p[1] << 8) | (((int8_t)p[2]) << 16));
+      p += 3;
+      pcmR[i] = ((p[0] << 0) | (p[1] << 8) | (((int8_t)p[2]) << 16));
+      p += 3;
+    }
+    uint32_t encoded_sample[2];
+    aptxhdbtenc_encodestereo(aptxhdbtenc, &pcmL, &pcmR, (void*)encoded_sample);
+
+    ASSERT_EQ(encoded_sample[0], codeword[0]);
+    ASSERT_EQ(encoded_sample[1], codeword[1]);
+  }
+  // void open_non_blocking(const char* filename, int&fd) {
+  //   int tmp_fd = open(filename, O_RDONLY | O_CLOEXEC);
+  //   ASSERT_NE(tmp_fd, -1);
+  //   int flags = fcntl(tmp_fd, F_GETFL, 0);
+  //   ASSERT_NE(flags, -1);
+  //   ASSERT_NE(fcntl(tmp_fd, F_SETFL, flags | O_NONBLOCK), -1);
+  //   fd = tmp_fd;
+  // }
+};
+
+TEST_F(LibAptxHdEncTest, encoder_size) { ASSERT_EQ(SizeofAptxhdbtenc(), 5256); }
+
+TEST_F(LibAptxHdEncTest, encode_fake_data) {
+  const char input[] =
+      "012345678901234567890123456789012345678901234567890123456789012345678901"
+      "234567890123456789012345678901234567890123456789";
+  const uint32_t aptxhd_codeword[] = {7585535, 7585535, 32767,   32767,
+                                      557055,  557027,  7586105, 7586109,
+                                      9748656, 10764446};
+
+  ASSERT_EQ((sizeof(input) - 1) % BYTES_PER_CODEWORD, 0);
+  ASSERT_EQ((sizeof(input) - 1) / BYTES_PER_CODEWORD,
+            sizeof(aptxhd_codeword) / sizeof(uint32_t) / 2);
+
+  size_t idx = 0;
+
+  uint8_t pcm[BYTES_PER_CODEWORD];
+  while (idx * BYTES_PER_CODEWORD < sizeof(input) - 1) {
+    memcpy(pcm, input + idx * BYTES_PER_CODEWORD, BYTES_PER_CODEWORD);
+    codeword_cmp(pcm, aptxhd_codeword + idx * 2);
+    ++idx;
+  }
+}
+
+// TEST_F(LibAptxHdEncTest, encode_sample_wav_data) {
+//   int pcm_fd;
+//   open_non_blocking("raw_data/pcm_24bit.raw", pcm_fd);
+//   int aptxhd_fd;
+//   open_non_blocking("raw_data/sample.aptxhd", aptxhd_fd);
+
+//   for (;;) {
+//     uint8_t buf[BYTES_PER_CODEWORD];
+//     const size_t read_length = read(pcm_fd, buf, BYTES_PER_CODEWORD);
+//     // success no more data to read
+//     if (read_length == 0) {
+//       break;
+//     }
+//     ASSERT_EQ(read_length, BYTES_PER_CODEWORD);
+//     uint32_t channel1 = 0;
+//     uint32_t channel2 = 0;
+//     ASSERT_EQ(read(aptxhd_fd, &channel1, 3), 3);
+//     ASSERT_EQ(read(aptxhd_fd, &channel2, 3), 3);
+//     const uint32_t aptxhd_codeword[2] = {channel1, channel2};
+//     codeword_cmp(buf, aptxhd_codeword);
+//   }
+
+//   close(pcm_fd);
+//   close(aptxhd_fd);
+// }
