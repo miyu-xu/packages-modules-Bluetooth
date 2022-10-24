938c68fd79a4bf10bcbfab00134469b67e2363e7
Ying Hsu <yinghsu@google.com>
2022-10-24 10:03:17 +0000

    floss: Changing advertising set state to update parameters
    Bluetooth controllers might not allow advertising set parameters
    change while it's being advertised. This patch disable the
    an active advertising set before changing its parameters, and then
    re-enable it.
    
    Bug: 233128828
    Tag: #floss
    Test: build.py --target test
    Test: manaul test on volteer with btclient and btmon
    Change-Id: I331714c56a97c6e5f07756d3aeb055cd8ec5b52b

--- system/gd/rust/linux/stack/src/bluetooth_gatt.rs
+++ system/gd/rust/linux/stack/src/bluetooth_gatt.rs
@@ -1046,7 +1046,7 @@ impl IBluetoothGatt for BluetoothGatt {
         let adv_timeout = clamp(duration, 0, 0xffff) as u16;
         let adv_events = clamp(max_ext_adv_events, 0, 0xff) as u8;
 
-        let s = AdvertisingSetInfo::new(callback_id);
+        let s = AdvertisingSetInfo::new(callback_id, adv_timeout, adv_events);
         let reg_id = s.reg_id();
         self.advertisers.add(s);
 
@@ -1130,7 +1130,24 @@ impl IBluetoothGatt for BluetoothGatt {
         let params = parameters.into();
 
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
+            let was_enabled = s.is_enabled();
+            if was_enabled {
+                self.gatt.as_mut().unwrap().advertiser.enable(
+                    s.adv_id(),
+                    false,
+                    s.adv_timeout(),
+                    s.adv_events(),
+                );
+            }
             self.gatt.as_mut().unwrap().advertiser.set_parameters(s.adv_id(), params);
+            if was_enabled {
+                self.gatt.as_mut().unwrap().advertiser.enable(
+                    s.adv_id(),
+                    true,
+                    s.adv_timeout(),
+                    s.adv_events(),
+                );
+            }
         }
     }
 
@@ -2412,7 +2429,8 @@ impl BtifGattAdvCallbacks for BluetoothGatt {
         );
 
         if let Some(s) = self.advertisers.get_mut_by_reg_id(reg_id) {
-            s.advertiser_id = Some(advertiser_id.into());
+            s.set_adv_id(Some(advertiser_id.into()));
+            s.set_enabled(status == GattStatus::Success);
         } else {
             return;
         }
@@ -2441,6 +2459,7 @@ impl BtifGattAdvCallbacks for BluetoothGatt {
         if None == self.advertisers.get_by_advertiser_id(advertiser_id) {
             return;
         }
+        self.advertisers.get_mut_by_advertiser_id(advertiser_id).unwrap().set_enabled(enabled);
         let s = self.advertisers.get_by_advertiser_id(advertiser_id).unwrap().clone();
 
         if let Some(cb) = self.advertisers.get_callback(&s) {
