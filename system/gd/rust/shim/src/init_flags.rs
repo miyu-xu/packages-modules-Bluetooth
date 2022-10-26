4f8cf4519c0d6f0b30b1cf07296d3aa1a533bf7a
Sunny Kapdi <quic_sunnyk@quicinc.com>
2022-10-25 18:21:22 -0700

    BT5.3 Enhanced Connection update
    Snapshot as of e71c3481c888c5dcb56c15d6b8e85f658152ef82
    
    Bug: 248126637
    Test: Not yet implemented
    Change-Id: I88151930690bfdb2ed7ab0cbf27fd9fb661e0db3

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -21,6 +21,7 @@ mod ffi {
         fn logging_debug_enabled_for_all_is_enabled() -> bool;
         fn pass_phy_update_callback_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
+        fn sydney_enhanced_connection_update_is_enabled() -> bool;
     }
 }
 
