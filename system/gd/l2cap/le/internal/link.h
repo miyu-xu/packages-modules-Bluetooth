2dc0ad437a300199f8b67f4c6cdea255e629ed06
Hui Peng <phui@google.com>
2022-10-26 22:23:27 +0000

    Replace Logging ILink related instances in system/gd/l2cap
    Test: refactoring CL. Existing unit tests still pass
    Bug: 174487588
    Tag: #security
    Change-Id: I6dcb40f60f109060c6391b06ac1df61cdd0fc6b3

--- system/gd/l2cap/le/internal/link.h
+++ system/gd/l2cap/le/internal/link.h
@@ -20,6 +20,7 @@
 #include <chrono>
 #include <memory>
 
+#include "common/interfaces/ILoggable.h"
 #include "hci/acl_manager/le_acl_connection.h"
 #include "l2cap/internal/data_pipeline_manager.h"
 #include "l2cap/internal/dynamic_channel_allocator.h"
@@ -42,7 +43,7 @@ namespace internal {
 
 class LinkManager;
 
-class Link : public l2cap::internal::ILink, public hci::acl_manager::LeConnectionManagementCallbacks {
+class Link : public l2cap::internal::ILink, public hci::acl_manager::LeConnectionManagementCallbacks, public ILoggable {
  public:
   Link(os::Handler* l2cap_handler, std::unique_ptr<hci::acl_manager::LeAclConnection> acl_connection,
        l2cap::internal::ParameterProvider* parameter_provider,
@@ -141,6 +142,10 @@ class Link : public l2cap::internal::ILink, public hci::acl_manager::LeConnectio
 
   virtual std::string ToString() {
     return GetDevice().ToString();
+  }
+
+  std::string ToStringForLogging() const override {
+    return GetDevice().ToStringForLogging();
   }
 
   virtual uint16_t GetMps() const;
