1f9bc5ce10908974f6e90a8473e41de245d164e6
Hui Peng <phui@google.com>
2022-10-26 22:23:27 +0000

    Replace Logging ILink related instances in system/gd/l2cap
    Test: refactoring CL. Existing unit tests still pass
    Bug: 174487588
    Tag: #security
    Change-Id: I6dcb40f60f109060c6391b06ac1df61cdd0fc6b3

--- system/gd/l2cap/classic/internal/fixed_channel_impl.h
+++ system/gd/l2cap/classic/internal/fixed_channel_impl.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include "common/bidi_queue.h"
+#include "common/interfaces/ILoggable.h"
 #include "l2cap/cid.h"
 #include "l2cap/classic/fixed_channel.h"
 #include "l2cap/internal/channel_impl.h"
@@ -57,13 +58,13 @@ class FixedChannelImpl : public l2cap::internal::ChannelImpl,
 
   virtual void OnClosed(hci::ErrorCode status);
 
-  std::string ToStringForLogging() override{
+  std::string ToStringForLogging() const override {
     std::ostringstream ss;
-    ss << "Device " << device_ << " Cid 0x" << std::hex << cid_;
+    ss << "Device " << device_.ToStringForLogging() << " Cid 0x" << std::hex << cid_;
     return ss.str();
   }
 
-  std::string ToRedactedStringForLogging() override{
+  std::string ToRedactedStringForLogging() const override {
     std::ostringstream ss;
     ss << "Device " << device_.ToRedactedStringForLogging() << " Cid 0x" << std::hex << cid_;
     return ss.str();
