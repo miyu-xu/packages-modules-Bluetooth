f64544cea38b7a7fd6e20769f9bd8b002f0143d5
Hui Peng <phui@google.com>
2022-10-26 22:27:56 +0000

    Add support for address hiding [1/3]
    - Add an interface IRedactedLoggable
    - Add ToStringForLogging and ToRedactedStringForLogging to address classes
      - RawAddress and tBLE_BD_ADDR do not implement ILoggable
        Some existing code relies on the size of these structs
      - hci::Address and hci::AddressWithType implement ILoggable
      - unit tests
    
    Test: atest bluetooth_test_gd_unit net_test_types
    Bug: 174487588
    Tag: #security
    Change-Id: I455abdd9c52f0e76ab1a352ccdfbae76edf5add6

--- system/types/raw_address.h
+++ system/types/raw_address.h
@@ -46,7 +46,22 @@ class RawAddress final {
 
   bool IsEmpty() const { return *this == kEmpty; }
 
+  // TODO: remove it and
+  // replace its usage with ToColonSepHexString
   std::string ToString() const;
+
+  // Return a string representation of in the form of
+  // hexadecimal string separated by colon (:), e.g.,
+  // "12:34:56:ab:cd:ef"
+  std::string ToColonSepHexString() const;
+  // same as ToColonSepHexString
+  std::string ToStringForLogging() const;
+
+  // Similar with ToColonHexString, ToStringForLogging returns a string
+  // colon separated hexadecimal reprentation of the address but, with the
+  // leftmost 4 bytes masked with "xx", e.g., "xx:xx:xx:xx:ab:cd".
+  // hexadecimal numbers are represented in lowercase.
+  std::string ToRedactedStringForLogging() const;
 
   // Converts |string| to RawAddress and places it in |to|. If |from| does
   // not represent a Bluetooth address, |to| is not modified and this function
