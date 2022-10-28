0597dc5e60772e2b82d0bcb51b219a6151f28799
Michał Narajowski <narajn@google.com>
2022-10-28 14:11:12 +0000

    snoop_logger: Add support for logging to a socket
    - Implements writing BTSnoop logs to socket.
    - A default socket is registered on start if logs are enabled.
    - snoop_logger allows to register a different socket interface
      which overrides the default one.
    - This patch allows to see btsnoop logs in Wireshark or btmon
      in real time.
    
    Test: atest bluetooth_test_gd
    Tag: #feature
    Bug: 247859568
    Change-Id: Ia1d3fd8698ef4d2a3bdf92d0c78d20044cfdf96e

---
+++ system/gd/hal/snoop_logger_socket_thread.cc
@@ -0,0 +1,110 @@
+/******************************************************************************
+ *
+ *  Copyright (C) 2022 Google, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#include "hal/snoop_logger_socket_thread.h"
+
+#include <arpa/inet.h>
+#include <base/logging.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <mutex>
+
+#include "common/init_flags.h"
+#include "hal/snoop_logger_common.h"
+#include "os/handler.h"
+#include "os/log.h"
+#include "os/thread.h"
+#include "os/utils.h"
+
+namespace bluetooth {
+namespace hal {
+
+SnoopLoggerSocketThread::SnoopLoggerSocketThread(std::unique_ptr<SnoopLoggerSocket>&& socket) {
+  socket_ = std::move(socket);
+}
+
+SnoopLoggerSocketThread::~SnoopLoggerSocketThread() {
+  Stop();
+}
+
+void SnoopLoggerSocketThread::Start() {
+  LOG_DEBUG("");
+  listen_thread_ = std::make_unique<std::thread>(&SnoopLoggerSocketThread::Run, this);
+  WaitThreadIsRunning();
+}
+
+void SnoopLoggerSocketThread::Stop() {
+  LOG_DEBUG("");
+  if (listen_thread_running_) {
+    socket_->NotifySocketListener();
+    listen_thread_->join();
+    listen_thread_.reset();
+
+    listen_thread_running_ = false;
+  }
+}
+
+void SnoopLoggerSocketThread::Write(const void* data, size_t length) {
+  socket_->Write(data, length);
+}
+
+bool SnoopLoggerSocketThread::ThreadIsRunning() const {
+  return listen_thread_running_;
+}
+
+bool SnoopLoggerSocketThread::WaitThreadIsRunning() {
+  std::unique_lock<std::mutex> lk(listen_thread_running_mutex_);
+  listen_thread_running_cv_.wait(lk, [this] { return listen_thread_running_; });
+  return listen_thread_running_;
+}
+
+SnoopLoggerSocket* SnoopLoggerSocketThread::GetSocket() {
+  return socket_.get();
+}
+
+void SnoopLoggerSocketThread::Run() {
+  LOG_DEBUG("");
+
+  socket_->InitializeCommunications();
+
+  {
+    std::lock_guard<std::mutex> lk(listen_thread_running_mutex_);
+    listen_thread_running_ = true;
+    listen_thread_running_cv_.notify_one();
+  }
+
+  while (socket_->ProcessIncomingRequest()) {
+  }
+
+  socket_->Cleanup();
+  listen_thread_running_ = false;
+}
+
+}  // namespace hal
+}  // namespace bluetooth
