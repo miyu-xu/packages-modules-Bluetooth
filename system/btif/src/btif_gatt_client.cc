47fb4ceea38b45cc591b3de7b562f30bca1aab74
Jakub Pawlowski <jpawlowski@google.com>
2022-11-02 16:13:53 +0100

    Fix GATT write above MAX_ATTR_LEN
    BT Spec says maximum characteristic value length is 512 bytes.
    This used to be enforced in older android versions, but was broken
    during some refactors. vector.resize(len) doesn't trim the array
    content, len must be adjusted before vector creation.
    
    Test: Try to perform GATT write longer than 512 bytes
    Bug: 248983780
    Change-Id: If8180f64dbe6872074456f680a562241a2692133

--- system/btif/src/btif_gatt_client.cc
+++ system/btif/src/btif_gatt_client.cc
@@ -34,6 +34,7 @@
 #include <hardware/bluetooth.h>
 #include <hardware/bt_gatt.h>
 
+#include <algorithm>
 #include <string>
 
 #include "bta_api.h"
@@ -527,9 +528,9 @@ static bt_status_t btif_gattc_write_char(int conn_id, uint16_t handle,
                                          const uint8_t* val, size_t len) {
   CHECK_BTGATT_INIT();
 
-  std::vector<uint8_t> value(val, val + len);
+  len = std::min(len, (size_t)BTGATT_MAX_ATTR_LEN);
 
-  if (value.size() > BTGATT_MAX_ATTR_LEN) value.resize(BTGATT_MAX_ATTR_LEN);
+  std::vector<uint8_t> value(val, val + len);
 
   return do_in_jni_thread(Bind(&BTA_GATTC_WriteCharValue, conn_id, handle,
                                write_type, std::move(value), auth_req,
@@ -557,9 +558,9 @@ static bt_status_t btif_gattc_write_char_descr(int conn_id, uint16_t handle,
                                                size_t len) {
   CHECK_BTGATT_INIT();
 
-  std::vector<uint8_t> value(val, val + len);
+  len = std::min(len, (size_t)BTGATT_MAX_ATTR_LEN);
 
-  if (value.size() > BTGATT_MAX_ATTR_LEN) value.resize(BTGATT_MAX_ATTR_LEN);
+  std::vector<uint8_t> value(val, val + len);
 
   return do_in_jni_thread(Bind(&BTA_GATTC_WriteCharDescr, conn_id, handle,
                                std::move(value), auth_req, write_descr_cb,
