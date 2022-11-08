6395f1d8ed6af101f52d0b6bc7ed12e3a28d88ec
howardchung <howardchung@google.com>
2022-11-08 06:46:57 +0000

    Floss: Implement GATT client commands
    This implements read/write characteritic and also commands to set
    preferences.
    
    Bug: 242266526
    Tag: #floss
    Test: run btclient and check the commands issued correctly by
    dbus-monitor
    
    Change-Id: I46c4974e9120f38aa929d54c6e92b3e8d73fa049

---
+++ system/gd/rust/linux/client/src/bt_gatt.rs
@@ -0,0 +1,48 @@
+use bitflags::bitflags;
+use bt_topshim::btif::BtTransport;
+use bt_topshim::profiles::gatt::LePhy;
+use btstack::bluetooth_gatt::GattWriteType;
+
+bitflags! {
+    pub(crate) struct AuthReq: i32 {
+        // reference to system/stack/include/gatt_api.h
+        const MITM   = 0x01;
+        const SIGNED = 0x10;
+    }
+}
+
+/// User preferenece of GATT operations
+pub(crate) struct GattClientContext {
+    /// If set, the registered GATT client id. None otherwise.
+    pub(crate) client_id: Option<i32>,
+    /// Type of GATT write.
+    pub(crate) write_type: GattWriteType,
+    /// Type of authentication requirement
+    pub(crate) auth_req: AuthReq,
+    /// Is connection going to be directed?
+    pub(crate) is_connect_direct: bool,
+    /// Transport of connection
+    pub(crate) connect_transport: BtTransport,
+    /// Is connection going to be opportunistic?
+    pub(crate) connect_opportunistic: bool,
+    /// Type of connect phy
+    pub(crate) connect_phy: LePhy,
+}
+
+impl GattClientContext {
+    pub(crate) fn new() -> Self {
+        GattClientContext {
+            client_id: None,
+            write_type: Default::default(),
+            auth_req: AuthReq::empty(),
+            is_connect_direct: false,
+            connect_transport: BtTransport::Le,
+            connect_opportunistic: false,
+            connect_phy: LePhy::Phy1m,
+        }
+    }
+
+    pub(crate) fn get_auth_req_bits(&self) -> i32 {
+        self.auth_req.bits()
+    }
+}
