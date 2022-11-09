1f3e0aa1a25cb6a8d413641c363f0bb6cefa033f
Mingbo Zhang <quic_mingboz@quicinc.com>
2022-11-09 23:01:30 +0800

    device_iot_config: store iot device info
    Store remote basic info (e.g. name, dev class, dev type, etc.)
    and profile info (a2dp/hfp/avrcp/gap) to local conf file.
    
    Snapshot as of:
    43ab2ffdf6bdc48d0fb4a4d715da8863599f24e0
    e080af8694d274d5db20204c48b9f6b714b17498
    48b1654da46649a01ee9ab422f20aa60b8b31efd
    964e674a41dbfdea612c66c684abc4d0e27500c1
    
    Sponsor: narajn@
    Bug: 261319328
    Tag: #feature
    Test: atest BluetoothInstrumentationTests
    Test: atest bluetooth_test_gd_unit
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: If4c2632d3874fbec1e3477f40cd11b4b1db7a118

--- system/stack/btm/btm_sco.cc
+++ system/stack/btm/btm_sco.cc
@@ -31,6 +31,7 @@
 #include <string>
 
 #include "device/include/controller.h"
+#include "device/include/device_iot_config.h"
 #include "embdrv/sbc/decoder/include/oi_codec_sbc.h"
 #include "embdrv/sbc/decoder/include/oi_status.h"
 #include "osi/include/allocator.h"
@@ -807,6 +808,10 @@ void btm_sco_conn_req(const RawAddress& bda, const DEV_CLASS& dev_class,
   tSCO_CONN* p = &p_sco->sco_db[0];
   tBTM_ESCO_CONN_REQ_EVT_DATA evt_data = {};
 
+#if (BT_IOT_LOGGING_ENABLED == TRUE)
+  device_iot_config_addr_int_add_one(bda, IOT_CONF_KEY_HFP_SCO_CONN_COUNT);
+#endif
+
   for (uint16_t sco_index = 0; sco_index < BTM_MAX_SCO_LINKS;
        sco_index++, p++) {
     /*
@@ -987,8 +992,14 @@ void btm_sco_connection_failed(tHCI_STATUS hci_status, const RawAddress& bda,
         if (p->state == SCO_ST_CONNECTING) {
           p->state = SCO_ST_UNUSED;
           (*p->p_disc_cb)(xx);
-        } else
+        } else {
           p->state = SCO_ST_LISTENING;
+#if (BT_IOT_LOGGING_ENABLED == TRUE)
+          if (bda != RawAddress::kEmpty)
+            device_iot_config_addr_int_add_one(
+                bda, IOT_CONF_KEY_HFP_SCO_CONN_FAIL_COUNT);
+#endif
+        }
         BTM_LogHistory(
             kBtmLogTag, bda, "Connection failed",
             base::StringPrintf(
