4648ec3a1bc8acf8e2dea3c3cfca4a062280ba52
Charlie Boutier <charliebout@google.com>
2022-11-09 19:07:17 +0000

    [Pandora] - Import new Host.proto
    Test: Not tested yet - DONT MERGE
    Bug: None
    
    Change-Id: Ie8ec73ac8bee33becbc2052a81e0ee4ac69d9e21

--- android/pandora/mmi2grpc/mmi2grpc/l2cap.py
+++ android/pandora/mmi2grpc/mmi2grpc/l2cap.py
@@ -6,7 +6,7 @@ from mmi2grpc._helpers import match_description
 from mmi2grpc._proxy import ProfileProxy
 
 from pandora_experimental.host_grpc import Host
-from pandora_experimental.host_pb2 import Connection, ConnectabilityMode, AddressType
+from pandora_experimental.host_pb2 import Connection, OwnAddressType
 from pandora_experimental.l2cap_grpc import L2CAP
 
 from typing import Optional
@@ -91,8 +91,8 @@ class L2CAPProxy(ProfileProxy):
         Place the IUT into LE connectable mode.
         """
         self.host.StartAdvertising(
-            connectability_mode=ConnectabilityMode.CONNECTABILITY_CONNECTABLE,
-            own_address_type=AddressType.PUBLIC,
+            connectable=True,
+            own_address_type=OwnAddressType.PUBLIC,
         )
         # not strictly necessary, but can save time on waiting connection
         tests_to_open_bluetooth_server_socket = [
@@ -346,7 +346,7 @@ class L2CAPProxy(ProfileProxy):
         """
         Initiate or create LE ACL connection to the PTS.
         """
-        self.connection = self.host.ConnectLE(address=pts_addr).connection
+        self.connection = self.host.ConnectLE(public=pts_addr).connection
         return "OK"
 
     @assert_description
