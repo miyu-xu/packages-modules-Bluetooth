44cf86b1cd5b287a837b51aab01af0c50116bc94
Henri Chataing <henrichataing@google.com>
2022-11-09 01:43:54 +0000

    RootCanal: Fix clang-tidy warnings
    Fix warnings for readability-inconsistent-declaration-parameter-name
    
    Test: ALLOW_LOCAL_TIDY_TRUE=1 m root-canal
    Change-Id: I7734b7013090db6cd202f863ee22ab20869eac33

--- tools/rootcanal/model/hci/h4_parser.cc
+++ tools/rootcanal/model/hci/h4_parser.cc
@@ -50,10 +50,10 @@ size_t H4Parser::HciGetPacketLengthForType(PacketType type,
   size_t offset = packet_length_offset[static_cast<size_t>(type)];
   size_t size = preamble[offset];
   if (type == PacketType::ACL) {
-    size |= ((size_t)preamble[offset + 1]) << 8u;
+    size |= ((size_t)preamble[offset + 1]) << 8;
   }
   if (type == PacketType::ISO) {
-    size |= ((size_t)preamble[offset + 1] & 0x0fu) << 8u;
+    size |= ((size_t)preamble[offset + 1] & 0x0fU) << 8;
   }
   return size;
 }
@@ -107,7 +107,8 @@ bool H4Parser::Consume(uint8_t* buffer, int32_t bytes_read) {
   if (bytes_read <= 0) {
     LOG_INFO("remote disconnected, or unhandled error?");
     return false;
-  } else if ((uint32_t)bytes_read > BytesRequested()) {
+  }
+  if ((uint32_t)bytes_read > BytesRequested()) {
     LOG_ALWAYS_FATAL("More bytes read (%u) than expected (%u)!",
                      static_cast<int>(bytes_read),
                      static_cast<int>(bytes_to_read));
