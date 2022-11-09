6876cc11fdbf4e9c69e452483ab6398a33bbecc3
Łukasz Rymanowski <rlukasz@google.com>
2022-11-09 15:58:00 +0000

    gatt: Prepare GATTC_OPEN API for more than direct and background
    Bug: 248340126
    Test: atest BluetoothInstrumentationTests
    Tag: #feature
    
    Change-Id: Iea86534687dc5f0febc4a9c493e011750ebf1823

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -1072,7 +1072,7 @@ class LeAudioClientImpl : public LeAudioClient {
           le_audio::ConnectionStatus::SUCCESS);
     }
 
-    BTA_GATTC_Open(gatt_if_, address, true, false);
+    BTA_GATTC_Open(gatt_if_, address, BTM_BLE_DIRECT_CONNECTION, false);
   }
 
   std::vector<RawAddress> GetGroupDevices(const int group_id) override {
@@ -1168,7 +1168,7 @@ class LeAudioClientImpl : public LeAudioClient {
       leAudioDevice->SetConnectionState(
           DeviceConnectState::CONNECTING_AUTOCONNECT);
       leAudioDevice->autoconnect_flag_ = true;
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTM_BLE_BKG_CONNECT_ALLOW_LIST, false);
     }
   }
 
@@ -1215,7 +1215,8 @@ class LeAudioClientImpl : public LeAudioClient {
 
     leAudioDevice->SetConnectionState(
         DeviceConnectState::CONNECTING_AUTOCONNECT);
-    BTA_GATTC_Open(gatt_if_, leAudioDevice->address_, false, false);
+    BTA_GATTC_Open(gatt_if_, leAudioDevice->address_,
+                   BTM_BLE_BKG_CONNECT_ALLOW_LIST, false);
   }
 
   void Disconnect(const RawAddress& address) override {
@@ -1785,7 +1786,7 @@ class LeAudioClientImpl : public LeAudioClient {
     if (reason != GATT_CONN_TERMINATE_LOCAL_HOST) {
       leAudioDevice->SetConnectionState(
           DeviceConnectState::CONNECTING_AUTOCONNECT);
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTM_BLE_BKG_CONNECT_ALLOW_LIST, false);
     } else {
       leAudioDevice->SetConnectionState(DeviceConnectState::DISCONNECTED);
     }
