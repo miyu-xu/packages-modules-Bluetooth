dfe0580bd9a026206383b1c77361e320b1e957a8
Henri Chataing <henrichataing@google.com>
2022-11-10 22:44:03 +0000

    RootCanal: Implement rootcanal_ll_test
    This change adds a python binding for the DualModeController
    class to enable isolated LL+HCI tests on one controller
    instance.
    
    Implement test LL/DDI/SCN/BV-13-C test from Bluetooth controller
    certification test suite.
    
    Test: atest --host rootcanal_ll_test
    Change-Id: I5b0b9fd9608769346deb9c20dbf1d7aa809e8a0a

---
+++ tools/rootcanal/test/main.py
@@ -0,0 +1,20 @@
+from importlib import resources
+from pathlib import Path
+import tempfile
+
+# Python is not able to load the module lib_rootcanal_python3.so
+# when the test target is configured with embedded_launcher: true.
+# This code loads the file to a temporary directory and adds the
+# path to the sys lookup.
+with tempfile.TemporaryDirectory() as cache:
+    with (Path('lib_rootcanal_python3.so').open('rb') as fin,
+          Path(cache, 'lib_rootcanal_python3.so').open('wb') as fout):
+        fout.write(fin.read())
+    sys.path.append(cache)
+    import lib_rootcanal_python3
+
+import unittest
+import test.LL.DDI.SCN.BV_13_C
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(unittest.defaultTestLoader.loadTestsFromModule(test.LL.DDI.SCN.BV_13_C))
