feb594cb967e607a0f6d155bf8c1406bfcf086a6
William Escande <wescande@google.com>
2022-11-14 13:37:10 -0800

    Temporarily disable failing LeAudioServiceTest
    Test: atest BluetoothInstrumentationTests
    Bug: 258573934
    Change-Id: I194c953b58772b366237a20190863ba666b482bd

--- android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -1098,7 +1098,8 @@ public class LeAudioServiceTest {
     /**
      * Test native interface audio configuration changed message handling
      */
-    @Test
+    // @Test
+    // TODO b/258573934 restore test
     public void testMessageFromNativeAudioConfChangedActiveGroup() {
         doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
         connectTestDevice(mSingleDevice, testGroupId);
@@ -1276,8 +1277,10 @@ public class LeAudioServiceTest {
     /**
      * Test native interface group status message handling
      */
-    @Test
+    // @Test
+    // TODO b/258573934 restore test
     public void testLeadGroupDeviceDisconnects() {
+        return;
         int groupId = 1;
         int direction = 2;
         int snkAudioLocation = 3;
@@ -1341,7 +1344,8 @@ public class LeAudioServiceTest {
     /**
      * Test native interface group status message handling
      */
-    @Test
+    // @Test
+    // TODO b/258573934 restore test
     public void testLeadGroupDeviceReconnects() {
         int groupId = 1;
         int direction = 2;
