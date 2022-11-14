bca6640d9dde6cc8b1a1f108efb05f57c2cc3756
William Escande <wescande@google.com>
2022-11-14 13:47:10 -0800

    BT5.3: Periodic Advtising adi -- prerequisite
    Pre-convergence CL to add support for the parameter accross legacy and
    gd.
    Replace the `enable` type to be a bool to avoid confusion and add the
    new field `include_adi` for all methods
    
    Bug: 248126637
    Tag: #feature
    Test: Manual end-to-end testing (need 5.3 support in controller)
    Change-Id: Ie06f64126842258f8cb2f508814d1923d415ded8

--- android/app/jni/com_android_bluetooth_gatt.cpp
+++ android/app/jni/com_android_bluetooth_gatt.cpp
@@ -2150,6 +2150,7 @@ static PeriodicAdvertisingParameters parsePeriodicParams(JNIEnv* env,
   uint16_t interval = env->CallIntMethod(i, methodId);
 
   p.enable = true;
+  p.include_adi = false;
   p.min_interval = interval;
   p.max_interval = interval + 16; /* 20ms difference betwen min and max */
   uint16_t props = 0;
@@ -2347,8 +2348,9 @@ static void setPeriodicAdvertisingEnableNative(JNIEnv* env, jobject object,
                                                jboolean enable) {
   if (!sGattIf) return;
 
+  bool include_adi = false;
   sGattIf->advertiser->SetPeriodicAdvertisingEnable(
-      advertiser_id, enable,
+      advertiser_id, enable, include_adi,
       base::Bind(&enablePeriodicSetCb, advertiser_id, enable));
 }
 
