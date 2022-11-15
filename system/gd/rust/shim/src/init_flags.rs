2cc92b7a20ecda89eb77f306e66028e8c04ece38
William Escande <wescande@google.com>
2022-11-14 20:39:31 -0800

    BT5.3: Periodic Advertising adi -- post cleanup
    Follow-up cl to add facade method
    
    Bug: 248126637
    Tag: #feature
    Test: Manual end-to-end testing (need 5.3 support in controller)
    Change-Id: I2813b992e14c3601fa8aa782b8252d31b855f492

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -23,6 +23,7 @@ mod ffi {
         fn periodic_advertising_adi_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
+        fn set_min_encryption_is_enabled() -> bool;
     }
 }
 
