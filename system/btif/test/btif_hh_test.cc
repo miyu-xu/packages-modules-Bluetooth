bd4494c9efe4a33d0e0d9171dae99792c7d8d322
yanggang.yang <yanggang.yang@mediatek.com>
2022-11-17 15:39:12 +0800

    Fix BT Stack crash when power off
    [Description]
    Fix BT stack crash when power off with HID device connected,
    
    [Root Cause]
    pthread_join will be called when thread_id value is -1, that will
    cause bt crash
    
    [Solution]
    add mutex to avoid
    
    Bug: 266193648
    Test: net_test_btif_hh unit test pass
    
    Change-Id: I7d4d4f120e3bbb429504d4535daced45302a2ebf

--- system/btif/test/btif_hh_test.cc
+++ system/btif/test/btif_hh_test.cc
@@ -22,6 +22,7 @@
 #include <array>
 #include <future>
 #include <vector>
+#include <fcntl.h>
 
 #include "bta/hh/bta_hh_int.h"
 #include "bta/include/bta_ag_api.h"
@@ -337,4 +338,21 @@ TEST_F(BtifHHVirtualUnplugTest, test_btif_hh_virtual_unplug_device_not_open) {
   ASSERT_STREQ(kDeviceAddressConnecting.ToString().c_str(),
                res.raw_address.ToString().c_str());
   ASSERT_EQ(BTHH_CONN_STATE_DISCONNECTED, res.state);
+}
+
+TEST_F(BtifHhWithDevice, test_btif_hh_remove_device) {
+  btif_hh_remove_device(kDeviceAddress);
+
+  ASSERT_EQ(0, btif_hh_cb.devices[0].hh_keep_polling);
+  ASSERT_EQ((pthread_t)-1, btif_hh_cb.devices[0].hh_poll_thread_id);
+}
+
+TEST_F(BtifHhWithDevice, test_cleanup) {
+  btif_hh_cb.status = BTIF_HH_DEV_CONNECTED;
+  btif_hh_cb.devices[0].fd = open("/dev/null", O_RDWR | O_CLOEXEC);
+  btif_hh_get_interface()->cleanup();
+
+  ASSERT_EQ(0, btif_hh_cb.devices[0].hh_keep_polling);
+  ASSERT_EQ((pthread_t)-1, btif_hh_cb.devices[0].hh_poll_thread_id);
+  close(btif_hh_cb.devices[0].fd);
 }
