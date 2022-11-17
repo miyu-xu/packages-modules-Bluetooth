916108da1677bef6ecd7891915326eae4aaf05dd
Josh Wu <joshwu@google.com>
2022-11-17 23:53:11 +0800

    gatts load bonded for service change
    Prepare list of bonded devices that will need service change updates
    after BT restarted.
    
    Cherry-picked from wear os
    
    Bug: 263323082
    Bug: 162948473
    Bug: 251157966
    Test: atest net_test_stack
    Change-Id: I7f3541a21e2c4a629498fd91d3a6438135a760e0
    (cherry picked from commit 5f3719499b6ca547086d5a2ad8f6d3fca748deb3)

--- system/test/mock/mock_stack_btm_dev.cc
+++ system/test/mock/mock_stack_btm_dev.cc
@@ -118,3 +118,8 @@ void BTM_SecDump(const std::string& label) {
 void BTM_SecDumpDev(const RawAddress& bd_addr) {
   mock_function_count_map[__func__]++;
 }
+std::vector<tBTM_SEC_DEV_REC*> btm_get_sec_dev_rec(
+    std::function<bool(tBTM_SEC_DEV_REC*)> filter) {
+  mock_function_count_map[__func__]++;
+  return {};
+}
