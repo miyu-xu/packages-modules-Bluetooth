5cac9213440d2d63949b685b07dffc29614a52f1
Huirong Liao <huirong.liao@mediatek.com>
2022-11-18 11:16:53 +0800

    Fix IOT device issue with master role
    [Root Cause]
    1. When connected with Mate 10,if DUT is mster,
    Mate 10 and DUT send SMP_Pairing_Request simultaneously sometimes
    2. DUT pair PCM, PIN cannot be entered
    
    [Solution]
    Add role switch as slave for IOT device
    
    Bug: 265917567
    Test: pair successfully
    
    Change-Id: Icca1122cf56c373d653633b7c32a685d31e39ef5

--- system/stack/l2cap/l2c_link.cc
+++ system/stack/l2cap/l2c_link.cc
@@ -41,6 +41,7 @@
 #include "stack/l2cap/l2c_int.h"
 #include "types/bt_transport.h"
 #include "types/raw_address.h"
+#include "device/include/interop.h"
 
 extern tBTM_CB btm_cb;
 
@@ -105,6 +106,12 @@ void l2c_link_hci_conn_req(const RawAddress& bd_addr) {
         p_lcb->SetLinkRoleAsCentral();
     }
 
+    LOG_DEBUG("%s: p_lcb->LinkRole()=%d", __func__, p_lcb->LinkRole());
+    if ((HCI_ROLE_CENTRAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_PERIPHERAL, &bd_addr)) {
+      p_lcb->SetLinkRoleAsPeripheral();
+    }
+
     /* Tell the other side we accept the connection */
     acl_accept_connection_request(bd_addr, p_lcb->LinkRole());
 
@@ -124,6 +131,12 @@ void l2c_link_hci_conn_req(const RawAddress& bd_addr) {
       p_lcb->SetLinkRoleAsPeripheral();
     else
       p_lcb->SetLinkRoleAsCentral();
+
+    LOG_DEBUG("%s: p_lcb->LinkRole()=%d", __func__, p_lcb->LinkRole());
+    if ((HCI_ROLE_CENTRAL == p_lcb->LinkRole()) &&
+      interop_match_addr(INTEROP_L2CAP_ACCEPT_CONN_AS_PERIPHERAL, &bd_addr)) {
+      p_lcb->SetLinkRoleAsPeripheral();
+    }
 
     acl_accept_connection_request(bd_addr, p_lcb->LinkRole());
 
