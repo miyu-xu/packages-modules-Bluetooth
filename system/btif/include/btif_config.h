06014ae35a9a4eee571a225048e7a525990b01ca
Huirong Liao <huirong.liao@mediatek.com>
2022-11-18 13:53:07 +0800

    Fix connect device with clock_offset=0 fail
    [Root Cause]
    When there are too much EIR or ble adv.
    the device info in inquiry db will be cleared.
    And when upper layer connect the device, there is no clock offset info in inquiry db and it may connect fail.
    
    [Solution]
    Record clock offset in config, then get it from config.
    
    Bug: 266249327
    Test: bond, connect successfully
    
    Change-Id: Ifd606c5c549e3c79ed51e5b79dcfe72b4e2fe027

--- system/btif/include/btif_config.h
+++ system/btif/include/btif_config.h
@@ -71,3 +71,8 @@ std::vector<RawAddress> btif_config_get_paired_devices();
 
 bool btif_config_clear(void);
 void btif_debug_config_dump(int fd);
+
+/** Bug fix connect device with clock_offset=0  @{ */
+bool btif_get_device_clockoffset(const RawAddress& bda, int* p_clock_offset);
+bool btif_set_device_clockoffset(const RawAddress& bda, int clock_offset);
+/** @} */
