6f5a4b0fbdc3f8db19974ab1875e97f50d4acb02
Hemant Gupta <quic_hemantg@quicinc.com>
2022-11-19 14:00:13 +0530

    Add scan filter for Transport Discovery data
    Snapshot of:
    e34905bdfd2e0064b8c4d0332234f590e1cafac3
    325e5fb544757097b32d9bb3ef2793c114b90ec6
    91b711eb0d46f5b05c3a6d7d68f817eb917e08f1
    cfd02ab820ac7869aee7ab9b3349d4bcab914d52
    060b1c3e022d1350012b2776b2e1b3e8dd9f3cbd
    
    Bug: 262744495
    Tag: #feature
    Test: Manual end-to-end testing (Need BT Controller Support for sub opcode 0x08 in APCF Scan Filter)
    Test: atest LeScanningManagerAndroidHciTest
    Test: atest CtsBluetoothTestCases
    
    Change-Id: I84b791d7424620bc7db5b19e8f6a14938b1524af

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -4389,6 +4389,25 @@ public class AdapterService extends Service {
             }
             return BluetoothStatusCodes.SUCCESS;
         }
+
+        @Override
+        public void isOffloadedTransportDiscoveryDataScanSupported(
+                SynchronousResultReceiver receiver) {
+            try {
+                receiver.send(isOffloadedTransportDiscoveryDataScanSupported());
+            } catch (RuntimeException e) {
+                receiver.propagateException(e);
+            }
+        }
+        private boolean isOffloadedTransportDiscoveryDataScanSupported() {
+            AdapterService service = getService();
+            if (service == null) {
+                return false;
+            }
+
+            return service.isOffloadedTransportDiscoveryDataScanSupported();
+        }
+
     }
 
     /**
@@ -5568,6 +5587,10 @@ public class AdapterService extends Service {
 
     public int getTotalNumOfTrackableAdvertisements() {
         return mAdapterProperties.getTotalNumOfTrackableAdvertisements();
+    }
+
+    public boolean isOffloadedTransportDiscoveryDataScanSupported() {
+        return mAdapterProperties.isOffloadedTransportDiscoveryDataScanSupported();
     }
 
     /**
