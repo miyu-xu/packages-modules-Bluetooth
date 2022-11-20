75c10c6912d3a326d213a4b7dc8b8cf53ad7f1f4
Chris Manton <cmanton@google.com>
2022-11-20 12:00:38 -0800

    headless: Iterate properties properly
    Bug: 257972065
    Test: gd/cert/run
    Tag: #refactor
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I64b4c92f0c148e340c17f6fab538b7d72586dea1

--- system/test/headless/discovery/discovery.cc
+++ system/test/headless/discovery/discovery.cc
@@ -116,7 +116,7 @@ int start_discovery([[maybe_unused]] unsigned int num_loops,
     LOG_CONSOLE("got remote services :%s", params.ToString().c_str());
 
     for (int i = 0; i < params.num_properties; i++) {
-      process_property(params.bd_addr, params.properties);
+      process_property(params.bd_addr, params.properties + i);
     }
   }
 
@@ -151,7 +151,7 @@ int start_discovery([[maybe_unused]] unsigned int num_loops,
     LOG_CONSOLE("got remote services :%s", params.ToString().c_str());
 
     for (int i = 0; i < params.num_properties; i++) {
-      process_property(params.bd_addr, params.properties);
+      process_property(params.bd_addr, params.properties + i);
     }
   }
 
