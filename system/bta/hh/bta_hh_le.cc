65e201847116b642d9a2aa04c4b13690c136c28b
Zero Liu <Zero.Liu@mediatek.com>
2022-11-21 17:46:35 +0800

    Add code to support output type report
    [Root Cause]
    Discover characteristic don't support hid output
    
    [Solution]
    Add hid output type in bta_hh_le_search_hid_chars api
    
    Bug: 249709008
    Test: hid, hogp connect, data successfully
    
    Change-Id: Ia747e328a30d95af1dc1a6db5381f2fd04e11f77

--- system/bta/hh/bta_hh_le.cc
+++ system/bta/hh/bta_hh_le.cc
@@ -1415,7 +1415,8 @@ static void bta_hh_le_search_hid_chars(tBTA_HH_DEV_CB* p_dev_cb,
           break;
         }
 
-        if (p_rpt->rpt_type != BTA_HH_RPTT_INPUT) break;
+        if ((p_rpt->rpt_type != BTA_HH_RPTT_INPUT) && (p_rpt->rpt_type != BTA_HH_RPTT_OUTPUT))
+          break;
 
         bta_hh_le_read_char_descriptor(p_dev_cb, charac.value_handle,
                                        GATT_UUID_RPT_REF_DESCR,
