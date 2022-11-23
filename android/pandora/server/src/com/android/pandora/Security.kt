34d17aff531157dcdd9f3f012cfae46e6f018c73
Charlie Boutier <charliebout@google.com>
2022-11-23 17:38:38 +0000

    [Pandora] - Security wip
    Test: untested yet
    Bug: 260264302
    
    Change-Id: Ie73698ed6f465a9da7c4e785d778dadf90dec87e

--- android/pandora/server/src/com/android/pandora/Security.kt
+++ android/pandora/server/src/com/android/pandora/Security.kt
@@ -19,9 +19,13 @@ package com.android.pandora
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothDevice.ACTION_PAIRING_REQUEST
+import android.bluetooth.BluetoothDevice.BOND_BONDED
+import android.bluetooth.BluetoothDevice.BOND_NONE
 import android.bluetooth.BluetoothDevice.DEVICE_TYPE_CLASSIC
 import android.bluetooth.BluetoothDevice.DEVICE_TYPE_LE
 import android.bluetooth.BluetoothDevice.EXTRA_PAIRING_VARIANT
+import android.bluetooth.BluetoothDevice.TRANSPORT_BREDR
+import android.bluetooth.BluetoothDevice.TRANSPORT_LE
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
@@ -29,6 +33,7 @@ import android.content.IntentFilter
 import android.util.Log
 import com.google.protobuf.ByteString
 import com.google.protobuf.Empty
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -44,6 +49,14 @@ import kotlinx.coroutines.flow.shareIn
 import pandora.HostProto.*
 import pandora.SecurityGrpc.SecurityImplBase
 import pandora.SecurityProto.*
+import pandora.SecurityProto.LESecurityLevel.LE_LEVEL1
+import pandora.SecurityProto.LESecurityLevel.LE_LEVEL2
+import pandora.SecurityProto.LESecurityLevel.LE_LEVEL3
+import pandora.SecurityProto.LESecurityLevel.LE_LEVEL4
+import pandora.SecurityProto.SecurityLevel.LEVEL0
+import pandora.SecurityProto.SecurityLevel.LEVEL1
+import pandora.SecurityProto.SecurityLevel.LEVEL2
+import pandora.SecurityProto.SecurityLevel.LEVEL3
 
 private const val TAG = "PandoraSecurity"
 
@@ -68,17 +81,64 @@ class Security(private val context: Context) : SecurityImplBase() {
     globalScope.cancel()
   }
 
-  override fun pair(request: PairRequest, responseObserver: StreamObserver<Empty>) {
+  override fun secure(request: SecureRequest, responseObserver: StreamObserver<SecureResponse>) {
     grpcUnary(globalScope, responseObserver) {
       val bluetoothDevice = request.connection.toBluetoothDevice(bluetoothAdapter)
-      Log.i(
-        TAG,
-        "pair: ${bluetoothDevice.address} (current bond state: ${bluetoothDevice.bondState})"
-      )
-      Log.d(TAG, "transport: ${request.connection.transport}")
-      bluetoothDevice.createBond(request.connection.transport)
-
-      Empty.getDefaultInstance()
+      val transport = request.connection.transport
+      Log.i(TAG, "secure: $bluetoothDevice transport: $transport")
+      var reached =
+        when (transport) {
+          TRANSPORT_LE -> {
+            check(request.hasLe())
+            val level = request.le
+            if (level == LE_LEVEL1) true
+            if (level == LE_LEVEL4) throw Status.UNKNOWN.asException()
+            false
+          }
+          TRANSPORT_BREDR -> {
+            check(request.hasClassic())
+            val level = request.classic
+            if (level == LEVEL0) true
+            if (level >= LEVEL3) throw Status.UNKNOWN.asException()
+            false
+          }
+          else -> throw Status.UNKNOWN.asException()
+        }
+      if (!reached) {
+        bluetoothDevice.createBond(transport)
+        val bondState =
+          flow
+            .filter { it.action == BluetoothDevice.ACTION_BOND_STATE_CHANGED }
+            .filter { it.getBluetoothDeviceExtra() == bluetoothDevice }
+            .map { it.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothAdapter.ERROR) }
+            .filter { it == BOND_BONDED || it == BOND_NONE }
+            .first()
+        val isEncrypted = bluetoothDevice.isEncrypted()
+        reached =
+          when (transport) {
+            TRANSPORT_LE -> {
+              val level = request.le
+              when (level) {
+                LE_LEVEL2 -> isEncrypted
+                LE_LEVEL3 -> isEncrypted && bondState == BOND_BONDED
+                else -> throw Status.UNKNOWN.asException()
+              }
+            }
+            TRANSPORT_BREDR -> {
+              val level = request.classic
+              when (level) {
+                LEVEL1 -> !isEncrypted || bondState == BOND_BONDED
+                LEVEL2 -> isEncrypted && bondState == BOND_BONDED
+                else -> throw Status.UNKNOWN.asException()
+              }
+            }
+            else -> throw Status.UNKNOWN.asException()
+          }
+      }
+      val secureResponseBuilder = SecureResponse.newBuilder()
+      if (reached) secureResponseBuilder.setSuccess(Empty.getDefaultInstance())
+      else secureResponseBuilder.setNotReached(Empty.getDefaultInstance())
+      secureResponseBuilder.build()
     }
   }
 
