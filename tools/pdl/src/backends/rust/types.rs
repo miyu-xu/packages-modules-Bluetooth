439ff14c0473a004a7a97b52065d00b78577f015
Martin Geisler <mgeisler@google.com>
2022-11-23 11:01:00 +0100

    pdl: Add support for enums fields
    This adds support for enum fields: we now generate getters and setters
    for them. The canonical test vectors work.
    
    Test: atest pdl_rust_generator_tests_le pdl_rust_generator_tests_be
    Change-Id: Id65bf831ddde8ade9780f89096842ef9516cf4c3

--- tools/pdl/src/backends/rust/types.rs
+++ tools/pdl/src/backends/rust/types.rs
@@ -1,5 +1,7 @@
 //! Utility functions for dealing with Rust integer types.
 
+use quote::format_ident;
+
 /// A Rust integer type such as `u8`.
 #[derive(Copy, Clone)]
 pub struct Integer {
@@ -26,6 +28,36 @@ impl quote::ToTokens for Integer {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
         let t: syn::Type = syn::parse_str(&format!("u{}", self.width))
             .expect("Could not parse integer, unsupported width?");
+        t.to_tokens(tokens);
+    }
+}
+
+/// A Rust enum type.
+#[derive(Copy, Clone)]
+pub struct Enum<'a> {
+    pub name: &'a str,
+    pub width: usize,
+}
+
+impl Enum<'_> {
+    /// Get the Rust enum type for the given bit width.
+    ///
+    /// This will round up the size to the nearest Rust integer size.
+    /// PDL supports integers up to 64 bit, so it is an error to call
+    /// this with a width larger than 64.
+    pub fn new(name: &str, width: usize) -> Enum<'_> {
+        for integer_width in [8, 16, 32, 64] {
+            if width <= integer_width {
+                return Enum { name, width: integer_width };
+            }
+        }
+        panic!("Cannot construct Enum {name} with width: {width}")
+    }
+}
+
+impl quote::ToTokens for Enum<'_> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let t = format_ident!("{}", self.name);
         t.to_tokens(tokens);
     }
 }
