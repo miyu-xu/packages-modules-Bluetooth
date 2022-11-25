5b9c678f6e31e466887422cfd1b5c30fe75a9808
Chris Manton <cmanton@google.com>
2022-11-25 14:45:26 -0800

    rnr: Add hci status to remote name read callback data
    Bug: 254358644
    Test: gd/cert/run
    Tag: #refactor
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I9110bf52569ec38b517c1de1e5a5d22fcbc52b8f

--- system/stack/btm/btm_inq.cc
+++ system/stack/btm/btm_inq.cc
@@ -829,6 +829,7 @@ void btm_inq_db_reset(void) {
 
     if (p_inq->p_remname_cmpl_cb) {
       rem_name.status = BTM_DEV_RESET;
+      rem_name.hci_status = HCI_SUCCESS;
 
       (*p_inq->p_remname_cmpl_cb)(&rem_name);
       p_inq->p_remname_cmpl_cb = NULL;
@@ -1485,6 +1486,7 @@ void btm_process_remote_name(const RawAddress* bda, const BD_NAME bdn,
       rem_name.length = (evt_len < BD_NAME_LEN) ? evt_len : BD_NAME_LEN;
       rem_name.remote_bd_name[rem_name.length] = 0;
       rem_name.status = BTM_SUCCESS;
+      rem_name.hci_status = hci_status;
       temp_evt_len = rem_name.length;
 
       while (temp_evt_len > 0) {
@@ -1492,12 +1494,11 @@ void btm_process_remote_name(const RawAddress* bda, const BD_NAME bdn,
         temp_evt_len--;
       }
       rem_name.remote_bd_name[rem_name.length] = 0;
-    }
-
-    /* If processing a stand alone remote name then report the error in the
-       callback */
-    else {
+    } else {
+      /* If processing a stand alone remote name then report the error in the
+         callback */
       rem_name.status = BTM_BAD_VALUE_RET;
+      rem_name.hci_status = hci_status;
       rem_name.length = 0;
       rem_name.remote_bd_name[0] = 0;
     }
