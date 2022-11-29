8e88e49f8cefea0fda94680d707b623d15f2acd4
Chris Manton <cmanton@google.com>
2022-11-28 18:51:07 -0800

    rnr: Condition upon hci status
    Bug: 254358644
    Test: gd/cert/run
    Tag: #refactor
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I69c866b0177bbee5be1d0962e07912e265435cbe

--- system/bta/dm/bta_dm_act.cc
+++ system/bta/dm/bta_dm_act.cc
@@ -2010,11 +2010,24 @@ static void bta_dm_remname_cback(void* p) {
       BTM_SecDeleteRmtNameNotifyCallback(&bta_dm_service_search_remname_cback);
     }
   } else {
-    // if we got a different response, ignore it
+    // if we got a different response, maybe ignore it
     // we will have made a request directly from BTM_ReadRemoteDeviceName so we
     // expect a dedicated response for us
-    LOG_INFO("ignoring remote name response in DM callback since it's for the wrong bd_addr");
-    return;
+    if (p_remote_name->hci_status == HCI_ERR_CONNECTION_EXISTS) {
+      if (bluetooth::shim::is_gd_security_enabled()) {
+        bluetooth::shim::BTM_SecDeleteRmtNameNotifyCallback(
+            &bta_dm_service_search_remname_cback);
+      } else {
+        BTM_SecDeleteRmtNameNotifyCallback(
+            &bta_dm_service_search_remname_cback);
+      }
+      LOG_INFO("Ignoring bad status with assumption already exists");
+    } else {
+      LOG_INFO(
+          "ignoring remote name response in DM callback since it's for the "
+          "wrong bd_addr");
+      return;
+    }
   }
 
   /* remote name discovery is done but it could be failed */
