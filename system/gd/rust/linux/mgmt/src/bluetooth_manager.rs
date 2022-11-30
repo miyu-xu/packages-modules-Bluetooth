310b70f614981fc9535707f4020945f8ac049a9d
Zhengping Jiang <jiangzp@google.com>
2022-11-30 01:41:07 -0800

    floss: add return value for set_ll_privacy
    Add return value to set_ll_privacy dbus call.
    set_ll_privacy will return false if file permission is wrong.
    
    Bug: 242100561
    Tag: #floss
    Test: local test with evtest
    Change-Id: I4cffeb639615d91c0def8285e8de3bfdbb58904b

--- system/gd/rust/linux/mgmt/src/bluetooth_manager.rs
+++ system/gd/rust/linux/mgmt/src/bluetooth_manager.rs
@@ -172,19 +172,21 @@ impl IBluetoothManager for BluetoothManager {
 
 /// Implementation of IBluetoothExperimental
 impl IBluetoothExperimental for BluetoothManager {
-    fn set_ll_privacy(&mut self, enabled: bool) {
+    fn set_ll_privacy(&mut self, enabled: bool) -> bool {
         let current_status = match config_util::read_floss_ll_privacy_enabled() {
             Ok(true) => true,
             _ => false,
         };
 
         if current_status == enabled {
-            return;
+            return true;
         }
 
         if let Err(e) = config_util::write_floss_ll_privacy_enabled(enabled) {
             error!("Failed to write ll privacy status: {}", e);
-            return;
+            return false;
         }
+
+        return true;
     }
 }
