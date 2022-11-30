4e0e7a4b55647695a6e135c3e152a73e4eb4411e
Chris Manton <cmanton@google.com>
2022-11-29 17:04:50 -0800

    headless: Update discovery with new property impl
    Bug: 257972065
    Test: gd/cert/run
    Tag: #refactor
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I6a4134160b48386e05887b6f5f92991a8992eabd

--- system/test/headless/discovery/discovery.cc
+++ system/test/headless/discovery/discovery.cc
@@ -26,12 +26,14 @@
 #include "stack/include/sdp_api.h"
 // #include "test/headless/bt_property.h"
 #include "test/headless/get_options.h"
+#include "test/headless/handler.h"
 #include "test/headless/headless.h"
 #include "test/headless/interface.h"
 #include "test/headless/log.h"
 #include "test/headless/messenger.h"
 #include "test/headless/sdp/sdp.h"
 #include "test/headless/stopwatch.h"
+#include "test/headless/tamper.h"
 #include "test/headless/timeout.h"
 #include "types/bluetooth/uuid.h"
 #include "types/raw_address.h"
@@ -41,49 +43,20 @@ using namespace std::chrono_literals;
 
 namespace {
 
-int start_discovery([[maybe_unused]] unsigned int num_loops,
-                    const RawAddress& raw_address) {
+int start_discovery(bluetooth::test::headless::Handler* handler,
+                    const DiscoveryOpt* opts, const RawAddress& raw_address) {
   RawAddress bd_addr{raw_address};
+
+  unsigned num_passes = opts->get_num_passes();
+  int disconnect_time_sec = opts->get_disconnect_time_sec();
 
   Stopwatch acl_stopwatch("ACL_connection");
   Stopwatch sdp_stopwatch("SDP_discovery");
 
   LOG_CONSOLE("Started service discovery");
-  auto check_point = messenger::sdp::get_check_point();
-  ASSERT(bluetoothInterface.get_remote_services(&bd_addr, 0) ==
-         BT_STATUS_SUCCESS);
-
-  if (!messenger::acl::await_connected(8s)) {
-    LOG_CONSOLE("TIMEOUT waiting for connection to %s",
-                raw_address.ToString().c_str());
-    return -1;
-  }
-  LOG_CONSOLE("ACL connected to %s :%sms", STR(raw_address),
-              STR(acl_stopwatch));
-
-  if (!messenger::sdp::await_service_discovery(8s, check_point, 1UL)) {
-    LOG_CONSOLE("TIMEOUT waiting for service discovery to %s",
-                raw_address.ToString().c_str());
-    return -1;
-  }
-  auto callback_queue = messenger::sdp::collect_from(check_point);
-  ASSERT_LOG(callback_queue.size() == 1,
-             "Received unexpected number of SDP queries");
-
-  auto params = callback_queue.front();
-  callback_queue.pop_front();
-
-  LOG_CONSOLE("got remote services :%s", params.ToString().c_str());
-
-  for (int i = 0; i < params.num_properties; i++) {
-    process_property(params.bd_addr, params.properties + i);
-  }
-
-  // Run a second fetch SDP
-  {
-    LOG_CONSOLE("Sending second SDP request");
+  for (unsigned i = 0; i < num_passes; i++) {
+    LOG_CONSOLE("  Pass:%d", i);
     auto check_point = messenger::sdp::get_check_point();
-
     ASSERT(bluetoothInterface.get_remote_services(&bd_addr, 0) ==
            BT_STATUS_SUCCESS);
 
@@ -92,7 +65,7 @@ int start_discovery([[maybe_unused]] unsigned int num_loops,
                   raw_address.ToString().c_str());
       return -1;
     }
-    LOG_CONSOLE("ACL connected to %s :%sms", STR(raw_address),
+    LOG_CONSOLE("ACL connected to %s :%s", STR(raw_address),
                 STR(acl_stopwatch));
 
     if (!messenger::sdp::await_service_discovery(8s, check_point, 1UL)) {
@@ -100,52 +73,27 @@ int start_discovery([[maybe_unused]] unsigned int num_loops,
                   raw_address.ToString().c_str());
       return -1;
     }
+    //    check_point = messenger::sdp::get_check_point();
+
     auto callback_queue = messenger::sdp::collect_from(check_point);
-    ASSERT_LOG(callback_queue.size() == 1,
-               "Received unexpected number of SDP queries");
+    const size_t queue_size = callback_queue.size();
+    LOG_CONSOLE("queue size:%zu", queue_size);
+    ASSERT_LOG(queue_size == 1, "Received unexpected number of SDP queries");
 
     auto params = callback_queue.front();
     callback_queue.pop_front();
 
     LOG_CONSOLE("got remote services :%s", params.ToString().c_str());
 
-    for (int i = 0; i < params.num_properties; i++) {
-      process_property(params.bd_addr, params.properties);
-    }
-  }
+    ASSERT_LOG(params.properties.size() == 1,
+               "This callback only returns a single property");
 
-  // Run a third fetch SDP
-  {
-    LOG_CONSOLE("Sending third SDP request");
-    auto check_point = messenger::sdp::get_check_point();
+    property::uuid_t* uuid_property =
+        get_property_type<property::uuid_t>(params.properties[0]);
+    auto uuids = uuid_property->get_uuids();
 
-    ASSERT(bluetoothInterface.get_remote_services(&bd_addr, 0) ==
-           BT_STATUS_SUCCESS);
-
-    if (!messenger::acl::await_connected(8s)) {
-      LOG_CONSOLE("TIMEOUT waiting for connection to %s",
-                  raw_address.ToString().c_str());
-      return -1;
-    }
-    LOG_CONSOLE("ACL connected to %s :%sms", STR(raw_address),
-                STR(acl_stopwatch));
-
-    if (!messenger::sdp::await_service_discovery(8s, check_point, 1UL)) {
-      LOG_CONSOLE("TIMEOUT waiting for service discovery to %s",
-                  raw_address.ToString().c_str());
-      return -1;
-    }
-    auto callback_queue = messenger::sdp::collect_from(check_point);
-    ASSERT_LOG(callback_queue.size() == 1,
-               "Received unexpected number of SDP queries");
-
-    auto params = callback_queue.front();
-    callback_queue.pop_front();
-
-    LOG_CONSOLE("got remote services :%s", params.ToString().c_str());
-
-    for (int i = 0; i < params.num_properties; i++) {
-      process_property(params.bd_addr, params.properties);
+    for (const auto& uuid : uuids) {
+      LOG_CONSOLE(" Uuid:%s", uuid.ToString().c_str());
     }
   }
 
@@ -179,6 +127,7 @@ int bluetooth::test::headless::Discovery::Run() {
     return -1;
   }
   return RunOnHeadlessStack<int>([this]() {
-    return start_discovery(options_.loop_, options_.device_.front());
+    const DiscoveryOpt* opts = options_.get_module_options<DiscoveryOpt>();
+    return start_discovery(headless_handler_, opts, options_.device_.front());
   });
 }
