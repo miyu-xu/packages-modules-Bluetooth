9cc03d88b075324f2ee5afecaa47912a690f3764
Chris Manton <cmanton@google.com>
2022-11-29 17:06:10 -0800

    headless: Use new module opt nop
    Bug: 257972065
    Test: gd/cert/run
    Tag: #refactor
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: I1a94efe4a5f143bc778f896805464db707b224d2

--- system/test/headless/nop/nop.cc
+++ system/test/headless/nop/nop.cc
@@ -16,19 +16,50 @@
 
 #define LOG_TAG "bt_headless_sdp"
 
+#include "test/headless/nop/nop.h"
+
 #include <future>
+#include <string>
 
 #include "base/logging.h"     // LOG() stdout and android log
 #include "osi/include/log.h"  // android log only
 #include "stack/include/sdp_api.h"
 #include "test/headless/get_options.h"
 #include "test/headless/headless.h"
-#include "test/headless/nop/nop.h"
 #include "types/raw_address.h"
+
+using namespace bluetooth::test::headless;
+
+class NopOpt : public ModOpt {
+ public:
+  int get_integer() const {
+    auto it = arg_map_.find("-i");
+    return (it == arg_map_.end()) ? 0 : std::stoi(it->second);
+  }
+
+  double get_double() const {
+    auto it = arg_map_.find("-d");
+    return (it == arg_map_.end()) ? 0.0 : std::stod(it->second);
+  }
+
+  std::string get_string() const {
+    auto it = arg_map_.find("-s");
+    return (it == arg_map_.end()) ? std::string("") : it->second;
+  }
+};
 
 int bluetooth::test::headless::Nop::Run() {
   return RunOnHeadlessStack<int>([this]() {
-    fprintf(stdout, "Nop loop:%lu\n", loop_);
+    // Module options
+    const NopOpt* opts = options_.get_module_options<NopOpt>();
+    const int one = opts->get_integer();
+    const double two = opts->get_double();
+    const std::string three = opts->get_string();
+    for (const auto& [k, v] : opts->GetDefaultShortArgMap()) {
+      LOG_CONSOLE("  arg string:%s:%s", k.c_str(), v.c_str());
+    }
+    LOG_CONSOLE("one:%d two:%f three:%s", one, two, three.c_str());
+    LOG_CONSOLE("Nop loop:%lu\n", loop_);
     return 0;
   });
 }
