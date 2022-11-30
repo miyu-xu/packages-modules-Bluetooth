34ed7e897669ebfa219f45fa8f2ed9c294ebb4e9
Rahul Sabnis <rahulsabnis@google.com>
2022-11-30 11:33:00 -0800

    Add APIs to set and get the preferred audio mode
    Tag: #feature
    Bug: 257881495
    Test: atest DatabaseManagerTest
    Change-Id: I603d639ae300adbf6c55ea5573452f8f179dbe2b

--- android/app/src/com/android/bluetooth/btservice/storage/MetadataDatabase.java
+++ android/app/src/com/android/bluetooth/btservice/storage/MetadataDatabase.java
@@ -526,4 +526,25 @@ public abstract class MetadataDatabase extends RoomDatabase {
             }
         }
     };
+
+    @VisibleForTesting
+    static final Migration MIGRATION_115_116 = new Migration(115, 116) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            try {
+                database.execSQL("ALTER TABLE metadata ADD COLUMN `preferred_output_only_profile` "
+                        + "INTEGER NOT NULL DEFAULT 0");
+                database.execSQL("ALTER TABLE metadata ADD COLUMN `preferred_duplex_profile` "
+                        + "INTEGER NOT NULL DEFAULT 0");
+            } catch (SQLException ex) {
+                // Check if user has new schema, but is just missing the version update
+                Cursor cursor = database.query("SELECT * FROM metadata");
+                if (cursor == null
+                        || cursor.getColumnIndex("preferred_output_only_profile") == -1
+                        || cursor.getColumnIndex("preferred_duplex_profile") == -1) {
+                    throw ex;
+                }
+            }
+        }
+    };
 }
