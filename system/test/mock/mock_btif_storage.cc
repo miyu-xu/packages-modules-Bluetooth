0ce46272e5bcf89f5af917176358a4f81ff3b684
Nitin Shivpure <quic_nshivpur@quicinc.com>
2022-11-30 12:54:30 +0530

    PBAP: Dynamic Version Upgrade for PBAP 1.2 supporting remote
    This change stores version of remote device if it supports
    PBAP 1.2. While sending SDP response, remote device support
    will be checked in conf file and if entry for remote device
    is found for 1.2 support, device will send 1.2 SDP response.
    
    Snapshot of:
    72f658a69d62794bf25f99fece089ccf3666750e
    6f313c106a71a12eb87e005a41213a54cda425da
    e909f6e5ef0ba5f56a3ee7ede5b86dc3f668c564
    6f66c22f26a7cd3fdc4cd44d8c8bb47af0a629b3
    009034241686da41a474bedf225d9f7af8595640
    680251a750a7b3e46de6cd71a6396f272dd91e51
    a156ef66075ad88f605304ec929389fabed236bf
    7ad3706865c28c08330023d7921dbc43fbb1111a
    b79f5b83684d3605b3ae44430bbff12f5c09f4c5
    9cc899b5529da7504ede4810ec8f1f47c01b8f42
    
    Test: atest net_test_stack_sdp
    Tag: #feature
    Bug: 252902891
    Change-Id: Idf4edbc4b39506855ecf6245a96eca5706082256

--- system/test/mock/mock_btif_storage.cc
+++ system/test/mock/mock_btif_storage.cc
@@ -243,3 +243,8 @@ void btif_storage_load_bonded_csis_devices() {
 void btif_storage_remove_csis_device(const RawAddress& address) {
   mock_function_count_map[__func__]++;
 }
+
+bool btif_storage_is_pce_version_102(const RawAddress& remote_bd_addr) {
+  mock_function_count_map[__func__]++;
+  return false;
+}
