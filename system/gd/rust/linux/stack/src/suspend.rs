aa1adb44fa6acaddf4a1c31a4d3028dddba18ba9
Andrew Walbran <qwandor@google.com>
2022-12-01 17:18:39 +0000

    Make dependencies explicit.
    extern crate allows dependencies on crates not listed in the Soong build
    rule. We don't want this, so remove extern crate.
    
    Bug: 261037227
    Test: atest pdl_tests
    Change-Id: I26b7311307a5415d5d9788692ae6b6d3f917c1b0

--- system/gd/rust/linux/stack/src/suspend.rs
+++ system/gd/rust/linux/stack/src/suspend.rs
@@ -5,6 +5,7 @@ use crate::callbacks::Callbacks;
 use crate::{bluetooth_gatt::IBluetoothGatt, BluetoothGatt, Message, RPCProxy};
 use bt_topshim::btif::BluetoothInterface;
 use log::warn;
+use num_derive::{FromPrimitive, ToPrimitive};
 use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc::Sender;
 
