40784d86db0047acafe850ebb213a1ed5522954e
My Name <thebestguy@google.com>
2022-12-01 11:01:45 +0000

    Fix OPP activities tests
    Fix BluetoothOppBtEnableActivity, BluetoothOppBtEnablingActivity,
    BluetoothOppIncomingFileConfirmActivity & BluetoothOppLauncherActivity. And
    make the opp activities testable using ActivityInstrumentationTestCase2
    Bug: 260295342
    Bug: 237467631
    Bug: 260188543
    Tag: #refactor
    Test: atest BluetoothOppBtEnableActivityTest
    Test: atest BluetoothOppBtEnablingActivityTest
    Test: atest BluetoothOppIncomingFileConfirmActivityTest
    Test: atest BluetoothOppLauncherActivityTest
    
    Change-Id: I94f5d93ce2a027fa37499826f9d5baf48a63753b

--- android/app/tests/unit/src/com/android/bluetooth/opp/BluetoothOppLauncherActivityTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/opp/BluetoothOppLauncherActivityTest.java
@@ -16,10 +16,6 @@
 
 package com.android.bluetooth.opp;
 
-import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
-import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
-import static android.content.pm.PackageManager.DONT_KILL_APP;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -30,6 +26,8 @@ import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.android.bluetooth.TestableActivities.STATE_DESTROYED;
+
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -38,21 +36,22 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 import android.bluetooth.BluetoothDevicePicker;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.net.Uri;
+import android.test.ActivityInstrumentationTestCase2;
 
-import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.filters.MediumTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.bluetooth.BluetoothMethodProxy;
 import com.android.bluetooth.R;
+import com.android.bluetooth.TestableActivities;
 
 import org.junit.After;
 import org.junit.Before;
@@ -63,14 +62,17 @@ import org.mockito.MockitoAnnotations;
 
 import java.io.File;
 
-
-@MediumTest
 @RunWith(AndroidJUnit4.class)
-public class BluetoothOppLauncherActivityTest {
+public class BluetoothOppLauncherActivityTest extends
+        ActivityInstrumentationTestCase2<BluetoothOppLauncherActivity> {
     Context mTargetContext;
     Intent mIntent;
 
     BluetoothMethodProxy mMethodProxy;
+
+    public BluetoothOppLauncherActivityTest() {
+        super(BluetoothOppLauncherActivity.class);
+    }
 
     @Before
     public void setUp() {
@@ -79,50 +81,59 @@ public class BluetoothOppLauncherActivityTest {
                 ApplicationProvider.getApplicationContext()));
         mMethodProxy = spy(BluetoothMethodProxy.getInstance());
         BluetoothMethodProxy.setInstanceForTesting(mMethodProxy);
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
 
         mIntent = new Intent();
         mIntent.setClass(mTargetContext, BluetoothOppLauncherActivity.class);
 
-        enableActivity(true);
+        BluetoothOppTestUtils.enableOppActivities(true, mTargetContext);
         Intents.init();
     }
 
     @After
     public void tearDown() {
+        BluetoothOppTestUtils.enableOppActivities(false, mTargetContext);
         BluetoothMethodProxy.setInstanceForTesting(null);
         Intents.release();
-        enableActivity(false);
     }
 
     @Test
     public void onCreate_withNoAction_returnImmediately() throws Exception {
-        ActivityScenario<BluetoothOppLauncherActivity> activityScenario = ActivityScenario.launch(
-                mIntent);
-        assertActivityState(activityScenario, Lifecycle.State.DESTROYED);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        assertThat(activity.waitForStage(TestableActivities.STATE_DESTROYED, 100)).isTrue();
     }
 
     @Test
     public void onCreate_withActionSend_withoutMetadata_finishImmediately() throws Exception {
         mIntent.setAction(Intent.ACTION_SEND);
-        ActivityScenario<BluetoothOppLauncherActivity> activityScenario = ActivityScenario.launch(
-                mIntent);
-        assertActivityState(activityScenario, Lifecycle.State.DESTROYED);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        assertThat(activity.waitForStage(TestableActivities.STATE_DESTROYED, 100)).isTrue();
     }
 
     @Test
-    public void onCreate_withActionSendMultiple_withoutMetadata_finishImmediately()
-            throws Exception {
+    public void onCreate_withActionSendMultiple_withoutMetadata_finishImmediately() {
         mIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
-        ActivityScenario<BluetoothOppLauncherActivity> activityScenario = ActivityScenario.launch(
-                mIntent);
-        assertActivityState(activityScenario, Lifecycle.State.DESTROYED);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        assertThat(activity.waitForStage(TestableActivities.STATE_DESTROYED, 100)).isTrue();
     }
 
     @Test
     public void onCreate_withActionOpen_sendBroadcast() throws Exception {
         mIntent.setAction(Constants.ACTION_OPEN);
         mIntent.setData(Uri.EMPTY);
-        ActivityScenario.launch(mIntent);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
         ArgumentCaptor<Intent> argument = ArgumentCaptor.forClass(Intent.class);
 
         verify(mMethodProxy).contextSendBroadcast(any(), argument.capture());
@@ -138,9 +149,11 @@ public class BluetoothOppLauncherActivityTest {
         doReturn(false).when(mMethodProxy).bluetoothAdapterIsEnabled(any());
         // Unsupported action, the activity will stay without being finished right the way
         mIntent.setAction("unsupported-action");
-        ActivityScenario<BluetoothOppLauncherActivity> scenario = ActivityScenario.launch(mIntent);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
-        scenario.onActivity(BluetoothOppLauncherActivity::launchDevicePicker);
+        activity.launchDevicePicker();
 
         onView(withText(mTargetContext.getText(R.string.bt_enable_cancel).toString())).inRoot(
                 isDialog()).check(matches(isDisplayed())).perform(click());
@@ -152,9 +165,11 @@ public class BluetoothOppLauncherActivityTest {
         doReturn(true).when(mMethodProxy).bluetoothAdapterIsEnabled(any());
         // Unsupported action, the activity will stay without being finished right the way
         mIntent.setAction("unsupported-action");
-        ActivityScenario<BluetoothOppLauncherActivity> scenario = ActivityScenario.launch(mIntent);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
-        scenario.onActivity(BluetoothOppLauncherActivity::launchDevicePicker);
+        activity.launchDevicePicker();
 
         intended(hasAction(BluetoothDevicePicker.ACTION_LAUNCH));
     }
@@ -166,38 +181,20 @@ public class BluetoothOppLauncherActivityTest {
         mIntent.setAction("unsupported-action");
         ActivityScenario<BluetoothOppLauncherActivity> scenario = ActivityScenario.launch(mIntent);
 
-        final Uri[] fileUri = new Uri[1];
+
         final String shareContent =
                 "a string to trigger pattern match with url: www.google.com, phone number: "
                         + "+821023456798, and email: abc@test.com";
-        scenario.onActivity(activity -> {
-            fileUri[0] = activity.createFileForSharedContent(activity, shareContent);
+        setActivityIntent(mIntent);
+        BluetoothOppLauncherActivity activity = getActivity();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
-        });
-        assertThat(fileUri[0].toString().endsWith(".html")).isTrue();
+        final Uri fileUri = activity.createFileForSharedContent(activity, shareContent);
 
-        File file = new File(fileUri[0].getPath());
+        assertThat(fileUri.toString().endsWith(".html")).isTrue();
+
+        File file = new File(fileUri.getPath());
         // new file is in html format that include the shared content, so length should increase
         assertThat(file.length()).isGreaterThan(shareContent.length());
-    }
-
-    private void assertActivityState(ActivityScenario activityScenario, Lifecycle.State state)
-            throws Exception {
-        Thread.sleep(2_000);
-        assertThat(activityScenario.getState()).isEqualTo(state);
-    }
-
-
-    private void enableActivity(boolean enable) {
-        int enabledState = enable ? COMPONENT_ENABLED_STATE_ENABLED
-                : COMPONENT_ENABLED_STATE_DEFAULT;
-
-        mTargetContext.getPackageManager().setApplicationEnabledSetting(
-                mTargetContext.getPackageName(), enabledState, DONT_KILL_APP);
-
-        ComponentName activityName = new ComponentName(mTargetContext,
-                BluetoothOppLauncherActivity.class);
-        mTargetContext.getPackageManager().setComponentEnabledSetting(
-                activityName, enabledState, DONT_KILL_APP);
     }
 }
