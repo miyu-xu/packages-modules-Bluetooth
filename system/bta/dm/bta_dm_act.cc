bc3e22cb36977ab1857b2314a4a97bffcc9d65ca
Sagar Verma <quic_sagarv@quicinc.com>
2022-12-02 12:41:04 +0530

    AVRCP: Dynamic AVRCP version feature enhancements
    - Cache AVRCP SDP related info at the time of
    pairing iteslf it will help us having info ready
    for next connection which might be incoming from
    peer device which will be handy.
    
    - Along with peer version store peer features as
    well for AVRCP as per version.
    
    - Ensure updating SDP response records dynamically
    per device basis ensuring minimum negotiated version
    of the DUT and stored peer version from config also
    AVRCP feature set.
    
    Bug: 252902891
    Change-Id: Ideda6914ffc48ad3c068ab7751ad06b98160d0ed

--- system/bta/dm/bta_dm_act.cc
+++ system/bta/dm/bta_dm_act.cc
@@ -63,6 +63,7 @@
 #include "stack/include/srvc_api.h"  // DIS_ReadDISInfo
 #include "types/bluetooth/uuid.h"
 #include "types/raw_address.h"
+#include "stack/include/avrc_api.h"
 
 #if (GAP_INCLUDED == TRUE)
 #include "gap_api.h"
@@ -1088,6 +1089,86 @@ void bta_dm_disc_rmt_name(tBTA_DM_MSG* p_data) {
   bta_dm_discover_device(p_data->rem_name.result.disc_res.bd_addr);
 }
 
+static void store_avrcp_profile_feature(tSDP_DISC_REC* sdp_rec) {
+  tSDP_DISC_ATTR* p_attr =
+      SDP_FindAttributeInRec(sdp_rec, ATTR_ID_SUPPORTED_FEATURES);
+  if (p_attr == NULL) {
+    return;
+  }
+
+  uint16_t avrcp_features = p_attr->attr_value.v.u16;
+  if (avrcp_features == 0) {
+    return;
+  }
+
+  if (btif_config_set_bin(sdp_rec->remote_bd_addr.ToString().c_str(),
+                          AV_REM_CTRL_FEATURES_CONFIG_KEY,
+                          (const uint8_t*)&avrcp_features,
+                          sizeof(avrcp_features))) {
+    LOG_INFO("Saving avrcp_features: 0x%x", avrcp_features);
+    btif_config_save();
+  } else {
+    LOG_INFO("Failed to store avrcp_features 0x%x for %s", avrcp_features
+             ADDRESS_TO_LOGGABLE_CSTR(sdp_rec->remote_bd_addr));
+  }
+}
+
+static void bta_dm_store_audio_profiles_version() {
+  struct AudioProfile {
+    const uint16_t servclass_uuid;
+    const uint16_t btprofile_uuid;
+    const char* profile_key;
+    void (*store_audio_profile_feature)(tSDP_DISC_REC*);
+  };
+
+  std::array<AudioProfile, 1> audio_profiles = {{
+      {
+          .servclass_uuid = UUID_SERVCLASS_AV_REMOTE_CONTROL,
+          .btprofile_uuid = UUID_SERVCLASS_AV_REMOTE_CONTROL,
+          .profile_key = AVRCP_CONTROLLER_VERSION_CONFIG_KEY,
+          .store_audio_profile_feature = store_avrcp_profile_feature,
+      },
+      // {
+      //     .servclass_uuid = HFPxx
+      //     .btprofile_uuid = HFPxx,
+      //     .profile_key = HFPxx,
+      //     .store_audio_version = store_hfp_profile_version,
+      // },
+      // {
+      //     .servclass_uuid = A2DPxx
+      //     .btprofile_uuid = A2DPxx,
+      //     .profile_key = A2DPxx,
+      //     .store_audio_version = nullptr, // A2DP don't have profile features
+      // },
+  }};
+
+  for (const auto& audio_profile : audio_profiles) {
+    tSDP_DISC_REC* sdp_rec = SDP_FindServiceInDb(
+        bta_dm_search_cb.p_sdp_db, audio_profile.servclass_uuid, NULL);
+    if (sdp_rec == NULL) continue;
+
+    if (SDP_FindAttributeInRec(sdp_rec, ATTR_ID_BT_PROFILE_DESC_LIST) == NULL)
+      continue;
+
+    uint16_t profile_version = 0;
+    /* get profile version (if failure, version parameter is not updated) */
+    SDP_FindProfileVersionInRec(sdp_rec, audio_profile.btprofile_uuid,
+                                &profile_version);
+    if (profile_version != 0) {
+      if (btif_config_set_bin(sdp_rec->remote_bd_addr.ToString().c_str(),
+                              audio_profile.profile_key,
+                              (const uint8_t*)&profile_version,
+                              sizeof(profile_version))) {
+        btif_config_save();
+      } else {
+        LOG_INFO("Failed to store peer profile version for %s",
+                 ADDRESS_TO_LOGGABLE_CSTR(sdp_rec->remote_bd_addr));
+      }
+    }
+    audio_profile.store_audio_version(sdp_rec);
+  }
+}
+
 /*******************************************************************************
  *
  * Function         bta_dm_sdp_result
@@ -1201,6 +1282,10 @@ void bta_dm_sdp_result(tBTA_DM_MSG* p_data) {
           }
         }
       } while (p_sdp_rec);
+    }
+
+    if (bta_dm_search_cb.services_to_search == 0) {
+      bta_dm_store_audio_profiles_version();
     }
 
 #if TARGET_FLOSS
