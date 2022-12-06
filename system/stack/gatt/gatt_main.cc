30ef69a7b502fcd8fb3fb76d6c79676c35974168
Rahul Arya <aryarahul@google.com>
2022-12-06 13:08:38 +0000

    Improve handling of GATT timeout
    Based on post-submit discussion on aosp/2266589, this keeps the ACL link
    open until service discovery completes, but closes it if no GATT clients
    exist.
    
    I think it actually doesn't matter since service discovery only happens if a
    GATT client is anyway open, but this CL at least improves readability in that
    case.
    
    Bug: 251954527
    Test: atest pts-bot
    Change-Id: I86cf1eeb358ce940cada326b6afa1cf3b02479f3

--- system/stack/gatt/gatt_main.cc
+++ system/stack/gatt/gatt_main.cc
@@ -114,10 +114,7 @@ void gatt_init(void) {
   fixed_reg.pL2CA_FixedConn_Cb = gatt_le_connect_cback;
   fixed_reg.pL2CA_FixedData_Cb = gatt_le_data_ind;
   fixed_reg.pL2CA_FixedCong_Cb = gatt_le_cong_cback; /* congestion callback */
-  fixed_reg.default_idle_tout =
-      bluetooth::common::init_flags::finite_att_timeout_is_enabled()
-          ? 2 /* We allow 2s for GATT clients to connect once the link is up */
-          : L2CAP_NO_IDLE_TIMEOUT;
+  fixed_reg.default_idle_tout = L2CAP_NO_IDLE_TIMEOUT;
 
   L2CA_RegisterFixedChannel(L2CAP_ATT_CID, &fixed_reg);
 
@@ -590,7 +587,7 @@ static void gatt_le_cong_cback(const RawAddress& remote_bda, bool congested) {
   if (!p_tcb) return;
 
   /* if uncongested, check to see if there is any more pending data */
-    gatt_channel_congestion(p_tcb, congested);
+  gatt_channel_congestion(p_tcb, congested);
 }
 
 /*******************************************************************************
@@ -611,7 +608,6 @@ static void gatt_le_cong_cback(const RawAddress& remote_bda, bool congested) {
  ******************************************************************************/
 static void gatt_le_data_ind(uint16_t chan, const RawAddress& bd_addr,
                              BT_HDR* p_buf) {
-
   /* Find CCB based on bd addr */
   tGATT_TCB* p_tcb = gatt_find_tcb_by_addr(bd_addr, BT_TRANSPORT_LE);
   if (p_tcb) {
@@ -740,7 +736,6 @@ void gatt_l2cif_config_ind_cback(uint16_t lcid, tL2CAP_CFG_INFO* p_cfg) {
 
 /** This is the L2CAP disconnect indication callback function */
 void gatt_l2cif_disconnect_ind_cback(uint16_t lcid, bool ack_needed) {
-
   /* look up clcb for this channel */
   tGATT_TCB* p_tcb = gatt_find_tcb_by_cid(lcid);
   if (!p_tcb) return;
@@ -824,6 +819,14 @@ static void gatt_send_conn_cback(tGATT_TCB* p_tcb) {
      * the idle timer */
     GATT_SetIdleTimeout(p_tcb->peer_bda, GATT_LINK_NO_IDLE_TIMEOUT,
                         p_tcb->transport);
+  } else {
+    if (bluetooth::common::init_flags::finite_att_timeout_is_enabled()) {
+      // Otherwise, release the idle timeout. Note that the
+      // L2CAP idle timeout still exists (and dominates this one, for now), this
+      // just releases the timeout at the ATT fixed channel.
+      GATT_SetIdleTimeout(p_tcb->peer_bda, GATT_LINK_IDLE_TIMEOUT_WHEN_NO_APP,
+                          p_tcb->transport);
+    }
   }
 }
 
