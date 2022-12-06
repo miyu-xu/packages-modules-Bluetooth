c8421ce4202fade3bb6af5e484c49c1fbe809bbf
Martin Geisler <mgeisler@google.com>
2022-12-06 22:01:33 +0100

    pdl: Rework reading of scalar fields
    This CL reverts the code reading scalar fields to match the logic in
    the Python backend. This approach will make it simpler to update the
    two backends going forward.
    
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: I49ddb3dae8eab1bb00cf730eda50a051d326ce2c

---
+++ tools/pdl/src/backends/rust/parser.rs
@@ -0,0 +1,164 @@
+use crate::ast;
+use crate::backends::rust::{mask_bits, types};
+use quote::{format_ident, quote};
+
+struct Chunk<'a> {
+    offset: usize,
+    width: usize,
+    field: &'a ast::Field,
+}
+
+pub struct FieldParser<'a> {
+    endianness: ast::EndiannessValue,
+    packet_name: &'a str,
+    span: &'a proc_macro2::Ident,
+    chunk: Vec<Chunk<'a>>,
+    code: Vec<proc_macro2::TokenStream>,
+    shift: usize,
+    offset: usize,
+}
+
+impl<'a> FieldParser<'a> {
+    pub fn new(
+        endianness: ast::EndiannessValue,
+        packet_name: &'a str,
+        span: &'a proc_macro2::Ident,
+    ) -> FieldParser<'a> {
+        FieldParser {
+            endianness,
+            packet_name,
+            span,
+            chunk: Vec::new(),
+            code: Vec::new(),
+            shift: 0,
+            offset: 0,
+        }
+    }
+
+    fn endianness_suffix(&self, width: usize) -> &'static str {
+        if width > 8 && self.endianness == ast::EndiannessValue::LittleEndian {
+            "_le"
+        } else {
+            ""
+        }
+    }
+
+    /// Parse an unsigned integer with the given `width`.
+    ///
+    /// The generated code requires that `self.span` is a mutable
+    /// `bytes::Buf` value.
+    fn get_uint(&self, width: usize) -> proc_macro2::TokenStream {
+        let span = &self.span;
+        let suffix = self.endianness_suffix(width);
+        let value_type = types::Integer::new(width);
+        if value_type.width == width {
+            let get_u = format_ident!("get_u{}{}", value_type.width, suffix);
+            quote! {
+                #span.#get_u()
+            }
+        } else {
+            let get_uint = format_ident!("get_uint{}", suffix);
+            let value_nbytes = proc_macro2::Literal::usize_unsuffixed(width / 8);
+            let cast = (value_type.width < 64).then(|| quote!(as #value_type));
+            quote! {
+                #span.#get_uint(#value_nbytes) #cast
+            }
+        }
+    }
+
+    pub fn add(&mut self, field: &'a ast::Field) {
+        if field.is_bitfield() {
+            self.add_bit_field(field);
+            return;
+        }
+
+        todo!("not yet supported: {field:?}")
+    }
+
+    fn add_bit_field(&mut self, field: &'a ast::Field) {
+        let width = field.width().unwrap();
+        self.chunk.push(Chunk { offset: self.shift, width, field });
+        self.shift += width;
+        if self.shift % 8 != 0 {
+            return;
+        }
+
+        let size = self.shift / 8;
+        let end_offset = self.offset + size;
+
+        let wanted = proc_macro2::Literal::usize_unsuffixed(size);
+        let packet_name = &self.packet_name;
+        self.code.push(quote! {
+            if bytes.remaining() < #wanted {
+                return Err(Error::InvalidLengthError {
+                    obj: #packet_name.to_string(),
+                    wanted: #wanted,
+                    got: bytes.remaining(),
+                });
+            }
+        });
+
+        let chunk_type = types::Integer::new(self.shift);
+        let chunk_name = format_ident!("chunk");
+
+        let get = self.get_uint(self.shift);
+        if self.chunk.len() > 1 {
+            // Multiple values: we read into a local variable.
+            self.code.push(quote! {
+                let #chunk_name = #get;
+            });
+        }
+
+        let single_value = self.chunk.len() == 1 && self.chunk[0].offset == 0;
+        for Chunk { offset, width, field } in self.chunk.drain(..) {
+            let mut v = if single_value {
+                // Single value: read directly.
+                quote! { #get }
+            } else {
+                // Multiple values: read from `chunk_name`.
+                quote! { #chunk_name }
+            };
+
+            if offset > 0 {
+                let shift = proc_macro2::Literal::usize_unsuffixed(offset);
+                v = quote! { (#v >> #shift) }
+            }
+
+            let value_type = types::Integer::new(width);
+            if width < self.shift && width < value_type.width {
+                // Mask value if we grabbed more than `width` and if
+                // `as #value_type` doesn't already do the masking.
+                let mask = mask_bits(width);
+                v = quote! { (#v & #mask) };
+            }
+
+            if value_type.width < chunk_type.width {
+                v = quote! { #v as #value_type };
+            }
+
+            self.code.push(match field {
+                ast::Field::Scalar { id, .. } => {
+                    let id = format_ident!("{id}");
+                    quote! {
+                        let #id = #v;
+                    }
+                }
+                _ => todo!(),
+            });
+        }
+
+        self.offset = end_offset;
+        self.shift = 0;
+    }
+
+    pub fn done(&mut self) {}
+}
+
+impl quote::ToTokens for FieldParser<'_> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let code = &self.code;
+        tokens.extend(quote! {
+            #(#code)*
+        });
+    }
+}
---
+++ tools/pdl/src/backends/rust/serializer.rs
@@ -0,0 +1,155 @@
+use crate::ast;
+use crate::backends::rust::{mask_bits, types};
+use quote::{format_ident, quote};
+
+pub struct FieldSerializer {
+    endianness: ast::EndiannessValue,
+    packet_name: String,
+    span: proc_macro2::Ident,
+    chunk: Vec<(proc_macro2::TokenStream, Option<proc_macro2::Literal>)>,
+    code: Vec<proc_macro2::TokenStream>,
+    shift: usize,
+}
+
+impl FieldSerializer {
+    pub fn new(
+        endianness: ast::EndiannessValue,
+        packet_name: &str,
+        span: &proc_macro2::Ident,
+    ) -> FieldSerializer {
+        FieldSerializer {
+            endianness,
+            packet_name: packet_name.to_string(),
+            span: span.clone(),
+            chunk: Vec::new(),
+            code: Vec::new(),
+            shift: 0,
+        }
+    }
+
+    fn endianness_suffix(&self, width: usize) -> &'static str {
+        if width > 8 && self.endianness == ast::EndiannessValue::LittleEndian {
+            "_le"
+        } else {
+            ""
+        }
+    }
+
+    /// Write an unsigned integer `value` to `self.span`.
+    ///
+    /// The generated code requires that `self.span` is a mutable
+    /// `bytes::BufMut` value.
+    fn put_uint(&self, value: &proc_macro2::TokenStream, width: usize) -> proc_macro2::TokenStream {
+        let span = &self.span;
+        let suffix = self.endianness_suffix(width);
+        let value_type = types::Integer::new(width);
+        if value_type.width == width {
+            let put_u = format_ident!("put_u{}{}", width, suffix);
+            quote! {
+                #span.#put_u(#value)
+            }
+        } else {
+            let put_uint = format_ident!("put_uint{}", suffix);
+            let value_nbytes = proc_macro2::Literal::usize_unsuffixed(width / 8);
+            let cast = (value_type.width < 64).then(|| quote!(as u64));
+            quote! {
+                #span.#put_uint(#value #cast, #value_nbytes)
+            }
+        }
+    }
+
+    pub fn add(&mut self, field: &ast::Field) {
+        if field.is_bitfield() {
+            self.add_bit_field(field);
+            return;
+        }
+
+        todo!("not yet supported: {field:?}")
+    }
+
+    fn add_bit_field(&mut self, field: &ast::Field) {
+        let width = field.width().unwrap();
+
+        match field {
+            ast::Field::Scalar { id, width, .. } => {
+                let field_name = format_ident!("{id}");
+                let field_type = types::Integer::new(*width);
+                if field_type.width > *width {
+                    let packet_name = &self.packet_name;
+                    let max_value = mask_bits(*width);
+                    self.code.push(quote! {
+                        if self.#field_name > #max_value {
+                            panic!(
+                                "Invalid value for {}::{}: {} > {}",
+                                #packet_name, #id, self.#field_name, #max_value
+                            );
+                        }
+                    });
+                }
+                let shift = if self.shift > 0 {
+                    Some(proc_macro2::Literal::usize_unsuffixed(self.shift))
+                } else {
+                    None
+                };
+                self.chunk.push((quote!(self.#field_name), shift));
+            }
+            _ => todo!(),
+        }
+
+        self.shift += width;
+        if self.shift % 8 == 0 {
+            self.pack_bit_fields()
+        }
+    }
+
+    fn pack_bit_fields(&mut self) {
+        assert_eq!(self.shift % 8, 0);
+        let chunk_type = types::Integer::new(self.shift);
+        let chunk_len = self.chunk.len();
+        let values = self
+            .chunk
+            .drain(..)
+            .map(|(mut value, shift)| {
+                if chunk_len > 1 {
+                    // We will be combining values with `|`, so we
+                    // need to cast them first. If there is a single
+                    // value in the chunk, `self.put_uint` will cast.
+                    value = quote! { (#value as #chunk_type) };
+                }
+                if let Some(shift) = shift {
+                    let op = quote!(<<);
+                    value = quote! { (#value #op #shift) };
+                }
+                value
+            })
+            .collect::<Vec<_>>();
+
+        match values.as_slice() {
+            [] => todo!(),
+            [value] => {
+                let put = self.put_uint(value, self.shift);
+                self.code.push(quote! {
+                    #put;
+                });
+            }
+            _ => {
+                let put = self.put_uint(&quote!(value), self.shift);
+                self.code.push(quote! {
+                    let value = #(#values)|*;
+                    #put;
+                });
+            }
+        }
+
+        self.shift = 0;
+    }
+}
+
+impl quote::ToTokens for FieldSerializer {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let code = &self.code;
+        tokens.extend(quote! {
+            #(#code)*
+        });
+    }
+}
