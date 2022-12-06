c11bf61f78c185b2ea3b209a8cfe03107b1aebc5
Rahul Arya <aryarahul@google.com>
2022-12-06 13:08:38 +0000

    Improve handling of GATT timeout
    If a dynamic channel is used, then on its closure, we should let the ATT
    channel timeout. Similarly, if an ATT client exists and then is closed,
    a timeout should occur.
    
    *HOWEVER*, if only an ATT server exists, then we should not timeout.
    This is to avoid breaking backwards compatibility.
    
    Bug: 2266589
    Test: atest pts-bot
    Change-Id: I86cf1eeb358ce940cada326b6afa1cf3b02479f3

--- system/stack/gatt/gatt_main.cc
+++ system/stack/gatt/gatt_main.cc
@@ -114,10 +114,11 @@ void gatt_init(void) {
   fixed_reg.pL2CA_FixedConn_Cb = gatt_le_connect_cback;
   fixed_reg.pL2CA_FixedData_Cb = gatt_le_data_ind;
   fixed_reg.pL2CA_FixedCong_Cb = gatt_le_cong_cback; /* congestion callback */
-  fixed_reg.default_idle_tout =
-      bluetooth::common::init_flags::finite_att_timeout_is_enabled()
-          ? 2 /* We allow 2s for GATT clients to connect once the link is up */
-          : L2CAP_NO_IDLE_TIMEOUT;
+
+  // the GATT timeout is updated after a connection
+  // is established, when we know whether any
+  // clients exist
+  fixed_reg.default_idle_tout = L2CAP_NO_IDLE_TIMEOUT;
 
   L2CA_RegisterFixedChannel(L2CAP_ATT_CID, &fixed_reg);
 
