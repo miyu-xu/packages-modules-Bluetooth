a2d7a163b260d9349b8af338b5e459cfa463565f
Martin Geisler <mgeisler@google.com>
2022-12-06 22:01:33 +0100

    pdl: Rework reading of scalar fields
    This CL reverts the code reading scalar fields to match the logic in
    the Python backend. This approach will make it simpler to update the
    two backends going forward.
    
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: I49ddb3dae8eab1bb00cf730eda50a051d326ce2c

--- tools/pdl/tests/generated/packet_decl_64bit_scalar_big_endian.rs
+++ tools/pdl/tests/generated/packet_decl_64bit_scalar_big_endian.rs
@@ -2,7 +2,7 @@
 
 #![allow(warnings, missing_docs)]
 
-use bytes::{BufMut, Bytes, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use num_derive::{FromPrimitive, ToPrimitive};
 use num_traits::{FromPrimitive, ToPrimitive};
 use std::convert::{TryFrom, TryInto};
@@ -37,70 +37,41 @@ pub trait Packet {
 
 #[derive(Debug)]
 struct FooData {
-    x: u8,
-    y: u16,
-    z: u32,
+    x: u64,
 }
-
 #[derive(Debug, Clone)]
 pub struct FooPacket {
     foo: Arc<FooData>,
 }
-
 #[derive(Debug)]
 pub struct FooBuilder {
-    pub x: u8,
-    pub y: u16,
-    pub z: u32,
+    pub x: u64,
 }
-
 impl FooData {
     fn conforms(bytes: &[u8]) -> bool {
-        bytes.len() >= 6
+        bytes.len() >= 8
     }
     fn parse(mut bytes: &[u8]) -> Result<Self> {
-        if bytes.remaining() < 1 {
+        if bytes.remaining() < 8 {
             return Err(Error::InvalidLengthError {
                 obj: "Foo".to_string(),
-                wanted: 1,
+                wanted: 8,
                 got: bytes.remaining(),
             });
         }
-        let x = bytes.get_u8();
-        if bytes.remaining() < 2 {
-            return Err(Error::InvalidLengthError {
-                obj: "Foo".to_string(),
-                wanted: 2,
-                got: bytes.remaining(),
-            });
-        }
-        let y = bytes.get_u16();
-        if bytes.remaining() < 3 {
-            return Err(Error::InvalidLengthError {
-                obj: "Foo".to_string(),
-                wanted: 3,
-                got: bytes.remaining(),
-            });
-        }
-        let z = bytes.get_uint(3) as u32;
-        Ok(Self { x, y, z })
+        let x = bytes.get_u64();
+        Ok(Self { x })
     }
     fn write_to(&self, buffer: &mut BytesMut) {
-        let x = self.x;
-        buffer.put_u8(x);
-        let y = self.y;
-        buffer.put_u16(y);
-        let z = self.z;
-        buffer.put_uint(z as u64, 3);
+        buffer.put_u64(self.x);
     }
     fn get_total_size(&self) -> usize {
         self.get_size()
     }
     fn get_size(&self) -> usize {
-        6
+        8
     }
 }
-
 impl Packet for FooPacket {
     fn to_bytes(self) -> Bytes {
         let mut buffer = BytesMut::with_capacity(self.foo.get_total_size());
@@ -121,7 +92,6 @@ impl From<FooPacket> for Vec<u8> {
         packet.to_vec()
     }
 }
-
 impl FooPacket {
     pub fn parse(mut bytes: &[u8]) -> Result<Self> {
         Ok(Self::new(Arc::new(FooData::parse(bytes)?)).unwrap())
@@ -130,20 +100,13 @@ impl FooPacket {
         let foo = root;
         Ok(Self { foo })
     }
-    pub fn get_x(&self) -> u8 {
+    pub fn get_x(&self) -> u64 {
         self.foo.as_ref().x
     }
-    pub fn get_y(&self) -> u16 {
-        self.foo.as_ref().y
-    }
-    pub fn get_z(&self) -> u32 {
-        self.foo.as_ref().z
-    }
 }
-
 impl FooBuilder {
     pub fn build(self) -> FooPacket {
-        let foo = Arc::new(FooData { x: self.x, y: self.y, z: self.z });
+        let foo = Arc::new(FooData { x: self.x });
         FooPacket::new(foo).unwrap()
     }
 }
