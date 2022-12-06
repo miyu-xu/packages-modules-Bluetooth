982a29ac2eb89da18aa599aae3249602c3b698a7
Martin Geisler <mgeisler@google.com>
2022-12-06 22:01:33 +0100

    pdl: Rework reading of scalar fields
    This CL reverts the code reading scalar fields to match the logic in
    the Python backend. This approach will make it simpler to update the
    two backends going forward.
    
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: I49ddb3dae8eab1bb00cf730eda50a051d326ce2c

--- tools/pdl/tests/generated/packet_decl_32bit_scalar_little_endian.rs
+++ tools/pdl/tests/generated/packet_decl_32bit_scalar_little_endian.rs
@@ -1,24 +1,38 @@
 #[derive(Debug)]
-struct FooData {}
+struct FooData {
+    x: u32,
+}
 #[derive(Debug, Clone)]
 pub struct FooPacket {
     foo: Arc<FooData>,
 }
 #[derive(Debug)]
-pub struct FooBuilder {}
+pub struct FooBuilder {
+    pub x: u32,
+}
 impl FooData {
     fn conforms(bytes: &[u8]) -> bool {
-        true
+        bytes.len() >= 4
     }
     fn parse(mut bytes: &[u8]) -> Result<Self> {
-        Ok(Self {})
+        if bytes.remaining() < 4 {
+            return Err(Error::InvalidLengthError {
+                obj: "Foo".to_string(),
+                wanted: 4,
+                got: bytes.remaining(),
+            });
+        }
+        let x = bytes.get_u32_le();
+        Ok(Self { x })
     }
-    fn write_to(&self, buffer: &mut BytesMut) {}
+    fn write_to(&self, buffer: &mut BytesMut) {
+        buffer.put_u32_le(self.x);
+    }
     fn get_total_size(&self) -> usize {
         self.get_size()
     }
     fn get_size(&self) -> usize {
-        0
+        4
     }
 }
 impl Packet for FooPacket {
@@ -49,10 +63,13 @@ impl FooPacket {
         let foo = root;
         Ok(Self { foo })
     }
+    pub fn get_x(&self) -> u32 {
+        self.foo.as_ref().x
+    }
 }
 impl FooBuilder {
     pub fn build(self) -> FooPacket {
-        let foo = Arc::new(FooData {});
+        let foo = Arc::new(FooData { x: self.x });
         FooPacket::new(foo).unwrap()
     }
 }
