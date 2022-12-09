eb1f870c7606147adbe9bd7726b062f0d6f500a8
Igor Chernyshev <igorc@google.com>
2022-12-08 20:38:05 -0800

    Upgrade RequireAPI annotation to prevent wrong lint error.
    CDM.getAllAssociations() is made public (was SystemApi)
    and linter will wrongly flag it as [NewApi] so we have to
    update the RequireApi and add a SuppressLint.
    
    Bug: 245972418
    Tag: #feature
    Test: atest FrameworkBluetoothTests
    
    Change-Id: I6fda55ee223db8c0e80b50e53b05fd7b8cd4eb37

--- android/app/src/com/android/bluetooth/Utils.java
+++ android/app/src/com/android/bluetooth/Utils.java
@@ -60,6 +60,8 @@ import android.provider.DeviceConfig;
 import android.provider.Telephony;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+
 import com.android.bluetooth.btservice.AdapterService;
 import com.android.bluetooth.btservice.ProfileService;
 
@@ -76,6 +78,7 @@ import java.nio.charset.CharsetDecoder;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -350,7 +353,7 @@ public final class Utils {
                     + " is inaccurate for calling uid " + callingUid);
         }
 
-        for (AssociationInfo association : cdm.getAllAssociations()) {
+        for (AssociationInfo association : getCdmAssociations(cdm)) {
             if (association.getPackageName().equals(callingPackage)
                     && !association.isSelfManaged() && device.getAddress() != null
                     && association.getDeviceMacAddress() != null
@@ -361,6 +364,21 @@ public final class Utils {
         }
         throw new SecurityException("The application with package name " + callingPackage
                 + " does not have a CDM association with the Bluetooth Device");
+    }
+
+    /**
+     * Obtains the complete list of registered CDM associations.
+     *
+     * @param cdm the CompanionDeviceManager object
+     * @return the list of AssociationInfo objects
+     */
+    @RequiresPermission("android.permission.MANAGE_COMPANION_DEVICES")
+    // CompanionDeviceManager#getAllAssociations() was made public in T,
+    // but existed in S as an identical SystemApi.
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    @SuppressLint("NewApi")
+    public static List<AssociationInfo> getCdmAssociations(CompanionDeviceManager cdm) {
+        return cdm.getAllAssociations();
     }
 
     /**
