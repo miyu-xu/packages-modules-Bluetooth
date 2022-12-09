338a69578b1d20466368dfd12d4507c787f05379
Aritra Sen <aritrasen@google.com>
2022-12-09 04:55:10 +0000

    Create a new topshim API for the BTIF hfp client (btif_hf_client).
    Bug: 262045500
    Test: mma -j $(nproc)
    Test: ./build.py
    Test: system/gd/cert/run --clean --topshim
    Tag: #floss
    Change-Id: I978aba3d5b0124c7562ab9b0ff6310b3f350e92b

--- system/gd/rust/topshim/build.rs
+++ system/gd/rust/topshim/build.rs
@@ -54,9 +54,9 @@ fn main() {
         .clang_args(clang_args)
         .enable_cxx_namespaces()
         .size_t_is_usize(true)
-        .allowlist_type("(bt_|bthh_|btgatt_|btsdp|bluetooth_sdp|btsock_).*")
+        .allowlist_type("(bt_|bthh_|btgatt_|btsdp|bluetooth_sdp|btsock_|bthf_).*")
         .allowlist_type("sock_connect_signal_t")
-        .allowlist_function("(bt_|bthh_|btgatt_|btsdp).*")
+        .allowlist_function("(bt_|bthh_|btgatt_|btsdp|bthf_).*")
         .allowlist_function("hal_util_.*")
         // We must opaque out std:: in order to prevent bindgen from choking
         .opaque_type("std::.*")
