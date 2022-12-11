e601b772194d151abbb19ca0ff145a78ddeaf966
Martin Geisler <mgeisler@google.com>
2022-12-11 14:42:52 +0100

    pdl: Handle simple scalar and enum arrays
    This implements support for integer and enum arrays with static sizes.
    Unsized or dynamically sized arrays are not yet supported.
    
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: Idc13ab75c5d6c4704f7edb70306be761ee815cab

---
+++ tools/pdl/tests/generated/packet_decl_8bit_scalar_array_big_endian_Foo.rs
@@ -0,0 +1,70 @@
+#[derive(Debug)]
+struct FooData {
+    x: [u8; 3],
+}
+#[derive(Debug, Clone)]
+pub struct FooPacket {
+    foo: Arc<FooData>,
+}
+#[derive(Debug)]
+pub struct FooBuilder {
+    pub x: [u8; 3],
+}
+impl FooData {
+    fn conforms(bytes: &[u8]) -> bool {
+        true
+    }
+    fn parse(mut bytes: &[u8]) -> Result<Self> {
+        let x = std::array::from_fn(|_| bytes.get_u8());
+        Ok(Self { x })
+    }
+    fn write_to(&self, buffer: &mut BytesMut) {
+        for elem in self.x {
+            buffer.put_u8(elem);
+        }
+    }
+    fn get_total_size(&self) -> usize {
+        self.get_size()
+    }
+    fn get_size(&self) -> usize {
+        0
+    }
+}
+impl Packet for FooPacket {
+    fn to_bytes(self) -> Bytes {
+        let mut buffer = BytesMut::with_capacity(self.foo.get_total_size());
+        self.foo.write_to(&mut buffer);
+        buffer.freeze()
+    }
+    fn to_vec(self) -> Vec<u8> {
+        self.to_bytes().to_vec()
+    }
+}
+impl From<FooPacket> for Bytes {
+    fn from(packet: FooPacket) -> Self {
+        packet.to_bytes()
+    }
+}
+impl From<FooPacket> for Vec<u8> {
+    fn from(packet: FooPacket) -> Self {
+        packet.to_vec()
+    }
+}
+impl FooPacket {
+    pub fn parse(mut bytes: &[u8]) -> Result<Self> {
+        Ok(Self::new(Arc::new(FooData::parse(bytes)?)).unwrap())
+    }
+    fn new(root: Arc<FooData>) -> std::result::Result<Self, &'static str> {
+        let foo = root;
+        Ok(Self { foo })
+    }
+    pub fn get_x(&self) -> &[u8; 3] {
+        &self.foo.as_ref().x
+    }
+}
+impl FooBuilder {
+    pub fn build(self) -> FooPacket {
+        let foo = Arc::new(FooData { x: self.x });
+        FooPacket::new(foo).unwrap()
+    }
+}
