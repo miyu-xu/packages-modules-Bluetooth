6a2ce601ae80401e2cdc4a50e2129b06954e293e
Patty Huang <plhuang@google.com>
2022-12-13 19:16:02 +0800

    Init LE Audio allow list mechanism
    Bug: 239768625
    Test: atest BluetoothInstrumentationTests
    Test: manual test
    Change-Id: I19dca62a16a5132db6d257c1e7a2683cb4687878

--- android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
+++ android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -318,6 +318,7 @@ final class RemoteDevices {
         private boolean mIsCoordinatedSetMember;
         private int mAshaCapability;
         private int mAshaTruncatedHiSyncId;
+        private String mModelName;
         @VisibleForTesting int mBondState;
         @VisibleForTesting int mDeviceType;
         @VisibleForTesting ParcelUuid[] mUuids;
@@ -673,6 +674,19 @@ final class RemoteDevices {
         public BluetoothAudioPolicy getHfAudioPolicyForRemoteAg() {
             return mAudioPolicy;
         }
+
+        public void setModelName(String modelName) {
+            mModelName = modelName;
+        }
+
+        /**
+         * @return the mModelName
+         */
+        String getModelName() {
+            synchronized (mObject) {
+                return mModelName;
+            }
+        }
     }
 
     private void sendUuidIntent(BluetoothDevice device, DeviceProperties prop) {
@@ -917,6 +931,10 @@ final class RemoteDevices {
                             break;
                         case AbstractionLayer.BT_PROPERTY_REMOTE_ASHA_TRUNCATED_HISYNCID:
                             deviceProperties.setAshaTruncatedHiSyncId(val[0]);
+                            break;
+                        case AbstractionLayer.BT_PROPERTY_REMOTE_MODEL_NUM:
+                            final String modelName = new String(val);
+                            deviceProperties.setModelName(modelName);
                             break;
                     }
                 }
