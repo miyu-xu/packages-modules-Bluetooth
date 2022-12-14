9c7def6609e4c394a154858c84219cb07dcc9009
howardchung <howardchung@google.com>
2022-12-14 07:53:30 +0000

    Floss: Implement commands for socket test schedule
    In qualification, tests in L2CAP/* require DUT to send data or
    verify the received data.
    Since the L2CAP connection can be disconnected due to a short time out,
    this CL implements some help schedules to automatically send and dump
    the data for the users to run qualification tests.
    
    Bug: 262813906
    Test: run L2CAP qualification tests locally
    Change-Id: Ia00953460959eb60ac86b81149235f87ea6bedcc

--- system/gd/rust/linux/client/src/callbacks.rs
+++ system/gd/rust/linux/client/src/callbacks.rs
@@ -1,3 +1,4 @@
+use crate::command_handler::SocketSchedule;
 use crate::dbus_iface::{
     export_admin_policy_callback_dbus_intf, export_advertising_set_callback_dbus_intf,
     export_bluetooth_callback_dbus_intf, export_bluetooth_connection_callback_dbus_intf,
@@ -29,7 +30,12 @@ use dbus::nonblock::SyncConnection;
 use dbus_crossroads::Crossroads;
 use dbus_projection::DisconnectWatcher;
 use manager_service::iface_bluetooth_manager::IBluetoothManagerCallback;
+use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+const SOCKET_TEST_WRITE: &[u8] = b"01234567890123456789";
 
 /// Callback context for manager interface callbacks.
 pub(crate) struct BtManagerCallback {
@@ -962,7 +968,6 @@ impl RPCProxy for BtGattServerCallback {
 pub(crate) struct BtSocketManagerCallback {
     objpath: String,
     context: Arc<Mutex<ClientContext>>,
-
     dbus_connection: Arc<SyncConnection>,
     dbus_crossroads: Arc<Mutex<Crossroads>>,
 }
@@ -975,6 +980,44 @@ impl BtSocketManagerCallback {
         dbus_crossroads: Arc<Mutex<Crossroads>>,
     ) -> Self {
         Self { objpath, context, dbus_connection, dbus_crossroads }
+    }
+
+    fn start_socket_schedule(&mut self, socket: BluetoothSocket) {
+        if let Some(s) = self.context.lock().unwrap().socket_test_schedule {
+            let SocketSchedule { num_frame, send_interval, disconnect_delay } = s;
+
+            match socket.fd {
+                Some(mut fd) => {
+                    thread::spawn(move || {
+                        for i in 0..num_frame {
+                            fd.write_all(SOCKET_TEST_WRITE);
+                            print_info!("data sent: {}", i + 1);
+                            thread::sleep(send_interval);
+                        }
+
+                        // dump any incoming data
+                        let mut d = Duration::from_millis(0);
+                        let interval = Duration::from_millis(100);
+                        while d <= disconnect_delay {
+                            let mut buf = [0; 128];
+                            let sz = fd.read(&mut buf).unwrap();
+                            let mut data = buf.to_vec();
+                            data.truncate(sz);
+                            if sz > 0 {
+                                print_info!("received {} bytes: {:?}", sz, data);
+                            }
+                            d += interval;
+                            thread::sleep(interval);
+                        }
+
+                        //|fd| is dropped automatically when the scope ends.
+                    });
+                }
+                None => {
+                    print_error!("incoming connection fd is None. Unable to send data");
+                }
+            }
+        }
     }
 }
 
@@ -1005,18 +1048,16 @@ impl IBluetoothSocketManagerCallbacks for BtSocketManagerCallback {
         let callback_id = self.context.lock().unwrap().socket_manager_callback_id.clone().unwrap();
 
         self.context.lock().unwrap().run_callback(Box::new(move |context| {
-            let status = context
-                .lock()
-                .unwrap()
-                .socket_manager_dbus
-                .as_mut()
-                .unwrap()
-                .close(callback_id, socket.id);
+            let status = context.lock().unwrap().socket_manager_dbus.as_mut().unwrap().accept(
+                callback_id,
+                socket.id,
+                None,
+            );
             if status != BtStatus::Success {
-                print_error!("Failed to close socket {}, status = {:?}", socket.id, status);
+                print_error!("Failed to accept socket {}, status = {:?}", socket.id, status);
                 return;
             }
-            print_info!("Requested for closing socket {}", socket.id);
+            print_info!("Requested for accepting socket {}", socket.id);
         }));
     }
 
@@ -1026,10 +1067,11 @@ impl IBluetoothSocketManagerCallbacks for BtSocketManagerCallback {
 
     fn on_handle_incoming_connection(
         &mut self,
-        _listener_id: SocketId,
-        _connection: BluetoothSocket,
+        listener_id: SocketId,
+        connection: BluetoothSocket,
     ) {
-        todo!();
+        print_info!("Socket {} connected", listener_id);
+        self.start_socket_schedule(connection);
     }
 
     fn on_outgoing_connection_result(
@@ -1040,6 +1082,7 @@ impl IBluetoothSocketManagerCallbacks for BtSocketManagerCallback {
     ) {
         if let Some(s) = socket {
             print_info!("Connection success on {}: {:?} for {}", connecting_id, result, s);
+            self.start_socket_schedule(s);
         } else {
             print_info!("Connection failed on {}: {:?}", connecting_id, result);
         }
