4521bd58c66421f8769b0b15cd162a064717602c
Rahul Sabnis <rahulsabnis@google.com>
2022-12-14 17:47:23 -0800

    Queue secure L2CAP connection requests while encryption is pending
    Tag: #feature
    Bug: 262171591
    Test: Manual
    Change-Id: Ia9bd56c7c5cb6db5b4a51989f42e064834958409

--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -991,7 +991,13 @@ tL2CAP_LE_RESULT_CODE btm_ble_start_sec_check(const RawAddress& bd_addr,
   }
 
   if (ble_sec_act == BTM_BLE_SEC_NONE) {
-    return result;
+    if (bluetooth::common::init_flags::queue_l2cap_coc_while_encrypting()) {
+      if (sec_act != BTM_SEC_ENC_PENDING) {
+        return result;
+      }
+    } else {
+      return result;
+    }
   }
 
   l2cble_update_sec_act(bd_addr, sec_act);
