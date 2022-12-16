2b776bb111be57a069a74ac7d015e6531bed52eb
jinyao.yu <jinyao.yu@mediatek.com>
2022-12-16 17:35:52 +0800

    Add A2DP src and sink co-exist feature init::flag (5/5)
    [Description]
    Add A2DP src and sink co-exist feature, that we can connect
    both sink and src remote device at the same time while only
    keep 1 streaming.
    
    Bug: 256938279
    Test: A2DP src/sink connect, streaming successully,
    net_test_btif_rc unit test pass
    
    Change-Id: I89ee8391f53daffaa992a5e7fafba36635a19645

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -31,6 +31,7 @@ mod ffi {
         fn set_min_encryption_is_enabled() -> bool;
         fn subrating_is_enabled() -> bool;
         fn trigger_advertising_callbacks_on_first_resume_after_pause_is_enabled() -> bool;
+        fn src_sink_coexit_is_enabled() -> bool;
     }
 }
 
