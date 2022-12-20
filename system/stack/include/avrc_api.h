3e0009a7db2f30f25447c0377783558e4591c045
jinyao.yu <jinyao.yu@mediatek.com>
2022-12-20 13:35:22 +0800

    Add A2DP src and sink co-exist feature stack layer (3/5)
    [Description]
    Add A2DP src and sink co-exist feature, that we can connect
    both sink and src remote device at the same time while only
    keep 1 streaming.
    
    Bug: 256938279
    Test: A2DP src/sink connect, streaming successully,
    net_test_btif_rc unit test pass
    
    Change-Id: I89ee7391253daffa8992a5e1fafba38635414641

--- system/stack/include/avrc_api.h
+++ system/stack/include/avrc_api.h
@@ -237,11 +237,11 @@ using tAVRC_MSG_CBACK = base::Callback<void(uint8_t handle, uint8_t label,
                                             uint8_t opcode, tAVRC_MSG* p_msg)>;
 
 typedef struct {
-  tAVRC_CTRL_CBACK ctrl_cback;    /* application control callback */
-  tAVRC_MSG_CBACK msg_cback;      /* application message callback */
-  uint32_t company_id;            /* the company ID  */
-  uint8_t conn;                   /* Connection role (Initiator/acceptor) */
-  uint8_t control;                /* Control role (Control/Target) */
+  tAVRC_CTRL_CBACK ctrl_cback; /* application control callback */
+  tAVRC_MSG_CBACK msg_cback;   /* application message callback */
+  uint32_t company_id;         /* the company ID  */
+  uint8_t conn;                /* Connection role (Initiator/acceptor) */
+  uint8_t control;             /* Control role (Control/Target) */
 } tAVRC_CONN_CB;
 
 typedef struct {
@@ -513,7 +513,7 @@ extern uint16_t AVRC_CloseBrowse(uint8_t handle);
  *
  *****************************************************************************/
 extern uint16_t AVRC_MsgReq(uint8_t handle, uint8_t label, uint8_t ctype,
-                            BT_HDR* p_pkt);
+                            BT_HDR* p_pkt, bool is_new_avrcp);
 
 /******************************************************************************
  *
@@ -842,5 +842,7 @@ extern bool AVRC_IsValidAvcType(uint8_t pdu_id, uint8_t avc_type);
  *
  ******************************************************************************/
 extern bool AVRC_IsValidPlayerAttr(uint8_t attr);
+
+extern void AVRC_UpdateCcb(RawAddress* addr, uint32_t company_id);
 
 #endif /* AVRC_API_H */
