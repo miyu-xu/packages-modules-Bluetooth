5e35ea2b6351c050929b588eb99e52e581c1a8af
Rahul Arya <aryarahul@google.com>
2022-12-20 22:29:06 +0000

    [Non-Discoverable Mode] Store discoverable bit in security record
    Thread it through the advertising manager -> connection manager -> shim
    -> legacy security.
    
    Followup CLs will actually do something with this bit.
    
    Bug: 254314964
    Test: compiles
    Change-Id: I5da4c60ba9a80fb950947be41e5eced7212740af

--- system/main/test/main_shim_test.cc
+++ system/main/test/main_shim_test.cc
@@ -146,7 +146,7 @@ void mock_connection_le_on_connected(
     const tBLE_BD_ADDR& address_with_type, uint16_t handle, tHCI_ROLE role,
     uint16_t conn_interval, uint16_t conn_latency, uint16_t conn_timeout,
     const RawAddress& local_rpa, const RawAddress& peer_rpa,
-    tBLE_ADDR_TYPE peer_addr_type) {}
+    tBLE_ADDR_TYPE peer_addr_type, bool is_discovering) {}
 void mock_connection_le_on_failed(const tBLE_BD_ADDR& address_with_type,
                                   uint16_t handle, bool enhanced,
                                   tHCI_STATUS status, bool locally_initiated) {}
