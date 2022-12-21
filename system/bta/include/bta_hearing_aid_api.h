ae46feea20e3da0086372ead231f1695c789b518
Aritra Sen <aritrasen@google.com>
2022-12-21 00:04:02 +0000

    Fix dangling pointer issues post Bluetooth shutdown cleanup for delayed task in main thread.
    Bug: 260332122
    Test: mma -j $(nproc)
    Change-Id: I0411becd603ccfac7e77ef45c33f2560b3b02ac8

--- system/bta/include/bta_hearing_aid_api.h
+++ system/bta/include/bta_hearing_aid_api.h
@@ -236,10 +236,10 @@ class HearingAid {
 
   static int GetDeviceCount();
 
-  virtual void Connect(const RawAddress& address) = 0;
-  virtual void Disconnect(const RawAddress& address) = 0;
-  virtual void AddToAcceptlist(const RawAddress& address) = 0;
-  virtual void SetVolume(int8_t volume) = 0;
+  static void Connect(const RawAddress& address);
+  static void Disconnect(const RawAddress& address);
+  static void AddToAcceptlist(const RawAddress& address);
+  static void SetVolume(int8_t volume);
 };
 
 /* Represents configuration of audio codec, as exchanged between hearing aid and
