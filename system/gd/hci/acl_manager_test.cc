474f3d0fb3a89d0fe195c18548b523a7be370cf0
Chienyuan Huang <chienyuanhuang@google.com>
2022-12-22 07:06:26 +0000

    [Test] fix flaky
    Change-Id: Id33f79e05abefb659f3105b0403e572e9a4d7735

--- system/gd/hci/acl_manager_test.cc
+++ system/gd/hci/acl_manager_test.cc
@@ -16,13 +16,14 @@
 
 #include "hci/acl_manager.h"
 
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
 #include <algorithm>
 #include <chrono>
 #include <future>
 #include <map>
-
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#include <mutex>
 
 #include "common/bind.h"
 #include "hci/address.h"
@@ -131,6 +132,7 @@ class TestHciLayer : public HciLayer {
   void EnqueueCommand(
       std::unique_ptr<CommandBuilder> command,
       common::ContextualOnceCallback<void(CommandStatusView)> on_status) override {
+    std::lock_guard<std::mutex> lock(mutex_);
     command_queue_.push(std::move(command));
     command_status_callbacks.push_back(std::move(on_status));
     if (command_promise_ != nullptr) {
@@ -142,6 +144,7 @@ class TestHciLayer : public HciLayer {
   void EnqueueCommand(
       std::unique_ptr<CommandBuilder> command,
       common::ContextualOnceCallback<void(CommandCompleteView)> on_complete) override {
+    std::lock_guard<std::mutex> lock(mutex_);
     command_queue_.push(std::move(command));
     command_complete_callbacks.push_back(std::move(on_complete));
     if (command_promise_ != nullptr) {
@@ -157,7 +160,7 @@ class TestHciLayer : public HciLayer {
   }
 
   CommandView GetLastCommand() {
-    if (command_queue_.size() == 0) {
+    if (command_queue_.empty()) {
       return CommandView::Create(PacketView<kLittleEndian>(std::make_shared<std::vector<uint8_t>>()));
     }
     auto last = std::move(command_queue_.front());
@@ -174,6 +177,7 @@ class TestHciLayer : public HciLayer {
       return ConnectionManagementCommandView::Create(AclCommandView::Create(
           CommandView::Create(PacketView<kLittleEndian>(std::make_shared<std::vector<uint8_t>>()))));
     }
+    std::lock_guard<std::mutex> lock(mutex_);
     CommandView command_packet_view = GetLastCommand();
     ConnectionManagementCommandView command =
         ConnectionManagementCommandView::Create(AclCommandView::Create(command_packet_view));
@@ -191,6 +195,7 @@ class TestHciLayer : public HciLayer {
       auto result = command_future_->wait_for(std::chrono::milliseconds(1000));
       EXPECT_NE(std::future_status::timeout, result);
     }
+    std::lock_guard<std::mutex> lock(mutex_);
     if (command_queue_.empty()) {
       return ConnectionManagementCommandView::Create(AclCommandView::Create(
           CommandView::Create(PacketView<kLittleEndian>(std::make_shared<std::vector<uint8_t>>()))));
@@ -314,6 +319,7 @@ class TestHciLayer : public HciLayer {
   std::queue<std::unique_ptr<CommandBuilder>> command_queue_;
   std::unique_ptr<std::promise<void>> command_promise_;
   std::unique_ptr<std::future<void>> command_future_;
+  mutable std::mutex mutex_;
 
   void do_disconnect(uint16_t handle, ErrorCode reason) {
     HciLayer::Disconnect(handle, reason);
@@ -767,7 +773,6 @@ TEST_F(AclManagerTest, cancel_le_connection) {
   ASSERT_NO_FATAL_FAILURE(test_hci_layer_->SetCommandFuture());
   test_hci_layer_->GetLastCommand(OpCode::LE_CREATE_CONNECTION);
   test_hci_layer_->IncomingEvent(LeCreateConnectionStatusBuilder::Create(ErrorCode::SUCCESS, 0x01));
-
   ASSERT_NO_FATAL_FAILURE(test_hci_layer_->SetCommandFuture());
   acl_manager_->CancelLeConnect(remote_with_type);
   auto packet = test_hci_layer_->GetLastCommand(OpCode::LE_CREATE_CONNECTION_CANCEL);
@@ -787,7 +792,6 @@ TEST_F(AclManagerTest, cancel_le_connection) {
       0x0010,
       0x0011,
       ClockAccuracy::PPM_30));
-
   ASSERT_NO_FATAL_FAILURE(test_hci_layer_->SetCommandFuture());
   packet = test_hci_layer_->GetLastCommand(OpCode::LE_REMOVE_DEVICE_FROM_FILTER_ACCEPT_LIST);
   le_connection_management_command_view = LeConnectionManagementCommandView::Create(AclCommandView::Create(packet));
