f3ef9bf3d03ec44be8574b52a7bd8efad2f53ac4
Rahul Arya <aryarahul@google.com>
2022-12-29 02:38:11 +0000

    Rust PDL backend
    This is a zero-copy Rust backend that supports most of PDL (excluding
    groups, checksums, custom types), BUT only little-endian packets.
    
    This is needed for Rust-GATT (scoped for U). It is not a permanent
    solution, I expect to unify / replace this with the official Rust PDL
    once it is ready, but until that point arrives I don't want to remain
    blocked.
    
    Bug: 255880936
    Test: unit
    
    Change-Id: I7d2305c20288d576aa1a5731b0d7e54727366947

---
+++ tools/pdl/src/backends/rust_no_allocation/preamble.rs
@@ -0,0 +1,142 @@
+// We inherit casing from the PDL file
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(warnings, missing_docs)]
+#![allow(clippy::all)]
+// this is now stable
+#![feature(mixed_integer_ops)]
+
+use std::ops::Deref;
+
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidEnumValue,
+    DivisionFailure,
+    ArithmeticOverflow,
+    OutOfBoundsAccess,
+    MisalignedPayload,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct BitSlice<'a> {
+    // note: the offsets are ENTIRELY UNRELATED to the size of this struct,
+    // so indexing needs to be checked to avoid panics
+    backing: &'a [u8],
+
+    // invariant: end_bit_offset >= start_bit_offset, so subtraction will NEVER wrap
+    start_bit_offset: usize,
+    end_bit_offset: usize,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct SizedBitSlice<'a>(BitSlice<'a>);
+
+impl<'a> BitSlice<'a> {
+    pub fn offset(&self, offset: usize) -> Result<BitSlice<'a>, ParseError> {
+        if self.end_bit_offset - self.start_bit_offset < offset {
+            return Err(ParseError::OutOfBoundsAccess);
+        }
+        Ok(Self {
+            backing: self.backing,
+            start_bit_offset: self
+                .start_bit_offset
+                .checked_add(offset)
+                .ok_or(ParseError::ArithmeticOverflow)?,
+            end_bit_offset: self.end_bit_offset,
+        })
+    }
+
+    pub fn slice(&self, len: usize) -> Result<SizedBitSlice<'a>, ParseError> {
+        if self.end_bit_offset - self.start_bit_offset < len {
+            return Err(ParseError::OutOfBoundsAccess);
+        }
+        Ok(SizedBitSlice(Self {
+            backing: self.backing,
+            start_bit_offset: self.start_bit_offset,
+            end_bit_offset: self
+                .start_bit_offset
+                .checked_add(len)
+                .ok_or(ParseError::ArithmeticOverflow)?,
+        }))
+    }
+
+    fn byte_at(&self, index: usize) -> Result<u8, ParseError> {
+        self.backing
+            .get(index)
+            .ok_or_else(|| panic!("eek (index={index}, backing={:?})", self.backing))
+            .copied()
+    }
+}
+
+impl<'a> Deref for SizedBitSlice<'a> {
+    type Target = BitSlice<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a> From<SizedBitSlice<'a>> for BitSlice<'a> {
+    fn from(x: SizedBitSlice<'a>) -> Self {
+        *x
+    }
+}
+
+impl<'a, 'b> From<&'b [u8]> for SizedBitSlice<'a>
+where
+    'b: 'a,
+{
+    fn from(backing: &'a [u8]) -> Self {
+        Self(BitSlice { backing, start_bit_offset: 0, end_bit_offset: backing.len() * 8 })
+    }
+}
+
+impl<'a> SizedBitSlice<'a> {
+    pub fn try_parse(&self) -> Result<u64, ParseError> {
+        if self.end_bit_offset < self.start_bit_offset {
+            return Err(ParseError::OutOfBoundsAccess);
+        }
+        let size_in_bits = self.end_bit_offset - self.start_bit_offset;
+
+        // fields that fit into a u64 don't need to be byte-aligned
+        if size_in_bits <= 64 {
+            let mut accumulator = 0u64;
+
+            // where we are in our accumulation
+            let mut curr_byte_index = self.start_bit_offset / 8;
+            let mut curr_bit_offset = self.start_bit_offset % 8;
+            let mut remaining_bits = size_in_bits;
+
+            while remaining_bits > 0 {
+                // how many bits to take from the current byte?
+                // check if this is the last byte
+                if curr_bit_offset + remaining_bits <= 8 {
+                    let tmp = ((self.byte_at(curr_byte_index)? >> curr_bit_offset) as u64)
+                        & ((1u64 << remaining_bits) - 1);
+                    accumulator += tmp << (size_in_bits - remaining_bits);
+                    break;
+                } else {
+                    // this is not the last byte, so we have 8 - curr_bit_offset bits to
+                    // consume in this byte
+                    let bits_to_consume = 8 - curr_bit_offset;
+                    let tmp = (self.byte_at(curr_byte_index)? >> curr_bit_offset) as u64;
+                    accumulator += tmp << (size_in_bits - remaining_bits);
+                    curr_bit_offset = 0;
+                    curr_byte_index += 1;
+                    remaining_bits -= bits_to_consume as usize;
+                }
+            }
+            println!(
+                "reading value {accumulator} out of buffer {:?} [start={}, end={}]",
+                self.backing, self.start_bit_offset, self.end_bit_offset
+            );
+            Ok(accumulator)
+        } else {
+            return Err(ParseError::MisalignedPayload);
+        }
+    }
+
+    pub fn get_size_in_bits(&self) -> usize {
+        self.end_bit_offset - self.start_bit_offset
+    }
+}
