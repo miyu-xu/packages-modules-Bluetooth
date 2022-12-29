5861ae094b476ec4002522f71479d1d1ec9b8169
Santiago Seifert <aquilescanta@google.com>
2022-12-29 17:01:19 +0000

    Make close part of the BluetoothProfile interface
    Enforces implementations to implement close, and clients
    to call close to release resources.
    
    Test: Pending
    Bug: 205124386
    Change-Id: Icfe17256c17519540817867635d3acb04362c1b0

--- framework/java/android/bluetooth/BluetoothA2dp.java
+++ framework/java/android/bluetooth/BluetoothA2dp.java
@@ -47,7 +47,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-
 /**
  * This class provides the public APIs to control the Bluetooth A2DP
  * profile.
@@ -291,7 +290,8 @@ public final class BluetoothA2dp implements BluetoothProfile {
     }
 
     @UnsupportedAppUsage
-    /*package*/ void close() {
+    @Override
+    public void close() {
         mProfileConnector.disconnect();
     }
 
