3d74f260f96fa7937bca845ce005b454e413cbad
William Escande <wescande@google.com>
2023-01-03 19:26:12 +0000

    BluetoothManagerService test
    Test: atest ServiceBluetoothTests
    Bug: 262605980
    Change-Id: I6706588d4cb4667870513252791dc0d268185a30

--- service/tests/src/com/android/server/bluetooth/BluetoothManagerServiceTest.java
+++ service/tests/src/com/android/server/bluetooth/BluetoothManagerServiceTest.java
@@ -16,8 +16,14 @@
 
 package com.android.server.bluetooth;
 
+import static com.android.server.bluetooth.BluetoothManagerService.MESSAGE_ENABLE;
+import static com.android.server.bluetooth.BluetoothManagerService.MESSAGE_TIMEOUT_BIND;
+
+import static com.google.common.truth.Truth.assertThat;
+
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -27,6 +33,8 @@ import static org.mockito.Mockito.verify;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -39,26 +47,36 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 @RunWith(AndroidJUnit4.class)
 public class BluetoothManagerServiceTest {
-    static int sTimeout = 3000;
+    static final int TIMEOUT = 3000;
     BluetoothManagerService mManagerService;
-    Context mContext;
-    @Mock
-    BluetoothServerProxy mBluetoothServerProxy;
-    @Mock
-    BluetoothManagerService.BluetoothHandler mHandler;
-    HandlerThread mHandlerThread;
+
+    Context mContext =
+            spy(
+                    new ContextWrapper(
+                            InstrumentationRegistry.getInstrumentation().getTargetContext()));
+
+    @Spy BluetoothServerProxy mBluetoothServerProxy;
+    HandlerThread mHandlerThread = new HandlerThread("BluetoothManagerServiceTest");
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        mContext = spy(new ContextWrapper(
-                InstrumentationRegistry.getInstrumentation().getTargetContext()));
-        mHandlerThread = new HandlerThread("BluetoothManagerServiceTest");
+
+        BluetoothServerProxy.setInstanceForTesting(mBluetoothServerProxy);
+
+        doReturn(mHandlerThread).when(mBluetoothServerProxy).createHandlerThread(any());
+        // Mock these functions so security errors won't throw
+        doReturn("name")
+                .when(mBluetoothServerProxy)
+                .settingsSecureGetString(any(), eq(Settings.Secure.BLUETOOTH_NAME));
+        doReturn("00:11:22:33:44:55").when(mBluetoothServerProxy).settingsSecureGetString(any(),
+                eq(Settings.Secure.BLUETOOTH_ADDRESS));
+
     }
 
     @After
@@ -71,11 +89,6 @@ public class BluetoothManagerServiceTest {
             throws InterruptedException {
         doReturn(mock(Intent.class)).when(mContext).registerReceiverForAllUsers(any(), any(),
                 eq(null), eq(null));
-        BluetoothServerProxy.setInstanceForTesting(mBluetoothServerProxy);
-        // Mock the handler to avoid handle message & to terminate the thread after
-        // test
-        doReturn(mHandlerThread).when(mBluetoothServerProxy).createHandlerThread(any());
-        doReturn(mHandler).when(mBluetoothServerProxy).newBluetoothHandler(any());
 
         // Mock these functions so security errors won't throw
         doReturn("name").when(mBluetoothServerProxy).settingsSecureGetString(any(),
@@ -97,12 +110,64 @@ public class BluetoothManagerServiceTest {
 
         // test run on user -1, should not turning Bluetooth off
         mManagerService.onUserRestrictionsChanged(UserHandle.CURRENT);
-        verify(mBluetoothServerProxy, timeout(sTimeout).times(0)).handlerSendWhatMessage(mHandler,
-                BluetoothManagerService.MESSAGE_DISABLE);
+        verify(mBluetoothServerProxy, timeout(TIMEOUT).times(0))
+                .handlerSendWhatMessage(
+                        any(BluetoothManagerService.BluetoothHandler.class),
+                        eq(BluetoothManagerService.MESSAGE_DISABLE));
 
         // called from SYSTEM user, should try to toggle Bluetooth off
         mManagerService.onUserRestrictionsChanged(UserHandle.SYSTEM);
-        verify(mBluetoothServerProxy, timeout(sTimeout)).handlerSendWhatMessage(mHandler,
-                BluetoothManagerService.MESSAGE_DISABLE);
+        verify(mBluetoothServerProxy, timeout(TIMEOUT))
+                .handlerSendWhatMessage(
+                        any(BluetoothManagerService.BluetoothHandler.class),
+                        eq(BluetoothManagerService.MESSAGE_DISABLE));
+    }
+
+    @Test
+    public void bindFailed() throws Exception {
+        InstrumentationRegistry.getInstrumentation()
+                .getUiAutomation()
+                .adoptShellPermissionIdentity();
+        BluetoothManagerService service = new BluetoothManagerService(mContext);
+        Handler handler = service.mHandler;
+
+        doReturn(false)
+                .when(mContext)
+                .bindServiceAsUser(
+                        any(Intent.class),
+                        any(ServiceConnection.class),
+                        anyInt(),
+                        any(UserHandle.class));
+        handler.handleMessage(handler.obtainMessage(MESSAGE_ENABLE));
+        // assertThat(service.mEnable).isFalse(); // TODO(b/280518177): Cleanup managerService state
+        assertThat(service.mBinding).isFalse();
+        assertThat(handler.hasMessages(MESSAGE_TIMEOUT_BIND)).isFalse();
+    }
+
+    @Test
+    public void bindTimeout() throws Exception {
+        InstrumentationRegistry.getInstrumentation()
+                .getUiAutomation()
+                .adoptShellPermissionIdentity();
+        BluetoothManagerService service = new BluetoothManagerService(mContext);
+        Handler handler = service.mHandler;
+
+        doReturn(true)
+                .when(mContext)
+                .bindServiceAsUser(
+                        any(Intent.class),
+                        any(ServiceConnection.class),
+                        anyInt(),
+                        any(UserHandle.class));
+        handler.handleMessage(handler.obtainMessage(MESSAGE_ENABLE));
+        assertThat(service.mEnable).isTrue();
+        assertThat(service.mBinding.isTrue());
+        assertThat(handler.hasMessages(MESSAGE_TIMEOUT_BIND).isTrue());
+        // Force handling the message now without waiting for the timeout to fire
+        handler.removeMessages(MESSAGE_TIMEOUT_BIND);
+        handler.handleMessage(handler.obtainMessage(MESSAGE_TIMEOUT_BIND));
+
+        assertThat(service.mBinding).isFalse();
+        // assertThat(service.mEnable).isFalse(); // TODO(b/280518177): Cleanup managerService state
     }
 }
