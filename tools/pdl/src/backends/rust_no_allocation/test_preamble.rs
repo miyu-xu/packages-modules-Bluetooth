4ca5daa09c74c7bee0c7178183a4a1b24e107b4f
Rahul Arya <aryarahul@google.com>
2023-01-10 02:34:49 +0000

    Rust PDL backend
    This is a zero-copy Rust backend that supports most of PDL (excluding
    groups, checksums, custom types), BUT only little-endian packets.
    
    This is needed for Rust-GATT (scoped for U). It is not a permanent
    solution, I expect to unify / replace this with the official Rust PDL
    once it is ready, but until that point arrives I don't want to remain
    blocked.
    
    Bug: 255880936
    Test: unit
    
    Change-Id: I7d2305c20288d576aa1a5731b0d7e54727366947

---
+++ tools/pdl/src/backends/rust_no_allocation/test_preamble.rs
@@ -0,0 +1,23 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(warnings, missing_docs)]
+#![allow(clippy::all)]
+
+use pdl_rust_noalloc_le_test_backend::*;
+
+fn hex_to_word(hex: u8) -> u8 {
+    if b'0' <= hex && hex <= b'9' {
+        hex - b'0'
+    } else if b'A' <= hex && hex <= b'F' {
+        hex - b'A' + 0xa
+    } else {
+        hex - b'a' + 0xa
+    }
+}
+
+fn hex_to_byte_string(hex: &str) -> Vec<u8> {
+    hex.as_bytes()
+        .chunks_exact(2)
+        .map(|chunk| hex_to_word(chunk[1]) + (hex_to_word(chunk[0]) << 4))
+        .collect()
+}
