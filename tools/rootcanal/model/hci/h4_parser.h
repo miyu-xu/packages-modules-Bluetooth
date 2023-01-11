c5925259b324c962f3bc0ccacb5f906d34d18a5a
Henri Chataing <henrichataing@google.com>
2023-01-11 13:11:08 +0000

    RootCanal H4: Add recovery state
    The recovery state is entered when an invalid Idc
    is received. In recovery state, all bytes are dropped
    until a valid HCI Reset command is received.
    The reasoning is that this issue occurs when the emulator
    is rebooted, leaving bytes in the VirtIO console.
    The next valid command should be HCI Reset command then.
    
    Test: atest rootcanal_test_host
    Bug: 261261502
    Change-Id: I6e5482d9b2642fa789e6ed415ce3833663097bff

--- tools/rootcanal/model/hci/h4_parser.h
+++ tools/rootcanal/model/hci/h4_parser.h
@@ -45,14 +45,14 @@ using ClientDisconnectCallback = std::function<void()>;
 // The parser keeps internal state and is not thread safe.
 class H4Parser {
  public:
-  enum State { HCI_TYPE, HCI_PREAMBLE, HCI_PAYLOAD };
+  enum State { HCI_TYPE, HCI_PREAMBLE, HCI_PAYLOAD, HCI_RECOVERY };
 
   H4Parser(PacketReadCallback command_cb, PacketReadCallback event_cb,
            PacketReadCallback acl_cb, PacketReadCallback sco_cb,
-           PacketReadCallback iso_cb);
+           PacketReadCallback iso_cb, bool enable_recovery_state = false);
 
   // Consumes the given number of bytes, returns true on success.
-  bool Consume(uint8_t* buffer, int32_t bytes);
+  bool Consume(const uint8_t* buffer, int32_t bytes);
 
   // The maximum number of bytes the parser can consume in the current state.
   size_t BytesRequested();
@@ -61,6 +61,9 @@ class H4Parser {
   void Reset();
 
   State CurrentState() { return state_; };
+
+  void EnableRecovery() { enable_recovery_state_ = true; }
+  void DisableRecovery() { enable_recovery_state_ = false; }
 
  private:
   void OnPacketReady();
@@ -101,13 +104,28 @@ class H4Parser {
   uint8_t packet_type_{};
   std::vector<uint8_t> packet_;
   size_t bytes_wanted_{0};
+  bool enable_recovery_state_{false};
 };
 
 inline std::ostream& operator<<(std::ostream& os,
                                 H4Parser::State const& state_) {
-  os << (state_ == H4Parser::State::HCI_TYPE       ? "HCI_TYPE"
-         : state_ == H4Parser::State::HCI_PREAMBLE ? "HCI_PREAMBLE"
-                                                   : "HCI_PAYLOAD");
+  switch (state_) {
+    case H4Parser::State::HCI_TYPE:
+      os << "HCI_TYPE";
+      break;
+    case H4Parser::State::HCI_PREAMBLE:
+      os << "HCI_PREAMBLE";
+      break;
+    case H4Parser::State::HCI_PAYLOAD:
+      os << "HCI_PAYLOAD";
+      break;
+    case H4Parser::State::HCI_RECOVERY:
+      os << "HCI_RECOVERY";
+      break;
+    default:
+      os << "unknown state " << static_cast<int>(state_);
+      break;
+  }
   return os;
 }
 
