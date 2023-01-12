ec9c96c5846d75b1c28dff9288cd6782723abc4c
Can Chen <can.chen@mediatek.com>
2023-01-12 10:56:39 +0800

    Fix a2dp sink no audio output
    [Root Cause]
    No audio output after wired headphone being plugged in or out
    while bluetooth headphone being connected and streaming.
    
    [Solution]
    Register audio error callback for wired headphone being plugged in or out
    
    Bug: 270271436
    Test: a2dp pass when wired headphone being plugged in or out
    
    Change-Id: I55ac52ffef3e9db6a47ab2a38b33722df8c5b698

--- system/btif/include/btif_a2dp_sink.h
+++ system/btif/include/btif_a2dp_sink.h
@@ -138,4 +138,8 @@ void btif_a2dp_sink_set_focus_state_req(btif_a2dp_sink_focus_state_t state);
 // |gain| is the audio track gain value to use.
 void btif_a2dp_sink_set_audio_track_gain(float gain);
 
+/*M: Get audio track handle for avrcp track operations. @{ */
+void * btif_a2dp_sink_get_audio_track();
+/** @} */
+
 #endif /* BTIF_A2DP_SINK_H */
