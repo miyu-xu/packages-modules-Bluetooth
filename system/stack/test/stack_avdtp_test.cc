6e1decb4a56aff0f45cfbfd6428d541c0a07eabe
William Escande <wescande@google.com>
2023-01-11 21:59:00 -0800

    Mock static initialization order 02
    cpp static order initialization is not guarantee.
    In order to avoid the mess, we need to have a lazy init schema.
    Remove extra declaration of map to the common mock declaration
    
    Bug: 265217208
    Test: atest --host
    Change-Id: Ic95c034968fa3699f169ce92d727df1012e01789

--- system/stack/test/stack_avdtp_test.cc
+++ system/stack/test/stack_avdtp_test.cc
@@ -21,6 +21,7 @@
 #include "stack/avdt/avdt_int.h"
 #include "stack/include/avdt_api.h"
 #include "stack/test/common/mock_stack_avdt_msg.h"
+#include "test/common/mock_functions.h"
 #include "types/raw_address.h"
 
 #ifndef UNUSED_ATTR
@@ -31,9 +32,6 @@
 uint8_t appl_trace_level = BT_TRACE_LEVEL_VERBOSE;
 
 void LogMsg(uint32_t trace_set_mask, const char* fmt_str, ...) { }
-
-// All mock requires this symbol to count calling times
-std::map<std::string, int> mock_function_count_map;
 
 class StackAvdtpTest : public ::testing::Test {
  protected:
