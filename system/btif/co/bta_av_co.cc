2c56dcb20f44dc24d53b6bce435121b92a631913
Can Chen <can.chen@mediatek.com>
2023-01-12 11:19:48 +0800

    BT NE when codec dynamic switch
    [Root Cause]
    1. Codec dynamic switch will make AVRCP close
    2. AVRCP close will trigger set kEmpty addr to active peer
    3. step 2 will cause codec config being formatted into 0
    
    [Solution]
    save new codec when decoder
    
    Bug: 270271435
    Test: a2dp switch codec pass
    
    Change-Id: Ic4acaf09cca4193cb3d243535a0fc3f5c62701f6

--- system/btif/co/bta_av_co.cc
+++ system/btif/co/bta_av_co.cc
@@ -388,6 +388,15 @@ class BtaAvCo {
    */
   bool SetActivePeer(const RawAddress& peer_address);
 
+  /**M: @{save new codec config for codec dynamic switch  */
+  /**
+   * Save the reconfig codec
+   *
+   * @param new_codec_config the new codec config
+   */
+   void SaveCodec(const uint8_t* new_codec_config);
+   /** @} */
+
   /**
    * Get the encoder parameters for a peer.
    *
@@ -1463,6 +1472,12 @@ bool BtaAvCo::SetActivePeer(const RawAddress& peer_address) {
   return true;
 }
 
+/**M: @{save new codec config for codec dynamic switch  */
+void BtaAvCo::SaveCodec(const uint8_t* new_codec_config) {
+  memcpy(codec_config_, new_codec_config, sizeof(codec_config_));
+}
+/** @} */
+
 void BtaAvCo::GetPeerEncoderParameters(
     const RawAddress& peer_address,
     tA2DP_ENCODER_INIT_PEER_PARAMS* p_peer_params) {
@@ -2244,6 +2259,12 @@ void bta_av_co_audio_update_mtu(tBTA_AV_HNDL bta_av_handle,
 bool bta_av_co_set_active_peer(const RawAddress& peer_address) {
   return bta_av_co_cb.SetActivePeer(peer_address);
 }
+
+/**M: @{save new codec config for codec dynamic switch  */
+void bta_av_co_save_codec(const uint8_t* new_codec_config) {
+  return bta_av_co_cb.SaveCodec(new_codec_config);
+}
+/**@}*/
 
 void bta_av_co_get_peer_params(const RawAddress& peer_address,
                                tA2DP_ENCODER_INIT_PEER_PARAMS* p_peer_params) {
