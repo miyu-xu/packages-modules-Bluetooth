39c87bbcaf39f84ba6318644fe2b0574d7e738fe
Can Chen <can.chen@mediatek.com>
2023-01-12 15:23:18 +0800

    Fix a2dp sink SUSPEND issue
    [Root Cause]
    Per 20ms, sink timer will call DoInThread
      to run btif_a2dp_sink_avk_handle_timer to get data from queue.
    btif_a2dp_sink_avk_handle_timer will not return
      if data queue is not empty.
    If system has performance issue, data queue will keep not empty,
      btif_a2dp_sink_avk_handle_timer can't return,
      work thread will always run btif_a2dp_sink_avk_handle_timer.
    But the sink timer will call DoInThread continuous.
      These are some btif_a2dp_sink_avk_handle_timer wait to be executed.
    If the codec is AAC and AVDTP_SUSPEND -> AVDTP_START now,
      aac_decodec_cleanup will be called when AVDTP_SUSPEND,
      peer will send a2dp media after AVDTP_START,
      DUT will store until 5 frames received, then DUT will init codec.
    The previous btif_a2dp_sink_avk_handle_timer will run and
      use aac_handle before codec init,
      it will cause NULL pointer crash
    
    [Solution]
    If btif_a2dp_sink_avk_handle_timer is running,
      not create new timer
    
    Bug: 269703378
    Test: atest net_test_btif_stack pass
    
    Change-Id: I54b8422ff98ce4989b4327379b9e853bc382ba4e

--- system/btif/src/btif_a2dp_sink.cc
+++ system/btif/src/btif_a2dp_sink.cc
@@ -129,6 +129,8 @@ class BtifA2dpSinkControlBlock {
 // Mutex for below data structures.
 static std::mutex g_mutex;
 
+static volatile int timer_running = 0;
+
 static BtifA2dpSinkControlBlock btif_a2dp_sink_cb("bt_a2dp_sink_worker_thread");
 
 static std::atomic<int> btif_a2dp_sink_state{BTIF_A2DP_SINK_STATE_OFF};
@@ -213,6 +215,10 @@ static void btif_a2dp_sink_init_delayed() {
 
 bool btif_a2dp_sink_startup() {
   LOG_INFO("%s", __func__);
+  {
+    LockGuard lock(g_mutex);
+    timer_running = 0;
+  }
   btif_a2dp_sink_cb.worker_thread.DoInThread(
       FROM_HERE, base::BindOnce(btif_a2dp_sink_startup_delayed));
   return true;
@@ -487,7 +493,14 @@ static void btif_a2dp_sink_audio_handle_stop_decoding() {
 }
 
 static void btif_decode_alarm_cb(UNUSED_ATTR void* context) {
-  LockGuard lock(g_mutex);
+  {
+    LockGuard lock(g_mutex);
+    if (timer_running == 1) {
+      APPL_TRACE_WARNING("%s: sink timer is running, skip", __func__);
+      return;
+    }
+    timer_running = 1;
+  }
   btif_a2dp_sink_cb.worker_thread.DoInThread(
       FROM_HERE, base::BindOnce(btif_a2dp_sink_avk_handle_timer));
 }
@@ -549,6 +562,7 @@ static void btif_a2dp_sink_avk_handle_timer() {
   BT_HDR* p_msg;
   if (fixed_queue_is_empty(btif_a2dp_sink_cb.rx_audio_queue)) {
     APPL_TRACE_DEBUG("%s: empty queue", __func__);
+    timer_running = 0;
     return;
   }
 
@@ -556,11 +570,13 @@ static void btif_a2dp_sink_avk_handle_timer() {
   if (btif_a2dp_sink_cb.rx_focus_state == BTIF_A2DP_SINK_FOCUS_NOT_GRANTED) {
     APPL_TRACE_DEBUG("%s: skipping frames since focus is not present",
                      __func__);
+    timer_running = 0;
     return;
   }
   /* Play only in BTIF_A2DP_SINK_FOCUS_GRANTED case */
   if (btif_a2dp_sink_cb.rx_flush) {
     fixed_queue_flush(btif_a2dp_sink_cb.rx_audio_queue, osi_free);
+    timer_running = 0;
     return;
   }
 
@@ -568,6 +584,7 @@ static void btif_a2dp_sink_avk_handle_timer() {
   while (true) {
     p_msg = (BT_HDR*)fixed_queue_try_dequeue(btif_a2dp_sink_cb.rx_audio_queue);
     if (p_msg == NULL) {
+      timer_running = 0;
       break;
     }
     APPL_TRACE_DEBUG("%s: number of packets in queue %zu", __func__,
