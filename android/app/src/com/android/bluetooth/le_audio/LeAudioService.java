8c8956302d70436a5d7588a864159316b5f129f0
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2023-01-13 12:07:51 +0000

    Remove dead intent related code for LeAudio
    This removes currently not used intent object from native evenet
    handler.
    
    Tag: #feature
    Bug: 261362580
    Test: atest LeAudioServiceTest
    Change-Id: I3cd2a83910c1f55ef29ce8c828914f56893d3316

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1282,7 +1282,6 @@ public class LeAudioService extends ProfileService {
     void messageFromNative(LeAudioStackEvent stackEvent) {
         Log.d(TAG, "Message from native: " + stackEvent);
         BluetoothDevice device = stackEvent.device;
-        Intent intent = null;
 
         if (stackEvent.type == LeAudioStackEvent.EVENT_TYPE_CONNECTION_STATE_CHANGED) {
             // Some events require device state machine
@@ -1555,10 +1554,6 @@ public class LeAudioService extends ProfileService {
                 Pair<Integer, Integer> ccidInformation = entry.getValue();
                 setCcidInformation(userUuid, ccidInformation.first, ccidInformation.second);
             }
-        }
-
-        if (intent != null) {
-            sendBroadcast(intent, BLUETOOTH_CONNECT);
         }
     }
 
