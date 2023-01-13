e7d7a10a0518592a97aa05fb40b7efb451910776
Martin Geisler <mgeisler@google.com>
2023-01-13 11:16:49 +0100

    pdl: Add support for reserved fields
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: Ie09c2fa09694f940e8584d56df3beb5019f91cf3

--- tools/pdl/src/backends/rust/parser.rs
+++ tools/pdl/src/backends/rust/parser.rs
@@ -128,6 +128,10 @@ impl<'a> FieldParser<'a> {
                         let #id = #type_id::#from_u(#v).unwrap();
                     }
                 }
+                ast::Field::Reserved { .. } => {
+                    // Nothing to do here.
+                    quote! {}
+                }
                 _ => todo!(),
             });
         }
