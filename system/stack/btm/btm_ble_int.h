ee505a5eba45ebe7d1ffdb146f34e24c767cca9b
Chris Manton <cmanton@google.com>
2023-01-13 17:00:38 -0800

    Stat-ify stack::btm::ble_gap::btm_ble_start-stop_scan
    Bug: 265486271
    Tag: #refactor
    Test: gd/cert/run
    Change-Id: Ibf5fca673d4ac8358cf5659d15a5ddea5a249e7c

--- system/stack/btm/btm_ble_int.h
+++ system/stack/btm/btm_ble_int.h
@@ -43,7 +43,6 @@ extern void btm_send_hci_set_scan_params(uint8_t scan_type, uint16_t scan_int,
                                          uint16_t scan_win,
                                          tBLE_ADDR_TYPE addr_type_own,
                                          uint8_t scan_filter_policy);
-extern void btm_ble_stop_scan(void);
 extern void btm_clear_all_pending_le_entry(void);
 
 extern void btm_ble_init(void);
@@ -54,10 +53,6 @@ extern void btm_ble_connected(const RawAddress& bda, uint16_t handle,
 extern void btm_ble_connected_from_address_with_type(
     const tBLE_BD_ADDR& address_with_type, uint16_t handle, uint8_t enc_mode,
     uint8_t role, bool addr_matched);
-
-extern tBTM_STATUS btm_ble_start_adv(void);
-extern tBTM_STATUS btm_ble_stop_adv(void);
-extern void btm_ble_start_scan(void);
 
 /* LE security function from btm_sec.cc */
 extern void btm_ble_link_sec_check(const RawAddress& bd_addr,
