1b0e3140a14ac277a6a84c3d79a6758d4e79e054
howardchung <howardchung@google.com>
2023-01-16 05:42:56 +0000

    Floss: Allow disabling one of HIDP or HOGP in EP
    With the support of libbluetooth, we are able to disable one of the HIDP
    or HOGP in hid host.
    
    Bug: 263755727
    Test: The below operations are tested from D-Bus layer
    1. set service allowlist to empty, check both classic and LE HID are
       functioning.
    2. set service allowlist to HIDP only, check only classic HID is
       functioning.
    3. set service allowlist to HOGP only, check only LE HID is functioning.
    4. set service allowlist to 00000000-0000-0000-0000-000000000000, check
       both classic and LE HID are not functioning.
    Tag: #floss
    
    Change-Id: I88b88bd8f84cc7bc218509e80da726758fb80677

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -4,8 +4,7 @@ use bt_topshim::btif::{
     BaseCallbacks, BaseCallbacksDispatcher, BluetoothInterface, BluetoothProperty, BtAclState,
     BtBondState, BtConnectionDirection, BtConnectionState, BtDeviceType, BtDiscoveryState,
     BtHciErrorCode, BtPinCode, BtPropertyType, BtScanMode, BtSspVariant, BtState, BtStatus,
-    BtTransport, BtVendorProductInfo, DisplayAddress, RawAddress, ToggleableProfile, Uuid,
-    Uuid128Bit,
+    BtTransport, BtVendorProductInfo, DisplayAddress, RawAddress, Uuid, Uuid128Bit,
 };
 use bt_topshim::{
     metrics,
@@ -472,7 +471,11 @@ impl Bluetooth {
 
         match profile {
             Profile::Hid => {
-                self.hh.as_mut().unwrap().disable();
+                self.hh.as_mut().unwrap().activate_hidp(false);
+            }
+
+            Profile::Hogp => {
+                self.hh.as_mut().unwrap().activate_hogp(false);
             }
 
             Profile::A2dpSource | Profile::Hfp => {
@@ -490,7 +493,11 @@ impl Bluetooth {
 
         match profile {
             Profile::Hid => {
-                self.hh.as_mut().unwrap().enable();
+                self.hh.as_mut().unwrap().activate_hidp(true);
+            }
+
+            Profile::Hogp => {
+                self.hh.as_mut().unwrap().activate_hogp(true);
             }
 
             Profile::A2dpSource | Profile::Hfp => {
@@ -507,7 +514,9 @@ impl Bluetooth {
         }
 
         match profile {
-            Profile::Hid => Some(!self.hh.is_none() && self.hh.as_ref().unwrap().is_enabled()),
+            Profile::Hid => Some(self.hh.as_ref().unwrap().is_hidp_activated),
+
+            Profile::Hogp => Some(self.hh.as_ref().unwrap().is_hogp_activated),
 
             Profile::A2dpSource | Profile::Hfp => {
                 self.bluetooth_media.lock().unwrap().is_profile_enabled(profile)
@@ -533,6 +542,22 @@ impl Bluetooth {
                 }
             }
         }
+
+        if self.hh.as_mut().unwrap().configure_enabled_profiles() {
+            self.hh.as_mut().unwrap().disable();
+            let txl = self.tx.clone();
+
+            tokio::spawn(async move {
+                // Wait 100 millis second to prevent race condition caused by quick disable then enable.
+                // TODO: enable until we're sure disable is done.
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                let _ = txl.send(Message::HidHostEnable()).await;
+            });
+        }
+    }
+
+    pub fn enable_hidhost(&mut self) {
+        self.hh.as_mut().unwrap().enable();
     }
 
     pub fn init_profiles(&mut self) {
@@ -547,8 +572,8 @@ impl Bluetooth {
             }),
         });
 
-        let hhtx = self.tx.clone();
         self.hh = Some(HidHost::new(&self.intf.lock().unwrap()));
+        let hhtx = self.tx.clone();
         self.hh.as_mut().unwrap().initialize(HHCallbacksDispatcher {
             dispatch: Box::new(move |cb| {
                 let txl = hhtx.clone();
