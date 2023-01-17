def4eb7f75dded0a552e3abcd201ed45af91c69e
Martin Geisler <mgeisler@google.com>
2023-01-17 17:23:20 +0100

    pdl: Add support for fixed fields
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: I2e3252f3a4756dc5339517d5287f5b7b7a28f285

--- tools/pdl/tests/generated/packet_decl_fixed_scalar_field_big_endian.rs
+++ tools/pdl/tests/generated/packet_decl_fixed_scalar_field_big_endian.rs
@@ -43,7 +43,7 @@ pub trait Packet {
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 struct FooData {
-    a: Vec<u16>,
+    b: u64,
 }
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -54,44 +54,42 @@ pub struct Foo {
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct FooBuilder {
-    pub a: Vec<u16>,
+    pub b: u64,
 }
 impl FooData {
     fn conforms(bytes: &[u8]) -> bool {
-        bytes.len() >= 1
+        bytes.len() >= 8
     }
     fn parse(mut bytes: &mut Cell<&[u8]>) -> Result<Self> {
-        if bytes.get_mut().remaining() < 1 {
+        if bytes.get_mut().remaining() < 8 {
             return Err(Error::InvalidLengthError {
                 obj: "Foo".to_string(),
-                wanted: 1,
+                wanted: 8,
                 got: bytes.get_mut().remaining(),
             });
         }
-        let a_count = bytes.get_mut().get_u8();
-        if bytes.get_mut().remaining() < a_count as usize {
-            return Err(Error::InvalidLengthError {
-                obj: "Foo".to_string(),
-                wanted: a_count as usize,
-                got: bytes.get_mut().remaining(),
+        let chunk = bytes.get_mut().get_u64();
+        if (chunk & 0x7f) as u8 != 7 {
+            return Err(Error::InvalidFixedValue {
+                expected: 7,
+                actual: (chunk & 0x7f) as u8 as u64,
             });
         }
-        let a = (0..a_count)
-            .map(|_| Ok::<_, Error>(bytes.get_mut().get_u16()))
-            .collect::<Result<Vec<_>>>()?;
-        Ok(Self { a })
+        let b = ((chunk >> 7) & 0x1ffffffffffffffu64);
+        Ok(Self { b })
     }
     fn write_to(&self, buffer: &mut BytesMut) {
-        buffer.put_u8(self.a.len() as u8);
-        for elem in &self.a {
-            buffer.put_u16(*elem);
+        if self.b > 0x1ffffffffffffffu64 {
+            panic!("Invalid value for {}::{}: {} > {}", "Foo", "b", self.b, 0x1ffffffffffffffu64);
         }
+        let value = (7 as u64) | (self.b << 7);
+        buffer.put_u64(value);
     }
     fn get_total_size(&self) -> usize {
         self.get_size()
     }
     fn get_size(&self) -> usize {
-        1 + self.a.len() * 2
+        8
     }
 }
 impl Packet for Foo {
@@ -131,8 +129,8 @@ impl Foo {
         let foo = root;
         Ok(Self { foo })
     }
-    pub fn get_a(&self) -> &Vec<u16> {
-        &self.foo.as_ref().a
+    pub fn get_b(&self) -> u64 {
+        self.foo.as_ref().b
     }
     fn write_to(&self, buffer: &mut BytesMut) {
         self.foo.write_to(buffer)
@@ -143,106 +141,7 @@ impl Foo {
 }
 impl FooBuilder {
     pub fn build(self) -> Foo {
-        let foo = Arc::new(FooData { a: self.a });
+        let foo = Arc::new(FooData { b: self.b });
         Foo::new(foo).unwrap()
-    }
-}
-
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-struct BarData {
-    x: Vec<Foo>,
-}
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Bar {
-    #[cfg_attr(feature = "serde", serde(flatten))]
-    bar: Arc<BarData>,
-}
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct BarBuilder {
-    pub x: Vec<Foo>,
-}
-impl BarData {
-    fn conforms(bytes: &[u8]) -> bool {
-        bytes.len() >= 1
-    }
-    fn parse(mut bytes: &mut Cell<&[u8]>) -> Result<Self> {
-        if bytes.get_mut().remaining() < 1 {
-            return Err(Error::InvalidLengthError {
-                obj: "Bar".to_string(),
-                wanted: 1,
-                got: bytes.get_mut().remaining(),
-            });
-        }
-        let x_count = bytes.get_mut().get_u8();
-        let x = (0..x_count).map(|_| Foo::parse_inner(bytes)).collect::<Result<Vec<_>>>()?;
-        Ok(Self { x })
-    }
-    fn write_to(&self, buffer: &mut BytesMut) {
-        buffer.put_u8(self.x.len() as u8);
-        for elem in &self.x {
-            elem.write_to(buffer);
-        }
-    }
-    fn get_total_size(&self) -> usize {
-        self.get_size()
-    }
-    fn get_size(&self) -> usize {
-        1 + self.x.iter().map(|elem| elem.get_size()).sum::<usize>()
-    }
-}
-impl Packet for Bar {
-    fn to_bytes(self) -> Bytes {
-        let mut buffer = BytesMut::with_capacity(self.bar.get_total_size());
-        self.bar.write_to(&mut buffer);
-        buffer.freeze()
-    }
-    fn to_vec(self) -> Vec<u8> {
-        self.to_bytes().to_vec()
-    }
-}
-impl From<Bar> for Bytes {
-    fn from(packet: Bar) -> Self {
-        packet.to_bytes()
-    }
-}
-impl From<Bar> for Vec<u8> {
-    fn from(packet: Bar) -> Self {
-        packet.to_vec()
-    }
-}
-impl Bar {
-    pub fn parse(bytes: &[u8]) -> Result<Self> {
-        let mut cell = Cell::new(bytes);
-        let packet = Self::parse_inner(&mut cell)?;
-        if !cell.get_mut().is_empty() {
-            return Err(Error::InvalidPacketError);
-        }
-        Ok(packet)
-    }
-    fn parse_inner(mut bytes: &mut Cell<&[u8]>) -> Result<Self> {
-        let packet = BarData::parse(&mut bytes)?;
-        Ok(Self::new(Arc::new(packet)).unwrap())
-    }
-    fn new(root: Arc<BarData>) -> std::result::Result<Self, &'static str> {
-        let bar = root;
-        Ok(Self { bar })
-    }
-    pub fn get_x(&self) -> &Vec<Foo> {
-        &self.bar.as_ref().x
-    }
-    fn write_to(&self, buffer: &mut BytesMut) {
-        self.bar.write_to(buffer)
-    }
-    pub fn get_size(&self) -> usize {
-        self.bar.get_size()
-    }
-}
-impl BarBuilder {
-    pub fn build(self) -> Bar {
-        let bar = Arc::new(BarData { x: self.x });
-        Bar::new(bar).unwrap()
     }
 }
