4b1cc720ed6c79fa50b0ac9d757272241fe3a501
Rahul Arya <aryarahul@google.com>
2023-01-18 01:29:43 +0000

    [Offload] Alternative API for getAclHandle()
    This is a synchronous API on BluetoothDevice rather than a
    callback-based one on BluetoothGatt.
    
    Test: compiles
    Bug: 261093955
    Change-Id: I3e008e84e1b2e19158984ecee963b9a63e74b8b1

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -763,6 +763,7 @@ pub(crate) trait BtifBluetoothCallbacks {
         link_type: BtTransport,
         hci_reason: BtHciErrorCode,
         conn_direction: BtConnectionDirection,
+        acl_handle: u16,
     ) {
     }
 
@@ -1124,6 +1125,7 @@ impl BtifBluetoothCallbacks for Bluetooth {
         link_type: BtTransport,
         hci_reason: BtHciErrorCode,
         conn_direction: BtConnectionDirection,
+        acl_handle: u16,
     ) {
         if status != BtStatus::Success {
             warn!(
