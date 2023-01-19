19315fa8e579db34866674344cd579731cc187bd
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2023-01-19 16:49:48 +0000

    Add context for LE Audio group descriptor logging
    Intention for such change is to make it more precise to locate from
    where this log was thrown.
    
    Tag: #feature
    Test: atest GoogleBluetoothInstrumentationTests
    Bug: 261362580
    Change-Id: I9b06acb8e646188ad5e35ba45d295729cab63cfd

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1429,7 +1429,8 @@ public class LeAudioService extends ProfileService {
         synchronized (mGroupLock) {
             LeAudioGroupDescriptor descriptor = getGroupDescriptor(groupId);
             if (descriptor == null || descriptor.mIsActive) {
-                Log.e(TAG, "no descriptors for group: " + groupId + " or group already active");
+                Log.e(TAG, "handleGroupTransitToActive: no descriptors for group: " + groupId
+                        + " or group already active");
                 return;
             }
 
@@ -1447,7 +1448,8 @@ public class LeAudioService extends ProfileService {
         synchronized (mGroupLock) {
             LeAudioGroupDescriptor descriptor = getGroupDescriptor(groupId);
             if (descriptor == null || !descriptor.mIsActive) {
-                Log.e(TAG, "no descriptors for group: " + groupId + " or group already inactive");
+                Log.e(TAG, "handleGroupTransitToInactive: no descriptors for group: " + groupId
+                        + " or group already inactive");
                 return;
             }
 
@@ -1713,7 +1715,7 @@ public class LeAudioService extends ProfileService {
                     descriptor.mAvailableContexts = available_contexts;
                     updateInbandRingtoneForTheGroup(groupId);
                 } else {
-                    Log.e(TAG, "no descriptors for group: " + groupId);
+                    Log.e(TAG, "messageFromNative: no descriptors for group: " + groupId);
                 }
             }
         } else if (stackEvent.type == LeAudioStackEvent.EVENT_TYPE_SINK_AUDIO_LOCATION_AVAILABLE) {
@@ -2009,7 +2011,8 @@ public class LeAudioService extends ProfileService {
             if (descriptor != null) {
                 descriptor.mIsConnected = true;
             } else {
-                Log.e(TAG, "no descriptors for group: " + deviceDescriptor.mGroupId);
+                Log.e(TAG, "connectionStateChanged(STATE_CONNECTED): no descriptors for group: "
+                        + deviceDescriptor.mGroupId);
             }
         }
         // Check if the device is disconnected - if unbond, remove the state machine
@@ -2025,7 +2028,8 @@ public class LeAudioService extends ProfileService {
             synchronized (mGroupLock) {
                 LeAudioGroupDescriptor descriptor = getGroupDescriptor(deviceDescriptor.mGroupId);
                 if (descriptor == null) {
-                    Log.e(TAG, "no descriptors for group: " + deviceDescriptor.mGroupId);
+                    Log.e(TAG, "connectionStateChanged(STATE_DISCONNECTED): no descriptors for "
+                            + "group: " + deviceDescriptor.mGroupId);
                     return;
                 }
 
