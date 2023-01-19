GrapheneOS/platform_packages_modules_Bluetooth

commit 70b63e4596e7fe133ab50bd80d329cd6beccb2fb

Ensure address removed from filter accept list
A direct connection not present in the internal
connection manager data structure prevents the address
from being removed from the filter accept list.

Should that occur, go ahead and try to remove it anyway.

Bug: 237353773
Test: gd/cert/run
Tag: #refactor
BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
Ignore-AOSP-First: cherry-pick

Merged-In: I0253b9d66b12bbee3c9994298ba054b47cd035dd
Change-Id: I0253b9d66b12bbee3c9994298ba054b47cd035dd

system/stack/gatt/gatt_utils.cc
@@ -1398,7 +1398,12 @@ void gatt_sr_update_prep_cnt(tGATT_TCB& tcb, tGATT_IF gatt_if, bool is_inc,
 /** Cancel LE Create Connection request */
 bool gatt_cancel_open(tGATT_IF gatt_if, const RawAddress& bda) {
   tGATT_TCB* p_tcb = gatt_find_tcb_by_addr(bda, BT_TRANSPORT_LE);
-  if (!p_tcb) return true;
+  if (!p_tcb) {
+    LOG_WARN(
+        "Unable to cancel open for unknown connection gatt_if:%hhu peer:%s",
+        gatt_if, PRIVATE_ADDRESS(bda));
+    return true;
+  }
 
   if (gatt_get_ch_state(p_tcb) == GATT_CH_OPEN) {
     LOG(ERROR) << __func__ << ": link connected Too late to cancel";
@@ -1407,9 +1412,21 @@ bool gatt_cancel_open(tGATT_IF gatt_if, const RawAddress& bda) {
 
   gatt_update_app_use_link_flag(gatt_if, p_tcb, false, false);
 
-  if (p_tcb->app_hold_link.empty()) gatt_disconnect(p_tcb);
+  if (p_tcb->app_hold_link.empty()) {
+    LOG_DEBUG(
+        "Client reference count is zero disconnecting device gatt_if:%hhu "
+        "peer:%s",
+        gatt_if, PRIVATE_ADDRESS(bda));
+    gatt_disconnect(p_tcb);
+  }
 
-  connection_manager::direct_connect_remove(gatt_if, bda);
+  if (!connection_manager::direct_connect_remove(gatt_if, bda)) {
+    BTM_AcceptlistRemove(bda);
+    LOG_INFO(
+        "GATT connection manager has no record but removed filter acceptlist "
+        "gatt_if:%hhu peer:%s",
+        gatt_if, PRIVATE_ADDRESS(bda));
+  }
   return true;
 }
 

miyu-xu/packages-modules-Bluetooth

commit 40709811513d03719f5cff741b06def531c7a181

gatt: Fix gatt reconnection
This is a regression after

70b63e4596 Ensure address removed from filter accept list

gatt/connection_manager is responsible for adding or removing device
from the allow list, based on the knownledge about number of interested
clients for the device.

It has been observed, that if single application disconnects non
connected device, it will be removed from the Allow list even there
might be others interested in the connection. This patch fixes that.

Bug: 266049504
Test: manual testing
Tag: #feature
Change-Id: Iaec02012f2766d290ce8fc2cb3bbf73436841f32

system/stack/gatt/gatt_utils.cc
@@ -1468,11 +1468,8 @@ bool gatt_cancel_open(tGATT_IF gatt_if, const RawAddress& bda) {
   }
 
   if (!connection_manager::direct_connect_remove(gatt_if, bda)) {
-    BTM_AcceptlistRemove(bda);
-    LOG_INFO(
-        "GATT connection manager has no record but removed filter acceptlist "
-        "gatt_if:%hhu peer:%s",
-        gatt_if, ADDRESS_TO_LOGGABLE_CSTR(bda));
+    LOG_INFO("GATT connection manager has no record gatt_if:%hhu peer:%s",
+             gatt_if, ADDRESS_TO_LOGGABLE_CSTR(bda));
   }
   return true;
 }