a8f8281edd5c4529a8d410c4f7cc1bf86563557b
Rongxuan Liu <rongxuan@google.com>
2023-01-23 19:44:11 +0000

    Add support to connect LE Gatt with address type (1/2)
    Bug: 266449499
    Tag: #feature
    Test: atest BluetoothInstrumentationTests; atest GattServiceTest
    Change-Id: I6d86151bd631a854a33b9787e46da8268dcd4538

--- android/app/jni/com_android_bluetooth_gatt.cpp
+++ android/app/jni/com_android_bluetooth_gatt.cpp
@@ -1364,8 +1364,9 @@ static void gattClientScanNative(JNIEnv* env, jobject object, jboolean start) {
 }
 
 static void gattClientConnectNative(JNIEnv* env, jobject object, jint clientif,
-                                    jstring address, jboolean isDirect,
-                                    jint transport, jboolean opportunistic,
+                                    jstring address, jint addressType,
+                                    jboolean isDirect, jint transport,
+                                    jboolean opportunistic,
                                     jint initiating_phys) {
   if (!sGattIf) return;
 
@@ -2575,7 +2576,7 @@ static JNINativeMethod sMethods[] = {
      (void*)gattClientRegisterAppNative},
     {"gattClientUnregisterAppNative", "(I)V",
      (void*)gattClientUnregisterAppNative},
-    {"gattClientConnectNative", "(ILjava/lang/String;ZIZI)V",
+    {"gattClientConnectNative", "(ILjava/lang/String;IZIZI)V",
      (void*)gattClientConnectNative},
     {"gattClientDisconnectNative", "(ILjava/lang/String;I)V",
      (void*)gattClientDisconnectNative},
