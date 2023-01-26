e32254c5679d85b11c25c0afd57d6ed84e908120
Georg Neis <neis@chromium.org>
2023-01-26 07:38:15 +0000

    Update {bind,callback,callback_forward,callback_helpers}.h includes
    These headers are now in base/functional/.
    
    Tag: #floss
    Bug: 266161904
    Test: emerge floss
    Cq-Depend: aosp:2399915
    Change-Id: Iaac7673f7c61d55e2d1572c92799d670fef65e72

--- system/test/mock/mock_main_shim_btm_api.cc
+++ system/test/mock/mock_main_shim_btm_api.cc
@@ -25,8 +25,8 @@
 
 extern std::map<std::string, int> mock_function_count_map;
 
-#include <base/bind.h>
-#include <base/callback.h>
+#include <base/functional/bind.h>
+#include <base/functional/callback.h>
 
 #include "main/shim/btm_api.h"
 #include "stack/include/bt_hdr.h"
