f03f1248eb82e4d36ff2a810f1ebeb4faf23f7a9
Jakub Rotkiewicz <rotkiewicz@google.com>
2023-01-27 16:53:11 +0000

    Bluetooth Quality Report feature flag
    Bug: 259448922
    Tag: #feature
    Test: atest bluetooth_test_gd
    Test: atest InitFlagsTest
    BYPASS_LONG_LINES_REASON: Bluetooth likes 120 lines
    
    Change-Id: Ie4dcf7ade01de98cdf8325cb8e7b0d8d9b3d80cc

--- system/gd/rust/common/src/init_flags.rs
+++ system/gd/rust/common/src/init_flags.rs
@@ -283,6 +283,7 @@ init_flags!(
         rust_event_loop,
         sdp_serialization = true,
         sdp_skip_rnr_if_known = true,
+        send_bluetooth_quality_report_as_intent,
         set_min_encryption = true,
         subrating = true,
         trigger_advertising_callbacks_on_first_resume_after_pause = true,
