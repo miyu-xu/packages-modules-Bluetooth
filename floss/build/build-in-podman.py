1645cd7e0baebaf0bf3ccb9e64e68d6f44808f21
Shiu Ng <shiu@google.com>
2023-01-28 01:06:04 +0000

    In Floss build scripts, add support for using Docker.
    Bug: 258863499
    Test: To be performed manually.
    Change-Id: I3c18c91fde844d6ba0d7107f2f9c78b270c09ebf

--- floss/build/build-in-podman.py
+++ floss/build/build-in-podman.py
@@ -10,8 +10,8 @@ SRC_MOUNT = "/root/src"
 STAGING_MOUNT = "/root/.floss"
 
 
-class FlossPodmanRunner:
-    """Runs Floss build inside podman container."""
+class FlossDockerRunner:
+    """Runs Floss build inside docker container."""
 
     # Commands to run for build
     BUILD_COMMANDS = [
@@ -28,20 +28,22 @@ class FlossPodmanRunner:
         [f'{SRC_MOUNT}/build.py', '--target', 'test'],
     ]
 
-    def __init__(self, workdir, rootdir, image_tag, volume_name, container_name, staging_dir):
+    def __init__(self, workdir, rootdir, image_tag, volume_name, container_name, staging_dir, use_docker):
         """ Constructor.
 
         Args:
             workdir: Current working directory (should be the script path).
             rootdir: Root directory for Bluetooth.
-            image_tag: Tag for podman image used for building.
+            image_tag: Tag for docker image used for building.
             volume_name: Volume name used for storing artifacts.
             container_name: Name for running container instance.
             staging_dir: Directory to mount for artifacts instead of using volume.
+            use_docker: Use docker binary if True (or podman when False).
         """
         self.workdir = workdir
         self.rootdir = rootdir
         self.image_tag = image_tag
+        self.docker_binary = 'docker' if use_docker else 'podman'
         self.env = os.environ.copy()
 
         # Name of running container
@@ -79,12 +81,12 @@ class FlossPodmanRunner:
     def _create_volume_if_needed(self):
         # Check if the volume exists. Otherwise create it.
         try:
-            subprocess.check_output(['podman', 'volume', 'inspect', self.volume_name])
+            subprocess.check_output([self.docker_binary, 'volume', 'inspect', self.volume_name])
         except:
-            self.run_command('podman volume create', ['podman', 'volume', 'create', self.volume_name])
+            self.run_command(self.docker_binary + ' volume create', [self.docker_binary, 'volume', 'create', self.volume_name])
 
     def start_container(self):
-        """Starts the podman container with correct mounts."""
+        """Starts the docker container with correct mounts."""
         # Stop any previously started container.
         self.stop_container(ignore_error=True)
 
@@ -99,17 +101,17 @@ class FlossPodmanRunner:
         # Mount the source directory
         mount_src_dir = 'type=bind,src={},dst={}'.format(self.rootdir, SRC_MOUNT)
 
-        # Run the podman image. It will run `tail` indefinitely so the container
-        # doesn't close and we can run `podman exec` on it.
-        self.run_command('podman run', [
-            'podman', 'run', '--name', self.container_name, '--mount', mount_output_volume, '--mount', mount_src_dir,
+        # Run the docker image. It will run `tail` indefinitely so the container
+        # doesn't close and we can run `docker exec` on it.
+        self.run_command(self.docker_binary + ' run', [
+            self.docker_binary, 'run', '--name', self.container_name, '--mount', mount_output_volume, '--mount', mount_src_dir,
             '-d', self.image_tag, 'tail', '-f', '/dev/null'
         ])
 
     def stop_container(self, ignore_error=False):
-        """Stops the podman container for build."""
-        self.run_command('podman stop', ['podman', 'stop', '-t', '1', self.container_name], ignore_rc=ignore_error)
-        self.run_command('podman rm', ['podman', 'rm', self.container_name], ignore_rc=ignore_error)
+        """Stops the docker container for build."""
+        self.run_command(self.docker_binary + ' stop', [self.docker_binary, 'stop', '-t', '1', self.container_name], ignore_rc=ignore_error)
+        self.run_command(self.docker_binary + ' rm', [self.docker_binary, 'rm', self.container_name], ignore_rc=ignore_error)
 
     def do_build(self):
         """Runs the basic build commands."""
@@ -119,21 +121,21 @@ class FlossPodmanRunner:
         try:
             # Run all commands
             for i, cmd in enumerate(self.BUILD_COMMANDS):
-                self.run_command('podman exec #{}'.format(i), ['podman', 'exec', '-it', self.container_name] + cmd)
+                self.run_command(self.docker_binary + ' exec #{}'.format(i), [self.docker_binary, 'exec', '-it', self.container_name] + cmd)
         finally:
             # Always stop container before exiting
             self.stop_container()
 
     def print_do_build(self):
         """Prints the commands for building."""
-        podman_exec = ['podman', 'exec', '-it', self.container_name]
+        docker_exec = [self.docker_binary, 'exec', '-it', self.container_name]
         print('Normally, build would run the following commands: \n')
         for cmd in self.BUILD_COMMANDS:
-            print(' '.join(podman_exec + cmd))
+            print(' '.join(docker_exec + cmd))
 
-    def check_podman_runnable(self):
+    def check_docker_runnable(self):
         try:
-            subprocess.check_output(['podman', 'ps'], stderr=subprocess.STDOUT)
+            subprocess.check_output([self.docker_binary, 'ps'], stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as err:
             if 'denied' in err.output.decode('utf-8'):
                 print('Run script as sudo')
@@ -142,19 +144,19 @@ class FlossPodmanRunner:
 
             return False
 
-        # No exception means podman is ok
+        # No exception means docker is ok
         return True
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('Builder Floss inside podman image.')
+    parser = argparse.ArgumentParser('Builder Floss inside docker image.')
     parser.add_argument(
         '--only-start',
         action='store_true',
         default=False,
         help='Only start the container. Prints the commands it would have ran.')
     parser.add_argument('--only-stop', action='store_true', default=False, help='Only stop the container and exit.')
-    parser.add_argument('--image-tag', default='floss:latest', help='Podman image to use to build.')
+    parser.add_argument('--image-tag', default='floss:latest', help='Docker image to use to build.')
     parser.add_argument(
         '--volume-tag',
         default='floss-out',
@@ -163,10 +165,11 @@ if __name__ == "__main__":
         '--staging-dir',
         default=None,
         help='Staging directory to use instead of volume. Build artifacts will be written here.')
-    parser.add_argument('--container-name', default='floss-podman-runner', help='What to name the started container')
+    parser.add_argument('--container-name', default='floss-docker-runner', help='What to name the started container.')
+    parser.add_argument('--use-docker', action='store_true', default=False, help='Use flag to use Docker to build Floss. Defaults to using podman.')
     args = parser.parse_args()
 
-    # cwd should be set to same directory as this script (that's where Podmanfile
+    # cwd should be set to same directory as this script (that's where Dockerfile
     # is kept).
     workdir = os.path.dirname(os.path.abspath(sys.argv[0]))
     rootdir = os.path.abspath(os.path.join(workdir, '../..'))
@@ -174,10 +177,10 @@ if __name__ == "__main__":
     # Determine staging directory absolute path
     staging = os.path.abspath(args.staging_dir) if args.staging_dir else None
 
-    fdr = FlossPodmanRunner(workdir, rootdir, args.image_tag, args.volume_tag, args.container_name, staging)
+    fdr = FlossDockerRunner(workdir, rootdir, args.image_tag, args.volume_tag, args.container_name, staging, args.use_docker)
 
-    # Make sure podman is runnable before continuing
-    if fdr.check_podman_runnable():
+    # Make sure docker is runnable before continuing
+    if fdr.check_docker_runnable():
         # Handle some flags
         if args.only_start:
             fdr.start_container()
