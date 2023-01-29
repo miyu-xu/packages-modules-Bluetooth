bdad9ff6bcdcd6c7a630518e8693559c1b99d5f8
Palash Ahuja <ahujapalash@google.com>
2023-01-29 22:07:47 +0000

    BluetoothMetrics: Adding command to log the IO Capability during exchange
    Change-Id: I442d2bd95e8d15434b5da675cd6327711a2928fa

--- system/gd/os/android/metrics.cc
+++ system/gd/os/android/metrics.cc
@@ -236,7 +236,7 @@ void LogMetricReadTxPowerLevelResult(
 }
 
 void LogMetricSmpPairingEvent(
-    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint16_t smp_fail_reason) {
+    const Address& address, uint16_t smp_cmd, android::bluetooth::DirectionEnum direction, uint16_t smp_fail_reason, uint8_t io_capability) {
   int metric_id = 0;
   if (!address.IsEmpty()) {
     metric_id = MetricIdManager::GetInstance().AllocateId(address);
