f9c5f8121243ffa206018c8f32a679daeecac72a
Rahul Arya <aryarahul@google.com>
2023-01-31 19:39:14 +0000

    [Private GATT] Add support for GATT write requests
    Bug: 255880936
    Test: unit
    Change-Id: Iec10e55b720729bd88e8a3889d9f47150fb693ee

--- system/rust/src/gatt/server/att_database.rs
+++ system/rust/src/gatt/server/att_database.rs
@@ -4,8 +4,8 @@ use crate::{
     core::{get_128_be_uuid_bytes, CxxUuid},
     gatt::ids::AttHandle,
     packets::{
-        AttAttributeDataChild, AttErrorCode, AttHandleBuilder, AttHandleView, ParseError,
-        Uuid128Builder, Uuid128View, Uuid16Builder, Uuid16View, UuidBuilder, UuidView,
+        AttAttributeDataChild, AttAttributeDataView, AttErrorCode, AttHandleBuilder, AttHandleView,
+        ParseError, Uuid128Builder, Uuid128View, Uuid16Builder, Uuid16View, UuidBuilder, UuidView,
     },
 };
 
@@ -161,6 +161,11 @@ pub trait AttDatabase {
         &self,
         handle: AttHandle,
     ) -> Result<AttAttributeDataChild, AttErrorCode>;
+    async fn write_attribute(
+        &self,
+        handle: AttHandle,
+        data: AttAttributeDataView<'_>,
+    ) -> Result<(), AttErrorCode>;
     fn list_attributes(&self) -> Vec<AttAttribute>;
 
     fn get_attribute(&self, handle: AttHandle) -> Option<AttAttribute> {
