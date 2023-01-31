7463494e24e99459ef6066f1429a86b1ba08d79a
Rahul Arya <aryarahul@google.com>
2023-01-31 19:33:42 +0000

    [Private GATT] Add basic ATT transaction implementation
    Bug: 255880936
    Test: unit
    Change-Id: I4868de5c67e0b724441718b886583736596e5423

--- system/rust/src/gatt/server.rs
+++ system/rust/src/gatt/server.rs
@@ -2,3 +2,10 @@
 //! existing C++ GATT client. See go/private-gatt-in-platform for the design.
 
 mod att_database;
+pub mod server_connection;
+mod transaction_handler;
+mod transactions;
+
+#[cfg(test)]
+mod test;
+mod utils;
---
+++ system/rust/src/gatt/server/test/test_att_db.rs
@@ -0,0 +1,46 @@
+use crate::{
+    gatt::{
+        ids::AttHandle,
+        server::att_database::{AttAttribute, AttDatabase},
+    },
+    packets::{AttAttributeDataChild, AttErrorCode},
+};
+
+use async_trait::async_trait;
+use log::info;
+use std::collections::HashMap;
+
+pub struct TestAttDatabase {
+    attributes: HashMap<AttHandle, (AttAttribute, Vec<u8>)>,
+}
+
+impl TestAttDatabase {
+    #[cfg(test)]
+    pub fn new(attributes: Vec<(AttAttribute, Vec<u8>)>) -> Self {
+        Self {
+            attributes: attributes
+                .into_iter()
+                .map(|(att, data)| (att.handle, (att, data)))
+                .collect(),
+        }
+    }
+}
+
+#[async_trait(?Send)]
+impl AttDatabase for TestAttDatabase {
+    async fn read_attribute(
+        &self,
+        handle: AttHandle,
+    ) -> Result<AttAttributeDataChild, AttErrorCode> {
+        info!("reading {handle:?}");
+        match self.attributes.get(&handle) {
+            Some((_, data)) => Ok(AttAttributeDataChild::RawData(data.clone().into_boxed_slice())),
+            None => Err(AttErrorCode::INVALID_HANDLE),
+        }
+    }
+    fn list_attributes(&self) -> Vec<AttAttribute> {
+        let mut attrs: Vec<_> = self.attributes.values().map(|(att, _)| att.clone()).collect();
+        attrs.sort_by_key(|attr| attr.handle);
+        attrs
+    }
+}
