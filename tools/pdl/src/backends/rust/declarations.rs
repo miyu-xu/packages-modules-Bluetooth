4391e3f5f85adda961629ab68d153c1d90c1837e
Martin Geisler <mgeisler@google.com>
2023-02-01 16:07:54 +0100

    pdl: Add support for payloads and constraints
    This CL adds support for payload fields and thus also for child
    packets.
    
    The data struct for each packet is now marked `pub`: this is because
    it can be mentioned in the enums we build to hold child data.
    
    Test: atest pdl_tests pdl_rust_generator_tests_{le,be}
    Change-Id: I8284f3a7f5b4a7e5c6ae12077a8466492d74cea0

--- tools/pdl/src/backends/rust/declarations.rs
+++ tools/pdl/src/backends/rust/declarations.rs
@@ -1,14 +1,17 @@
 use crate::backends::rust::types;
+use crate::lint;
 use crate::parser::ast as parser_ast;
 use quote::{format_ident, quote};
 
-pub struct FieldDeclarations {
+pub struct FieldDeclarations<'a> {
+    scope: &'a lint::Scope<'a>,
+    packet_name: &'a str,
     code: Vec<proc_macro2::TokenStream>,
 }
 
-impl FieldDeclarations {
-    pub fn new() -> FieldDeclarations {
-        FieldDeclarations { code: Vec::new() }
+impl<'a> FieldDeclarations<'a> {
+    pub fn new(scope: &'a lint::Scope<'a>, packet_name: &'a str) -> FieldDeclarations<'a> {
+        FieldDeclarations { scope, packet_name, code: Vec::new() }
     }
 
     pub fn add(&mut self, field: &parser_ast::Field) {
@@ -22,9 +25,18 @@ impl FieldDeclarations {
             #id: #field_type,
         })
     }
+
+    pub fn done(&mut self) {
+        let packet_data_child = format_ident!("{}DataChild", self.packet_name);
+        if self.scope.children.contains_key(self.packet_name) {
+            self.code.push(quote! {
+                child: #packet_data_child,
+            });
+        }
+    }
 }
 
-impl quote::ToTokens for FieldDeclarations {
+impl quote::ToTokens for FieldDeclarations<'_> {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
         let code = &self.code;
         tokens.extend(quote! {
