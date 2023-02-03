90ee447b4a67b93971ce8b28c543170b89fa09d8
Rahul Arya <aryarahul@google.com>
2023-02-03 02:08:52 +0000

    [Private GATT] Mirror GATT server inside rust module
    Since servers can be registered as "private" after creation,
    we mirror their services in Rust by snooping on the JNI
    calls/callbacks.
    
    Bug: 255880936
    Test: unit
    Change-Id: If14d1a97a0f57a0a8bb8094fb48d7a0c7d5c8128

--- system/rust/src/core/mod.rs
+++ system/rust/src/core/mod.rs
@@ -16,12 +16,63 @@
 
 mod ffi;
 
-use std::{rc::Rc, thread};
+use std::{fmt::Debug, rc::Rc, thread};
+
+#[cfg(not(test))]
+pub use ffi::CxxUuid;
 
 use crate::{gatt::ffi::AttTransportImpl, GlobalModuleRegistry};
+
+/// A 6-byte MAC address corresponding to a Bluetooth device
+///
+/// Try to avoid using in favor of an Address tagged with the AddressType
+#[repr(C)]
+pub struct RawAddress(pub [u8; 6]);
+
+impl Debug for RawAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "xx:xx:xx:xx:{:02x}:{:02x}", self.0[4], self.0[5],)
+    }
+}
 
 fn init() {
     thread::spawn(move || {
         GlobalModuleRegistry::start(Rc::new(AttTransportImpl()));
     });
+}
+
+/// Get the raw bytes (in big-endian order) for a C++ UUID
+#[cfg(not(test))]
+pub fn get_128_be_uuid_bytes(uuid: &CxxUuid) -> &[u8; 16] {
+    ffi::get_128_be_uuid_bytes(uuid).try_into().expect("ffi should give us exactly 16 bytes")
+}
+
+#[cfg(test)]
+pub use mock::*;
+
+/// Get the raw bytes (in big-endian order) for a C++ UUID
+#[cfg(test)]
+mod mock {
+    use cxx::{type_id, ExternType};
+
+    use crate::gatt::server::gatt_database::Uuid;
+
+    pub struct CxxUuid([u8; 16]);
+
+    unsafe impl ExternType for CxxUuid {
+        type Id = type_id!("bluetooth::Uuid");
+        type Kind = cxx::kind::Opaque;
+    }
+
+    impl CxxUuid {
+        pub fn new_mocked(uuid: Uuid) -> Box<Self> {
+            let mut data = uuid.0;
+            data.reverse();
+            Box::new(Self(data))
+        }
+    }
+
+    pub fn get_128_be_uuid_bytes(uuid: &CxxUuid) -> &[u8; 16] {
+        &uuid.0
+    }
 }
