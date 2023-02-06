3c8e2294dc3d374575781b244e838a4b48d3584f
Rahul Arya <aryarahul@google.com>
2023-02-06 00:08:58 +0000

    [Private GATT] Add basic ATT transaction implementation
    Bug: 255880936
    Test: unit
    Change-Id: I4868de5c67e0b724441718b886583736596e5423

--- system/rust/src/gatt/server/att_database.rs
+++ system/rust/src/gatt/server/att_database.rs
@@ -1,8 +1,10 @@
+use async_trait::async_trait;
+
 use crate::{
     gatt::ids::AttHandle,
     packets::{
-        AttHandleBuilder, AttHandleView, ParseError, Uuid128Builder, Uuid128View, Uuid16Builder,
-        Uuid16View, UuidBuilder, UuidView,
+        AttAttributeDataChild, AttErrorCode, AttHandleBuilder, AttHandleView, ParseError,
+        Uuid128Builder, Uuid128View, Uuid16Builder, Uuid16View, UuidBuilder, UuidView,
     },
 };
 
@@ -120,6 +122,42 @@ impl TryFrom<Uuid> for Uuid16Builder {
 impl From<Uuid> for Uuid128Builder {
     fn from(value: Uuid) -> Self {
         Uuid128Builder { data: value.0.into_iter().collect() }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AttAttribute {
+    pub handle: AttHandle,
+    pub type_: Uuid,
+    pub permissions: AttPermissions,
+}
+
+/// The attribute properties supported by the current GATT server implementation
+/// Unimplemented properties will default to false.
+#[derive(Debug, Clone)]
+pub struct AttPermissions {
+    /// Whether an attribute is readable
+    pub readable: bool,
+    /// Whether an attribute is writable
+    /// (using ATT_WRITE_REQ, so a response is expected)
+    pub writable: bool,
+}
+
+#[async_trait(?Send)]
+pub trait AttDatabase {
+    /// Read an attribute by handle
+    async fn read_attribute(
+        &self,
+        handle: AttHandle,
+    ) -> Result<AttAttributeDataChild, AttErrorCode>;
+
+    /// List all the attributes in this database.
+    ///
+    /// Expected to return them in sorted order.
+    fn list_attributes(&self) -> Vec<AttAttribute>;
+
+    fn find_attribute(&self, handle: AttHandle) -> Option<AttAttribute> {
+        self.list_attributes().into_iter().find(|attr| attr.handle == handle)
     }
 }
 
