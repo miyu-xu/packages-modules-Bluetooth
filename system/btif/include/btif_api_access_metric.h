d61cf6f44042005be6188ef56090fbef5e6b7c44
cmanton@google.com <cmanton@google.com>
2023-02-07 14:39:44 -0800

    Introduce deployment for api interface and callback collection
    Bug: 266989331
    Test: gd/cert/run
    Tag: #refactor
    Change-Id: I466539a9b897934f83519a7c33a34ad81418392a

---
+++ system/btif/include/btif_api_access_metric.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "common/circular_buffer.h"
+
+struct ApiEntry {
+  uint32_t token;
+  std::string name;
+  uint64_t count;
+};
+
+// Enable or disable the collection of api counter metrics.
+void api_access_metric_disable();
+void api_access_metric_enable();
+
+// Output the API timestamp access and name to filedescriptor.
+void api_access_metric_dump(int fd);
+
+// Add API main surface entry or callback with given name.
+void api_access_metric_add_api(const char* name);
+void api_access_metric_add_cb(const char* name);
+
+// Add internal entry or callback with given name.
+void api_access_metric_add(uint32_t token, const char* name);
