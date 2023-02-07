16cf2368bea50b8cf23796358cc145f9f9333264
Rahul Arya <aryarahul@google.com>
2023-02-07 02:39:17 +0000

    Make API for LeAddressManager clearer
    We use the initiator address policy as a proxy for whether LE privacy is
    supported. This is hacky but changing it will break ATV. This CL at
    least makes it a bit less ugly from the advertising manager by putting
    the hack in just one place.
    
    Bug: 268112598
    Test: unit
    Change-Id: I51da3a2ce4f215e14f2f0f86a378211839f98407

--- system/gd/hci/le_address_manager.h
+++ system/gd/hci/le_address_manager.h
@@ -71,7 +71,8 @@ class LeAddressManager {
       crypto_toolbox::Octet16 rotation_irk,
       std::chrono::milliseconds minimum_rotation_time,
       std::chrono::milliseconds maximum_rotation_time);
-  virtual AddressPolicy GetAddressPolicy();
+  AddressPolicy GetAddressPolicy();
+  bool SupportsPrivacy();
   virtual void AckPause(LeAddressManagerCallback* callback);
   virtual void AckResume(LeAddressManagerCallback* callback);
   virtual AddressPolicy Register(LeAddressManagerCallback* callback);
@@ -100,6 +101,9 @@ class LeAddressManager {
   size_t NumberCachedCommands() const {
     return cached_commands_.size();
   }
+
+ protected:
+  AddressPolicy address_policy_ = AddressPolicy::POLICY_NOT_SET;
 
  private:
   enum ClientState {
@@ -163,7 +167,6 @@ class LeAddressManager {
   os::Handler* handler_;
   std::map<LeAddressManagerCallback*, ClientState> registered_clients_;
 
-  AddressPolicy address_policy_ = AddressPolicy::POLICY_NOT_SET;
   AddressWithType le_address_;
   AddressWithType cached_address_;
   Address public_address_;
