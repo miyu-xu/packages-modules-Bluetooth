354c5f898a43454ea20cda5a29fdd56cfb1311e5
Michael Sun <michaelfsun@google.com>
2023-02-14 16:03:52 -0800

    floss: rejecting the unbonded media profile connections
    Rejecting the unbonded connection after we finished our profile
    reconnecting logic to avoid a collision.
    
    BUG: 259745456
    Tag: #floss
    Test: forget and reconnect device
    Change-Id: I19d5e7c3f698fda4968eb87a3b10b5201ae66adc

--- system/gd/rust/linux/stack/src/bluetooth_media.rs
+++ system/gd/rust/linux/stack/src/bluetooth_media.rs
@@ -1,7 +1,7 @@
 //! Anything related to audio and media API.
 
 use bt_topshim::btif::{
-    BluetoothInterface, BtConnectionDirection, BtStatus, RawAddress, ToggleableProfile,
+    BluetoothInterface, BtBondState, BtConnectionDirection, BtStatus, RawAddress, ToggleableProfile,
 };
 use bt_topshim::profiles::a2dp::{
     A2dp, A2dpCallbacks, A2dpCallbacksDispatcher, A2dpCodecBitsPerSample, A2dpCodecChannelMode,
@@ -791,6 +791,44 @@ impl BluetoothMedia {
                 guard.insert(addr, Some((task, first_conn_ts)));
             }
             DeviceConnectionStates::FullyConnected => {
+                // Rejecting the unbonded connection after we finished our profile
+                // reconnectinglogic to avoid a collision.
+                if let Some(adapter) = &self.adapter {
+                    if BtBondState::Bonded
+                        != adapter
+                            .lock()
+                            .unwrap()
+                            .get_bond_state(BluetoothDevice::new(addr.to_string(), "".to_string()))
+                    {
+                        warn!(
+                            "[{}]: Rejecting a unbonded device's attempt to connect to media profiles",
+                            addr.to_string());
+                        let fallback_tasks = self.fallback_tasks.clone();
+                        let device_states = self.device_states.clone();
+                        let txl = self.tx.clone();
+                        let task = topstack::get_runtime().spawn(async move {
+                            {
+                                let mut states = device_states.lock().unwrap();
+                                let mut guard = fallback_tasks.lock().unwrap();
+                                states.insert(addr, DeviceConnectionStates::Disconnecting);
+                                guard.insert(addr, None);
+                            }
+
+                            info!(
+                                "[{}]: Device connection state: {:?}.",
+                                addr.to_string(),
+                                DeviceConnectionStates::Disconnecting
+                            );
+
+                            let _ = txl
+                                .send(Message::Media(MediaActions::Disconnect(addr.to_string())))
+                                .await;
+                        });
+                        guard.insert(addr, Some((task, first_conn_ts)));
+                    }
+                    return;
+                }
+
                 let cur_a2dp_caps = self.a2dp_caps.get(&addr);
                 let cur_hfp_cap = self.hfp_cap.get(&addr);
                 let name = self.adapter_get_remote_name(addr);
