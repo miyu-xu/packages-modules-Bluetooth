a4eb11d8da6f04d18d803769bb58794eb4fb6c2a
Rahul Arya <aryarahul@google.com>
2023-02-19 01:38:50 +0000

    [Private GATT] Refactor to remove Rc<>
    This CL introduces the abstraction of SharedBox<>, which
    wraps Rc<> to avoid accidental circular references / leaks.
    
    Without this, we could leak AttServerBearers by e.g. storing
    an Rc<> to them in a spawned future, or by actually having a
    circular reference.
    
    Bug: 255880936
    Test: unit
    
    Change-Id: If60bd51d4ecda799de465d2e54a2976cf4e6793a

--- system/rust/tests/gatt_server_test.rs
+++ system/rust/tests/gatt_server_test.rs
@@ -10,6 +10,7 @@ use bluetooth_core::{
             mock_transport::MockAttTransport,
         },
         server::{
+            att_server_bearer::AttServerBearer,
             gatt_database::{AttPermissions, GattCharacteristicWithHandle, GattServiceWithHandle},
             GattModule,
         },
@@ -124,12 +125,11 @@ fn test_service_read() {
         data_rx.recv().await.unwrap();
 
         // act
-        gatt.handle_packet(
-            CONN_ID,
+        AttServerBearer::handle_packet(
+            &gatt.get_bearer(CONN_ID).unwrap(),
             build_att_view_or_crash(AttReadRequestBuilder { attribute_handle: HANDLE_1.into() })
                 .view(),
-        )
-        .unwrap();
+        );
         let (tcb_idx, resp) = transport_rx.recv().await.unwrap();
 
         // assert
@@ -162,12 +162,11 @@ fn test_server_closed_while_connected() {
         gatt.close_gatt_server(SERVER_ID).unwrap();
 
         // act: read from the closed server
-        gatt.handle_packet(
-            CONN_ID,
+        AttServerBearer::handle_packet(
+            &gatt.get_bearer(CONN_ID).unwrap(),
             build_att_view_or_crash(AttReadRequestBuilder { attribute_handle: HANDLE_1.into() })
                 .view(),
-        )
-        .unwrap();
+        );
         let (_, resp) = transport_rx.recv().await.unwrap();
 
         // assert that the read failed, but that a response was provided
@@ -196,12 +195,11 @@ fn test_characteristic_read() {
         data_rx.recv().await.unwrap();
 
         // act
-        gatt.handle_packet(
-            CONN_ID,
+        AttServerBearer::handle_packet(
+            &gatt.get_bearer(CONN_ID).unwrap(),
             build_att_view_or_crash(AttReadRequestBuilder { attribute_handle: HANDLE_2.into() })
                 .view(),
-        )
-        .unwrap();
+        );
         let tx = if let MockDatastoreEvents::ReadCharacteristic(CONN_ID, HANDLE_2, tx) =
             data_rx.recv().await.unwrap()
         {
@@ -236,15 +234,14 @@ fn test_characteristic_write() {
         data_rx.recv().await.unwrap();
 
         // act
-        gatt.handle_packet(
-            CONN_ID,
+        AttServerBearer::handle_packet(
+            &gatt.get_bearer(CONN_ID).unwrap(),
             build_att_view_or_crash(AttWriteRequestBuilder {
                 handle: HANDLE_2.into(),
                 value: build_att_data(data.clone()),
             })
             .view(),
-        )
-        .unwrap();
+        );
         let (tx, written_data) =
             if let MockDatastoreEvents::WriteCharacteristic(CONN_ID, HANDLE_2, written_data, tx) =
                 data_rx.recv().await.unwrap()
