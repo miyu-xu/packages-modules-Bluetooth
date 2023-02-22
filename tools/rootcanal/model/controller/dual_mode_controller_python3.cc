4b75fcdb8d9595cd43e2973cda9af40054be201d
Henri Chataing <henrichataing@google.com>
2023-02-22 01:26:10 +0000

    RootCanal: Implement the task scheduler directly in LinkLayerController
    Implement a synchronous version of the task scheduler in
    LinkLayerController to remove the dependency on an external asynchronous
    manager. The callbacks are executed during the tick() invocation.
    
    Test: atest pts-bot:A2DP
    Bug: 270203380
    Change-Id: I082fd0b63783f8a62d47badb734428aea6be573f

--- tools/rootcanal/model/controller/dual_mode_controller_python3.cc
+++ tools/rootcanal/model/controller/dual_mode_controller_python3.cc
@@ -39,20 +39,7 @@ enum Type {
 // SendLinkLayerPacket as forwarding packets to a registered handler.
 class BaseController : public DualModeController {
  public:
-  BaseController() {
-    RegisterTaskScheduler(
-        [this](std::chrono::milliseconds delay, TaskCallback const& task) {
-          return this->async_manager_.ExecAsync(0, delay, task);
-        });
-    RegisterPeriodicTaskScheduler([this](std::chrono::milliseconds delay,
-                                         std::chrono::milliseconds period,
-                                         TaskCallback const& task) {
-      return this->async_manager_.ExecAsyncPeriodically(0, delay, period, task);
-    });
-    RegisterTaskCancel([this](AsyncTaskId task_id) {
-      this->async_manager_.CancelAsyncTask(task_id);
-    });
-  }
+  BaseController() {}
   ~BaseController() = default;
 
   void Start() {
--- tools/rootcanal/model/controller/link_layer_controller.cc
+++ tools/rootcanal/model/controller/link_layer_controller.cc
@@ -25,7 +25,6 @@
 #include "log.h"
 #include "packet/raw_builder.h"
 
-using std::vector;
 using namespace std::chrono;
 using bluetooth::hci::Address;
 using bluetooth::hci::AddressType;
@@ -38,6 +37,8 @@ using bluetooth::hci::SubeventCode;
 using namespace model::packets;
 using model::packets::PacketType;
 using namespace std::literals;
+
+using TaskId = rootcanal::LinkLayerController::TaskId;
 
 // Temporay define, to be replaced when verbose log level is implemented.
 #define LOG_VERB(...) LOG_INFO(__VA_ARGS__)
@@ -1414,7 +1415,7 @@ LinkLayerController::LinkLayerController(const Address& address,
 LinkLayerController::~LinkLayerController() {
   // Clear out periodic tasks for opened SCO connections in the
   // connection manager state.
-  connections_.Reset(cancel_task_);
+  connections_.Reset([this](TaskId task_id) { CancelScheduledTask(task_id); });
 }
 
 void LinkLayerController::SendLeLinkLayerPacket(
@@ -2053,8 +2054,10 @@ void LinkLayerController::IncomingDisconnectPacket(
 #ifdef ROOTCANAL_LMP
   auto is_br_edr = connections_.GetPhyType(handle) == Phy::Type::BR_EDR;
 #endif
-  ASSERT_LOG(connections_.Disconnect(handle, cancel_task_),
-             "GetHandle() returned invalid handle %hx", handle);
+  ASSERT_LOG(
+      connections_.Disconnect(
+          handle, [this](TaskId task_id) { CancelScheduledTask(task_id); }),
+      "GetHandle() returned invalid handle %hx", handle);
 
   uint8_t reason = disconnect.GetReason();
   SendDisconnectionCompleteEvent(handle, ErrorCode(reason));
@@ -3646,7 +3649,8 @@ void LinkLayerController::IncomingScoDisconnect(
       incoming.GetSourceAddress().ToString().c_str());
 
   if (handle != kReservedHandle) {
-    connections_.Disconnect(handle, cancel_task_);
+    connections_.Disconnect(
+        handle, [this](TaskId task_id) { CancelScheduledTask(task_id); });
     SendDisconnectionCompleteEvent(handle, ErrorCode(reason));
   }
 }
@@ -4737,6 +4741,7 @@ void LinkLayerController::IncomingPageResponsePacket(
 }
 
 void LinkLayerController::Tick() {
+  RunPendingTasks();
   if (inquiry_timer_task_id_ != kInvalidTaskId) {
     Inquiry();
   }
@@ -4782,47 +4787,6 @@ void LinkLayerController::RegisterRemoteChannel(
         void(std::shared_ptr<model::packets::LinkLayerPacketBuilder>, Phy::Type,
              int8_t)>& send_to_remote) {
   send_to_remote_ = send_to_remote;
-}
-
-void LinkLayerController::RegisterTaskScheduler(
-    std::function<AsyncTaskId(milliseconds, TaskCallback)> task_scheduler) {
-  schedule_task_ = task_scheduler;
-}
-
-AsyncTaskId LinkLayerController::ScheduleTask(milliseconds delay_ms,
-                                              TaskCallback task_callback) {
-  if (schedule_task_) {
-    return schedule_task_(delay_ms, std::move(task_callback));
-  }
-  LOG_ERROR("Unable to schedule task on delay");
-  return 0;
-}
-
-AsyncTaskId LinkLayerController::SchedulePeriodicTask(
-    milliseconds delay_ms, milliseconds period_ms, TaskCallback task_callback) {
-  if (schedule_periodic_task_) {
-    return schedule_periodic_task_(delay_ms, period_ms,
-                                   std::move(task_callback));
-  }
-  LOG_ERROR("Unable to schedule task on delay");
-  return 0;
-}
-
-void LinkLayerController::RegisterPeriodicTaskScheduler(
-    std::function<AsyncTaskId(milliseconds, milliseconds, TaskCallback)>
-        periodic_task_scheduler) {
-  schedule_periodic_task_ = periodic_task_scheduler;
-}
-
-void LinkLayerController::CancelScheduledTask(AsyncTaskId task_id) {
-  if (schedule_task_ && cancel_task_) {
-    cancel_task_(task_id);
-  }
-}
-
-void LinkLayerController::RegisterTaskCancel(
-    std::function<void(AsyncTaskId)> task_cancel) {
-  cancel_task_ = task_cancel;
 }
 
 #ifdef ROOTCANAL_LMP
@@ -5453,7 +5417,8 @@ ErrorCode LinkLayerController::Disconnect(uint16_t handle, ErrorCode reason) {
     SendLinkLayerPacket(model::packets::ScoDisconnectBuilder::Create(
         GetAddress(), remote, static_cast<uint8_t>(reason)));
 
-    connections_.Disconnect(handle, cancel_task_);
+    connections_.Disconnect(
+        handle, [this](TaskId task_id) { CancelScheduledTask(task_id); });
     SendDisconnectionCompleteEvent(handle, reason);
     return ErrorCode::SUCCESS;
   }
@@ -5473,7 +5438,8 @@ ErrorCode LinkLayerController::Disconnect(uint16_t handle, ErrorCode reason) {
       SendLinkLayerPacket(model::packets::ScoDisconnectBuilder::Create(
           GetAddress(), remote.GetAddress(), static_cast<uint8_t>(reason)));
 
-      connections_.Disconnect(sco_handle, cancel_task_);
+      connections_.Disconnect(
+          sco_handle, [this](TaskId task_id) { CancelScheduledTask(task_id); });
       SendDisconnectionCompleteEvent(sco_handle, reason);
     }
 
@@ -5487,7 +5453,8 @@ ErrorCode LinkLayerController::Disconnect(uint16_t handle, ErrorCode reason) {
         static_cast<uint8_t>(reason)));
   }
 
-  connections_.Disconnect(handle, cancel_task_);
+  connections_.Disconnect(
+      handle, [this](TaskId task_id) { CancelScheduledTask(task_id); });
   SendDisconnectionCompleteEvent(handle, ErrorCode(reason));
 #ifdef ROOTCANAL_LMP
   if (is_br_edr) {
@@ -6371,7 +6338,7 @@ void LinkLayerController::IncomingPingRequest(
       incoming.GetDestinationAddress(), incoming.GetSourceAddress()));
 }
 
-AsyncTaskId LinkLayerController::StartScoStream(Address address) {
+TaskId LinkLayerController::StartScoStream(Address address) {
   auto sco_builder = bluetooth::hci::ScoBuilder::Create(
       connections_.GetScoHandle(address), PacketStatusFlag::CORRECTLY_RECEIVED,
       {0, 0, 0, 0, 0});
@@ -6389,4 +6356,61 @@ AsyncTaskId LinkLayerController::StartScoStream(Address address) {
     SendScoToRemote(sco_view);
   });
 }
+
+TaskId LinkLayerController::ScheduleTask(std::chrono::milliseconds delay,
+                                         TaskCallback task_callback) {
+  TaskId task_id = task_counter_++;
+  task_queue_.emplace(std::chrono::steady_clock::now() + delay,
+                      std::move(task_callback), task_id);
+  return task_id;
+}
+
+TaskId LinkLayerController::SchedulePeriodicTask(
+    std::chrono::milliseconds delay, std::chrono::milliseconds period,
+    TaskCallback task_callback) {
+  TaskId task_id = task_counter_++;
+  task_queue_.emplace(std::chrono::steady_clock::now() + delay, period,
+                      std::move(task_callback), task_id);
+  return task_id;
+}
+
+void LinkLayerController::CancelScheduledTask(TaskId task_id) {
+  auto it = task_queue_.cbegin();
+  for (; it != task_queue_.cend(); it++) {
+    if (it->task_id == task_id) {
+      task_queue_.erase(it);
+      return;
+    }
+  }
+}
+
+void LinkLayerController::RunPendingTasks() {
+  std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+  auto it = task_queue_.begin();
+  auto next = task_queue_.begin();
+  for (; it != task_queue_.end(); it = next) {
+    next = it;
+    next++;
+    if (it->time > now) {
+      continue;
+    }
+    // Run periodic tasks as many times as the number of
+    // periods that elapsed since the last synchronization point.
+    if (it->periodic) {
+      Task task = *it;
+      while (task.time <= now) {
+        task.callback();
+        task.time += task.period;
+      }
+      task_queue_.erase(it);
+      task_queue_.insert(task);
+    }
+    // Run non-periodic tasks once and remove the task.
+    else {
+      it->callback();
+      task_queue_.erase(it);
+    }
+  }
+}
+
 }  // namespace rootcanal
