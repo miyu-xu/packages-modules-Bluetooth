95eac866d3ac8c50f4ade92c3b6d21f848bb53a3
Rahul Arya <aryarahul@google.com>
2023-02-22 02:11:55 +0000

    [Private GATT] Add support for descriptors
    Bug: 255880936
    Test: unit
    Change-Id: I62a0f715cbf27e5a895dcdb39cd9f06233fb19ea

--- system/rust/src/gatt/server/att_server_bearer.rs
+++ system/rust/src/gatt/server/att_server_bearer.rs
@@ -199,6 +199,7 @@ mod test {
         core::{shared_box::SharedBox, uuid::Uuid},
         gatt::{
             callbacks::GattDatastore,
+            ffi::AttributeBackingType,
             ids::ConnectionId,
             mocks::mock_datastore::{MockDatastore, MockDatastoreEvents},
             server::{
@@ -313,11 +314,13 @@ mod test {
                     handle: VALID_HANDLE,
                     type_: Uuid::new(2),
                     permissions: AttPermissions::READABLE,
+                    descriptors: vec![],
                 },
                 GattCharacteristicWithHandle {
                     handle: ANOTHER_VALID_HANDLE,
                     type_: Uuid::new(2),
                     permissions: AttPermissions::READABLE,
+                    descriptors: vec![],
                 },
             ],
         })
@@ -346,7 +349,7 @@ mod test {
             });
             AttServerBearer::handle_packet(&conn.as_ref(), req2.view());
             // handle first reply
-            let MockDatastoreEvents::ReadCharacteristic(CONN_ID, VALID_HANDLE, data_resp) =
+            let MockDatastoreEvents::Read(CONN_ID, VALID_HANDLE, AttributeBackingType::Characteristic, data_resp) =
                 data_rx.recv().await.unwrap() else {
                     unreachable!();
             };
