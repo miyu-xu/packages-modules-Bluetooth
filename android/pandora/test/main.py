502e29fd41eeb9d190178ce07292e165d5f97d1d
bidsharma <bidsharma@google.com>
2023-02-22 19:12:33 +0000

    [SMP] Remove duplicate device record
    Dlete old duplicate device record when a BLE device bonds again
    using new random address and IRK but the same identity address.
    
    Bug: 265454970
    Tag: #refactor
    Test: manual
    
    Change-Id: I23167da80a9fc2cebc0e6d513ecd60081ea50a0b

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -3,6 +3,7 @@ from avatar import bumble_server
 
 import example
 import asha_test, gatt_test
+import smp_test
 
 import logging
 import sys
@@ -10,11 +11,11 @@ import sys
 from bumble_experimental.gatt import GATTService
 from pandora_experimental.gatt_grpc_aio import add_GATTServicer_to_server
 
-_TEST_CLASSES_LIST = [example.ExampleTest, asha_test.ASHATest, gatt_test.GattTest]
+_TEST_CLASSES_LIST = [example.ExampleTest, asha_test.ASHATest, gatt_test.GattTest, smp_test.SMPTest]
 
 
 def _bumble_servicer_hook(server: bumble_server.Server) -> None:
-  add_GATTServicer_to_server(GATTService(server.bumble.device), server.server)
+    add_GATTServicer_to_server(GATTService(server.bumble.device), server.server)
 
 
 if __name__ == "__main__":
@@ -23,10 +24,10 @@ if __name__ == "__main__":
   # This is a hack because of `b/166468397`
   argv = sys.argv[idx+1:] if (idx := sys.argv.index('--')) else sys.argv[1:]
 
-  # Mobly tradefed is using these arguments for specific java tests
+    # Mobly tradefed is using these arguments for specific java tests
   argv = [arg for arg in argv if not arg.startswith(('--device_serial', '--log_path'))]
 
-  # register experimental bumble servicers hook.
+    # register experimental bumble servicers hook.
   bumble_server.register_servicer_hook(_bumble_servicer_hook)
 
   suite_runner.run_suite(  # type: ignore
--- system/stack/btm/btm_dev.cc
+++ system/stack/btm/btm_dev.cc
@@ -524,6 +524,40 @@ void btm_dev_consolidate_existing_connections(const RawAddress& bd_addr) {
   }
 }
 
+bool btm_sec_delete_duplicate_device(const RawAddress& pairing_bd_addr,
+                                     const RawAddress& identity_bd_addr) {
+  LOG_DEBUG("%s=> pairing_bd_addr: %s and identity_bd_addr: %s", __func__,
+            ADDRESS_TO_LOGGABLE_CSTR(pairing_bd_addr),
+            ADDRESS_TO_LOGGABLE_CSTR(identity_bd_addr));
+  if (btm_cb.sec_dev_rec == nullptr) return false;
+
+  list_node_t* end = list_end(btm_cb.sec_dev_rec);
+  for (list_node_t* node = list_begin(btm_cb.sec_dev_rec); node != end;
+       node = list_next(node)) {
+    tBTM_SEC_DEV_REC* p_dev_rec =
+        static_cast<tBTM_SEC_DEV_REC*>(list_node(node));
+
+    LOG_DEBUG(
+        "Found device record with bd_addr:%s and "
+        "identity_bd_addr:%s",
+        ADDRESS_TO_LOGGABLE_CSTR(p_dev_rec->ble.pseudo_addr),
+        ADDRESS_TO_LOGGABLE_CSTR(
+            p_dev_rec->ble.identity_address_with_type.bda));
+
+    if (p_dev_rec->ble.identity_address_with_type.bda == identity_bd_addr &&
+        p_dev_rec->ble.pseudo_addr != pairing_bd_addr) {
+      LOG_DEBUG(
+          "Removing duplicate device record with bd_addr:%s and "
+          "identity_bd_addr:%s",
+          ADDRESS_TO_LOGGABLE_CSTR(p_dev_rec->ble.pseudo_addr),
+          ADDRESS_TO_LOGGABLE_CSTR(identity_bd_addr));
+      BTM_SecDeleteDevice(p_dev_rec->bd_addr);
+      return true;
+    }
+  }
+  return false;
+}
+
 /*******************************************************************************
  *
  * Function         btm_find_or_alloc_dev
