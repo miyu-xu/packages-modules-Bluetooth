7190d136a290ed23b888ae2b1177421f307e90b3
Rahul Arya <aryarahul@google.com>
2023-02-24 01:00:36 +0000

    Drop packets in-flight after disconnection
    Change-Id: I4c002c252094f4c53345d26bd9b354bc5372147e
    Test: no
    Bug: no

--- tools/rootcanal/model/controller/link_layer_controller.cc
+++ tools/rootcanal/model/controller/link_layer_controller.cc
@@ -1825,6 +1825,11 @@ void LinkLayerController::IncomingAclPacket(
 
   uint16_t local_handle =
       connections_.GetHandleOnlyAddress(incoming.GetSourceAddress());
+    
+  if (local_handle == kReservedHandle) {
+      LOG_INFO("Dropping packet since connection does not exist");
+      return;
+  }
 
   std::vector<uint8_t> payload_data(acl_view.GetPayload().begin(),
                                     acl_view.GetPayload().end());
