42de05fe051bb0cd5a1efbe425d1a95118845aa0
Łukasz Rymanowski <rlukasz@google.com>
2023-02-27 10:01:01 +0000

    LeAudioService: Handle no Ringtone in the available contexts
    If remote device has removed RINGTONE context type, we should respect
    that and not try to open ISO channels before the call is accepted.
    
    Bug: 242685105
    Test: atest BluetoothInstrumentationTests
    Tag: #feature
    Change-Id: I3051965605981387d78a5a6b8218693a8c9b6c2e

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -153,6 +153,7 @@ public class LeAudioService extends ProfileService {
             mCodecStatus = null;
             mLostLeadDeviceWhileStreaming = null;
             mInbandRingtoneEnabled = isInbandRingtonEnabled;
+            mAvailableContexts = 0;
         }
 
         public Boolean mIsConnected;
@@ -162,6 +163,7 @@ public class LeAudioService extends ProfileService {
         /* This can be non empty only for the streaming time */
         BluetoothDevice mLostLeadDeviceWhileStreaming;
         Boolean mInbandRingtoneEnabled;
+        Integer mAvailableContexts;
     }
 
     private static class LeAudioDeviceDescriptor {
@@ -1505,7 +1507,9 @@ public class LeAudioService extends ProfileService {
             return;
         }
 
-        boolean isRingtoneEnabled = groupDescriptor.mIsActive;
+        boolean isRingtoneEnabled = ((groupDescriptor.mIsActive)
+                                        && ((groupDescriptor.mAvailableContexts
+                                                & BluetoothLeAudio.CONTEXT_TYPE_RINGTONE) != 0));
 
         if (DBG) {
             Log.d(TAG, "updateInbandRingtoneForTheGroup old: "
@@ -1696,6 +1700,8 @@ public class LeAudioService extends ProfileService {
                         }
                     }
                     descriptor.mDirection = direction;
+                    descriptor.mAvailableContexts = available_contexts;
+                    updateInbandRingtoneForTheGroup(groupId);
                 } else {
                     Log.e(TAG, "no descriptors for group: " + groupId);
                 }
