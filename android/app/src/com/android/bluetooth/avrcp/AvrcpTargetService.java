e2ab6f3198cf0ce8cc9006438432f262d0868502
Jakub Rotkiewicz <rotkiewicz@google.com>
2023-02-28 12:32:25 +0000

    AVRCP Player App Settings native implementation
    Tag: #feature
    Test: atest net_test_btpackets
    Test: atest AvrcpDeviceTest
    Bug: 259590659
    Change-Id: I7020192bf21334c98e998349fdc175162101b6a2

--- android/app/src/com/android/bluetooth/avrcp/AvrcpTargetService.java
+++ android/app/src/com/android/bluetooth/avrcp/AvrcpTargetService.java
@@ -500,11 +500,17 @@ public class AvrcpTargetService extends ProfileService {
     /**
      * Called from player callback to indicate new settings to remote device.
      */
-    public void sendPlayerSettings(int shuffleMode, int repeatMode) {
-        String activeDeviceAddress = getA2dpActiveDevice().getAddress();
+    public void sendPlayerSettings(int repeatMode, int shuffleMode) {
+        BluetoothDevice activeDevice = getA2dpActiveDevice();
+        if (activeDevice == null) {
+            return;
+        }
+
+        String activeDeviceAddress = activeDevice.getAddress();
         if (activeDeviceAddress == null) {
             return;
         }
+
         mNativeInterface.sendPlayerSettings(activeDeviceAddress, repeatMode, shuffleMode);
     }
 
