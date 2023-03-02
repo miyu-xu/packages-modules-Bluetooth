a20c71593ef050bdd80a78e84c03decb79798bb2
Sebastian Pickl <spickl@google.com>
2023-03-02 11:37:04 +0000

    Revert "Add support to connect LE Gatt with address type (2/2)"
    Revert submission 2403119-le-gatt-conn-with-type
    
    Reason for revert: finding culprit for https://android-build.googleplex.com/builds/tests/view?testResultId=TR91328527203720093&invocationId=I08400010136572005 v2/android-aosp-tests/device-boot-health-check-extra break
    
    Reverted changes: /q/submissionid:2403119-le-gatt-conn-with-type
    
    Change-Id: Ia4d76112ce416c218a0c5a5439e3f13fb5cd8109

--- system/stack/acl/btm_acl.cc
+++ system/stack/acl/btm_acl.cc
@@ -2789,15 +2789,12 @@ void acl_write_automatic_flush_timeout(const RawAddress& bd_addr,
   btsnd_hcic_write_auto_flush_tout(p_acl->hci_handle, flush_timeout_in_ticks);
 }
 
-bool acl_create_le_connection_with_id(uint8_t id, const RawAddress& bd_addr,
-                                      tBLE_ADDR_TYPE addr_type) {
+bool acl_create_le_connection_with_id(uint8_t id, const RawAddress& bd_addr) {
   tBLE_BD_ADDR address_with_type{
       .bda = bd_addr,
-      .type = addr_type,
+      .type = BLE_ADDR_PUBLIC,
   };
-  if (address_with_type.type == BLE_ADDR_PUBLIC) {
-    gatt_find_in_device_record(bd_addr, &address_with_type);
-  }
+  gatt_find_in_device_record(bd_addr, &address_with_type);
   LOG_DEBUG("Creating le direct connection to:%s",
             ADDRESS_TO_LOGGABLE_CSTR(address_with_type));
 
@@ -2809,13 +2806,9 @@ bool acl_create_le_connection_with_id(uint8_t id, const RawAddress& bd_addr,
     return false;
   }
 
-  bluetooth::shim::ACL_AcceptLeConnectionFrom(address_with_type,
-                                              /* is_direct */ true);
-  return true;
-}
-
-bool acl_create_le_connection_with_id(uint8_t id, const RawAddress& bd_addr) {
-  return acl_create_le_connection_with_id(id, bd_addr, BLE_ADDR_PUBLIC);
+    bluetooth::shim::ACL_AcceptLeConnectionFrom(address_with_type,
+                                                /* is_direct */ true);
+    return true;
 }
 
 bool acl_create_le_connection(const RawAddress& bd_addr) {
