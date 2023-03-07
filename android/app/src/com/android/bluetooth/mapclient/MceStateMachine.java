a91f5f9297f6966af9917c7c1c044ff519f01d16
Hui Peng <phui@google.com>
2023-03-07 22:55:36 +0000

    [redact_log]Redact addresses java code of  of bt app
    Bug: 174487588
    Test: refactoring CL, exiting tests still pass
    Tag: security
    Change-Id: Id4646ef907421aab707f4d8ff09ad577b183d416

--- android/app/src/com/android/bluetooth/mapclient/MceStateMachine.java
+++ android/app/src/com/android/bluetooth/mapclient/MceStateMachine.java
@@ -285,7 +285,7 @@ class MceStateMachine extends StateMachine {
 
     public boolean disconnect() {
         if (DBG) {
-            Log.d(TAG, "Disconnect Request " + mDevice.getAddress());
+            Log.d(TAG, "Disconnect Request " + mDevice);
         }
         sendMessage(MSG_DISCONNECT, mDevice);
         return true;
@@ -442,7 +442,7 @@ class MceStateMachine extends StateMachine {
     }
 
     public void dump(StringBuilder sb) {
-        ProfileService.println(sb, "mCurrentDevice: " + mDevice.getAddress() + "("
+        ProfileService.println(sb, "mCurrentDevice: " + mDevice + "("
                 + Utils.getName(mDevice) + ") " + this.toString());
         if (mDatabase != null) {
             mDatabase.dump(sb);
