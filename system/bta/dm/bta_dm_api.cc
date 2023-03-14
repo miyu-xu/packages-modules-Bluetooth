292ec89e5204bfc0d2eeef7207d615d4ca75e8b2
Chris Manton <cmanton@google.com>
2023-03-14 16:00:13 -0700

    Remove unused bta::dm::BTA_VendorInit
    Bug:191555414
    Test: compiles
    
    Change-Id: I413877a55bf4014342e8619be155d3bd8db12c94

--- system/bta/dm/bta_dm_api.cc
+++ system/bta/dm/bta_dm_api.cc
@@ -572,17 +572,6 @@ void BTA_DmBleCsisObserve(bool observe, tBTA_DM_SEARCH_CBACK* p_results_cb) {
 
 /*******************************************************************************
  *
- * Function         BTA_VendorInit
- *
- * Description      This function initializes vendor specific
- *
- * Returns          void
- *
- ******************************************************************************/
-void BTA_VendorInit(void) { APPL_TRACE_API("BTA_VendorInit"); }
-
-/*******************************************************************************
- *
  * Function         BTA_DmClearEventFilter
  *
  * Description      This function clears the event filter
