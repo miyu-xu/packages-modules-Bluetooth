5673ca3f6cd0246e7a0b4c7842133d13742364ae
Sungsoo Lim <sungsoo@google.com>
2023-03-14 04:05:59 +0000

    Check ActivityManager.getCurrentUser()
    There is some a timing issue of sForegroundUserId update
    when switching users. To avoid such issue, add a check of
    ActivityManager.getCurrentUser() that updated the user id
    when the switch user started.
    
    Bug: Bug: 269973219
    Test: atest BluetoothInstrumentationTests
    Change-Id: I76441c1b2880a56a5629c0b23a298e9373192751

--- android/app/src/com/android/bluetooth/Utils.java
+++ android/app/src/com/android/bluetooth/Utils.java
@@ -34,6 +34,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.RequiresPermission;
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
 import android.app.BroadcastOptions;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -485,6 +486,7 @@ public final class Utils {
         if (isInstrumentationTestMode()) {
             return true;
         }
+
         // STOPSHIP(b/188391719): enable this security enforcement
         // attributionSource.enforceCallingUid();
         AttributionSource currentAttribution = new AttributionSource
@@ -652,6 +654,7 @@ public final class Utils {
         UserHandle callingUser = UserHandle.getUserHandleForUid(callingUid);
 
         return (sForegroundUserId == callingUser.getIdentifier())
+                || (sForegroundUserId == ActivityManager.getCurrentUser())
                 || (UserHandle.getAppId(sSystemUiUid) == UserHandle.getAppId(callingUid))
                 || (UserHandle.getAppId(Process.SYSTEM_UID) == UserHandle.getAppId(callingUid));
     }
@@ -702,6 +705,7 @@ public final class Utils {
 
             // Always allow SystemUI/System access.
             return (sForegroundUserId == callingUser.getIdentifier())
+                    //|| (sForegroundUserId == ActivityManager.getCurrentUser())
                     || (sForegroundUserId == parentUser)
                     || (UserHandle.getAppId(sSystemUiUid) == UserHandle.getAppId(callingUid))
                     || (UserHandle.getAppId(Process.SYSTEM_UID) == UserHandle.getAppId(callingUid));
@@ -714,9 +718,6 @@ public final class Utils {
     }
 
     public static boolean checkCallerIsSystemOrActiveOrManagedUser(Context context, String tag) {
-        if (isInstrumentationTestMode()) {
-            return true;
-        }
         final boolean res = checkCallerIsSystemOrActiveOrManagedUser(context);
         if (!res) {
             Log.w(TAG, tag + " - Not allowed for"
