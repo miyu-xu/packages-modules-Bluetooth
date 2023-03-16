03e8a06cd5f063586a6c3de81bd92d1536acacfd
Hoa Xuan <hoax@google.com>
2023-03-16 12:54:01 +0100

    gd: switch to use new timeout properties
    Watches take longer to finish downloading firmware when the system
    is busy. ag/21548077 sets BLE timeout in Java to 8 seconds for Wear.
    So, switch to use new timeout properties:
    - bluetooth.gd.hci_hal.start_timeout
    - bluetooth.gd.hci_layer.timeout
    - bluetooth.gd.start_timeout
    - bluetooth.gd.stop_timeout
    These properties are in miliseconds, and can be overridden by targets.
    
    Tag: #stability
    NO_IFTTT= no impact on fuzz_hci_layer
    Bug: 271859293
    Test:
     Run the following script for 3 hours and
     check if there is no BT timeout issues
    
    Script: while true
     do
      adb shell su 0 svc bluetooth enable
      sleep 10
      adb shell su 0 svc bluetooth disable
      sleep 5
     done
    Change-Id: Ia8388ef8176adc7ddddb9e642cb69bb94b4b452e

--- system/gd/hci/hci_layer_test.cc
+++ system/gd/hci/hci_layer_test.cc
@@ -63,7 +63,7 @@ namespace bluetooth {
 namespace hci {
 namespace {
 
-constexpr std::chrono::milliseconds kTimeout = HciLayer::kHciTimeoutMs / 2;
+const std::chrono::milliseconds kTimeout = HciLayer::get_hci_timeout_ms() / 2;
 
 class DependsOnHci : public Module {
  public:
@@ -328,7 +328,7 @@ TEST_F(HciTest, DISABLED_hciTimeOut) {
   auto reset = ResetView::Create(*sent_command);
   ASSERT_TRUE(reset.IsValid());
 
-  auto event = upper->GetReceivedEvent(HciLayer::kHciTimeoutMs);
+  auto event = upper->GetReceivedEvent(HciLayer::get_hci_timeout_ms());
   ASSERT_FALSE(event.has_value());
 
   sent_command = hal->GetSentCommand();
