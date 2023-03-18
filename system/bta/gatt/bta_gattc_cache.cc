1d0844040f884bbeec53ec0e1b6f7a4434df27e2
Rahul Arya <aryarahul@google.com>
2023-03-18 00:39:25 +0000

    Skip service discovery if robust caching is unavailable
    For a bonded peer device for which we know robust caching is unavailable, do
    not perform service discovery on connection, since we will get a service
    changed indication if we need to rediscover.
    
    Thus, our behavior is:
    1. If cached DB is empty, do discovery
    2. If not bonded, do discovery
    3. If we know the peer supports DB hash from a prior discovery, do discovery
    4. Else, skip discovery
    
    Then, while doing discovery, skip the DB hash read if it is known to be
    unsupported.
    
    Test: manual: connect to a bonded phone, verify the db hash is read; connect to
    a HID device, verify that discovery is skipped
    Bug: 273302622
    
    Change-Id: Ie88185d4f137a85b7db8ab0523d2ce950a4824ae

--- system/bta/gatt/bta_gattc_cache.cc
+++ system/bta/gatt/bta_gattc_cache.cc
@@ -36,6 +36,7 @@
 #include "bt_target.h"  // Must be first to define build configuration
 #include "bta/gatt/bta_gattc_int.h"
 #include "bta/gatt/database.h"
+#include "device/include/interop.h"
 #include "osi/include/allocator.h"
 #include "osi/include/log.h"
 #include "stack/btm/btm_sec.h"
@@ -120,6 +121,73 @@ const Service* bta_gattc_find_matching_service(
   }
 
   return nullptr;
+}
+
+/// Whether the peer device uses robust caching
+RobustCachingSupport GetRobustCachingSupport(const tBTA_GATTC_CLCB* p_clcb,
+                                             const gatt::Database& db) {
+  // If the feature is disabled, then we never support it
+  if (!bta_gattc_is_robust_caching_enabled()) {
+    return RobustCachingSupport::UNSUPPORTED;
+  }
+
+  // An empty database means that discovery hasn't taken place yet, so
+  // we can't infer anything from that
+  if (!db.IsEmpty()) {
+    // Here, we can simply check whether the database hash is present
+    for (const auto& service : db.Services()) {
+      if (service.uuid.As16Bit() != UUID_SERVCLASS_GATT_SERVER) {
+        continue;
+      }
+      for (const auto& characteristic : service.characteristics) {
+        if (characteristic.uuid.As16Bit() == GATT_UUID_DATABASE_HASH) {
+          // the hash was found, so we should read it
+          return RobustCachingSupport::SUPPORTED;
+        }
+      }
+    }
+
+    // The database hash characteristic was not found, so there's no point
+    // searching for it. Even if the hash was previously not present but is now,
+    // we will still get the service changed indication, so there's no need to
+    // speculatively check for the hash every time.
+    return RobustCachingSupport::UNSUPPORTED;
+  }
+
+  // This is workaround for the embedded devices being already on the market
+  // and having a serious problem with handle Read By Type with
+  // GATT_UUID_DATABASE_HASH. With this workaround, Android will assume that
+  // embedded device having LMP version lower than 5.1 (0x0a), it does not
+  // support GATT Caching.
+  uint8_t lmp_version = 0;
+  if (!BTM_ReadRemoteVersion(p_clcb->bda, &lmp_version, nullptr, nullptr)) {
+    LOG_WARN("Could not read remote version for %s",
+             ADDRESS_TO_LOGGABLE_CSTR(p_clcb->bda));
+  }
+
+  if (lmp_version < 0x0a) {
+    LOG_WARN(
+        " Device LMP version 0x%02x < Bluetooth 5.1. Ignore database cache "
+        "read.",
+        lmp_version);
+    return RobustCachingSupport::UNSUPPORTED;
+  }
+
+  // Some LMP 5.2 devices also don't support robust caching. This workaround
+  // conditionally disables the feature based on a combination of LMP
+  // version and OUI prefix.
+  if (lmp_version < 0x0c &&
+      interop_match_addr(INTEROP_DISABLE_ROBUST_CACHING, &p_clcb->bda)) {
+    LOG_WARN(
+        "Device LMP version 0x%02x <= Bluetooth 5.2 and MAC addr on "
+        "interop list, skipping robust caching",
+        lmp_version);
+    return RobustCachingSupport::UNSUPPORTED;
+  }
+
+  // If we have no cached database and no interop considerations,
+  // it is unknown whether or not robust caching is supported
+  return RobustCachingSupport::UNKNOWN;
 }
 
 /** Start primary service discovery */
