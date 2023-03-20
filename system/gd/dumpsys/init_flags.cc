d19d4a6b847e3d2d1a8f350960bc32ae8d97f60b
David Duarte <licorne@google.com>
2023-03-20 04:56:53 +0000

    Unplug gd rust
    Test: TreeHugger
    Change-Id: I741d570daf1ac7b295a3329f475ff7c14b0b661d

--- system/gd/dumpsys/init_flags.cc
+++ system/gd/dumpsys/init_flags.cc
@@ -63,7 +63,6 @@ flatbuffers::Offset<bluetooth::common::InitFlagsData> bluetooth::dumpsys::InitFl
   builder.add_gd_l2cap_is_enabled(initFlags::gd_l2cap_is_enabled());
   builder.add_gd_link_policy_is_enabled(initFlags::gd_link_policy_is_enabled());
   builder.add_gd_remote_name_request_is_enabled(initFlags::gd_remote_name_request_is_enabled());
-  builder.add_gd_rust_is_enabled(initFlags::gd_rust_is_enabled());
   builder.add_gd_security_is_enabled(initFlags::gd_security_is_enabled());
   builder.add_get_default_log_level(initFlags::get_default_log_level());
   builder.add_get_hci_adapter(initFlags::get_hci_adapter());
