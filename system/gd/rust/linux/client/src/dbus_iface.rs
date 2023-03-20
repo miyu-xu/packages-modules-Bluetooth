2bec6463ee306a0c9451cc3f39ff351477bf32d0
Hsin-chen Chuang <chharry@google.com>
2023-03-20 15:28:25 +0800

    floss: btclient: Add MPS SDP record on telephony enabled
    Bug: 264820755
    Tag: #floss
    Test: MPS/SDR/SDP/CTH/SD/BV-01-I
    
    Change-Id: I2df5c5cb224b1f16f5e268eb28a2e1fe90db74e6

--- system/gd/rust/linux/client/src/dbus_iface.rs
+++ system/gd/rust/linux/client/src/dbus_iface.rs
@@ -7,8 +7,9 @@ use bt_topshim::btif::{
 use bt_topshim::profiles::gatt::{AdvertisingStatus, GattStatus, LePhy};
 use bt_topshim::profiles::hid_host::BthhReportType;
 use bt_topshim::profiles::sdp::{
-    BtSdpDipRecord, BtSdpHeaderOverlay, BtSdpMasRecord, BtSdpMnsRecord, BtSdpOpsRecord,
-    BtSdpPceRecord, BtSdpPseRecord, BtSdpRecord, BtSdpSapRecord, BtSdpType, SupportedFormatsList,
+    BtSdpDipRecord, BtSdpHeaderOverlay, BtSdpMasRecord, BtSdpMnsRecord, BtSdpMpsRecord,
+    BtSdpOpsRecord, BtSdpPceRecord, BtSdpPseRecord, BtSdpRecord, BtSdpSapRecord, BtSdpType,
+    SupportedFormatsList,
 };
 use bt_topshim::profiles::socket::SocketType;
 use bt_topshim::profiles::ProfileConnectionState;
@@ -181,6 +182,14 @@ struct BtSdpDipRecordDBus {
     primary_record: bool,
 }
 
+#[dbus_propmap(BtSdpMpsRecord)]
+pub struct BtSdpMpsRecordDBus {
+    hdr: BtSdpHeaderOverlay,
+    supported_scenarios_mpsd: u64,
+    supported_scenarios_mpmd: u64,
+    supported_dependencies: u16,
+}
+
 fn read_propmap_value<T: 'static + DirectDBus>(
     propmap: &dbus::arg::PropMap,
     key: &str,
@@ -278,6 +287,10 @@ impl DBusArg for BtSdpRecord {
                 let arg_0 = parse_propmap_value::<BtSdpDipRecord>(&data, "0")?;
                 BtSdpRecord::Dip(arg_0)
             }
+            BtSdpType::Mps => {
+                let arg_0 = parse_propmap_value::<BtSdpMpsRecord>(&data, "0")?;
+                BtSdpRecord::Mps(arg_0)
+            }
         };
         Ok(record)
     }
@@ -313,6 +326,9 @@ impl DBusArg for BtSdpRecord {
             }
             BtSdpRecord::Dip(dip_record) => {
                 write_propmap_value::<BtSdpDipRecord>(&mut map, dip_record, &String::from("0"))?
+            }
+            BtSdpRecord::Mps(mps_record) => {
+                write_propmap_value::<BtSdpMpsRecord>(&mut map, mps_record, &String::from("0"))?
             }
         }
         Ok(map)
