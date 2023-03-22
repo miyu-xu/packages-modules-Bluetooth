e1bbf83f62fef73c41f13c80f56eeb81f424df44
bidsharmag <bidsharma@google.com>
2023-03-22 15:38:46 +0000

    [Avatar Test] Remove duplicate device record
    Bug: 265454970
    Tag: #refactor
    Test: atest avatar:SMPTest.test_le_pairing__twice_with_same_device -v
    
    Change-Id: I41aecc87832a88c827e4972f884713ecbd9f1c68

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -7,6 +7,7 @@ import asha_test
 import example
 import gatt_test
 import le_advertising_test
+import smp_test
 import logging
 import os
 import sys
@@ -20,6 +21,7 @@ _TEST_CLASSES_LIST = [
     asha_test.ASHATest,
     gatt_test.GattTest,
     le_advertising_test.LeAdvertisingTest,
+    smp_test.SMPTest,
 ]
 
 
