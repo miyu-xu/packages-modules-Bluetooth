02f9d9a76d30e7c50c043edb04aa9bc76f6cbfb3
Chen Chen <cncn@google.com>
2023-03-22 15:19:40 -0700

    BluetoothMetrics: Upload readable sha256, instead of bytes
    Bug: 264937355
    Test: atest BluetoothInstrumentationTests
    Change-Id: Ica31ffcc22d5a75aa82b9e2f91b9239d63506a40

--- android/app/src/com/android/bluetooth/btservice/MetricsLogger.java
+++ android/app/src/com/android/bluetooth/btservice/MetricsLogger.java
@@ -292,12 +292,14 @@ public class MetricsLogger {
         if (matchedSha256 == null) {
             return false;
         }
-        statslogBluetoothDeviceNames(metricId, matchedString, matchedSha256);
+        statslogBluetoothDeviceNames(
+                metricId,
+                matchedString,
+                Hashing.sha256().hashString(matchedString, StandardCharsets.UTF_8).toString());
         return true;
     }
 
-    protected void statslogBluetoothDeviceNames(int metricId, String matchedString, byte[] sha256) {
-        String sha256String = new String(sha256);
+    protected void statslogBluetoothDeviceNames(int metricId, String matchedString, String sha256) {
         Log.d(TAG,
                 "Uploading sha256 hash of matched bluetooth device name: " + sha256String);
         BluetoothStatsLog.write(
