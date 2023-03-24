de7d46bdd7fe902d34dcd480331da6b9259a7483
sunxiang <sunxiang@xiaomi.com>
2023-03-24 14:20:53 +0800

    [Bluetooth][BugFix]:Fix Bluetooth unable to enable
    PIPA-5324
    
    Test apk test Bluetooth enable and disable, In rare scenarios,mBluetooth not set to null,and when apk enable Bluetooth, it will send MESSAGE_RESTART_BLUETOOTH_SERVICE
    Continuously and unable to enable BT
    
    solution: when enable BT and Reach maximum retry to restart Bluetooth, set mBluetooth null, it can enble BT successfully
    
    Signed-off-by: sunxiang <sunxiang@xiaomi.com>
    Change-Id: Ic88886042fa550825e30196c37f36c61f7c2fb6b

--- service/java/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/java/com/android/server/bluetooth/BluetoothManagerService.java
@@ -2533,6 +2533,8 @@ public class BluetoothManagerService extends IBluetoothManager.Stub {
                                 mContext.getPackageName(), true);
                         handleEnable(mQuietEnable);
                     } else {
+                        //MIUI ADD
+                        mBluetooth = null;
                         Log.e(TAG, "Reach maximum retry to restart Bluetooth!");
                     }
                     break;
