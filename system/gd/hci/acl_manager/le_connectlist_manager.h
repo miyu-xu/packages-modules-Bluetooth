a002f3b0dd4a90594e54ef1865b6c77da3552666
Rahul Arya <aryarahul@google.com>
2023-03-27 23:30:19 +0000

    [Connection Manager] Pull out connectionlist management from le_impl
    The idea is to make the state machine clearer, so it can be swapped out
    without disrupting the rest of the logic.
    
    Specifically, le_impl will handle incoming events only, and not actually
    send any initiation-related HCI commands to the controller. The
    connection manager will send commands (via the LeConnectHciManager) and
    receive events from le_impl.
    
    As a first step, we split out the GD "connection manager" so it sends
    commands and is driven by events from le_impl while the rest of le_impl does
    not send commands but is instead driven by events from the controller.
    
    Followup CLs will prepare a replacement module for
    le_connectlist_manager, with a different API, and then swap the two
    modules out (deleting most of the API of le_connectlist_manager) while
    leaving le_impl essentially unchanged.
    
    Test: all existing
    Bug: 272572974
    
    Change-Id: Icd69ced603293b5036c98176ecb0f7244c52d440

---
+++ system/gd/hci/acl_manager/le_connectlist_manager.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <gtest/gtest_prod.h>
+
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "common/callback.h"
+#include "hci/acl_manager/le_connect_hci_manager.h"
+#include "hci/address_with_type.h"
+#include "hci/le_address_manager.h"
+#include "os/alarm.h"
+#include "os/handler.h"
+
+namespace bluetooth {
+namespace hci {
+namespace acl_manager {
+
+enum class ConnectionResult {
+  Success,
+  SuccessButNotInFilterAcceptList,
+  Failure,
+  Ignore,
+};
+
+class LeConnectlistManager : public bluetooth::hci::LeAddressManagerCallback {
+ public:
+  LeConnectlistManager(
+      LeAddressManager* le_address_manager,
+      LeConnectHciManager* le_connect_hci_manager,
+      os::Handler* handler);
+  ~LeConnectlistManager();
+  LeConnectlistManager(const LeConnectlistManager&) = delete;
+  LeConnectlistManager& operator=(const LeConnectlistManager&) = delete;
+
+  // API
+  void RegisterTimeoutCallback(
+      common::ContextualCallback<void(AddressWithType, ErrorCode)> on_timeout);
+  void UnregisterTimeoutCallback();
+
+  void CreateLeConnection(
+      AddressWithType address_with_type, bool add_to_connect_list, bool is_direct);
+  void CancelConnect(AddressWithType address_with_type);
+  void OnLeDisconnect(AddressWithType address_with_type);
+
+  void AddDeviceToBackgroundConnectionList(AddressWithType address_with_type);
+  void IsOnBackgroundConnectionList(AddressWithType address_with_type, std::promise<bool> promise);
+  void RemoveDeviceFromBackgroundConnectionList(AddressWithType address_with_type);
+
+  void ClearFilterAcceptList();
+
+  // LeAddressManagerCallback
+  void OnPause() override;
+  void OnResume() override;
+
+  // HCI events
+  ConnectionResult OnConnectionComplete(
+      AddressWithType remote_address, ErrorCode status, Role role);
+
+ private:
+  void OnCancelConnectionComplete(ErrorCode status);
+
+  void ArmConnectability();
+  void DisarmConnectability();
+  void UpdateConnectabilityStateAfterArmed(ErrorCode status);
+
+  void AddDeviceToConnectList(AddressWithType address_with_type);
+  bool IsDeviceInConnectList(AddressWithType address_with_type);
+  void RemoveDeviceFromConnectList(AddressWithType address_with_type);
+
+  void RegisterWithAddressManager();
+  void CheckForUnregister();
+
+  void OnCreateConnectionTimeout(AddressWithType address_with_type);
+  void OnLeConnectionCancelledOnPause();
+
+  LeAddressManager* le_address_manager_ = nullptr;
+  LeConnectHciManager* le_connect_hci_manager_ = nullptr;
+  common::ContextualCallback<void(AddressWithType, ErrorCode)> on_timeout_;
+  os::Handler* handler_;
+
+  std::unordered_set<AddressWithType> connecting_le_{};
+  bool arm_on_resume_{};
+  std::unordered_set<AddressWithType> direct_connections_{};
+  // Set of devices that will not be removed from connect list after direct connect timeout
+  std::unordered_set<AddressWithType> background_connections_;
+  std::unordered_set<AddressWithType> connect_list;
+  bool address_manager_registered = false;
+  bool ready_to_unregister = false;
+  bool pause_connection = false;
+  bool disarmed_while_arming_ = false;
+
+  enum class ConnectabilityState {
+    DISARMED = 0,
+    ARMING = 1,
+    ARMED = 2,
+    DISARMING = 3,
+  };
+  static std::string ConnectabilityStateMachineText(const ConnectabilityState& state);
+
+  LeConnectlistManager::ConnectabilityState connectability_state_{ConnectabilityState::DISARMED};
+  std::map<AddressWithType, os::Alarm> create_connection_timeout_alarms_{};
+
+  friend class LeImplTest;
+  friend class LeImplRegisteredWithAddressManagerTest;
+  FRIEND_TEST(LeImplTest, add_device_to_connect_list);
+  FRIEND_TEST(LeImplTest, remove_device_from_connect_list);
+  FRIEND_TEST(LeImplTest, connection_complete_with_periperal_role);
+  FRIEND_TEST(LeImplTest, enhanced_connection_complete_with_periperal_role);
+  FRIEND_TEST(LeImplTest, connection_complete_with_central_role);
+  FRIEND_TEST(LeImplTest, enhanced_connection_complete_with_central_role);
+  FRIEND_TEST(LeImplTest, DISABLED_register_with_address_manager__AddressPolicyNotSet);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_DISARMED);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_DISARMED_extended);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_ARMING);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_ARMING_extended);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_ARMED);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_ARMED_extended);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_DISARMING);
+  FRIEND_TEST(LeImplTest, DISABLED_disarm_connectability_DISARMING_extended);
+  FRIEND_TEST(LeImplTest, DISABLED_register_with_address_manager__AddressPolicyPublicAddress);
+  FRIEND_TEST(LeImplTest, DISABLED_register_with_address_manager__AddressPolicyStaticAddress);
+  FRIEND_TEST(
+      LeImplTest, DISABLED_register_with_address_manager__AddressPolicyNonResolvableAddress);
+  FRIEND_TEST(LeImplTest, DISABLED_register_with_address_manager__AddressPolicyResolvableAddress);
+  FRIEND_TEST(LeImplTest, connectability_state_machine_text);
+  FRIEND_TEST(LeImplRegisteredWithAddressManagerTest, ignore_on_pause_on_resume_after_unregistered);
+  FRIEND_TEST(LeImplTest, on_le_connection_canceled_on_pause);
+  FRIEND_TEST(LeImplTest, on_create_connection_timeout);
+  FRIEND_TEST(LeImplTest, DISABLED_on_common_le_connection_complete__NoPriorConnection);
+  FRIEND_TEST(LeImplTest, cancel_connect);
+};
+
+}  // namespace acl_manager
+}  // namespace hci
+}  // namespace bluetooth
