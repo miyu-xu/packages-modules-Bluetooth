7f4ab413367d913d5121b6da94e0ba1f9d65b600
Huirong Liao <huirong.liao@mediatek.com>
2023-03-29 15:28:54 +0800

    Fix upper (framework) can not get RC's name or other properties
    [Description]
    Fix Post-submit nit: We can reduce this code by one level of indentation by removing this check
    which now must be true (due to the check on line 127).
    
    [Root Cause]
    Some RC may send directed adv packet firstly with non-random address,
    and no name info included, then upper can not get RC's name correctly.
    
    [Solution]
    Update device name property if the device name exists without check the address type.
    
    Bug: 266249323
    Test: ble scan successfully
    
    Change-Id: I3938bfbc26b2b9261f947b051d5a41fc4f9796f0

--- system/btif/src/btif_ble_scanner.cc
+++ system/btif/src/btif_ble_scanner.cc
@@ -128,24 +128,22 @@ void bta_scan_results_cb_impl(RawAddress bd_addr, tBT_DEVICE_TYPE device_type,
     if (!btif_address_cache_find(bd_addr)) {
       btif_address_cache_add(bd_addr, addr_type);
 
-      if (p_eir_remote_name) {
-        if (remote_name_len > BD_NAME_LEN + 1 ||
-            (remote_name_len == BD_NAME_LEN + 1 &&
-             p_eir_remote_name[BD_NAME_LEN] != '\0')) {
-          LOG_INFO("%s dropping invalid packet - device name too long: %d",
-                   __func__, remote_name_len);
-          return;
-        }
-
-        bt_bdname_t bdname;
-        memcpy(bdname.name, p_eir_remote_name, remote_name_len);
-        if (remote_name_len < BD_NAME_LEN + 1)
-          bdname.name[remote_name_len] = '\0';
-
-        LOG_VERBOSE("%s BLE device name=%s len=%d dev_type=%d", __func__,
-                    bdname.name, remote_name_len, device_type);
-        btif_dm_update_ble_remote_properties(bd_addr, bdname.name, device_type);
+      if (remote_name_len > BD_NAME_LEN + 1 ||
+          (remote_name_len == BD_NAME_LEN + 1 &&
+           p_eir_remote_name[BD_NAME_LEN] != '\0')) {
+        LOG_INFO("%s dropping invalid packet - device name too long: %d",
+                 __func__, remote_name_len);
+        return;
       }
+
+      bt_bdname_t bdname;
+      memcpy(bdname.name, p_eir_remote_name, remote_name_len);
+      if (remote_name_len < BD_NAME_LEN + 1)
+        bdname.name[remote_name_len] = '\0';
+
+      LOG_VERBOSE("%s BLE device name=%s len=%d dev_type=%d", __func__,
+                  bdname.name, remote_name_len, device_type);
+      btif_dm_update_ble_remote_properties(bd_addr, bdname.name, device_type);
     }
   }
 
