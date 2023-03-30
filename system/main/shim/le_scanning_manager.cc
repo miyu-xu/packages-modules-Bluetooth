a554f9d52e3f6d1192bbc70baf1dd849f1c75e62
bidsharmag <bidsharma@google.com>
2023-03-30 23:36:36 +0000

    Get device type using adv flag data
    Device type is incorrectly set to LE only if flag data is missing.
    The CL does the following to set the device type:
    1. If adv is connectable and flag data is not present, it is DUAL mode.
    2. If adv is not connectable and flag data is not present, it is
       UNKNOWN.
    3. If flag data is present, use `BR/EDR Not Supported` bit
    
    Bug: 273890240
    Tag: #refactor
    Test: manual
    
    Change-Id: I773680f865e3f786e6b9805568e93aa72f10a799

--- system/main/shim/le_scanning_manager.cc
+++ system/main/shim/le_scanning_manager.cc
@@ -513,8 +513,8 @@ void BleScannerInterfaceImpl::OnScanResult(
   do_in_jni_thread(
       FROM_HERE,
       base::BindOnce(&BleScannerInterfaceImpl::handle_remote_properties,
-                     base::Unretained(this), raw_address, ble_addr_type,
-                     advertising_data));
+                     base::Unretained(this), event_type, raw_address,
+                     ble_addr_type, advertising_data));
 
   do_in_jni_thread(
       FROM_HERE,
@@ -726,7 +726,7 @@ bool BleScannerInterfaceImpl::parse_filter_command(
 }
 
 void BleScannerInterfaceImpl::handle_remote_properties(
-    RawAddress bd_addr, tBLE_ADDR_TYPE addr_type,
+    uint16_t event_type, RawAddress bd_addr, tBLE_ADDR_TYPE addr_type,
     std::vector<uint8_t> advertising_data) {
   if (!bluetooth::shim::is_gd_stack_started_up()) {
     LOG_WARN("Gd stack is stopped, return");
@@ -738,15 +738,36 @@ void BleScannerInterfaceImpl::handle_remote_properties(
     return;
   }
 
-  auto device_type = bluetooth::hci::DeviceType::LE;
   uint8_t flag_len;
   const uint8_t* p_flag = AdvertiseDataParser::GetFieldByType(
       advertising_data, BTM_BLE_AD_TYPE_FLAG, &flag_len);
-  if (p_flag != NULL && flag_len != 0) {
-    if ((BTM_BLE_BREDR_NOT_SPT & *p_flag) == 0) {
-      device_type = bluetooth::hci::DeviceType::DUAL;
-    }
+  auto device_type = bluetooth::hci::DeviceType::UNKNOWN;
+  bool is_adv_connectable = event_type & (1 << BLE_EVT_CONNECTABLE_BIT);
+  bool has_flag_data = (p_flag != NULL && flag_len != 0);
+
+  /**
+    1. If adv is connectable and flag data is not present, device type is DUAL
+    mode.
+    2. If adv is not connectable and flag data is not present, device type is
+    UNKNOWN.
+    3. If flag data is present, use `BR/EDR Not Supported` bit to find device
+    type.
+  */
+  if (is_adv_connectable) {
+    device_type = has_flag_data && ((BTM_BLE_BREDR_NOT_SPT & *p_flag) ==
+                                    BTM_BLE_BREDR_NOT_SPT)
+                      ? bluetooth::hci::DeviceType::LE
+                      : bluetooth::hci::DeviceType::DUAL;
+  } else if (has_flag_data) {
+    device_type = (BTM_BLE_BREDR_NOT_SPT & *p_flag) == BTM_BLE_BREDR_NOT_SPT
+                      ? bluetooth::hci::DeviceType::LE
+                      : bluetooth::hci::DeviceType::DUAL;
   }
+  LOG_DEBUG(
+      "%s event_type: %d, is_adv_connectable: %d, flag data: %d, device_type: "
+      "%d",
+      __func__, event_type, is_adv_connectable, (p_flag == NULL ? 0 : *p_flag),
+      device_type);
 
   uint8_t remote_name_len;
   const uint8_t* p_eir_remote_name = AdvertiseDataParser::GetFieldByType(
