b65bd16171326cc2c09459ce5ddbb61698addc77
Rahul Arya <aryarahul@google.com>
2023-03-30 02:04:47 +0000

    [Connection Manager] Add FFI to access le_impl from Rust
    This uses the API documented in le_manager.rs. This is an interim solution,
    since the API does not match what would be ideal, but it works for now.
    
    Bug: 272572974
    Test: unit
    Change-Id: Ifc38fcd868c7fef295f89b1c65bcaa8879146890

--- system/rust/src/core/mod.rs
+++ system/rust/src/core/mod.rs
@@ -12,18 +12,23 @@ use bt_common::init_flags::rust_event_loop_is_enabled;
 use cxx::UniquePtr;
 
 use crate::{
+    connection::{LeAclManagerImpl, LeAclManagerShim},
     gatt::ffi::{AttTransportImpl, GattCallbacksImpl},
     GlobalModuleRegistry,
 };
 
 use self::ffi::GattServerCallbacks;
 
-fn init(gatt_server_callbacks: UniquePtr<GattServerCallbacks>) {
+fn init(
+    gatt_server_callbacks: UniquePtr<GattServerCallbacks>,
+    le_acl_manager: UniquePtr<LeAclManagerShim>,
+) {
     if rust_event_loop_is_enabled() {
         thread::spawn(move || {
             GlobalModuleRegistry::start(
                 Rc::new(GattCallbacksImpl(gatt_server_callbacks)),
                 Rc::new(AttTransportImpl()),
+                LeAclManagerImpl(le_acl_manager),
             );
         });
     }
