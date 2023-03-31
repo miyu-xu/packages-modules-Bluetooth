5976cb6aae72f97f22baca26c19ae1e42d98facb
hoax <hoax@google.com>
2023-03-31 08:16:05 +0000

    gd: hci_hal: switch HCI HAL start timeout to use new sysprop
    Watches take longer to find the HAL service. This adds new property
    bluetooth.gd.hci_hal.start_timeout, so that the timeout can be adjusted
    by targets.
    
    Tag: #stability
    NO_IFTTT= no impact on fuzz_hci_layer
    Bug: 271859293
    Test:
     Run the following script for 3 hours and
     check if there is no BT timeout issues.
    Script:
     while true
     do
       adb shell su 0 svc bluetooth enable
       sleep 10
       adb shell su 0 svc bluetooth disable
       sleep 5
     done
    
    Change-Id: Ia8388ef8176adc7ddddb9e642cb69bb94b4b452e

--- system/gd/hal/hci_hal_android_hidl.cc
+++ system/gd/hal/hci_hal_android_hidl.cc
@@ -439,20 +439,12 @@ class HciHalHidl : public HciHal {
     LOG_INFO("Trying to find a HIDL interface");
 
     auto get_service_alarm = new os::Alarm(GetHandler());
+
+    hal_start_timeout_ms_ = os::GetSystemPropertyUint32("bluetooth.gd.hci_hal.start_timeout", 500);
+
     get_service_alarm->Schedule(
-        BindOnce([] {
-          const std::string kBoardProperty = "ro.product.board";
-          const std::string kCuttlefishBoard = "cutf";
-          auto board_name = os::GetSystemProperty(kBoardProperty);
-          bool emulator = board_name.has_value() && board_name.value() == kCuttlefishBoard;
-          if (emulator) {
-            LOG_ERROR("board_name: %s", board_name.value().c_str());
-            LOG_ERROR("Unable to get a Bluetooth service after 500ms, start the HAL before starting Bluetooth");
-            return;
-          }
-          LOG_ALWAYS_FATAL("Unable to get a Bluetooth service after 500ms, start the HAL before starting Bluetooth");
-        }),
-        std::chrono::milliseconds(500));
+        BindOnce(&HciHalHidl::onStartTimeout, base::Unretained(this)),
+        std::chrono::milliseconds(hal_start_timeout_ms_));
 
     bt_hci_1_1_ = IBluetoothHci_1_1::getService();
 
@@ -528,6 +520,23 @@ class HciHalHidl : public HciHal {
   ::ndk::ScopedAIBinder_DeathRecipient aidl_death_recipient_;
   activity_attribution::ActivityAttribution* btaa_logger_;
   SnoopLogger* btsnoop_logger_;
+  uint32_t hal_start_timeout_ms_;
+
+  void onStartTimeout() {
+    const std::string kBoardProperty = "ro.product.board";
+    const std::string kCuttlefishBoard = "cutf";
+    auto board_name = os::GetSystemProperty(kBoardProperty);
+    bool emulator = board_name.has_value() && board_name.value() == kCuttlefishBoard;
+    if (emulator) {
+      LOG_ERROR("board_name: %s", board_name.value().c_str());
+      LOG_ERROR("Unable to get a Bluetooth service after %u ms,"
+          " start the HAL before starting Bluetooth", hal_start_timeout_ms_);
+      return;
+    }
+    LOG_ALWAYS_FATAL("Unable to get a Bluetooth service after %u ms,"
+        " start the HAL before starting Bluetooth", hal_start_timeout_ms_);
+  }
+
 };
 
 const ModuleFactory HciHal::Factory = ModuleFactory([]() { return new HciHalHidl(); });
