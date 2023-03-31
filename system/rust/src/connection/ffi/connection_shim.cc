6cdd3a2901ec3fbe1fa900acf57ce2175e9dfc7f
Rahul Arya <aryarahul@google.com>
2023-03-31 18:37:58 +0000

    [Connection Manager] Expose connection manager API from C++
    This API is essentially the same as the old connection manager API. The
    difference is that we handle address resolution + connection tracking within the
    connection manager, rather than assuming all addresses are public.
    
    Bug: 272572974
    Test: compiles
    Change-Id: I066a9a1fececa608186db3ea9253ea442b018a69

--- system/rust/src/connection/ffi/connection_shim.cc
+++ system/rust/src/connection/ffi/connection_shim.cc
@@ -26,9 +26,6 @@
 #include "src/connection/ffi.rs.h"
 #include "stack/btm/btm_dev.h"
 
-extern const tBLE_BD_ADDR convert_to_address_with_type(
-    const RawAddress& bd_addr, const tBTM_SEC_DEV_REC* p_dev_rec);
-
 namespace bluetooth {
 namespace connection {
 
@@ -43,13 +40,6 @@ hci::AddressWithType ToCppAddress(core::AddressWithType address) {
 core::AddressWithType ToRustAddress(hci::AddressWithType address) {
   return core::AddressWithType{address.GetAddress().address,
                                (core::AddressType)address.GetAddressType()};
-}
-core::AddressWithType ToRustAddress(tBLE_BD_ADDR address) {
-  auto array = address.bda.ToArray();
-  std::reverse(std::begin(array), std::end(array));
-  return core::AddressWithType{array, address.IsPublic()
-                                          ? core::AddressType::Public
-                                          : core::AddressType::Random};
 }
 
 }  // namespace
@@ -104,6 +94,36 @@ void LeAclManagerShim::CancelLeConnect(core::AddressWithType address) const {
 void LeAclManagerShim::RegisterRustCallbacks(
     ::rust::Box<LeAclManagerCallbackShim> callbacks) {
   pimpl_->RegisterRustCallbacks(std::move(callbacks));
+}
+
+namespace {
+
+std::optional<RustConnectionManager> connection_manager;
+
+}  // namespace
+
+RustConnectionManager& GetConnectionManager() {
+  return connection_manager.value();
+}
+
+void RegisterRustApis(
+    ::rust::Fn<void(uint8_t client_id, core::AddressWithType address)>
+        start_direct_connection,
+    ::rust::Fn<void(uint8_t client_id, core::AddressWithType address)>
+        stop_direct_connection,
+    ::rust::Fn<void(uint8_t client_id, core::AddressWithType address)>
+        add_background_connection,
+    ::rust::Fn<void(uint8_t client_id, core::AddressWithType address)>
+        remove_background_connection,
+    ::rust::Fn<void(uint8_t client_id)> remove_client,
+    ::rust::Fn<void(core::AddressWithType address)>
+        stop_all_connections_to_device) {
+  connection_manager = {start_direct_connection,
+                        stop_direct_connection,
+                        add_background_connection,
+                        remove_background_connection,
+                        remove_client,
+                        stop_all_connections_to_device};
 }
 
 }  // namespace connection
