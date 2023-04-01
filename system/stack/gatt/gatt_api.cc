db623722d4c2ac2272ea893ef23e5a409909282a
Rahul Arya <aryarahul@google.com>
2023-04-01 01:52:32 +0000

    [Connection Manager] Call into connection manager from C++
    We are basically taking all the old entry points for the C++ connection manager,
    as well as all other entrypoints into le_impl, and sending them to Rust instead.
    
    Note that APIs are not implemented and so will crash on invocation.
    
    Bug: 272572974
    Test: compiles
    Change-Id: I918d0ede0f42c74e1d00082c97854b0359b57ef6

--- system/stack/gatt/gatt_api.cc
+++ system/stack/gatt/gatt_api.cc
@@ -38,6 +38,7 @@
 #include "osi/include/allocator.h"
 #include "osi/include/list.h"
 #include "osi/include/log.h"
+#include "rust/src/connection/ffi/connection_shim.h"
 #include "stack/arbiter/acl_arbiter.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/gatt/connection_manager.h"
@@ -1327,7 +1328,12 @@ void GATT_Deregister(tGATT_IF gatt_if) {
     }
   }
 
-  connection_manager::on_app_deregistered(gatt_if);
+  if (bluetooth::common::init_flags::
+          use_unified_connection_manager_is_enabled()) {
+    bluetooth::connection::GetConnectionManager().remove_client(gatt_if);
+  } else {
+    connection_manager::on_app_deregistered(gatt_if);
+  }
 
   *p_reg = {};
 }
@@ -1444,7 +1450,10 @@ bool GATT_Connect(tGATT_IF gatt_if, const RawAddress& bd_addr,
       LOG_DEBUG("Adding to background connect to device:%s",
                 ADDRESS_TO_LOGGABLE_CSTR(bd_addr));
       if (connection_type == BTM_BLE_BKG_CONNECT_ALLOW_LIST) {
-        ret = connection_manager::background_connect_add(gatt_if, bd_addr);
+        bluetooth::connection::GetConnectionManager().add_background_connection(
+            gatt_if, bluetooth::connection::ResolveRawAddress(bd_addr));
+        ret = true;
+        // ret = connection_manager::background_connect_add(gatt_if, bd_addr);
       } else {
         ret = connection_manager::background_connect_targeted_announcement_add(
             gatt_if, bd_addr);
@@ -1526,11 +1535,18 @@ bool GATT_CancelConnect(tGATT_IF gatt_if, const RawAddress& bd_addr,
     }
   }
 
-  if (!connection_manager::remove_unconditional(bd_addr)) {
-    LOG(ERROR)
-        << __func__
-        << ": no app associated with the bg device for unconditional removal";
-    return false;
+  if (bluetooth::common::init_flags::
+          use_unified_connection_manager_is_enabled()) {
+    bluetooth::connection::GetConnectionManager()
+        .stop_all_connections_to_device(
+            bluetooth::connection::ResolveRawAddress(bd_addr));
+  } else {
+    if (!connection_manager::remove_unconditional(bd_addr)) {
+      LOG(ERROR) << __func__
+                 << ": no app associated with the bg device for unconditional "
+                    "removal ";
+      return false;
+    }
   }
 
   return true;
