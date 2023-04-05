b18bc180fb1da63ae7d61ba54e67ca1e1756058d
Jakub Pawlowski <jpawlowski@google.com>
2023-04-05 14:34:35 +0200

    BTA: merge LE scans
    Previously we had just one source of BLE scans, it was Java, through
    BTIF. After this, CSIS was another place that start/stop scan.
    Now BAP have to manage scans for Targetted Announcements.
    
    Add scan source as parameter to APIs, so we can properly merge all
    start/stop request.
    
    Test: trigger CSIS while any Java app is scanning, also have LE Audio
          enabled with TA paired at same time.
    Bug: 275753723
    Change-Id: I05b6e0ef164613427569895f77862419d28ff76d

--- system/test/mock/mock_bta_dm_api.h
+++ system/test/mock/mock_bta_dm_api.h
@@ -128,10 +128,13 @@ extern struct BTA_DmBleConfirmReply BTA_DmBleConfirmReply;
 // Params: bool observe, tBTA_DM_SEARCH_CBACK* p_results_cb
 // Return: void
 struct BTA_DmBleCsisObserve {
-  std::function<void(bool observe, tBTA_DM_SEARCH_CBACK* p_results_cb)> body{
-      [](bool observe, tBTA_DM_SEARCH_CBACK* p_results_cb) {}};
-  void operator()(bool observe, tBTA_DM_SEARCH_CBACK* p_results_cb) {
-    body(observe, p_results_cb);
+  std::function<void(uint8_t scan_src, bool observe,
+                     tBTA_DM_SEARCH_CBACK* p_results_cb)>
+      body{[](uint8_t scan_src, bool observe,
+              tBTA_DM_SEARCH_CBACK* p_results_cb) {}};
+  void operator()(uint8_t scan_src, bool observe,
+                  tBTA_DM_SEARCH_CBACK* p_results_cb) {
+    body(scan_src, observe, p_results_cb);
   };
 };
 extern struct BTA_DmBleCsisObserve BTA_DmBleCsisObserve;
@@ -152,13 +155,13 @@ extern struct BTA_DmBleGetEnergyInfo BTA_DmBleGetEnergyInfo;
 // Params: bool start, uint8_t duration, tBTA_DM_SEARCH_CBACK* p_results_cb
 // Return: void
 struct BTA_DmBleObserve {
-  std::function<void(bool start, uint8_t duration,
+  std::function<void(uint8_t scan_src, bool start, uint8_t duration,
                      tBTA_DM_SEARCH_CBACK* p_results_cb)>
-      body{[](bool start, uint8_t duration,
+      body{[](uint8_t scan_src, bool start, uint8_t duration,
               tBTA_DM_SEARCH_CBACK* p_results_cb) {}};
-  void operator()(bool start, uint8_t duration,
+  void operator()(uint8_t scan_src, bool start, uint8_t duration,
                   tBTA_DM_SEARCH_CBACK* p_results_cb) {
-    body(start, duration, p_results_cb);
+    body(scan_src, start, duration, p_results_cb);
   };
 };
 extern struct BTA_DmBleObserve BTA_DmBleObserve;
@@ -189,9 +192,11 @@ extern struct BTA_DmBleRequestMaxTxDataLength BTA_DmBleRequestMaxTxDataLength;
 // Params: bool start, uint8_t duration
 // Return: void
 struct BTA_DmBleScan {
-  std::function<void(bool start, uint8_t duration)> body{
-      [](bool start, uint8_t duration) {}};
-  void operator()(bool start, uint8_t duration) { body(start, duration); };
+  std::function<void(uint8_t scan_src, bool start, uint8_t duration)> body{
+      [](uint8_t scan_src, bool start, uint8_t duration) {}};
+  void operator()(uint8_t scan_src, bool start, uint8_t duration) {
+    body(scan_src, start, duration);
+  };
 };
 extern struct BTA_DmBleScan BTA_DmBleScan;
 
