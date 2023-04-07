e55d28aa204916da3d0e83e267fe6e531052bdcb
Chris Manton <cmanton@google.com>
2023-04-07 14:37:16 -0700

    Unify remote name complete bta_dm_remote_name_cmpl
    Introduce common tBTA_DM_REMOTE_NAME
    
    Bug: 277388611
    Test: Works on 3.0 device
    
    Change-Id: I43c95f8265657f7d558f30073d62f26b207682ea

--- system/stack/include/bt_name.h
+++ system/stack/include/bt_name.h
@@ -24,6 +24,8 @@
 #include <string.h>
 #endif
 
+#include "osi/include/compat.h"  // strlcpy
+
 // NOTE: Shared with internal_include/bt_target.h
 /* Maximum device name length used in btm database. */
 #ifndef BTM_MAX_REM_BD_NAME_LEN
@@ -45,4 +47,10 @@ typedef uint8_t tBTM_LOC_BD_NAME[BTM_MAX_LOC_BD_NAME_LEN + 1];
 
 #ifdef __cplusplus
 inline constexpr tBTM_BD_NAME kBtmBdNameEmpty = {};
+
+inline size_t bd_name_copy(BD_NAME bd_name_dest, const BD_NAME bd_name_src) {
+  return strlcpy(reinterpret_cast<char*>(bd_name_dest),
+                 reinterpret_cast<char*>(const_cast<uint8_t*>(bd_name_src)),
+                 kBdNameLength + 1);
+}
 #endif
