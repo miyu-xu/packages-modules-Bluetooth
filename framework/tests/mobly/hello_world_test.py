861253dbc33345764e3185bd5fd7fcedcf22e288
Qiao Yang <qiaoccolato@google.com>
2023-04-08 02:49:46 +0000

    test mobly aosp
    Change-Id: Ic679a133f6b53eaf0d82ff212bc05284eba43494

---
+++ framework/tests/mobly/hello_world_test.py
@@ -0,0 +1,20 @@
+import os
+
+from mobly import base_test
+from mobly import test_runner
+from mobly.controllers import android_device
+
+class HelloWorldTest(base_test.BaseTestClass):
+
+  def setup_class(self):
+    self.ads = self.register_controller(android_device)
+    self.dut = self.ads[0]
+
+    # Start Mobly Bundled Snippets (MBS).
+    self.dut.load_snippet('mbs', android_device.MBS_PACKAGE)
+
+  def test_hello(self):
+    self.dut.mbs.makeToast('Hello World!')
+
+if __name__ == '__main__':
+  test_runner.main()
