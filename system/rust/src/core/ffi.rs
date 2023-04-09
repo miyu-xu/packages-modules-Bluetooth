909c6cd90836525ef07694dbd83f095054a815bd
Rahul Arya <aryarahul@google.com>
2023-04-09 21:08:17 +0000

    [Connection Manager] Add FFI to access le_impl from Rust
    This uses the API documented in le_manager.rs. This is an interim solution,
    since the API does not match what would be ideal, but it works for now.
    
    Bug: 272572974
    Test: unit
    Change-Id: Ifc38fcd868c7fef295f89b1c65bcaa8879146890

--- system/rust/src/core/ffi.rs
+++ system/rust/src/core/ffi.rs
@@ -56,8 +56,17 @@ mod inner {
         type GattServerCallbacks = crate::gatt::GattServerCallbacks;
     }
 
+    #[namespace = "bluetooth::connection"]
+    unsafe extern "C++" {
+        include!("src/connection/ffi/connection_shim.h");
+        type LeAclManagerShim = crate::connection::LeAclManagerShim;
+    }
+
     #[namespace = "bluetooth::rust_shim"]
     extern "Rust" {
-        fn init(gatt_server_callbacks: UniquePtr<GattServerCallbacks>);
+        fn init(
+            gatt_server_callbacks: UniquePtr<GattServerCallbacks>,
+            le_acl_manager: UniquePtr<LeAclManagerShim>,
+        );
     }
 }
