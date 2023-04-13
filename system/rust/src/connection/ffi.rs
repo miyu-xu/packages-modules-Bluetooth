fb5dafd72cdf2d480ea51c2a79b0a4d70ee349d6
Rahul Arya <aryarahul@google.com>
2023-04-13 22:14:08 +0000

    [Connection Manager] Handle address resolution
    When we bond/unbond, update the filter accept list accordingly depending
    on what we know we are connected to.
    
    Bug: 272572974
    Bug: 270423722
    Test: unit
    Change-Id: I75d10f921fbb9756bfe523f82bd4c67ed7f8dd8d

--- system/rust/src/connection/ffi.rs
+++ system/rust/src/connection/ffi.rs
@@ -2,23 +2,32 @@
 
 use std::{fmt::Debug, pin::Pin};
 
-use cxx::UniquePtr;
+use async_trait::async_trait;
+use cxx::{SharedPtr, UniquePtr};
 pub use inner::*;
 use log::warn;
 use tokio::{
-    sync::mpsc::{unbounded_channel, UnboundedSender},
+    sync::{mpsc::{unbounded_channel, UnboundedSender}, oneshot},
     task::spawn_local,
 };
 
-use crate::{core::address::AddressWithType, do_in_rust_thread};
+use crate::{
+    core::{address::AddressWithType, Callback, invoke_callback},
+    do_in_rust_thread,
+};
 
 use super::{
     attempt_manager::ConnectionMode,
-    le_manager::{ErrorCode, InactiveLeAclManager, LeAclManager, LeAclManagerConnectionCallbacks},
+    le_manager::{
+        AddressResolver, CanonicalAddress, ErrorCode, InactiveLeAclManager, LeAclManager,
+        LeAclManagerConnectionCallbacks,
+    },
     ConnectionManagerClient, LeConnection,
 };
 
 unsafe impl Send for LeAclManagerShim {}
+unsafe impl Send for AddressResolverShim {}
+unsafe impl Sync for AddressResolverShim {}
 
 #[cxx::bridge]
 #[allow(clippy::needless_lifetimes)]
@@ -26,6 +35,7 @@ unsafe impl Send for LeAclManagerShim {}
 #[allow(missing_docs)]
 mod inner {
     impl UniquePtr<LeAclManagerShim> {}
+    impl SharedPtr<AddressResolverShim> {}
 
     #[namespace = "bluetooth::core"]
     extern "C++" {
@@ -64,6 +74,35 @@ mod inner {
 
     #[namespace = "bluetooth::connection"]
     extern "Rust" {
+        type ResolveAddressCallback;
+
+        #[cxx_name = "Invoke"]
+        fn invoke_callback(callback: Box<ResolveAddressCallback>, address: AddressWithType);
+    }
+
+    #[namespace = "bluetooth::connection"]
+    unsafe extern "C++" {
+        include!("src/connection/ffi/connection_shim.h");
+
+        /// This lets us resolve RPAs to an identity address, using the security database
+        type AddressResolverShim;
+
+        /// Resolve an address into "canonical form", that can be passed to the create/cancel
+        /// callbacks. The exact means of resolution is implementation-defined (i.e. it could
+        /// be the identity address, or the pseudo-address, or anything else)
+        /// 
+        /// # Safety
+        /// `on_resolved` must be Send, since we use it from a C++ thread.
+        #[cxx_name = "ResolveAddress"]
+        unsafe fn unchecked_resolve_address(
+            &self,
+            address: AddressWithType,
+            on_resolved: Box<ResolveAddressCallback>,
+        );
+    }
+
+    #[namespace = "bluetooth::connection"]
+    extern "Rust" {
         type LeAclManagerCallbackShim;
         #[cxx_name = "OnLeConnectSuccess"]
         fn on_le_connect_success(&self, address: AddressWithType);
@@ -71,6 +110,8 @@ mod inner {
         fn on_le_connect_fail(&self, address: AddressWithType, status: u8);
         #[cxx_name = "OnLeDisconnection"]
         fn on_disconnect(&self, address: AddressWithType);
+        #[cxx_name = "OnResolvingListChange"]
+        fn on_resolving_list_change(&self);
     }
 
     #[namespace = "bluetooth::connection"]
@@ -129,6 +170,12 @@ impl LeAclManagerCallbackShim {
             callback.on_disconnect(address);
         }));
     }
+
+    fn on_resolving_list_change(&self) {
+        let _ = self.0.send(Box::new(move |callback| {
+            callback.on_resolving_list_change();
+        }));
+    }
 }
 
 impl InactiveLeAclManager for LeAclManagerImpl {
@@ -151,6 +198,8 @@ impl InactiveLeAclManager for LeAclManagerImpl {
     }
 }
 
+type ResolveAddressCallback = Callback<AddressWithType>;
+
 impl LeAclManager for LeAclManagerImpl {
     fn add_to_direct_list(&self, address: AddressWithType) {
         self.0.create_le_connection(address, /* is_direct= */ true)
@@ -171,63 +220,104 @@ impl Debug for LeAclManagerImpl {
     }
 }
 
+/// Implementation of AddressResolver wrapping the corresponding C++ methods
+#[derive(Clone)]
+pub struct AddressResolverImpl(pub SharedPtr<AddressResolverShim>);
+
+#[async_trait(?Send)]
+impl AddressResolver for AddressResolverImpl {
+    async fn resolve_address(&self, address: AddressWithType) -> CanonicalAddress {
+        let (tx, rx) = oneshot::channel();
+        // SAFETY: Since Callback<T> is always Send, this is safe.
+        let callback = Box::new(tx.into());
+        {
+            fn check(_: &impl Send) {}
+            check(&callback);
+        }
+        unsafe {
+            self.0.unchecked_resolve_address(address, callback);
+        }
+        CanonicalAddress::new(rx.await.unwrap())
+    }
+}
+
+impl Debug for AddressResolverImpl {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("AddressResolverImpl").finish()
+    }
+}
+
 /// Registers all connection-manager callbacks into C++ dependencies
 pub fn register_callbacks() {
     RegisterRustApis(
         |client, address| {
             let client = ConnectionManagerClient::GattClient(client);
             do_in_rust_thread(move |modules| {
-                let result =
-                    modules.connection_manager.as_ref().start_direct_connection(client, address);
-                if let Err(err) = result {
-                    warn!("Failed to start direct connection from {client:?} to {address:?} ({err:?})")
-                }
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    let result = connection_manager.start_direct_connection(client, address).await;
+                    if let Err(err) = result {
+                        warn!("Failed to start direct connection from {client:?} to {address:?} ({err:?})")
+                    }
+                });
             });
         },
         |client, address| {
             let client = ConnectionManagerClient::GattClient(client);
             do_in_rust_thread(move |modules| {
-                let result = modules.connection_manager.cancel_connection(
-                    client,
-                    address,
-                    ConnectionMode::Direct,
-                );
-                if let Err(err) = result {
-                    warn!("Failed to cancel direct connection from {client:?} to {address:?} ({err:?})")
-                }
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    let result = connection_manager
+                        .cancel_connection(client, address, ConnectionMode::Direct)
+                        .await;
+                    if let Err(err) = result {
+                        warn!("Failed to cancel direct connection from {client:?} to {address:?} ({err:?})")
+                    }
+                });
             })
         },
         |client, address| {
             let client = ConnectionManagerClient::GattClient(client);
             do_in_rust_thread(move |modules| {
-                let result = modules.connection_manager.add_background_connection(client, address);
-                if let Err(err) = result {
-                    warn!("Failed to add background connection from {client:?} to {address:?} ({err:?})")
-                }
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    let result =
+                        connection_manager.add_background_connection(client, address).await;
+                    if let Err(err) = result {
+                        warn!("Failed to add background connection from {client:?} to {address:?} ({err:?})")
+                    }
+                });
             })
         },
         |client, address| {
             let client = ConnectionManagerClient::GattClient(client);
             do_in_rust_thread(move |modules| {
-                let result = modules.connection_manager.cancel_connection(
-                    client,
-                    address,
-                    ConnectionMode::Background,
-                );
-                if let Err(err) = result {
-                    warn!("Failed to remove background connection from {client:?} to {address:?} ({err:?})")
-                }
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    let result = connection_manager
+                        .cancel_connection(client, address, ConnectionMode::Background)
+                        .await;
+                    if let Err(err) = result {
+                        warn!("Failed to remove background connection from {client:?} to {address:?} ({err:?})")
+                    }
+                });
             })
         },
         |client| {
             let client = ConnectionManagerClient::GattClient(client);
             do_in_rust_thread(move |modules| {
-                modules.connection_manager.remove_client(client);
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    connection_manager.remove_client(client).await;
+                });
             })
         },
         |address| {
             do_in_rust_thread(move |modules| {
-                modules.connection_manager.cancel_unconditionally(address);
+                let connection_manager = modules.connection_manager.clone();
+                spawn_local(async move {
+                    connection_manager.cancel_unconditionally(address).await;
+                });
             })
         },
     )
--- system/rust/src/core/mod.rs
+++ system/rust/src/core/mod.rs
@@ -9,19 +9,22 @@ pub mod uuid;
 use std::{pin::Pin, rc::Rc, thread};
 
 use bt_common::init_flags::rust_event_loop_is_enabled;
-use cxx::UniquePtr;
+use cxx::{SharedPtr, UniquePtr};
 
 use crate::{
-    connection::{LeAclManagerImpl, LeAclManagerShim},
+    connection::{AddressResolverImpl, AddressResolverShim, LeAclManagerImpl, LeAclManagerShim},
     gatt::ffi::{AttTransportImpl, GattCallbacksImpl},
     GlobalModuleRegistry, MainThreadTxMessage, GLOBAL_MODULE_REGISTRY,
 };
 
 use self::ffi::{future_ready, Future, GattServerCallbacks};
 
+pub use ffi::{Callback, invoke_callback};
+
 fn start(
     gatt_server_callbacks: UniquePtr<GattServerCallbacks>,
     le_acl_manager: UniquePtr<LeAclManagerShim>,
+    address_resolver: SharedPtr<AddressResolverShim>,
     on_started: Pin<&'static mut Future>,
 ) {
     if rust_event_loop_is_enabled() {
@@ -30,6 +33,7 @@ fn start(
                 Rc::new(GattCallbacksImpl(gatt_server_callbacks)),
                 Rc::new(AttTransportImpl()),
                 LeAclManagerImpl(le_acl_manager),
+                AddressResolverImpl(address_resolver),
                 || {
                     future_ready(on_started);
                 },
