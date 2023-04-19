cccd229d3434662598b162275f5e33fbdec0e667
Hieu Dang <thebestguy@google.com>
2023-04-19 07:01:52 +0000

    Fix a test in IncomingFileConfirmActivityTest
    Bug: 277593460
    Tag: #refactor
    Test: BluetoothInstrumentationTests
    Change-Id: Ibc204f121b719580d37ba49a3d783f37d774028c

--- android/app/tests/unit/src/com/android/bluetooth/opp/IncomingFileConfirmActivityTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/opp/IncomingFileConfirmActivityTest.java
@@ -23,6 +23,9 @@ import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static com.android.bluetooth.opp.BluetoothOppIncomingFileConfirmActivity.DISMISS_TIMEOUT_DIALOG;
+import static com.android.bluetooth.opp.BluetoothOppIncomingFileConfirmActivity.DISMISS_TIMEOUT_DIALOG_VALUE;
+
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -30,6 +33,7 @@ import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 import android.content.Context;
@@ -178,9 +182,8 @@ public class IncomingFileConfirmActivityTest {
         ), nullable(String.class), nullable(String[].class));
     }
 
-    @Ignore("b/277593460")
     @Test
-    public void onTimeout_sendIntentWithUSER_CONFIRMATION_TIMEOUT_ACTION_finish() throws Exception {
+    public void onTimeout_broadcastUserConfirmationTimeoutAction_sendDismissTimeoutDialogMessage() {
         BluetoothOppTestUtils.setUpMockCursor(mCursor, mCursorMockDataList);
         ActivityScenario<BluetoothOppIncomingFileConfirmActivity> scenario =
                 ActivityScenario.launch(mIntent);
@@ -189,11 +192,9 @@ public class IncomingFileConfirmActivityTest {
         Intent in = new Intent(BluetoothShare.USER_CONFIRMATION_TIMEOUT_ACTION);
         mTargetContext.sendBroadcast(in);
 
-        // To work around (possibly) ActivityScenario's bug.
-        // The dialog button is clicked (no error throw) but onClick() is not triggered.
-        // It works normally if sleep for a few seconds
-        Thread.sleep(3_000);
-        assertThat(scenario.getState()).isEqualTo(Lifecycle.State.DESTROYED);
+        long timeOutValue = DISMISS_TIMEOUT_DIALOG_VALUE;
+        verify(mBluetoothMethodProxy, timeout(5000)).handlerSendMessageDelayed(any(),
+                eq(DISMISS_TIMEOUT_DIALOG), eq(timeOutValue));
     }
 
     @Test
