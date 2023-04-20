3b64b407307924bcfd472fec0a0607bf0cc904cd
Rahul Arya <aryarahul@google.com>
2023-04-20 08:17:23 +0000

    [Connection Manager] Add support for Targeted Announcements
    Migrates support from connection_manager.cc into Rust module, with
    proper support for address resolution + interaction with direct
    connections.
    
    Bug: 272572974
    Test: unit
    Change-Id: Ibd42daf81aabc27a7a33b7aaf74ca2e60c3a317f

--- system/rust/src/connection/target_state.rs
+++ system/rust/src/connection/target_state.rs
@@ -5,20 +5,38 @@ use std::collections::HashSet;
 
 use futures::future::join_all;
 
+use crate::core::address::AddressWithType;
+
 use super::{
     attempt_manager::{ConnectionAttempt, ConnectionMode},
     le_manager::{AddressResolver, CanonicalAddress},
     LeConnection,
 };
 
+/// This struct represents the target state of the components the connection manager
+/// controls.
+pub struct TargetState {
+    /// The state we want le_impl to be in
+    pub target_acceptlist_state: TargetAcceptlistState,
+    /// Whether we should do a targeted-announcement scan
+    pub scan_for_targeted_announcements: bool,
+}
+
 /// This struct represents the target state of the LeManager based on the
 /// set of all active connection attempts. Only canonical addresses can be
 /// placed here.
-pub struct TargetState {
+pub struct TargetAcceptlistState {
     /// These addresses should go to the LE background connect list
     pub background_list: HashSet<CanonicalAddress>,
     /// These addresses should go to the direct list (we are not connected to any of them)
     pub direct_list: HashSet<CanonicalAddress>,
+}
+
+async fn get_addresses(
+    it: impl Iterator<Item = AddressWithType>,
+    resolver: &(impl AddressResolver + ?Sized),
+) -> HashSet<CanonicalAddress> {
+    HashSet::from_iter(join_all(it.map(|addr| resolver.resolve_address(addr))).await.into_iter())
 }
 
 /// Takes a list of connection attempts, and determines the target state of the LE ACL manager
@@ -27,52 +45,82 @@ pub async fn determine_target_state(
     resolver: &(impl AddressResolver + ?Sized),
     current_connections: impl Iterator<Item = LeConnection>,
 ) -> TargetState {
-    let canonical_addr_current_connections: HashSet<CanonicalAddress> = HashSet::from_iter(
-        join_all(
-            current_connections
-                .map(|conn| conn.remote_address)
-                .map(|addr| resolver.resolve_address(addr)),
-        )
-        .await
-        .into_iter(),
-    );
+    let canonical_addr_current_connections: HashSet<CanonicalAddress> =
+        get_addresses(current_connections.map(|conn: LeConnection| conn.remote_address), resolver)
+            .await;
 
-    let background_list = join_all(
+    let background_list = get_addresses(
         attempts
             .iter()
-            .filter(|attempt| attempt.mode == ConnectionMode::Background)
-            .map(|attempt| attempt.remote_address)
-            .map(|addr| resolver.resolve_address(addr)),
+            .filter(|attempt| attempt.key.mode == ConnectionMode::Background)
+            .map(|attempt| attempt.key.remote_address),
+        resolver,
     )
-    .await
-    .into_iter()
-    .filter(|addr| !canonical_addr_current_connections.contains(addr))
-    .collect();
+    .await;
 
-    let direct_list: HashSet<_> = join_all(
+    let direct_list = get_addresses(
         attempts
             .iter()
-            .filter(|attempt| attempt.mode == ConnectionMode::Direct)
-            .map(|attempt| attempt.remote_address)
-            .map(|addr| resolver.resolve_address(addr)),
+            .filter(|attempt| attempt.key.mode == ConnectionMode::Direct)
+            .map(|attempt| attempt.key.remote_address),
+        resolver,
     )
-    .await
-    .into_iter()
-    .collect();
+    .await;
 
+    // Note: we put this assertion before adding targeted announcements, since we keep targeted announcement attempts
+    // even for connected devices - we just don't keep explicit *direct* attempts.
     assert_eq!(
         direct_list.intersection(&canonical_addr_current_connections).next(),
         None,
         "We should never have a direct connection attempt to a device we are already connected to"
     );
 
-    TargetState { background_list, direct_list }
+    // Targeted announcement attempts that have found the target device add the device to the direct list, while other
+    // targeted announcements attempts remove the device from the background list
+    let pending_targeted_announcements = get_addresses(
+        attempts
+            .iter()
+            .filter(|attempt| {
+                attempt.key.mode == ConnectionMode::TargetedAnnouncement
+                    && !attempt.last_seen.is_recent()
+            })
+            .map(|attempt| attempt.key.remote_address),
+        resolver,
+    )
+    .await;
+
+    let resolved_targeted_announcements = get_addresses(
+        attempts
+            .iter()
+            .filter(|attempt| {
+                attempt.key.mode == ConnectionMode::TargetedAnnouncement
+                    && attempt.last_seen.is_recent()
+            })
+            .map(|attempt| attempt.key.remote_address),
+        resolver,
+    )
+    .await;
+
+    let background_list =
+        &(&background_list - &pending_targeted_announcements) - &canonical_addr_current_connections;
+    let direct_list =
+        &(&direct_list | &resolved_targeted_announcements) - &canonical_addr_current_connections;
+
+    let scan_for_targeted_announcements = !pending_targeted_announcements.is_empty();
+
+    TargetState {
+        target_acceptlist_state: TargetAcceptlistState { background_list, direct_list },
+        scan_for_targeted_announcements,
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::{
-        connection::{mocks::mock_address_resolver::MockAddressResolver, ConnectionManagerClient},
+        connection::{
+            attempt_manager::ConnectionAttemptKey,
+            mocks::mock_address_resolver::MockAddressResolver, ConnectionManagerClient,
+        },
         core::address::{AddressType, AddressWithType},
         utils::task::block_on_locally,
     };
@@ -100,44 +148,49 @@ mod test {
 
     #[test]
     fn test_determine_target_state() {
-        let target = block_on_locally(determine_target_state(
+        let TargetState { target_acceptlist_state, .. } = block_on_locally(determine_target_state(
             &[
-                ConnectionAttempt {
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_1.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_1.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_2.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Direct,
                     remote_address: ADDRESS_2.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Direct,
                     remote_address: ADDRESS_3.addr(),
-                },
+                }
+                .into(),
             ],
             &MockAddressResolver::new(),
             [].into_iter(),
         ));
 
-        assert_eq!(target.background_list.len(), 2);
-        assert!(target.background_list.contains(&ADDRESS_1));
-        assert!(target.background_list.contains(&ADDRESS_2));
-        assert_eq!(target.direct_list.len(), 2);
-        assert!(target.direct_list.contains(&ADDRESS_2));
-        assert!(target.direct_list.contains(&ADDRESS_3));
+        assert_eq!(target_acceptlist_state.background_list.len(), 2);
+        assert!(target_acceptlist_state.background_list.contains(&ADDRESS_1));
+        assert!(target_acceptlist_state.background_list.contains(&ADDRESS_2));
+        assert_eq!(target_acceptlist_state.direct_list.len(), 2);
+        assert!(target_acceptlist_state.direct_list.contains(&ADDRESS_2));
+        assert!(target_acceptlist_state.direct_list.contains(&ADDRESS_3));
     }
 
     #[test]
@@ -147,28 +200,30 @@ mod test {
         resolver.associate_address(ADDRESS_2, ADDRESS_1.addr());
 
         // act: reconcile connection attempts of both types to the RPA
-        let target = block_on_locally(determine_target_state(
+        let TargetState { target_acceptlist_state, .. } = block_on_locally(determine_target_state(
             &[
-                ConnectionAttempt {
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Direct,
                     remote_address: ADDRESS_1.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_1.addr(),
-                },
+                }
+                .into(),
             ],
             &resolver,
             [].into_iter(),
         ));
 
         // assert: we resolved the RPAs to the canonical address
-        assert_eq!(target.background_list.len(), 1);
-        assert!(target.background_list.contains(&ADDRESS_2));
-        assert_eq!(target.direct_list.len(), 1);
-        assert!(target.direct_list.contains(&ADDRESS_2));
+        assert_eq!(target_acceptlist_state.background_list.len(), 1);
+        assert!(target_acceptlist_state.background_list.contains(&ADDRESS_2));
+        assert_eq!(target_acceptlist_state.direct_list.len(), 1);
+        assert!(target_acceptlist_state.direct_list.contains(&ADDRESS_2));
     }
 
     #[test]
@@ -184,11 +239,12 @@ mod test {
 
         // act: let us be connected to ADDRESS_1, and making a direct connection to ADDRESS_2
         block_on_locally(determine_target_state(
-            &[ConnectionAttempt {
+            &[ConnectionAttemptKey {
                 client: CLIENT,
                 mode: ConnectionMode::Direct,
                 remote_address: ADDRESS_2.addr(),
-            }],
+            }
+            .into()],
             &resolver,
             [LeConnection { remote_address: ADDRESS_1.addr() }].into_iter(),
         ));
@@ -207,25 +263,27 @@ mod test {
         );
 
         // act: let us be connected to ADDRESS_1, and making background connections to ADDRESS_2 and ADDRESS_4
-        let target = block_on_locally(determine_target_state(
+        let TargetState { target_acceptlist_state, .. } = block_on_locally(determine_target_state(
             &[
-                ConnectionAttempt {
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_2.addr(),
-                },
-                ConnectionAttempt {
+                }
+                .into(),
+                ConnectionAttemptKey {
                     client: CLIENT,
                     mode: ConnectionMode::Background,
                     remote_address: ADDRESS_4.addr(),
-                },
+                }
+                .into(),
             ],
             &resolver,
             [LeConnection { remote_address: ADDRESS_1.addr() }].into_iter(),
         ));
 
         // assert: only ADDRESS_4 is retained, since we are already connected to the device associated with ADDRESS_2
-        assert_eq!(target.background_list.len(), 1);
-        assert!(target.background_list.contains(&ADDRESS_4));
+        assert_eq!(target_acceptlist_state.background_list.len(), 1);
+        assert!(target_acceptlist_state.background_list.contains(&ADDRESS_4));
     }
 }
