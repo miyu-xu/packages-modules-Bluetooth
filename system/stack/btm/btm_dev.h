0486c734c79f9a7000af28c5ff5f9d0944a46fa6
Chris Manton <cmanton@google.com>
2023-04-19 17:46:56 -0700

    Delete security record using ptr/ptr semantics
    Make it obvious that the ownership of the security
    device record pointer is relinquished.
    
    Bug: 277249213
    Test: net_test_stack_btm64
    
    Change-Id: Idc74d74defc35603a85eb0f89052f6890c76d658

--- system/stack/btm/btm_dev.h
+++ system/stack/btm/btm_dev.h
@@ -23,7 +23,7 @@
 #include "stack/include/bt_octets.h"
 #include "types/raw_address.h"
 
-void wipe_secrets_and_remove(tBTM_SEC_DEV_REC* p_dev_rec);
+void wipe_secrets_and_remove(tBTM_SEC_DEV_REC** p_dev_rec);
 
 /** Free resources associated with the device associated with |bd_addr| address.
  *
