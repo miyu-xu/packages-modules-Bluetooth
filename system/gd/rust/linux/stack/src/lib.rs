e0b6e91ca101d2c5a367b2a0ee0c667a789611fe
JohnLai <johnlai@google.com>
2023-04-21 13:26:01 +0800

    Floss: Caches the discoverable mode
    This change caches the discoverable mode in BluetoothQA for later use.
    
    Bug: 278653204
    Test: Manually test with btclient:
            adapter discoverable limited 60
            adapter show
    Tag: #floss
    Change-Id: I8c6de804f12acc88a0545d4182fd653cd6df901b

--- system/gd/rust/linux/stack/src/lib.rs
+++ system/gd/rust/linux/stack/src/lib.rs
@@ -20,6 +20,8 @@ pub mod socket_manager;
 pub mod suspend;
 pub mod uuid;
 
+use bluetooth_qa::BluetoothQA;
+use bt_topshim::btif::BtDiscMode;
 use log::debug;
 use num_derive::{FromPrimitive, ToPrimitive};
 use std::sync::{Arc, Mutex};
@@ -127,6 +129,7 @@ pub enum Message {
 
     // Qualification Only
     QaAddMediaPlayer(String, bool),
+    QaOnDiscoverableModeChanged(BtDiscMode),
 }
 
 /// Represents suspend mode of a module.
@@ -163,6 +166,7 @@ impl Stack {
         bluetooth_socketmgr: Arc<Mutex<Box<BluetoothSocketManager>>>,
         bluetooth_admin: Arc<Mutex<Box<BluetoothAdmin>>>,
         bluetooth_dis: Arc<Mutex<Box<DeviceInformation>>>,
+        bluetooth_qa: Arc<Mutex<Box<BluetoothQA>>>,
     ) {
         loop {
             let m = rx.recv().await;
@@ -352,6 +356,9 @@ impl Stack {
                 // Qualification Only
                 Message::QaAddMediaPlayer(name, browsing_supported) => {
                     bluetooth_media.lock().unwrap().add_player(name, browsing_supported);
+                }
+                Message::QaOnDiscoverableModeChanged(mode) => {
+                    bluetooth_qa.lock().unwrap().handle_discoverable_mode_changed(mode);
                 }
             }
         }
