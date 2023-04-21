9d7d2decbf740f65ac4fb44340489e0386689a7c
Henri Chataing <henrichataing@google.com>
2023-04-21 16:14:16 +0000

    Read the adapter property BT_PROPERTY_LOCAL_LE_FEATURES
    The getters for LE feature support bits otherwise return invalid
    values.
    
    Bug: 275847929
    Test: atest CtsRootBluetoothTestCases
    Change-Id: I5a9e61e1ca283c57224a0d14f2f7ded920b66a46

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -489,6 +489,7 @@ public class AdapterService extends Service {
                         initProfileServices();
                         getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_LOCAL_IO_CAPS);
                         getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_LOCAL_IO_CAPS_BLE);
+                        getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_LOCAL_LE_FEATURES);
                         getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_DYNAMIC_AUDIO_BUFFER);
                         mAdapterStateMachine.sendMessage(AdapterState.BREDR_STARTED);
                         mBtCompanionManager.loadCompanionInfo();
