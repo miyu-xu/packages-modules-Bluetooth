7432f96feaf99be780650650ddd15349cf2f24a2
Ying Hsu <yinghsu@google.com>
2023-04-26 13:42:08 +0000

    floss: Fix RNR Command Disallowed error in discovery classic only mode
    Bluetooth controller would reject Remote Name Request if it's in
    inquiry mode. This patch fixes the duration of classic_inquiry_timer
    and waits for the Inquiry Complete event before starting RNR.
    
    Bug: 277977567
    Tag: #floss
    Test: Manual - No Command Disallowed error for RNR
    Change-Id: Ibe3c757d3ac8733a41ac6431867279cba44bfb60

--- system/stack/btm/btm_inq.cc
+++ system/stack/btm/btm_inq.cc
@@ -535,10 +535,11 @@ void BTM_CancelInquiry(void) {
 }
 
 static void btm_classic_inquiry_timeout(UNUSED_ATTR void* data) {
-  // We mark both classic inquiry and BLE inquiry complete so that we stop
-  // processing BLE results as inquiry results.
-  btm_process_inq_complete(HCI_SUCCESS,
-                           (BTM_BR_INQUIRY_MASK | BTM_BLE_INQUIRY_MASK));
+  // When the Inquiry Complete event is received, the classic inquiry
+  // will be marked as completed. Therefore, we only need to mark
+  // the BLE inquiry as completed here to stop processing BLE results
+  // as inquiry results.
+  btm_process_inq_complete(HCI_SUCCESS, BTM_BLE_INQUIRY_MASK);
 }
 
 /*******************************************************************************
@@ -665,7 +666,7 @@ tBTM_STATUS BTM_StartInquiry(tBTM_INQ_RESULTS_CB* p_results_cb,
   if (bluetooth::shim::is_classic_discovery_only_enabled() &&
       btm_cb.btm_inq_vars.inqparms.duration != 0) {
     /* start inquiry timer */
-    uint64_t duration_ms = btm_cb.btm_inq_vars.inqparms.duration * 1000;
+    uint64_t duration_ms = btm_cb.btm_inq_vars.inqparms.duration * 1280;
     alarm_set_on_mloop(btm_cb.btm_inq_vars.classic_inquiry_timer, duration_ms,
                        btm_classic_inquiry_timeout, NULL);
   }
