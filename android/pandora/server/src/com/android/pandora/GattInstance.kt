71978b58077f463a4640fdeec79664c6bf966252
Duo Ho <duoho@google.com>
2023-04-27 05:19:01 +0000

    [Avatar] Clear GattInstance in FactoryReset
    Test: avatar run --mobly-std-log --include-filter 'ASHATest'
    Bug: 279599889
    Change-Id: I843629a524eea7e687437048cd1974e2edc9cb35

--- android/pandora/server/src/com/android/pandora/GattInstance.kt
+++ android/pandora/server/src/com/android/pandora/GattInstance.kt
@@ -62,6 +62,7 @@ class GattInstance(val mDevice: BluetoothDevice, val mTransport: Int, val mConte
   private var mGattInstanceValueWrote = GattInstanceValueWrote(null, 0, AttStatusCode.UNKNOWN_ERROR)
 
   companion object GattManager {
+    const val TAG = "GattInstance.GattManager"
     val gattInstances: MutableMap<String, GattInstance> = mutableMapOf<String, GattInstance>()
     fun get(address: String): GattInstance {
       val instance = gattInstances.get(address)
@@ -72,6 +73,13 @@ class GattInstance(val mDevice: BluetoothDevice, val mTransport: Int, val mConte
       val instance = gattInstances.get(address.toByteArray().decodeToString())
       requireNotNull(instance) { "Unable to find GATT instance for $address" }
       return instance
+    }
+    fun clearAllInstances() {
+        var size = gattInstances.count()
+        if (size > 0) {
+            Log.w(TAG, "clear $size instances")
+            gattInstances.clear()
+        }
     }
   }
 
