b78db3a12abb9b6c0a2fcb761887511a7d808799
Henri Chataing <henrichataing@google.com>
2023-05-02 18:12:15 +0000

    RootCanal: Implement LL tests CIS/CEN/*, CIS/PER/*
    Implement the tests:
      - LL/CIS/CEN/BV-01-C
      - LL/CIS/CEN/BV-03-C
      - LL/CIS/PER/BV-01-C
      - LL/CIS/PER/BV-02-C
    
    Bug: 253535400
    Test: atest rootcanal_ll_test
    Change-Id: I29547ac07e624bee5b07e4346f52d595f01b1599

--- tools/rootcanal/test/main.py
+++ tools/rootcanal/test/main.py
@@ -6,6 +6,10 @@ import tempfile
 import unittest
 
 tests = [
+    'LL.CIS.CEN.BV_01_C',
+    'LL.CIS.CEN.BV_03_C',
+    'LL.CIS.PER.BV_01_C',
+    'LL.CIS.PER.BV_02_C',
     'LL.CON_.CEN.BV_41_C',
     'LL.CON_.CEN.BV_43_C',
     'LL.CON_.PER.BV_40_C',
