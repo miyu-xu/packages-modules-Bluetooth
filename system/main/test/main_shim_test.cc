9d5c0020973b1c61fe5c3f2043f8cdd631c46850
Chris Manton <cmanton@google.com>
2023-05-09 11:12:47 -0700

    dumpsys: Check for scan history log existence
    Bug: 281498218
    Test: net_test_main_shim
    
    Change-Id: I5212fa766c6f709b708fa3f3f65d0e2b71bfa6aa

--- system/main/test/main_shim_test.cc
+++ system/main/test/main_shim_test.cc
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <unistd.h>
 
 #include <cstddef>
 #include <cstdio>
@@ -782,5 +783,39 @@ TEST_F(MainShimTest, ticks_to_seconds) {
 
 TEST_F(MainShimTest, DumpConnectionHistory) {
   auto acl = MakeAcl();
-  acl->DumpConnectionHistory(1);
+  acl->DumpConnectionHistory(STDOUT_FILENO);
+}
+
+void DumpsysNeighbor(int fd);
+TEST_F(MainShimTest, DumpsysNeighbor) {
+  btm_cb.neighbor = {};
+
+  btm_cb.neighbor.inquiry_history_->Push({
+      .status = tBTM_INQUIRY_CMPL::CANCELED,
+      .num_resp = 45,
+      .resp_type = {20, 30, 40},
+      .start_time_ms = 0,
+  });
+
+  btm_cb.neighbor.inquiry_history_->Push({
+      .status = tBTM_INQUIRY_CMPL::CANCELED,
+      .num_resp = 123,
+      .resp_type = {50, 60, 70},
+      .start_time_ms = -1,
+  });
+
+  DumpsysNeighbor(STDOUT_FILENO);
+
+  const auto copy = btm_cb.neighbor.inquiry_history_->Pull();
+
+  for (const auto& it : copy) {
+    LOG_INFO(
+        "  duration_ms:%-5Lu num_resp:%-2u"
+        " std:%-2u rssi:%-2u ext:%-2u %12s",
+        it.timestamp - it.entry.start_time_ms, it.entry.num_resp,
+        it.entry.resp_type[BTM_INQ_RESULT_STANDARD],
+        it.entry.resp_type[BTM_INQ_RESULT_WITH_RSSI],
+        it.entry.resp_type[BTM_INQ_RESULT_EXTENDED],
+        btm_inquiry_cmpl_status_text(it.entry.status).c_str());
+  }
 }
