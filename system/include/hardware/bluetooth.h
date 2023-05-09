f5197d7821520981f9679b56e70919d16f7deb95
Etienne Ruffieux <eruffieux@google.com>
2023-05-09 01:16:34 +0000

    Revert "[HFP1.9] Add SDP related functions over HFP LC3"
    Revert submission 2541310-hfp1.9_lc3
    
    Reason for revert: b/281493813 call audio broken on older Android devices
    
    Reverted changes: /q/submissionid:2541310-hfp1.9_lc3
    
    Change-Id: Ib878cd05113d179c740c80eb17154bf53dbfb517

--- system/include/hardware/bluetooth.h
+++ system/include/hardware/bluetooth.h
@@ -890,13 +890,6 @@ typedef struct {
   bool (*get_wbs_supported)();
 
   /**
-   *
-   * Is swb supported by the controller
-   *
-   */
-  bool (*get_swb_supported)();
-
-  /**
    * Data passed from BluetoothDevice.metadata_changed
    *
    * @param remote_bd_addr remote address
