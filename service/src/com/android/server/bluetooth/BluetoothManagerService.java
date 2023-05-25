87f3c9ba930e8cf1b61eff45c46f28c7182424a5
William Escande <wescande@google.com>
2023-05-25 15:27:50 -0700

    Use mContext in constructor
    Since mContext is initialized, we should make use of it
    
    Bug: 262605980
    Test: atest ServiceBluetoothTests (No-Op)
    Change-Id: Iff251342704596367129dc7ee33193820ca9030a

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -660,11 +660,11 @@ class BluetoothManagerService {
                         mContext.getSystemService(UserManager.class),
                         "UserManager system service cannot be null");
 
-        mBinder = new BluetoothServiceBinder(this, context, mUserManager);
+        mBinder = new BluetoothServiceBinder(this, mContext, mUserManager);
         mBluetoothHandlerThread.start();
         mHandler = new BluetoothHandler(mBluetoothHandlerThread.getLooper());
 
-        mContentResolver = context.getContentResolver();
+        mContentResolver = mContext.getContentResolver();
 
         // Observe BLE scan only mode settings change.
         registerForBleScanModeChange();
@@ -739,13 +739,13 @@ class BluetoothManagerService {
                     new BluetoothAirplaneModeListener(
                             this,
                             mBluetoothHandlerThread.getLooper(),
-                            context,
+                            mContext,
                             mBluetoothNotificationManager);
         }
 
         mBluetoothSatelliteModeListener =
                 new BluetoothSatelliteModeListener(
-                        this, mBluetoothHandlerThread.getLooper(), context);
+                        this, mBluetoothHandlerThread.getLooper(), mContext);
     }
 
     IBluetoothManager.Stub getBinder() {
