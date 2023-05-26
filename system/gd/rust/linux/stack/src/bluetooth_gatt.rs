3d098402ec9936e46f34678699bddaf132428d2c
Ying Hsu <yinghsu@google.com>
2023-05-26 07:53:25 +0000

    floss: Validate advertisement data
    This patch validates advertisement data used in IBluetoothGatt
    advertising API before actually starting or updating an advertising
    set.
    
    Bug: 284234145
    Tag: #floss
    Test: manual test with btclient
    Test: nearby sharing and phone hub on brya
    Change-Id: I3dfcdbb3eb15ce955491ecf29573614c5ed12f82

--- system/gd/rust/linux/stack/src/bluetooth_gatt.rs
+++ system/gd/rust/linux/stack/src/bluetooth_gatt.rs
@@ -2012,10 +2012,17 @@ impl IBluetoothGatt for BluetoothGatt {
         }
 
         let device_name = self.get_adapter_name();
+        let is_legacy = parameters.is_legacy;
         let params = parameters.into();
         let adv_bytes = advertise_data.make_with(&device_name);
+        if !AdvertiseData::validate_raw_data(is_legacy, &adv_bytes) {
+            return INVALID_REG_ID;
+        }
         let scan_bytes =
             if let Some(d) = scan_response { d.make_with(&device_name) } else { Vec::<u8>::new() };
+        if !AdvertiseData::validate_raw_data(is_legacy, &scan_bytes) {
+            return INVALID_REG_ID;
+        }
         let periodic_params = if let Some(p) = periodic_parameters {
             p.into()
         } else {
@@ -2023,10 +2030,13 @@ impl IBluetoothGatt for BluetoothGatt {
         };
         let periodic_bytes =
             if let Some(d) = periodic_data { d.make_with(&device_name) } else { Vec::<u8>::new() };
+        if !AdvertiseData::validate_raw_data(false, &adv_bytes) {
+            return INVALID_REG_ID;
+        }
         let adv_timeout = clamp(duration, 0, 0xffff) as u16;
         let adv_events = clamp(max_ext_adv_events, 0, 0xff) as u8;
 
-        let s = AdvertisingSetInfo::new(callback_id, adv_timeout, adv_events);
+        let s = AdvertisingSetInfo::new(callback_id, adv_timeout, adv_events, is_legacy);
         let reg_id = s.reg_id();
         self.advertisers.add(s);
 
@@ -2105,6 +2115,9 @@ impl IBluetoothGatt for BluetoothGatt {
         let bytes = data.make_with(&device_name);
 
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
+            if !AdvertiseData::validate_raw_data(s.is_legacy(), &bytes) {
+                return;
+            }
             self.gatt.as_ref().unwrap().lock().unwrap().advertiser.set_data(
                 s.adv_id(),
                 false,
@@ -2119,6 +2132,9 @@ impl IBluetoothGatt for BluetoothGatt {
         }
 
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
+            if !AdvertiseData::validate_raw_data(s.is_legacy(), &data) {
+                return;
+            }
             self.gatt.as_ref().unwrap().lock().unwrap().advertiser.set_data(
                 s.adv_id(),
                 false,
@@ -2136,6 +2152,9 @@ impl IBluetoothGatt for BluetoothGatt {
         let bytes = data.make_with(&device_name);
 
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
+            if !AdvertiseData::validate_raw_data(s.is_legacy(), &bytes) {
+                return;
+            }
             self.gatt.as_ref().unwrap().lock().unwrap().advertiser.set_data(
                 s.adv_id(),
                 true,
@@ -2214,6 +2233,9 @@ impl IBluetoothGatt for BluetoothGatt {
         let bytes = data.make_with(&device_name);
 
         if let Some(s) = self.advertisers.get_by_advertiser_id(advertiser_id) {
+            if !AdvertiseData::validate_raw_data(false, &bytes) {
+                return;
+            }
             self.gatt
                 .as_ref()
                 .unwrap()
