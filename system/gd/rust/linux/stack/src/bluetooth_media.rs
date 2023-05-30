19ca1bec327e3d6466b04b61befe596ad652b887
Jeremy Wu <jrwu@google.com>
2023-05-30 03:50:59 +0000

    Floss: rework `force_cvsd` to `disabled_codecs`
    We used to have a parameter in `ConnectAudio` during SCO creation to
    force using CVSD.
    
    In this CL, we extend it so that we can force un/using certain codecs,
    along with the new codec, LC3.
    
    This should be landed only after https://crrev.com/c/4574163.
    
    Bug: 269970706
    Tag: #floss
    Test: emerge-brya floss
    Change-Id: Ie23ee2eff052a8c313c58d9da2c5032a6a4de614

--- system/gd/rust/linux/stack/src/bluetooth_media.rs
+++ system/gd/rust/linux/stack/src/bluetooth_media.rs
@@ -111,7 +111,7 @@ pub trait IBluetoothMedia {
     fn get_presentation_position(&mut self) -> PresentationPosition;
 
     /// Start the SCO setup to connect audio
-    fn start_sco_call(&mut self, address: String, sco_offload: bool, force_cvsd: bool) -> bool;
+    fn start_sco_call(&mut self, address: String, sco_offload: bool, disabled_codecs: i32) -> bool;
     fn stop_sco_call(&mut self, address: String);
 
     /// Set the current playback status: e.g., playing, paused, stopped, etc. The method is a copy
@@ -646,7 +646,7 @@ impl BluetoothMedia {
                         // This is only used for Bluetooth HFP qualification.
                         if self.phone_ops_enabled && self.phone_state.num_active > 0 {
                             debug!("[{}]: Connect SCO due to active call.", DisplayAddress(&addr));
-                            self.start_sco_call_impl(addr.to_string(), false, false);
+                            self.start_sco_call_impl(addr.to_string(), false, 0);
                         }
                     }
                     BthfConnectionState::Disconnected => {
@@ -852,7 +852,7 @@ impl BluetoothMedia {
                 self.phone_state_change("".into());
 
                 debug!("[{}]: Start SCO call due to ATA", DisplayAddress(&addr));
-                self.start_sco_call_impl(addr.to_string(), false, false);
+                self.start_sco_call_impl(addr.to_string(), false, 0);
             }
             HfpCallbacks::HangupCall(addr) => {
                 if !self.hangup_call_impl() {
@@ -1358,7 +1358,7 @@ impl BluetoothMedia {
         &mut self,
         address: String,
         sco_offload: bool,
-        force_cvsd: bool,
+        disabled_codecs: i32,
     ) -> bool {
         match (|| -> Result<(), &str> {
             let addr = RawAddress::from_string(address.clone())
@@ -1366,7 +1366,7 @@ impl BluetoothMedia {
             info!("Start sco call for {}", DisplayAddress(&addr));
 
             let hfp = self.hfp.as_mut().ok_or("Uninitialized HFP to start the sco call")?;
-            if hfp.connect_audio(addr, sco_offload, force_cvsd) != 0 {
+            if hfp.connect_audio(addr, sco_offload, disabled_codecs) != 0 {
                 return Err("SCO connect_audio status failed");
             }
             info!("SCO connect_audio status success");
@@ -2149,8 +2149,8 @@ impl IBluetoothMedia for BluetoothMedia {
         };
     }
 
-    fn start_sco_call(&mut self, address: String, sco_offload: bool, force_cvsd: bool) -> bool {
-        self.start_sco_call_impl(address, sco_offload, force_cvsd)
+    fn start_sco_call(&mut self, address: String, sco_offload: bool, disabled_codecs: i32) -> bool {
+        self.start_sco_call_impl(address, sco_offload, disabled_codecs)
     }
 
     fn stop_sco_call(&mut self, address: String) {
@@ -2356,7 +2356,7 @@ impl IBluetoothTelephony for BluetoothMedia {
             }
         }) {
             info!("Start SCO call due to call answered");
-            self.start_sco_call_impl(addr.to_string(), false, false);
+            self.start_sco_call_impl(addr.to_string(), false, 0);
         }
 
         true
@@ -2416,7 +2416,7 @@ impl IBluetoothTelephony for BluetoothMedia {
     }
 
     fn audio_connect(&mut self, address: String) -> bool {
-        self.start_sco_call_impl(address, false, false)
+        self.start_sco_call_impl(address, false, 0)
     }
 
     fn audio_disconnect(&mut self, address: String) {
