c1e962f2514ae3419e5a5da51efa9944b492551c
Fahad Afroze <fafroze@google.com>
2023-06-01 11:45:16 +0000

    [Avatar] Implemented sdp tests in avatar.
    Implemented sdp connect, sdp disconnect, search services and attributes using bumble sdp api
    
    Bug: 285289635
    
    Test: avatar run --mobly-std-log --include-filter 'SdpTest'
    Change-Id: Ib60480a5735a110ce5690d17ab1b8153bd1561ad

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -20,6 +20,7 @@ import cases.le_host_test
 import cases.le_security_test
 import cases.security_test
 import gatt_test
+import sdp_test
 import smp_test
 
 _TEST_CLASSES_LIST = [
@@ -27,6 +28,7 @@ _TEST_CLASSES_LIST = [
     cases.le_host_test.LeHostTest,
     cases.security_test.SecurityTest,
     cases.le_security_test.LeSecurityTest,
+    sdp_test.SdpTest,
     smp_test.SmpTest,
     gatt_test.GattTest,
     asha_test.AshaTest,
@@ -45,7 +47,7 @@ if __name__ == "__main__":
     # This is a hack for `tradefed` because of `b/166468397`.
     if '--' in sys.argv:
         index = sys.argv.index('--')
-        sys.argv = sys.argv[:1] + sys.argv[index + 1 :]
+        sys.argv = sys.argv[:1] + sys.argv[index + 1:]
 
     # Enable bumble snoop logger.
     ns, argv = _parse_cli_args()
