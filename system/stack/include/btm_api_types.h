bd8a7d111d81391d982fdcd51dcf4256928e9e39
Chris Manton <cmanton@google.com>
2023-06-03 17:01:17 -0700

    [2/8] Replace BTM_LKEY_TYPE_DEBUG_COMB => HCI_LKEY_TYPE_DEBUG_COMB
    Bug: 285713519
    Test: mma .
    
    Change-Id: I1a6cab481e502ca1676ee3aff80b4ce95fc26796

--- system/stack/include/btm_api_types.h
+++ system/stack/include/btm_api_types.h
@@ -319,7 +319,6 @@ enum : uint16_t {
 /* Link Key types used to generate the new link key.
  * returned in link key notification callback function
  */
-#define BTM_LKEY_TYPE_DEBUG_COMB HCI_LKEY_TYPE_DEBUG_COMB
 #define BTM_LKEY_TYPE_UNAUTH_COMB HCI_LKEY_TYPE_UNAUTH_COMB
 #define BTM_LKEY_TYPE_AUTH_COMB HCI_LKEY_TYPE_AUTH_COMB
 #define BTM_LKEY_TYPE_CHANGED_COMB HCI_LKEY_TYPE_CHANGED_COMB
@@ -333,7 +332,7 @@ inline std::string linkkey_type_text(const int linkkey_type) {
       return std::string("COMBINATION");
     case HCI_LKEY_TYPE_REMOTE_UNIT:
       return std::string("REMOTE_UNIT");
-    case BTM_LKEY_TYPE_DEBUG_COMB:
+    case HCI_LKEY_TYPE_DEBUG_COMB:
       return std::string("DEBUG_COMB");
     case BTM_LKEY_TYPE_UNAUTH_COMB:
       return std::string("UNAUTH_COMB");
