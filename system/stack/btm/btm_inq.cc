579da218eb5d5df5cb2d41b67dfdf7751d35de4b
Chris Manton <cmanton@google.com>
2023-06-06 14:27:27 -0700

    Introduce stack::btm::btm_eir.h
    Bug: 313724096
    Test: m .
    Change-Id: I413d3199f47974c4ed743015a450a85af92fa51f

--- system/stack/btm/btm_inq.cc
+++ system/stack/btm/btm_inq.cc
@@ -45,6 +45,7 @@
 #include "osi/include/osi.h"
 #include "osi/include/properties.h"
 #include "osi/include/stack_power_telemetry.h"
+#include "stack/btm/btm_eir.h"
 #include "stack/btm/btm_int_types.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/include/acl_api.h"
