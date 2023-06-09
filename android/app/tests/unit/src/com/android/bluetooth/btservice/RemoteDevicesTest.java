167854378219a9c4ea36c2d9e167e99d22d55893
Alice Kuo <aliceypkuo@google.com>
2023-06-09 09:37:35 +0800

    Do not expose the cordinated set status as LE audio disabled
    If the LE audio disabled, the phone would treat the remote LE audio
    TWS be a single device. Update the cordinated set that should only
    supported as CSIP profile enabled.
    
    Bug: 284410495
    Test: atest BluetoothInstrumentationTests
    Change-Id: Ibccf52ae4d26715c2d67be308bc6b576550c0512

--- android/app/tests/unit/src/com/android/bluetooth/btservice/RemoteDevicesTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/RemoteDevicesTest.java
@@ -635,6 +635,21 @@ public class RemoteDevicesTest {
                 .getHfAudioPolicyForRemoteAg());
     }
 
+    @Test
+    public void testIsCoordinatedSetMemberAsLeAudioDisabled() {
+        doReturn((long) (1 << BluetoothProfile.CSIP_SET_COORDINATOR))
+                .when(mAdapterService)
+                .getSupportedProfilesBitMask();
+
+        // Verify that device property is null initially
+        Assert.assertNull(mRemoteDevices.getDeviceProperties(mDevice1));
+
+        DeviceProperties deviceProp = mRemoteDevices.getDeviceProperties(mDevice1);
+        deviceProp.setIsCoordinatedSetMember(true);
+
+        Assert.assertFalse(deviceProp.isCoordinatedSetMember());
+    }
+
     private static void verifyBatteryLevelChangedIntent(BluetoothDevice device, int batteryLevel,
             ArgumentCaptor<Intent> intentArgument) {
         verifyBatteryLevelChangedIntent(device, batteryLevel, intentArgument.getValue());
