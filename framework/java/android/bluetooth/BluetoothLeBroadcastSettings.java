1950f9a34bdbfb69545548fd88f5d344f46fc646
Rongxuan Liu <rongxuan@google.com>
2023-06-14 18:59:41 +0000

    [le audio] Add length check for broadcast code and name
    Tag: #feature
    Bug: 286606320
    Test: atest BluetoothLeBroadcastSettingsTest
    Change-Id: I21a63cd29ead53a29151c7b644db62181fde24da

--- framework/java/android/bluetooth/BluetoothLeBroadcastSettings.java
+++ framework/java/android/bluetooth/BluetoothLeBroadcastSettings.java
@@ -265,13 +265,24 @@ public final class BluetoothLeBroadcastSettings implements Parcelable {
         /**
          * Set broadcast name for the broadcast group.
          *
+         * <p>As defined in Public Broadcast Profile V1.0, section 5.1.
+         *
+         * <p>Broadcast_Name AD Type is a UTF-8 encoded string containing a minimum of 4
+         *
+         * <p>and a maximum of 32 human-readable characters.
+         *
          * @param broadcastName Broadcast name for this broadcast group, null if no name provided
+         * @throws IllegalArgumentException if name length is invalid
          * @return this builder
          * @hide
          */
         @SystemApi
         @NonNull
         public Builder setBroadcastName(@Nullable String broadcastName) {
+            if (broadcastName != null
+                    && ((broadcastName.length() > 32) || (broadcastName.length() < 4))) {
+                throw new IllegalArgumentException("Invalid broadcast name length");
+            }
             mBroadcastName = broadcastName;
             return this;
         }
@@ -287,13 +298,18 @@ public final class BluetoothLeBroadcastSettings implements Parcelable {
          * <p>It must be a UTF-8 string that has at least 4 octets and should not exceed 16 octets.
          *
          * @param broadcastCode Broadcast Code for this broadcast group, null if code is not
-         *     required
+         *     required for non-encrypted broadcast
+         * @throws IllegalArgumentException if code length is invalid
          * @return this builder
          * @hide
          */
         @SystemApi
         @NonNull
         public Builder setBroadcastCode(@Nullable byte[] broadcastCode) {
+            if (broadcastCode != null
+                    && ((broadcastCode.length > 16) || (broadcastCode.length < 4))) {
+                throw new IllegalArgumentException("Invalid broadcast code length");
+            }
             mBroadcastCode = broadcastCode;
             return this;
         }
