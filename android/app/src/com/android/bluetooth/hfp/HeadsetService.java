1de79022732314f43169531cf7e28ee87a3657d6
Eric Shih <shiheric@google.com>
2023-07-03 05:42:59 +0000

    Set bt_stack's active device to default if java removeActiveDevice
    Bug: 239907969
    Test: atest BluetoothInstrumentationTests
    Change-Id: I9e28093cea2e55260e1835e3873c88de81f4440b

--- android/app/src/com/android/bluetooth/hfp/HeadsetService.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetService.java
@@ -1373,6 +1373,7 @@ public class HeadsetService extends ProfileService {
             }
             mActiveDevice = null;
             broadcastActiveDevice(null);
+            mNativeInterface.setActiveDevice(null);
         }
     }
 
