1aeb5e2dc703e72622e312cdcc8cab7982fe71d9
Chris Manton <cmanton@google.com>
2023-07-19 15:49:24 -0700

    IA2: Use btm_interface for bta::dm::discovery
    Bug: 291994773
    Test: m .
    
    Change-Id: Ib98c8445a4707b1e694589fe63325ede616b9f7d

--- system/bta/test/bta_sdp_test.cc
+++ system/bta/test/bta_sdp_test.cc
@@ -91,6 +91,8 @@ class BtaSdpRegisteredTest : public BtaSdpTest {
 
 TEST_F(BtaSdpTest, nop) {}
 
+const char kName[] = "Hello";
+
 TEST_F(BtaSdpRegisteredTest, bta_dm_sdp_result_SDP_SUCCESS) {
   bta_dm_search_cb.service_index = BTA_MAX_SERVICE_ID;
 
@@ -100,5 +102,9 @@ TEST_F(BtaSdpRegisteredTest, bta_dm_sdp_result_SDP_SUCCESS) {
               .sdp_result = SDP_SUCCESS,
           },
   };
+  btm_client_interface.security.BTM_SecReadDevName =
+      [](const RawAddress& bd_addr) -> const char* { return kName; };
+  btm_client_interface.security.BTM_SecDeleteRmtNameNotifyCallback =
+      [](tBTM_RMT_NAME_CALLBACK*) -> bool { return true; };
   bta_dm_sdp_result(&msg);
 }
