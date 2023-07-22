9f1bc502aa68b54466f7a1e9a9b672f6cfcad4e7
Alice Kuo <aliceypkuo@google.com>
2023-07-22 17:34:29 +0800

    bluetooth: Make use of metadata v7 version
    With this change, Bluetooth is able to receive Tags along with a
    metadata which is a feature of v7 structs for sink and source metadata.
    
    Bug: 283349711
    Test: build
    Change-Id: I0e88cd2d744584328bed355ca75d41a83d2d956c

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -4408,8 +4408,7 @@ class LeAudioClientImpl : public LeAudioClient {
     return true;
   }
 
-  void OnLocalAudioSourceMetadataUpdate(
-      std::vector<struct playback_track_metadata> source_metadata) {
+  void OnLocalAudioSourceMetadataUpdate(source_metadata_v7 source_metadata) {
     if (active_group_id_ == bluetooth::groups::kGroupUnknown) {
       LOG(WARNING) << ", cannot start streaming if no active group set";
       return;
@@ -4558,8 +4557,7 @@ class LeAudioClientImpl : public LeAudioClient {
               ToString(contexts_pair.source).c_str());
   }
 
-  void OnLocalAudioSinkMetadataUpdate(
-      std::vector<struct record_track_metadata> sink_metadata) {
+  void OnLocalAudioSinkMetadataUpdate(sink_metadata_v7 sink_metadata) {
     if (active_group_id_ == bluetooth::groups::kGroupUnknown) {
       LOG(WARNING) << ", cannot start streaming if no active group set";
       return;
@@ -5533,8 +5531,7 @@ class SourceCallbacksImpl : public LeAudioSourceAudioHalClient::Callbacks {
     if (instance) instance->OnLocalAudioSourceResume();
   }
 
-  void OnAudioMetadataUpdate(
-      std::vector<struct playback_track_metadata> source_metadata) override {
+  void OnAudioMetadataUpdate(source_metadata_v7 source_metadata) override {
     if (instance)
       instance->OnLocalAudioSourceMetadataUpdate(std::move(source_metadata));
   }
@@ -5550,8 +5547,7 @@ class SinkCallbacksImpl : public LeAudioSinkAudioHalClient::Callbacks {
     if (instance) instance->OnLocalAudioSinkResume();
   }
 
-  void OnAudioMetadataUpdate(
-      std::vector<struct record_track_metadata> sink_metadata) override {
+  void OnAudioMetadataUpdate(sink_metadata_v7 sink_metadata) override {
     if (instance)
       instance->OnLocalAudioSinkMetadataUpdate(std::move(sink_metadata));
   }
