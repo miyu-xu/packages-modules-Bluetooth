f24b1a73c7fea1549f3a815fb2da26e6ddc9905e
William Escande <wescande@google.com>
2023-07-24 10:47:41 -0700

    GattService: No longer start a remote service
    Since the Bluetooth app is self binding its own service, it does not
    need to be started as a remote service.
    
    Bug: 285959170
    Test: atest BluetoothInstrumentationTests
    Test: atest ProfileServiceTest
    Test: atest AdapterServiceTest
    Test: atest AdapterServiceRestartTest
    Test: atest AdapterServiceFactoryResetTest
    Test: atest GattServiceTest
    Test: boot bluetooth | having a working gattService is a unskippable
          step of startup. Simply booting the bluetooth validate this CL
    Change-Id: I9474b0fc5d85b811b148698b143b4b3b03ba2756

--- android/app/src/com/android/bluetooth/gatt/GattService.java
+++ android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -56,6 +56,7 @@ import android.bluetooth.le.ScanSettings;
 import android.companion.AssociationInfo;
 import android.companion.CompanionDeviceManager;
 import android.content.AttributionSource;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager.PackageInfoFlags;
@@ -294,6 +295,11 @@ public class GattService extends ProfileService {
     private String mExposureNotificationPackage;
     private Handler mTestModeHandler;
     private final Object mTestModeLock = new Object();
+
+    public GattService(Context ctx) {
+        attachBaseContext(ctx);
+        onCreate();
+    }
 
     public static boolean isEnabled() {
         return BluetoothProperties.isProfileGattEnabled().orElse(true);
@@ -606,34 +612,6 @@ public class GattService extends ProfileService {
             }
             Log.e(TAG, "getService() - Service requested, but not available!");
             return null;
-        }
-
-        @Override
-        public void startService() {
-            GattService service = mService;
-            if (service == null) {
-                Log.e(TAG, "startService: Service is null");
-                return;
-            }
-            if (!Utils.checkConnectPermissionForDataDelivery(
-                    service, null, "GattService startService")) {
-                return;
-            }
-            service.doStart();
-        }
-
-        @Override
-        public void stopService() {
-            GattService service = mService;
-            if (service == null) {
-                Log.e(TAG, "stopService: Service is null");
-                return;
-            }
-            if (!Utils.checkConnectPermissionForDataDelivery(
-                    service, null, "GattService stopService")) {
-                return;
-            }
-            service.doStop();
         }
 
         @Override
@@ -1772,15 +1750,6 @@ public class GattService extends ProfileService {
                 return;
             }
             service.disconnectAll(attributionSource);
-        }
-
-        @Override
-        public void unregAll(AttributionSource attributionSource) {
-            GattService service = getService();
-            if (service == null) {
-                return;
-            }
-            service.unregAll(attributionSource);
         }
 
         @Override
@@ -3431,7 +3400,7 @@ public class GattService extends ProfileService {
     }
 
     @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
-    void unregAll(AttributionSource attributionSource) {
+    public void unregAll(AttributionSource attributionSource) {
         for (Integer appId : mClientMap.getAllAppsIds()) {
             if (DBG) {
                 Log.d(TAG, "unreg:" + appId);
