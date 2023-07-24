368ddb1345d5268902cf680714ddada2e9ee1273
Chienyuan Huang <chienyuanhuang@google.com>
2023-07-24 07:38:15 +0000

    CS: Add PDL for CS command/event
    Bug: 292460350
    Test: mma packages/modules/Bluetooth
    Change-Id: I9f05e9faf8636eec7e8d39be75e323d2b32b6b4d

--- system/gd/hci/controller.cc
+++ system/gd/hci/controller.cc
@@ -1042,6 +1042,20 @@ struct Controller::impl {
         return true;
       case OpCode::NONE:
         return false;
+      case OpCode::LE_CS_READ_LOCAL_SUPPORTED_CAPABILITIES:
+      case OpCode::LE_CS_READ_REMOTE_SUPPORTED_CAPABILITIES:
+      case OpCode::LE_CS_SECURITY_ENABLE:
+      case OpCode::LE_CS_SET_DEFAULT_SETTINGS:
+      case OpCode::LE_CS_CREATE_CONFIG:
+      case OpCode::LE_CS_REMOVE_CONFIG:
+      case OpCode::LE_CS_SET_CHANNEL_CLASSIFICATION:
+      case OpCode::LE_CS_PROCEDURE_ENABLE:
+      case OpCode::LE_CS_TEST:
+      case OpCode::LE_CS_TEST_END:
+      case OpCode::LE_CS_SET_PROCEDURE_PARAMETERS:
+      case OpCode::LE_CS_WRITE_REMOTE_SUPPORTED_CAPABILITIES:
+        // TODO add to OP_CODE_MAPPING list
+        return false;
     }
     return false;
   }
