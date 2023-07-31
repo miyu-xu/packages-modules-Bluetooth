990a232ce8e59a128f04c8a183f6882edaabfbe7
Wei-Luan Wang <weiluanwang@google.com>
2023-07-31 04:35:09 +0000

    floss: Migrate base::Bind in do_in_(jni|main)_thread
    base::Bind was deprecated. Use base::BindOnce for the second argument of
    do_in_main_thread and do_in_jni_thread since the function expects a
    base::OnceCallback.
    
    This is a mechanical change by replacing base::Bind with base::BindOnce
    for do_in_main_thread and do_in_jni_thread.
    
    Bug: 272116782
    Tag: #floss
    Test: presubmit
    Change-Id: I325890bf22926eebbdc9f4c6ef39986acc6b758e

--- system/bta/le_audio/le_audio_client_test.cc
+++ system/bta/le_audio/le_audio_client_test.cc
@@ -1543,8 +1543,9 @@ class UnicastTestNoInit : public Test {
     EXPECT_CALL(mock_gatt_interface_, Close(conn_id)).Times(1);
 
     do_in_main_thread(
-        FROM_HERE, base::Bind(&LeAudioClient::Disconnect,
-                              base::Unretained(LeAudioClient::Get()), address));
+        FROM_HERE,
+        base::BindOnce(&LeAudioClient::Disconnect,
+                       base::Unretained(LeAudioClient::Get()), address));
     SyncOnMainLoop();
     Mock::VerifyAndClearExpectations(&mock_btm_interface_);
     Mock::VerifyAndClearExpectations(&mock_gatt_interface_);
@@ -3133,18 +3134,18 @@ TEST_F(UnicastTestNoInit, ConnectFailedDueToInvalidParameters) {
   EXPECT_CALL(mock_storage_load, Call()).WillOnce([&]() {
     do_in_main_thread(
         FROM_HERE,
-        base::Bind(&LeAudioClient::AddFromStorage, test_address0, autoconnect,
-                   codec_spec_conf::kLeAudioLocationFrontLeft,
-                   codec_spec_conf::kLeAudioLocationFrontLeft, 0xff, 0xff,
-                   std::move(handles), std::move(snk_pacs), std::move(src_pacs),
-                   std::move(ases)));
+        base::BindOnce(&LeAudioClient::AddFromStorage, test_address0,
+                       autoconnect, codec_spec_conf::kLeAudioLocationFrontLeft,
+                       codec_spec_conf::kLeAudioLocationFrontLeft, 0xff, 0xff,
+                       std::move(handles), std::move(snk_pacs),
+                       std::move(src_pacs), std::move(ases)));
     do_in_main_thread(
         FROM_HERE,
-        base::Bind(&LeAudioClient::AddFromStorage, test_address1, autoconnect,
-                   codec_spec_conf::kLeAudioLocationFrontRight,
-                   codec_spec_conf::kLeAudioLocationFrontRight, 0xff, 0xff,
-                   std::move(handles), std::move(snk_pacs), std::move(src_pacs),
-                   std::move(ases)));
+        base::BindOnce(&LeAudioClient::AddFromStorage, test_address1,
+                       autoconnect, codec_spec_conf::kLeAudioLocationFrontRight,
+                       codec_spec_conf::kLeAudioLocationFrontRight, 0xff, 0xff,
+                       std::move(handles), std::move(snk_pacs),
+                       std::move(src_pacs), std::move(ases)));
   });
 
   // Expect stored device0 to connect automatically (first directed connection )
