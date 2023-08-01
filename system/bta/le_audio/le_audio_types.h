8835da681ac4a7b2a1e4a6e5807618bd236329eb
Jakub Tyszkowski <tyszkowski@google.com>
2023-08-01 11:23:42 +0000

    LeAudio: Improve data path setup
    The data path configuration should be provided by the audio set
    configuration provider. For now, these are based on the current
    codec location (either HOST or ADSP). In future this should
    be provided from the vendor interface over the AIDL.
    
    Bug: 295972694
    Test: atest bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine
    Change-Id: If39a1d1ac089f236e4ed9258644c197a57f1e723

--- system/bta/le_audio/le_audio_types.h
+++ system/bta/le_audio/le_audio_types.h
@@ -718,6 +718,11 @@ struct ase {
   uint8_t framing;
   uint8_t preferred_phy;
 
+  /* Whether the codec location is transparent to the controller */
+  bool is_codec_in_controller;
+  /* Datapath ID used to configure an ISO channel for these ASEs */
+  uint8_t data_path_id;
+
   /* Qos configuration */
   uint16_t max_sdu_size;
   uint8_t retrans_nb;
@@ -794,6 +799,12 @@ struct SetConfiguration {
   uint8_t direction;  /* Direction of set */
   uint8_t device_cnt; /* How many devices must be in set */
   uint8_t ase_cnt;    /* How many ASE we need in configuration */
+
+  /* Whether the codec location is transparent to the controller */
+  bool is_codec_in_controller = false;
+  /* Datapath ID used to configure an ISO channel for these ASEs */
+  uint8_t data_path_id = bluetooth::hci::iso_manager::kIsoDataPathHci;
+
   CodecCapabilitySetting codec;
   QosConfigSetting qos;
   types::LeAudioConfigurationStrategy strategy;
