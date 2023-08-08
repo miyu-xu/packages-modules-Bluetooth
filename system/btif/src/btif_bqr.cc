a2943af213a3be1094a31f62a105a2a79cba0b86
Jakub Rotkiewicz <rotkiewicz@google.com>
2023-08-08 07:59:17 +0000

    BQR: Log error instead of crash if event too short
    When the Bluetooth Quality Report Event came too short the process
    crashed. As there is prossibility for BQR to appear shorter,
    because not all controllers' vendors extended the BQR event,
    a temporary solution is to only print the error and return.
    
    Bug: 293849013
    Test: m <target>
    Test: manual - check BQR event logs
    Change-Id: Iab7ec1f83135ddf8d517bbe919d3c9df1648885a

--- system/btif/src/btif_bqr.cc
+++ system/btif/src/btif_bqr.cc
@@ -54,7 +54,7 @@ std::unique_ptr<BluetoothQualityReportInterface> bluetoothQualityReportInstance;
 void BqrVseSubEvt::ParseBqrLinkQualityEvt(uint8_t length,
                                           const uint8_t* p_param_buf) {
   if (length < kLinkQualityParamTotalLen) {
-    LOG(FATAL) << __func__
+    LOG(ERROR) << __func__
                << ": Parameter total length: " << std::to_string(length)
                << " is abnormal. It shall be not shorter than: "
                << std::to_string(kLinkQualityParamTotalLen);
