ec6f5cf9f1994d834687e6819279e34a95e86ebe
William Escande <wescande@google.com>
2023-08-15 18:01:21 -0700

    AvrcpTarget: Extract NativeInterface
    Bug: 295237486
    Test: atest ProfileServiceTest
    Change-Id: I99ab6b81031ecc6ee383222061782ca683a67de2

--- android/app/src/com/android/bluetooth/avrcp/AvrcpNativeInterface.java
+++ android/app/src/com/android/bluetooth/avrcp/AvrcpNativeInterface.java
@@ -26,6 +26,8 @@ import com.android.bluetooth.audio_util.PlayStatus;
 import com.android.bluetooth.audio_util.PlayerInfo;
 import com.android.bluetooth.audio_util.PlayerSettingsManager.PlayerSettingsValues;
 import com.android.bluetooth.btservice.AdapterService;
+import com.android.internal.annotations.GuardedBy;
+import com.android.internal.annotations.VisibleForTesting;
 
 import java.util.List;
 import java.util.Objects;
@@ -38,12 +40,21 @@ public class AvrcpNativeInterface {
     private static final String TAG = "AvrcpNativeInterface";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
-    private static AvrcpNativeInterface sInstance;
+    @GuardedBy("INSTANCE_LOCK")
+    @VisibleForTesting
+    public static AvrcpNativeInterface sInstance;
+
+    private static final Object INSTANCE_LOCK = new Object();
+
     private AvrcpTargetService mAvrcpService;
     private AdapterService mAdapterService;
 
     static {
-        classInitNative();
+        if (Utils.isInstrumentationTestMode()) {
+            Log.w(TAG, "App is instrumented. Skip loading the native");
+        } else {
+            classInitNative();
+        }
     }
 
     private AvrcpNativeInterface() {
@@ -51,9 +62,11 @@ public class AvrcpNativeInterface {
                 "AdapterService cannot be null when AvrcpNativeInterface init");
     }
 
-    static AvrcpNativeInterface getInterface() {
-        if (sInstance == null) {
-            sInstance = new AvrcpNativeInterface();
+    static AvrcpNativeInterface getInstance() {
+        synchronized (INSTANCE_LOCK) {
+            if (sInstance == null) {
+                sInstance = new AvrcpNativeInterface();
+            }
         }
 
         return sInstance;
