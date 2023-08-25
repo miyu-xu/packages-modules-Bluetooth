fc4523e9316bfa15e53405f1478d3415880b5a66
Himanshu Rawat <rwt@google.com>
2023-08-25 00:12:54 +0000

    SMP MITM Security Request on encrypted link
    Test steps
    1. Connect over LE
    2. Perform authenticated LE pairing
    3. Disconnect
    4. Reconnect as central
    5. Wait for the link to be encrypted
    6. Send SMP Security Request with MITM protection requirement
    
    Expectation: The link is encrypted again. No repairing.
    
    Cosmetic: Split SMP tests into separate classes/files
    
    Bug: 295061381
    Test: avatar run --include-filter="SmpTest"
    Test: atest avatar -v
    Change-Id: Ifbd291eb639a0858b184c04c52edad6d7ae42e6e

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -22,7 +22,8 @@ import cases.security_test
 import gatt_test
 import hfpclient_test
 import sdp_test
-import smp_test
+import smp.smp_test as smp_test
+import hfpclient_test
 
 _TEST_CLASSES_LIST = [
     cases.host_test.HostTest,
