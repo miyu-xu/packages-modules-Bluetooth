d76994968c327dc9d7b08d7bd5cf5536c90d66d1
Archie Pusaka <apusaka@google.com>
2023-09-04 20:09:21 +0800

    Floss: Add DBus logging for inbound traffic
    Inbound and outbound DBus traffic should be logged if it's not noisy,
    as it contains critical debugging information.
    
    This CL enables logging the inbound traffic.
    
    Bug: 298149797
    Tag: #floss
    Test: Manually observe bluetooth.log
    Test: mma -j
    
    Change-Id: I370dbbc1bb369feb3947c2fb916114567063eacc

--- system/gd/rust/linux/mgmt/src/bluetooth_manager_dbus.rs
+++ system/gd/rust/linux/mgmt/src/bluetooth_manager_dbus.rs
@@ -2,7 +2,7 @@ use dbus::arg::RefArg;
 use dbus::strings::Path;
 use dbus_crossroads;
 use dbus_macros::{dbus_method, dbus_propmap, dbus_proxy_obj, generate_dbus_exporter};
-use dbus_projection::{dbus_generated, DisconnectWatcher};
+use dbus_projection::prelude::*;
 
 use btstack::RPCProxy;
 
@@ -81,7 +81,7 @@ impl IBluetoothManager for BluetoothManagerDBus {
 /// D-Bus projection of IBluetoothManagerCallback.
 struct BluetoothManagerCallbackDBus {}
 
-#[dbus_proxy_obj(BluetoothManagerCallback, "org.chromium.bluetooth.ManagerCallback")]
+#[dbus_proxy_obj(BluetoothManagerCallbackkkkk, "org.chromium.bluetooth.ManagerCallback")]
 impl IBluetoothManagerCallback for BluetoothManagerCallbackDBus {
     #[dbus_method("OnHciDeviceChanged")]
     fn on_hci_device_changed(&mut self, hci_interface: i32, present: bool) {}
