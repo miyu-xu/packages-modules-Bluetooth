300952ee3a2d887667e1f9dde105842b1a562636
Chris Manton <cmanton@google.com>
2023-09-06 16:45:44 -0700

    Introduce unit_test net_test_btif_av
    Bug: 299365537
    Test: atest net_test_btif_av
    
    Change-Id: Ie0ece807cd412db00c7f967c8a7507ad4b6a126c

---
+++ system/btif/test/btif_av_test.cc
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bta/include/bta_ag_api.h"
+#include "btif/include/stack_manager.h"
+
+static const stack_manager_t interface = {nullptr, nullptr, nullptr, nullptr,
+                                          nullptr};
+
+const stack_manager_t* stack_manager_get_interface() { return &interface; }
+
+void bte_load_did_conf(const char* p_path);
+
+// Stubbed
+const tBTA_AG_RES_DATA tBTA_AG_RES_DATA::kEmpty = {};
