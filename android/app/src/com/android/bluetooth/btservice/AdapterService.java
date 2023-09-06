a012f56ecdecd6c6fd9a080e563709cfc76c291e
Aritra Sen <aritrasen@google.com>
2023-09-06 18:47:40 +0000

    Make Phone Policy changes on active device changes handled via direct calls routed via adapter service.
    Move active device changes logic in adapter properties to handle it more cleanly.
    
    Tag: #refactor
    Bug: 296932947
    Test: atest BluetoothInstrumentationTests
    Change-Id: I998246f8da045921c018a5a89b355665b229050b

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -6905,6 +6905,12 @@ public class AdapterService extends Service {
         mGattService.notifyProfileConnectionStateChange(profile, fromState, toState);
     }
 
+    /** Handle Bluetooth app state when active device changes for a given {@code profile}. */
+    public void handleActiveDeviceChange(int profile, BluetoothDevice device) {
+        mActiveDeviceManager.profileActiveDeviceChanged(profile, device);
+        mPhonePolicy.profileActiveDeviceChanged(profile, device);
+    }
+
     static int convertScanModeToHal(int mode) {
         switch (mode) {
             case BluetoothAdapter.SCAN_MODE_NONE:
