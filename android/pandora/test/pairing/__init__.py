313ccba89b47cc05d422b74d7b3b676c53c61a68
Hui Peng <phui@google.com>
2023-09-06 09:21:23 -0700

    Add tests for validating access to services
    Changes tested:
    - ag/24734374
    - ag/24734375
    - ag/24734377
    - ag/24706639
    - ag/24706640
    
    avatar run --mobly-std-log --include-filter 'ServiceAccessTempBondingTest' --mobly-options="-v"
    
    Bug: 294854926
    Test: atest avatar
    Flag: EXEMPT, Tests
    Change-Id: I90861edcceab3a2c7bb8ee80945feb030f6332e9

--- android/pandora/test/pairing/__init__.py
+++ android/pandora/test/pairing/__init__.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .misc import br_edr_service_access_tests
 from .smp import SmpTest
 
 _test_class_list = [
+    br_edr_service_access_tests.ServiceAccessTempBondingTest,
     SmpTest,
 ]
 
