526d6ceb190a988685aadda85d2d8012c460365a
Hsin-chen Chuang <chharry@google.com>
2023-09-07 18:23:13 +0800

    SMP over BR: Clear LE link key info for temporary bond
    In case a temporary bond triggered SMP over BR, the link key would be
    dropped in btif layer but btm only reset the classic link key info on
    disconnected.
    
    The LE link key info can't be reset together with classic because at
    that point we can't distinguish a LTK is generated by the current
    temporary bond or not (it may be generated by a previous bond).
    
    As a result, this patch clears the LE flags on SMP_COMPLT_EVT when it's
    a temporary bond and there is no LE ACL handle.
    
    Bug: 298459880
    Test: mmm packages/modules/Bluetooth
    Test: bluetooth_AdapterCLHealth.all_floss
    Test: Manually tested NearbyShare and CrossDevice on ChromeOS
    Change-Id: Ib588fc62dff7aa37ff278ce0657ea14400215760

--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -1872,6 +1872,17 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
             p_dev_rec->sec_state = BTM_SEC_STATE_IDLE;
             /* add all bonded device into resolving list if IRK is available*/
             btm_ble_resolving_list_load_dev(*p_dev_rec);
+
+            if (p_dev_rec->bond_type == tBTM_SEC_DEV_REC::BOND_TYPE_TEMPORARY &&
+                p_dev_rec->ble_hci_handle == 0xffff) {
+              // At this point LTK should have been dropped by btif and the
+              // flags need to be reset here.
+              LOG_DEBUG(
+                  "SMP over BR triggered by temporary bond has completed,"
+                  " resetting the LK flags");
+              p_dev_rec->sec_flags &= ~(BTM_SEC_LE_LINK_KEY_KNOWN);
+              p_dev_rec->ble.key_type = BTM_LE_KEY_NONE;
+            }
           }
 
           btm_sec_dev_rec_cback_event(p_dev_rec, res, true);
