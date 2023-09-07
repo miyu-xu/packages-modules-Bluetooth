988203233fbc95484a1603eb67232998e0a60610
William Escande <wescande@google.com>
2023-09-06 21:44:10 -0700

    Gatt: App exception aren't from SystemServer
    Calling into mService(IBluetoothGatt) will call the GattService from
    Bluetooth app that does not live in the systemServer.
    Therefore, exception should be rethrown as runtimeException
    
    Bug: 298264617
    Test: atest CtsBluetoothTestCases | Cts does not handled failure in the
          binder nor any of the Test framework we currently have
    Change-Id: Ie2ed38e5076242c1001e231f70105890c86ea116

--- framework/java/android/bluetooth/BluetoothGatt.java
+++ framework/java/android/bluetooth/BluetoothGatt.java
@@ -1498,7 +1498,7 @@ public final class BluetoothGatt implements BluetoothProfile {
             synchronized (mDeviceBusyLock) {
                 mDeviceBusy = false;
             }
-            throw e.rethrowFromSystemServer();
+            e.rethrowAsRuntimeException();
         }
 
         return requestStatus;
@@ -1641,7 +1641,7 @@ public final class BluetoothGatt implements BluetoothProfile {
             synchronized (mDeviceBusyLock) {
                 mDeviceBusy = false;
             }
-            e.rethrowFromSystemServer();
+            e.rethrowAsRuntimeException();
         }
         return BluetoothStatusCodes.ERROR_UNKNOWN;
     }
