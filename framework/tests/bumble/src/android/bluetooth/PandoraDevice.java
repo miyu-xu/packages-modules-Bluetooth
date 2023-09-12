23f18a7611db3414a3cf908f79df1847adf5baf4
Jack He <siyuanh@google.com>
2023-09-12 03:36:34 -0700

    Bumble: Add PairingTest Using Bumble
    Test: atest BumbleBluetoothTests
    Bug: 300052980
    Change-Id: Ia7ef28477f852f1f89eb1f9ab3369e9e4059546f

--- framework/tests/bumble/src/android/bluetooth/PandoraDevice.java
+++ framework/tests/bumble/src/android/bluetooth/PandoraDevice.java
@@ -18,6 +18,7 @@ package android.bluetooth;
 
 import android.util.Log;
 
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Empty;
 
 import io.grpc.ManagedChannel;
@@ -28,17 +29,20 @@ import org.junit.rules.ExternalResource;
 import java.util.concurrent.TimeUnit;
 
 import pandora.HostGrpc;
+import pandora.HostProto;
+import pandora.SecurityGrpc;
 
 public final class PandoraDevice extends ExternalResource {
     private static final String TAG = PandoraDevice.class.getSimpleName();
 
-    private final String mAddress;
+    private final String mNetworkAddress;
+    private ByteString mBrEdrAddress;
     private final int mPort;
 
     private ManagedChannel mChannel;
 
-    public PandoraDevice(String address, int port) {
-        mAddress = address;
+    public PandoraDevice(String networkAddress, int port) {
+        mNetworkAddress = networkAddress;
         mPort = port;
     }
 
@@ -52,7 +56,7 @@ public final class PandoraDevice extends ExternalResource {
         // FactoryReset is killing the server and restarting all channels created before the server
         // restarted that cannot be reused
         ManagedChannel channel =
-                OkHttpChannelBuilder.forAddress(mAddress, mPort).usePlaintext().build();
+                OkHttpChannelBuilder.forAddress(mNetworkAddress, mPort).usePlaintext().build();
 
         HostGrpc.HostBlockingStub stub = HostGrpc.newBlockingStub(channel);
         stub.factoryReset(Empty.getDefaultInstance());
@@ -64,10 +68,12 @@ public final class PandoraDevice extends ExternalResource {
             throw new RuntimeException(e);
         }
 
-        mChannel = OkHttpChannelBuilder.forAddress(mAddress, mPort).usePlaintext().build();
+        mChannel = OkHttpChannelBuilder.forAddress(mNetworkAddress, mPort).usePlaintext().build();
         stub = HostGrpc.newBlockingStub(mChannel);
 
-        stub.withWaitForReady().readLocalAddress(Empty.getDefaultInstance());
+        HostProto.ReadLocalAddressResponse readLocalAddressResponse =
+                stub.withWaitForReady().readLocalAddress(Empty.getDefaultInstance());
+        mBrEdrAddress = readLocalAddressResponse.getAddress();
     }
 
     @Override
@@ -82,8 +88,25 @@ public final class PandoraDevice extends ExternalResource {
         }
     }
 
+    /**
+     * @return device's BR/EDR address as a byte string
+     */
+    public ByteString getBrEdrAddress() {
+        return mBrEdrAddress;
+    }
+
     /** Get Pandora Host service */
     public HostGrpc.HostStub host() {
         return HostGrpc.newStub(mChannel);
+    }
+
+    /** Get blocking Pandora Host service */
+    public HostGrpc.HostBlockingStub hostBlocking() {
+        return HostGrpc.newBlockingStub(mChannel);
+    }
+
+    /** Get Pandora Security service */
+    public SecurityGrpc.SecurityStub security() {
+        return SecurityGrpc.newStub(mChannel);
     }
 }
