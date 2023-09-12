a291baa25239117141adaf2ff58d8a851162b062
Charanpreet Singh <quic_charsing@quicinc.com>
2023-09-12 11:47:28 -0700

    Encrypted Data Key Material Characteristic
    - Add Encrypted Data Key Material Characteristic
    - Add support for GATT client to read Encrypted Data Key Material
      characteristic value after bonding is completed.
    - Add support for GATT client to configure CCCD Descriptor
      for receiving indications.
    
    Test: None
    Bug: 301978669
    Bug: 308855997
    Change-Id: Id015aa7e0e23488e5552fbc6d879f789757bef6a

--- system/stack/btm/btm_ble_sec.cc
+++ system/stack/btm/btm_ble_sec.cc
@@ -57,6 +57,10 @@
 #include "stack/include/smp_api.h"
 #include "stack/include/smp_api_types.h"
 #include "types/raw_address.h"
+#include "stack/include/gap_api.h"
+#include "stack/gatt/gatt_int.h"
+#include "btif/include/btif_config.h"
+#include "android_bluetooth_flags.h"
 
 extern tBTM_CB btm_cb;
 
@@ -1342,6 +1346,22 @@ void btm_ble_link_encrypted(const RawAddress& bd_addr, uint8_t encr_enable) {
   } else {
     btm_ble_notify_enc_cmpl(p_dev_rec->ble.pseudo_addr, encr_enable);
   }
+
+  if (encr_enable && btm_sec_is_a_bonded_dev(p_dev_rec->ble.pseudo_addr) &&
+      IS_FLAG_ENABLED(encrypted_advertising_data)) {
+    size_t length = btif_storage_get_enc_key_material_length(&p_dev_rec->ble.pseudo_addr);
+
+    tGATT_TCB* p_tcb =
+      gatt_find_tcb_by_addr(p_dev_rec->ble.pseudo_addr, BT_TRANSPORT_LE);
+    /* Resume pending read of encrypted data key material*/
+    if (p_tcb && (p_tcb->is_read_enc_key_pending ||
+          (!p_tcb->is_read_enc_key_pending && (length > 0)))) {
+      LOG_DEBUG(" btm_ble_link_encrypted, read enc key values");
+      GAP_BleGetEncKeyMaterialInfo(p_dev_rec->ble.pseudo_addr);
+      p_tcb->is_read_enc_key_pending = false;
+    }
+  }
+
 }
 
 /*******************************************************************************
@@ -2011,4 +2031,31 @@ std::optional<tBLE_BD_ADDR> BTM_BleGetIdentityAddress(
   }
 
   return p_dev_rec->ble.identity_address_with_type;
+}
+
+/*******************************************************************************
+ *
+ * Function         BTM_BleGetEncKeyMaterial
+ *
+ * Description      This function is called to get the local device Encrypted
+ *                  Data Key Material characteristic value associated with
+ *                  GAP service.
+ *
+ * params           enc_key_value with size > 24bytes
+ * 
+ * Returns          void
+ *
+ ******************************************************************************/
+void BTM_BleGetEncKeyMaterial(uint8_t* enc_key_value) {
+  //Length of enc_key_value is always 24 bytesi(Key + IV).
+  //Since this is local device encrypted data key characteristic.
+
+  LOG_DEBUG("BTM_BleGetEncKeyMaterial");
+  size_t len = btif_storage_get_enc_key_material_length(NULL);
+  if (len > 0) {
+    if (btif_storage_get_enc_key_material(NULL, enc_key_value, &len) ==
+        BT_STATUS_SUCCESS) {
+      VLOG(1) << __func__ << " Found Adapter Enc Key Material value";
+    }
+  }
 }
