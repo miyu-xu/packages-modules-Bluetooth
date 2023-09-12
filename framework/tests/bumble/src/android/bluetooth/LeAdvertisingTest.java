d4f5ff34a5ca06025c5647931fcb52daa909c8b5
Jack He <siyuanh@google.com>
2023-09-12 03:36:34 -0700

    Bumble: Add PairingTest Using Bumble
    Test: atest BumbleBluetoothTests
    Bug: 300052980
    Change-Id: Ia7ef28477f852f1f89eb1f9ab3369e9e4059546f

--- framework/tests/bumble/src/android/bluetooth/LeAdvertisingTest.java
+++ framework/tests/bumble/src/android/bluetooth/LeAdvertisingTest.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package android.bluetooth;
 
 import static android.bluetooth.Utils.factoryResetAndCreateNewChannel;
+import static com.google.common.truth.Truth.assertThat;
 
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertisingSet;
@@ -11,10 +28,9 @@ import android.util.Log;
 
 import androidx.core.util.Pair;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
-import static com.google.common.truth.Truth.assertThat;
 import com.google.protobuf.Empty;
 
 import io.grpc.Context.CancellableContext;
@@ -22,14 +38,14 @@ import io.grpc.Deadline;
 import io.grpc.ManagedChannel;
 import io.grpc.stub.StreamObserver;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import pandora.HostGrpc;
 import pandora.HostProto.ScanRequest;
