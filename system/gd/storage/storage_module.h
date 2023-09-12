0c3beb7f4929a623e943124816f39a34306a12a6
Charanpreet Singh <quic_charsing@quicinc.com>
2023-09-12 11:44:04 -0700

    Scanning Decryption (Denver)
    - Scanner support for decryption of Advertising data.
    - Scanner support for decryption of Scan Response data.
    - Scanner support for decryption of Periodic adv data.
    
    Test: None
    Bug: 301978669
    Bug: 306223534
    Change-Id: Ide5893ddfb6a8c58ac86c5afd7255c81bc27f191

--- system/gd/storage/storage_module.h
+++ system/gd/storage/storage_module.h
@@ -29,6 +29,7 @@
 #include "storage/config_cache.h"
 #include "storage/device.h"
 #include "storage/mutation.h"
+#include "btif/include/btif_storage.h"
 
 namespace bluetooth {
 
@@ -42,6 +43,10 @@ class SecurityManagerImpl;
 
 namespace hci {
 class AclManager;
+class LeAdvertisingManager;
+class LeScanningManager;
+class LeScanningReassembler;
+class PeriodicSyncManager;
 }
 
 namespace storage {
@@ -122,6 +127,10 @@ class StorageModule : public bluetooth::Module {
   friend shim::BtifConfigInterface;
   friend hci::AclManager;
   friend security::internal::SecurityManagerImpl;
+  friend hci::LeAdvertisingManager;
+  friend hci::LeScanningManager;
+  friend hci::LeScanningReassembler;
+  friend hci::PeriodicSyncManager;
   // For unit test only
   ConfigCache* GetMemoryOnlyConfigCache();
   // Normally, underlying config will be saved at most 3 seconds after the first config change in a series of changes
