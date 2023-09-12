092d4698d30bb33228dc4838e9c8b5b351b757d2
Charanpreet Singh <quic_charsing@quicinc.com>
2023-09-12 11:47:28 -0700

    Encrypted Data Key Material Characteristic
    - Add Encrypted Data Key Material Characteristic
    - Add support for GATT client to read Encrypted Data Key Material
      characteristic value after bonding is completed.
    - Add support for GATT client to configure CCCD Descriptor
      for receiving indications.
    
    Test: None
    Bug: 301978669
    Bug: 308855997
    Change-Id: Id015aa7e0e23488e5552fbc6d879f789757bef6a

--- system/main/shim/le_advertising_manager.h
+++ system/main/shim/le_advertising_manager.h
@@ -25,6 +25,10 @@ namespace bluetooth {
 namespace shim {
 
 BleAdvertiserInterface* get_ble_advertiser_instance();
+void enc_key_cb();
+namespace legacy {
+void GAP_DB_Callback(std::vector<uint8_t> temp, uint8_t attr_uuid);
+}
 void init_advertising_manager();
 
 }  // namespace shim
