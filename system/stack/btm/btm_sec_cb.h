18ec646511278ea2c2357c13d4ae73d2588bc152
Charanpreet Singh <quic_charsing@quicinc.com>
2023-09-12 11:05:18 -0700

    Encrypted Advertisement(Denver)
    - Includes Support for Encrypted Advertising of
      Extended Advertising Data, Scan Response Data, and
      Periodic Advertising Data
    - Pause Advertising when RPA changes and
      Generate new Randomizer for Encrypted Advertising each time
    - Includes BoringSSL CCM algorithm for encryption
    
    Test: None
    Bug: 301978669
    Bug: 308855997
    Change-Id: Ia55fd592774aa15079badc32f61b2514d1ce7f3b

--- system/stack/btm/btm_sec_cb.h
+++ system/stack/btm/btm_sec_cb.h
@@ -59,6 +59,9 @@ class tBTM_SEC_CB {
   bool pin_type_changed{false};      /* pin type changed during bonding */
   bool sec_req_pending{false};       /*   true if a request is pending */
 
+  bool enc_adv_data_enabled{true};
+  bool enc_adv_data_log_enabled{false};
+
   uint8_t pin_code_len{0}; /* for legacy devices */
   PIN_CODE pin_code;       /* for legacy devices */
   tBTM_PAIRING_STATE pairing_state{
