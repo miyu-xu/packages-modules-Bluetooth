fdb8182a46265bf4e77f254edfe27a6e93a100f0
Jakub Tyszkowski <tyszkowski@google.com>
2023-09-13 09:04:41 +0000

    LeAudio: Rename codec specific structure
    To not confuse CodecCapabilitySettings with the PAC service
    codec cpabilities, and since this struct is being used to pass
    the codec specific configuration (in codec manager and audio
    set configuration provider) it was renamed to CodecSpecificParameters.
    
    In future this should wrap not only tha base configuration for the
    already known codec specific parameter types, but also the vendor
    specific ones, thus will migrate from LeAudioCodecConfigBase to more
    generic LTV map.
    
    Bug: 295972694
    Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test
    Change-Id: I89faef9f2facb7a498f972ffe1562ffd87a09d5f

--- system/bta/le_audio/devices.h
+++ system/bta/le_audio/devices.h
@@ -172,8 +172,8 @@ class LeAudioDevice {
   bool HasCisId(uint8_t id);
   uint8_t GetMatchingBidirectionCisId(const struct types::ase* base_ase);
   const struct types::acs_ac_record* GetCodecConfigurationSupportedPac(
-      uint8_t direction, const set_configurations::CodecCapabilitySetting&
-                             codec_capability_setting);
+      uint8_t direction,
+      const set_configurations::CodecConfigSetting& codec_capability_setting);
   uint8_t GetLc3SupportedChannelCount(uint8_t direction);
   uint8_t GetPhyBitmask(void);
   bool ConfigureAses(
