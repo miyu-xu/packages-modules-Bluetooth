8210320bc7f4b3ca1fad75fcd0601f18e29f389a
Sarvesh Kalwit <sarveshkalwit@google.com>
2023-09-15 00:24:58 +0000

    [WIP][Floss] First attempt, works, move on
    Change-Id: I1d848403264379172784e9fe15e1885c7c6c4b6b

--- system/gd/rust/linux/client/src/main.rs
+++ system/gd/rust/linux/client/src/main.rs
@@ -14,12 +14,12 @@ use tokio::time::timeout;
 use crate::bt_adv::AdvSet;
 use crate::bt_gatt::GattClientContext;
 use crate::callbacks::{
-    AdminCallback, AdvertisingSetCallback, BtCallback, BtConnectionCallback, BtManagerCallback,
+    AdminCallback, AdvertisingSetCallback, BatteryManagerCallback, BtCallback, BtConnectionCallback, BtManagerCallback,
     BtSocketManagerCallback, MediaCallback, QACallback, ScannerCallback, SuspendCallback,
 };
 use crate::command_handler::{CommandHandler, SocketSchedule};
 use crate::dbus_iface::{
-    BluetoothAdminDBus, BluetoothDBus, BluetoothGattDBus, BluetoothManagerDBus, BluetoothMediaDBus,
+    BatteryManagerDBus, BluetoothAdminDBus, BluetoothDBus, BluetoothGattDBus, BluetoothManagerDBus, BluetoothMediaDBus,
     BluetoothQADBus, BluetoothQALegacyDBus, BluetoothSocketManagerDBus, BluetoothTelephonyDBus,
     SuspendDBus,
 };
@@ -101,6 +101,9 @@ pub(crate) struct ClientContext {
     /// Proxy for Media interface.
     pub(crate) media_dbus: Option<BluetoothMediaDBus>,
 
+    /// Proxy for battery manager interface.
+    pub(crate) battery_manager_dbus: Option<BatteryManagerDBus>,
+
     /// Channel to send actions to take in the foreground
     fg: mpsc::Sender<ForegroundActions>,
 
@@ -145,6 +148,9 @@ pub(crate) struct ClientContext {
 
     /// The set of client commands that need to wait for callbacks.
     client_commands_with_callbacks: Vec<String>,
+    
+    /// A set of addresses whose battery changes are being tracked.
+    pub(crate) battery_address_filter: HashSet<String>,
 }
 
 impl ClientContext {
@@ -179,6 +185,7 @@ impl ClientContext {
             socket_manager_dbus: None,
             telephony_dbus: None,
             media_dbus: None,
+            battery_manager_dbus: None,
             fg: tx,
             dbus_connection,
             dbus_crossroads,
@@ -194,6 +201,7 @@ impl ClientContext {
             socket_test_schedule: None,
             mps_sdp_handle: None,
             client_commands_with_callbacks,
+            battery_address_filter: HashSet::new(),
         }
     }
 
@@ -241,6 +249,8 @@ impl ClientContext {
         self.telephony_dbus = Some(BluetoothTelephonyDBus::new(conn.clone(), idx));
 
         self.media_dbus = Some(BluetoothMediaDBus::new(conn.clone(), idx));
+
+        self.battery_manager_dbus = Some(BatteryManagerDBus::new(conn.clone(), idx));
 
         // Trigger callback registration in the foreground
         let fg = self.fg.clone();
@@ -540,6 +550,8 @@ async fn handle_client_command(
                     format!("/org/chromium/bluetooth/client/{}/qa_manager_callback", adapter);
                 let media_cb_objpath: String =
                     format!("/org/chromium/bluetooth/client/{}/bluetooth_media_callback", adapter);
+                let battery_cb_objpath: String =
+                    format!("/org/chromium/bluetooth/client/{}/battery_manager_callback", adapter);
 
                 let dbus_connection = context.lock().unwrap().dbus_connection.clone();
                 let dbus_crossroads = context.lock().unwrap().dbus_crossroads.clone();
@@ -686,6 +698,22 @@ async fn handle_client_command(
                     )))
                     .await
                     .expect("D-Bus error on IBluetoothMedia::RegisterCallback");
+
+                context
+                    .lock()
+                    .unwrap()
+                    .battery_manager_dbus
+                    .as_mut()
+                    .unwrap()
+                    .rpc
+                    .register_battery_callback(Box::new(BatteryManagerCallback::new(
+                        battery_cb_objpath,
+                        context.clone(),
+                        dbus_connection.clone(),
+                        dbus_crossroads.clone(),
+                    )))
+                    .await
+                    .expect("D-Bus error on IBatteryManagerDBus::RegisterBatteryCallback");
 
                 context.lock().unwrap().adapter_ready = true;
                 let adapter_address = context.lock().unwrap().update_adapter_address();
