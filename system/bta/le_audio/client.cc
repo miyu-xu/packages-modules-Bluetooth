039e01acf09034f4871be20f95666d425d09f60c
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2023-09-19 12:45:01 +0000

    le_audio: Avoid activation unicast and broadcast at the same time
    A pending state is introducted to proper maintain switching of LE Audio
    devices e.g. Unicast <-> Broadcast. While one type device is activating,
    second should be aware of such process. Without knowledge if
    device/group is activating it may happen that service will call stack to
    activate broadcast and unicast at the same time which is not supported
    by now, especially by audio framework side.
    
    Tag: #feature
    Bug: 299238288
    Test: atest bluetooth_le_audio_test
    Change-Id: I8c7ea4d0dc3038e6e204e3fa1286fc2c6e3f74b9

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -1123,6 +1123,7 @@ class LeAudioClientImpl : public LeAudioClient {
 
     if (group_id == bluetooth::groups::kGroupUnknown) {
       if (active_group_id_ == bluetooth::groups::kGroupUnknown) {
+        callbacks_->OnGroupStatus(group_id, GroupStatus::INACTIVE);
         /* Nothing to do */
         return;
       }
@@ -1145,6 +1146,7 @@ class LeAudioClientImpl : public LeAudioClient {
     if (!group) {
       LOG(ERROR) << __func__
                  << ", Invalid group: " << static_cast<int>(group_id);
+      callbacks_->OnGroupStatus(group_id, GroupStatus::INACTIVE);
       return;
     }
 
@@ -1163,6 +1165,7 @@ class LeAudioClientImpl : public LeAudioClient {
           LeAudioSourceAudioHalClient::AcquireUnicast();
       if (!le_audio_source_hal_client_) {
         LOG(ERROR) << __func__ << ", could not acquire audio source interface";
+        callbacks_->OnGroupStatus(group_id, GroupStatus::INACTIVE);
         return;
       }
     }
@@ -1171,6 +1174,7 @@ class LeAudioClientImpl : public LeAudioClient {
       le_audio_sink_hal_client_ = LeAudioSinkAudioHalClient::AcquireUnicast();
       if (!le_audio_sink_hal_client_) {
         LOG(ERROR) << __func__ << ", could not acquire audio sink interface";
+        callbacks_->OnGroupStatus(group_id, GroupStatus::INACTIVE);
         return;
       }
     }
@@ -1199,6 +1203,7 @@ class LeAudioClientImpl : public LeAudioClient {
     if (current_source_codec_config.IsInvalid() &&
         current_sink_codec_config.IsInvalid()) {
       LOG_ERROR("Unsupported device configurations");
+      callbacks_->OnGroupStatus(group_id, GroupStatus::INACTIVE);
       return;
     }
 
