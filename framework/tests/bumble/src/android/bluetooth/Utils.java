431014486d48afe46b41d87e28cabf5d52172fbf
Jack He <siyuanh@google.com>
2023-09-18 18:25:37 -0700

    BumbleBluetoothTests: Improve test infra
    * Switch to AndroidX Junit 4 runners
    * Add RootTargetPreparer to ensure we are root
    * Add FilePullerLogCollector to pull snoop log
    * Add snoop log commands to enable btsnoop logging and debug level
      logging
    * Add commands to disable BLE only mode
    * Add Bumble configuration to set IO cap as no input no output
    
    Test: atest BumbleBluetoothTests
    Bug: 300052980
    Change-Id: Ia9734084413d27a5f9a975da18350f0eb915e272

--- framework/tests/bumble/src/android/bluetooth/Utils.java
+++ framework/tests/bumble/src/android/bluetooth/Utils.java
@@ -17,12 +17,23 @@
 package android.bluetooth;
 
 import static com.google.common.io.BaseEncoding.base16;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
 import com.google.protobuf.ByteString;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public final class Utils {
+    private static final String TAG = Utils.class.getSimpleName();
+
     public static final String BUMBLE_RANDOM_ADDRESS = "51:F7:A8:75:AC:5E";
 
     public static String addressStringFromByteString(ByteString bs) {
@@ -42,5 +53,68 @@ public final class Utils {
      */
     public static byte[] addressBytesFromString(String address) {
         return base16().upperCase().withSeparator(":", 2).decode(address.toUpperCase(Locale.US));
+    }
+
+    /**
+     * Wait and verify that an item has been received.
+     *
+     * @param timeoutMs the time (in milliseconds) to wait for the item
+     * @param queue the queue for the item
+     * @return the received intent
+     */
+    public static <T> T waitForItem(int timeoutMs, BlockingQueue<T> queue) {
+        try {
+            return queue.poll(timeoutMs, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Cannot obtain an item from the queue: " + e.getMessage());
+        }
+        return null;
+    }
+
+    /** Device based broadcast receiver */
+    public static class DeviceBasedBroadcastReceiver extends BroadcastReceiver {
+        private final HashMap<BluetoothDevice, LinkedBlockingQueue<Intent>> mDeviceQueueMap =
+                new HashMap<>();
+        private final LinkedBlockingQueue<Intent> mDefaultQueue = new LinkedBlockingQueue<>();
+
+        /**
+         * Add a device into the tracker
+         *
+         * @param device to be added
+         */
+        public void addDevice(BluetoothDevice device) {
+            mDeviceQueueMap.put(device, new LinkedBlockingQueue<>());
+        }
+
+        /**
+         * Get the blocking queue for the device
+         *
+         * @param device device must be added before
+         * @return null if device wasn't added earlier, the blocking queue if device was added
+         */
+        public LinkedBlockingQueue<Intent> getQueue(BluetoothDevice device) {
+            return mDeviceQueueMap.get(device);
+        }
+
+        /**
+         * Get the default queue when no EXTRA_DEVICE is included
+         *
+         * @return the default queue
+         */
+        public LinkedBlockingQueue<Intent> getDefaultQueue() {
+            return mDefaultQueue;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BluetoothDevice device =
+                    intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice.class);
+            if (device == null) {
+                assertThat(mDefaultQueue.add(intent)).isTrue();
+            } else if (mDeviceQueueMap.containsKey(device)) {
+                LinkedBlockingQueue<Intent> queue = mDeviceQueueMap.get(device);
+                assertThat(queue.add(intent)).isTrue();
+            }
+        }
     }
 }
