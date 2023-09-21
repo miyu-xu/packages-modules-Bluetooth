c03d5c93f5dcfc8624659344312565d6e88ae23d
shihchienc <shihchienc@google.com>
2023-09-21 10:50:13 +0000

    [LE Audio] Handle later join case on offload
    bluetooth.leaudio.dual_bidirection_swb.supported set to false is to
    disable on both software and offload path in the system perspective.
    
    When it turns to true, it means system supports this feature (and
    software is default to be supported based on current design).
    
    However offload path might not support due to limitation. Under this
    condition, going through the offload path, for dual device it is not
    swb but when one device is disconnected, it is swb, which makes user have
    bad user experience. This patch fixed this.
    
    Bug: 301358026
    Test: manual
    Test: atest -c --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_le_audio_codec_manager_test  --no-bazel-mode
    Change-Id: I4b46d504963e69190d02ce0e4201ecad4fce35a0

--- system/bta/le_audio/le_audio_set_configuration_provider_json.cc
+++ system/bta/le_audio/le_audio_set_configuration_provider_json.cc
@@ -85,7 +85,7 @@ struct AudioSetConfigurationProviderJson {
 
   AudioSetConfigurationProviderJson(types::CodecLocation location) {
     dual_bidirection_swb_supported_ = osi_property_get_bool(
-        "bluetooth.leaudio.dual_bidirection_swb.supported", true);
+        "bluetooth.leaudio.dual_bidirection_swb.supported", false);
     ASSERT_LOG(LoadContent(kLeAudioSetConfigs, kLeAudioSetScenarios, location),
                ": Unable to load le audio set configuration files.");
   }
@@ -764,6 +764,27 @@ bool AudioSetConfigurationProvider::CheckConfigurationIsBiDirSwb(
     }
   }
   return dir == le_audio::types::kLeAudioDirectionBoth;
+}
+
+bool AudioSetConfigurationProvider::CheckConfigurationIsDualBiDirSwb(
+    const set_configurations::AudioSetConfiguration& set_configuration) const {
+  bool single_dev_dual_bidir_swb = false;
+  bool dual_dev_dual_bidir_swb = false;
+
+  for (const auto& conf : set_configuration.confs) {
+    if (conf.codec.GetConfigSamplingFrequency() <
+        le_audio::LeAudioCodecConfiguration::kSampleRate32000) {
+      return false;
+    }
+    if (conf.device_cnt == 1 && conf.ase_cnt == 2) {
+      single_dev_dual_bidir_swb |= conf.direction;
+    } else if (conf.device_cnt == 2 && conf.ase_cnt == 2) {
+      dual_dev_dual_bidir_swb |= conf.direction;
+    }
+  }
+
+  return single_dev_dual_bidir_swb == le_audio::types::kLeAudioDirectionBoth ||
+         dual_dev_dual_bidir_swb == le_audio::types::kLeAudioDirectionBoth;
 }
 
 }  // namespace le_audio
