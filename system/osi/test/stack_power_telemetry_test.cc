84178ad8b000b3275c789d888adf27ec33d870b0
Eric Shih <shiheric@google.com>
2023-09-25 04:29:52 +0000

    Power Telemetry Stage 2
    1. SCO on (off already has logs)
    2. HCI cmd/evt
    3. BLE Scan
    
    Bug: 237393639
    Test: atest net_test_osi
    Change-Id: If05bd9028a17cbe1f530de3384e6abf6c514f008

--- system/osi/test/stack_power_telemetry_test.cc
+++ system/osi/test/stack_power_telemetry_test.cc
@@ -21,8 +21,6 @@ class PowerTelemetryTest : public ::testing::Test {
   }
 
   void SetUp() override {
-    ::testing::Test::SetUp();
-
     // Enable the feature flag
     power_telemerty_enabled_ = true;
     RawAddress::FromString("00:00:00:00:00:00", bdaddr);
@@ -59,6 +57,13 @@ TEST_F(PowerTelemetryTest, test_LogInqScanDetails) {
 
   power_telemetry::GetInstance().LogInqScanStarted();
   ASSERT_EQ(1, power_telemetry::GetInstance().pimpl_->inq_scan.count_);
+}
+
+TEST_F(PowerTelemetryTest, test_LogBleScan) {
+  reset();
+
+  power_telemetry::GetInstance().LogBleScan(10);
+  ASSERT_EQ(10, (int)power_telemetry::GetInstance().pimpl_->ble_scan.count_);
 }
 
 TEST_F(PowerTelemetryTest, test_LogBleAdvDetails) {
@@ -341,6 +346,9 @@ TEST_F(PowerTelemetryTest, test_feature_flag) {
   dummy_res.tx_power = 100;
   power_telemetry::GetInstance().LogTxPower(p);
   ASSERT_EQ(0, ldc.acl.link_details_map[handle].tx_power_level);
+
+  power_telemetry::GetInstance().LogBleScan(10);
+  ASSERT_EQ(0, (int)power_telemetry::GetInstance().pimpl_->ble_scan.count_);
 
   power_telemetry::GetInstance().LogBleAdvStarted();
   ASSERT_EQ(0, (int)ldc.adv_list.size());
