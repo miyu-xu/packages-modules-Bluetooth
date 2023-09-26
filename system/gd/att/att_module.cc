7cdd6a54d97c3e62725a68e0be028feda28d9eea
Chris Manton <cmanton@google.com>
2023-09-25 19:19:48 -0700

    IA2: Split out libbt-gd-module
    Bug: 302049854
    Test: m .
    Change-Id: Ibba7c9380b711dfd04ab7e0d9b8f0f7d129e5e34

--- system/gd/att/att_module.cc
+++ system/gd/att/att_module.cc
@@ -33,12 +33,12 @@ const ModuleFactory AttModule::Factory = ModuleFactory([]() { return new AttModu
 
 namespace {
 void OnAttRegistrationCompleteLe(
-    l2cap::le::FixedChannelManager::RegistrationResult result,
-    std::unique_ptr<l2cap::le::FixedChannelService> le_smp_service) {
+    l2cap::le::FixedChannelManager::RegistrationResult,
+    std::unique_ptr<l2cap::le::FixedChannelService>) {
   LOG_INFO("ATT channel registration complete");
 }
 
-void OnAttConnectionOpenLe(std::unique_ptr<l2cap::le::FixedChannel> channel) {
+void OnAttConnectionOpenLe(std::unique_ptr<l2cap::le::FixedChannel>) {
   LOG_INFO("ATT conneciton opened");
 }
 }  // namespace
