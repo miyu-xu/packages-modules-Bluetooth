e358fe2cece2999a7cf7df11a7b3ba385c661766
William Escande <wescande@google.com>
2023-09-28 15:32:56 -0700

    Enable 5.4 PTS test: AD Type – Encrypted Data
    Test: atest pts-bot:GAP/ADV/BV-20-C -v
    Bug: 301978669
    Change-Id: Ieb62b9f8c59f499a4d2e036f9f5cd6c427544d27

--- android/pandora/mmi2grpc/mmi2grpc/gap.py
+++ android/pandora/mmi2grpc/mmi2grpc/gap.py
@@ -1161,6 +1161,51 @@ class GAPProxy(ProfileProxy):
 
         return "OK"
 
+    @match_description
+    def _mmi_2004(self, digit: str, **kwargs):
+        """
+        Please confirm that 6 digit number is matched with (?P<digit>[0-9]+).
+        """
+
+        # TODO
+
+        return "OK"
+
+    @assert_description
+    def _mmi_503(self, **kwargs):
+        """
+        Please authorize that PTS to read Encrypted Data Key value.
+        """
+
+        return "OK"
+
+    @assert_description
+    def _mmi_500(self, **kwargs):
+        """
+        Please prepare IUT to send an advertising report using Encrypted
+        Advertising Data with payload 1.
+        """
+
+        self.advertise = self.host.Advertise(legacy=True,
+                                             own_address_type=PUBLIC,
+                                             data=DataTypes(encrypted_data=DataTypes(include_tx_power_level=True)))
+
+        return "OK"
+
+    @assert_description
+    def _mmi_501(self, **kwargs):
+        """
+        Please prepare IUT to send an advertising report using Encrypted
+        Advertising Data with payload 2.
+        """
+
+        self.advertise = self.host.Advertise(
+            legacy=True,
+            own_address_type=PUBLIC,
+            data=DataTypes(encrypted_data=DataTypes(complete_service_class_uuids16="1234")))
+
+        return "OK"
+
     def _auto_confirm_requests(self, times=None):
 
         def task():
