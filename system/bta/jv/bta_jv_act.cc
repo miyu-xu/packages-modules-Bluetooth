960286122d6a6521506a58c56bf0857f3e856649
Pomai Ahlo <poahlo@google.com>
2023-09-28 18:48:38 -0700

    Remove scn[] from java control block
    We currently keep track of scn usage in both BTM and BTA.  Remove the
    scn array from bta to prepare for the btm counterpart to move to bta.
    
    Bug: 299158235
    Test: m Bluetooth
    Change-Id: Ic209c62da9137dfe103565017fa49eab158a12d4

--- system/bta/jv/bta_jv_act.cc
+++ system/bta/jv/bta_jv_act.cc
@@ -717,22 +717,13 @@ void bta_jv_get_channel_id(
   switch (type) {
     case BTA_JV_CONN_TYPE_RFCOMM: {
       uint8_t scn = 0;
-      if (channel > 0) {
-        if (!BTM_TryAllocateSCN(channel)) {
-          LOG(ERROR) << "rfc channel=" << channel
-                     << " already in use or invalid";
-          channel = 0;
-        }
-      } else {
-        channel = BTM_AllocateSCN();
-        if (channel == 0) {
+      if (channel > 0 && !BTM_TryAllocateSCN(channel)) {
+        LOG(ERROR) << "rfc channel=" << channel << " already in use or invalid";
+      } else if (channel <= 0) {
+        scn = BTM_AllocateSCN();
+        if (scn == 0) {
           LOG(ERROR) << "run out of rfc channels";
-          channel = 0;
         }
-      }
-      if (channel != 0) {
-        bta_jv_cb.scn[channel - 1] = true;
-        scn = (uint8_t)channel;
       }
       if (bta_jv_cb.p_dm_cback) {
         tBTA_JV bta_jv;
@@ -769,14 +760,9 @@ void bta_jv_get_channel_id(
 void bta_jv_free_scn(int32_t type /* One of BTA_JV_CONN_TYPE_ */,
                      uint16_t scn) {
   switch (type) {
-    case BTA_JV_CONN_TYPE_RFCOMM: {
-      if (scn > 0 && scn <= RFCOMM_MAX_SCN && bta_jv_cb.scn[scn - 1]) {
-        /* this scn is used by JV */
-        bta_jv_cb.scn[scn - 1] = false;
-        BTM_FreeSCN(scn);
-      }
+    case BTA_JV_CONN_TYPE_RFCOMM:
+      BTM_FreeSCN(scn);
       break;
-    }
     case BTA_JV_CONN_TYPE_L2CAP:
       bta_jv_set_free_psm(scn);
       break;
