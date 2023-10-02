55d2325f823d6b0b398d49f63a227088ee7227b4
Alexey Patosin <apatosin@google.com>
2023-10-02 10:29:13 +0000

    Disable system HfpClientConnectionService for wearables.
    Bug: 300026493
    Test: atest BluetoothInstrumentationTests
    Change-Id: Iee82a38db62783f88a74252dca49da22e9b0a307

--- android/app/src/com/android/bluetooth/hfpclient/HeadsetClientService.java
+++ android/app/src/com/android/bluetooth/hfpclient/HeadsetClientService.java
@@ -16,6 +16,8 @@
 
 package com.android.bluetooth.hfpclient;
 
+import static android.content.pm.PackageManager.FEATURE_WATCH;
+
 import android.annotation.RequiresPermission;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadsetClient;
@@ -134,9 +136,12 @@ public class HeadsetClientService extends ProfileService {
             registerReceiver(mBroadcastReceiver, filter);
 
             // Start the HfpClientConnectionService to create connection with telecom when HFP
-            // connection is available.
-            Intent startIntent = new Intent(this, HfpClientConnectionService.class);
-            startService(startIntent);
+            // connection is available on non-wearable device.
+            if (getPackageManager() != null
+                    && !getPackageManager().hasSystemFeature(FEATURE_WATCH)) {
+                Intent startIntent = new Intent(this, HfpClientConnectionService.class);
+                startService(startIntent);
+            }
 
             // Create the thread on which all State Machines will run
             mSmThread = new HandlerThread("HeadsetClient.SM");
@@ -156,9 +161,12 @@ public class HeadsetClientService extends ProfileService {
                     return false;
                 }
 
-                // Stop the HfpClientConnectionService.
-                Intent stopIntent = new Intent(this, HfpClientConnectionService.class);
-                sHeadsetClientService.stopService(stopIntent);
+                // Stop the HfpClientConnectionService for non-wearables devices.
+                if (getPackageManager() != null
+                        && !getPackageManager().hasSystemFeature(FEATURE_WATCH)) {
+                    Intent stopIntent = new Intent(this, HfpClientConnectionService.class);
+                    sHeadsetClientService.stopService(stopIntent);
+                }
             }
 
             setHeadsetClientService(null);
