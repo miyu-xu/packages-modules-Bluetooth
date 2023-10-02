97ac5846d45284454f12983ee87312e5e1bf4c12
Charlie Boutier <charliebout@google.com>
2023-10-02 15:53:37 +0000

    BumbleBluetoothTest: Add Dck Gatt Discover tests
    Add a test to verify the DK gatt implementation on Bumble and
    be sure it is discoverable from the Android side
    
    Test: atest BumbleBluetoothTest
    Bug: 294274195
    Change-Id: Icd05237fa77e38e9d6dc1de4d3e10fa270796c2e

---
+++ framework/tests/bumble/src/bumble_server.py
@@ -0,0 +1,12 @@
+from apps import pandora_server
+from bumble import pandora as bumble_server
+from bumble_experimental.dck import DckService
+from pandora_experimental.dck_grpc_aio import add_DckServicer_to_server
+import logging
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    bumble_server.register_servicer_hook(
+        lambda bumble, _, server: add_DckServicer_to_server(DckService(bumble.device), server)
+    )
+    pandora_server.main()  # pylint: disable=no-value-for-parameter
