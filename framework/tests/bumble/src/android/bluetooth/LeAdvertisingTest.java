b175e64f92c87bdfb95733271bdf293776839a08
Charlie Boutier <charliebout@google.com>
2023-10-02 15:53:37 +0000

    BumbleBluetoothTest: Add Dck Gatt Discover tests
    Add a test to verify the DK gatt implementation on Bumbe and
    be sure it is discoverable from the Android side
    
    Test: atest BumbleBluetoothTest
    Bug: 294274195
    Change-Id: Icd05237fa77e38e9d6dc1de4d3e10fa270796c2e

--- framework/tests/bumble/src/android/bluetooth/LeAdvertisingTest.java
+++ framework/tests/bumble/src/android/bluetooth/LeAdvertisingTest.java
@@ -27,8 +27,8 @@ import android.util.Log;
 
 import androidx.core.util.Pair;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import io.grpc.Deadline;
 
