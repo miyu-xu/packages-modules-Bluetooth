74a45a6f7d700e7cb6876e5015ab13a5f5549b26
Hui Peng <phui@google.com>
2023-10-02 18:29:43 -0700

    Test: Add sources in subdirectories of test to avatar
    - include all python sources (including sub direcotries)
      in android/pandora/test in avatar module
    - Prepare pairing_tests directory to host all pairing tests
    - Add utility functions to scan tests automatically
    
    Bug: 277282212
    Test: atest avatar
    Change-Id: Idf6e9de9aff36b210d3b83044ac66df79f966435

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -23,6 +23,7 @@ import gatt_test
 import hfpclient_test
 import sdp_test
 import smp_test
+import pairing_tests
 
 _TEST_CLASSES_LIST = [
     avatar.cases.host_test.HostTest,
@@ -36,6 +37,9 @@ _TEST_CLASSES_LIST = [
     hfpclient_test.HfpClientTest,
 ]
 
+
+def get_complete_test_class_list():
+    return _TEST_CLASSES_LIST + pairing_tests.get_test_class_list()
 
 def _parse_cli_args() -> Tuple[Namespace, List[str]]:
     parser = argparse.ArgumentParser(description='Avatar test runner.')
@@ -57,4 +61,4 @@ if __name__ == "__main__":
         os.environ.setdefault('BUMBLE_SNOOPER', f'btsnoop:file:{ns.log_path}/{_BUMBLE_BTSNOOP_FMT}')
 
     # Run the test suite.
-    suite_runner.run_suite(_TEST_CLASSES_LIST, argv)  # type: ignore
+    suite_runner.run_suite(get_complete_test_class_list(), argv)  # type: ignore
