6b998ab6242b493c49391b80ff835cd7da47036d
Eric Shih <shiheric@google.com>
2023-10-03 03:53:58 +0000

    Add SDP related functions over LC3 for HFPC
    - Add LC3 supported features in HFP 1.9
    
    Bug: 278469604
    Test: atest BtifHfClientTest
    Change-Id: Ieb94cdeff6a3d85e22f4f6b2687172690fea5c24

--- system/bta/hf_client/bta_hf_client_sdp.cc
+++ system/bta/hf_client/bta_hf_client_sdp.cc
@@ -30,6 +30,7 @@
 #include "bta/include/bta_ag_api.h"
 #include "bta/include/bta_hf_client_api.h"
 #include "bta/sys/bta_sys.h"
+#include "os/log.h"
 #include "osi/include/allocator.h"
 #include "osi/include/osi.h"  // UNUSED_ATTR
 #include "osi/include/properties.h"
@@ -108,6 +109,7 @@ bool bta_hf_client_add_record(const char* p_service_name, uint8_t scn,
   uint16_t sdp_features = 0;
 
   APPL_TRACE_DEBUG("bta_hf_client_add_record");
+  LOG_INFO("features: %d", features);
 
   memset(proto_elem_list, 0,
          BTA_HF_CLIENT_NUM_PROTO_ELEMS * sizeof(tSDP_PROTOCOL_ELEM));
@@ -156,7 +158,12 @@ bool bta_hf_client_add_record(const char* p_service_name, uint8_t scn,
   if (features & BTA_HF_CLIENT_FEAT_VOL) sdp_features |= BTA_HF_CLIENT_FEAT_VOL;
 
   /* Codec bit position is different in SDP (bit 5) and in BRSF (bit 7) */
-  if (features & BTA_HF_CLIENT_FEAT_CODEC) sdp_features |= 0x0020;
+  if (features & BTA_HF_CLIENT_FEAT_CODEC)
+    sdp_features |= BTA_HF_CLIENT_WBS_SUPPORT;
+
+  /* Support swb */
+  if (features & BTA_HF_CLIENT_FEAT_SWB)
+    features |= BTA_HF_CLIENT_FEAT_SWB_SUPPORT;
 
   UINT16_TO_BE_FIELD(buf, sdp_features);
   result &= get_legacy_stack_sdp_api()->handle.SDP_AddAttribute(
