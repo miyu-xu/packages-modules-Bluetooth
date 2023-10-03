e4c2df676fd1ca152294004162f28fd7fe979bca
Charlie Boutier <charliebout@google.com>
2023-10-03 22:21:42 +0000

    BumbleBluetoothTests: Enhance gRPC callbacks with Iterator
    Created a custom class that implements Iterator and
    StreamObserver to improve the management of gRPC callbacks.
    
    Test: atest BumbleBluetoothTests
    Bug: 303302417
    Change-Id: I3e48c7ca7b51c086f8aa1a4023cd63dd23a799e3

---
+++ framework/tests/bumble/src/android/bluetooth/StreamObserverIterator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth;
+
+import io.grpc.stub.StreamObserver;
+
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class StreamObserverIterator<T> implements Iterator<T>, StreamObserver<T> {
+    private BlockingQueue<T> mQueue = new LinkedBlockingQueue<>();
+    private Throwable mError;
+    private boolean mCompleted = false;
+
+    @Override
+    public void onNext(T value) {
+        mQueue.add(value);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        mError = t;
+    }
+
+    @Override
+    public void onCompleted() {
+        mCompleted = true;
+    }
+
+    @Override
+    public boolean hasNext() throws RuntimeException {
+        if (mError != null) {
+            throw new RuntimeException(mError);
+        }
+        return !mCompleted | !mQueue.isEmpty();
+    }
+
+    @Override
+    public T next() throws RuntimeException {
+        if (mError != null) {
+            throw new RuntimeException(mError);
+        }
+        try {
+            return mQueue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
