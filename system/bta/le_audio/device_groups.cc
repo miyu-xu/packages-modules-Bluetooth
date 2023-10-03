cc8a145dbc0a4a334a39cb976a18f5543a8fcd1b
Jakub Tyszkowski <tyszkowski@google.com>
2023-10-03 13:54:32 +0000

    [WIP] Move configuration provider logic to Codec Manager
    Codec manager should be the only entry point for the BAP code, which in
    turn should not care for the codec location and from where the
    configurations are provided.
    
    This is part of the prepration for the multi-codec support.
    
    Bug: 295972694
    Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test
    Change-Id: Ibfe28cba5419d92af41ede4117a1f7d72dbbc8c4

--- system/bta/le_audio/device_groups.cc
+++ system/bta/le_audio/device_groups.cc
@@ -912,7 +912,7 @@ void LeAudioDeviceGroup::CigConfiguration_::GenerateCisIds(
   }
 
   const set_configurations::AudioSetConfigurations* confs =
-      AudioSetConfigurationProvider::Get()->GetConfigurations(context_type);
+      CodecManager::GetInstance()->GetCodecConfig(context_type);
 
   uint8_t cis_count_bidir = 0;
   uint8_t cis_count_unidir_sink = 0;
@@ -1322,15 +1322,9 @@ bool LeAudioDeviceGroup::IsAudioSetConfigurationSupported(
    * sampling rate matches with the sampling rate which is used
    * when all devices in the group are connected.
    */
-  bool dual_bidirection_swb_supported_ =
-      AudioSetConfigurationProvider::Get()->IsDualBiDirSwbSupported();
-  if (Size() > 1 &&
-      AudioSetConfigurationProvider::Get()->CheckConfigurationIsBiDirSwb(
-          *audio_set_conf)) {
-    if (!dual_bidirection_swb_supported_ ||
-        (CodecManager::GetInstance()->GetCodecLocation() ==
-             types::CodecLocation::ADSP &&
-         !CodecManager::GetInstance()->IsOffloadDualBiDirSwbSupported())) {
+  if (Size() > 1 && CodecManager::GetInstance()->CheckCodecConfigIsBiDirSwb(
+                        *audio_set_conf)) {
+    if (!CodecManager::GetInstance()->IsDualBiDirSwbSupported()) {
       /* two conditions
        * 1) dual bidirection swb is not supported for both software/offload
        * 2) offload not supported
@@ -1798,7 +1792,7 @@ const set_configurations::AudioSetConfiguration*
 LeAudioDeviceGroup::FindFirstSupportedConfiguration(
     LeAudioContextType context_type) const {
   const set_configurations::AudioSetConfigurations* confs =
-      AudioSetConfigurationProvider::Get()->GetConfigurations(context_type);
+      CodecManager::GetInstance()->GetCodecConfig(context_type);
 
   LOG_DEBUG("context type: %s,  number of connected devices: %d",
             bluetooth::common::ToString(context_type).c_str(),
