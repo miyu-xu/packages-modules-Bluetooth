edde2620fbbfdd98a8db826d5ec43174e7ef536f
Hui Peng <phui@google.com>
2023-10-02 18:29:43 -0700

    Test: Add sources in subdirectories of test to avatar
    - include all python sources (including sub direcotries)
      in android/pandora/test in avatar module
    - Prepare pairing_tests directory to host all pairing tests
    - Add utility functions to scan tests in pairing tests automatically
    
    Bug: 277282212
    Test: atest avatar
    Change-Id: Idf6e9de9aff36b210d3b83044ac66df79f966435

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -8,8 +8,8 @@ import os
 import sys
 
 from argparse import Namespace
-from mobly import suite_runner
-from typing import List, Tuple
+from mobly import suite_runner, base_test
+from typing import List, Tuple, Type
 
 _BUMBLE_BTSNOOP_FMT = 'bumble_btsnoop_{pid}_{instance}.log'
 
@@ -21,6 +21,7 @@ import avatar.cases.le_security_test
 import avatar.cases.security_test
 import gatt_test
 import hfpclient_test
+import pairing_tests
 import sdp_test
 import smp_test
 
@@ -36,6 +37,9 @@ _TEST_CLASSES_LIST = [
     hfpclient_test.HfpClientTest,
 ]
 
+
+def get_complete_test_class_list() -> list[Type[base_test.BaseTestClass]]:
+    return _TEST_CLASSES_LIST + pairing_tests.get_test_class_list()
 
 def _parse_cli_args() -> Tuple[Namespace, List[str]]:
     parser = argparse.ArgumentParser(description='Avatar test runner.')
@@ -57,4 +61,4 @@ if __name__ == "__main__":
         os.environ.setdefault('BUMBLE_SNOOPER', f'btsnoop:file:{ns.log_path}/{_BUMBLE_BTSNOOP_FMT}')
 
     # Run the test suite.
-    suite_runner.run_suite(_TEST_CLASSES_LIST, argv)  # type: ignore
+    suite_runner.run_suite(get_complete_test_class_list(), argv)  # type: ignore
