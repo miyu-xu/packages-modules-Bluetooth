fa658c8f4e42fc83dd2acb8487ea50c5afd35bf6
Charlie Boutier <charliebout@google.com>
2023-10-03 22:21:42 +0000

    BumbleBluetoothTests: Enhance gRPC callbacks with Iterator
    Created a custom class that implements Iterator and
    StreamObserver to improve the management of gRPC callbacks.
    
    Test: atest BumbleBluetoothTests
    Bug: 303302417
    Change-Id: I3e48c7ca7b51c086f8aa1a4023cd63dd23a799e3

---
+++ framework/tests/bumble/src/android/bluetooth/StreamObserverIterator.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth;
+
+import io.grpc.stub.StreamObserver;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class StreamObserverIterator<T> implements Iterator<T>, StreamObserver<T> {
+    private BlockingQueue<Object> mQueue = new LinkedBlockingQueue<>();
+    private boolean mCompleted = false;
+    private static final Object sCompletedIndicator = new Object();
+
+    @Override
+    public void onNext(T value) {
+        mQueue.add(value);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        mQueue.add(t);
+    }
+
+    @Override
+    public void onCompleted() {
+        mCompleted = true;
+        mQueue.add(sCompletedIndicator);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !mCompleted || !mQueue.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        try {
+            Object item = mQueue.take();
+            if (item == sCompletedIndicator) {
+                throw new RuntimeException(new NoSuchElementException("Iterator completed"));
+            }
+            if (item instanceof Throwable) {
+                throw new RuntimeException((Throwable) item);
+            }
+            return (T) item;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
