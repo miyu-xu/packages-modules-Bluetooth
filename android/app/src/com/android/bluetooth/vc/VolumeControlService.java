a96de301d18d95c07be1d6decc6080100d5f4613
Łukasz Rymanowski <rlukasz@google.com>
2023-10-10 10:18:40 +0000

    VolumeControlService: Fix setting volume on active group
    Volume control service was missing information about group being active
    and was no able to change the volume for the active group.
    
    Issue could be observed after phone restart or when switching between
    two LE HS
    
    Bug: 302142886
    Test: atest VolumeControlServiceTest
    Tag: #feature
    Change-Id: Idc11e503d2c0cea67ce0738fabde0adb0797a358

--- android/app/src/com/android/bluetooth/vc/VolumeControlService.java
+++ android/app/src/com/android/bluetooth/vc/VolumeControlService.java
@@ -18,7 +18,6 @@
 package com.android.bluetooth.vc;
 
 import static android.Manifest.permission.BLUETOOTH_CONNECT;
-
 import static com.android.bluetooth.Utils.enforceBluetoothPrivilegedPermission;
 
 import android.annotation.RequiresPermission;
@@ -637,6 +636,28 @@ public class VolumeControlService extends ProfileService {
     public int getGroupVolume(int groupId) {
         return mGroupVolumeCache.getOrDefault(groupId,
                         IBluetoothVolumeControl.VOLUME_CONTROL_UNKNOWN_VOLUME);
+    }
+
+    /**
+     * {@hide}
+     *
+     * @param groupId
+     * @param active
+     */
+    public void setGroupActive(int groupId, boolean active) {
+        if (DBG) {
+            Log.d(TAG, "setGroupActive: " + groupId + ", active: " + active);
+        }
+        if (!active) {
+            return;
+        }
+
+        int groupVolume = getGroupVolume(groupId);
+        Boolean groupMute = getGroupMute(groupId);
+
+        if (groupVolume != IBluetoothVolumeControl.VOLUME_CONTROL_UNKNOWN_VOLUME) {
+            updateGroupCacheAndAudioSystem(groupId, groupVolume, groupMute);
+        }
     }
 
     /**
@@ -1384,6 +1405,25 @@ public class VolumeControlService extends ProfileService {
             }
         }
 
+        @Override
+        public void setGroupActive(
+                int groupId,
+                boolean active,
+                AttributionSource source,
+                SynchronousResultReceiver receiver) {
+            try {
+                Objects.requireNonNull(source, "source cannot be null");
+                Objects.requireNonNull(receiver, "receiver cannot be null");
+
+                VolumeControlService service = getService(source);
+                receiver.send(null);
+                if (service != null) {
+                    service.setGroupActive(groupId, active);
+                }
+            } catch (RuntimeException e) {
+                receiver.propagateException(e);
+            }
+        }
 
         @Override
         public void mute(BluetoothDevice device,  AttributionSource source,
