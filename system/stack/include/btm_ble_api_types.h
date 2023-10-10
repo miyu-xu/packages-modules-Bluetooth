34cf274049d4339fd79dbf41fd4b7c5d2f72fbc4
Christine Hallstrom <hallstrom@google.com>
2023-10-10 13:18:07 -0700

    Delete legacy batch scan implementation
    This has been replaced by the GD impl.
    
    Test: m com.android.btservices
    Bug: 304574849
    Change-Id: I729e88db02a7cf10edbcc86950278ba55d0bfcdd

--- system/stack/include/btm_ble_api_types.h
+++ system/stack/include/btm_ble_api_types.h
@@ -319,7 +319,8 @@ typedef void(tBTM_RAND_ENC_CB)(tBTM_RAND_ENC* p1);
 typedef struct {
   uint8_t adv_inst_max; /* max adv instance supported in controller */
   uint8_t rpa_offloading;
-  uint16_t tot_scan_results_strg;
+  uint16_t tot_scan_results_strg; /* unused, present for vendor compatibility
+                                     reasons */
   uint8_t max_irk_list_sz;
   uint8_t filter_support;
   uint8_t max_filter;
