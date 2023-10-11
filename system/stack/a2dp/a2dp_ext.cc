8337c5c88d408c6999821794b97fe25742b6c48b
Henri Chataing <henrichataing@google.com>
2023-10-11 23:13:28 +0000

    Implement A2dpCodecConfigExt to extend A2dpCodecs with unknown offloaded codecs
    Bug: 308958546
    Test: m Bluetooth
    Change-Id: I29abee42c03ba76c09f21b728f672bc6b7402211

---
+++ system/stack/a2dp/a2dp_ext.cc
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "a2dp_ext.h"
+
+#include <base/logging.h>
+
+#include "a2dp_codec_api.h"
+#include "audio_hal_interface/a2dp_encoding.h"
+
+A2dpCodecConfigExt::A2dpCodecConfigExt(btav_a2dp_codec_index_t codec_index,
+                                       bool is_source)
+    : A2dpCodecConfig(
+          codec_index,
+          bluetooth::audio::a2dp::provider::codec_index_str(codec_index)
+              .value(),
+          BTAV_A2DP_CODEC_PRIORITY_DEFAULT),
+      is_source_(is_source) {
+  // Load the local capabilities from the provider info.
+  auto result = ::bluetooth::audio::a2dp::provider::codec_info(
+      codec_index, ota_codec_config_, &codec_capability_);
+  LOG_ASSERT(result) << "provider::codec_info unexpectdly failed";
+  codec_selectable_capability_ = codec_capability_;
+}
+
+bool A2dpCodecConfigExt::setCodecConfig(const uint8_t* p_peer_codec_info,
+                                        bool is_capability,
+                                        uint8_t* p_result_codec_config) {
+  // Call get_a2dp_config to recompute best capabilities.
+  // This method need to update codec_capability_, codec_config_,
+  // and ota_codec_config_ using the local codec_user_config_, and input
+  // peer_codec_info.
+  using namespace bluetooth::audio::a2dp;
+  provider::a2dp_remote_capabilities capabilities = {
+      .seid = 0,  // the SEID does not matter here.
+      .capabilities = p_peer_codec_info,
+  };
+
+  auto result = provider::get_a2dp_configuration(
+      std::vector<provider::a2dp_remote_capabilities>{capabilities},
+      codec_user_config_);
+  if (!result.has_value()) {
+    LOG(ERROR) << "Failed to set a configuration for " << name_;
+    return false;
+  }
+
+  memcpy(ota_codec_config_, result->codec_config.data(),
+         result->codec_config.size());
+  codec_config_ = result->codec_parameters;
+  codec_capability_ = result->codec_parameters;
+  return true;
+}
+
+bool A2dpCodecConfigExt::setPeerCodecCapabilities(
+    const uint8_t* p_peer_codec_capabilities) {
+  // setPeerCodecCapabilities updates the selectable
+  // capabilities in the codec config. It can be safely
+  // ignored as providing a superset of the selectable
+  // capabilities is safe.
+  return true;
+}
