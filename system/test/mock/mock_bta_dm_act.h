c181d89360bef0132c73f91b5df7d6c9fe7f71a7
Hui Peng <phui@google.com>
2023-10-13 18:49:07 +0000

    [Invisalign2] Separate security related fields from bta_dm_cb
    Bug: 301661850
    Test: mma packages/modules/Bluetooth
    Change-Id: I3a0e12a21ae434ef3811fc48306f34ddf03c7c54

--- system/test/mock/mock_bta_dm_act.h
+++ system/test/mock/mock_bta_dm_act.h
@@ -37,6 +37,7 @@
 #include <cstdint>
 
 #include "bta/dm/bta_dm_int.h"
+#include "bta/dm/bta_dm_sec.h"
 #include "bta/gatt/bta_gattc_int.h"
 #include "bta/include/bta_dm_ci.h"
 #include "btif/include/btif_dm.h"
