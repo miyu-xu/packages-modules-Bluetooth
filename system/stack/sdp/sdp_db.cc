49fe7596f6eea5e71bc384fe9fa01429c064fa48
Henri Chataing <henrichataing@google.com>
2023-10-17 18:06:21 -0700

    bt_trace: Prepare conversion to gd LOG_xx macros
      - Add includes for osi/include/log.h and LOG_TAG where missing
      - Add trailing ; where missing
    
    Bug: 305066880
    Test: m com.android.btservices
    Change-Id: I2d47a87214823d00753dcbf73e596a79236f3db0

--- system/stack/sdp/sdp_db.cc
+++ system/stack/sdp/sdp_db.cc
@@ -23,12 +23,15 @@
  *
  ******************************************************************************/
 
+#define LOG_TAG "sdp"
+
 #include <string.h>
 
 #include <cstdint>
 
 #include "bt_target.h"
 #include "osi/include/allocator.h"
+#include "osi/include/log.h"
 #include "stack/include/bt_types.h"
 #include "stack/include/sdpdefs.h"
 #include "stack/sdp/sdp_discovery_db.h"
