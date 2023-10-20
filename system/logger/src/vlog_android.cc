556367ff892066d0f6e2c4a81f2a5bf8e7615965
Henri Chataing <henrichataing@google.com>
2023-10-20 09:06:51 -0700

    system: fmtlib logger implementation
    - The fmtlib logger is implemented in <system/bluetooth/logger.h>.
      The header defines the followign templated logs function:
    
        template<typename T...>
        logger::fatal(fmt::format_string<T...> fmt, T...args);
        logger::error(..);
        logger::warn(..);
        logger::info(..);
        logger::debug(..);
        logger::verbose(..);
    
    - Front-end, logs are printed out by invoking these
      macros with the macro LOG_TAG defined _before_ the
      inclusion of #include <bluetooth/logger.h>
    
    - Back-end, a single method must be implemented for all supported
      platforms (android, floss, host):
    
      void vlog(Level level, char const *tag, char const *file_name, int line,
                std::string_view fmt, fmt::format_args vargs);
    
    - Default implementations are already provided:
      + vlog_android: outputs to <log/log.h> __android_log_write_log_message
      + vlog_syslog: outputs to <syslog.h> syslog
      + vlog_stdout: outputs to stdout
    
    Bug: 305066880
    Test: m Bluetooth
    Change-Id: Ic8a80f113b25d874c372d7dce8252d5428842ee8

---
+++ system/logger/src/vlog_android.cc
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <log/log.h>
+
+#include "bluetooth/logger.h"
+#include "truncating_buffer.h"
+
+namespace logger {
+
+static constexpr size_t kBufferSize = 1024;
+
+static android_LogPriority level_to_priority(Level level) {
+  switch (level) {
+    case kVerbose:
+      return ANDROID_LOG_VERBOSE;
+    case kDebug:
+      return ANDROID_LOG_DEBUG;
+    case kInfo:
+      return ANDROID_LOG_INFO;
+    case kWarn:
+      return ANDROID_LOG_WARN;
+    case kError:
+      return ANDROID_LOG_ERROR;
+    case kFatal:
+      return ANDROID_LOG_FATAL;
+    default:
+      break;
+  }
+  return ANDROID_LOG_DEFAULT;
+}
+
+void vlog(Level level, char const* tag, char const* file_name, int line,
+          char const* function_name, fmt::string_view fmt,
+          fmt::format_args vargs) {
+  // Check if log is enabled.
+  if (!__android_log_is_loggable(level, tag, ANDROID_LOG_DEFAULT) &&
+      !__android_log_is_loggable(level, "bluetooth", ANDROID_LOG_DEFAULT)) {
+    return;
+  }
+
+  // Format to stack buffer.
+  truncating_buffer<kBufferSize> buffer;
+  fmt::format_to(std::back_insert_iterator(buffer), "{}: ", function_name);
+  auto result = fmt::vformat_to(std::back_insert_iterator(buffer), fmt, vargs);
+  buffer.buffer[buffer.len] = '\0';
+
+  // Send message to liblog.
+  struct __android_log_message message = {
+      .struct_size = sizeof(__android_log_message),
+      .buffer_id = LOG_ID_MAIN,
+      .priority = level_to_priority(level),
+      .tag = tag,
+      .file = file_name,
+      .line = static_cast<uint32_t>(line),
+      .message = buffer.buffer,
+  };
+  __android_log_write_log_message(&message);
+}
+
+}  // namespace logger
