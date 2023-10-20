8b2c3a581c5afbfed6e9ab5ee156faa2d5095fde
Henri Chataing <henrichataing@google.com>
2023-10-20 09:06:51 -0700

    system: fmtlib logger implementation
    - The fmtlib logger is implemented in <system/bluetooth/logger.h>.
      The header defines the followign templated logs function:
    
        template<typename T...>
        logger::fatal(fmt::format_string<T...> fmt, T...args);
        logger::error(..);
        logger::warn(..);
        logger::info(..);
        logger::debug(..);
        logger::verbose(..);
    
    - Front-end, logs are printed out by invoking these
      macros with the macro LOG_TAG defined _before_ the
      inclusion of #include <bluetooth/logger.h>
    
    - Back-end, a single method must be implemented for all supported
      platforms (android, floss, host):
    
      void vlog(Level level, char const *tag, char const *file_name, int line,
                std::string_view fmt, fmt::format_args vargs);
    
    - Default implementations are already provided:
      + vlog_android: outputs to <log/log.h> __android_log_write_log_message
      + vlog_syslog: outputs to <syslog.h> syslog
      + vlog_stdout: outputs to stdout
    
    Bug: 305066880
    Test: m Bluetooth
    Change-Id: Ic8a80f113b25d874c372d7dce8252d5428842ee8

---
+++ system/logger/src/truncating_buffer.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cstddef>
+
+namespace logger {
+
+/// Truncating write buffer.
+///
+/// This buffer can be used with `std::back_insert_iterator` to create
+/// an output iterator. All write actions beyond the maximum length of
+/// the buffer are silently ignored.
+template <int buffer_size>
+struct truncating_buffer {
+  using value_type = char;
+
+  void push_back(char c) {
+    if (len < buffer_size) {
+      buffer[len++] = c;
+    }
+  }
+
+  char buffer[buffer_size];
+  size_t len{0};
+};
+
+}  // namespace logger
