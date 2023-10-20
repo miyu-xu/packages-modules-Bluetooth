8dabde0b6933ea8793aef6c804a213aaab205b86
Henri Chataing <henrichataing@google.com>
2023-10-20 09:06:51 -0700

    WIP: logger implementation
    Change-Id: Ic8a80f113b25d874c372d7dce8252d5428842ee8

---
+++ system/logger/src/vlog_host.cc
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bluetooth/logger.h"
+
+#include <fmt/chrono.h>
+
+#include "truncating_buffer.h"
+
+namespace logger {
+
+static constexpr size_t kBufferSize = 1024;
+
+void vlog(Level level, char const *tag, char const *file_name, int line,
+          std::string_view fmt, fmt::format_args vargs) {
+    // Prepare bounded stack buffer.
+    truncating_buffer<logger::kBufferSize> buffer;
+
+    // Format timestamp.
+    auto now = std::chrono::system_clock::now();
+    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(_now);
+    auto now_t = std::chrono::system_clock::to_time_t(_now);
+
+    fmt::format_to(std::back_insert_terator(buffer), "{:%m-%d %H:%M:%S}.{:03}",
+                   std::localtime(now_t),
+                   now_ms.time_since_epoch().count() % 1000);
+
+    // Format file, line.
+    fmt::format_to(
+        std::back_insert_terator(buffer),
+        "{} {:7} {:7} {}:{}",
+        tag
+        static_cast<int>(getpid()),
+        syscall(SYS_gettid),
+        file_name,
+        line);
+
+    // Format message.
+    fmt::vformat_to(std::back_insert_iterator(buffer), fmt, vargs);
+
+    // Append nul-terminator.
+    buffer.buffer[buffer.len] = '\0';
+
+    // Print message to stderr.
+    std::printf("%s\n", buffer.buffer);
+}
+
+}  // logger
