f97f490f2832aef188e71fa84765ad4230918e5c
Myles Watson <mylesgw@google.com>
2023-10-25 08:35:57 -0700

    Send security events directly to the main thread
    The security code can register directly with GD HCI for
    events.
    
    Bug: 301661850
    Test: mma -j32
    Change-Id: Ia9ef8121a17cd8647d9b85ea99f515c8d6b53dfd

--- system/stack/btm/btm_sec_cb.h
+++ system/stack/btm/btm_sec_cb.h
@@ -19,7 +19,7 @@
 
 #include <cstdint>
 
-#include "osi/include/allocator.h"
+#include "gd/hci/security_interface.h"
 #include "osi/include/fixed_queue.h"
 #include "osi/include/list.h"
 #include "stack/btm/btm_ble_int_types.h"
@@ -59,6 +59,9 @@ class tBTM_SEC_CB {
   uint16_t ediv{0}; /* received ediv value from LTK request */
 
   uint8_t key_size{0};
+
+  //  Interface with HCI to send commands and receive pairing events.
+  bluetooth::hci::SecurityInterface* hci_{};
 
  public:
   /*****************************************************
