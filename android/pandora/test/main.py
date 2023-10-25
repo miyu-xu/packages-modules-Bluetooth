1ed6b9333fc560afd91dc447aa29c3e32b040ba8
Hui Peng <phui@google.com>
2023-10-25 13:46:11 -0700

    Add bredr/ssp pairing tests (display_yesno)
    Bug: 277282212
    Test: atest avatar
    Change-Id: Ib5fe2130b27c769ef9953ba1fa729fc6e943812a

--- android/pandora/test/main.py
+++ android/pandora/test/main.py
@@ -24,6 +24,7 @@ import hfpclient_test
 import sdp_test
 import smp_test
 import pairing_tests.br_edr.legacy.tests as bredr_legacy_tests
+import pairing_tests.br_edr.ssp.display_output_and_yes_no_input.tests as bredr_ssp_displayyesno_tests
 
 _TEST_CLASSES_LIST = [
     avatar.cases.host_test.HostTest,
@@ -36,6 +37,7 @@ _TEST_CLASSES_LIST = [
     asha_test.AshaTest,
     hfpclient_test.HfpClientTest,
     bredr_legacy_tests.BREDRLegacyTestClass,
+    bredr_ssp_displayyesno_tests.BREDRDisplayYesNoTestClass,
 ]
 
 def _parse_cli_args() -> Tuple[Namespace, List[str]]:
