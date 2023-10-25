ecf778d0434c983c4ac73b5608a989452dc73120
Hui Peng <phui@google.com>
2023-10-25 13:46:11 -0700

    Add bredr/ssp pairing tests (display_yesno)
    Bug: 277282212
    Test: atest avatar
    Flag: EXEMPT, Tests
    Change-Id: Ib5fe2130b27c769ef9953ba1fa729fc6e943812a

---
+++ android/pandora/test/pairing_tests/br_edr/ssp/display_output_and_yes_no_input/tests.py
@@ -0,0 +1,44 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mobly import base_test
+
+from pairing_tests.br_edr.acl_connection_builder_impl import BREDRAclConnectionBuilder
+from pairing_tests.br_edr.pairing_processors_impl import BREDRNumericComparisonPairingProcessor
+from pairing_tests.br_edr.service_accessors_impl import BumbleHidServiceAccessor
+
+from pairing_tests.decorators import add_common_tests, add_boilerplate_methods
+
+@add_common_tests(
+    acl_connection_builder_classes=[BREDRAclConnectionBuilder],
+    pairing_process_class=BREDRNumericComparisonPairingProcessor,
+    service_accessor_classes=[BumbleHidServiceAccessor],
+)
+@add_boilerplate_methods
+class BREDRDisplayYesNoTestClass(base_test.BaseTestClass):
+
+    ref_config = {
+        'classic_enabled': True,
+        'le_enabled': False,
+        'classic_ssp_enabled': True,
+        'classic_sc_enabled': False,
+        'server': {
+            'pairing_sc_enable': False,
+            'pairing_mitm_enable': True,
+            'pairing_bonding_enable': True,
+            # Android IO CAP: Display_YESNO
+            # Ref IO CAP:
+            'io_capability': 'display_output_and_yes_no_input',
+        },
+    }
