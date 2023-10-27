8bf2c84a9093b0e4eb9de11367ff07ff8f587774
Hui Peng <phui@google.com>
2023-10-26 20:02:27 -0700

    Add ble/sc pairing tests (display_yesno)
    Bug: 277282212
    Test: atest avatar
    Flag: EXEMPT, Tests
    Change-Id: Ia3e552d5ee45e8bbf2f5f4bde29a0566e9d410a2

--- android/pandora/test/pairing/__init__.py
+++ android/pandora/test/pairing/__init__.py
@@ -21,6 +21,7 @@ from pairing.br_edr.ssp.no_output_no_input.tests import BREDRNoOutputNoInputTest
 
 
 from pairing.ble.legacy.display_output_and_yes_no_input.tests import BLELegDisplayYesNoTestClass
+from pairing.ble.sc.display_output_and_yes_no_input.tests import BLESCDisplayYesNoTestClass
 
 
 from pairing.smp_test import SmpTest
@@ -28,6 +29,7 @@ from pairing.smp_test import SmpTest
 
 _test_class_list = [
     BLELegDisplayYesNoTestClass,
+    BLESCDisplayYesNoTestClass,
     BREDRDisplayYesNoTestClass,
     BREDRDisplayOnlyTestClass,
     # BREDRKeyboardOnlyTestClass,
