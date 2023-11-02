4c184e4ee3edc1d1600fb84dd6373130c5eaf99c
Rajat Soni <quic_rajasoni@quicinc.com>
2023-11-02 18:58:53 +0530

    Delay report timer
    Some devices do not respond for delay report
    after set_config, to handle such scenerio we
    start a timer for 2 sec and wait for remote
    to send delay report. If timer expires then
    we initiate AVDTP_OPEN.
    
    Change-Id: Ida12409751fcb4bf0fbdda4db580b4ddf207365b

--- system/stack/avdt/avdt_int.h
+++ system/stack/avdt/avdt_int.h
@@ -87,6 +87,9 @@ enum {
 /* scb transport channel disconnect timeout value (in milliseconds) */
 #define AVDT_SCB_TC_DISC_TIMEOUT_MS (10 * 1000)
 
+/*Delay report timer for remote response*/
+#define AVDT_DELAY_REPORT_TIMEOUT_MS (2 * 1000)
+
 /* maximum number of command retransmissions */
 #ifndef AVDT_RET_MAX
 #define AVDT_RET_MAX 1
@@ -486,6 +489,7 @@ class AvdtpScb {
   AvdtpSepConfig curr_cfg;           // Current configuration
   AvdtpSepConfig req_cfg;            // Requested configuration
   alarm_t* transport_channel_timer;  // Transport channel connect timer
+  alarm_t* delay_report_timer;
   BT_HDR* p_pkt;                     // Packet waiting to be sent
   AvdtpCcb* p_ccb;                   // CCB associated with this SCB
   uint16_t media_seq;                // Media packet sequence number
@@ -939,6 +943,7 @@ void avdt_ccb_idle_ccb_timer_timeout(void* data);
 void avdt_ccb_ret_ccb_timer_timeout(void* data);
 void avdt_ccb_rsp_ccb_timer_timeout(void* data);
 void avdt_scb_transport_channel_timer_timeout(void* data);
+void avdt_delay_report_timer_timeout(void* data);
 
 /*****************************************************************************
  * macros
