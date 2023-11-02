9170daf3d5f854f81e975c99b069a23d16827b5f
Myles Watson <mylesgw@google.com>
2023-11-02 10:05:28 -0700

    Explicitly serialize GATT DB entries
    Bug: 304830775
    Test: mma -j32
    Change-Id: Ib2a40d3ea853ea3977738850f72429a9441fa285

--- system/bta/gatt/bta_gattc_db_storage.cc
+++ system/bta/gatt/bta_gattc_db_storage.cc
@@ -18,7 +18,6 @@
 
 #define LOG_TAG "bt_bta_gattc"
 
-#include <base/logging.h>
 #include <base/strings/string_number_conversions.h>
 #include <dirent.h>
 #include <sys/stat.h>
@@ -27,7 +26,10 @@
 #include <vector>
 
 #include "bta/gatt/bta_gattc_int.h"
-#include "osi/include/log.h"
+#include "gatt/database.h"
+#include "os/log.h"
+#include "stack/include/gattdefs.h"
+#include "types/bluetooth/uuid.h"
 
 using gatt::StoredAttribute;
 using std::string;
@@ -165,6 +167,67 @@ gatt::Database bta_gattc_hash_load(const Octet16& hash) {
   return bta_gattc_load_db(fname);
 }
 
+void StoredAttribute::SerializeStoredAttribute(const StoredAttribute& attr,
+                                               std::vector<uint8_t> bytes) {
+  size_t padding_needed = StoredAttribute::kSizeOnDisk;
+  // handle
+  bytes.push_back(attr.handle & 0xff);
+  bytes.push_back(attr.handle >> 8);
+  auto uuid = attr.type.To128BitLE();
+  bytes.insert(bytes.cend(), uuid.cbegin(), uuid.cend());
+  padding_needed -= sizeof(attr.handle) - sizeof(attr.type);
+
+  ASSERT(attr.type.Is16Bit());
+  switch (attr.type.As16Bit()) {
+    /* primary or secondary service definition */
+    case GATT_UUID_PRI_SERVICE:
+    case GATT_UUID_SEC_SERVICE:
+      uuid = attr.value.service.uuid.To128BitLE();
+      bytes.insert(bytes.cend(), uuid.cbegin(), uuid.cend());
+      bytes.push_back(attr.value.service.end_handle & 0xff);
+      bytes.push_back(attr.value.service.end_handle >> 8);
+      padding_needed -= sizeof(attr.value.service.uuid) -
+                        sizeof(attr.value.service.end_handle);
+      break;
+    case GATT_UUID_INCLUDE_SERVICE:
+      /* included service definition */
+      bytes.push_back(attr.value.included_service.handle & 0xff);
+      bytes.push_back(attr.value.included_service.handle >> 8);
+      padding_needed -= sizeof(attr.value.included_service.handle);
+      bytes.push_back(attr.value.included_service.end_handle & 0xff);
+      bytes.push_back(attr.value.included_service.end_handle >> 8);
+      padding_needed -= sizeof(attr.value.included_service.end_handle);
+      uuid = attr.value.included_service.uuid.To128BitLE();
+      bytes.insert(bytes.cend(), uuid.cbegin(), uuid.cend());
+      padding_needed -= sizeof(attr.value.included_service.uuid);
+      break;
+    case GATT_UUID_CHAR_DECLARE:
+      /* characteristic definition */
+      bytes.push_back(attr.value.characteristic.properties);
+      padding_needed -= sizeof(attr.value.characteristic.properties);
+      bytes.push_back(attr.value.characteristic.value_handle & 0xff);
+      bytes.push_back(attr.value.characteristic.value_handle >> 8);
+      padding_needed -= sizeof(attr.value.characteristic.value_handle);
+      uuid = attr.value.characteristic.uuid.To128BitLE();
+      bytes.insert(bytes.cend(), uuid.cbegin(), uuid.cend());
+      padding_needed -= sizeof(attr.value.characteristic.uuid);
+      break;
+    case GATT_UUID_CHAR_EXT_PROP:
+      /* for descriptor we store value only for
+       * «Characteristic Extended Properties» */
+      bytes.push_back(attr.value.characteristic_extended_properties & 0xff);
+      bytes.push_back(attr.value.characteristic_extended_properties >> 8);
+      padding_needed -= sizeof(attr.value.characteristic_extended_properties);
+      break;
+    default:
+      LOG_WARN("Unhandled type UUID %d", attr.type.As16Bit());
+  }
+  // padding
+  for (size_t i = 0; i < padding_needed; i++) {
+    bytes.push_back(0);
+  }
+}
+
 /*******************************************************************************
  *
  * Function         bta_gattc_store_db
@@ -201,7 +264,14 @@ static bool bta_gattc_store_db(const char* fname,
     return false;
   }
 
-  if (fwrite(attr.data(), sizeof(StoredAttribute), num_attr, fd) != num_attr) {
+  std::vector<uint8_t> db_bytes;
+  db_bytes.reserve(num_attr * StoredAttribute::kSizeOnDisk);
+  for (const auto attribute : attr) {
+    StoredAttribute::SerializeStoredAttribute(attribute, db_bytes);
+  }
+
+  if (fwrite(db_bytes.data(), sizeof(uint8_t), db_bytes.size(), fd) !=
+      db_bytes.size()) {
     LOG(ERROR) << __func__ << ": can't write GATT cache attributes: " << fname;
     fclose(fd);
     return false;
--- system/bta/test/gatt/database_test.cc
+++ system/bta/test/gatt/database_test.cc
@@ -290,4 +290,81 @@ TEST(GattCacheTest,
   // LOG(ERROR) << " " << base::HexEncode(&attr, len);
   EXPECT_EQ(memcmp(binary_form, &attr, len), 0);
 }
+/* This test makes sure that Descriptor represented in StoredAttribute have
+ * proper binary format. */
+TEST(
+    GattCacheTest,
+    stored_attribute_serialized_to_binary_characteristic_extended_properties_test) {
+  StoredAttribute attr;
+
+  attr = {.handle = 0x0003,
+          .type = Uuid::FromString("2900"),
+          .value = {.characteristic_extended_properties = 0x0001}};
+
+  std::vector<uint8_t> serialized;
+  StoredAttribute::SerializeStoredAttribute(attr, serialized);
+
+  constexpr size_t len = StoredAttribute::kSizeOnDisk;
+  // clang-format off
+  uint8_t binary_form[len] = {
+      /*handle */ 0x03, 0x00,
+      /* type */ 0x00, 0x00, 0x29, 0x00, 0x00, 0x00, 0x10, 0x00, 0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB,
+      /* characteristic extended properties */ 0x01, 0x00,
+      /* clear padding    */ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00};
+  // clang-format on
+
+  // useful for debugging:
+  // LOG(ERROR) << " " << base::HexEncode(&attr, len);
+  EXPECT_EQ(memcmp(binary_form, serialized.data(), len), 0);
+}
+
+// Example from Bluetooth SPEC V5.2, Vol 3, Part G, APPENDIX B
+TEST(GattDatabaseTest, serialized_hash_test) {
+  DatabaseBuilder builder;
+  builder.AddService(0x0001, 0x0005, Uuid::From16Bit(0x1800), true);
+  builder.AddService(0x0006, 0x000D, Uuid::From16Bit(0x1801), true);
+  builder.AddService(0x000E, 0x0013, Uuid::From16Bit(0x1808), true);
+  builder.AddService(0x0014, 0xFFFF, Uuid::From16Bit(0x180F), false);
+
+  builder.AddCharacteristic(0x0002, 0x0003, Uuid::From16Bit(0x2A00), 0x0A);
+  builder.AddCharacteristic(0x0004, 0x0005, Uuid::From16Bit(0x2A01), 0x02);
+
+  builder.AddCharacteristic(0x0007, 0x0008, Uuid::From16Bit(0x2A05), 0x20);
+  builder.AddDescriptor(0x0009, Uuid::From16Bit(0x2902));
+  builder.AddCharacteristic(0x000A, 0x000B, Uuid::From16Bit(0x2B29), 0x0A);
+  builder.AddCharacteristic(0x000C, 0x000D, Uuid::From16Bit(0x2B2A), 0x02);
+
+  builder.AddIncludedService(0x000F, Uuid::From16Bit(0x180F), 0x0014, 0x0016);
+  builder.AddCharacteristic(0x0010, 0x0011, Uuid::From16Bit(0x2A18), 0xA2);
+  builder.AddDescriptor(0x0012, Uuid::From16Bit(0x2902));
+  builder.AddDescriptor(0x0013, Uuid::From16Bit(0x2900));
+
+  builder.AddCharacteristic(0x0015, 0x0016, Uuid::From16Bit(0x2A19), 0x02);
+
+  // set characteristic extended properties descriptor values
+  std::vector<uint16_t> descriptorValues = {0x0000};
+  builder.SetValueOfDescriptors(descriptorValues);
+
+  Database db = builder.Build();
+
+  auto serialized = db.Serialize();
+  std::vector<uint8_t> bytes;
+  for (auto attr : serialized) {
+    StoredAttribute::SerializeStoredAttribute(attr, bytes);
+  }
+  std::vector<StoredAttribute> attr_from_disk(serialized.size());
+  std::copy(bytes.cbegin(), bytes.cend(), (uint8_t*)attr_from_disk.data());
+  bool is_successful = false;
+  Database db_from_disk =
+      gatt::Database::Deserialize(attr_from_disk, &is_successful);
+  ASSERT_TRUE(is_successful);
+  is_successful = false;
+  Database db_from_serialized =
+      gatt::Database::Deserialize(serialized, &is_successful);
+  ASSERT_TRUE(is_successful);
+
+  EXPECT_EQ(db_from_disk.Hash(), db_from_serialized.Hash());
+}
+
 }  // namespace gatt
