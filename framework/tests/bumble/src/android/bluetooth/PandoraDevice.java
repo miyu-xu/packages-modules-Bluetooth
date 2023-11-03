83dd9be011035e18ba7ea8765be5aad456c38116
Brian Delwiche <delwiche@google.com>
2023-11-03 22:38:33 +0000

    Add Bumble test for GATT Characteristic Read/Write
    Also includes a basic service discovery test and new Bumble features.
    
    Test: atest BumbleBluetoothTests
    Bug: 303248992
    Change-Id: I709d945463b3e3fdc83dc2ea24ba2cd9e147b024

--- framework/tests/bumble/src/android/bluetooth/PandoraDevice.java
+++ framework/tests/bumble/src/android/bluetooth/PandoraDevice.java
@@ -30,6 +30,7 @@ import org.junit.rules.ExternalResource;
 import java.util.concurrent.TimeUnit;
 
 import pandora.DckGrpc;
+import pandora.GATTGrpc;
 import pandora.HostGrpc;
 import pandora.HostProto;
 import pandora.SecurityGrpc;
@@ -113,6 +114,16 @@ public final class PandoraDevice extends ExternalResource {
     /** Get Pandora Dck blocking service */
     public DckGrpc.DckBlockingStub dckBlocking() {
         return DckGrpc.newBlockingStub(mChannel);
+    }
+
+    /** Get Pandora GATT service */
+    public GATTGrpc.GATTStub gatt() {
+        return GATTGrpc.newStub(mChannel);
+    }
+
+    /** Get Pandora GATT blocking service */
+    public GATTGrpc.GATTBlockingStub gattBlocking() {
+        return GATTGrpc.newBlockingStub(mChannel);
     }
 
     /** Get Pandora Security service */
