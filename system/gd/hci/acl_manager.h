e96093053b35318cbd04a0893e29df34e9bddc48
Chris Manton <cmanton@google.com>
2023-11-08 11:20:59 -0800

    Remove unused main::shim::l2cap::L2CA_UseLegacySecurityModule
    Bug: 309799239
    Test: m .
    
    Change-Id: I39b2207bebf85820ca641e322e2c96aa9db39421

--- system/gd/hci/acl_manager.h
+++ system/gd/hci/acl_manager.h
@@ -46,7 +46,6 @@ class Acl;
 }
 
 class Btm;
-void L2CA_UseLegacySecurityModule();
 bool L2CA_SetAclPriority(uint16_t, bool);
 }
 
@@ -55,7 +54,6 @@ namespace hci {
 class AclManager : public Module {
  friend class bluetooth::shim::Btm;
  friend class bluetooth::shim::legacy::Acl;
- friend void bluetooth::shim::L2CA_UseLegacySecurityModule();
  friend bool bluetooth::shim::L2CA_SetAclPriority(uint16_t, bool);
  friend class bluetooth::hci::LeScanningManager;
  friend class bluetooth::hci::DistanceMeasurementManager;
