82d6827372a34687e660291ec7987686bcf68cab
Myles Watson <mylesgw@google.com>
2023-11-07 16:05:58 -0800

    main/shim: Remove unused includes
    Bug: 301661850
    Test: mma -j32
    Change-Id: I0014bd9923c2fa736c60ccf8fdf457a59c409784

--- system/main/shim/dumpsys.h
+++ system/main/shim/dumpsys.h
@@ -17,8 +17,6 @@
 #pragma once
 
 #include <functional>
-#include <list>
-#include <string>
 
 #define LOG_DUMPSYS(fd, fmt, args...)                 \
   do {                                                \
