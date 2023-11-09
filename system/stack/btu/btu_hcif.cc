5834f8ad5fdc57c5ca64fd9630c416de563ec22a
Myles Watson <mylesgw@google.com>
2023-11-09 08:30:19 -0800

    Security: Move parsing to HCI: Remote OOB
    The security code should not have to parse packets.
    
    Bug: 301661850
    Test: mma -j32
    Change-Id: I0791250571fdacadaf297e3c32b6f0e159905c68

--- system/stack/btu/btu_hcif.cc
+++ system/stack/btu/btu_hcif.cc
@@ -95,6 +95,7 @@ static void btu_hcif_esco_connection_chg_evt(uint8_t* p);
 /* Parsing functions for btm functions */
 
 static void btu_hcif_proc_sp_req_evt(tBTM_SP_EVT event, const uint8_t* p);
+static void btu_hcif_rem_oob_req(const uint8_t* p);
 static void btu_hcif_simple_pair_complete(const uint8_t* p);
 static void btu_hcif_create_conn_cancel_complete(const uint8_t* p,
                                                  uint16_t evt_len);
@@ -310,7 +311,7 @@ void btu_hcif_process_event(UNUSED_ATTR uint8_t controller_id,
       btu_hcif_proc_sp_req_evt(BTM_SP_KEY_REQ_EVT, p);
       break;
     case HCI_REMOTE_OOB_DATA_REQUEST_EVT:
-      btm_rem_oob_req(p);
+      btu_hcif_rem_oob_req(p);
       break;
     case HCI_SIMPLE_PAIRING_COMPLETE_EVT:
       btu_hcif_simple_pair_complete(p);
@@ -1432,6 +1433,11 @@ void btu_hcif_proc_sp_req_evt(tBTM_SP_EVT event, const uint8_t* p) {
       btm_proc_sp_req_evt(BTM_SP_KEY_REQ_EVT, bda, value);
       break;
   }
+}
+void btu_hcif_rem_oob_req(const uint8_t* p) {
+  RawAddress bda;
+  STREAM_TO_BDADDR(bda, p);
+  btm_rem_oob_req(bda);
 }
 void btu_hcif_simple_pair_complete(const uint8_t* p) {
   RawAddress bd_addr;
