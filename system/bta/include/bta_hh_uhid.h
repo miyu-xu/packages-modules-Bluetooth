93bf9c020a13bef6ee3630c5caf6b28b42f7800e
Zyan Wu <zyanwu@google.com>
2023-11-09 14:27:30 +0800

    Add a thread-safe uhid event queue
    The queue is used to send all uhid_event in uhid_write
    to bta_hh_co.cc's uhid_thread so writes to uhid device
    can be serialized.
    
    Bug: 299192582
    Bug: 291522341
    Bug: 297186590
    Test: atest net_test_btif_hh
    Change-Id: Ia6cabe97c334fd7c1a9e2dae6b697e8fb2c3adf6

---
+++ system/bta/include/bta_hh_uhid.h
@@ -0,0 +1,113 @@
+/******************************************************************************
+ *
+ *  Copyright Google Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+/******************************************************************************
+ *
+ *  This is the interface file for hid host UHID queue related structure and
+ *  functions.
+ *
+ ******************************************************************************/
+#ifndef BTA_HH_UHID_H
+#define BTA_HH_UHID_H
+
+#include <mutex>
+
+#include <linux/uhid.h>
+
+typedef struct {
+  void* p_event_queue;
+  std::mutex* p_mutex;
+} tBTA_HH_UHID_EVT_QUEUE;
+
+/*******************************************************************************
+ *
+ * Function         bta_hh_uhid_evt_queue_init
+ *
+ * Description      This function initializes a tBTA_HH_UHID_EVT_QUEUE
+ *
+ * Parameters       queue: The queue to be inited and it shall not be nullptr.
+ *                  thread_safe: true if the queue needs to be used in multiple
+ *                               threaded cases.
+ *
+ * Returns          void.
+ *
+ ******************************************************************************/
+void bta_hh_uhid_evt_queue_init(
+    tBTA_HH_UHID_EVT_QUEUE* queue, bool thread_safe = true);
+
+/*******************************************************************************
+ *
+ * Function         bta_hh_uhid_evt_queue_destroy
+ *
+ * Description      This function destroys a tBTA_HH_UHID_EVT_QUEUE. The
+ *                  destroed queue can be destroyed multiple times.
+ *
+ * Parameters       queue: The queue to be destroyed and it shall not be used
+ *                         after destroy.
+ *
+ * Returns          void.
+ *
+ ******************************************************************************/
+void bta_hh_uhid_evt_queue_destroy(tBTA_HH_UHID_EVT_QUEUE* queue);
+
+/*******************************************************************************
+ *
+ * Function         bta_hh_uhid_evt_queue_cleanup
+ *
+ * Description      This function cleans up a tBTA_HH_UHID_EVT_QUEUE that all
+ *                  events in the queue are removed.
+ *
+ * Parameters       queue: The queue to be cleaned up and it shall be inited.
+ *
+ * Returns          void.
+ *
+ ******************************************************************************/
+void bta_hh_uhid_evt_queue_cleanup(tBTA_HH_UHID_EVT_QUEUE* queue);
+
+/*******************************************************************************
+ *
+ * Function         bta_hh_uhid_evt_queue_dequeue
+ *
+ * Description      This function dequeues an event from the queue.
+ *
+ * Parameters       queue: The queue to be dequeued.
+ *                  event[out]: the dequeued event.
+ *
+ * Returns          True if dequeue is successful or false if the queue is
+ *                  empty.
+ *
+ ******************************************************************************/
+bool bta_hh_uhid_evt_queue_dequeue(
+    tBTA_HH_UHID_EVT_QUEUE* queue, struct uhid_event& event);
+
+/*******************************************************************************
+ *
+ * Function         bta_hh_uhid_evt_queue_enqueue
+ *
+ * Description      This function enqueue an event into the queue.
+ *
+ * Parameters       queue: The queue to be enqueued.
+ *                  event: The event to be put into the queue.
+ *
+ * Returns          True if enqueue is successful or false if the queue is full.
+ *
+ ******************************************************************************/
+bool bta_hh_uhid_evt_queue_enqueue(
+    tBTA_HH_UHID_EVT_QUEUE* queue, const struct uhid_event& event);
+
+#endif  // BTA_HH_UHID_H
---
+++ system/btif/co/bta_hh_uhid.cc
@@ -0,0 +1,118 @@
+/******************************************************************************
+ *
+ *  Copyright Google Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#include <base/logging.h>
+#include <linux/uhid.h>
+#include <queue>
+
+#include "bt_trace.h"
+#include "bta_hh_uhid.h"
+#include "osi/include/allocator.h"
+
+typedef std::queue<struct uhid_event> uhid_event_queue_t;
+
+static bool uhid_event_queue_dequeue(
+    uhid_event_queue_t& queue, struct uhid_event& event) {
+  if (queue.empty()) {
+    return false;
+  }
+  event = queue.front();
+  queue.pop();
+  return true;
+}
+
+static bool uhid_event_queue_enqueue(
+    uhid_event_queue_t& queue, const struct uhid_event& event) {
+  static constexpr size_t MAX_EVENT_COUNT = 20;
+  if (queue.size() >= MAX_EVENT_COUNT) {
+    return false;
+  }
+  queue.push(event);
+  return true;
+}
+
+void bta_hh_uhid_evt_queue_init(tBTA_HH_UHID_EVT_QUEUE* queue, bool thread_safe) {
+  CHECK(queue) << "uhid queue shall not be nullptr.";
+  queue->p_event_queue = new (std::nothrow) uhid_event_queue_t();
+
+  CHECK(queue->p_event_queue) << "uhid event queue cannot be allocated.";
+
+  queue->p_mutex = NULL;
+  if (thread_safe) {
+    queue->p_mutex = new (std::nothrow) std::mutex();
+    CHECK(queue->p_mutex) << "uhid event queue mutex cannot be allocated.";
+  }
+}
+
+void bta_hh_uhid_evt_queue_destroy(tBTA_HH_UHID_EVT_QUEUE* queue) {
+  CHECK(queue) << "uhid queue shall not be nullptr.";
+  if (queue->p_event_queue == NULL) {
+    return;
+  }
+
+  delete static_cast<uhid_event_queue_t*>(queue->p_event_queue);
+  queue->p_event_queue = NULL;
+
+  if (queue->p_mutex) {
+    delete queue->p_mutex;
+  }
+  queue->p_mutex = NULL;
+}
+
+void bta_hh_uhid_evt_queue_cleanup(tBTA_HH_UHID_EVT_QUEUE* queue) {
+  CHECK(queue) << "uhid queue shall not be nullptr.";
+  CHECK(queue->p_event_queue) << "uhid queue shall be initialized.";
+
+  if (queue->p_mutex) {
+    std::lock_guard<std::mutex> guard(*queue->p_mutex);
+    *static_cast<uhid_event_queue_t*>(queue->p_event_queue)
+        = uhid_event_queue_t();
+    return;
+  }
+  *static_cast<uhid_event_queue_t*>(queue->p_event_queue)
+      = uhid_event_queue_t();
+}
+
+bool bta_hh_uhid_evt_queue_dequeue(
+    tBTA_HH_UHID_EVT_QUEUE* queue, struct uhid_event& event) {
+  CHECK(queue) << "uhid queue shall not be nullptr.";
+  CHECK(queue->p_event_queue) << "uhid queue shall be initialized.";
+
+  if (queue->p_mutex) {
+    std::lock_guard<std::mutex> guard(*queue->p_mutex);
+    return uhid_event_queue_dequeue(
+        *static_cast<uhid_event_queue_t*>(queue->p_event_queue), event);
+  }
+  return uhid_event_queue_dequeue(
+      *static_cast<uhid_event_queue_t*>(queue->p_event_queue), event);
+}
+
+bool bta_hh_uhid_evt_queue_enqueue(
+    tBTA_HH_UHID_EVT_QUEUE* queue, const struct uhid_event& event) {
+  CHECK(queue) << "uhid queue shall not be nullptr.";
+  CHECK(queue->p_event_queue) << "uhid queue shall be initialized.";
+
+  if (queue->p_mutex) {
+    std::lock_guard<std::mutex> guard(*queue->p_mutex);
+    return uhid_event_queue_enqueue(
+        *static_cast<uhid_event_queue_t*>(queue->p_event_queue), event);
+  }
+  return uhid_event_queue_enqueue(
+      *static_cast<uhid_event_queue_t*>(queue->p_event_queue), event);
+}
+
