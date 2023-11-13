a3f38cc10fa3eacf542f203fbc09aac92033a7ef
Chris Manton <cmanton@google.com>
2023-11-13 13:38:28 -0800

    bta: Formalize legacy bta device and service discovery APIs
    Bug: 310712967
    Test: atest net_test_bta
    
    Change-Id: I647051369291ea6b7fc51ca35a839d5bde82c7b1

--- system/bta/dm/bta_dm_disc.cc
+++ system/bta/dm/bta_dm_disc.cc
@@ -27,7 +27,7 @@
 #include <cstdint>
 
 #include "bta/dm/bta_dm_disc.h"
-#include "bta/dm/bta_dm_int.h"
+#include "bta/dm/bta_dm_disc_int.h"
 #include "bta/include/bta_api.h"
 #include "bta/include/bta_gatt_api.h"
 #include "bta/include/bta_sdp_api.h"
@@ -55,7 +55,6 @@
 #include "stack/include/sdp_status.h"
 #include "stack/sdp/sdpint.h"
 #include "types/raw_address.h"
-
 #ifdef TARGET_FLOSS
 #include "stack/include/srvc_api.h"
 #endif
@@ -2421,6 +2420,43 @@ void bta_dm_disc_acl_down(const RawAddress& bd_addr, tBT_TRANSPORT transport) {
 }
 
 void bta_dm_disc_stop() { bta_dm_disc_reset(); }
+
+void bta_dm_disc_start_device_discovery(tBTA_DM_SEARCH_CBACK* p_cback) {
+  tBTA_DM_API_SEARCH* p_msg =
+      (tBTA_DM_API_SEARCH*)osi_calloc(sizeof(tBTA_DM_API_SEARCH));
+
+  p_msg->hdr.event = BTA_DM_API_SEARCH_EVT;
+  p_msg->p_cback = p_cback;
+
+  bta_sys_sendmsg(p_msg);
+}
+
+void bta_dm_disc_stop_device_discovery() {
+  tBTA_DM_API_DISCOVERY_CANCEL* p_msg =
+      (tBTA_DM_API_DISCOVERY_CANCEL*)osi_calloc(
+          sizeof(tBTA_DM_API_DISCOVERY_CANCEL));
+
+  p_msg->hdr.event = BTA_DM_API_SEARCH_CANCEL_EVT;
+  bta_sys_sendmsg(p_msg);
+}
+
+void bta_dm_disc_start_service_discovery(tBTA_DM_SEARCH_CBACK* p_cback,
+                                         const RawAddress& bd_addr,
+                                         tBT_TRANSPORT transport) {
+  tBTA_DM_API_DISCOVER* p_msg =
+      (tBTA_DM_API_DISCOVER*)osi_calloc(sizeof(tBTA_DM_API_DISCOVER));
+
+  p_msg->hdr.event = BTA_DM_API_DISCOVER_EVT;
+  p_msg->bd_addr = bd_addr;
+  p_msg->transport = transport;
+  p_msg->p_cback = p_cback;
+
+  bta_sys_sendmsg(p_msg);
+}
+
+void bta_dm_disc_stop_service_discovery() {
+  LOG_WARN("Stop service discovery not yet implemented for legacy module");
+}
 
 #define DUMPSYS_TAG "shim::legacy::bta::dm"
 void DumpsysBtaDmDisc(int fd) {
