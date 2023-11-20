9aa5968d93bca5d25b3a41e2b779e7af0c1935f3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-11-20 13:11:52 +0000

    Revert "leaudio: Fix for race in late connect scenario"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/91654550-7e2a-4272-93f8-043c01f9d579). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I56e153d07c4eeb872b4acc307521d1c4a49fc6fb

--- system/bta/le_audio/device_groups.cc
+++ system/bta/le_audio/device_groups.cc
@@ -852,10 +852,7 @@ bool LeAudioDeviceGroup::IsGroupStreamReady(void) const {
     auto dev = weak.lock();
     if (!dev) return false;
 
-    /* We are interested here in devices which are connected on profile level
-     * and devices which are configured (meaning, have actived ASE(s))*/
-    if (dev->GetConnectionState() == DeviceConnectState::CONNECTED &&
-        dev->HaveActiveAse()) {
+    if (dev->GetConnectionState() == DeviceConnectState::CONNECTED) {
       if (!dev->IsReadyToStream()) {
         return false;
       }
