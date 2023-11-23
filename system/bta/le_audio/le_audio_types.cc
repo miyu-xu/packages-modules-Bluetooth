7d234277cd64036ad94e72e6001cc4947d94ed28
Jakub Tyszkowski <tyszkowski@google.com>
2023-11-23 11:06:10 +0000

    LeAudio: Improve LTV map structure
    Add the compare operators, a way to get two maps intersection
    and a more convenient raw data parsing helper.
    
    This is not used with the current set of features but will be
    needed for the upcoming multi-codec support.
    
    Bug: 295972694
    Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test
    Flag: EXEMPT; not used yet (will be used by the upcoming multicodec feature), verified with unit tests
    Change-Id: Ia65e480497121e624ff87d471b65d325e7010528

--- system/bta/le_audio/le_audio_types.cc
+++ system/bta/le_audio/le_audio_types.cc
@@ -503,12 +503,18 @@ void LeAudioLtvMap::Append(const LeAudioLtvMap& other) {
   for (auto& el : other.values) {
     values[el.first] = el.second;
   }
+
+  value_hash = 0;
 }
 
 LeAudioLtvMap LeAudioLtvMap::Parse(const uint8_t* p_value, uint8_t len,
                                    bool& success) {
   LeAudioLtvMap ltv_map;
+  success = ltv_map.Parse(p_value, len);
+  return ltv_map;
+}
 
+bool LeAudioLtvMap::Parse(const uint8_t* p_value, uint8_t len) {
   if (len > 0) {
     const auto p_value_end = p_value + len;
 
@@ -522,8 +528,7 @@ LeAudioLtvMap LeAudioLtvMap::Parse(const uint8_t* p_value, uint8_t len,
       if (p_value_end < (p_value + ltv_len)) {
         LOG(ERROR) << __func__
                    << " Invalid ltv_len: " << static_cast<int>(ltv_len);
-        success = false;
-        return LeAudioLtvMap();
+        return false;
       }
 
       uint8_t ltv_type;
@@ -534,12 +539,11 @@ LeAudioLtvMap LeAudioLtvMap::Parse(const uint8_t* p_value, uint8_t len,
       p_value += ltv_len;
 
       std::vector<uint8_t> ltv_value(p_temp, p_value);
-      ltv_map.values.emplace(ltv_type, std::move(ltv_value));
+      values.emplace(ltv_type, std::move(ltv_value));
     }
   }
 
-  success = true;
-  return ltv_map;
+  return true;
 }
 
 size_t LeAudioLtvMap::RawPacketSize() const {
@@ -593,6 +597,24 @@ LeAudioLtvMap::GetAsCoreCodecCapabilities() const {
     core_capabilities = LtvMapToCoreCodecCapabilities(*this);
   }
   return *core_capabilities;
+}
+
+void LeAudioLtvMap::RemoveAllTypes(const LeAudioLtvMap& other) {
+  for (auto const& [key, _] : other.values) {
+    Remove(key);
+  }
+}
+
+LeAudioLtvMap LeAudioLtvMap::GetIntersection(const LeAudioLtvMap& other) const {
+  LeAudioLtvMap result;
+  for (auto const& [key, value] : values) {
+    auto entry = other.Find(key);
+    if (entry->size() != value.size()) continue;
+    if (memcmp(entry->data(), value.data(), value.size()) == 0) {
+      result.Add(key, value);
+    }
+  }
+  return result;
 }
 
 }  // namespace types
