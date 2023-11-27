f732c62e51530a033e13066994403d6874ee1df7
Chris Manton <cmanton@google.com>
2023-11-27 13:06:00 -0800

    Prefer errno.h ==> cerrno
    Also remove unused inclusion and other misc. reinclusion
    
    Bug: 313491963
    Test: m .
    
    Change-Id: I5e21c1df6d1598b8c04817fa5251023f118cb9b5

--- system/stack/mmc/codec_client/codec_client.cc
+++ system/stack/mmc/codec_client/codec_client.cc
@@ -21,17 +21,14 @@
 #include <dbus/bus.h>
 #include <dbus/message.h>
 #include <dbus/object_proxy.h>
-#include <errno.h>
 #include <poll.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
+#include <cerrno>
 #include <cstring>
-#include <memory>
 
-#include "mmc/daemon/constants.h"
-#include "mmc/metrics/mmc_rtt_logger.h"
 #include "mmc/proto/mmc_config.pb.h"
 #include "mmc/proto/mmc_service.pb.h"
 
