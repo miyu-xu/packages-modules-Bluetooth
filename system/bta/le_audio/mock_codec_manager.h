75efccc9b45e00eab95b422ff3167214062367a0
Jakub Tyszkowski <tyszkowski@google.com>
2023-12-05 13:25:56 +0000

    LeAudio: Configuration provider dependency inversion - Part 3
    Part 3: Moving the Unicast audio configuration provider API
    
    This change moves the Unicast audio stream configuration provider
    behind the Codec Manager API so that the unicast audio code would
    not care where the codec is being located.
    
    Bug: 308428860
    Bug: 295972694
    Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test
    Flag: EXEMPT; refactor for the multicodec support - verified with unit tests and manual testing
    Change-Id: Ia756c241612cb330935da218f4450440b54685bd

--- system/bta/le_audio/mock_codec_manager.h
+++ system/bta/le_audio/mock_codec_manager.h
@@ -41,9 +41,15 @@ class MockCodecManager {
                std::function<void(const ::le_audio::offload_config& config,
                                   uint8_t direction)>
                    update_receiver));
-  MOCK_METHOD((le_audio::set_configurations::AudioSetConfigurations*),
-              GetOffloadCodecConfig,
-              (le_audio::types::LeAudioContextType ctx_type), (const));
+  MOCK_METHOD(
+      (std::unique_ptr<le_audio::set_configurations::AudioSetConfiguration>),
+      GetCodecConfig,
+      (le_audio::types::LeAudioContextType ctx_type,
+       std::function<const le_audio::set_configurations::AudioSetConfiguration*(
+           le_audio::types::LeAudioContextType context_type,
+           const le_audio::set_configurations::AudioSetConfigurations* confs)>
+           non_vendor_config_matcher),
+      (const));
   MOCK_METHOD(
       (bool), CheckCodecConfigIsBiDirSwb,
       (const le_audio::set_configurations::AudioSetConfiguration& config),
