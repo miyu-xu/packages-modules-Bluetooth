19f8276bfedebeaebc94e6c60033f22ef44de230
Rongxuan Liu <rongxuan@google.com>
2023-12-05 02:01:18 +0000

    [le audio] Fix the Volume control callback on registration
    In current design, there is race condition that callbacks are triggered
    before callback is added to mCallbackExecutorMap. We need to update the
    map first before calling registerCallback in service.
    
    Flagged by: leaudio_broadcast_volume_control_for_connected_devices
    
    Bug: 314738219
    Bug: 307408418
    Test: atest BluetoothVolumeControlTest BluetoothVolumeControlTest
    Change-Id: I61c4e8ff758b33d44bc2aa3977f72ed0aef430ad

--- android/app/src/com/android/bluetooth/vc/VolumeControlService.java
+++ android/app/src/com/android/bluetooth/vc/VolumeControlService.java
@@ -807,16 +807,17 @@ public class VolumeControlService extends ProfileService {
                     continue;
                 }
             }
-            // notify volume level for all vc devices
-            if (mFeatureFlags.leaudioBroadcastVolumeControlForConnectedDevices()) {
-                notifyDevicesVolumeChanged(getDevices(), Optional.empty());
-            }
         }
 
         tempCallbackList.finishBroadcast();
 
         /* User is notified, remove callback from temporary list */
         tempCallbackList.unregister(callback);
+
+        // notify volume level for all vc devices with mCallbacks handle
+        if (mFeatureFlags.leaudioBroadcastVolumeControlForConnectedDevices()) {
+            notifyDevicesVolumeChanged(getDevices(), Optional.empty());
+        }
     }
 
     void registerCallback(IBluetoothVolumeControlCallback callback) {
