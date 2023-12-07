b64c302abbc15784fbb039e4e5ee7bf364e166cf
William Escande <wescande@google.com>
2023-12-06 20:24:12 -0800

    SystemServer: ClearIdentity before binding
    Bug: 290732757
    Test: m service-bluetooth | no-op change from a logical aspect
    Flag: EXEMPT, to allow onboarding in 24Q1
    Change-Id: I62f2143b990b6fb80f82a20c25d86e371a6e8075

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1359,8 +1359,15 @@ class BluetoothManagerService {
                 psc =
                         new ProfileServiceConnections(
                                 new Intent(PROFILE_TO_SERVICE_NAME.get(bluetoothProfile)));
-                if (!psc.bindService(DEFAULT_REBIND_COUNT)) {
-                    return false;
+
+                // TODO(291815510 | 288450479): Remove clearCallingIdentity
+                final long callingIdentity = Binder.clearCallingIdentity();
+                try {
+                    if (!psc.bindService(DEFAULT_REBIND_COUNT)) {
+                        return false;
+                    }
+                } finally {
+                    Binder.restoreCallingIdentity(callingIdentity);
                 }
 
                 mProfileServices.put(bluetoothProfile, psc);
