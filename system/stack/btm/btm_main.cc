213836e7d75e6d9dae9c97104d91e4ac33ae6f71
Chris Manton <cmanton@google.com>
2023-12-07 16:42:04 -0800

    bt_target:[8/37] IWYU stack::
    Bug: 315176531
    Test: m .
    Flag: EXEMPT, IWYU
    Change-Id: I70fa868b6c01bfac57d8c0dd333b7d3861a3af20

--- system/stack/btm/btm_main.cc
+++ system/stack/btm/btm_main.cc
@@ -25,13 +25,9 @@
 #include <memory>
 #include <string>
 
-#include "bt_target.h"
 #include "btm_int_types.h"
-#include "btm_sec_cb.h"
-#include "main/shim/dumpsys.h"
-#include "osi/include/log.h"
-#include "stack/include/btm_client_interface.h"
-#include "stack/include/inq_hci_link_interface.h"
+#include "internal_include/bt_target.h"
+#include "os/log.h"
 #include "stack/include/security_client_callbacks.h"
 #include "types/raw_address.h"
 
