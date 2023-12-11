36923a3954e4bea0e5a19e76c9b63c7415d41aa7
Chris Manton <cmanton@google.com>
2023-12-11 12:46:21 -0800

    tBTM_BLE_INQ_CB:[10/10] Use longform btm_ble_write_adv_enable_complete
    Bug: 315850105
    Test: m .
    Flag: EXEMPT, mechanical refactor
    
    Change-Id: I0e801ab0b54528d70069571d289d759c0644f485

--- system/stack/btm/btm_ble_gap.cc
+++ system/stack/btm/btm_ble_gap.cc
@@ -3342,12 +3342,10 @@ err_out:
  *
  ******************************************************************************/
 void btm_ble_write_adv_enable_complete(uint8_t* p, uint16_t evt_len) {
-  tBTM_BLE_INQ_CB* p_cb = &btm_cb.ble_ctr_cb.inq_var;
-
   /* if write adv enable/disbale not succeed */
   if (evt_len < 1 || *p != HCI_SUCCESS) {
     /* toggle back the adv mode */
-    p_cb->adv_mode = !p_cb->adv_mode;
+    btm_cb.ble_ctr_cb.inq_var.adv_mode = !btm_cb.ble_ctr_cb.inq_var.adv_mode;
   }
 }
 
