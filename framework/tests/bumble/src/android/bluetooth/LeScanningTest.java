056b9325bcf4b341ac9639bc5efe12d2a86a22a7
Srinivas Patibandla <patibandlas@google.com>
2023-12-12 22:49:16 +0000

    LeScanningTest: Move BUMBLE_IRK to Utils
    Also, re-organize imports and static fields.
    
    Test: atest BumbleBluetoothTests
    Bug: 315513588
    Flag: TEST_ONLY
    Change-Id: I071aff128f963ea8179286fd579f238a11dd9019

--- framework/tests/bumble/src/android/bluetooth/LeScanningTest.java
+++ framework/tests/bumble/src/android/bluetooth/LeScanningTest.java
@@ -16,7 +16,6 @@
 
 package android.bluetooth;
 
-import static com.google.common.io.BaseEncoding.base16;
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -53,41 +52,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 
+import pandora.HostProto;
+import pandora.HostProto.AdvertiseRequest;
+import pandora.HostProto.AdvertiseResponse;
+import pandora.HostProto.OwnAddressType;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
-import pandora.HostProto;
-import pandora.HostProto.AdvertiseRequest;
-import pandora.HostProto.AdvertiseResponse;
-import pandora.HostProto.OwnAddressType;
-
 @RunWith(AndroidJUnit4.class)
 public class LeScanningTest {
     private static final String TAG = "LeScanningTest";
     private static final int TIMEOUT_SCANNING_MS = 2000;
+    private static final String TEST_UUID_STRING = "00001805-0000-1000-8000-00805f9b34fb";
+    private static final String TEST_ADDRESS_RANDOM_STATIC = "F0:43:A8:23:10:11";
+    private static final String ACTION_DYNAMIC_RECEIVER_SCAN_RESULT =
+            "android.bluetooth.test.ACTION_DYNAMIC_RECEIVER_SCAN_RESULT";
 
     @Rule public final AdoptShellPermissionsRule mPermissionRule = new AdoptShellPermissionsRule();
 
     @Rule public final PandoraDevice mBumble = new PandoraDevice();
-
-    private static final String TEST_ADDRESS_RANDOM_STATIC = "F0:43:A8:23:10:11";
-
-    // IRK must match what's defined in bumble_config.json
-    private static final byte[] TEST_IRK = base16().decode("1F66F4B5F0C742F807DD0DDBF64E9213");
 
     private final Context mContext = ApplicationProvider.getApplicationContext();
     private final BluetoothManager mBluetoothManager =
             mContext.getSystemService(BluetoothManager.class);
     private final BluetoothAdapter mBluetoothAdapter = mBluetoothManager.getAdapter();
     private final BluetoothLeScanner mLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
-
-    private static final String TEST_UUID_STRING = "00001805-0000-1000-8000-00805f9b34fb";
-
-    private static final String ACTION_DYNAMIC_RECEIVER_SCAN_RESULT =
-            "android.bluetooth.test.ACTION_DYNAMIC_RECEIVER_SCAN_RESULT";
 
     @Test
     public void startBleScan_withCallbackTypeAllMatches() {
@@ -117,7 +110,7 @@ public class LeScanningTest {
                         .setDeviceAddress(
                                 TEST_ADDRESS_RANDOM_STATIC,
                                 BluetoothDevice.ADDRESS_TYPE_RANDOM,
-                                TEST_IRK)
+                                Utils.BUMBLE_IRK)
                         .build();
 
         List<ScanResult> results =
