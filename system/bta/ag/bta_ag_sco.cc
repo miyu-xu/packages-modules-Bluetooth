10bf45e5f5cefc2a7ef6c3a870f02d4c1b1affba
Bhushan Shinkar <shinkar@google.com>
2023-12-12 15:42:41 +0000

    Fall back to SCO connection if eSCO connection is failed
    Bug: 303168931
    Test: m .
    Change-Id: I202f7277f8dc5b7633f400de9de9fdb65b291efc

--- system/bta/ag/bta_ag_sco.cc
+++ system/bta/ag/bta_ag_sco.cc
@@ -250,8 +250,15 @@ static void bta_ag_sco_disc_cback(uint16_t sco_idx) {
           }
         }
       }
-    } else if (bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb)) {
-      LOG_ERROR("%s: eSCO/SCO failed to open, no more fall back", __func__);
+    } else if (IS_FLAG_ENABLED(
+                   fall_back_to_sco_connection_if_esco_connection_is_failed) &&
+               bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb)) {
+      if (bta_ag_cb.sco.p_curr_scb->try_zero_retransmission_effort == false) {
+        bta_ag_cb.sco.p_curr_scb->try_zero_retransmission_effort = true;
+        bta_ag_cb.sco.p_curr_scb->state = BTA_AG_SCO_CODEC_ST;
+        LOG_WARN("%s: eSCO/SCO failed to open, retry", __func__);
+      } else
+        LOG_ERROR("%s: eSCO/SCO failed to open, no more fall back", __func__);
     }
 
     bta_ag_cb.sco.p_curr_scb->inuse_codec = BTM_SCO_CODEC_NONE;
@@ -483,6 +490,13 @@ void bta_ag_create_sco(tBTA_AG_SCB* p_scb, bool is_orig) {
       // HFP <=1.6 eSCO
       params = esco_parameters_for_codec(ESCO_CODEC_CVSD_S3, offload);
     }
+  }
+
+  if (IS_FLAG_ENABLED(
+          fall_back_to_sco_connection_if_esco_connection_is_failed) &&
+      p_scb->try_zero_retransmission_effort == true) {
+    p_scb->try_zero_retransmission_effort = false;
+    params = esco_parameters_for_codec(SCO_CODEC_CVSD_D1, offload);
   }
 
   /* Configure input/output data path based on HAL settings. */
@@ -1418,6 +1432,7 @@ void bta_ag_sco_shutdown(tBTA_AG_SCB* p_scb,
  ******************************************************************************/
 void bta_ag_sco_conn_open(tBTA_AG_SCB* p_scb,
                           UNUSED_ATTR const tBTA_AG_DATA& data) {
+  p_scb->try_zero_retransmission_effort = false;
   bta_ag_sco_event(p_scb, BTA_AG_SCO_CONN_OPEN_E);
 
   bta_sys_sco_open(BTA_ID_AG, p_scb->app_id, p_scb->peer_addr);
@@ -1464,6 +1479,9 @@ void bta_ag_sco_conn_close(tBTA_AG_SCB* p_scb,
         p_scb->codec_msbc_settings == BTA_AG_SCO_MSBC_SETTINGS_T1) ||
        (p_scb->sco_codec == BTM_SCO_CODEC_LC3 &&
         p_scb->codec_lc3_settings == BTA_AG_SCO_LC3_SETTINGS_T1) ||
+       (p_scb->try_zero_retransmission_effort == true &&
+        IS_FLAG_ENABLED(
+            fall_back_to_sco_connection_if_esco_connection_is_failed)) ||
        aptx_voice)) {
     bta_ag_sco_event(p_scb, BTA_AG_SCO_REOPEN_E);
   } else {
