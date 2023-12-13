5a412606d4f1551b5ca22d479e8772ed5c8ea91f
William Escande <wescande@google.com>
2023-12-13 10:27:02 +0100

    SystemServer: Un/Register adapter sync pre-impl
    ServiceMessenger Explanation:
    Since the messenger will be using the correct looper, it can directly
    call the sync method from the ManagerService.
    ~
    This CL is adding register and unregister adapter sync method
    ~
    
    Bug: 288450479
    Bug: 321804999
    Test: m service-bluetooth | No-op change
          Flag testing will be done in follow-up, before flag rollout
    Change-Id: I5b10892e493edb8d54745955ccac8c18f6181925

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -859,14 +859,44 @@ class BluetoothManagerService {
 
     IBluetooth registerAdapter(IBluetoothManagerCallback callback) {
         synchronized (mCallbacks) {
-            mCallbacks.register(callback);
+            return registerAdapter_sync(callback);
         }
-        return mAdapter != null ? mAdapter.getAdapterBinder() : null;
     }
 
     void unregisterAdapter(IBluetoothManagerCallback callback) {
         synchronized (mCallbacks) {
-            mCallbacks.unregister(callback);
+            unregisterAdapter_sync(callback);
+        }
+    }
+
+    IBluetooth registerAdapter_sync(IBluetoothManagerCallback callback) {
+        mCallbacks.register(callback);
+        return mAdapter != null ? mAdapter.getAdapterBinder() : null;
+    }
+
+    void unregisterAdapter_sync(IBluetoothManagerCallback callback) {
+        mCallbacks.unregister(callback);
+    }
+
+    @FunctionalInterface
+    public interface RemoteExceptionConsumer<T> {
+        void accept(T t) throws RemoteException;
+    }
+
+    private void broadcastToAdapters(
+            String logAction, RemoteExceptionConsumer<IBluetoothManagerCallback> action) {
+        final int itemCount = mCallbacks.beginBroadcast();
+        try {
+            Log.d(TAG, "Broadcasting " + logAction + "() to " + itemCount + " receivers.");
+            for (int i = 0; i < itemCount; i++) {
+                try {
+                    action.accept(mCallbacks.getBroadcastItem(i));
+                } catch (RemoteException e) {
+                    Log.e(TAG, "RemoteException while calling " + logAction + "()#" + i, e);
+                }
+            }
+        } finally {
+            mCallbacks.finishBroadcast();
         }
     }
 
@@ -1364,6 +1394,11 @@ class BluetoothManagerService {
     }
 
     private void sendBluetoothOnCallback() {
+        if (Flags.systemServerMessenger()) {
+            broadcastToAdapters(
+                    "sendBluetoothOnCallback", IBluetoothManagerCallback::onBluetoothOn);
+            return;
+        }
         synchronized (mCallbacks) {
             try {
                 int n = mCallbacks.beginBroadcast();
@@ -1382,6 +1417,11 @@ class BluetoothManagerService {
     }
 
     private void sendBluetoothOffCallback() {
+        if (Flags.systemServerMessenger()) {
+            broadcastToAdapters(
+                    "sendBluetoothOffCallback", IBluetoothManagerCallback::onBluetoothOff);
+            return;
+        }
         synchronized (mCallbacks) {
             try {
                 int n = mCallbacks.beginBroadcast();
@@ -1401,6 +1441,12 @@ class BluetoothManagerService {
 
     /** Inform BluetoothAdapter instances that Adapter service is up */
     private void sendBluetoothServiceUpCallback() {
+        if (Flags.systemServerMessenger()) {
+            broadcastToAdapters(
+                    "sendBluetoothServiceUpCallback",
+                    (item) -> item.onBluetoothServiceUp(mAdapter.getAdapterBinder()));
+            return;
+        }
         synchronized (mCallbacks) {
             mAdapterLock.readLock().lock();
             try {
@@ -1424,6 +1470,12 @@ class BluetoothManagerService {
 
     /** Inform BluetoothAdapter instances that Adapter service is down */
     private void sendBluetoothServiceDownCallback() {
+        if (Flags.systemServerMessenger()) {
+            broadcastToAdapters(
+                    "sendBluetoothServiceDownCallback",
+                    IBluetoothManagerCallback::onBluetoothServiceDown);
+            return;
+        }
         synchronized (mCallbacks) {
             try {
                 int n = mCallbacks.beginBroadcast();
