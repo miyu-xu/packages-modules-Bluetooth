02918014e85039181d6372ccecb50c724e9a6740
Jakub Tyszkowski <tyszkowski@google.com>
2023-12-14 14:36:07 +0000

    LeAudio: Clean-up the QoS structures in ASE
    Bug: 308427707
    Flag: EXEMPT; only grouped the fields into structures - mechanical refactor - correctness confirmed with unit tests
    Test: atest --host bluetooth_le_audio_test bluetooth_le_audio_client_test  bluetooth_test_broadcaster bluetooth_test_broadcaster_state_machine bluetooth_le_audio_codec_manager_test --disable-upload-result --minimal-build --no-metrics
    Change-Id: I3bbda232fca9da8ea1ba14187568b0e89898ae65

--- system/bta/le_audio/state_machine_test.cc
+++ system/bta/le_audio/state_machine_test.cc
@@ -4464,8 +4464,8 @@ TEST_F(StateMachineTest, testAttachDeviceToTheStream) {
 
   /* Verify that ASE of first device are still good*/
   auto ase = fistDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest, testAttachDeviceToTheStreamV2) {
@@ -4585,8 +4585,8 @@ TEST_F(StateMachineTest, testAttachDeviceToTheStreamV2) {
 
   /* Verify that ASE of first device are still good*/
   auto ase = fistDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest, testAttachDeviceToTheStreamDeviceNoAvailableContext) {
@@ -4829,8 +4829,8 @@ TEST_F(StateMachineTest, testAutonomousConfiguredAndAttachToStream) {
 
   /* Verify that ASE of first device are still good*/
   auto ase = fistDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest,
@@ -4943,8 +4943,8 @@ TEST_F(StateMachineTest,
 
   /* Verify that ASE of first device are still good*/
   auto ase = fistDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest, testAttachDeviceToTheStreamDoNotAttach) {
@@ -5115,8 +5115,8 @@ TEST_F(StateMachineTest, testReconfigureAfterLateDeviceAttached) {
   /* Verify that ASE of first device are still good*/
   ase = fistDevice->GetFirstActiveAse();
   ASSERT_NE(nullptr, ase);
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest, testStreamToGettingReadyDevice) {
@@ -5346,8 +5346,8 @@ TEST_F(StateMachineTest, testAttachDeviceToTheConversationalStream) {
 
   /* Verify that ASE of first device are still good*/
   auto ase = firstDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 
   // Make sure ASEs with reconnected CIS are in STREAMING state
   ASSERT_TRUE(lastDevice->HaveAllActiveAsesSameState(
@@ -6633,8 +6633,8 @@ TEST_F(StateMachineTest, testAttachDeviceToTheStreamCisFailure) {
 
   /* Verify that ASE of first device are still good*/
   auto ase = fistDevice->GetFirstActiveAse();
-  ASSERT_NE(ase->max_transport_latency, 0);
-  ASSERT_NE(ase->retrans_nb, 0);
+  ASSERT_NE(ase->qos_config.max_transport_latency, 0);
+  ASSERT_NE(ase->qos_config.retrans_nb, 0);
 }
 
 TEST_F(StateMachineTest, testAclDropWithoutApriorCisDisconnection) {
