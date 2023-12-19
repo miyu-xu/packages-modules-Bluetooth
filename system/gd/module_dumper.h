4c119353b1e8a6a05f1a2dcea562d713fdb97132
Chris Manton <cmanton@google.com>
2023-12-19 14:21:55 -0800

    dumpsys: Extract flatbuffer to allow additional implementations
    Bug: 317135880
    Test: atest bluetooth_test_gdx_unit
    Flag: EXEMPT, test infrastructure
    
    Change-Id: Ia172cb23ab0c852d34d0a5ea5d57eba5a77a1d25

--- system/gd/module_dumper.h
+++ system/gd/module_dumper.h
@@ -18,17 +18,20 @@
 
 #include <string>
 
+#include "module.h"
+
 namespace bluetooth {
 
 class ModuleRegistry;
 
 class ModuleDumper {
  public:
-  ModuleDumper(const ModuleRegistry& module_registry, const char* title)
-      : module_registry_(module_registry), title_(title) {}
-  void DumpState(std::string* output) const;
+  ModuleDumper(int fd, const ModuleRegistry& module_registry, const char* title)
+      : fd_(fd), module_registry_(module_registry), title_(title) {}
+  void DumpState(std::string* output, std::ostringstream& oss) const;
 
  private:
+  const int fd_{-1};
   const ModuleRegistry& module_registry_;
   const std::string title_;
 };
