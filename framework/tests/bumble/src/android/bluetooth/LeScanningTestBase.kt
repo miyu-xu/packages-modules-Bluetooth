1ffdae1112547b7892ab6b2464b057fbaa968ae4
Srinivas Patibandla <patibandlas@google.com>
2023-12-20 01:04:55 +0000

    Refactor #advertiseWithBumble and #startScanning
    Test: atest BumbleBluetoothTests
    Bug: 315513588
    Flag: TEST_ONLY
    Change-Id: I77b1db0b3ba843de5e2b96dadf19f7fa9c46c969

--- framework/tests/bumble/src/android/bluetooth/LeScanningTestBase.kt
+++ framework/tests/bumble/src/android/bluetooth/LeScanningTestBase.kt
@@ -42,8 +42,16 @@ abstract class LeScanningTestBase {
     @JvmField val mBluetoothAdapter = mBluetoothManager.adapter
     @JvmField val mLeScanner = mBluetoothAdapter.bluetoothLeScanner
 
-    fun advertiseWithBumble(serviceUuid: String?, addressType: OwnAddressType) {
-        val requestBuilder = AdvertiseRequest.newBuilder().setOwnAddressType(addressType)
+    @JvmOverloads
+    fun advertiseWithBumble(
+        addressType: OwnAddressType,
+        serviceUuid: String? = null,
+        isConnectable: Boolean = false
+    ) {
+        val requestBuilder =
+            AdvertiseRequest.newBuilder()
+                .setOwnAddressType(addressType)
+                .setConnectable(isConnectable)
 
         if (serviceUuid != null) {
             val dataTypeBuilder = HostProto.DataTypes.newBuilder()
@@ -64,15 +72,9 @@ abstract class LeScanningTestBase {
         mBumble.host().advertise(requestBuilder.build(), responseObserver)
     }
 
-    fun startScanning(scanFilter: ScanFilter, callbackType: Int): List<ScanResult>? {
-        val future = CompletableFuture<List<ScanResult>?>()
+    fun scanWithCallback(scanFilter: ScanFilter, scanSettings: ScanSettings): List<ScanResult> {
+        val future = CompletableFuture<List<ScanResult>>()
         val scanResults = mutableListOf<ScanResult>()
-
-        val scanSettings =
-            ScanSettings.Builder()
-                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                .setCallbackType(callbackType)
-                .build()
 
         val scanCallback =
             object : ScanCallback() {
@@ -104,14 +106,14 @@ abstract class LeScanningTestBase {
 
                 override fun onScanFailed(errorCode: Int) {
                     Log.i(TAG, "onScanFailed errorCode: $errorCode")
-                    future.complete(null)
+                    future.complete(listOf())
                 }
             }
 
         mLeScanner.startScan(listOf(scanFilter), scanSettings, scanCallback)
 
-        val results: List<ScanResult>? =
-            future.completeOnTimeout(null, TIMEOUT_SCANNING_MS, TimeUnit.MILLISECONDS).join()
+        val results =
+            future.completeOnTimeout(listOf(), TIMEOUT_SCANNING_MS, TimeUnit.MILLISECONDS).join()
 
         mLeScanner.stopScan(scanCallback)
 
