4a29bb722ec590f2cad6600b9510314d586da0da
Chris Manton <cmanton@google.com>
2023-12-19 19:59:59 -0800

    stack::test: IWYU and remove unused
    Bug: 317217438
    Test: m .
    Flag: EXEMPT, IWYU
    
    Change-Id: I1cc889fdd7d2c8f59e0e0b0f850f9de3b0655478

--- system/stack/test/sdp/stack_sdp_test.cc
+++ system/stack/test/sdp/stack_sdp_test.cc
@@ -21,8 +21,6 @@
 #include <cstddef>
 
 #include "osi/include/allocator.h"
-#include "stack/include/bt_uuid16.h"
-#include "stack/include/sdpdefs.h"
 #include "stack/sdp/internal/sdp_api.h"
 #include "stack/sdp/sdpint.h"
 #include "test/mock/mock_osi_allocator.h"
