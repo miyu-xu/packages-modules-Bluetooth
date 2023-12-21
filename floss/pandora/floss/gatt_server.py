7781f909428edbfb4678b07ba2fe518f4f597528
Saba Sarabta <saba.sarabta@exalt.ps>
2023-12-21 15:37:14 +0200

    Floss: Implement Pandora GATT profile (part2).
    Bug: 301000123, Bug: 300942866, Bug: 300942867, Bug: 300954033, Bug: 300954035, Bug: 301000124
    Test: mma packages/modules/Bluetooth && pts-bot GATT
    Change-Id: I36965d425321368b6ae6d9b886c4a52924bc2d80

--- floss/pandora/floss/gatt_server.py
+++ floss/pandora/floss/gatt_server.py
@@ -18,6 +18,7 @@ import logging
 from floss.pandora.floss import floss_enums
 from floss.pandora.floss import observer_base
 from floss.pandora.floss import utils
+from gi.repository import GLib
 
 
 class GattServerCallbacks:
@@ -508,6 +509,8 @@ class FlossGattServer(GattServerCallbacks):
         self.callbacks = self.ExportedGattServerCallbacks()
         self.callbacks.add_observer('gatt_testing_server', self)
         self.bus.register_object(self.cb_dbus_objpath, self.callbacks, None)
+        self.server_connect_id = None
+        self.server_id = None
 
     def __del__(self):
         """Destructor."""
@@ -526,6 +529,7 @@ class FlossGattServer(GattServerCallbacks):
         if status != floss_enums.GattStatus.SUCCESS:
             logging.error('Failed to register server with id: %s, status = %s', server_id, status)
             return
+        self.server_id = server_id
 
     @utils.glib_callback()
     def on_server_connection_state(self, server_id, connected, addr):
@@ -536,8 +540,8 @@ class FlossGattServer(GattServerCallbacks):
             connected: A boolean value that indicates whether the GATT server is connected.
             addr: Remote device MAC address.
         """
-        logging.debug('on_server_connection_state: server_id: %s, connection_state: %s, device address: %s',
-                      server_id, connected, addr)
+        logging.debug('on_server_connection_state: server_id: %s, connection_state: %s, device address: %s', server_id,
+                      connected, addr)
 
     @utils.glib_callback()
     def on_service_added(self, status, service):
@@ -571,8 +575,9 @@ class FlossGattServer(GattServerCallbacks):
                      ATT PDU.
             handle: The characteristic handle.
         """
-        logging.debug('on_characteristic_read_request: device address: %s, trans_id: %s, offset: %s, is_long: %s, '
-                      'handle: %s', addr, trans_id, offset, is_long, handle)
+        logging.debug(
+            'on_characteristic_read_request: device address: %s, trans_id: %s, offset: %s, is_long: %s, handle: %s',
+            addr, trans_id, offset, is_long, handle)
 
     @utils.glib_callback()
     def on_descriptor_read_request(self, addr, trans_id, offset, is_long, handle):
@@ -586,8 +591,9 @@ class FlossGattServer(GattServerCallbacks):
                      ATT PDU.
             handle: The descriptor handle.
         """
-        logging.debug('on_descriptor_read_request: device address: %s, trans_id: %s, offset: %s, is_long: %s, '
-                      'handle: %s', addr, trans_id, offset, is_long, handle)
+        logging.debug(
+            'on_descriptor_read_request: device address: %s, trans_id: %s, offset: %s, is_long: %s, handle: %s', addr,
+            trans_id, offset, is_long, handle)
 
     @utils.glib_callback()
     def on_characteristic_write_request(self, addr, trans_id, offset, len, is_prep, need_rsp, handle, value):
@@ -603,9 +609,9 @@ class FlossGattServer(GattServerCallbacks):
             handle: The characteristic handle.
             value: The value that should be written to the attribute.
         """
-        logging.debug('on_characteristic_write_request: device address: %s, trans_id: %s, offset: %s, length: %s, '
-                      'is_prep: %s, need_rsp: %s, handle: %s, values: %s', addr, trans_id, offset, len, is_prep,
-                      need_rsp, handle, value)
+        logging.debug(
+            'on_characteristic_write_request: device address: %s, trans_id: %s, offset: %s, length: %s, is_prep: %s, '
+            'need_rsp: %s, handle: %s, values: %s', addr, trans_id, offset, len, is_prep, need_rsp, handle, value)
 
     @utils.glib_callback()
     def on_descriptor_write_request(self, addr, trans_id, offset, len, is_prep, need_rsp, handle, value):
@@ -621,9 +627,9 @@ class FlossGattServer(GattServerCallbacks):
             handle: The descriptor handle.
             value: The value that should be written to the descriptor.
         """
-        logging.debug('on_descriptor_write_request: device address: %s, trans_id: %s, offset: %s, length: %s, '
-                      'is_prep: %s, need_rsp: %s, handle: %s, values: %s', addr, trans_id, offset, len, is_prep,
-                      need_rsp, handle, value)
+        logging.debug(
+            'on_descriptor_write_request: device address: %s, trans_id: %s, offset: %s, length: %s, is_prep: %s, '
+            'need_rsp: %s, handle: %s, values: %s', addr, trans_id, offset, len, is_prep, need_rsp, handle, value)
 
     @utils.glib_callback()
     def on_execute_write(self, addr, trans_id, exec_write):
@@ -634,8 +640,7 @@ class FlossGattServer(GattServerCallbacks):
             trans_id: Transaction id.
             exec_write: A boolean value that indicates whether the write operation should be executed or canceled.
         """
-        logging.debug('on_execute_write: device address: %s, trans_id: %s, exec_write: %s', addr, trans_id,
-                      exec_write)
+        logging.debug('on_execute_write: device address: %s, trans_id: %s, exec_write: %s', addr, trans_id, exec_write)
 
     @utils.glib_callback()
     def on_notification_sent(self, addr, status):
@@ -667,8 +672,8 @@ class FlossGattServer(GattServerCallbacks):
             rx_phy: The new RX PHY for the connection.
             status: floss_enums.GattStatus.
         """
-        logging.debug('on_phy_update: device address: %s, tx_phy: %s, rx_phy: %s, status: %s', addr, tx_phy,
-                      rx_phy, status)
+        logging.debug('on_phy_update: device address: %s, tx_phy: %s, rx_phy: %s, status: %s', addr, tx_phy, rx_phy,
+                      status)
 
     @utils.glib_callback()
     def on_phy_read(self, addr, tx_phy, rx_phy, status):
@@ -680,8 +685,8 @@ class FlossGattServer(GattServerCallbacks):
             rx_phy: The current receive PHY for the connection.
             status: floss_enums.GattStatus.
         """
-        logging.debug('on_phy_read: device address: %s, tx_phy: %s, rx_phy: %s, status: %s', addr, tx_phy,
-                      rx_phy, status)
+        logging.debug('on_phy_read: device address: %s, tx_phy: %s, rx_phy: %s, status: %s', addr, tx_phy, rx_phy,
+                      status)
 
     @utils.glib_callback()
     def on_connection_updated(self, addr, interval, latency, timeout, status):
@@ -694,8 +699,8 @@ class FlossGattServer(GattServerCallbacks):
             timeout: Supervision timeout for this connection in milliseconds.
             status: floss_enums.GattStatus.
         """
-        logging.debug('on_connection_updated: device address: %s, interval: %s, latency: %s, timeout: %s, '
-                      'status: %s', addr, interval, latency, timeout, status)
+        logging.debug('on_connection_updated: device address: %s, interval: %s, latency: %s, timeout: %s, status: %s',
+                      addr, interval, latency, timeout, status)
 
     @utils.glib_callback()
     def on_subrate_change(self, addr, subrate_factor, latency, cont_num, timeout, status):
@@ -709,8 +714,9 @@ class FlossGattServer(GattServerCallbacks):
             timeout: Supervision timeout for this connection in milliseconds.
             status: floss_enums.GattStatus.
         """
-        logging.debug('on_subrate_change: device address: %s, subrate_factor: %s, latency: %s, cont_num: %s, '
-                      'timeout: %s, status: %s', addr, subrate_factor, latency, cont_num, timeout, status)
+        logging.debug(
+            'on_subrate_change: device address: %s, subrate_factor: %s, latency: %s, cont_num: %s, timeout: %s, '
+            'status: %s', addr, subrate_factor, latency, cont_num, timeout, status)
 
     @utils.glib_call(False)
     def has_proxy(self):
@@ -736,24 +742,20 @@ class FlossGattServer(GattServerCallbacks):
         return True
 
     @utils.glib_call(False)
-    def unregister_server(self, server_id):
+    def unregister_server(self):
         """Unregisters GATT server for this client.
-
-        Args:
-            server_id: Bluetooth GATT server id.
 
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().UnregisterServer(server_id)
+        self.proxy().UnregisterServer(self.server_id)
         return True
 
     @utils.glib_call(None)
-    def server_connect(self, server_id, addr, is_direct, transport):
+    def server_connect(self, addr, is_direct, transport):
         """Connects remote device to GATT server.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
             is_direct: A boolean value that specifies whether the connection should be made using direct connection.
             transport: BtTransport type.
@@ -761,23 +763,22 @@ class FlossGattServer(GattServerCallbacks):
         Returns:
             Server connect as boolean on success, None otherwise.
         """
-        return self.proxy().ServerConnect(server_id, addr, is_direct, transport)
+        return self.proxy().ServerConnect(self.server_id, addr, is_direct, transport)
 
     @utils.glib_call(None)
-    def server_disconnect(self, server_id, addr):
+    def server_disconnect(self, addr):
         """Disconnects remote device from the GATT server.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
 
         Returns:
             Server disconnect as boolean on success, None otherwise.
         """
-        return self.proxy().ServerDisconnect(server_id, addr)
+        return self.proxy().ServerDisconnect(self.server_id, addr)
 
     @utils.glib_call(False)
-    def add_service(self, server_id, service):
+    def add_service(self, service):
         """Adds GATT service.
 
         Args:
@@ -787,42 +788,37 @@ class FlossGattServer(GattServerCallbacks):
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().AddService(server_id, service)
+        self.proxy().AddService(self.server_id, service)
         return True
 
     @utils.glib_call(False)
-    def remove_service(self, server_id, handle):
+    def remove_service(self, handle):
         """Removes GATT service.
 
         Args:
-            server_id: Bluetooth GATT server id.
             handle: Service record handle.
 
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().RemoveService(server_id, handle)
+        self.proxy().RemoveService(self.server_id, handle)
         return True
 
     @utils.glib_call(False)
-    def clear_services(self, server_id):
+    def clear_services(self):
         """Clears GATT services.
-
-        Args:
-            server_id: Bluetooth GATT server id.
 
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().ClearServices(server_id)
+        self.proxy().ClearServices(self.server_id)
         return True
 
     @utils.glib_call(None)
-    def send_response(self, server_id, addr, request_id, status, offset, value):
+    def send_response(self, addr, request_id, status, offset, value):
         """Sends GATT response.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
             request_id: Request id.
             status: floss_enums.GattStatus.
@@ -832,14 +828,13 @@ class FlossGattServer(GattServerCallbacks):
         Returns:
             Response send as boolean on success, None otherwise.
         """
-        return self.proxy().SendResponse(server_id, addr, request_id, status, offset, value)
+        return self.proxy().SendResponse(self.server_id, addr, request_id, status, offset, value)
 
     @utils.glib_call(None)
-    def send_notification(self, server_id, addr, handle, confirm, value):
+    def send_notification(self, addr, handle, confirm, value):
         """Sends GATT notification.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
             handle: The attribute handle of the attribute to send the notification for.
             confirm: A boolean value indicating whether the client should send a confirmation in response to
@@ -849,14 +844,13 @@ class FlossGattServer(GattServerCallbacks):
         Returns:
             Notification send as boolean on success, None otherwise.
         """
-        return self.proxy().SendNotification(server_id, addr, handle, confirm, value)
+        return self.proxy().SendNotification(self.server_id, addr, handle, confirm, value)
 
     @utils.glib_call(False)
-    def server_set_preferred_phy(self, server_id, addr, tx_phy, rx_phy, phy_options):
+    def server_set_preferred_phy(self, addr, tx_phy, rx_phy, phy_options):
         """Sets preferred phy for server.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
             tx_phy: Preferred PHY for transmitting data.
             rx_phy: Preferred PHY for receiving data.
@@ -865,19 +859,110 @@ class FlossGattServer(GattServerCallbacks):
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().ServerSetPreferredPhy(server_id, addr, tx_phy, rx_phy, phy_options)
+        self.proxy().ServerSetPreferredPhy(self.server_id, addr, tx_phy, rx_phy, phy_options)
         return True
 
     @utils.glib_call(False)
-    def server_read_phy(self, server_id, addr):
+    def server_read_phy(self, addr):
         """Reads phy of server.
 
         Args:
-            server_id: Bluetooth GATT server id.
             addr: Remote device MAC address.
 
         Returns:
             True on success, False otherwise.
         """
-        self.proxy().ServerReadPhy(server_id, addr)
+        self.proxy().ServerReadPhy(self.server_id, addr)
         return True
+
+    def register_callback_observer(self, name, observer):
+        """Add an observer for all callbacks.
+
+        Args:
+            name: Name of the observer.
+            observer: Observer that implements all callback classes.
+        """
+        if isinstance(observer, GattServerCallbacks):
+            self.callbacks.add_observer(name, observer)
+
+    def unregister_callback_observer(self, name, observer):
+        """Remove an observer for all callbacks.
+
+        Args:
+            name: Name of the observer.
+            observer: Observer that implements all callback classes.
+        """
+        if isinstance(observer, GattServerCallbacks):
+            self.callbacks.remove_observer(name, observer)
+
+    def make_dbus_descriptor(self, uuid, instance_id, permissions):
+        """Makes struct for descriptor D-Bus.
+
+        Args:
+            uuid : Descriptor UUID as array of bytes.
+            instance_id: Descriptor identifier.
+            permissions: Descriptor permissions.
+
+        Returns:
+            Dictionary of descriptor.
+        """
+        return {
+            'uuid': GLib.Variant('ay', uuid),
+            'instance_id': GLib.Variant('i', instance_id),
+            'permissions': GLib.Variant('i', permissions)
+        }
+
+    def make_dbus_characteristic(self, uuid, instance_id, properties, permissions, key_size, write_type, descriptors):
+        """Makes struct for characteristic D-Bus.
+
+        Args:
+            uuid : Characteristic UUID as array of bytes.
+            instance_id: Characteristic handle id.
+            properties: Characteristic properties.
+            permissions: Characteristic permissions.
+            key_size: Characteristic key size.
+            write_type: Characteristic write type.
+            descriptors: Characteristic descriptors.
+
+        Returns:
+            Dictionary of characteristic.
+        """
+        desc = []
+        for d in descriptors:
+            desc.append(self.make_dbus_descriptor(d['uuid'], d['instance_id'], d['permissions']))
+        return {
+            'uuid': GLib.Variant('ay', uuid),
+            'instance_id': GLib.Variant('i', instance_id),
+            'properties': GLib.Variant('i', properties),
+            'permissions': GLib.Variant('i', permissions),
+            'key_size': GLib.Variant('i', key_size),
+            'write_type': GLib.Variant('u', write_type),
+            'descriptors': GLib.Variant('aa{sv}', desc)
+        }
+
+    def make_dbus_service(self, service):
+        """Makes struct for service D-Bus.
+
+        Args:
+            service: The struct of BluetoothGattService.
+
+        Returns:
+            Dictionary of service.
+        """
+        characteristics = []
+        for c in service["characteristics"]:
+            characteristics.append(
+                self.make_dbus_characteristic(c['uuid'], c['instance_id'], c['properties'], c['permissions'],
+                                              c['key_size'], c['write_type'], c['descriptors']))
+
+        included_services = []
+        for s in service["included_services"]:
+            included_services.append(self.make_dbus_service(s))
+
+        return {
+            'uuid': GLib.Variant('ay', service['uuid']),
+            'instance_id': GLib.Variant('i', service['instance_id']),
+            'service_type': GLib.Variant('i', service['service_type']),
+            'characteristics': GLib.Variant('aa{sv}', characteristics),
+            'included_services': GLib.Variant('aa{sv}', included_services)
+        }
