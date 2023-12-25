2c710acc57078c280ad95df214feb215b5285df7
Mohammad Sabri <mohammad.kh.sabri@exalt.ps>
2023-12-25 10:53:09 +0200

    Floss: Implement Pandora HFP profile (part1).
    Bug: 300954040, Bug: 301036159, Bug: 300954041, Bug: 300954043, Bug: 300954042, Bug: 300942872, Bug: 300942873, Bug:301000128, Bug: 300954044, Bug: 301000132, Bug: 301000135
    Test: mma packages/modules/Bluetooth && pts-bot HFP
    Change-Id: I76df131eeb6ccd18d8a2e5b324f76af571c6e805

--- floss/pandora/server/server.py
+++ floss/pandora/server/server.py
@@ -18,6 +18,7 @@ import logging
 
 from floss.pandora.server import bluetooth as bluetooth_module
 from floss.pandora.server import gatt
+from floss.pandora.server import hfp
 from floss.pandora.server import hid
 from floss.pandora.server import host
 from floss.pandora.server import l2cap
@@ -28,6 +29,7 @@ import grpc
 from pandora import host_grpc_aio
 from pandora import security_grpc_aio
 from pandora_experimental import gatt_grpc_aio
+from pandora_experimental import hfp_grpc_aio
 from pandora_experimental import hid_grpc_aio
 from pandora_experimental import l2cap_grpc_aio
 from pandora_experimental import modem_grpc_aio
@@ -60,6 +62,9 @@ async def serve(port):
 
             modem_service = modem.Modem(bluetooth)
             modem_grpc_aio.add_ModemServicer_to_server(modem_service, server)
+
+            hfp_service = hfp.HFPService(bluetooth)
+            hfp_grpc_aio.add_HFPServicer_to_server(hfp_service, server)
 
             hid_service = hid.HIDService(bluetooth)
             hid_grpc_aio.add_HIDServicer_to_server(hid_service, server)
