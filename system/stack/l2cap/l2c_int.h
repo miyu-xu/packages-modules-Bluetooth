d5937a821415ff633be804a79e045f4dd8095f39
Jakub Pawlowski <jpawlowski@google.com>
2024-01-05 19:34:17 +0100

    Faster LE Audio device pairing
    Test: Bond wiht LE Audio capable device, note profile connection time
          reduction.
    Bug: 296836982
    Change-Id: Id238ca459333e82581cf71952ce8abc9a2b8aafe

--- system/stack/l2cap/l2c_int.h
+++ system/stack/l2cap/l2c_int.h
@@ -526,6 +526,9 @@ typedef struct t_l2c_linkcb {
 
   uint8_t conn_update_mask;
 
+  bool conn_update_blocked_by_service_discovery;
+  bool conn_update_blocked_by_audio_setup;
+
   uint16_t min_interval; /* parameters as requested by peripheral */
   uint16_t max_interval;
   uint16_t latency;
