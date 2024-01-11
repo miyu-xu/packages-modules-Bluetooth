4d26eb4c4c94600e65870b6299c3bb34b0742e92
shihchienc <shihchienc@google.com>
2024-01-11 13:08:57 +0000

    Avoid scanning if device is discovered but not connected
    If the device is connected but not yet add to group since sirk exchange
    is not completed, we cannot start active scan because this device would
    be in the group eventually.
    
    Test: atest -c bluetooth_csis_test
    Bug: 315392056
    Change-Id: Ibbb0a8e98bcf475b4f294b1fbba6bf135606c23a

--- system/bta/csis/csis_client.cc
+++ system/bta/csis/csis_client.cc
@@ -902,7 +902,10 @@ class CsisClientImpl : public CsisClient {
         auto g = FindCsisGroup(group_id_to_discover);
         LOG_DEBUG("Group size  %d  target size %d", g->GetDesiredSize(),
                   g->GetCurrentSize());
-        if (g->GetDesiredSize() > g->GetCurrentSize()) {
+
+        if (g->GetDesiredSize() >
+            g->GetCurrentSize() + GetNumOfKnownExpectedDevicesWaitingForBonding(
+                                      g->GetGroupId())) {
           CsisActiveDiscovery(g);
         }
       }
@@ -1302,6 +1305,14 @@ class CsisClientImpl : public CsisClient {
     }
 
     return std::move(devices);
+  }
+
+  int GetNumOfKnownExpectedDevicesWaitingForBonding(int group_id) {
+    return std::count_if(
+        devices_.begin(), devices_.end(), [group_id](const auto& device) {
+          return device->GetExpectedGroupIdMember() == group_id &&
+                 !device->GetCsisInstanceByGroupId(group_id);
+        });
   }
 
   void CacheAndAdvertiseExpectedMember(const RawAddress& address,
