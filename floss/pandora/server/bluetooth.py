ec42a2092f5bd391c760fbf5950cefd6e6b6e66f
Nagham Masalmah <nagham.masalmah@exalt.corp-partner.google.com>
2024-01-15 16:02:27 +0200

    Pandora: Implements a2dp gRPC service methods (testing)
    Change-Id: Id1027081029625e637ac3b995ed46887018128bc

--- floss/pandora/server/bluetooth.py
+++ floss/pandora/server/bluetooth.py
@@ -20,9 +20,11 @@ import traceback
 from floss.pandora.floss import adapter_client
 from floss.pandora.floss import advertising_client
 from floss.pandora.floss import manager_client
+from floss.pandora.floss import media_client
 from floss.pandora.floss import qa_client
 from floss.pandora.floss import scanner_client
 from floss.pandora.floss import gatt_client
+from floss.pandora.floss import gatt_server
 from floss.pandora.floss import floss_enums
 from floss.pandora.floss import utils
 from gi.repository import GLib
@@ -65,6 +67,9 @@ class Bluetooth(object):
         self.scanner_client = scanner_client.FlossScannerClient(self.bus, self.DEFAULT_ADAPTER)
         self.qa_client = qa_client.FlossQAClient(self.bus, self.DEFAULT_ADAPTER)
         self.gatt_client = gatt_client.FlossGattClient(self.bus, self.DEFAULT_ADAPTER)
+        self.gatt_server = gatt_server.FlossGattServer(self.bus, self.DEFAULT_ADAPTER)
+        self.media_client = media_client.FlossMediaClient(self.bus, self.DEFAULT_ADAPTER)
+
 
     def __del__(self):
         if not self.is_clean:
@@ -132,6 +137,12 @@ class Bluetooth(object):
         if not self.gatt_client.register_client(self.FAKE_GATT_APP_ID, False):
             logging.error('gatt_client: Failed to register callbacks')
             return False
+        if not self.gatt_server.register_server(self.FAKE_GATT_APP_ID, False):
+            logging.error('gatt_server: Failed to register callbacks')
+            return False
+        if not self.media_client.register_callback():
+            logging.error('media_client: Failed to register callbacks')
+            return False
         return True
 
     def is_bluetoothd_proxy_valid(self):
@@ -144,6 +155,8 @@ class Bluetooth(object):
             self.scanner_client.has_proxy(),
             self.qa_client.has_proxy(),
             self.gatt_client.has_proxy(),
+            self.gatt_server.has_proxy(),
+            self.media_client.has_proxy(),
         ])
 
         if not proxy_ready:
@@ -178,6 +191,8 @@ class Bluetooth(object):
             self.scanner_client = scanner_client.FlossScannerClient(self.bus, default_adapter)
             self.qa_client = qa_client.FlossQAClient(self.bus, default_adapter)
             self.gatt_client = gatt_client.FlossGattClient(self.bus, default_adapter)
+            self.gatt_server = gatt_server.FlossGattServer(self.bus, default_adapter)
+            self.media_client = media_client.FlossMediaClient(self.bus, default_adapter)
 
             try:
                 utils.poll_for_condition(
@@ -297,6 +312,45 @@ class Bluetooth(object):
 
     def set_hid_report(self, addr, report_type, report):
         return self.qa_client.set_hid_report(addr, report_type, report)
+
+    def read_characteristic(self, address, handle, auth_re):
+        return self.gatt_client.read_characteristic(address, handle, auth_re)
+
+    def read_descriptor(self, address, handle, auth_req):
+        return self.gatt_client.read_descriptor(address, handle, auth_req)
+
+    def discover_services(self, address):
+        return self.gatt_client.discover_services(address)
+
+    def get_bond_state(self, address):
+        self.adapter_client.get_bond_state(address)
+
+    def fetch_remote(self, address):
+        return self.adapter_client.fetch_remote_uuids(address)
+
+    def get_remote_uuids(self, address):
+        return self.adapter_client.get_remote_property(address, 'Uuids')
+
+    def btif_gattc_discover_service_by_uuid(self, address, uuid):
+        return self.gatt_client.btif_gattc_discover_service_by_uuid(address, uuid)
+
+    def configure_mtu(self, address, mtu):
+        return self.gatt_client.configure_mtu(address, mtu)
+
+    def refresh_device(self, address):
+        return self.gatt_client.refresh_device(address)
+
+    def write_descriptor(self, address, handle, auth_req, value):
+        return self.gatt_client.write_descriptor(address, handle, auth_req, value)
+
+    def write_characteristic(self, address, handle, write_type, auth_req, value):
+        return self.gatt_client.write_characteristic(address, handle, write_type, auth_req, value)
+
+    def get_connected_audio_devices(self):
+        return self.media_client.devices
+
+    def media_disconnect(self, address):
+        return self.media_client.disconnect(address)
 
     def gatt_connect(self, address, is_direct, transport):
         return self.gatt_client.connect_client(address, is_direct, transport)
