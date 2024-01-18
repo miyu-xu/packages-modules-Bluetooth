87f6f25ca1501d0dfb8d8195baae4308c29c3bb3
Hsin-chen Chuang <chharry@google.com>
2024-01-18 17:34:48 +0800

    btm_sco_hfp_hal_linux: Refactor MGMT command to provide only driver info
    The MGMT_OP_GET_SCO_CODEC_CAPABILITIES command actually relies on some
    codec info that's cached in kernel space, while the info is considered
    as memory leak and removed by https://crrev.com/c/4918531.
    
    Since Floss can send the HCI commands directly, refactor the MGMT
    command to provide only the info that can't be queried by HCI commands,
    that is, the driver info. In HAL, mimic the original behavior of the
    MGMT command.
    
    Bug: 316077719
    Tag: #floss
    Test: mmm packages/modules/Bluetooth
    Test: Manual test on Corsola, WBS headset works
    Test: bluetooth_AdapterAUHealth.au_hfp_wbs_dut_as_source_test.floss
    Flag: EXEMPT floss only changes
    Change-Id: If3f07b4aa6f2a7b9709aa3ad5b845fa59b17e75b

--- system/device/include/controller.h
+++ system/device/include/controller.h
@@ -120,6 +120,7 @@ typedef struct controller_t {
   uint8_t (*get_ble_resolving_list_max_size)(void);
   void (*set_ble_resolving_list_max_size)(int resolving_list_max_size);
   uint8_t* (*get_local_supported_codecs)(uint8_t* number_of_codecs);
+  std::vector<uint8_t> (*GetLocalSupportedBrEdrCodecIds)(void);
   uint8_t (*get_le_all_initiating_phys)(void);
   uint8_t (*clear_event_filter)(void);
   uint8_t (*clear_event_mask)(void);
