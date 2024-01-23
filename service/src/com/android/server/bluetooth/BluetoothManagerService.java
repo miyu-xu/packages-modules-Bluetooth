2a3b1929b862926e0965017ff03a13e5b57367c4
William Escande <wescande@google.com>
2024-01-22 18:31:31 -0800

    SystemServer: enable sync pre-impl
    ServiceMessenger Explanation:
    Since the messenger will be using the correct looper, it can directly
    call the sync method from the ManagerService.
    ~
    This CL is adding enable sync method
    ~
    
    Bug: 288450479
    Bug: 321804999
    Test: m service-bluetooth | No-op change
          Flag testing will be done in follow-up, before flag rollout
    Change-Id: I686b76398672a5ea885454aad0f616dc2e02a4a5

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1222,32 +1222,57 @@ class BluetoothManagerService {
                         + (" isBinding=" + isBinding())
                         + (" mState=" + mState));
 
-        if (isSatelliteModeOn()) {
-            Log.d(TAG, "enable: not enabling - satellite mode is on.");
-            return false;
-        }
-
         synchronized (mReceiver) {
-            mQuietEnableExternal = false;
-            mEnableExternal = true;
-            if (!mUseNewAirplaneMode) {
-                mBluetoothAirplaneModeListener.notifyUserToggledBluetooth(true);
-            } else {
-                // TODO(b/288450479): Remove clearCallingIdentity when threading is fixed
-                final long callingIdentity = Binder.clearCallingIdentity();
-                try {
-                    AirplaneModeListener.notifyUserToggledBluetooth(
-                            mContentResolver, mCurrentUserContext, true);
-                } finally {
-                    Binder.restoreCallingIdentity(callingIdentity);
-                }
+            if (!canEnable()) {
+                return false;
             }
+
             sendEnableMsg(
                     false,
                     BluetoothProtoEnums.ENABLE_DISABLE_REASON_APPLICATION_REQUEST,
                     packageName);
         }
         return true;
+    }
+
+    boolean enable_sync(String packageName) {
+        Log.i(
+                TAG,
+                ("enable_sync(" + packageName + "):")
+                        + (" mAdapter=" + mAdapter)
+                        + (" isBinding=" + isBinding())
+                        + (" mState=" + mState));
+
+        if (!canEnable()) {
+            return false;
+        }
+
+        addActiveLog(
+                BluetoothProtoEnums.ENABLE_DISABLE_REASON_APPLICATION_REQUEST, packageName, true);
+        mLastEnabledTime = SystemClock.elapsedRealtime();
+        handleEnableMessage(0, 0);
+    }
+
+    private boolean canEnable() {
+        if (isSatelliteModeOn()) {
+            Log.d(TAG, "enable: not enabling - satellite mode is on.");
+            return false;
+        }
+
+        mQuietEnableExternal = false;
+        mEnableExternal = true;
+        if (!mUseNewAirplaneMode) {
+            mBluetoothAirplaneModeListener.notifyUserToggledBluetooth(true);
+        } else {
+            // TODO(b/288450479): Remove clearCallingIdentity when threading is fixed
+            final long callingIdentity = Binder.clearCallingIdentity();
+            try {
+                AirplaneModeListener.notifyUserToggledBluetooth(
+                        mContentResolver, mCurrentUserContext, true);
+            } finally {
+                Binder.restoreCallingIdentity(callingIdentity);
+            }
+        }
     }
 
     boolean disable(String packageName, boolean persist) {
