a3821d42e72bb4d682b0207190aa35dcc6eea84e
Ömer Faruk Yılmaz <ofy@google.com>
2024-01-24 14:46:51 +0000

    Move PeriodicScanManager from package `gatt` to `le_scan`
    See go/scan-manager-refactor for more details.
    
    Test: m com.android.btservices
    Bug: 313335632
    Bug: 267361243
    Change-Id: I1fe2f74a9e625c9ede5af3c9a853f1ae8b6918b5

--- android/app/tests/unit/src/com/android/bluetooth/gatt/PeriodicScanManagerTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/gatt/PeriodicScanManagerTest.java
@@ -39,6 +39,7 @@ import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.bluetooth.btservice.AdapterService;
+import com.android.bluetooth.le_scan.PeriodicScanManager;
 
 import org.junit.After;
 import org.junit.Before;
