faa26d6887d196e688936f3421a8130bb5899df6
Myles Watson <mylesgw@google.com>
2024-01-25 10:03:35 -0800

    Use Rand instead of local name to synchronize
    Bug: 321273521
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I8eb160af051ea8b69cc988f065e87a4c01f6a72b

--- system/test/mock/mock_stack_btm_interface.cc
+++ system/test/mock/mock_stack_btm_interface.cc
@@ -238,8 +238,6 @@ struct btm_client_interface_t default_btm_client_interface = {
         },
     .local =
         {
-            .BTM_ReadLocalDeviceNameFromController =
-                BTM_ReadLocalDeviceNameFromController,
             .BTM_SetLocalDeviceName = BTM_SetLocalDeviceName,
             .BTM_SetDeviceClass = BTM_SetDeviceClass,
             .BTM_IsDeviceUp = BTM_IsDeviceUp,
