d39d653061cdec57137630ea8474ee79b149a787
Myles Watson <mylesgw@google.com>
2024-01-31 10:28:45 -0800

    Use PDL for user passkey reply
    Bug: 322230000
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I1c0333450c1c0afdb3d7f51c049ce8b6bd4774dd

--- system/stack/hcic/hcicmds.cc
+++ system/stack/hcic/hcicmds.cc
@@ -1298,22 +1298,6 @@ void btsnd_hcic_read_local_oob_data(void) {
   btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
 }
 
-void btsnd_hcic_user_passkey_reply(const RawAddress& bd_addr, uint32_t value) {
-  BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
-  uint8_t* pp = (uint8_t*)(p + 1);
-
-  p->len = HCIC_PREAMBLE_SIZE + HCIC_PARAM_SIZE_U_PKEY_REPLY;
-  p->offset = 0;
-
-  UINT16_TO_STREAM(pp, HCI_USER_PASSKEY_REQ_REPLY);
-  UINT8_TO_STREAM(pp, HCIC_PARAM_SIZE_U_PKEY_REPLY);
-
-  BDADDR_TO_STREAM(pp, bd_addr);
-  UINT32_TO_STREAM(pp, value);
-
-  btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
-}
-
 void btsnd_hcic_rem_oob_reply(const RawAddress& bd_addr, const Octet16& c,
                               const Octet16& r) {
   BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
