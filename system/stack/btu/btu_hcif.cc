3496d2323076571ed877c3c64b2800697ec332f7
Myles Watson <mylesgw@google.com>
2024-01-31 09:11:41 -0800

    Use PDL for btm_sec create_conn_cancel
    Bug: 322230000
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I377828121711fdc32aa83b9bdecfdce58d501837

--- system/stack/btu/btu_hcif.cc
+++ system/stack/btu/btu_hcif.cc
@@ -101,8 +101,6 @@ static void btu_hcif_proc_sp_req_evt(tBTM_SP_EVT event, const uint8_t* p);
 static void btu_hcif_rem_oob_req(const uint8_t* p);
 static void btu_hcif_simple_pair_complete(const uint8_t* p);
 static void btu_hcif_proc_sp_req_evt(const tBTM_SP_EVT event, const uint8_t* p);
-static void btu_hcif_create_conn_cancel_complete(const uint8_t* p,
-                                                 uint16_t evt_len);
 static void btu_hcif_read_local_oob_complete(const uint8_t* p,
                                              uint16_t evt_len);
 
@@ -375,7 +373,6 @@ static void btu_hcif_log_command_metrics(uint16_t opcode, const uint8_t* p_cmd,
 
   switch (opcode) {
     case HCI_CREATE_CONNECTION:
-    case HCI_CREATE_CONNECTION_CANCEL:
       STREAM_TO_BDADDR(bd_addr, p_cmd);
       log_link_layer_connection_event(
           &bd_addr, bluetooth::common::kUnknownConnectionHandle,
@@ -943,10 +940,6 @@ static void btu_hcif_hdl_command_complete(uint16_t opcode, uint8_t* p,
       btm_read_tx_power_complete(p, evt_len, false);
       break;
 
-    case HCI_CREATE_CONNECTION_CANCEL:
-      btu_hcif_create_conn_cancel_complete(p, evt_len);
-      break;
-
     case HCI_READ_LOCAL_OOB_DATA:
       btu_hcif_read_local_oob_complete(p, evt_len);
       break;
@@ -1274,19 +1267,6 @@ void btu_hcif_proc_sp_req_evt(tBTM_SP_EVT event, const uint8_t* p) {
       break;
   }
   btm_proc_sp_req_evt(event, bda, value);
-}
-void btu_hcif_create_conn_cancel_complete(const uint8_t* p, uint16_t evt_len) {
-  uint8_t status;
-
-  if (evt_len < 1 + BD_ADDR_LEN) {
-    log::error("malformatted event packet, too short");
-    return;
-  }
-
-  STREAM_TO_UINT8(status, p);
-  RawAddress bd_addr;
-  STREAM_TO_BDADDR(bd_addr, p);
-  btm_create_conn_cancel_complete(status, bd_addr);
 }
 void btu_hcif_read_local_oob_complete(const uint8_t* p, uint16_t evt_len) {
   tBTM_SP_LOC_OOB evt_data;
