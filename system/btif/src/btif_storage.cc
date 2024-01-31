7579576d4bb1c2ef8ba64b8295905251c41ece53
William Escande <wescande@google.com>
2024-01-31 11:21:09 -0800

    stack storage: unify storage key gd X legacy
    Bug: 301978669
    Flag: Exempt, Mechanical refactor
    Test: m . | No-op change
    Change-Id: Icf87ed8a4e21a64a37157de2f889166b20f0d757

--- system/btif/src/btif_storage.cc
+++ system/btif/src/btif_storage.cc
@@ -48,6 +48,7 @@
 #include "btif_util.h"
 #include "core_callbacks.h"
 #include "device/include/controller.h"
+#include "gd/storage/config_keys.h"
 #include "internal_include/bt_target.h"
 #include "osi/include/allocator.h"
 #include "stack/include/bt_octets.h"
@@ -57,52 +58,6 @@
 
 using base::Bind;
 using bluetooth::Uuid;
-
-/*******************************************************************************
- *  Constants & Macros
- ******************************************************************************/
-
-// TODO(armansito): Find a better way than using a hardcoded path.
-#define BTIF_STORAGE_PATH_BLUEDROID "/data/misc/bluedroid"
-
-#define BTIF_STORAGE_SECTION_ADAPTER "Adapter"
-
-#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_IR "LE_LOCAL_KEY_IR"
-#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_IRK "LE_LOCAL_KEY_IRK"
-#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_DHK "LE_LOCAL_KEY_DHK"
-#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_ER "LE_LOCAL_KEY_ER"
-
-#define BTIF_STORAGE_KEY_TIMESTAMP "Timestamp"
-#define BTIF_STORAGE_KEY_DEV_CLASS "DevClass"
-#define BTIF_STORAGE_KEY_DEV_TYPE "DevType"
-#define BTIF_STORAGE_KEY_NAME "Name"
-#define BTIF_STORAGE_KEY_APPEARANCE "Appearance"
-#define BTIF_STORAGE_KEY_ADDR_TYPE "AddrType"
-#define BTIF_STORAGE_KEY_ALIASE "Aliase"
-#define BTIF_STORAGE_KEY_SCANMODE "ScanMode"
-#define BTIF_STORAGE_KEY_LOCAL_IO_CAPS "LocalIOCaps"
-#define BTIF_STORAGE_KEY_DISC_TIMEOUT "DiscoveryTimeout"
-#define BTIF_STORAGE_KEY_GATT_CLIENT_SUPPORTED "GattClientSupportedFeatures"
-#define BTIF_STORAGE_KEY_GATT_CLIENT_DB_HASH "GattClientDatabaseHash"
-#define BTIF_STORAGE_KEY_GATT_SERVER_SUPPORTED "GattServerSupportedFeatures"
-#define BTIF_STORAGE_KEY_LINK_KEY "LinkKey"
-#define BTIF_STORAGE_KEY_LINK_KEY_TYPE "LinkKeyType"
-#define BTIF_STORAGE_KEY_PIN_LENGTH "PinLength"
-#define BTIF_STORAGE_KEY_LE_KEY_PENC "LE_KEY_PENC"
-#define BTIF_STORAGE_KEY_LE_KEY_PID "LE_KEY_PID"
-#define BTIF_STORAGE_KEY_LE_KEY_PCSRK "LE_KEY_PCSRK"
-#define BTIF_STORAGE_KEY_LE_KEY_LENC "LE_KEY_LENC"
-#define BTIF_STORAGE_KEY_LE_KEY_LCSRK "LE_KEY_LCSRK"
-#define BTIF_STORAGE_KEY_LE_KEY_LID "LE_KEY_LID"
-#define BTIF_STORAGE_KEY_VENDOR_ID_SOURCE "VendorIdSource"
-#define BTIF_STORAGE_KEY_VENDOR_ID "VendorId"
-#define BTIF_STORAGE_KEY_PRODUCT_ID "ProductId"
-#define BTIF_STORAGE_KEY_VERSION "ProductVersion"
-#define BTIF_STORAGE_KEY_RESTRICTED "Restricted"
-#define BTIF_STORAGE_KEY_ADDR_TYPE "AddrType"
-
-/* This is a local property to add a device found */
-#define BT_PROPERTY_REMOTE_DEVICE_TIMESTAMP 0xFF
 
 struct BtifStorageKey {
   uint8_t type;
---
+++ system/gd/storage/config_keys.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#define BTIF_STORAGE_PATH_BLUEDROID "/data/misc/bluedroid"
+
+#define BTIF_STORAGE_SECTION_ADAPTER "Adapter"
+
+#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_IR "LE_LOCAL_KEY_IR"
+#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_IRK "LE_LOCAL_KEY_IRK"
+#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_DHK "LE_LOCAL_KEY_DHK"
+#define BTIF_STORAGE_KEY_LE_LOCAL_KEY_ER "LE_LOCAL_KEY_ER"
+
+#define BTIF_STORAGE_KEY_TIMESTAMP "Timestamp"
+#define BTIF_STORAGE_KEY_DEV_CLASS "DevClass"
+#define BTIF_STORAGE_KEY_DEV_TYPE "DevType"
+#define BTIF_STORAGE_KEY_NAME "Name"
+#define BTIF_STORAGE_KEY_APPEARANCE "Appearance"
+#define BTIF_STORAGE_KEY_ADDR_TYPE "AddrType"
+#define BTIF_STORAGE_KEY_ALIASE "Aliase"
+#define BTIF_STORAGE_KEY_SCANMODE "ScanMode"
+#define BTIF_STORAGE_KEY_LOCAL_IO_CAPS "LocalIOCaps"
+#define BTIF_STORAGE_KEY_DISC_TIMEOUT "DiscoveryTimeout"
+#define BTIF_STORAGE_KEY_GATT_CLIENT_SUPPORTED "GattClientSupportedFeatures"
+#define BTIF_STORAGE_KEY_GATT_CLIENT_DB_HASH "GattClientDatabaseHash"
+#define BTIF_STORAGE_KEY_GATT_SERVER_SUPPORTED "GattServerSupportedFeatures"
+#define BTIF_STORAGE_KEY_LINK_KEY "LinkKey"
+#define BTIF_STORAGE_KEY_LINK_KEY_TYPE "LinkKeyType"
+#define BTIF_STORAGE_KEY_PIN_LENGTH "PinLength"
+#define BTIF_STORAGE_KEY_LE_KEY_PENC "LE_KEY_PENC"
+#define BTIF_STORAGE_KEY_LE_KEY_PID "LE_KEY_PID"
+#define BTIF_STORAGE_KEY_LE_KEY_PCSRK "LE_KEY_PCSRK"
+#define BTIF_STORAGE_KEY_LE_KEY_LENC "LE_KEY_LENC"
+#define BTIF_STORAGE_KEY_LE_KEY_LCSRK "LE_KEY_LCSRK"
+#define BTIF_STORAGE_KEY_LE_KEY_LID "LE_KEY_LID"
+#define BTIF_STORAGE_KEY_VENDOR_ID_SOURCE "VendorIdSource"
+#define BTIF_STORAGE_KEY_VENDOR_ID "VendorId"
+#define BTIF_STORAGE_KEY_PRODUCT_ID "ProductId"
+#define BTIF_STORAGE_KEY_VERSION "ProductVersion"
+#define BTIF_STORAGE_KEY_RESTRICTED "Restricted"
+#define BTIF_STORAGE_KEY_ADDR_TYPE "AddrType"
+
+/* This is a local property to add a device found */
+#define BT_PROPERTY_REMOTE_DEVICE_TIMESTAMP 0xFF
+
