eb61bea263bbef4d824cefcda486414be0597430
Ben <benlawson@google.com>
2024-01-31 00:20:56 +0000

    BumbleBluetoothTests: Fix reconnectExistingClient flake
    Fix GattClientTest#reconnectExistingClient flake where
    the gatt callback was sometimes being called to notify
    of disconnection at the end of the test. The end of the
    test and the disconnection were racing. This is fixed
    by explicitly disconnecting at the end of the test.
    
    Bug: 322517596
    Flag: TEST_ONLY
    Test: atest BumbleBluetoothTests:android.bluetooth.GattClientTest
    Change-Id: I428a3c6230aa139fc0479c1fcb5b09dd97686f57

--- framework/tests/bumble/src/android/bluetooth/GattClientTest.java
+++ framework/tests/bumble/src/android/bluetooth/GattClientTest.java
@@ -27,7 +27,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockingDetails;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import android.bluetooth.le.BluetoothLeScanner;
 import android.content.Context;
@@ -45,15 +44,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.invocation.Invocation;
 
+import java.util.Collection;
+import java.util.UUID;
+
 import pandora.GattProto.GattCharacteristicParams;
 import pandora.GattProto.GattServiceParams;
 import pandora.GattProto.RegisterServiceRequest;
 import pandora.HostProto.AdvertiseRequest;
 import pandora.HostProto.AdvertiseResponse;
 import pandora.HostProto.OwnAddressType;
-
-import java.util.Collection;
-import java.util.UUID;
 
 @RunWith(AndroidJUnit4.class)
 public class GattClientTest {
@@ -134,8 +133,9 @@ public class GattClientTest {
         inOrder.verify(gattCallback, timeout(1000))
                 .onConnectionStateChange(any(), anyInt(), eq(BluetoothProfile.STATE_CONNECTED));
 
-        gatt.close();
-        verifyNoMoreInteractions(gattCallback);
+        gatt.disconnect();
+        inOrder.verify(gattCallback, timeout(1000))
+                .onConnectionStateChange(any(), anyInt(), eq(BluetoothProfile.STATE_DISCONNECTED));
     }
 
     @Test
