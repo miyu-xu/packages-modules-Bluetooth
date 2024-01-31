2ebc7912ddbe118d9258299bbe8e5076714ce06d
Myles Watson <mylesgw@google.com>
2024-01-31 09:32:58 -0800

    Use PDL for send_pin_code_req_reply
    Bug: 322230000
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: Icded99c824f5574536837b106257eabd331613d0

--- system/stack/include/hcimsgs.h
+++ system/stack/include/hcimsgs.h
@@ -84,10 +84,6 @@ void btsnd_hcic_link_key_neg_reply(const RawAddress& bd_addr);
 
 /* Link Key Request Neg Reply  */
 
-/* PIN Code Request Reply */
-void btsnd_hcic_pin_code_req_reply(const RawAddress& bd_addr,
-                                   uint8_t pin_code_len, PIN_CODE pin_code);
-
 /* Change Connection Type */
 void btsnd_hcic_change_conn_type(uint16_t handle, uint16_t packet_types);
 
