1af08748cc50ae2824ccc3a228fabecbfec4867e
Myles Watson <mylesgw@google.com>
2024-01-31 11:43:14 -0800

    USe PDL for connection reject
    Bug: 322230000
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: Iaa5ff0aed09ad414533b8781f373a5469f9750a8

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -166,6 +166,8 @@ static void send_read_local_oob_data();
 
 static void send_rem_oob_reply(const RawAddress& bd_addr, Octet16 c, Octet16 r);
 
+static void send_reject_conn_hack(const RawAddress& bd_addr, uint8_t reason);
+
 /* true - authenticated link key is possible */
 static const bool btm_sec_io_map[BTM_IO_CAP_MAX][BTM_IO_CAP_MAX] = {
     /*   OUT,    IO,     IN,     NONE */
@@ -1939,7 +1941,7 @@ void btm_sec_conn_req(const RawAddress& bda, const DEV_CLASS dc) {
 
     /* incoming connection from bonding device is rejected */
     btm_sec_cb.pairing_flags |= BTM_PAIR_FLAGS_REJECTED_CONNECT;
-    btsnd_hcic_reject_conn(bda, HCI_ERR_HOST_REJECT_DEVICE);
+    send_reject_conn_hack(bda, HCI_ERR_HOST_REJECT_DEVICE);
     return;
   }
 
@@ -5247,4 +5249,15 @@ static void send_rem_oob_reply(const RawAddress& bd_addr, Octet16 c,
       get_main_thread()->BindOnce(
           bluetooth::hci::check_complete<
               bluetooth::hci::RemoteOobDataRequestReplyCompleteView>));
+}
+
+static void send_reject_conn_hack(const RawAddress& bd_addr, uint8_t reason) {
+  // TODO: This command shouldn't be sent from here
+  bluetooth::shim::GetHciLayer()->EnqueueCommand(
+      bluetooth::hci::RejectConnectionRequestBuilder::Create(
+          bluetooth::ToGdAddress(bd_addr),
+          static_cast<bluetooth::hci::RejectConnectionReason>(reason)),
+      get_main_thread()->BindOnce(
+          bluetooth::hci::check_status<
+              bluetooth::hci::RejectConnectionRequestStatusView>));
 }
