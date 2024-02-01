61e11111faf8fe84e1eae576772587200aa4c615
Myles Watson <mylesgw@google.com>
2024-01-31 16:22:55 -0800

    Use PDL for LTK Request Negative reply
    Bug: 322230000
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: Ic2890456ebee68dc9833f6ab6162411c6df63d65

--- system/stack/hcic/hciblecmds.cc
+++ system/stack/hcic/hciblecmds.cc
@@ -275,21 +275,6 @@ void btsnd_hcic_ble_rand(base::Callback<void(BT_OCTET8)> cb) {
                                 std::move(cb)));
 }
 
-void btsnd_hcic_ble_ltk_req_neg_reply(uint16_t handle) {
-  BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
-  uint8_t* pp = (uint8_t*)(p + 1);
-
-  p->len = HCIC_PREAMBLE_SIZE + HCIC_PARAM_SIZE_LTK_REQ_NEG_REPLY;
-  p->offset = 0;
-
-  UINT16_TO_STREAM(pp, HCI_BLE_LTK_REQ_NEG_REPLY);
-  UINT8_TO_STREAM(pp, HCIC_PARAM_SIZE_LTK_REQ_NEG_REPLY);
-
-  UINT16_TO_STREAM(pp, handle);
-
-  btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
-}
-
 void btsnd_hcic_ble_receiver_test(uint8_t rx_freq) {
   BT_HDR* p = (BT_HDR*)osi_malloc(HCI_CMD_BUF_SIZE);
   uint8_t* pp = (uint8_t*)(p + 1);
