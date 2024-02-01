491773ce89812bbf05558352751e155be956511c
yamei du <yameidu@amazon.com>
2024-02-01 02:29:10 +0000

    btm_sec: Fix crash caused by uncleared authentication collision info
    Authentication collision info was not cleared correctly after
    authentication failure, the remaining data may cause crash in
    btm_sec_link_key_request() in furture.
    
    Bug: 323960893
    Bug: 327208896
    
    Test: m com.android.btservices
    Test: Manual | Simultaneously initiate classic Bluetooth pairing from
    both the local and remote devices, decline the pairing request on the
    local device, and then repeat the test.
    
    Change-Id: I93297db7b833430b165bd68c0319bd5e00e545fa

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -3034,7 +3034,17 @@ void btm_sec_auth_complete(uint16_t handle, tHCI_STATUS status) {
     return;
   }
 
-  btm_sec_cb.collision_start_time = 0;
+  if (IS_FLAG_ENABLED(clear_collision_state_on_pairing_complete)) {
+    if (p_dev_rec && btm_sec_cb.p_collided_dev_rec &&
+        p_dev_rec->bd_addr == btm_sec_cb.p_collided_dev_rec->bd_addr) {
+      btm_sec_cb.collision_start_time = 0;
+      btm_sec_cb.p_collided_dev_rec = NULL;
+      if (alarm_is_scheduled(btm_sec_cb.sec_collision_timer))
+        alarm_cancel(btm_sec_cb.sec_collision_timer);
+    }
+  } else {
+    btm_sec_cb.collision_start_time = 0;
+  }
 
   btm_restore_mode();
 
@@ -3783,6 +3793,16 @@ void btm_sec_disconnected(uint16_t handle, tHCI_REASON reason,
   /* clear unused flags */
   p_dev_rec->sm4 &= BTM_SM4_TRUE;
 
+  if (IS_FLAG_ENABLED(clear_collision_state_on_pairing_complete)) {
+    if (btm_sec_cb.p_collided_dev_rec &&
+        p_dev_rec->bd_addr == btm_sec_cb.p_collided_dev_rec->bd_addr) {
+      LOG_DEBUG("clear auth collision info after disconnection");
+      btm_sec_cb.collision_start_time = 0;
+      btm_sec_cb.p_collided_dev_rec = NULL;
+      if (alarm_is_scheduled(btm_sec_cb.sec_collision_timer))
+        alarm_cancel(btm_sec_cb.sec_collision_timer);
+    }
+  }
   /* If we are in the process of bonding we need to tell client that auth failed
    */
   const uint8_t old_pairing_flags = btm_sec_cb.pairing_flags;
