ec01e23c7d573f779535f83bcdbe4692789e7eb5
Jakub Tyszkowski <tyszkowski@google.com>
2024-02-05 08:16:20 +0000

    LeAudio: Improve group config strategy recalculation
    Currently the group strategy is always recalculated from the PAC record
    LTV map entries. This change adds the strategy caching to avoid recalcualtions
    on each GetGroupStrategy call, and potentially allows for other strategy
    recalculation methods, not related to AudioChannelCounts LTV type, which
    may be absent in the vendor codec PAC records.
    
    - recalculate group configuration trategy only when the cached
      value is invalidated
    - invalidate the group configuration strategy only when needed
      (group configuration is changed)
    - properly name the helper function (it is Sink only) to avoid
      misunderstanding and misuse
    
    Bug: 308427707
    Test: atest bluetooth_le_audio_test bluetooth_le_audio_client_test
    Flag: EXEMPT; refactor for the multi-codec feature, verified with unit tests
    Change-Id: Ic72f7d84eabf5e68e6abbb946be2e6fb31ca03d7

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -550,17 +550,11 @@ class LeAudioClientImpl : public LeAudioClient {
     if (group_conf_changed) {
       /* All the configurations should be recalculated for the new conditions */
       group->InvalidateCachedConfigurations();
+      group->InvalidateGroupStrategy();
       callbacks_->OnAudioConf(group->audio_directions_, group->group_id_,
                               group->snk_audio_locations_.to_ulong(),
                               group->src_audio_locations_.to_ulong(),
                               group->GetAvailableContexts().value());
-    }
-  }
-
-  void UpdateLocationsAndContextsAvailability(int group_id) {
-    LeAudioDeviceGroup* group = aseGroups_.FindById(group_id);
-    if (group) {
-      UpdateLocationsAndContextsAvailability(group);
     }
   }
 
@@ -641,7 +635,6 @@ class LeAudioClientImpl : public LeAudioClient {
     LeAudioDevice* leAudioDevice = leAudioDevices_.FindByAddress(address);
     LeAudioDeviceGroup* new_group;
     LeAudioDeviceGroup* old_group = nullptr;
-    int old_group_id = bluetooth::groups::kGroupUnknown;
 
     if (!leAudioDevice) {
       /* TODO This part possible to remove as this is to handle adding device to
@@ -659,7 +652,6 @@ class LeAudioClientImpl : public LeAudioClient {
     } else {
       if (leAudioDevice->group_id_ != bluetooth::groups::kGroupUnknown) {
         old_group = aseGroups_.FindById(leAudioDevice->group_id_);
-        old_group_id = old_group->group_id_;
       }
     }
 
@@ -706,11 +698,6 @@ class LeAudioClientImpl : public LeAudioClient {
     /* If device is connected and added to the group, lets read ASE states */
     if (leAudioDevice->conn_id_ != GATT_INVALID_CONN_ID)
       AseInitialStateReadRequest(leAudioDevice);
-
-    /* Group may be destroyed once moved its last node to new group, so don't
-     * use `old_group` pointer anymore.
-     * Removing node from group requires updating group context availability */
-    UpdateLocationsAndContextsAvailability(old_group_id);
 
     if (leAudioDevice->GetConnectionState() == DeviceConnectState::CONNECTED) {
       UpdateLocationsAndContextsAvailability(new_group);
@@ -1773,6 +1760,8 @@ class LeAudioClientImpl : public LeAudioClient {
        * PACs are updated.
        */
       if (group) {
+        /* Changes in PAC record channel counts may change the strategy */
+        group->InvalidateGroupStrategy();
         group->InvalidateCachedConfigurations();
       }
       if (notify) {
@@ -1798,6 +1787,8 @@ class LeAudioClientImpl : public LeAudioClient {
        * PACs are updated.
        */
       if (group) {
+        /* Changes in PAC record channel counts may change the strategy */
+        group->InvalidateGroupStrategy();
         group->InvalidateCachedConfigurations();
       }
       if (notify) {
@@ -1834,7 +1825,7 @@ class LeAudioClientImpl : public LeAudioClient {
             leAudioDevice->snk_audio_locations_.to_ulong(),
             leAudioDevice->src_audio_locations_.to_ulong());
         if (group && group->IsReleasingOrIdle()) {
-          UpdateLocationsAndContextsAvailability(leAudioDevice->group_id_);
+          UpdateLocationsAndContextsAvailability(group);
         }
       }
     } else if (hdl == leAudioDevice->src_audio_locations_hdls_.val_hdl) {
@@ -1862,7 +1853,7 @@ class LeAudioClientImpl : public LeAudioClient {
             leAudioDevice->snk_audio_locations_.to_ulong(),
             leAudioDevice->src_audio_locations_.to_ulong());
         if (group && group->IsReleasingOrIdle()) {
-          UpdateLocationsAndContextsAvailability(leAudioDevice->group_id_);
+          UpdateLocationsAndContextsAvailability(group);
         }
       }
     } else if (hdl == leAudioDevice->audio_avail_hdls_.val_hdl) {
@@ -5566,7 +5557,7 @@ class LeAudioClientImpl : public LeAudioClient {
         CleanCachedMicrophoneData();
 
         if (group) {
-          UpdateLocationsAndContextsAvailability(group->group_id_);
+          UpdateLocationsAndContextsAvailability(group);
           if (group->IsPendingConfiguration()) {
             SuspendedForReconfiguration();
             auto remote_direction =
