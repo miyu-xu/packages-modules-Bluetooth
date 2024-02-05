b1989df0c20ca3bae34aae023b0aa1dd5f7021bb
zero.liu <zero.liu@mediatek.com>
2024-02-05 16:22:29 +0800

    Fix Set phy 0 0 0 on server cannot clear prefer Phy
    [Description]
    The TX MTU remains unchanged but tx time changed by PHY change.
    
    [Root Cause]
    according to spec,all phy should't set to 0 when tx phy
    or rx phy set to 0
    
    [Solution]
    Modify logcal of bt stack code set phy 0 0 0
    
    Bug: 329161110
    Test: atest net_test_btif pass
    
    Change-Id: I4d07d667a662cafab81c25c93cf5b65431ab82f4

--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -201,8 +201,8 @@ void BTM_BleSetPhy(const RawAddress& bd_addr, uint8_t tx_phys, uint8_t rx_phys,
   }
 
   uint8_t all_phys = 0;
-  if (tx_phys == 0) all_phys &= 0x01;
-  if (rx_phys == 0) all_phys &= 0x02;
+  if (tx_phys == 0) all_phys |= 0x01;
+  if (rx_phys == 0) all_phys |= 0x02;
 
   uint16_t handle = BTM_GetHCIConnHandle(bd_addr, BT_TRANSPORT_LE);
 
