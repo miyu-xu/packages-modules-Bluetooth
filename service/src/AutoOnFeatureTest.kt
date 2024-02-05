a8fd0e09c9eea34bcd96ed529b790e71243d6eb2
William Escande <wescande@google.com>
2024-02-05 15:37:55 -0800

    SystemServer: AutoOn: Hidden api listener
    Since the auto on feature may be used before the release of android V
    APIs, we are hadding a hacky way to interact with it
    
    Bug: 323060869
    Bug: 316946334
    Test: atest ServiceBluetoothRoboTests
    Change-Id: I774ad4eece375f48aa77e6113c0d1f2e62643aad

--- service/src/AutoOnFeatureTest.kt
+++ service/src/AutoOnFeatureTest.kt
@@ -22,6 +22,7 @@ import android.os.Looper
 import android.provider.Settings
 import androidx.test.core.app.ApplicationProvider
 import com.android.server.bluetooth.BluetoothAdapterState
+import com.android.server.bluetooth.HiddenApiListener
 import com.android.server.bluetooth.Log
 import com.android.server.bluetooth.Timer
 import com.android.server.bluetooth.USER_SETTINGS_KEY
@@ -29,6 +30,7 @@ import com.android.server.bluetooth.cancel
 import com.android.server.bluetooth.isUserEnabled
 import com.android.server.bluetooth.isUserSupported
 import com.android.server.bluetooth.pause
+import com.android.server.bluetooth.registerHiddenApiListener
 import com.android.server.bluetooth.satellite.test.ModeListenerTest as SatelliteListener
 import com.android.server.bluetooth.setUserEnabled
 import com.android.server.bluetooth.setupNewTimer
@@ -49,6 +51,8 @@ import org.robolectric.Shadows.shadowOf
 @RunWith(RobolectricTestRunner::class)
 @kotlinx.coroutines.ExperimentalCoroutinesApi
 class AutoOnFeatureTest {
+    private val SETTING_URI = Settings.Secure.getUriFor(USER_SETTINGS_KEY)
+
     private val looper = Looper.getMainLooper()
     private val state = BluetoothAdapterState()
     private val context = ApplicationProvider.getApplicationContext<Context>()
@@ -71,6 +75,9 @@ class AutoOnFeatureTest {
     @After
     fun tearDown() {
         Log.i("AutoOnFeatureTest", "\t<-- tearDown(${testName.getMethodName()})\n")
+
+        HiddenApiListener.listener?.let { it.remove() }
+        HiddenApiListener.listener = null
 
         callback_count = 0
         Timer.cancel()
@@ -353,5 +360,48 @@ class AutoOnFeatureTest {
         expect.that(Timer.timer).isNull()
         expect.that(callback_count).isEqualTo(1)
         expectNoStorageTime()
+    }
+
+    @Test
+    fun registerHiddenListener_whenNothing_is() {
+        registerHiddenApiListener(looper, context, state, this::callback_on)
+
+        assertThat(HiddenApiListener.listener).isNotNull()
+        HiddenApiListener.listener?.let { it.remove() }
+        assertThat(shadowOf(resolver).getContentObservers(SETTING_URI).size).isEqualTo(0)
+    }
+
+    @Test
+    fun registerHiddenListener_whenAlreadyRegistered_is() {
+        registerHiddenApiListener(looper, context, state, this::callback_on)
+
+        registerHiddenApiListener(looper, context, state, this::callback_on)
+
+        expect.that(shadowOf(resolver).getContentObservers(SETTING_URI).size).isEqualTo(1)
+        expect.that(HiddenApiListener.listener).isNotNull()
+    }
+
+    @Test
+    fun changeSettingsToDisabled_whenHiddenApiIsRegisteredandScheduled_isNotSchedule() {
+        setupTimer()
+        registerHiddenApiListener(looper, context, state, this::callback_on)
+
+        disableUserSettings()
+
+        expect.that(Timer.timer).isNull()
+        expect.that(callback_count).isEqualTo(0)
+        expectNoStorageTime()
+    }
+
+    @Test
+    fun changeSettingsToEnabled_whenHiddenApiIsRegisteredandScheduled_isNotSchedule() {
+        disableUserSettings()
+        registerHiddenApiListener(looper, context, state, this::callback_on)
+
+        enableUserSettings()
+
+        expect.that(Timer.timer).isNotNull()
+        expect.that(callback_count).isEqualTo(0)
+        expectStorageTime()
     }
 }
