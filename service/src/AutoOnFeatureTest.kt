b94cd30481e44fbeb5544aab905a1370984cf250
William Escande <wescande@google.com>
2024-02-05 15:37:03 -0800

    SystemServer: AutoOn: Add system API
    Bug: 316946334
    Bug: 323060869
    Test: m Bluetooth
    Test: atest CtsBluetoothTestCases
    Test: atest ServiceBluetoothRoboTests
    Change-Id: I0d8c37a9b3684a0b2c45111ddb8b0738a9682792

--- service/src/AutoOnFeatureTest.kt
+++ service/src/AutoOnFeatureTest.kt
@@ -25,8 +25,12 @@ import com.android.server.bluetooth.Log
 import com.android.server.bluetooth.Timer
 import com.android.server.bluetooth.USER_SETTINGS_KEY
 import com.android.server.bluetooth.cancel
+import com.android.server.bluetooth.isUserEnabled
+import com.android.server.bluetooth.isUserSupported
+import com.android.server.bluetooth.setUserEnabled
 import com.android.server.bluetooth.setupNewTimer
 import com.google.common.truth.Truth.assertThat
+import kotlin.test.assertFailsWith
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -150,6 +154,52 @@ class AutoOnFeatureTest {
 
         cancel(resolver)
 
-        assertThat(Settings.Secure.getInt(resolver, USER_SETTINGS_KEY, 42)).isEqualTo(1)
+        assertThat(isUserSupported(resolver)).isTrue()
+    }
+
+    @Test
+    fun apiIsUserEnable_whenNotSupported_throwException() {
+        restoreSettings()
+
+        assertFailsWith<IllegalStateException> { isUserEnabled(resolver) }
+    }
+
+    @Test
+    fun apiSetUserEnabled_whenNotSupported_throwException() {
+        restoreSettings()
+
+        assertFailsWith<IllegalStateException> { setUserEnabled(resolver, true) }
+    }
+
+    @Test
+    fun apiIsUserEnable_whenEnabled_isTrue() {
+        assertThat(isUserEnabled(resolver)).isTrue()
+    }
+
+    @Test
+    fun apiIsUserEnable_whenDisabled_isFalse() {
+        disableUserSettings()
+        assertThat(isUserEnabled(resolver)).isFalse()
+    }
+
+    @Test
+    fun apiSetUserEnableToFalse_whenScheduled_isNotScheduled() {
+        setupTimer()
+
+        setUserEnabled(resolver, false)
+
+        assertThat(isUserEnabled(resolver)).isFalse()
+        assertThat(callback_count).isEqualTo(0)
+        assertThat(Timer.timer).isNull()
+    }
+
+    @Test
+    fun apiSetUserEnableToTrue_whenIdle_canSchedule() {
+        disableUserSettings()
+
+        setUserEnabled(resolver, true)
+        setupTimer()
+
+        assertThat(Timer.timer).isNotNull()
     }
 }
