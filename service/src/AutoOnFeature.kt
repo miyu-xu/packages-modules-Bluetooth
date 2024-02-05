6687e57e4a83f79fde1b3e662844ebba4f406f75
William Escande <wescande@google.com>
2024-02-05 15:37:31 -0800

    SystemServer: AutoOn: Satellite override
    Suspend timer during Satellite time
    
    Bug: 323060869
    Bug: 316946334
    Test: m Bluetooth
    Change-Id: Ia2f6e1b540477793084cac6e70edd977cc68d523

--- service/src/AutoOnFeature.kt
+++ service/src/AutoOnFeature.kt
@@ -24,6 +24,7 @@ import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
+import com.android.server.bluetooth.satellite.isOn as isSatelliteModeOn
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
@@ -45,6 +46,10 @@ public fun setupNewTimer(
     }
     if (state.oneOf(STATE_ON)) {
         Log.d(TAG, "Bluetooth already on, no need for timer")
+        return
+    }
+    if (isSatelliteModeOn) {
+        Log.d(TAG, "Satellite prevent feature activation")
         return
     }
     Timer.start(looper, context, callback_on)
