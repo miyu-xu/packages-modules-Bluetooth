4fb9076aa84cfa4ed62d598d183fb1460770083a
Daniel Norman <danielnorman@google.com>
2024-02-09 22:25:39 +0000

    Revert "Remove types/class_of_device"
    Revert submission 2904410
    
    Reason for revert: b/324633930
    
    Reverted changes: /q/submissionid:2904410
    
    Change-Id: Ie9167ebe87aae58757a82108b8763a513ee9fd23

--- system/main/shim/acl.cc
+++ system/main/shim/acl.cc
@@ -1619,8 +1619,11 @@ void shim::legacy::Acl::OnConnectRequest(hci::Address address,
                                          hci::ClassOfDevice cod) {
   const RawAddress bd_addr = ToRawAddress(address);
 
+  types::ClassOfDevice legacy_cod;
+  legacy_cod.FromOctets(cod.data());
+
   TRY_POSTING_ON_MAIN(acl_interface_.connection.classic.on_connect_request,
-                      bd_addr, cod);
+                      bd_addr, legacy_cod);
   LOG_DEBUG("Received connect request remote:%s",
             ADDRESS_TO_LOGGABLE_CSTR(address));
   BTM_LogHistory(kBtmLogTag, ToRawAddress(address), "Connection request");
