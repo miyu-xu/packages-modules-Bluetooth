3415095ca189f03d725cdead519001dbb528edee
Sandeep Samdaria <samdaria@google.com>
2024-02-14 15:10:01 -0800

    Route A2DP Traffic
    Problem: When both the a2dp profiles are enabled concurrently, the
    method calls to btif_av is unaware which profile to redirect the
    incoming/outgoing traffic.
    
    Solution: Pass the type of profile that the client is interested
    in. BTIF AV redirects the traffic based on the type of the profile.
    
    Test: atest net_*
    
    Bug: 315334681
    Bug: 321806163
    Change-Id: I374773e3274708dc00242bf260bc2a5fbca549b4

--- system/btif/co/bta_av_co.cc
+++ system/btif/co/bta_av_co.cc
@@ -277,7 +277,7 @@ tA2DP_STATUS BtaAvCo::ProcessSourceGetConfig(
       return A2DP_FAIL;
     }
   } else {
-    if (btif_av_peer_prefers_mandatory_codec(p_peer->addr)) {
+    if (btif_av_peer_prefers_mandatory_codec(p_peer->addr, A2dpType::kSource)) {
       // Apply user preferred codec directly before first codec selected.
       p_sink = peer_cache_->FindPeerSink(
           p_peer, BTAV_A2DP_CODEC_INDEX_SOURCE_SBC, ContentProtectFlag());
@@ -735,7 +735,7 @@ void BtaAvCo::ProcessAudioDelay(tBTA_AV_HNDL bta_av_handle,
   LOG_VERBOSE("%s: peer %s bta_av_handle: 0x%x delay:0x%x", __func__,
               ADDRESS_TO_LOGGABLE_CSTR(peer_address), bta_av_handle, delay);
 
-  btif_av_set_audio_delay(peer_address, delay);
+  btif_av_set_audio_delay(peer_address, delay, A2dpType::kSource);
 }
 
 void BtaAvCo::UpdateMtu(tBTA_AV_HNDL bta_av_handle,
@@ -832,9 +832,10 @@ void BtaAvCo::GetPeerEncoderParameters(
     if (p_peer->mtu < min_mtu) min_mtu = p_peer->mtu;
   }
   p_peer_params->peer_mtu = min_mtu;
-  p_peer_params->is_peer_edr = btif_av_is_peer_edr(peer_address);
+  p_peer_params->is_peer_edr =
+      btif_av_is_peer_edr(peer_address, A2dpType::kSource);
   p_peer_params->peer_supports_3mbps =
-      btif_av_peer_supports_3mbps(peer_address);
+      btif_av_peer_supports_3mbps(peer_address, A2dpType::kSource);
   LOG_VERBOSE(
       "%s: peer_address=%s peer_mtu=%d is_peer_edr=%s peer_supports_3mbps=%s",
       __func__, ADDRESS_TO_LOGGABLE_CSTR(peer_address), p_peer_params->peer_mtu,
