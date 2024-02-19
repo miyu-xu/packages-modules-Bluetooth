7e4c9cb707d5dbdc229b5af1efa35c4f7dc64db9
Yun-Hao Chung <howardchung@google.com>
2024-02-19 08:31:10 +0000

    Floss: Hcidoc: Add external controller signal
    Some users plug an external controller, if the stack somehow use that,
    it is likely to fail in various situation as it wasn't tested.
    
    This adds a signal to detect if the chosen controller is an external
    controller by checking if the local name and manufacture are in the
    known list.
    
    Bug: 325403872
    Test: m -j
    Test: Using report 91563648247, verify every read local name is reported
    Tag: #floss
    Flag: EXEMPT, Floss only change
    
    Change-Id: Ia25c4f8deaaa20d4529d412938c3cc1fdcf915c0

--- floss/hcidoc/src/groups/controllers.rs
+++ floss/hcidoc/src/groups/controllers.rs
@@ -1,23 +1,45 @@
 ///! Rule group for tracking controller related issues.
 use chrono::NaiveDateTime;
+use lazy_static::lazy_static;
 use std::convert::Into;
 use std::io::Write;
 
 use crate::engine::{Rule, RuleGroup, Signal};
 use crate::parser::{Packet, PacketChild};
-use bt_packets::hci::EventChild;
+use bt_packets::hci::{CommandCompleteChild, ErrorCode, EventChild, LocalVersionInformation};
 
 enum ControllerSignal {
-    HardwareError, // Controller reports HCI event: Hardware Error
+    HardwareError,            // Controller reports HCI event: Hardware Error
+    LikelyExternalController, // The controller report name is not as expected, indicating an external controller is being used.
 }
 
 impl Into<&'static str> for ControllerSignal {
     fn into(self) -> &'static str {
         match self {
             ControllerSignal::HardwareError => "HardwareError",
+            ControllerSignal::LikelyExternalController => "LikelyExternalController",
         }
     }
 }
+
+lazy_static! {
+    static ref KNOWN_CONTROLLER_NAMES: Vec<String> = vec![
+        String::from("Bluemoon Universal Bluetooth Host Controller"),    // AC7625
+        String::from("MTK MT7961 #1"),    // MT7921LE/MT7921LS
+        String::from("MTK MT7922 #1"),    // MT7922
+        String::from("MTK MT7922 #1"),    // MT7922
+        String::from("RTK_BT_5.0"),       // RTL8822CE
+        String::from("RT_BT"),            // RTL8852AE
+        String::from(""),                 // AC9260/AC9560/AC200/AC201/AC203/AX211/MVL8897/QCA6174A3/QCA6174A5/QC_WCN6856
+    ];
+}
+const KNOWN_CONTROLLER_MANUFACTURE: [u16; 5] = [
+    2,  // Intel.
+    29, // Qualcomm
+    70, // MediaTek
+    72, // Marvell
+    93, // Realtek
+];
 
 struct ControllerRule {
     /// Pre-defined signals discovered in the logs.
@@ -41,6 +63,35 @@ impl ControllerRule {
 
         self.reportable.push((packet.ts, format!("controller reported hardware error")));
     }
+
+    fn process_local_name(&mut self, local_name: &[u8; 248], packet: &Packet) {
+        match String::from_utf8(local_name.to_vec()) {
+            Ok(name) => {
+                if !KNOWN_CONTROLLER_NAMES.contains(&name) {
+                    self.signals.push(Signal {
+                        index: packet.index,
+                        ts: packet.ts,
+                        tag: ControllerSignal::LikelyExternalController.into(),
+                    })
+                }
+            }
+            Err(err) => self.signals.push(Signal {
+                index: packet.index,
+                ts: packet.ts,
+                tag: ControllerSignal::LikelyExternalController.into(),
+            }),
+        }
+    }
+
+    fn process_local_version(&mut self, version_info: &LocalVersionInformation, packet: &Packet) {
+        if !KNOWN_CONTROLLER_MANUFACTURE.contains(&version_info.manufacturer_name) {
+            self.signals.push(Signal {
+                index: packet.index,
+                ts: packet.ts,
+                tag: ControllerSignal::LikelyExternalController.into(),
+            })
+        }
+    }
 }
 
 impl Rule for ControllerRule {
@@ -50,6 +101,23 @@ impl Rule for ControllerRule {
                 EventChild::HardwareError(_ev) => {
                     self.report_hardware_error(&packet);
                 }
+                EventChild::CommandComplete(ev) => match ev.specialize() {
+                    CommandCompleteChild::ReadLocalNameComplete(ev) => {
+                        if ev.get_status() != ErrorCode::Success {
+                            return;
+                        }
+
+                        self.process_local_name(ev.get_local_name(), &packet);
+                    }
+                    CommandCompleteChild::ReadLocalVersionInformationComplete(ev) => {
+                        if ev.get_status() != ErrorCode::Success {
+                            return;
+                        }
+
+                        self.process_local_version(ev.get_local_version_information(), &packet);
+                    }
+                    _ => {}
+                },
                 _ => {}
             },
             _ => {}
