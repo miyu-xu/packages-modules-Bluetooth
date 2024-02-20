3a39238114d76ac5ee9a20d5b6626267eac58f02
Nagham Masalmah <nagham.masalmah@exalt.corp-partner.google.com>
2024-02-20 12:07:50 +0200

    Floss: Implement Pandora A2DP profile
    Bug: 301000136
    Bug: 301036162
    Bug: 301036165
    Bug: 301000140
    Bug: 301036166
    Bug: 301036167
    Bug: 301037359
    
    Test: mma packages/modules/Bluetooth && pts-bot A2DP
    Tag: #floss
    Flag: EXEMPT floss only changes
    Change-Id: I5e160ecada07029e7aedf59d283d7303421557e5

--- floss/pandora/server/server.py
+++ floss/pandora/server/server.py
@@ -16,12 +16,14 @@
 import asyncio
 import logging
 
+from floss.pandora.server import a2dp
 from floss.pandora.server import bluetooth as bluetooth_module
 from floss.pandora.server import gatt
 from floss.pandora.server import hid
 from floss.pandora.server import host
 from floss.pandora.server import security
 import grpc
+from pandora import a2dp_grpc_aio
 from pandora import host_grpc_aio
 from pandora import security_grpc_aio
 from pandora_experimental import gatt_grpc_aio
@@ -54,6 +56,9 @@ async def serve(port):
 
             hid_service = hid.HIDService(bluetooth)
             hid_grpc_aio.add_HIDServicer_to_server(hid_service, server)
+
+            a2dp_service = a2dp.A2DPService(bluetooth)
+            a2dp_grpc_aio.add_A2DPServicer_to_server(a2dp_service, server)
 
             server.add_insecure_port(f'[::]:{port}')
 
