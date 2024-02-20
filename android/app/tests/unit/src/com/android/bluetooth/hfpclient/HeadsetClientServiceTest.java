793538ae4db28697d3df515c96314b1defd8035e
Bhushan Shinkar <shinkar@google.com>
2024-02-20 13:52:14 +0000

    Adding a delay after calling startService() to prevent potential failure caused by delayed responses.
    Bug:308388378
    Change-Id: If758a99011a4a52575bbf4a91c9f19d1ef4702d7
    Test: atest HeadsetClientServiceTest
    Flag: EXEMPT test only change

--- android/app/tests/unit/src/com/android/bluetooth/hfpclient/HeadsetClientServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/hfpclient/HeadsetClientServiceTest.java
@@ -17,6 +17,7 @@
 package com.android.bluetooth.hfpclient;
 
 import static android.content.pm.PackageManager.FEATURE_WATCH;
+import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
@@ -124,6 +125,7 @@ public class HeadsetClientServiceTest {
     public void testUpdateBatteryLevel() throws Exception {
         startService();
 
+        TimeUnit.MILLISECONDS.sleep(STANDARD_WAIT_MILLIS);
         // Put mock state machine
         BluetoothDevice device =
                 BluetoothAdapter.getDefaultAdapter().getRemoteDevice("00:01:02:03:04:05");
