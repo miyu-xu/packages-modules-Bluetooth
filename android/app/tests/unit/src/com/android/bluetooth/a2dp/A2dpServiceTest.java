74fb0e227fb6be6d9fb3f26569fc83b568adfc56
Myles Watson <mylesgw@google.com>
2024-02-21 13:40:01 -0800

    A2dpTest: Check last connectionStateChanged action
    A2dpService.connectionStateChanged makes multiple calls.  Verify
    that the last one occurred before tearing down the test.
    
    Bug: 321252288
    Test: mma -j32
    Flag: EXEMPT, test-only change
    Change-Id: I07c30fdd12b4a8a853970146dfb34f2fd085320e

--- android/app/tests/unit/src/com/android/bluetooth/a2dp/A2dpServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/a2dp/A2dpServiceTest.java
@@ -418,6 +418,13 @@ public class A2dpServiceTest {
                     BluetoothProfile.STATE_CONNECTED, mA2dpService.getConnectionState(testDevice));
             // Verify the list of connected devices
             Assert.assertTrue(mA2dpService.getConnectedDevices().contains(testDevice));
+            // Verify that AdapterService received the connection state change
+            verify(mAdapterService)
+                    .updateProfileConnectionAdapterProperties(
+                            sTestDevice,
+                            BluetoothProfile.A2DP,
+                            BluetoothProfile.STATE_CONNECTED,
+                            BluetoothProfile.STATE_CONNECTING);
         }
 
         // Prepare and connect the extra test device. The connect request should fail
