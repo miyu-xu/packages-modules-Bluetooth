2898ec3a273e5e254bb06132339c22b4e783607c
Brian Delwiche <delwiche@google.com>
2024-02-22 01:49:05 +0000

    For b/323409865 testing; do not merge
    Test: forthcoming
    Bug: 323409865
    
    Change-Id: I6c54a49537c2f341f6f2bcdad915259df59e41e8

--- system/gd/hci/le_scanning_manager.cc
+++ system/gd/hci/le_scanning_manager.cc
@@ -458,7 +458,8 @@ struct LeScanningManager::impl : public LeAddressManagerCallback {
     // scan responses are still reported too.
     scanning_reassembler_.SetIgnoreScanResponses(
         le_scan_type_ == LeScanType::PASSIVE ||
-        filter_policy_ == LeScanningFilterPolicy::FILTER_ACCEPT_LIST_ONLY);
+        filter_policy_ == LeScanningFilterPolicy::FILTER_ACCEPT_LIST_ONLY &&
+            api_type_ == ScanApiType::ANDROID_HCI);
 
     std::optional<LeScanningReassembler::CompleteAdvertisingData> processed_report =
         scanning_reassembler_.ProcessAdvertisingReport(
