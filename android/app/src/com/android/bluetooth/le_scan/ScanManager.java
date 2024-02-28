52fb8447e8917d5fcb241006f4d969915895fe61
David Duarte <licorne@google.com>
2024-02-28 01:34:27 +0000

    Bluetooth: Fix Unused{Method,Variable} errorprone warnings
    And make it into an error so that we don't add more
    in the future.
    
    Test: m RUN_ERROR_PRONE=true Bluetooth
    Bug: 327516738
    Flag: Exempt, no logical change
    Change-Id: If43b34c592532aa31677c1e96267bfdd79f656de

--- android/app/src/com/android/bluetooth/le_scan/ScanManager.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanManager.java
@@ -1351,7 +1351,7 @@ public class ScanManager {
             }
             int deliveryMode = getDeliveryMode(client);
             if (deliveryMode == DELIVERY_MODE_ON_FOUND_LOST) {
-                for (ScanFilter filter : client.filters) {
+                for (ScanFilter unused : client.filters) {
                     int entriesToFree = getNumOfTrackingAdvertisements(client.settings);
                     if (!manageAllocationOfTrackingAdvertisement(entriesToFree, false)) {
                         Log.e(TAG, "Error freeing for onfound/onlost filter resources "
@@ -1670,9 +1670,8 @@ public class ScanManager {
             int rssiThreshold = Byte.MIN_VALUE;
             ScanSettings settings = client.settings;
             int onFoundTimeout = getOnFoundOnLostTimeoutMillis(settings, true);
-            int onLostTimeout = getOnFoundOnLostTimeoutMillis(settings, false);
             int onFoundCount = getOnFoundOnLostSightings(settings);
-            onLostTimeout = 10000;
+            int onLostTimeout = 10000;
             if (DBG) {
                 Log.d(TAG, "configureFilterParamter " + onFoundTimeout + " " + onLostTimeout + " "
                         + onFoundCount + " " + numOfTrackingEntries);
