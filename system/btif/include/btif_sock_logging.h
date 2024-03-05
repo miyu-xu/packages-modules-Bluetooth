8ce346f215cd42611f2de9f79e93006a06ed1129
Aritra Sen <aritrasen@google.com>
2024-03-05 04:14:08 +0000

    Move logging code for btif_sock in a different module and aggregate dumpsys and metric logic in one place.
    Bug: 295023364
    Test: m -j $(nproc)
    Flag: EXEMPT ; these are logging changes (Metrics/dumpsys)
    Change-Id: I96b2773e34211a19b30b0261329867ce95a7c2dc

--- system/btif/include/btif_sock_logging.h
+++ system/btif/include/btif_sock_logging.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2014 Google, Inc.
+ *  Copyright 2024 Google, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,32 +18,10 @@
 
 #pragma once
 
-#include <hardware/bt_sock.h>
-
-#include "btif_uid.h"
 #include "types/raw_address.h"
 
-enum {
-  SOCKET_CONNECTION_STATE_UNKNOWN,
-  // Socket acts as a server waiting for connection
-  SOCKET_CONNECTION_STATE_LISTENING,
-  // Socket acts as a client trying to connect
-  SOCKET_CONNECTION_STATE_CONNECTING,
-  // Socket is connected
-  SOCKET_CONNECTION_STATE_CONNECTED,
-  // Socket tries to disconnect from remote
-  SOCKET_CONNECTION_STATE_DISCONNECTING,
-  // This socket is closed
-  SOCKET_CONNECTION_STATE_DISCONNECTED,
-};
-
-enum {
-  SOCKET_ROLE_UNKNOWN,
-  SOCKET_ROLE_LISTEN,
-  SOCKET_ROLE_CONNECTION,
-};
-
-const btsock_interface_t* btif_sock_get_interface(void);
-
-bt_status_t btif_sock_init(uid_set_t* uid_set);
-void btif_sock_cleanup(void);
+void btif_sock_connection_logger(const RawAddress& address, int port, int type,
+                                 int state, int role, int uid, int server_port,
+                                 int64_t tx_bytes, int64_t rx_bytes,
+                                 const char* server_name);
+void btif_sock_dump(int fd);
