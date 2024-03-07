a19097a7245bc41a97b63ef2bb34d32e51db82df
Jakub Tyszkowski <tyszkowski@google.com>
2024-03-07 13:58:40 +0000

    (Rebasing) LeAudio: Ase configuration data refactor - part 2
    Borrowing from aosp/3003283 so that it doesn't break POC in CrOS.
    
    Do not submit.
    
    Change-Id: I4f1b3ae955bfcb7c0520e57318765e96cb4ef08c

--- system/bta/le_audio/le_audio_types.cc
+++ system/bta/le_audio/le_audio_types.cc
@@ -44,28 +44,6 @@ using types::kLeAudioDirectionSink;
 using types::kLeAudioDirectionSource;
 using types::LeAudioCoreCodecConfig;
 
-static uint8_t min_req_devices_cnt(
-    const AudioSetConfiguration* audio_set_conf) {
-  log::assert_that(
-      audio_set_conf->topology_info.has_value(),
-      "No topology info, which is required to properly configure the ASEs");
-  return std::max(audio_set_conf->topology_info->device_count.sink,
-                  audio_set_conf->topology_info->device_count.source);
-}
-
-static uint8_t min_req_devices_cnt(
-    const AudioSetConfigurations* audio_set_confs) {
-  uint8_t curr_min_req_devices_cnt = 0xff;
-
-  for (auto ent : *audio_set_confs) {
-    uint8_t req_devices_cnt = min_req_devices_cnt(ent);
-    if (req_devices_cnt < curr_min_req_devices_cnt)
-      curr_min_req_devices_cnt = req_devices_cnt;
-  }
-
-  return curr_min_req_devices_cnt;
-}
-
 void get_cis_count(LeAudioContextType context_type, int expected_device_cnt,
                    types::LeAudioConfigurationStrategy strategy,
                    int avail_group_ase_snk_cnt, int avail_group_ase_src_count,
@@ -135,33 +113,9 @@ void get_cis_count(LeAudioContextType context_type, int expected_device_cnt,
       out_cis_count_unidir_source);
 }
 
-bool check_if_may_cover_scenario(const AudioSetConfigurations* audio_set_confs,
-                                 uint8_t group_size) {
-  if (!audio_set_confs) {
-    log::error("no audio requirements for group");
-    return false;
-  }
-
-  return group_size >= min_req_devices_cnt(audio_set_confs);
-}
-
-bool check_if_may_cover_scenario(const AudioSetConfiguration* audio_set_conf,
-                                 uint8_t group_size) {
-  if (!audio_set_conf) {
-    log::error("no audio requirement for group");
-    return false;
-  }
-
-  return group_size >= min_req_devices_cnt(audio_set_conf);
-}
-
-uint8_t get_num_of_devices_in_configuration(
-    const AudioSetConfiguration* audio_set_conf) {
-  return min_req_devices_cnt(audio_set_conf);
-}
-
 static bool IsCodecConfigCoreSupported(const types::LeAudioLtvMap& pacs,
-                                       const types::LeAudioLtvMap& reqs) {
+                                       const types::LeAudioLtvMap& reqs,
+                                       uint8_t channel_cnt_per_ase) {
   auto caps = pacs.GetAsCoreCodecCapabilities();
   auto config = reqs.GetAsCoreCodecConfig();
 
@@ -181,10 +135,7 @@ static bool IsCodecConfigCoreSupported(const types::LeAudioLtvMap& pacs,
   }
 
   /* Channel counts */
-  if (!caps.IsAudioChannelCountsSupported(
-          config.GetChannelCountPerIsoStream())) {
-    log::debug("Cfg: Allocated channel count= 0x{:04x}",
-               config.GetChannelCountPerIsoStream());
+  if (!caps.IsAudioChannelCountsSupported(channel_cnt_per_ase)) {
     log::debug("Cap: Supported channel counts= 0x{:04x}",
                caps.supported_audio_channel_counts.value_or(1));
     log::debug("Channel count not supported");
@@ -237,8 +188,9 @@ bool IsCodecConfigSettingSupported(
     log::assert_that(
         !pac.codec_spec_caps.IsEmpty(),
         "Codec specific capabilities are not parsed approprietly.");
-    return IsCodecConfigCoreSupported(pac.codec_spec_caps,
-                                      codec_config_setting.params);
+    return IsCodecConfigCoreSupported(
+        pac.codec_spec_caps, codec_config_setting.params,
+        codec_config_setting.GetChannelCountPerIsoStream());
   }
 
   log::error("Codec {}, seems to be not supported here.",
