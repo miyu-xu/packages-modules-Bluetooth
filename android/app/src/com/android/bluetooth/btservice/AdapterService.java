14d76a96a4fe9bb20d5d895f282ed3fd429cb8d9
William Escande <wescande@google.com>
2024-03-07 21:16:53 -0800

    AdapterService: Delete one time variable
    Executor let think the user there is a real executor while instead this
    is creating a raw thread in the code
    
    Bug: 311772251
    Test: m Bluetooth | no-op clean up
    Flag: Exempt, basic cleanup
    Change-Id: Ib783386e27dbbb2b5a2d013589b1dceaeaff8955

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -354,7 +354,6 @@ public class AdapterService extends Service {
     private HashMap<String, CallerInfo> mBondAttemptCallerInfo = new HashMap<>();
 
     private final Map<UUID, RfcommListenerData> mBluetoothServerSockets = new ConcurrentHashMap<>();
-    private final Executor mSocketServersExecutor = r -> new Thread(r).start();
 
     private BatteryStatsManager mBatteryStatsManager;
     private PowerManager mPowerManager;
@@ -2088,7 +2087,7 @@ public class AdapterService extends Service {
 
         mBluetoothServerSockets.put(uuid, listenerData);
 
-        mSocketServersExecutor.execute(() -> handleIncomingRfcommConnections(uuid));
+        new Thread(() -> handleIncomingRfcommConnections(uuid)).start();
     }
 
     private void stopRfcommServerSockets() {
