2f0997a8efde2d9249b95f2e6cba8a68fea8c862
William Escande <wescande@google.com>
2024-03-08 19:57:59 -0800

    SystemServer: [ALPS08129411] enable race condition
    Calling enable when we are already enabling BLE_ON can lead to a race
    where Bluetooth is never turned on
    
    Bug: 328846833
    Fix: 328846833
    Test: m service-bluetooth | until state machine is implemented there is
          no test for this part of the code
    Flag: Exempt, obvious fix without remote interraction possible
    Change-Id: I7e63cbe8d8d41231566f949ca2e878ef4b087238

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1141,7 +1141,7 @@ class BluetoothManagerService {
                 mAdapter.stopBle(mContext.getAttributionSource());
                 return;
             }
-            if (isBluetoothPersistedStateOnBluetooth() || !isBleAppPresent()) {
+            if (isBluetoothPersistedStateOnBluetooth() || !isBleAppPresent() || mEnableExternal) {
                 Log.i(TAG, "continueFromBleOnState: Starting br edr");
                 // This triggers transition to STATE_ON
                 mAdapter.startBrEdr(mContext.getAttributionSource());
