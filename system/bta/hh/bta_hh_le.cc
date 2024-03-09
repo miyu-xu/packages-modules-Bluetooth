c8c845228643298fbbc19f0e1756be98a24fda3b
Michael Sun <michaelfsun@google.com>
2024-03-08 17:15:07 -0800

    interop: allow hid host to connect to not-first HOG service
    Due to the limitations of the current HID host implementation, it
    assumes that the HID peripheral has only one HOG service, or if there
    are multiple HOG services, the first one is always active. To address
    this issue temporarily, an interop quirk has been introduced to allow
    the HID host to connect to a non-first HID service for certain devices.
    However, for a long-term solution, the HID stack needs to be refactored
    to handle multiple HID services simultaneously.
    
    Bug: 286413526
    Bug: 328129633
    Tag: #floss
    Test: mmm packages/modules/Bluetooth
    Test: Manual test on Brya, Logitech CASA working
    Flag: EXEMPT, floss-only: interop is no-op for android
    
    Change-Id: I0733f1597d9058c992e46b99c6a31748714d8dfa

--- system/bta/hh/bta_hh_le.cc
+++ system/bta/hh/bta_hh_le.cc
@@ -1511,11 +1511,25 @@ static void bta_hh_le_srvc_search_cmpl(tBTA_GATTC_SEARCH_CMPL* p_data) {
   const gatt::Service* gap_service = nullptr;
   const gatt::Service* scp_service = nullptr;
 
+  int num_hid_service = 0;
   bool have_hid = false;
   for (const gatt::Service& service : *services) {
     if (service.uuid == Uuid::From16Bit(UUID_SERVCLASS_LE_HID) &&
         service.is_primary && !have_hid) {
-      have_hid = true;
+
+      // TODO(b/286413526): The current implementation connects to the first HID
+      // service, in the case of multiple HID services being present. As a
+      // temporary mitigation, connect to the third HID service for some
+      // particular devices. The long-term fix should refactor HID stack to
+      // connect to multiple HID services simultaneously.
+      if (interop_match_vendor_product_ids(
+              INTEROP_MULTIPLE_HOGP_SERVICE_CHOOSE_THIRD,
+              p_dev_cb->dscp_info.vendor_id, p_dev_cb->dscp_info.product_id)) {
+        if (num_hid_service != 3) {
+          num_hid_service++;
+          continue;
+        }
+      }
 
       /* found HID primamry service */
       p_dev_cb->hid_srvc.state = BTA_HH_SERVICE_DISCOVERED;
