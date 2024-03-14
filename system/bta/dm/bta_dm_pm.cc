fe2acc8f68ebe40eabe2eed4d835bf78fbdcde40
Eric Shih <shiheric@google.com>
2024-03-14 06:53:39 +0000

    Add property to sniff max slots
    Bug: 312631766
    Test: atest BluetoothInstrumentationTests
    Change-Id: I3822fa077e410c32fc30621c605a5fa71214caf8

--- system/bta/dm/bta_dm_pm.cc
+++ system/bta/dm/bta_dm_pm.cc
@@ -80,6 +80,8 @@ static std::recursive_mutex pm_timer_schedule_mutex;
 static std::recursive_mutex pm_timer_state_mutex;
 
 /* Sysprop paths for sniff parameters */
+static const char kPropertyBtaDmPmSniffMaxIntervalConfig[] =
+    "bluetooth.core.classic.bta_dm_pm_sniff_max_interval.config";
 static const char kPropertySniffMaxIntervals[] =
     "bluetooth.core.classic.sniff_max_intervals";
 static const char kPropertySniffMinIntervals[] =
@@ -812,6 +814,8 @@ static tBTM_PM_PWR_MD get_sniff_entry(uint8_t index) {
       osi_property_get_uintlist(kPropertySniffAttempts, invalid_list);
   std::vector<uint32_t> timeout =
       osi_property_get_uintlist(kPropertySniffTimeouts, invalid_list);
+  uint16_t bta_dm_pm_sniff_max_interval_config = uint16_t(
+      osi_property_get_int32(kPropertyBtaDmPmSniffMaxIntervalConfig, 0));
 
   // If any of the sysprops are malformed or don't exist, use default table
   // value
@@ -824,12 +828,22 @@ static tBTM_PM_PWR_MD get_sniff_entry(uint8_t index) {
   for (auto i = 0; i < BTA_DM_PM_PARK_IDX; i++) {
     if (use_defaults) {
       pwr_mds_cache.push_back(p_bta_dm_pm_md[i]);
+      log::debug("Checking previous result: {}", pwr_mds_cache[i].max);
     } else {
       pwr_mds_cache.push_back(tBTM_PM_PWR_MD{
           static_cast<uint16_t>(max[i]), static_cast<uint16_t>(min[i]),
           static_cast<uint16_t>(attempt[i]), static_cast<uint16_t>(timeout[i]),
           BTM_PM_MD_SNIFF});
     }
+  }
+
+  log::debug("Testing: {}", bta_dm_pm_sniff_max_interval_config);
+  if (bta_dm_pm_sniff_max_interval_config > 0) {
+    pwr_mds_cache[0].max = bta_dm_pm_sniff_max_interval_config;
+  }
+
+  for (auto i = 0; i < BTA_DM_PM_PARK_IDX; i++) {
+    log::debug("Checking previous result: {}", pwr_mds_cache[i].max);
   }
 
   if (index >= BTA_DM_PM_PARK_IDX) {
