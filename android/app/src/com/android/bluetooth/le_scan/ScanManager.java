d59bf28c58381cb729997891da24266605582194
Liang Li <lianglli@google.com>
2024-03-14 19:20:56 -0700

    Add logging for BLE scan and adv new atoms
    Bug: 326310753
    Bug: 328303508
    Test: atest BluetoothInstrumentationTests
    Change-Id: Iee944f390634c88801921ef0bb998532ca3846dd

--- android/app/src/com/android/bluetooth/le_scan/ScanManager.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanManager.java
@@ -1066,7 +1066,12 @@ public class ScanManager {
                     mNativeInterface.gattSetScanParameters(
                             client.scannerId, scanInterval, scanWindow, scanPhyMask);
                     mNativeInterface.gattClientScan(true);
-                    if (!AppScanStats.recordScanRadioStart(curScanSetting)) {
+                    if (!AppScanStats.recordScanRadioStart(
+                            curScanSetting,
+                            client.scannerId,
+                            client.stats,
+                            scanWindowMs,
+                            scanIntervalMs)) {
                         Log.w(TAG, "Scan radio already started");
                     }
                     mLastConfiguredScanSetting = curScanSetting;
@@ -1104,7 +1109,12 @@ public class ScanManager {
                     && client.settings.getScanMode() != ScanSettings.SCAN_MODE_OPPORTUNISTIC) {
                 Log.d(TAG, "start gattClientScanNative from startRegularScan()");
                 mNativeInterface.gattClientScan(true);
-                if (!AppScanStats.recordScanRadioStart(client.settings.getScanMode())) {
+                if (!AppScanStats.recordScanRadioStart(
+                        client.settings.getScanMode(),
+                        client.scannerId,
+                        client.stats,
+                        getScanWindowMillis(client.settings),
+                        getScanIntervalMillis(client.settings))) {
                     Log.w(TAG, "Scan radio already started");
                 }
             }
@@ -1353,7 +1363,8 @@ public class ScanManager {
                     client.updateScanMode(getMinScanMode(scanMode, maxScanMode));
                 }
                 client.stats.setScanTimeout(client.scannerId);
-                client.stats.recordScanTimeoutCountMetrics();
+                client.stats.recordScanTimeoutCountMetrics(
+                        client.scannerId, mAdapterService.getScanTimeoutMillis());
             }
 
             // The scan should continue for background scans
@@ -1496,7 +1507,10 @@ public class ScanManager {
                                     TAG,
                                     "No hardware resources for onfound/onlost filter "
                                             + trackEntries);
-                            client.stats.recordTrackingHwFilterNotAvailableCountMetrics();
+                            client.stats.recordTrackingHwFilterNotAvailableCountMetrics(
+                                    client.scannerId,
+                                    AdapterService.getAdapterService()
+                                            .getTotalNumOfTrackableAdvertisements());
                             try {
                                 mScanHelper.onScanManagerErrorCallback(
                                         scannerId, ScanCallback.SCAN_FAILED_INTERNAL_ERROR);
@@ -1595,7 +1609,9 @@ public class ScanManager {
                 return true;
             }
             if (client.filters.size() > mFilterIndexStack.size()) {
-                client.stats.recordHwFilterNotAvailableCountMetrics();
+                client.stats.recordHwFilterNotAvailableCountMetrics(
+                        client.scannerId,
+                        AdapterService.getAdapterService().getNumOfOffloadedScanFilterSupported());
                 return true;
             }
             return false;
