d6354a956c279315943829f6b83e48354200ac4d
Pomai Ahlo <poahlo@google.com>
2024-03-19 15:26:35 -0700

    Adjust rfcomm_cback log levels
    - Move logs to individual cases for more control over log levels
    - Set conjest and write events to verbose to avoid spam
    
    Bug: 329507884
    Test: m Bluetooth
    Test: Manual - send data between phones and verify log levels are
    appropriate
    Flag: EXEMPT log changes only
    Change-Id: I28ff48f93fe64d2bd1bf2e195da376d85885925c
    
    Change-Id: Ib1e7e36ad288696437df3a6b7b82d229e6ac4b3c

--- system/btif/src/btif_sock_rfc.cc
+++ system/btif/src/btif_sock_rfc.cc
@@ -678,45 +678,50 @@ static uint32_t rfcomm_cback(tBTA_JV_EVT event, tBTA_JV* p_data,
                              uint32_t rfcomm_slot_id) {
   uint32_t id = 0;
 
-  // Write events are too frequent to log at info level
-  if (event != BTA_JV_RFCOMM_WRITE_EVT) {
-    log::info("handling event:{} id:{}", event, rfcomm_slot_id);
-  } else {
-    log::verbose("handling event:{} id:{}", event, rfcomm_slot_id);
-  }
-
   switch (event) {
     case BTA_JV_RFCOMM_START_EVT:
+      log::info("handling BTA_JV_RFCOMM_START_EVT, rfcomm_slot_id:{}",
+                rfcomm_slot_id);
       on_srv_rfc_listen_started(&p_data->rfc_start, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_CL_INIT_EVT:
+      log::info("handling BTA_JV_RFCOMM_CL_INIT_EVT, rfcomm_slot_id:{}",
+                rfcomm_slot_id);
       on_cl_rfc_init(&p_data->rfc_cl_init, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_OPEN_EVT:
+      log::info("handling BTA_JV_RFCOMM_OPEN_EVT, rfcomm_slot_id:{}",
+                rfcomm_slot_id);
       BTA_JvSetPmProfile(p_data->rfc_open.handle, BTA_JV_PM_ID_1,
                          BTA_JV_CONN_OPEN);
       on_cli_rfc_connect(&p_data->rfc_open, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_SRV_OPEN_EVT:
+      log::info("handling BTA_JV_RFCOMM_SRV_OPEN_EVT, rfcomm_slot_id:{}",
+                rfcomm_slot_id);
       BTA_JvSetPmProfile(p_data->rfc_srv_open.handle, BTA_JV_PM_ALL,
                          BTA_JV_CONN_OPEN);
       id = on_srv_rfc_connect(&p_data->rfc_srv_open, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_CLOSE_EVT:
-      log::verbose("BTA_JV_RFCOMM_CLOSE_EVT: rfcomm_slot_id:{}",
-                   rfcomm_slot_id);
+      log::info("handling BTA_JV_RFCOMM_CLOSE_EVT, rfcomm_slot_id:{}",
+                rfcomm_slot_id);
       on_rfc_close(&p_data->rfc_close, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_WRITE_EVT:
+      log::verbose("handling BTA_JV_RFCOMM_WRITE_EVT, rfcomm_slot_id:{}",
+                   rfcomm_slot_id);
       on_rfc_write_done(&p_data->rfc_write, rfcomm_slot_id);
       break;
 
     case BTA_JV_RFCOMM_CONG_EVT:
+      log::verbose("handling BTA_JV_RFCOMM_CONG_EVT, rfcomm_slot_id:{}",
+                   rfcomm_slot_id);
       on_rfc_outgoing_congest(&p_data->rfc_cong, rfcomm_slot_id);
       break;
 
@@ -725,7 +730,7 @@ static uint32_t rfcomm_cback(tBTA_JV_EVT event, tBTA_JV* p_data,
       break;
 
     default:
-      log::error("unhandled event {}, slot id: {}", event, rfcomm_slot_id);
+      log::warn("unhandled event {}, slot id: {}", event, rfcomm_slot_id);
       break;
   }
   return id;
@@ -818,7 +823,7 @@ static void jv_dm_cback(tBTA_JV_EVT event, tBTA_JV* p_data, uint32_t id) {
     }
 
     default:
-      log::debug("unhandled event:{}, slot id:{}", event, id);
+      log::warn("unhandled event:{}, slot id:{}", event, id);
       break;
   }
 }
