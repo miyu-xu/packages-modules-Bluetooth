4b2cd654721cf41bd60b0541cecd6d5353b16799
Gopi Sakshihally Bhuthaiah <bhuthaiah@google.com>
2024-03-20 17:07:14 +0000

    Fix to avoid duplicate instance entry in btif_hh_cb.devices
    Bug: 330514181
    
    Test: mma packages/modules/Bluetooth
    Flag: EXEMPT minor bug fix
    Change-Id: I01b7719fc4ce783418e74c765c966a77c2d381ef

--- system/btif/co/bta_hh_co.cc
+++ system/btif/co/bta_hh_co.cc
@@ -438,7 +438,7 @@ bool bta_hh_co_open(uint8_t dev_handle, uint8_t sub_class,
       }
       break;
     } else if (IS_FLAG_ENABLED(allow_switching_hid_and_hogp) &&
-               p_dev->dev_status == BTHH_CONN_STATE_ACCEPTING &&
+               p_dev->dev_status != BTHH_CONN_STATE_UNKNOWN &&
                p_dev->dev_handle == BTA_HH_INVALID_HANDLE) {
       if (!hh_co_update_device(p_dev, dev_handle, sub_class, attr_mask,
                                app_id)) {
