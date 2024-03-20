6fb548f38ef659f4e361d7a0d18f64acfb9b1fa1
Jakub Pawlowski <jpawlowski@google.com>
2024-03-20 13:50:36 +0100

    BTA_DmAddDevice and BTM_SecAddDevice cleanup
    BTM_SecAddDevice always return true, so get rit of return value.
    BTA_DmAddDevice always passes empty device name, so get rid of it.
    Parameters are packed and unpacked into message unnecesarly
    
    Flag: exempt, just code removal
    Bug: 329458883
    Test: mma -j32
    Change-Id: I599293fc291d72d50f80a2eec5c094b47ea157ee

--- system/stack/include/security_client_callbacks.h
+++ system/stack/include/security_client_callbacks.h
@@ -97,10 +97,9 @@ typedef struct {
   void (*BTM_BleLoadLocalKeys)(uint8_t key_type, tBTM_BLE_LOCAL_KEYS* p_key);
 
   // Update/Query in-memory device records
-  bool (*BTM_SecAddDevice)(const RawAddress& bd_addr, const DEV_CLASS dev_class,
-                           const BD_NAME& bd_name, uint8_t* features,
-                           LinkKey* link_key, uint8_t key_type,
-                           uint8_t pin_length);
+  void (*BTM_SecAddDevice)(const RawAddress& bd_addr, const DEV_CLASS dev_class,
+                           uint8_t* features, LinkKey link_key,
+                           uint8_t key_type, uint8_t pin_length);
   void (*BTM_SecAddBleDevice)(const RawAddress& bd_addr,
                               tBT_DEVICE_TYPE dev_type,
                               tBLE_ADDR_TYPE addr_type);
