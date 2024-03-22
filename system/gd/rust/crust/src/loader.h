1b2a071719e7625ad555a8512e8a3ebf5723075c
Jesse Melhuish <melhuishj@google.com>
2024-03-22 20:06:37 +0000

    WIP Create Crust JNI loader FFI layer
    Bug: 327029117
    Test: m -j100, manual
    Flag: EXEMPT current implementation is no-op, flag will be added in
    Java code in future changes
    
    Change-Id: Ic3bf20b04427de28b8d3abcc7a3c08e28e45c12e

---
+++ system/gd/rust/crust/src/loader.h
@@ -0,0 +1,6 @@
+#ifndef CRUST_LOADER_H
+#define CRUST_LOADER_H
+
+bool load_initial_crust_jni();
+
+#endif  // CRUST_LOADER_H
---
+++ system/gd/rust/crust/src/loader.rs
@@ -0,0 +1,6 @@
+// use jni::JNIEnv;
+
+#[no_mangle]
+pub extern "C" fn load_initial_crust_jni() -> bool {
+    true
+}
