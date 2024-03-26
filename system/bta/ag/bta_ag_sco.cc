9484852dd49e6acd1f880be810264f81ec4a8b46
Yun-Hao Chung <howardchung@google.com>
2024-03-26 09:37:47 +0000

    Floss: Don't retry sco settings if we are not initiator
    It should be initiator's responsibility to retry the sco settings.
    
    This patch will be needed to pass HFP/HF/ACS/BI-13-C once we allow CVSD
    fallback to S1.
    
    Bug: 324157546
    Test: Run above qualification tests
    Test: m -j
    Tag: #floss
    Change-Id: I5500470dd3dea7e016978e3b29763978e1f8a84e

--- system/bta/ag/bta_ag_sco.cc
+++ system/bta/ag/bta_ag_sco.cc
@@ -232,7 +232,13 @@ static void bta_ag_sco_disc_cback(uint16_t sco_idx) {
       /* If SCO open was initiated by AG and failed for mSBC T2, try mSBC T1
        * 'Safe setting' first. If T1 also fails, try CVSD
        * same operations for LC3 settings */
-      if (bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb)) {
+      if (bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb) &&
+#if TARGET_FLOSS
+          (false ||
+#else
+          (!IS_FLAG_ENABLED(fix_sco_retry_on_not_local) ||
+#endif
+           bta_ag_cb.sco.is_local)) {
 #if TARGET_FLOSS
         if (true) {
 #else
@@ -269,7 +275,13 @@ static void bta_ag_sco_disc_cback(uint16_t sco_idx) {
           }
         }
       }
-    } else if (bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb)) {
+    } else if (bta_ag_sco_is_opening(bta_ag_cb.sco.p_curr_scb) &&
+#if TARGET_FLOSS
+               (false ||
+#else
+               (!IS_FLAG_ENABLED(fix_sco_retry_on_not_local) ||
+#endif
+                bta_ag_cb.sco.is_local)) {
       if (IS_FLAG_ENABLED(retry_esco_with_zero_retransmission_effort) &&
           bta_ag_cb.sco.p_curr_scb->retransmission_effort_retries == 0) {
         bta_ag_cb.sco.p_curr_scb->retransmission_effort_retries++;
