cd5d03024e4b0e9009e4383ebdb1bff50ad973c5
Jakub Pawlowski <jpawlowski@google.com>
2024-03-26 13:54:55 +0100

    Search end event to callback
    Bug: 330675788
    Flag: exempt, no logic changes
    Test: mma -j32
    Change-Id: I6f4ee6c01a7334d33b542c8982e0b4ee0c7e7b12

--- system/bta/test/bta_disc_test.cc
+++ system/bta/test/bta_disc_test.cc
@@ -237,26 +237,20 @@ TEST_F(BtaInitializedTest, bta_dm_disc_stop_device_discovery) {
 
 TEST_F(BtaInitializedTest,
        bta_dm_disc_start_service_discovery__BT_TRANSPORT_AUTO) {
-  bta_dm_disc_start_service_discovery(
-      {[](tBTA_DM_SEARCH_EVT event, tBTA_DM_SEARCH* p_data) {}, nullptr,
-       nullptr, nullptr},
-      kRawAddress, BT_TRANSPORT_AUTO);
+  bta_dm_disc_start_service_discovery({nullptr, nullptr, nullptr, nullptr},
+                                      kRawAddress, BT_TRANSPORT_AUTO);
 }
 
 TEST_F(BtaInitializedTest,
        bta_dm_disc_start_service_discovery__BT_TRANSPORT_BR_EDR) {
-  bta_dm_disc_start_service_discovery(
-      {[](tBTA_DM_SEARCH_EVT event, tBTA_DM_SEARCH* p_data) {}, nullptr,
-       nullptr, nullptr},
-      kRawAddress, BT_TRANSPORT_BR_EDR);
+  bta_dm_disc_start_service_discovery({nullptr, nullptr, nullptr, nullptr},
+                                      kRawAddress, BT_TRANSPORT_BR_EDR);
 }
 
 TEST_F(BtaInitializedTest,
        bta_dm_disc_start_service_discovery__BT_TRANSPORT_LE) {
-  bta_dm_disc_start_service_discovery(
-      {[](tBTA_DM_SEARCH_EVT event, tBTA_DM_SEARCH* p_data) {}, nullptr,
-       nullptr, nullptr},
-      kRawAddress, BT_TRANSPORT_LE);
+  bta_dm_disc_start_service_discovery({nullptr, nullptr, nullptr, nullptr},
+                                      kRawAddress, BT_TRANSPORT_LE);
 }
 
 TEST_F(BtaInitializedTest,
