949c60d5136e31c99846b0d93259fcc89973a64e
William Escande <wescande@google.com>
2024-03-26 19:21:12 -0700

    AdapterService: Fix fastBindToApp
    Some fields need to be initialized earlier when we are attempting the
    fast bind path
    
    Bug: 323672160
    Test: atest BluetoothInstrumentationTests with & without fastBindToApp
    Change-Id: I5e85c9c5b96b937fa79a9129b52fd106449d823e

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -621,6 +621,7 @@ public class AdapterService extends Service {
             return;
         }
         // OnCreate must perform the minimum of infaillible and mandatory initialization
+        mRemoteDevices = new RemoteDevices(this, mLooper);
         mAdapterProperties = new AdapterProperties(this);
         mAdapterStateMachine = new AdapterState(this, mLooper);
         mBinder = new AdapterServiceBinder(this);
@@ -629,6 +630,7 @@ public class AdapterService extends Service {
         mPowerManager = getNonNullSystemService(PowerManager.class);
         mBatteryStatsManager = getNonNullSystemService(BatteryStatsManager.class);
         mCompanionDeviceManager = getNonNullSystemService(CompanionDeviceManager.class);
+        setAdapterService(this);
     }
 
     private void init() {
@@ -644,9 +646,9 @@ public class AdapterService extends Service {
             mPowerManager = getNonNullSystemService(PowerManager.class);
             mBatteryStatsManager = getNonNullSystemService(BatteryStatsManager.class);
             mCompanionDeviceManager = getNonNullSystemService(CompanionDeviceManager.class);
+            mRemoteDevices = new RemoteDevices(this, mLooper);
         }
 
-        mRemoteDevices = new RemoteDevices(this, mLooper);
         clearDiscoveringPackages();
         if (!Flags.fastBindToApp()) {
             mBinder = new AdapterServiceBinder(this);
@@ -734,7 +736,9 @@ public class AdapterService extends Service {
 
         mBluetoothSocketManagerBinder = new BluetoothSocketManagerBinder(this);
 
-        setAdapterService(this);
+        if (!Flags.fastBindToApp()) {
+            setAdapterService(this);
+        }
 
         invalidateBluetoothCaches();
 
