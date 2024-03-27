0cbd047fd177ff11e8c85994ae8c70988feabd1e
Chris Manton <cmanton@google.com>
2024-03-27 13:49:14 -0700

    Add explicit methods to enact contextual callback
    Also Expand empty conditional
    
    Bug: 331661725
    Test: m .
    Flag: EXEMPT, Mechanical refactor
    Change-Id: I3c40264665e2cddc49db8b5a4cc3dbe6beff9004

--- system/gd/common/contextual_callback.h
+++ system/gd/common/contextual_callback.h
@@ -40,6 +40,15 @@ class ContextualOnceCallback<R(Args...)> {
   ContextualOnceCallback(ContextualOnceCallback&&) noexcept = default;
   ContextualOnceCallback& operator=(ContextualOnceCallback&&) noexcept = default;
 
+  void operator()(Args... args) {
+    context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
+    context_ = nullptr;
+  }
+
+  operator bool() const {
+    return context_ && callback_;
+  }
+
   void Invoke(Args... args) {
     context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
   }
@@ -79,6 +88,14 @@ class ContextualCallback<R(Args...)> {
   ContextualCallback& operator=(const ContextualCallback&) = default;
   ContextualCallback(ContextualCallback&&) noexcept = default;
   ContextualCallback& operator=(ContextualCallback&&) noexcept = default;
+
+  void operator()(Args... args) {
+    context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
+  }
+
+  operator bool() const {
+    return context_ && callback_;
+  }
 
   void Invoke(Args... args) {
     context_->Post(common::BindOnce(callback_, std::forward<Args>(args)...));
