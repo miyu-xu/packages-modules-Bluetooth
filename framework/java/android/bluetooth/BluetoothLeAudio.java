53db569836cbf594d68ddc98680dbaa5d8487088
Łukasz Rymanowski <rlukasz@google.com>
2024-03-27 09:03:15 +0000

    BluetoothLeAudio: Add MONO location
    As per errata, not used "invalid" location has been replaced with
    a new meaning: Mono
    
    Bug: 330847930
    Bug: 331139722
    Test: mmm packages/modules/Bluetooth
    
    Change-Id: Ibadaee63edd123ba4b31edade524344d6689135e

--- framework/java/android/bluetooth/BluetoothLeAudio.java
+++ framework/java/android/bluetooth/BluetoothLeAudio.java
@@ -370,6 +370,15 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
     @Deprecated @SystemApi public static final int AUDIO_LOCATION_INVALID = 0;
 
     /**
+     * This represents an Mono audio location.
+     *
+     * @hide
+     */
+    @FlaggedApi(Flags.FLAG_LEAUDIO_MONO_LOCATION_ERRATA)
+    @SystemApi
+    public static final int AUDIO_LOCATION_MONO = 0;
+
+    /**
      * This represents an audio location front left.
      *
      * @hide
@@ -570,7 +579,7 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
             flag = true,
             prefix = "AUDIO_LOCATION_",
             value = {
-                AUDIO_LOCATION_INVALID,
+                AUDIO_LOCATION_MONO,
                 AUDIO_LOCATION_FRONT_LEFT,
                 AUDIO_LOCATION_FRONT_RIGHT,
                 AUDIO_LOCATION_FRONT_CENTER,
