100870fc01ff523c9d019213e491f976c4acf1c6
WhaleChang <whalechang@google.com>
2024-03-28 19:13:41 +0800

    Floss: Implement Ring Central reject call functionality via Drop HID event
    Previously, Ring Central incoming calls could not
    be rejected from a headset.
    
    This change send Drop HID input event to reject
    the incoming call on Ring Central.
    
    Bug: 329304145
    Test: Conduct the following manual tests
      pair Bluetooth Headset
      login Ring Central and place an incoming-call
      reject the call by headset
    Test: atest bluetooth_test_gd
    Tag: #floss
    Flag: EXEMPT, no behavior change on Android; Floss-only changes
    
    Change-Id: I2307df0e22cf0e10bb295c0f812416663f553d88

--- system/gd/rust/linux/stack/src/bluetooth_media.rs
+++ system/gd/rust/linux/stack/src/bluetooth_media.rs
@@ -22,9 +22,9 @@ use bt_topshim::profiles::ProfileConnectionState;
 use bt_topshim::{metrics, topstack};
 use bt_utils::at_command_parser::{calculate_battery_percent, parse_at_command_data};
 use bt_utils::uhid_hfp::{
-    OutputEvent, UHidHfp, BLUETOOTH_TELEPHONY_UHID_REPORT_ID, UHID_INPUT_HOOK_SWITCH,
-    UHID_INPUT_PHONE_MUTE, UHID_OUTPUT_MUTE, UHID_OUTPUT_NONE, UHID_OUTPUT_OFF_HOOK,
-    UHID_OUTPUT_RING,
+    OutputEvent, UHidHfp, BLUETOOTH_TELEPHONY_UHID_REPORT_ID, UHID_INPUT_DROP,
+    UHID_INPUT_HOOK_SWITCH, UHID_INPUT_PHONE_MUTE, UHID_OUTPUT_MUTE, UHID_OUTPUT_NONE,
+    UHID_OUTPUT_OFF_HOOK, UHID_OUTPUT_RING,
 };
 use bt_utils::uinput::UInput;
 
@@ -1233,7 +1233,10 @@ impl BluetoothMedia {
             return;
         }
         if let Some(uhid) = self.uhid.get_mut(addr) {
-            let data = 0;
+            let mut data = 0;
+            if self.phone_state.state == CallState::Incoming {
+                data |= UHID_INPUT_DROP;
+            }
             info!("[{}]: UHID: Send hangup call hid report {}", DisplayAddress(&addr), data);
             match uhid.handle.send_input(data) {
                 Err(e) => log::error!(
