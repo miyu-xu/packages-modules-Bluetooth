b754ca09fa657e74f602e654546d0ed9b8406e38
yamei du <yameidu@amazon.com>
2024-03-29 05:38:41 +0000

    One PM service for each A2DP device
    A2DP allocates 2 services in bta_dm_conn_srvcs in PM. One service is
    allocated when A2DP is connected, with p_scb->app_id as the app_id.
    The other service is allocated when streaming starts, with
    bta_av_cb.audio_open_cnt as the app_id.
    In scenarios with multiple A2DP devices, the value of
    bta_av_cb.audio_open_cnt changes when A2DP devices are connected or
    disconnected. As a result, for the second service of the same A2DP
    device, the app_id passed by AV to the PM may change over time. This
    variation in app_ids causes the PM to consider a new service
    allocation is required, leading PM service leakge. Even worse, in
    current code, the second service is not closed and remains in IDLE
    state when A2DP is disconnected, this causes unnecessary PM timer
    restarts, if the link is down and then re-established before the
    timer expires, the new established link will enter sinff mode
    unexpectedly when the timer expires.
    Actually, for A2DP one PM service is enough.
    
    Bug: 332382763
    Test: m com.android.btservices
    Test: Manual
    Get connected with 2 A2DP devices, play music, switch active device
    then initiate disconnection.
    
    Change-Id: I571223ce50e23d89575a81857fd4fefcb493d40f

--- system/bta/av/bta_av_aact.cc
+++ system/bta/av/bta_av_aact.cc
@@ -621,7 +621,7 @@ void bta_av_role_res(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
       p_scb->wait &= ~BTA_AV_WAIT_ROLE_SW_BITS;
       if (p_data->role_res.hci_status != HCI_SUCCESS) {
         p_scb->role &= ~BTA_AV_ROLE_START_INT;
-        bta_sys_idle(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+        bta_sys_idle(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
         /* start failed because of role switch. */
         tBTA_AV bta_av_data = {
                 .start =
@@ -1834,7 +1834,7 @@ void bta_av_do_start(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   }
 
   p_scb->role |= BTA_AV_ROLE_START_INT;
-  bta_sys_busy(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+  bta_sys_busy(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
   /* disallow role switch during streaming, only if we are the central role
    * i.e. allow role switch, if we are peripheral.
    * It would not hurt us, if the peer device wants us to be central
@@ -1875,7 +1875,7 @@ void bta_av_str_stopped(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   log::info("peer {} bta_handle:0x{:x} audio_open_cnt:{}, p_data {} start:{}", p_scb->PeerAddress(),
             p_scb->hndl, bta_av_cb.audio_open_cnt, fmt::ptr(p_data), start);
 
-  bta_sys_idle(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+  bta_sys_idle(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
   BTM_unblock_role_switch_and_sniff_mode_for(p_scb->PeerAddress());
 
   if (p_scb->co_started) {
@@ -2228,7 +2228,7 @@ void bta_av_start_ok(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   if (p_data && (p_data->hdr.offset != BTA_AV_RS_NONE)) {
     p_scb->wait &= ~BTA_AV_WAIT_ROLE_SW_BITS;
     if (p_data->hdr.offset == BTA_AV_RS_FAIL) {
-      bta_sys_idle(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+      bta_sys_idle(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
       tBTA_AV bta_av_data = {
               .start =
                       {
@@ -2277,7 +2277,7 @@ void bta_av_start_ok(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   /* tell role manager to check M/S role */
   bta_sys_conn_open(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
 
-  bta_sys_busy(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+  bta_sys_busy(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
 
   if (p_scb->media_type == AVDT_MEDIA_TYPE_AUDIO) {
     /* in normal logic, conns should be bta_av_cb.audio_count - 1,
@@ -2375,7 +2375,7 @@ void bta_av_start_failed(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* /* p_data */) {
              p_scb->co_started);
 
   if (!p_scb->started && !p_scb->co_started) {
-    bta_sys_idle(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+    bta_sys_idle(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
     notify_start_failed(p_scb);
   }
 
@@ -2503,7 +2503,7 @@ void bta_av_suspend_cfm(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
     p_scb->cong = false;
   }
 
-  bta_sys_idle(BTA_ID_AV, bta_av_cb.audio_open_cnt, p_scb->PeerAddress());
+  bta_sys_idle(BTA_ID_AV, p_scb->app_id, p_scb->PeerAddress());
   BTM_unblock_role_switch_and_sniff_mode_for(p_scb->PeerAddress());
 
   /* in case that we received suspend_ind, we may need to call co_stop here */
