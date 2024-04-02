5283dbca9e6203b9f318d48ad74b39b1e8286f95
Jeremy Wu <jrwu@google.com>
2024-04-02 06:17:15 +0000

    TEST: notify exact group stream status to audio server
    WIP
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: Ib5b61f1dd3fe15c7b07ab81b67ea416c71ee58bc

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -5463,7 +5463,11 @@ class LeAudioClientImpl : public LeAudioClient {
       return;
     }
 
+#ifdef TARGET_FLOSS
+    GroupStreamStatus newGroupStreamStatus = groupStreamStatus;
+#else
     GroupStreamStatus newGroupStreamStatus = GroupStreamStatus::IDLE;
+#endif
     if (groupStreamStatus == GroupStreamStatus::STREAMING) {
       newGroupStreamStatus = GroupStreamStatus::STREAMING;
     }
@@ -5609,6 +5613,7 @@ class LeAudioClientImpl : public LeAudioClient {
          * STREAMING. Peer device uses cache. For the moment
          * it is handled same as IDLE
          */
+        [[fallthrough]];
       case GroupStreamStatus::IDLE: {
         if (sw_enc_left) sw_enc_left.reset();
         if (sw_enc_right) sw_enc_right.reset();
