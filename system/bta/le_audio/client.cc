b293a2d5194c36b77e5e27cf78af099ff739cb1a
Jeremy Wu <jrwu@google.com>
2024-04-02 06:17:15 +0000

    Floss: notify exact group stream status to audio server
    As being pointed out in a TODO comment in `le_audio_client_test.cc` that
    "We should have a way to confirm Stop() otherwise, audio framework might
    have different state that it is in the le_audio code", the audio server
    in CrOS needs to be notified when the group is released (i.e., CIS is
    teared down) so that it is allowed to start another one.
    
    `GroupStreamStatus` appears to be very close to meeting the demand,
    despite the API collapses all states to either `STREAMING` or everything
    else as `IDLE`. This masks the `RELEASING` state as `IDLE` while we need
    to reliably distinguish between the two states (we cannot establish
    CISes while it is in `RELEASING` state).
    
    Since it's unclear how this might affect subscribers of this callback
    in Android, while there is no other more suitable alternative for Floss,
    we make this change for Floss-only.
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: Ib5b61f1dd3fe15c7b07ab81b67ea416c71ee58bc

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -5460,7 +5460,11 @@ class LeAudioClientImpl : public LeAudioClient {
       return;
     }
 
+#ifdef TARGET_FLOSS
+    GroupStreamStatus newGroupStreamStatus = groupStreamStatus;
+#else
     GroupStreamStatus newGroupStreamStatus = GroupStreamStatus::IDLE;
+#endif
     if (groupStreamStatus == GroupStreamStatus::STREAMING) {
       newGroupStreamStatus = GroupStreamStatus::STREAMING;
     }
@@ -5606,6 +5610,7 @@ class LeAudioClientImpl : public LeAudioClient {
          * STREAMING. Peer device uses cache. For the moment
          * it is handled same as IDLE
          */
+        [[fallthrough]];
       case GroupStreamStatus::IDLE: {
         if (sw_enc_left) sw_enc_left.reset();
         if (sw_enc_right) sw_enc_right.reset();
