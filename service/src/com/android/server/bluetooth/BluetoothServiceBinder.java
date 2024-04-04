74f865510c459539f1cbc65fa0646c8a52208d2c
William Escande <wescande@google.com>
2024-04-03 17:38:15 -0700

    SystemServer: Messenger for autoOnFeature
    ServiceMessenger Explanation:
    Since the messenger will be using the correct looper, it can directly
    call the sync method from the ManagerService.
    ~
    This CL is implementing the messenger for autoOnFeature
    ~
    
    Bug: 288450479
    Bug: 321804999
    Test: atest ServiceBluetoothRoboTests
    Test: m service-bluetooth | No-op change
          Flag testing will be done in follow-up, before flag rollout
    Change-Id: I66a5dbbe4cc8faa7a12a3d45b6c892cae481e484

--- service/src/com/android/server/bluetooth/BluetoothServiceBinder.java
+++ service/src/com/android/server/bluetooth/BluetoothServiceBinder.java
@@ -388,6 +388,9 @@ class BluetoothServiceBinder extends IBluetoothManager.Stub {
     @Override
     @RequiresPermission(BLUETOOTH_PRIVILEGED)
     public boolean isAutoOnSupported() {
+        if (Flags.systemServerMessenger()) {
+            throw new IllegalStateException("Binder call unavailable when using messenger");
+        }
         BtPermissionUtils.enforcePrivileged(mContext);
         return mBluetoothManagerService.isAutoOnSupported();
     }
@@ -395,6 +398,9 @@ class BluetoothServiceBinder extends IBluetoothManager.Stub {
     @Override
     @RequiresPermission(BLUETOOTH_PRIVILEGED)
     public boolean isAutoOnEnabled() {
+        if (Flags.systemServerMessenger()) {
+            throw new IllegalStateException("Binder call unavailable when using messenger");
+        }
         BtPermissionUtils.enforcePrivileged(mContext);
         return mBluetoothManagerService.isAutoOnEnabled();
     }
@@ -403,6 +409,9 @@ class BluetoothServiceBinder extends IBluetoothManager.Stub {
     @RequiresPermission(BLUETOOTH_PRIVILEGED)
     @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
     public void setAutoOnEnabled(boolean status) {
+        if (Flags.systemServerMessenger()) {
+            throw new IllegalStateException("Binder call unavailable when using messenger");
+        }
         BtPermissionUtils.enforcePrivileged(mContext);
         mBluetoothManagerService.setAutoOnEnabled(status);
     }
