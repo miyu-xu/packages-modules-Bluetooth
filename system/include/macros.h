4777591b98506fbaa09b59cd75fb5b7ee53fa4dd
Myles Watson <mylesgw@google.com>
2024-04-05 04:57:59 -0700

    Use stringstream to build enum cases
    Bug: 330709932
    Test: mma -j32
    Flag: EXEMPT, logging-only change
    Change-Id: Ie6728e643c895b2d15cb198cdb0ec3e9281b6df8

--- system/include/macros.h
+++ system/include/macros.h
@@ -16,6 +16,27 @@
 
 #pragma once
 
+#include <ios>
+#include <sstream>
+#include <string>
+
 #define CASE_RETURN_TEXT(code) \
   case code:                   \
     return #code
+
+#define CASE_RETURN_STRING(enumerator)                     \
+  case enumerator:                                         \
+    return []() {                                          \
+      std::stringstream builder;                           \
+      builder << #enumerator << "(0x" << std::hex          \
+              << static_cast<uint64_t>(enumerator) << ")"; \
+      return builder.str();                                \
+    }()
+
+#define RETURN_UNKNOWN_TYPE_STRING(type, variable)      \
+  return [variable]() {                                 \
+    std::stringstream builder;                          \
+    builder << "Unknown " << #type << "(0x" << std::hex \
+            << static_cast<uint64_t>(variable) << ")";  \
+    return builder.str();                               \
+  }()
