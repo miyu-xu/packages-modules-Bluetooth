c32b255fcec8f0ade80c0cadfc286e160c5483ba
Henri Chataing <henrichataing@google.com>
2024-04-05 17:27:11 -0700

    Inline DISALLOW_COPY_AND_ASSIGN
    Used in one location only, defined twice
    
    Bug: 331817295
    Test: m com.android.btservices
    Flag: EXEMPT, macro inlining
    Change-Id: I4c992ed51ed2df258a69bf7e8aeac6e2317c970b

--- android/app/jni/com_android_bluetooth.h
+++ android/app/jni/com_android_bluetooth.h
@@ -124,7 +124,8 @@ private:
     JNIEnv *mCallbackEnv;
     const char *mName;
 
-    DISALLOW_COPY_AND_ASSIGN(CallbackEnv);
+    CallbackEnv(const CallbackEnv&) = delete;
+    void CallbackEnv=(const CallbackEnv&) = delete;
 };
 
 const bt_interface_t* getBluetoothInterface();
