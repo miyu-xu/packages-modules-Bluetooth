950ba80f20a4f3a548b291517372d2731219f021
Jeremy Wu <jrwu@google.com>
2024-04-07 12:24:01 +0000

    Floss: address clock drift in asrc
    Since ISO packets are sent out at fixed intervals, the |RepeatingTimer|
    utility determines an important time domain.
    
    By default, it uses NTP-adjusted clocks, the difference between the
    timestamps of which is compatible with
    |CLOCK_REALTIME/MONOTONIC/BOOTTIME| and such, aligning with Android's
    audio server.
    
    However, CrAS uses |CLOCK_MONOTONIC_RAW|, which is another time domain,
    unaffected by NTP, and this can cause xruns when the clock drift
    accumulates, which does relatively quickly.
    
    This CL adds a parameter to |RepeatingTimer| to allow scheduling based
    on |CLOCK_MONOTONIC_RAW|, where despite each |DelayedCallback| can still
    drift (since libchrome schedulers use NTP-adjusted clocks), in the long
    run they are aligned.
    
    Bug: 333334341
    Test: m Bluetooth
    Flag: EXEMPT floss only changes
    
    Change-Id: Ie90952138d53a67171f526db55edd604306a96f6

--- system/bta/le_audio/audio_hal_client/audio_source_hal_client.cc
+++ system/bta/le_audio/audio_hal_client/audio_source_hal_client.cc
@@ -74,7 +74,11 @@ class SourceImpl : public LeAudioSourceAudioHalClient {
   // Internal functionality
   SourceImpl(bool is_broadcaster)
       : le_audio_sink_hal_state_(HAL_UNINITIALIZED),
-        is_broadcaster_(is_broadcaster){};
+#ifdef TARGET_FLOSS
+        audio_timer_(/* is_clock_monotonic_raw= */ true),
+#endif
+        is_broadcaster_(is_broadcaster) {
+  }
   ~SourceImpl() override {
     if (le_audio_sink_hal_state_ != HAL_UNINITIALIZED) Release();
   }
