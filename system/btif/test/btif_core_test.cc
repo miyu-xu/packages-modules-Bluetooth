c5acc3ddffb1e310a39c48f5173889994e4c65a1
Chris Manton <cmanton@google.com>
2024-04-09 12:26:29 -0700

    net_test_btif_stack: Add jni thread for callback tests
    Bug: 333614197
    Test: atest net_test_btif_stack
    Flag: EXEMPT, Test Infrastructure
    
    Change-Id: I8b00d236e9b786aab8502dfad00a6dcf2433a5f2

--- system/btif/test/btif_core_test.cc
+++ system/btif/test/btif_core_test.cc
@@ -29,6 +29,7 @@
 #include "btif/include/btif_api.h"
 #include "btif/include/btif_common.h"
 #include "btif/include/btif_util.h"
+#include "common/message_loop_thread.h"
 #include "hci/controller_interface_mock.h"
 #include "include/hardware/bluetooth.h"
 #include "include/hardware/bt_av.h"
@@ -108,6 +109,9 @@ void generate_local_oob_data_callback(tBT_TRANSPORT /* transport */,
                                       bt_oob_data_t /* oob_data */) {}
 void switch_buffer_size_callback(bool /* is_low_latency_buffer_size */) {}
 void switch_codec_callback(bool /* is_low_latency_buffer_size */) {}
+void le_rand_callback(uint64_t /* random */) {}
+void key_missing_callback(const RawAddress /* bd_addr */) {}
+
 #undef TESTCB
 
 bt_callbacks_t callbacks = {
@@ -131,15 +135,36 @@ bt_callbacks_t callbacks = {
     .generate_local_oob_data_cb = generate_local_oob_data_callback,
     .switch_buffer_size_cb = switch_buffer_size_callback,
     .switch_codec_cb = switch_codec_callback,
+    .le_rand_cb = le_rand_callback,
+    .key_missing_cb = key_missing_callback,
 };
 
 }  // namespace
+
+static bluetooth::common::MessageLoopThread jni_thread("bt_jni_thread");
 
 class BtifCoreTest : public ::testing::Test {
  protected:
   void SetUp() override {
     callback_map_.clear();
     bluetooth::hci::testing::mock_controller_ = &controller_;
+    jni_thread.StartUp();
+  }
+
+  void TearDown() override {
+    jni_thread.ShutDown();
+    bluetooth::hci::testing::mock_controller_ = nullptr;
+    callback_map_.erase("callback_thread_event");
+  }
+  bluetooth::hci::testing::MockControllerInterface controller_;
+};
+
+TEST_F(BtifCoreTest, default_callbacks) {}
+
+class BtifInitializedTest : public BtifCoreTest {
+ protected:
+  void SetUp() override {
+    BtifCoreTest::SetUp();
     set_hal_cbacks(&callbacks);
     auto promise = std::promise<void>();
     auto future = promise.get_future();
@@ -159,27 +184,26 @@ class BtifCoreTest : public ::testing::Test {
     };
     CleanCoreInterface();
     ASSERT_EQ(std::future_status::ready, future.wait_for(timeout_time));
-    bluetooth::hci::testing::mock_controller_ = nullptr;
-    callback_map_.erase("callback_thread_event");
+    BtifCoreTest::TearDown();
   }
   bluetooth::hci::testing::MockControllerInterface controller_;
 };
 
-class BtifCoreWithControllerTest : public BtifCoreTest {
+class BtifCoreWithControllerTest : public BtifInitializedTest {
   void SetUp() override {
-    BtifCoreTest::SetUp();
+    BtifInitializedTest::SetUp();
     ON_CALL(controller_, SupportsSniffSubrating).WillByDefault(Return(true));
   }
 
-  void TearDown() override { BtifCoreTest::TearDown(); }
+  void TearDown() override { BtifInitializedTest::TearDown(); }
 };
 
 std::promise<int> promise0;
 void callback0(int val) { promise0.set_value(val); }
 
-TEST_F(BtifCoreTest, test_nop) {}
+TEST_F(BtifInitializedTest, test_nop) {}
 
-TEST_F(BtifCoreTest, test_post_on_bt_simple0) {
+TEST_F(BtifInitializedTest, test_post_on_bt_simple0) {
   const int val = 123;
   promise0 = std::promise<int>();
   std::future<int> future0 = promise0.get_future();
@@ -188,14 +212,14 @@ TEST_F(BtifCoreTest, test_post_on_bt_simple0) {
   ASSERT_EQ(val, future0.get());
 }
 
-TEST_F(BtifCoreTest, test_post_on_bt_jni_simple1) {
+TEST_F(BtifInitializedTest, test_post_on_bt_jni_simple1) {
   std::promise<void> promise;
   std::future<void> future = promise.get_future();
   post_on_bt_jni([=, &promise]() { promise.set_value(); });
   ASSERT_EQ(std::future_status::ready, future.wait_for(timeout_time));
 }
 
-TEST_F(BtifCoreTest, test_post_on_bt_jni_simple2) {
+TEST_F(BtifInitializedTest, test_post_on_bt_jni_simple2) {
   std::promise<void> promise;
   std::future<void> future = promise.get_future();
   BtJniClosure closure = [&promise]() { promise.set_value(); };
@@ -203,7 +227,7 @@ TEST_F(BtifCoreTest, test_post_on_bt_jni_simple2) {
   ASSERT_EQ(std::future_status::ready, future.wait_for(timeout_time));
 }
 
-TEST_F(BtifCoreTest, test_post_on_bt_jni_simple3) {
+TEST_F(BtifInitializedTest, test_post_on_bt_jni_simple3) {
   const int val = 456;
   std::promise<int> promise;
   auto future = promise.get_future();
@@ -214,7 +238,7 @@ TEST_F(BtifCoreTest, test_post_on_bt_jni_simple3) {
 }
 
 extern const char* dump_av_sm_event_name(int event);
-TEST_F(BtifCoreTest, dump_av_sm_event_name) {
+TEST_F(BtifInitializedTest, dump_av_sm_event_name) {
   std::vector<std::pair<int, std::string>> events = {
       std::make_pair(BTA_AV_ENABLE_EVT, "BTA_AV_ENABLE_EVT"),
       std::make_pair(BTA_AV_REGISTER_EVT, "BTA_AV_REGISTER_EVT"),
@@ -251,7 +275,7 @@ TEST_F(BtifCoreTest, dump_av_sm_event_name) {
                dump_av_sm_event_name(std::numeric_limits<int>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_dm_search_event) {
+TEST_F(BtifInitializedTest, dump_dm_search_event) {
   std::vector<std::pair<uint16_t, std::string>> events = {
       std::make_pair(BTA_DM_INQ_RES_EVT, "BTA_DM_INQ_RES_EVT"),
       std::make_pair(BTA_DM_INQ_CMPL_EVT, "BTA_DM_INQ_CMPL_EVT"),
@@ -269,7 +293,7 @@ TEST_F(BtifCoreTest, dump_dm_search_event) {
                dump_dm_search_event(std::numeric_limits<uint16_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_property_type) {
+TEST_F(BtifInitializedTest, dump_property_type) {
   std::vector<std::pair<bt_property_type_t, std::string>> types = {
       std::make_pair(BT_PROPERTY_BDNAME, "BT_PROPERTY_BDNAME"),
       std::make_pair(BT_PROPERTY_BDADDR, "BT_PROPERTY_BDADDR"),
@@ -297,7 +321,7 @@ TEST_F(BtifCoreTest, dump_property_type) {
                    std::numeric_limits<uint16_t>::max())));
 }
 
-TEST_F(BtifCoreTest, dump_dm_event) {
+TEST_F(BtifInitializedTest, dump_dm_event) {
   std::vector<std::pair<uint8_t, std::string>> events = {
       std::make_pair(BTA_DM_PIN_REQ_EVT, "BTA_DM_PIN_REQ_EVT"),
       std::make_pair(BTA_DM_AUTH_CMPL_EVT, "BTA_DM_AUTH_CMPL_EVT"),
@@ -329,7 +353,7 @@ TEST_F(BtifCoreTest, dump_dm_event) {
                dump_dm_event(std::numeric_limits<uint8_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_hf_event) {
+TEST_F(BtifInitializedTest, dump_hf_event) {
   std::vector<std::pair<uint8_t, std::string>> events = {
       std::make_pair(BTA_AG_ENABLE_EVT, "BTA_AG_ENABLE_EVT"),
       std::make_pair(BTA_AG_REGISTER_EVT, "BTA_AG_REGISTER_EVT"),
@@ -374,7 +398,7 @@ TEST_F(BtifCoreTest, dump_hf_event) {
                dump_hf_event(std::numeric_limits<uint8_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_hf_client_event) {
+TEST_F(BtifInitializedTest, dump_hf_client_event) {
   std::vector<std::pair<int, std::string>> events = {
       std::make_pair(BTA_HF_CLIENT_ENABLE_EVT, "BTA_HF_CLIENT_ENABLE_EVT"),
       std::make_pair(BTA_HF_CLIENT_REGISTER_EVT, "BTA_HF_CLIENT_REGISTER_EVT"),
@@ -418,7 +442,7 @@ TEST_F(BtifCoreTest, dump_hf_client_event) {
                dump_hf_client_event(std::numeric_limits<uint16_t>::max()));
 }
 
-TEST_F(BtifCoreTest, bta_hh_event_text) {
+TEST_F(BtifInitializedTest, bta_hh_event_text) {
   std::vector<std::pair<int, std::string>> events = {
       std::make_pair(BTA_HH_EMPTY_EVT, "BTA_HH_EMPTY_EVT"),
       std::make_pair(BTA_HH_ENABLE_EVT, "BTA_HH_ENABLE_EVT"),
@@ -446,7 +470,7 @@ TEST_F(BtifCoreTest, bta_hh_event_text) {
   ASSERT_EQ(oss.str(), bta_hh_event_text(unknown_evt));
 }
 
-TEST_F(BtifCoreTest, dump_hd_event) {
+TEST_F(BtifInitializedTest, dump_hd_event) {
   std::vector<std::pair<uint16_t, std::string>> events = {
       std::make_pair(BTA_HD_ENABLE_EVT, "BTA_HD_ENABLE_EVT"),
       std::make_pair(BTA_HD_DISABLE_EVT, "BTA_HD_DISABLE_EVT"),
@@ -471,7 +495,7 @@ TEST_F(BtifCoreTest, dump_hd_event) {
                dump_hd_event(std::numeric_limits<uint16_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_thread_evt) {
+TEST_F(BtifInitializedTest, dump_thread_evt) {
   std::vector<std::pair<bt_cb_thread_evt, std::string>> events = {
       std::make_pair(ASSOCIATE_JVM, "ASSOCIATE_JVM"),
       std::make_pair(DISASSOCIATE_JVM, "DISASSOCIATE_JVM"),
@@ -485,7 +509,7 @@ TEST_F(BtifCoreTest, dump_thread_evt) {
                                       std::numeric_limits<uint16_t>::max())));
 }
 
-TEST_F(BtifCoreTest, dump_av_conn_state) {
+TEST_F(BtifInitializedTest, dump_av_conn_state) {
   std::vector<std::pair<uint16_t, std::string>> events = {
       std::make_pair(BTAV_CONNECTION_STATE_DISCONNECTED,
                      "BTAV_CONNECTION_STATE_DISCONNECTED"),
@@ -505,7 +529,7 @@ TEST_F(BtifCoreTest, dump_av_conn_state) {
                dump_av_conn_state(std::numeric_limits<uint16_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_av_audio_state) {
+TEST_F(BtifInitializedTest, dump_av_audio_state) {
   std::vector<std::pair<uint16_t, std::string>> events = {
       std::make_pair(BTAV_AUDIO_STATE_REMOTE_SUSPEND,
                      "BTAV_AUDIO_STATE_REMOTE_SUSPEND"),
@@ -521,7 +545,7 @@ TEST_F(BtifCoreTest, dump_av_audio_state) {
                dump_av_audio_state(std::numeric_limits<uint16_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_adapter_scan_mode) {
+TEST_F(BtifInitializedTest, dump_adapter_scan_mode) {
   std::vector<std::pair<bt_scan_mode_t, std::string>> events = {
       std::make_pair(BT_SCAN_MODE_NONE, "BT_SCAN_MODE_NONE"),
       std::make_pair(BT_SCAN_MODE_CONNECTABLE, "BT_SCAN_MODE_CONNECTABLE"),
@@ -538,7 +562,7 @@ TEST_F(BtifCoreTest, dump_adapter_scan_mode) {
                    std::numeric_limits<int>::max())));
 }
 
-TEST_F(BtifCoreTest, dump_bt_status) {
+TEST_F(BtifInitializedTest, dump_bt_status) {
   std::vector<std::pair<bt_status_t, std::string>> events = {
       std::make_pair(BT_STATUS_SUCCESS, "BT_STATUS_SUCCESS"),
       std::make_pair(BT_STATUS_FAIL, "BT_STATUS_FAIL"),
@@ -556,7 +580,7 @@ TEST_F(BtifCoreTest, dump_bt_status) {
                                       std::numeric_limits<int>::max())));
 }
 
-TEST_F(BtifCoreTest, dump_rc_event) {
+TEST_F(BtifInitializedTest, dump_rc_event) {
   std::vector<std::pair<uint8_t, std::string>> events = {
       std::make_pair(BTA_AV_RC_OPEN_EVT, "BTA_AV_RC_OPEN_EVT"),
       std::make_pair(BTA_AV_RC_CLOSE_EVT, "BTA_AV_RC_CLOSE_EVT"),
@@ -579,7 +603,7 @@ TEST_F(BtifCoreTest, dump_rc_event) {
                dump_rc_event(std::numeric_limits<uint8_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_rc_notification_event_id) {
+TEST_F(BtifInitializedTest, dump_rc_notification_event_id) {
   std::vector<std::pair<uint8_t, std::string>> events = {
       std::make_pair(AVRC_EVT_PLAY_STATUS_CHANGE,
                      "AVRC_EVT_PLAY_STATUS_CHANGE"),
@@ -613,7 +637,7 @@ TEST_F(BtifCoreTest, dump_rc_notification_event_id) {
                                       std::numeric_limits<uint8_t>::max()));
 }
 
-TEST_F(BtifCoreTest, dump_rc_pdu) {
+TEST_F(BtifInitializedTest, dump_rc_pdu) {
   std::vector<std::pair<uint8_t, std::string>> pdus = {
       std::make_pair(AVRC_PDU_LIST_PLAYER_APP_ATTR,
                      "AVRC_PDU_LIST_PLAYER_APP_ATTR"),
