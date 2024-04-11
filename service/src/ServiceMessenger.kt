570bfa4070fe3369a0f1438d070f03da13457ced
William Escande <wescande@google.com>
2024-04-10 18:09:52 -0700

    SystemServer: Messenger for getAddress
    ServiceMessenger Explanation:
    Since the messenger will be using the correct looper, it can directly
    call the sync method from the ManagerService.
    ~
    This CL is implementing the messenger for getAddress
    ~
    
    Bug: 288450479
    Bug: 321804999
    Test: atest ServiceBluetoothRoboTests
    Test: m service-bluetooth | No-op change
          Flag testing will be done in follow-up, before flag rollout
    Change-Id: I665196bf7fb8bedf617504780994aada80e92ec2

--- service/src/ServiceMessenger.kt
+++ service/src/ServiceMessenger.kt
@@ -15,6 +15,7 @@
  */
 package com.android.server.bluetooth
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -112,6 +113,20 @@ internal class ServiceMessenger(
                         } catch (e: PermissionChecker.BluetoothPermissionException) {
                             Log.e(TAG, "${obj}: FAILED", e)
                             false
+                        }
+                }
+            }
+            is SystemServiceMessage.GetAddress -> {
+                val source = obj.attributionSource
+
+                SystemServiceMessage.GetAddress.Reply().apply {
+                    value =
+                        try {
+                            checker.getAddressAllowed(sendingUid, source)
+                            managerService.getAddress_sync()
+                        } catch (e: PermissionChecker.BluetoothPermissionException) {
+                            Log.e(TAG, "${obj}: FAILED", e)
+                            BluetoothAdapter.DEFAULT_MAC_ADDRESS
                         }
                 }
             }
