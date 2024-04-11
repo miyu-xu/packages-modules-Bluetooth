a48f32c25f465bc298ca261e918ee503abea2265
Himanshu Rawat <rwt@google.com>
2024-04-11 23:45:24 +0000

    Renaming some local variables for readability
    Test: mmm packages/modules/Bluetooth
    Bug: 322360482
    Flag: EXEMPT no logical changes
    Change-Id: I8692eeb502dc58f53a59f984837218ab109f98f3

--- android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -668,36 +668,40 @@ class AdapterProperties {
         }
     }
 
-    void cleanupPrevBondRecordsFor(BluetoothDevice currentDevice) {
-        String currentAddress = currentDevice.getAddress();
-        String currentBrEdrAddress =
+    void cleanupPrevBondRecordsFor(BluetoothDevice device) {
+        String address = device.getAddress();
+        String identityAddress =
                 Flags.identityAddressNullIfUnknown()
-                        ? Utils.getBrEdrAddress(currentDevice, mService)
-                        : mService.getIdentityAddress(currentAddress);
-        debugLog("cleanupPrevBondRecordsFor: " + currentDevice);
-        if (currentBrEdrAddress == null) {
+                        ? Utils.getBrEdrAddress(device, mService)
+                        : mService.getIdentityAddress(address);
+        debugLog("cleanupPrevBondRecordsFor: " + device);
+        if (identityAddress == null) {
             return;
         }
 
-        for (BluetoothDevice device : mBondedDevices) {
-            String address = device.getAddress();
-            String brEdrAddress =
+        for (BluetoothDevice existingDevice : mBondedDevices) {
+            String existingAddress = existingDevice.getAddress();
+            String existingIdentityAddress =
                     Flags.identityAddressNullIfUnknown()
-                            ? Utils.getBrEdrAddress(device, mService)
-                            : mService.getIdentityAddress(address);
-            if (currentBrEdrAddress.equals(brEdrAddress) && !currentAddress.equals(address)) {
-                if (mService.getNative()
-                        .removeBond(Utils.getBytesFromAddress(device.getAddress()))) {
-                    mBondedDevices.remove(device);
-                    infoLog("Removing old bond record: "
-                                    + device
+                            ? Utils.getBrEdrAddress(existingDevice, mService)
+                            : mService.getIdentityAddress(existingAddress);
+
+            if (identityAddress.equals(existingIdentityAddress)
+                    && !address.equals(existingAddress)) {
+                if (mService.getNative().removeBond(Utils.getBytesFromAddress(existingAddress))) {
+                    mBondedDevices.remove(existingDevice);
+                    infoLog(
+                            "Removing old bond record: "
+                                    + existingDevice
                                     + " for current device: "
-                                    + currentDevice);
+                                    + device);
                 } else {
-                    Log.e(TAG, "Unexpected error while removing old bond record:"
-                                    + device
+                    Log.e(
+                            TAG,
+                            "Unexpected error while removing old bond record:"
+                                    + existingDevice
                                     + " for current device: "
-                                    + currentDevice);
+                                    + device);
                 }
                 break;
             }
