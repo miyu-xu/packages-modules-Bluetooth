9d15d42df8e442daced62aefdf77bf68d2c464cd
William Escande <wescande@google.com>
2024-04-10 18:09:52 -0700

    SystemServer: Messenger for getAddress
    ServiceMessenger Explanation:
    Since the messenger will be using the correct looper, it can directly
    call the sync method from the ManagerService.
    ~
    This CL is implementing the messenger for getAddress
    ~
    
    Bug: 288450479
    Bug: 321804999
    Test: atest ServiceBluetoothRoboTests
    Test: m service-bluetooth | No-op change
          Flag testing will be done in follow-up, before flag rollout
    Change-Id: I665196bf7fb8bedf617504780994aada80e92ec2

--- service/src/com/android/server/bluetooth/BluetoothServiceBinder.java
+++ service/src/com/android/server/bluetooth/BluetoothServiceBinder.java
@@ -223,6 +223,9 @@ class BluetoothServiceBinder extends IBluetoothManager.Stub {
 
     @Override
     public String getAddress(AttributionSource source) {
+        if (Flags.systemServerMessenger()) {
+            throw new IllegalStateException("Binder call unavailable when using messenger");
+        }
         requireNonNull(source, "AttributionSource cannot be null in getAddress");
 
         if (!checkConnectPermissionForDataDelivery(
