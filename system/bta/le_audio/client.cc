870adae3a186959482824ea8953e9560c965fa13
David Duarte <licorne@google.com>
2024-04-11 18:02:38 +0000

    Re-enable -Wformat as error
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT, build change
    Bug: 315250603
    Change-Id: Ia4b367609f278103494c3870b17bea6f127b1f7c

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -3853,9 +3853,9 @@ class LeAudioClientImpl : public LeAudioClient {
             (reconnection_mode_ == BTM_BLE_BKG_CONNECT_ALLOW_LIST
                  ? "Allow List"
                  : "Targeted Announcements"));
-    dprintf(fd, "  configuration: %s  (0x%08hx)\n",
+    dprintf(fd, "  configuration: %s  (0x%08x)\n",
             bluetooth::common::ToString(configuration_context_type_).c_str(),
-            configuration_context_type_);
+            static_cast<unsigned>(configuration_context_type_));
     dprintf(fd, "  local source metadata context type mask: %s\n",
             local_metadata_context_types_.source.to_string().c_str());
     dprintf(fd, "  local sink metadata context type mask: %s\n",
@@ -3865,7 +3865,7 @@ class LeAudioClientImpl : public LeAudioClient {
             sink_monitor_mode_ ? "true" : "false");
     if (sink_monitor_notified_status_) {
       dprintf(fd, "  Local sink notified state: %d\n",
-              sink_monitor_notified_status_.value());
+              static_cast<int>(sink_monitor_notified_status_.value()));
     }
     dprintf(fd, "  Source monitor mode: %s\n",
             source_monitor_mode_ ? "true" : "false");
