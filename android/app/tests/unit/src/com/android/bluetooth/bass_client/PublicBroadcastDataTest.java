1e799430b63db9e99099957d30bb48b53872a2ba
Michal Belusiak <belusiak@google.com>
2024-04-16 12:10:17 +0000

    PublicBroadcastData: remove exception throw
    Ignore invalid Public Broadcast Announcement
    
    Bug: 335154833
    Flag: Exempt, trivial fix
    Test: atest PublicBroadcastDataTest
    Change-Id: I465a4d9fee142a031c3f914cf26d53a7f21c61e8

--- android/app/tests/unit/src/com/android/bluetooth/bass_client/PublicBroadcastDataTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/bass_client/PublicBroadcastDataTest.java
@@ -18,7 +18,7 @@ package com.android.bluetooth.bass_client;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertNull;
 
 import android.bluetooth.BluetoothLeBroadcastMetadata;
 
@@ -47,22 +47,19 @@ public class PublicBroadcastDataTest {
 
     @Test
     public void parsePublicBroadcastData() {
-        assertThrows(IllegalArgumentException.class, () ->
-                PublicBroadcastData.parsePublicBroadcastData(null));
+        assertNull(PublicBroadcastData.parsePublicBroadcastData(null));
 
         byte[] serviceDataInvalid = new byte[] {
                 (byte) 0x02, // features, non-encrypted, standard quality prsent
         };
-        assertThrows(IllegalArgumentException.class, () ->
-                PublicBroadcastData.parsePublicBroadcastData(serviceDataInvalid));
+        assertNull(PublicBroadcastData.parsePublicBroadcastData(serviceDataInvalid));
 
         byte[] serviceDataInvalid2 = new byte[] {
                 (byte) 0x02, // features, non-encrypted, standard quality prsent
                 (byte) 0x03,  // metaDataLength
                 (byte) 0x06, (byte) 0x07,  // invalid metaData
         };
-        assertThrows(IllegalArgumentException.class, () ->
-                PublicBroadcastData.parsePublicBroadcastData(serviceDataInvalid2));
+        assertNull(PublicBroadcastData.parsePublicBroadcastData(serviceDataInvalid2));
 
         byte[] serviceData = new byte[] {
                 (byte) 0x07, // features
