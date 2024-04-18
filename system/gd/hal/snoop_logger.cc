a5cd10e852a6e32d388505da9666a61aa762d035
Antoine SOULIER <asoulier@google.com>
2024-04-18 00:23:44 +0000

    bt_snoop: Add intermediate copy preventing source packet modification
    Bug: 335520123
    Test: m
    Flag: EXEMPT, no logical change
    Change-Id: I09e126dc1f46a2e87cf754d4ea2bedec55cd291e

--- system/gd/hal/snoop_logger.cc
+++ system/gd/hal/snoop_logger.cc
@@ -1187,7 +1187,11 @@ void SnoopLogger::FilterCapturedPacket(
   }
 }
 
-void SnoopLogger::Capture(HciPacket& packet, Direction direction, PacketType type) {
+void SnoopLogger::Capture(const HciPacket& immutable_packet, Direction direction, PacketType type) {
+  //// NEED TO BE FIXED : The code below modify the packet ////
+  HciPacket mutable_packet(immutable_packet);
+  HciPacket& packet = mutable_packet;
+  /////////////////////////////////////////////////////////////
   uint64_t timestamp_us =
       std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch())
           .count();
