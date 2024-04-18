635f8a21386f17ca8e0aad7ea7feb2bd9f7e5803
Chris Manton <cmanton@google.com>
2024-04-18 10:09:24 -0700

    net_test_btif: Properly call btif_dm_cleanup in tear down
    Bug: 335696151
    Test: atest net_test_btif
    Flag: EXEMPT, Test Infrastructure
    Change-Id: Id1e9ebf157624360c5483f7fd5121e82fa74d639

--- system/btif/test/btif_dm_test.cc
+++ system/btif/test/btif_dm_test.cc
@@ -17,6 +17,7 @@
 #include "btif/include/btif_dm.h"
 
 #include <android_bluetooth_flags.h>
+#include <bluetooth/log.h>
 #include <flag_macros.h>
 #include <gtest/gtest.h>
 
@@ -32,6 +33,8 @@
 #include "storage/storage_module.h"
 #include "test/fake/fake_osi.h"
 #include "test/mock/mock_osi_properties.h"
+
+using namespace bluetooth;
 
 using bluetooth::core::testing::MockCoreInterface;
 using ::testing::ElementsAre;
@@ -68,23 +71,36 @@ constexpr tBTM_BLE_IDLE_TIME_MS idle_time = 0x2468acd0;
 constexpr tBTM_BLE_ENERGY_USED energy_used = 0x13579bdf;
 }  // namespace
 
-class BtifDmTest : public ::testing::Test {
+class BtifDmWithMocksTest : public ::testing::Test {
+ protected:
+  void SetUp() override { fake_osi_ = std::make_unique<test::fake::FakeOsi>(); }
+
+  void TearDown() override { fake_osi_.reset(); }
+
+  std::unique_ptr<test::fake::FakeOsi> fake_osi_;
+};
+
+class BtifDmTest : public BtifDmWithMocksTest {
  protected:
   void SetUp() override {
-    fake_osi_ = std::make_unique<test::fake::FakeOsi>();
+    BtifDmWithMocksTest::SetUp();
     mock_core_interface_ = std::make_unique<MockCoreInterface>();
     bluetooth::legacy::testing::set_interface_to_profiles(
         mock_core_interface_.get());
   }
 
-  void TearDown() override {}
+  void TearDown() override {
+    bluetooth::legacy::testing::set_interface_to_profiles(nullptr);
+    mock_core_interface_.reset();
+    BtifDmWithMocksTest::TearDown();
+  }
 
-  std::unique_ptr<test::fake::FakeOsi> fake_osi_;
   std::unique_ptr<MockCoreInterface> mock_core_interface_;
 };
 
+static bool invoke_energy_info_cb_entered = false;
 TEST_F(BtifDmTest, bta_energy_info_cb__with_no_uid) {
-  static bool invoke_energy_info_cb_entered = false;
+  invoke_energy_info_cb_entered = false;
   bluetooth::core::testing::mock_event_callbacks.invoke_energy_info_cb =
       [](bt_activity_energy_info /* energy_info */,
          bt_uid_traffic_t* /* uid_data */) {
@@ -106,13 +122,13 @@ class BtifDmWithUidTest : public BtifDmTest {
   }
 
   void TearDown() override {
-    void btif_dm_cleanup();
+    btif_dm_cleanup();
     BtifDmTest::TearDown();
   }
 };
 
 TEST_F(BtifDmWithUidTest, bta_energy_info_cb__with_uid) {
-  static bool invoke_energy_info_cb_entered = false;
+  invoke_energy_info_cb_entered = false;
   bluetooth::core::testing::mock_event_callbacks.invoke_energy_info_cb =
       [](bt_activity_energy_info /* energy_info */,
          bt_uid_traffic_t* /* uid_data */) {
