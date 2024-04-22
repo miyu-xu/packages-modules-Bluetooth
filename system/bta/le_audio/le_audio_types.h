1d6f0076d0f093950e832c874cb7a9a3f46b989d
Bao Do <quocbaodo@google.com>
2024-04-22 14:33:44 +0800

    Prioritize HAL audioLocation if present
    Bug: 335544211
    Bug: 331809026
    Test: atest bluetooth_le_audio_test
    Change-Id: I98c5c83be804a9dac250abb83c217959a432d97a

--- system/bta/le_audio/le_audio_types.h
+++ system/bta/le_audio/le_audio_types.h
@@ -134,7 +134,7 @@ constexpr uint8_t kLeAudioCodecFrameDur7500us = 0x00;
 constexpr uint8_t kLeAudioCodecFrameDur10000us = 0x01;
 
 /* Audio Allocations */
-constexpr uint32_t kLeAudioLocationNotAllowed = 0x00000000;
+constexpr uint32_t kLeAudioLocationMono = 0x00000000;
 constexpr uint32_t kLeAudioLocationFrontLeft = 0x00000001;
 constexpr uint32_t kLeAudioLocationFrontRight = 0x00000002;
 constexpr uint32_t kLeAudioLocationFrontCenter = 0x00000004;
