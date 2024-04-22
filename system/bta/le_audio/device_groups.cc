99d0022492da68b3e3321a17a109d661d15f83df
Bao Do <quocbaodo@google.com>
2024-04-22 14:33:44 +0800

    Prioritize HAL audioLocation if present
    Bug: 335544211
    Bug: 331809026
    Test: atest bluetooth_le_audio_test
    Change-Id: I98c5c83be804a9dac250abb83c217959a432d97a

--- system/bta/le_audio/device_groups.cc
+++ system/bta/le_audio/device_groups.cc
@@ -932,8 +932,8 @@ types::BidirectionalPair<AudioContexts> LeAudioDeviceGroup::GetLatestAvailableCo
 }
 
 bool LeAudioDeviceGroup::ReloadAudioLocations(void) {
-  AudioLocations updated_snk_audio_locations_ = codec_spec_conf::kLeAudioLocationNotAllowed;
-  AudioLocations updated_src_audio_locations_ = codec_spec_conf::kLeAudioLocationNotAllowed;
+  AudioLocations updated_snk_audio_locations_ = codec_spec_conf::kLeAudioLocationMono;
+  AudioLocations updated_src_audio_locations_ = codec_spec_conf::kLeAudioLocationMono;
 
   for (const auto& device : leAudioDevices_) {
     if (device.expired() ||
