b8979577700db3ed215061f59f888af7aadf1b97
Myles Watson <mylesgw@google.com>
2024-04-24 16:24:36 -0700

    Add tests for BQR Events
    Test sending BQR events and make sure callbacks
    are called and the evenet FIFO contains the correct
    events.
    
    Bug: 322230000
    Test: atest net_test_btif_stack
    Flag: EXEMPT, simple bug fix
    Change-Id: I480d626ed6bd83a37e2518654481ce0bb5571047

--- system/btif/include/btif_bqr.h
+++ system/btif/include/btif_bqr.h
@@ -20,7 +20,7 @@
 #include <bluetooth/log.h>
 
 #include "btm_api_types.h"
-#include "common/leaky_bonded_queue.h"
+#include "common/postable_context.h"
 #include "include/hardware/bt_bqr.h"
 #include "osi/include/osi.h"
 #include "raw_address.h"
@@ -412,9 +412,8 @@ std::string PacketTypeToString(uint8_t packet_type);
 // And the minimum time interval of quality event reporting depends on the
 // setting of property "persist.bluetooth.bqr.min_interval_ms".
 //
-// @param is_enable True/False to enable/disable Bluetooth Quality Report
-//   mechanism in the Bluetooth controller.
-void EnableBtQualityReport(bool is_enable);
+// @param to_bind gives the postable for the callback, or null if disabling.
+void EnableBtQualityReport(common::PostableContext* to_bind);
 
 // Configure Bluetooth Quality Report setting to the Bluetooth controller.
 //
