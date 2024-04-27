eca510c01bb134d14c735fd850ab908f31165b13
Zhengping Jiang <jiangzp@google.com>
2024-04-26 17:34:24 -0700

    floss: don't add device to acceptlist if already connected
    If there is already a valid handle, there is no need to add the address
    to the accept list. If the device is paired with a resolvable address
    (RPA), the random address is used to search the handle.
    
    Bug: 297629431
    Bug: 338112215
    Test: CTSV secure client test
    Test: mma -j32
    Flag: verify_handle_before_add_accept_list
    Change-Id: Ice1aaeccac1ea8613b6471a1e5ccc30288d9de95

--- system/stack/btm/btm_ble_bgconn.cc
+++ system/stack/btm/btm_ble_bgconn.cc
@@ -27,6 +27,7 @@
 #include "stack/btm/btm_ble_bgconn.h"
 
 #include <bluetooth/log.h>
+#include <com_android_bluetooth_flags.h>
 
 #include <cstdint>
 #include <unordered_map>
@@ -38,6 +39,7 @@
 #include "stack/btm/btm_ble_int.h"
 #include "stack/btm/btm_dev.h"
 #include "stack/btm/btm_int_types.h"
+#include "stack/include/acl_api.h"
 #include "types/raw_address.h"
 
 using namespace bluetooth;
@@ -114,6 +116,15 @@ bool BTM_AcceptlistAdd(const RawAddress& address, bool is_direct) {
     return false;
   }
 
+  if (com::android::bluetooth::flags::verify_handle_before_add_accept_list()) {
+    // Type is set to 0x01, because it is not used in the function.
+    if (BTM_BLE_IS_RESOLVE_BDA(address) &&
+        bluetooth::shim::ACL_DeviceAlreadyConnected(
+            {.type = 0x01, .bda = address})) {
+      log::info("Already connected, not adding to accept list.");
+      return true;
+    }
+  }
   return bluetooth::shim::ACL_AcceptLeConnectionFrom(
       BTM_Sec_GetAddressWithType(address), is_direct);
 }
