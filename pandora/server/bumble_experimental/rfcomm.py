f1e490e93a195344bc8b53a701bb1eafaf09450c
Pomai Ahlo <poahlo@google.com>
2024-05-09 16:55:04 -0700

    RFCOMM Client BumbleBluetooth: Receive
    Add tests for receiving data
    
    Bug: 331415222
    Test: atest BumbleBluetoothTests:android.bluetooth.RfcommTest
    Flag: TEST_ONLY
    Change-Id: I02c7b284110fd15e6cad20a135ac74b902e85464

--- pandora/server/bumble_experimental/rfcomm.py
+++ pandora/server/bumble_experimental/rfcomm.py
@@ -108,6 +108,14 @@ class RFCOMMService(RFCOMMServicer):
         return StopServerResponse()
 
     @utils.rpc
+    async def Send(self, request: TxRequest, context: grpc.ServicerContext) -> TxResponse:
+        logging.info(f"Send")
+        dlc = self.connections[request.connection.id]
+        if dlc is not None:
+            dlc.write(request.data)
+        return TxResponse()
+
+    @utils.rpc
     async def Receive(self, request: RxRequest, context: grpc.ServicerContext) -> RxResponse:
         logging.info(f"Receive")
         received_data = await self.data_queue.get()
