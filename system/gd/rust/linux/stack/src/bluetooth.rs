2cb9e20825ba8e950da67bfa619df5e9e8307408
Jeremy Wu <jrwu@google.com>
2024-05-13 09:26:59 +0000

    Floss: add csis to profile policy
    This is only enabled when the chrome flag
    |CrOSLateBootBluetoothAudioLEAudioOnly| is enabled.
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: Id98fd212e23895bff6579de0a7eae64c62f48406

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -668,7 +668,8 @@ impl Bluetooth {
             | Profile::Hfp
             | Profile::AvrcpTarget
             | Profile::LeAudio
-            | Profile::VolumeControl => {
+            | Profile::VolumeControl
+            | Profile::CoordinatedSet => {
                 self.bluetooth_media.lock().unwrap().disable_profile(profile);
             }
             // Ignore profiles that we don't connect.
@@ -694,7 +695,8 @@ impl Bluetooth {
             | Profile::Hfp
             | Profile::AvrcpTarget
             | Profile::LeAudio
-            | Profile::VolumeControl => {
+            | Profile::VolumeControl
+            | Profile::CoordinatedSet => {
                 self.bluetooth_media.lock().unwrap().enable_profile(profile);
             }
             // Ignore profiles that we don't connect.
@@ -716,7 +718,8 @@ impl Bluetooth {
             | Profile::Hfp
             | Profile::AvrcpTarget
             | Profile::LeAudio
-            | Profile::VolumeControl => {
+            | Profile::VolumeControl
+            | Profile::CoordinatedSet => {
                 self.bluetooth_media.lock().unwrap().is_profile_enabled(profile)
             }
             // Ignore profiles that we don't connect.
@@ -2871,7 +2874,9 @@ impl IBluetooth for Bluetooth {
                             }
 
                             // TODO(b/317682584): implement policy to connect to LEA, VC, and CSIS
-                            Profile::LeAudio | Profile::VolumeControl if !has_le_media_profile => {
+                            Profile::LeAudio | Profile::VolumeControl | Profile::CoordinatedSet
+                                if !has_le_media_profile =>
+                            {
                                 has_le_media_profile = true;
                                 let txl = self.tx.clone();
                                 let address = device.address.clone();
@@ -2999,7 +3004,9 @@ impl IBluetooth for Bluetooth {
                             }
 
                             // TODO(b/317682584): implement policy to disconnect from LEA, VC, and CSIS
-                            Profile::LeAudio | Profile::VolumeControl if !has_le_media_profile => {
+                            Profile::LeAudio | Profile::VolumeControl | Profile::CoordinatedSet
+                                if !has_le_media_profile =>
+                            {
                                 has_le_media_profile = true;
                                 let txl = self.tx.clone();
                                 let address = device.address.clone();
