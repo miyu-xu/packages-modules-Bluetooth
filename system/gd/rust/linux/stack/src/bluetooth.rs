5f4a232b1959b65342d06fb5b7dace30473c511d
Jeremy Wu <jrwu@google.com>
2024-05-13 09:11:32 +0000

    Floss: add volume_control to profile policy
    This is only enabled when the chrome flag
    |CrOSLateBootBluetoothAudioLEAudioOnly| is enabled.
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: I80d609e19ac89d2dc1503a7804b00f6c94f23ad6

--- system/gd/rust/linux/stack/src/bluetooth.rs
+++ system/gd/rust/linux/stack/src/bluetooth.rs
@@ -663,7 +663,11 @@ impl Bluetooth {
                 self.hh.as_mut().unwrap().activate_hogp(false);
             }
 
-            Profile::A2dpSource | Profile::Hfp | Profile::AvrcpTarget | Profile::LeAudio => {
+            Profile::A2dpSource
+            | Profile::Hfp
+            | Profile::AvrcpTarget
+            | Profile::LeAudio
+            | Profile::VolumeControl => {
                 self.bluetooth_media.lock().unwrap().disable_profile(profile);
             }
             // Ignore profiles that we don't connect.
@@ -685,7 +689,11 @@ impl Bluetooth {
                 self.hh.as_mut().unwrap().activate_hogp(true);
             }
 
-            Profile::A2dpSource | Profile::Hfp | Profile::AvrcpTarget | Profile::LeAudio => {
+            Profile::A2dpSource
+            | Profile::Hfp
+            | Profile::AvrcpTarget
+            | Profile::LeAudio
+            | Profile::VolumeControl => {
                 self.bluetooth_media.lock().unwrap().enable_profile(profile);
             }
             // Ignore profiles that we don't connect.
@@ -703,7 +711,11 @@ impl Bluetooth {
 
             Profile::Hogp => Some(self.hh.as_ref().unwrap().is_hogp_activated),
 
-            Profile::A2dpSource | Profile::Hfp | Profile::AvrcpTarget | Profile::LeAudio => {
+            Profile::A2dpSource
+            | Profile::Hfp
+            | Profile::AvrcpTarget
+            | Profile::LeAudio
+            | Profile::VolumeControl => {
                 self.bluetooth_media.lock().unwrap().is_profile_enabled(profile)
             }
             // Ignore profiles that we don't connect.
@@ -2762,7 +2774,7 @@ impl IBluetooth for Bluetooth {
                             }
 
                             // TODO(b/317682584): implement policy to connect to LEA, VC, and CSIS
-                            Profile::LeAudio if !has_le_media_profile => {
+                            Profile::LeAudio | Profile::VolumeControl if !has_le_media_profile => {
                                 has_le_media_profile = true;
                                 let txl = self.tx.clone();
                                 let address = device.address.clone();
@@ -2886,7 +2898,7 @@ impl IBluetooth for Bluetooth {
                             }
 
                             // TODO(b/317682584): implement policy to disconnect from LEA, VC, and CSIS
-                            Profile::LeAudio if !has_le_media_profile => {
+                            Profile::LeAudio | Profile::VolumeControl if !has_le_media_profile => {
                                 has_le_media_profile = true;
                                 let txl = self.tx.clone();
                                 let address = device.address.clone();
