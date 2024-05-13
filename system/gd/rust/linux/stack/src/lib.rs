07beb62223c49d4e96d8b368e216a1002a7d6a33
Jeremy Wu <jrwu@google.com>
2024-05-13 09:11:32 +0000

    Floss: add volume_control to profile policy
    This is only enabled when the chrome flag
    |CrOSLateBootBluetoothAudioLEAudioOnly| is enabled.
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: I80d609e19ac89d2dc1503a7804b00f6c94f23ad6

--- system/gd/rust/linux/stack/src/lib.rs
+++ system/gd/rust/linux/stack/src/lib.rs
@@ -60,6 +60,7 @@ use bt_topshim::{
         hid_host::{BthhReportType, HHCallbacks},
         le_audio::LeAudioClientCallbacks,
         sdp::SdpCallbacks,
+        vc::VolumeControlCallbacks,
     },
 };
 
@@ -89,6 +90,7 @@ pub enum Message {
     HidHost(HHCallbacks),
     Hfp(HfpCallbacks),
     Sdp(SdpCallbacks),
+    VolumeControl(VolumeControlCallbacks),
     CreateBond(BluetoothDevice, BtTransport, u32, u64),
 
     // Actions within the stack
@@ -300,6 +302,10 @@ impl Stack {
 
                 Message::LeAudioClient(a) => {
                     bluetooth_media.lock().unwrap().dispatch_le_audio_callbacks(a);
+                }
+
+                Message::VolumeControl(a) => {
+                    bluetooth_media.lock().unwrap().dispatch_vc_callbacks(a);
                 }
 
                 Message::LeScanner(m) => {
