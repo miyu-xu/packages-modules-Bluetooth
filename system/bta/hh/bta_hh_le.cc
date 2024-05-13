150ed5b753acd22b9f799453d0a1df281d036368
Archie Pusaka <apusaka@google.com>
2024-05-13 20:12:28 +0800

    Simplify call to bta_hh_co_data
    Out of 8 input parameters to this function, only 3 are actually used,
    the rest 5 are either unused or only used to log some uninteresting
    event. Remove them to simplify the following patches.
    
    Bug: 291522341
    Test: m -j
    
    Change-Id: I670cbbee202fca9d2a45ee758e1e659be018f61c

--- system/bta/hh/bta_hh_le.cc
+++ system/bta/hh/bta_hh_le.cc
@@ -1621,7 +1621,6 @@ static void bta_hh_le_srvc_search_cmpl(tBTA_GATTC_SEARCH_CMPL* p_data) {
  ******************************************************************************/
 static void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
   tBTA_HH_DEV_CB* p_dev_cb = bta_hh_le_find_dev_cb_by_conn_id(p_data->conn_id);
-  uint8_t app_id;
   uint8_t* p_buf;
   tBTA_HH_LE_RPT* p_rpt;
 
@@ -1638,8 +1637,6 @@ static void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
     return;
   }
 
-  app_id = p_dev_cb->app_id;
-
   const gatt::Service* p_svc =
       BTA_GATTC_GetOwningService(p_dev_cb->conn_id, p_char->value_handle);
 
@@ -1651,11 +1648,6 @@ static void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
                p_char->uuid.ToString(), p_char->value_handle);
     return;
   }
-
-  if (p_char->uuid == Uuid::From16Bit(GATT_UUID_HID_BT_MOUSE_INPUT))
-    app_id = BTA_HH_APP_ID_MI;
-  else if (p_char->uuid == Uuid::From16Bit(GATT_UUID_HID_BT_KB_INPUT))
-    app_id = BTA_HH_APP_ID_KB;
 
   log::verbose("report ID: {}", p_rpt->rpt_id);
 
@@ -1670,9 +1662,7 @@ static void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
     p_buf = p_data->value;
   }
 
-  bta_hh_co_data((uint8_t)p_dev_cb->hid_handle, p_buf, p_data->len,
-                 p_dev_cb->mode, 0, /* no sub class*/
-                 p_dev_cb->dscp_info.ctry_code, p_dev_cb->link_spec, app_id);
+  bta_hh_co_data((uint8_t)p_dev_cb->hid_handle, p_buf, p_data->len);
 
   if (p_buf != p_data->value) osi_free(p_buf);
 }
@@ -2344,8 +2334,6 @@ static bool bta_hh_le_iso_data_callback(const RawAddress& addr,
     return false;
   }
 
-  bta_hh_co_data(p_dev_cb->hid_handle, data, size, p_dev_cb->mode, 0,
-                 p_dev_cb->dscp_info.ctry_code, p_dev_cb->link_spec,
-                 BTA_HH_APP_ID_LE);
+  bta_hh_co_data(p_dev_cb->hid_handle, data, size);
   return true;
 }
