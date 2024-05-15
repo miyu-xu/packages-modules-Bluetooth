fbde8a29c0116e5e83d6279a062a7d6948fd3d86
Jakub Pawlowski <jpawlowski@google.com>
2024-05-15 12:55:00 +0200

    bta_dm_disc: move SDP related code to bta_dm_disc_sdp.cc
    Bug: 340188796
    Test: mma -j68
    Flag: exempt, trivial
    Change-Id: I6a1da0c210d7caa2f0de966f036bfb5f69934813

--- system/bta/dm/bta_dm_disc_int.h
+++ system/bta/dm/bta_dm_disc_int.h
@@ -84,6 +84,7 @@ inline std::string bta_dm_state_text(
 #define MAX_DISC_RAW_DATA_BUF (4096)
 
 typedef struct {
+  RawAddress bd_addr;
   tBTA_SERVICE_MASK services_to_search;
   tBTA_SERVICE_MASK services_found;
 
@@ -116,6 +117,8 @@ typedef struct {
 
 extern const uint32_t bta_service_id_to_btm_srv_id_lkup_tbl[];
 extern const uint16_t bta_service_id_to_uuid_lkup_tbl[];
+
+void bta_dm_find_services(tBTA_DM_SDP_STATE* sdp_state);
 
 namespace fmt {
 template <>
