d2619eb04f380369c8801d4f451aa5e898fe2857
William Escande <wescande@google.com>
2024-05-24 16:01:42 -0700

    SystemServer: AutoOnFeatureTest complete testing
    Bug: 323060869
    Test: atest ServiceBluetoothRoboTests
    Flag: com.android.bluetooth.flags.auto_on_feature
    Change-Id: I2487d28f4aa56190ca91869f400989a7d00b75ee

--- service/src/AutoOnFeatureTest.kt
+++ service/src/AutoOnFeatureTest.kt
@@ -24,8 +24,9 @@ import android.os.Looper
 import android.provider.Settings
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.truth.content.IntentSubject.assertThat
+import com.android.dx.mockito.inline.extended.ExtendedMockito
+import com.android.modules.utils.testing.ExtendedMockitoRule
 import com.android.server.bluetooth.BluetoothAdapterState
-import com.android.server.bluetooth.Log
 import com.android.server.bluetooth.Timer
 import com.android.server.bluetooth.USER_SETTINGS_KEY
 import com.android.server.bluetooth.airplane.isOnOverrode as isAirplaneModeOn
@@ -45,11 +46,13 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import kotlin.test.assertFailsWith
 import org.junit.After
+import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestName
 import org.junit.runner.RunWith
+import org.mockito.Mockito.anyInt
+import org.mockito.Mockito.eq
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
 
@@ -65,13 +68,14 @@ class AutoOnFeatureTest {
 
     private var callback_count = 0
 
-    @JvmField @Rule val testName = TestName()
     @JvmField @Rule val expect = Expect.create()
+    @JvmField
+    @Rule
+    val extendedMockito =
+        ExtendedMockitoRule.Builder(this).spyStatic(Settings::Secure::class.java).build()
 
     @Before
     fun setUp() {
-        Log.i("AutoOnFeatureTest", "\t--> setUp(${testName.getMethodName()})")
-
         enableUserSettings()
     }
 
@@ -210,6 +214,18 @@ class AutoOnFeatureTest {
     }
 
     @Test
+    fun notifyBluetoothOn_whenItWasNeverUsedAndDatabaseFail_settingsIsNotEnabled() {
+        restoreSettings()
+        ExtendedMockito.doReturn(false).`when` {
+            Settings.Secure.putInt(eq(resolver), eq(USER_SETTINGS_KEY), anyInt())
+        }
+
+        notifyBluetoothOn(context)
+
+        assertThat(isUserSupported(resolver)).isFalse()
+    }
+
+    @Test
     fun notifyBluetoothOn_whenStorage_resetStorage() {
         Settings.Secure.putString(resolver, Timer.STORAGE_KEY, timerTarget.toString())
         shadowOf(looper).idle()
@@ -299,6 +315,14 @@ class AutoOnFeatureTest {
                 .integer(BluetoothAdapter.EXTRA_AUTO_ON_STATE)
                 .isEqualTo(BluetoothAdapter.AUTO_ON_STATE_DISABLED)
         }
+    }
+
+    @Test
+    fun apiSetUserEnableToFalse_whenDatabaseFail_throw() {
+        ExtendedMockito.doReturn(false).`when` {
+            Settings.Secure.putInt(eq(resolver), eq(USER_SETTINGS_KEY), anyInt())
+        }
+        assertThrows(IllegalStateException::class.java) { setUserEnabled(false) }
     }
 
     @Test
