45de9754977013b23866b21550bba212c85b5749
Chris Manton <cmanton@google.com>
2024-05-25 08:54:45 -0700

    [14/19] get_btm_client_interface().peer.BTM_ReadRemoteVersion
    Bug: 343772702
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: I5a715234b17c2ce5f98b197a92854100dab6cb8b

--- system/stack/btm/btm_ble_sec.cc
+++ system/stack/btm/btm_ble_sec.cc
@@ -1272,8 +1272,8 @@ static void btm_ble_notify_enc_cmpl(const RawAddress& bd_addr,
                                     bool encr_enable) {
   if (encr_enable) {
     uint8_t remote_lmp_version = 0;
-    if (!BTM_ReadRemoteVersion(bd_addr, &remote_lmp_version, nullptr,
-                               nullptr) ||
+    if (!get_btm_client_interface().peer.BTM_ReadRemoteVersion(
+            bd_addr, &remote_lmp_version, nullptr, nullptr) ||
         remote_lmp_version == 0) {
       log::warn("BLE Unable to determine remote version");
     }
