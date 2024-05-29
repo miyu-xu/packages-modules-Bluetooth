23d6c15286f6f67fa1b5157c9bc497c68b47d665
Jeremy Wu <jrwu@google.com>
2024-05-29 05:01:12 +0000

    floss: encapsulate cancelled info to |stream_started|
    In some cases, the start request can be cancelled. We encapsulate this
    information for the DBus method |GetXStreamStarted| to retrieve.
    
    Bug: 317682584
    Test: m Bluetooth
    Change-Id: Ie39cac261dc2a63214f2ff314b4779fa8f8f8e85

--- system/gd/rust/linux/service/src/iface_bluetooth_media.rs
+++ system/gd/rust/linux/service/src/iface_bluetooth_media.rs
@@ -7,7 +7,7 @@ use bt_topshim::profiles::hfp::{HfpCodecBitId, HfpCodecFormat};
 use bt_topshim::profiles::le_audio::{
     BtLeAudioContentType, BtLeAudioDirection, BtLeAudioGroupNodeStatus, BtLeAudioGroupStatus,
     BtLeAudioGroupStreamStatus, BtLeAudioSource, BtLeAudioUnicastMonitorModeStatus, BtLeAudioUsage,
-    BtLePcmConfig,
+    BtLePcmConfig, BtLeStreamStartedStatus,
 };
 use btstack::bluetooth_media::{BluetoothAudioDevice, IBluetoothMedia, IBluetoothMediaCallback};
 use btstack::RPCProxy;
@@ -68,6 +68,7 @@ impl_dbus_arg_from_into!(BtLeAudioSource, i32);
 impl_dbus_arg_from_into!(BtLeAudioGroupStatus, i32);
 impl_dbus_arg_from_into!(BtLeAudioGroupNodeStatus, i32);
 impl_dbus_arg_from_into!(BtLeAudioUnicastMonitorModeStatus, i32);
+impl_dbus_arg_from_into!(BtLeStreamStartedStatus, i32);
 impl_dbus_arg_from_into!(BtLeAudioDirection, i32);
 impl_dbus_arg_from_into!(BtLeAudioGroupStreamStatus, i32);
 impl_dbus_arg_enum!(A2dpCodecIndex);
@@ -446,12 +447,12 @@ impl IBluetoothMedia for IBluetoothMediaDBus {
     }
 
     #[dbus_method("GetHostStreamStarted")]
-    fn get_host_stream_started(&mut self) -> bool {
+    fn get_host_stream_started(&mut self) -> BtLeStreamStartedStatus {
         dbus_generated!()
     }
 
     #[dbus_method("GetPeerStreamStarted")]
-    fn get_peer_stream_started(&mut self) -> bool {
+    fn get_peer_stream_started(&mut self) -> BtLeStreamStartedStatus {
         dbus_generated!()
     }
 
