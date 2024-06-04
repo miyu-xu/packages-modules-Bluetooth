7bc2f04fd10c3af0882f268fc3b5c03b64b849f4
Ömer Faruk Yılmaz <ofy@google.com>
2024-06-04 07:47:17 +0000

    Move scan related objects from GattObjectsFactory to new GattObjectsFactory
    Bug: 344711538
    Flag: Exempt mechanicol refactor only
    Test: m com.android.btservices
    Change-Id: I1ec059bca861b37481255dc5d0038cc7b79414f0

---
+++ android/app/src/com/android/bluetooth/le_scan/ScanObjectsFactory.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.le_scan;
+
+import android.content.Context;
+import android.os.Looper;
+
+import com.android.bluetooth.btservice.AdapterService;
+import com.android.bluetooth.btservice.BluetoothAdapterProxy;
+
+/** Factory class for object initialization to help with unit testing */
+public class c {
+    private static final String TAG = ScanObjectsFactory.class.getSimpleName();
+    private static ScanObjectsFactory sInstance;
+    private static final Object INSTANCE_LOCK = new Object();
+
+    private ScanObjectsFactory() {}
+
+    /**
+     * Get the singleton instance of object factory
+     *
+     * @return the singleton instance, guaranteed not null
+     */
+    public static ScanObjectsFactory getInstance() {
+        synchronized (INSTANCE_LOCK) {
+            if (sInstance == null) {
+                sInstance = new ScanObjectsFactory();
+            }
+        }
+        return sInstance;
+    }
+
+    public ScanNativeInterface getScanNativeInterface() {
+        return ScanNativeInterface.getInstance();
+    }
+
+    /**
+     * Create an instance of ScanManager
+     *
+     * @param context a Context instance
+     * @param scanHelper a TransitionalScanHelper instance
+     * @param adapterService an AdapterService instance
+     * @param bluetoothAdapterProxy a bluetoothAdapterProxy instance
+     * @param looper the looper to be used for processing messages
+     * @return the created ScanManager instance
+     */
+    public ScanManager createScanManager(
+            Context context,
+            TransitionalScanHelper scanHelper,
+            AdapterService adapterService,
+            BluetoothAdapterProxy bluetoothAdapterProxy,
+            Looper looper) {
+        return new ScanManager(context, scanHelper, adapterService, bluetoothAdapterProxy, looper);
+    }
+
+    public PeriodicScanManager createPeriodicScanManager(AdapterService adapterService) {
+        return new PeriodicScanManager(adapterService);
+    }
+}
