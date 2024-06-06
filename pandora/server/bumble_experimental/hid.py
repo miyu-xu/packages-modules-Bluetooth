c80b984d1cfbdea1f6ee78299e8c388c30ac23b8
Gopi Sakshihally Bhuthaiah <bhuthaiah@google.com>
2024-06-06 07:40:40 +0000

    Bumble Java  HID Test cases :
    1. Preferred transport selection -Infra Changes
    
    Bug: 340549544
    Test: atest -v HidHostTest
    Flag: EXEMPT Test case
    Change-Id: I047f58c2d3c3348810b6b95714707e6f464f08e9

--- pandora/server/bumble_experimental/hid.py
+++ pandora/server/bumble_experimental/hid.py
@@ -3,6 +3,7 @@ import asyncio
 import grpc
 import grpc.aio
 import logging
+import struct
 
 from bumble.device import Device
 from google.protobuf import empty_pb2  # pytype: disable=pyi-error
@@ -212,6 +213,135 @@ HID_REPORT_MAP = bytes(  # Text String, 50 Octet Report Descriptor
 # Default protocol mode set to report protocol
 protocol_mode = Message.ProtocolMode.REPORT_PROTOCOL
 
+from bumble.core import AdvertisingData
+from bumble.device import Device, Connection, Peer
+from bumble.gatt import (
+    Descriptor,
+    Service,
+    Characteristic,
+    CharacteristicValue,
+    GATT_DEVICE_INFORMATION_SERVICE,
+    GATT_HUMAN_INTERFACE_DEVICE_SERVICE,
+    GATT_BATTERY_SERVICE,
+    GATT_BATTERY_LEVEL_CHARACTERISTIC,
+    GATT_MANUFACTURER_NAME_STRING_CHARACTERISTIC,
+    GATT_REPORT_CHARACTERISTIC,
+    GATT_REPORT_MAP_CHARACTERISTIC,
+    GATT_PROTOCOL_MODE_CHARACTERISTIC,
+    GATT_HID_INFORMATION_CHARACTERISTIC,
+    GATT_HID_CONTROL_POINT_CHARACTERISTIC,
+    GATT_REPORT_REFERENCE_DESCRIPTOR,
+)
+
+# -----------------------------------------------------------------------------
+
+# Protocol Modes
+HID_BOOT_PROTOCOL = 0x00
+HID_REPORT_PROTOCOL = 0x01
+
+# Report Types
+HID_INPUT_REPORT = 0x01
+HID_OUTPUT_REPORT = 0x02
+HID_FEATURE_REPORT = 0x03
+
+# Report Map
+HID_KEYBOARD_REPORT_MAP = bytes(
+    # pylint: disable=line-too-long
+    [
+        0x05,
+        0x01,  # Usage Page (Generic Desktop Controls)
+        0x09,
+        0x06,  # Usage (Keyboard)
+        0xA1,
+        0x01,  # Collection (Application)
+        0x85,
+        0x01,  # . Report ID (1)
+        0x05,
+        0x07,  # . Usage Page (Keyboard/Keypad)
+        0x19,
+        0xE0,  # . Usage Minimum (0xE0)
+        0x29,
+        0xE7,  # . Usage Maximum (0xE7)
+        0x15,
+        0x00,  # . Logical Minimum (0)
+        0x25,
+        0x01,  # . Logical Maximum (1)
+        0x75,
+        0x01,  # . Report Size (1)
+        0x95,
+        0x08,  # . Report Count (8)
+        0x81,
+        0x02,  # . Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x95,
+        0x01,  # . Report Count (1)
+        0x75,
+        0x08,  # . Report Size (8)
+        0x81,
+        0x01,  # . Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x95,
+        0x06,  # . Report Count (6)
+        0x75,
+        0x08,  # . Report Size (8)
+        0x15,
+        0x00,  # . Logical Minimum (0x00)
+        0x25,
+        0x94,  # . Logical Maximum (0x94)
+        0x05,
+        0x07,  # . Usage Page (Keyboard/Keypad)
+        0x19,
+        0x00,  # . Usage Minimum (0x00)
+        0x29,
+        0x94,  # . Usage Maximum (0x94)
+        0x81,
+        0x00,  # . Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+        0x95,
+        0x05,  # . Report Count (5)
+        0x75,
+        0x01,  # . Report Size (1)
+        0x05,
+        0x08,  # . Usage Page (LEDs)
+        0x19,
+        0x01,  # . Usage Minimum (Num Lock)
+        0x29,
+        0x05,  # . Usage Maximum (Kana)
+        0x91,
+        0x02,  # . Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+        0x95,
+        0x01,  # . Report Count (1)
+        0x75,
+        0x03,  # . Report Size (3)
+        0x91,
+        0x01,  # . Output (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+        0xC0,  # End Collection
+    ])
+
+
+# -----------------------------------------------------------------------------
+# pylint: disable=invalid-overridden-method
+class ServerListener(Device.Listener, Connection.Listener):
+
+    def __init__(self, device):
+        self.device = device
+
+    @AsyncRunner.run_in_task()
+    async def on_connection(self, connection):
+        logging.info(f'=== Connected to {connection}')
+        connection.listener = self
+
+    @AsyncRunner.run_in_task()
+    async def on_disconnection(self, reason):
+        logging.info(f'### Disconnected, reason={reason}')
+
+
+# -----------------------------------------------------------------------------
+def on_hid_control_point_write(_connection, value):
+    logging.info(f'Control Point Write: {value}')
+
+
+# -----------------------------------------------------------------------------
+def on_report(characteristic, value):
+    logging.info(color('Report:', 'cyan'), value.hex(), 'from', characteristic)
+
 
 # -----------------------------------------------------------------------------
 def sdp_records():
@@ -355,6 +485,117 @@ def sdp_records():
     }
 
 
+# -----------------------------------------------------------------------------
+def hogp_device(device):
+    global input_report_characteristic
+    # Create an 'input report' characteristic to send keyboard reports to the host
+    input_report_characteristic = Characteristic(
+        GATT_REPORT_CHARACTERISTIC,
+        Characteristic.Properties.READ | Characteristic.Properties.WRITE | Characteristic.Properties.NOTIFY,
+        Characteristic.READABLE | Characteristic.WRITEABLE,
+        bytes([0, 0, 0, 0, 0, 0, 0, 0]),
+        [Descriptor(
+            GATT_REPORT_REFERENCE_DESCRIPTOR,
+            Descriptor.READABLE,
+            bytes([0x01, HID_INPUT_REPORT]),
+        )],
+    )
+
+    # Create an 'output report' characteristic to receive keyboard reports from the host
+    output_report_characteristic = Characteristic(
+        GATT_REPORT_CHARACTERISTIC,
+        Characteristic.Properties.READ | Characteristic.Properties.WRITE | Characteristic.WRITE_WITHOUT_RESPONSE,
+        Characteristic.READABLE | Characteristic.WRITEABLE,
+        bytes([0]),
+        [Descriptor(
+            GATT_REPORT_REFERENCE_DESCRIPTOR,
+            Descriptor.READABLE,
+            bytes([0x01, HID_OUTPUT_REPORT]),
+        )],
+    )
+
+    # Add the services to the GATT sever
+    device.add_services([
+        Service(
+            GATT_DEVICE_INFORMATION_SERVICE,
+            [
+                Characteristic(
+                    GATT_MANUFACTURER_NAME_STRING_CHARACTERISTIC,
+                    Characteristic.Properties.READ,
+                    Characteristic.READABLE,
+                    'Bumble',
+                )
+            ],
+        ),
+        Service(
+            GATT_HUMAN_INTERFACE_DEVICE_SERVICE,
+            [
+                Characteristic(
+                    GATT_PROTOCOL_MODE_CHARACTERISTIC,
+                    Characteristic.Properties.READ,
+                    Characteristic.READABLE,
+                    bytes([HID_REPORT_PROTOCOL]),
+                ),
+                Characteristic(
+                    GATT_HID_INFORMATION_CHARACTERISTIC,
+                    Characteristic.Properties.READ,
+                    Characteristic.READABLE,
+                    # bcdHID=1.1, bCountryCode=0x00,
+                    # Flags=RemoteWake|NormallyConnectable
+                    bytes([0x11, 0x01, 0x00, 0x03]),
+                ),
+                Characteristic(
+                    GATT_HID_CONTROL_POINT_CHARACTERISTIC,
+                    Characteristic.WRITE_WITHOUT_RESPONSE,
+                    Characteristic.WRITEABLE,
+                    CharacteristicValue(write=on_hid_control_point_write),
+                ),
+                Characteristic(
+                    GATT_REPORT_MAP_CHARACTERISTIC,
+                    Characteristic.Properties.READ,
+                    Characteristic.READABLE,
+                    HID_KEYBOARD_REPORT_MAP,
+                ),
+                input_report_characteristic,
+                output_report_characteristic,
+            ],
+        ),
+        Service(
+            GATT_BATTERY_SERVICE,
+            [
+                Characteristic(
+                    GATT_BATTERY_LEVEL_CHARACTERISTIC,
+                    Characteristic.Properties.READ,
+                    Characteristic.READABLE,
+                    bytes([100]),
+                )
+            ],
+        ),
+    ])
+
+    # Debug print
+    for attribute in device.gatt_server.attributes:
+        logging.info(attribute)
+
+    # Set the advertising data
+    device.advertising_data = bytes(
+        AdvertisingData([
+            (
+                AdvertisingData.COMPLETE_LOCAL_NAME,
+                bytes('Bumble Keyboard', 'utf-8'),
+            ),
+            (
+                AdvertisingData.INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS,
+                bytes(GATT_HUMAN_INTERFACE_DEVICE_SERVICE),
+            ),
+            (AdvertisingData.APPEARANCE, struct.pack('<H', 0x03C1)),
+            (AdvertisingData.FLAGS, bytes([0x05])),
+        ]))
+
+    # Attach a listener
+    device.listener = ServerListener(device)
+
+
 # This class implements the Hid Pandora interface.
 class HIDService(HIDServicer):
 
@@ -364,6 +605,7 @@ class HIDService(HIDServicer):
         super().__init__()
         self.device = device
         self.device.sdp_service_records.update(sdp_records())
+        hogp_device(self.device)
         logging.info(f'Hid device register: ')
         global hid_device
         hid_device = HID_Device(self.device)
