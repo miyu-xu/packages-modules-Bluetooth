d79284a431bf5db264282bd3a5aee92b52e1dd82
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-06-10 22:40:21 +0000

    libbluetooth: Add BT_STATUS_SOCKET_STREAM_ERROR metric
    Add granularity to BT_STATUS_FAIL error codes by introducing a new
    BT_STATUS_SOCKET_STREAM_ERROR metric, to be used when an effort to find
    a device by address or other identifier fails.
    
    Bug: 340296381
    Test: m -j
    Flag: Exempt, metrics-only change
    
    Change-Id: I030ea3e1545d6979f3634d89d3d6aefbdca619b2

--- system/btif/src/btif_sock_rfc.cc
+++ system/btif/src/btif_sock_rfc.cc
@@ -396,7 +396,7 @@ bt_status_t btsock_rfc_connect(const RawAddress* bd_addr,
     if (!send_app_scn(slot)) {
       log::error("send_app_scn() failed, closing slot->id:{}", slot->id);
       cleanup_rfc_slot(slot);
-      return BT_STATUS_FAIL;
+      return BT_STATUS_SOCKET_STREAM_ERROR;
     }
   } else {
     log::info("service_uuid:{}, bd_addr:{}, slot_id:{}",
