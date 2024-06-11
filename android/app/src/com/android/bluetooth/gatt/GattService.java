19506c9a6929fdef731f79d28e3e800d5abec8fe
William Escande <wescande@google.com>
2024-06-10 17:48:36 -0700

    ErrorProne: Enforce & fix NonApiType
    See https://errorprone.info/bugpattern/NonApiType
    
    Bug: 344658662
    Test: m BluetoothInstrumentationTests
    Flag: Exempt Build and test only
    Change-Id: Ibf6b02e1ca0e8446013e388c03c305e25eac4d8d

--- android/app/src/com/android/bluetooth/gatt/GattService.java
+++ android/app/src/com/android/bluetooth/gatt/GattService.java
@@ -1516,7 +1516,7 @@ public class GattService extends ProfileService {
         return new GattDbElement();
     }
 
-    void onGetGattDb(int connId, ArrayList<GattDbElement> db) throws RemoteException {
+    void onGetGattDb(int connId, List<GattDbElement> db) throws RemoteException {
         String address = mClientMap.addressByConnId(connId);
 
         Log.d(TAG, "onGetGattDb() - address=" + address);
