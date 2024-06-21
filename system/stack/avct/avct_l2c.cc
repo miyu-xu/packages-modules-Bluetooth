f499cb076aed877cac329ba7ba7adf3616b998ff
Chris Manton <cmanton@google.com>
2024-06-20 17:08:48 -0700

    stack::l2cap Introduce l2cap interface
    Bug: 343808590
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: I11370fb3538152461caf48b410b8fea51b3b2c88

--- system/stack/avct/avct_l2c.cc
+++ system/stack/avct/avct_l2c.cc
@@ -29,11 +29,10 @@
 #include "avct_int.h"
 #include "btif/include/btif_av.h"
 #include "internal_include/bt_target.h"
-#include "l2c_api.h"
-#include "l2cdefs.h"
 #include "osi/include/allocator.h"
 #include "osi/include/osi.h"
 #include "stack/include/bt_hdr.h"
+#include "stack/include/l2cap_interface.h"
 #include "types/raw_address.h"
 
 using namespace bluetooth;
@@ -138,7 +137,7 @@ void avct_l2c_connect_ind_cback(const RawAddress& bd_addr, uint16_t lcid, uint16
 
   /* If we reject the connection, send DisconnectReq */
   if (result != L2CAP_CONN_OK) {
-    if (!L2CA_DisconnectReq(lcid)) {
+    if (!stack::l2cap::get_interface().L2CA_DisconnectReq(lcid)) {
       log::warn("Unable to send L2CAP disconnect request peer:{} cid:{}", bd_addr, lcid);
     }
   }
@@ -187,7 +186,7 @@ static void avct_on_l2cap_error(uint16_t lcid, uint16_t result) {
     p_lcb->ch_result = result;
 
     /* Send L2CAP disconnect req */
-    if (!L2CA_DisconnectReq(lcid)) {
+    if (!stack::l2cap::get_interface().L2CA_DisconnectReq(lcid)) {
       log::warn("Unable to send L2CAP disconnect request peer:{} cid:{}", p_lcb->peer_addr, lcid);
     }
   }
@@ -231,7 +230,7 @@ void avct_l2c_connect_cfm_cback(uint16_t lcid, uint16_t result) {
       if (result == L2CAP_CONN_OK) {
         /* just in case the peer also accepts our connection - Send L2CAP
          * disconnect req */
-        if (!L2CA_DisconnectReq(lcid)) {
+        if (!stack::l2cap::get_interface().L2CA_DisconnectReq(lcid)) {
           log::warn("Unable to send L2CAP disconnect request peer:{} cid:{}", p_lcb->peer_addr,
                     lcid);
         }
@@ -322,7 +321,7 @@ void avct_l2c_disconnect_ind_cback(uint16_t lcid, bool /* ack_needed */) {
 }
 
 void avct_l2c_disconnect(uint16_t lcid, uint16_t result) {
-  if (!L2CA_DisconnectReq(lcid)) {
+  if (!stack::l2cap::get_interface().L2CA_DisconnectReq(lcid)) {
     log::warn("Unable to send L2CAP disconnect request cid:{}", lcid);
   }
 
