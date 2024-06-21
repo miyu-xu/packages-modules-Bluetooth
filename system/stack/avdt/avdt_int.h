124ce2bf010371b6983d934783097ed608c3fa88
Chris Manton <cmanton@google.com>
2024-06-20 17:08:48 -0700

    stack::l2cap Introduce l2cap interface
    Bug: 343808590
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: I11370fb3538152461caf48b410b8fea51b3b2c88

--- system/stack/avdt/avdt_int.h
+++ system/stack/avdt/avdt_int.h
@@ -30,10 +30,10 @@
 #include "avdt_defs.h"
 #include "avdtc_api.h"
 #include "internal_include/bt_target.h"
-#include "l2c_api.h"
 #include "osi/include/alarm.h"
 #include "osi/include/fixed_queue.h"
 #include "stack/include/bt_hdr.h"
+#include "stack/include/l2cap_interface.h"
 #include "types/raw_address.h"
 
 #ifndef AVDT_DEBUG
