5f1b41ae0af43997d1a60f6412d69849dcaaa058
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-06-21 11:13:40 +0000

    bta: le_audio: state_machine: Avoid reconfiguration of non-active ASE
    It may happen that Peripheral reconfigures itself and send ASE
    Codec Configured state notification. Such notification related
    to non-active ASE shall not trigger us to set up QoS.
    
    Bug: 357472821
    Flag: Exempt; regression covered by unit test added
    Test: atest bluetooth_le_audio_test
    Change-Id: I64499859a2391a3997e3507c740224e3e24b2144

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -2028,6 +2028,11 @@ private:
           return;
         }
 
+        if (!ase->active) {
+          log::debug("Ignoring reconfiguration of non-active ASE");
+          return;
+        }
+
         if (leAudioDevice->HaveAnyUnconfiguredAses()) {
           /* More ASEs notification from this device has to come for this group
            */
@@ -2125,6 +2130,11 @@ private:
         /* This may be a notification from a re-configured ASE */
         ase->reconfigure = false;
         qos_config_update(rsp, ase->qos_preferences, ase->qos_config);
+
+        if (!ase->active) {
+          log::debug("Ignoring reconfiguration of non-active ASE");
+          return;
+        }
 
         if (leAudioDevice->HaveAnyUnconfiguredAses()) {
           /* Waiting for others to be reconfigured */
