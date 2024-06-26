df697a912eb06b06251580772c5bbafbf2dcc829
Myles Watson <mylesgw@google.com>
2024-06-26 16:19:16 -0700

    InitFlags: Remove redact_log
    Bug: 174487588
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I964c7a8cb3320a0b958012181221fa080effde94

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -14,7 +14,6 @@ mod ffi {
 
         fn classic_discovery_only_is_enabled() -> bool;
         fn get_hci_adapter() -> i32;
-        fn redact_log_is_enabled() -> bool;
         fn sco_codec_timeout_clear_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
         fn sdp_skip_rnr_if_known_is_enabled() -> bool;
