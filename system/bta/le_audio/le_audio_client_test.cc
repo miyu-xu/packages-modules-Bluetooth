16c64865fb7b28e1bd7d7831bb993b2b0a2e4e3f
Myles Watson <mylesgw@google.com>
2024-06-26 16:04:25 -0700

    InitFlags: Remove leaudio_targeted_announcement_reconnection
    Bug: 248340126
    Fixes: 290845708
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I7ae4a2ab070b174192c45e41916a27d6c9095104

--- system/bta/le_audio/le_audio_client_test.cc
+++ system/bta/le_audio/le_audio_client_test.cc
@@ -101,17 +101,15 @@ constexpr bluetooth::le_audio::types::LeAudioContextType
 static constexpr char kNotifyUpperLayerAboutGroupBeingInIdleDuringCall[] =
     "persist.bluetooth.leaudio.notify.idle.during.call";
 const char* test_flags[] = {
-    "INIT_default_log_level_str=LOG_VERBOSE",
-    "INIT_leaudio_targeted_announcement_reconnection_mode=true",
-    "INIT_leaudio_broadcast_audio_handover_policies=false",
-    nullptr,
+        "INIT_default_log_level_str=LOG_VERBOSE",
+        "INIT_leaudio_broadcast_audio_handover_policies=false",
+        nullptr,
 };
 
 const char* test_flags_with_handover_mode[] = {
-    "INIT_default_log_level_str=LOG_VERBOSE",
-    "INIT_leaudio_targeted_announcement_reconnection_mode=true",
-    "INIT_leaudio_broadcast_audio_handover_policies=true",
-    nullptr,
+        "INIT_default_log_level_str=LOG_VERBOSE",
+        "INIT_leaudio_broadcast_audio_handover_policies=true",
+        nullptr,
 };
 
 void osi_property_set_bool(const char* key, bool value);
