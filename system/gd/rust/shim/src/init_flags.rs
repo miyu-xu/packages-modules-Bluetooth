abb00c16bc07ebf850a17bb56993d66858a6df7b
Myles Watson <mylesgw@google.com>
2024-06-26 16:16:03 -0700

    InitFlags: Remove pbap_pse_dynamic_attributes
    Bug: 252902891
    Fixes: 290845736
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: Ibf3586b4fbfc7967efe35a72269aa68dc769dcc3

--- system/gd/rust/shim/src/init_flags.rs
+++ system/gd/rust/shim/src/init_flags.rs
@@ -14,7 +14,6 @@ mod ffi {
 
         fn classic_discovery_only_is_enabled() -> bool;
         fn get_hci_adapter() -> i32;
-        fn pbap_pse_dynamic_version_upgrade_is_enabled() -> bool;
         fn redact_log_is_enabled() -> bool;
         fn sco_codec_timeout_clear_is_enabled() -> bool;
         fn sdp_serialization_is_enabled() -> bool;
