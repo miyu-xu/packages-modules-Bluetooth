1740dbf0222c4d9c5a80c217378501bdd423bf83
Myles Watson <mylesgw@google.com>
2024-06-26 16:43:30 -0700

    InitFlags: Remove hci_adapter
    Bug: 193261880
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I5618a152eb4588284d40dca9596148ee3e7ef239

--- system/gd/hal/mgmt.cc
+++ system/gd/hal/mgmt.cc
@@ -30,8 +30,7 @@
 
 #include <cerrno>
 
-#include "common/init_flags.h"
-#include "os/log.h"
+static constexpr int kHciAdapterIndex = 0;
 
 namespace bluetooth {
 namespace hal {
@@ -82,7 +81,7 @@ static int btsocket_open_mgmt(uint16_t hci) {
  * be HCI_OP_NOP (0x0000).
  */
 uint16_t Mgmt::get_vs_opcode(uint16_t vendor_specification) {
-  int hci = bluetooth::common::InitFlags::GetAdapterIndex();
+  int hci = kHciAdapterIndex;
   int fd = btsocket_open_mgmt(hci);
   uint16_t ret_opcode = HCI_OP_NOP;
 
