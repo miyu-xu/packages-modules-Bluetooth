893924449bef7e08792a98673d53878674ada8a0
William Escande <wescande@google.com>
2024-06-26 17:33:24 -0700

    RequiresPermission: properly annotate MethodProxy
    Bug: 349682934
    Flag: Exempt annotation
    Test: m Bluetooth
    Change-Id: Ib75486ddada2772667f36448e01fb45243c6c64f

--- android/app/src/com/android/bluetooth/BluetoothMethodProxy.java
+++ android/app/src/com/android/bluetooth/BluetoothMethodProxy.java
@@ -226,6 +226,7 @@ public class BluetoothMethodProxy {
      * Proxies {@link PeriodicAdvertisingManager#registerSync(ScanResult, int, int,
      * PeriodicAdvertisingCallback, Handler)}.
      */
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_SCAN)
     public void periodicAdvertisingManagerRegisterSync(
             PeriodicAdvertisingManager manager,
             ScanResult scanResult,
@@ -237,6 +238,7 @@ public class BluetoothMethodProxy {
     }
 
     /** Proxies {@link PeriodicAdvertisingManager#unregisterSync(PeriodicAdvertisingCallback)}. */
+    @RequiresPermission(android.Manifest.permission.BLUETOOTH_SCAN)
     public void periodicAdvertisingManagerUnregisterSync(
             PeriodicAdvertisingManager manager, PeriodicAdvertisingCallback callback) {
         manager.unregisterSync(callback);
