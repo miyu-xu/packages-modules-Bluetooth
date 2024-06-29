7898d1929460134e997523be99dd5629d883ef63
Omair Kamil <okamil@google.com>
2024-06-29 01:24:31 -0700

    Refactor advertiser functions out of ContextMap.
    Bug: 327849650
    Flag: EXEMPT, mechanical refactor
    Test: atest BluetoothInstrumentationTests, atest BumbleBluetoothTests, atest pts-bot:GATT
    Change-Id: Ib887c178f3ec2c9f8c836a78599af08babbe02bc

--- android/app/tests/unit/src/com/android/bluetooth/gatt/AdvertiseManagerTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/gatt/AdvertiseManagerTest.java
@@ -54,7 +54,7 @@ public class AdvertiseManagerTest {
 
     @Mock private GattService mService;
 
-    @Mock private GattService.AdvertiserMap mAdvertiserMap;
+    @Mock private AdvertiserMap mAdvertiserMap;
 
     @Mock private AdvertiseManagerNativeInterface mNativeInterface;
 
@@ -67,10 +67,11 @@ public class AdvertiseManagerTest {
 
     @Before
     public void setUp() throws Exception {
-
         TestUtils.setAdapterService(mAdapterService);
 
-        mAdvertiseManager = new AdvertiseManager(mService, mNativeInterface, mAdvertiserMap);
+        AdvertiseManagerNativeInterface.setInstance(mNativeInterface);
+        mAdvertiseManager = new AdvertiseManager(mService);
+        mAdvertiseManager.mAdvertiserMap = mAdvertiserMap;
 
         AdvertisingSetParameters parameters = new AdvertisingSetParameters.Builder().build();
         AdvertiseData advertiseData = new AdvertiseData.Builder().build();
@@ -100,6 +101,7 @@ public class AdvertiseManagerTest {
 
     @After
     public void tearDown() throws Exception {
+        AdvertiseManagerNativeInterface.setInstance(null);
         TestUtils.clearAdapterService(mAdapterService);
     }
 
