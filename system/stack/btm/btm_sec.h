b3c146b1f1397e21366687909005864cf8241dbc
Jakub Pawlowski <jpawlowski@google.com>
2024-07-03 16:06:19 +0200

    BTM_SecIsSecurityPending -> BTM_SecIsLeSecurityPending
    find -type f -exec sed -i 's/BTM_SecIsSecurityPending/BTM_SecIsLeSecurityPending/' "{}" +;
    Twice, because it replaces just first occurence lin line by default
    
    Bug: 343639888
    Test: m -j32;
    Flag: exempt, trivial
    Change-Id: I61e37bdc1ce779090e4acdcfdcd616b203a778a8

--- system/stack/btm/btm_sec.h
+++ system/stack/btm/btm_sec.h
@@ -270,7 +270,7 @@ tBTM_STATUS BTM_SetEncryption(const RawAddress& bd_addr,
                               tBTM_SEC_CALLBACK* p_callback, void* p_ref_data,
                               tBTM_BLE_SEC_ACT sec_act);
 
-bool BTM_SecIsSecurityPending(const RawAddress& bd_addr);
+bool BTM_SecIsLeSecurityPending(const RawAddress& bd_addr);
 
 /*******************************************************************************
  *
