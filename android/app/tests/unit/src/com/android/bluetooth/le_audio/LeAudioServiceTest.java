2464c0d27013f11fb5451e4a16693d2829945bdc
Rongxuan Liu <rongxuan@google.com>
2024-07-03 22:23:20 +0000

    [le audio] Set BASS connection policy with other LEA GATT profiles
    If setting LE audio policies is triggered by CSIP profile connected,
    we didn't set BASS with other LEA GATT profiles. This will cause the
    issue that one bud enabled BASS while the other bud didn't.
    Adding this to ensure the connection policies are aligned among member
    devices. And this will also support in LE audio only device case.
    
    Bug: 350877103
    Flag: EXEMPT, trivial change, covered by unit test
    Test: atest LeAudioServiceTest
    Test: manual tested private broadcast
    Change-Id: I4c5e11e2d6a348ec232b0cce05885e5921576a93

--- android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -1196,6 +1196,8 @@ public class LeAudioServiceTest {
         when(mVolumeControlService.setConnectionPolicy(any(), anyInt())).thenReturn(true);
         when(mCsipSetCoordinatorService.setConnectionPolicy(any(), anyInt())).thenReturn(true);
         when(mHapClientService.setConnectionPolicy(any(), anyInt())).thenReturn(true);
+        when(mBassClientService.isEnabled()).thenReturn(true);
+        when(mBassClientService.setConnectionPolicy(any(), anyInt())).thenReturn(true);
         when(mDatabaseManager.getProfileConnectionPolicy(mSingleDevice, BluetoothProfile.LE_AUDIO))
                 .thenReturn(BluetoothProfile.CONNECTION_POLICY_UNKNOWN);
 
@@ -1211,7 +1213,8 @@ public class LeAudioServiceTest {
                 .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_ALLOWED);
         verify(mHapClientService, times(1))
                 .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_ALLOWED);
-
+        verify(mBassClientService, times(1))
+                .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_ALLOWED);
         // Verify the connection state broadcast, and that we are in Connecting state
         verifyConnectionStateIntent(
                 TIMEOUT_MS,
@@ -1251,7 +1254,8 @@ public class LeAudioServiceTest {
                 .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_FORBIDDEN);
         verify(mHapClientService, times(1))
                 .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_FORBIDDEN);
-
+        verify(mBassClientService, times(1))
+                .setConnectionPolicy(mSingleDevice, BluetoothProfile.CONNECTION_POLICY_FORBIDDEN);
         // Verify the connection state broadcast, and that we are in Connecting state
         verifyConnectionStateIntent(
                 TIMEOUT_MS,
