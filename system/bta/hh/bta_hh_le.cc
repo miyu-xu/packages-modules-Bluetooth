7d8c495404cfa1cb93eac8d77850dca6abdceae8
Himanshu Rawat <rwt@google.com>
2024-07-03 01:15:20 +0000

    Remove checks for 16-bit UUID in HOGP host
    Android Headtracker Service uses 128-bit UUIDs. bta_hh_get_uuid16()
    already checks and maps its 128-bit UUIDs into 16-bit UUIDs. There is no
    need to separately check for 16-bit UUIDs.
    
    Test: mmm packages/modules/Bluetooth
    Test: Manual | Test DSA 2.0
    Flag: EXEMPT bugfix
    Bug: 350826957
    Change-Id: I5ef84b73174b42cb8df6fb148a48b51ac0985005

--- system/bta/hh/bta_hh_le.cc
+++ system/bta/hh/bta_hh_le.cc
@@ -670,11 +670,6 @@ static void write_rpt_clt_cfg_cb(uint16_t conn_id, tGATT_STATUS status,
     log::error("Characteristic with handle {} not found clt cfg", handle);
     return;
   }
-  if (!characteristic->uuid.Is16Bit()) {
-    log::error("Unexpected len characteristic ID clt cfg: {}",
-               characteristic->uuid.ToString());
-    return;
-  }
 
   uint16_t char_uuid = bta_hh_get_uuid16(p_dev_cb, characteristic->uuid);
 
@@ -1657,11 +1652,6 @@ static void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
   const gatt::Service* p_svc =
       BTA_GATTC_GetOwningService(p_dev_cb->conn_id, p_char->value_handle);
 
-  if (!p_char->uuid.Is16Bit()) {
-    log::error("Unexpected characteristic len: {}", p_char->uuid.ToString());
-    return;
-  }
-
   p_rpt = bta_hh_le_find_report_entry(p_dev_cb, p_svc->handle,
                                       bta_hh_get_uuid16(p_dev_cb, p_char->uuid),
                                       p_char->value_handle);
@@ -1847,10 +1837,6 @@ static void read_report_cb(uint16_t conn_id, tGATT_STATUS status,
     log::error("Unknown handle");
     return;
   }
-  if (!p_char->uuid.Is16Bit()) {
-    log::error("Unexpected characteristic len: {}", p_char->uuid.ToString());
-    return;
-  }
 
   uint16_t char_uuid = bta_hh_get_uuid16(p_dev_cb, p_char->uuid);
 
@@ -1936,9 +1922,7 @@ static void write_report_cb(uint16_t conn_id, tGATT_STATUS status,
   const gatt::Characteristic* p_char =
       BTA_GATTC_GetCharacteristic(conn_id, handle);
 
-  if (p_char == nullptr) return;
-  if (!p_char->uuid.Is16Bit()) {
-    log::error("Unexpected characteristic len: {}", p_char->uuid.ToString());
+  if (p_char == nullptr) {
     return;
   }
 
