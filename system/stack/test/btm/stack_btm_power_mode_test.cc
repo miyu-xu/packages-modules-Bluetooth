9aa70544bed4037403119d616b9c842ba5e80cb8
Chris Manton <cmanton@google.com>
2024-07-05 10:52:40 -0700

    [4/24] Canonical form tBTM_STATUS::BTM_CMD_STARTED
    Prepare for class enum-ify tBTM_STATUS
    
    Bug: 358402071
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: Ifae45f7ff698e07e03086d9ee6fb0e5f9dafe887

--- system/stack/test/btm/stack_btm_power_mode_test.cc
+++ system/stack/test/btm/stack_btm_power_mode_test.cc
@@ -105,7 +105,8 @@ TEST_F(StackBtmPowerModeConnected, BTM_SetPowerMode__ActiveToSniff) {
   tBTM_PM_PWR_MD mode = {
           .mode = BTM_PM_MD_SNIFF,
   };
-  ASSERT_EQ("BTM_CMD_STARTED", btm_status_text(::BTM_SetPowerMode(pm_id_, kRawAddress, &mode)));
+  ASSERT_EQ("tBTM_STATUS::BTM_CMD_STARTED",
+            btm_status_text(::BTM_SetPowerMode(pm_id_, kRawAddress, &mode)));
   ASSERT_EQ(1, get_func_call_count("btsnd_hcic_sniff_mode"));
 
   // Respond with successful command status for mode command
@@ -156,7 +157,8 @@ TEST_F(StackBtmPowerModeConnected, BTM_SetPowerMode__ActiveToSniffTwice) {
   tBTM_PM_PWR_MD mode = {
           .mode = BTM_PM_MD_SNIFF,
   };
-  ASSERT_EQ("BTM_CMD_STARTED", btm_status_text(::BTM_SetPowerMode(pm_id_, kRawAddress, &mode)));
+  ASSERT_EQ("tBTM_STATUS::BTM_CMD_STARTED",
+            btm_status_text(::BTM_SetPowerMode(pm_id_, kRawAddress, &mode)));
   ASSERT_EQ(1, get_func_call_count("btsnd_hcic_sniff_mode"));
 
   // Respond with successful command status for mode command
