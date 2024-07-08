bd37afef63945ac02a40fb41ff46bb9256f0304c
Zhengping Jiang <jiangzp@google.com>
2024-07-08 09:14:01 -0700

    DO_NOT_SUBMIT: Add custom service to send Android API level
    Register a custom service when Android API level is defined. Send the
    API level as a custom characteristic.
    
    Bug: 348512828, 350876196
    Test: m com.android.btservices
    Test: SDP/SR/BRW/BV-01-C
    Flag: TBD
    Change-Id: Ifcdfa4b8b05d7bf32d4b1a285ea2d5c0d8f7f42c

--- system/stack/gatt/gatt_int.h
+++ system/stack/gatt/gatt_int.h
@@ -408,6 +408,7 @@ typedef struct {
   uint16_t last_service_handle; /* handle of last service */
   tGATT_SVC_CHG gattp_attr;     /* GATT profile attribute service change */
   tGATT_IF gatt_if;
+  tGATT_IF custom_if;
   std::list<tGATT_HDL_LIST_ELEM>* hdl_list_info;
   std::list<tGATT_SRV_LIST_ELEM>* srv_list_info;
 
@@ -434,6 +435,7 @@ typedef struct {
   uint16_t handle_cl_supported_feat;
   uint16_t handle_sr_supported_feat;
   uint8_t gatt_svr_supported_feat_mask; /* Local supported features as a server */
+  uint16_t handle_of_aosp_ver;
 
   /* Supported features as a client. To be written to remote device.
    * Note this is NOT a value of the characteristic with handle
