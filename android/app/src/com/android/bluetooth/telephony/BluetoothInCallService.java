191d73f9ad56ddd16dfbd0f405e4f7c3d1aaca39
Yuyang Huang <yuyangh@google.com>
2024-07-09 14:07:15 -0700

    improve intent comparison
    Bug: 346498545
    Test: atest BluetoothInCallServiceTest
    Flag: exempt, small fix
    Change-Id: I359da4956d435953cc09de02ab59542eb04df827

--- android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
+++ android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -188,7 +188,7 @@ public class BluetoothInCallService extends InCallService {
         @Override
         public void onReceive(Context context, Intent intent) {
             synchronized (LOCK) {
-                if (intent.getAction() != BluetoothAdapter.ACTION_STATE_CHANGED) {
+                if (!Objects.equals(intent.getAction(), BluetoothAdapter.ACTION_STATE_CHANGED)) {
                     Log.w(TAG, "BluetoothAdapterReceiver: Intent action " + intent.getAction());
                     return;
                 }
@@ -203,7 +203,6 @@ public class BluetoothInCallService extends InCallService {
             }
         }
     }
-    ;
 
     /** Receives events for global state changes of the bluetooth adapter. */
     // TODO: The code is moved from Telecom stack. Since we're running in the BT process itself,
