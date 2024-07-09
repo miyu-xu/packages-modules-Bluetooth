81b409929429e9fa4ddf50bb7af93672f1c644f9
Chris Manton <cmanton@google.com>
2024-07-09 12:58:30 -0700

    stack::sdp: Declare and assign tCONN_CB together
    Bug: 352106391
    Test: m .
    Flag: EXEMPT, Mechanical refactor
    Change-Id: I39b01a83d5b76f2ee5fc8d0edf9302c58591b214

--- system/stack/sdp/sdp_api.cc
+++ system/stack/sdp/sdp_api.cc
@@ -142,11 +142,8 @@ bool SDP_CancelServiceSearch(const tSDP_DISCOVERY_DB* p_db) {
  ******************************************************************************/
 bool SDP_ServiceSearchRequest(const RawAddress& bd_addr, tSDP_DISCOVERY_DB* p_db,
                               tSDP_DISC_CMPL_CB* p_cb) {
-  tCONN_CB* p_ccb;
-
   /* Specific BD address */
-  p_ccb = sdp_conn_originate(bd_addr);
-
+  tCONN_CB* p_ccb = sdp_conn_originate(bd_addr);
   if (!p_ccb) {
     return false;
   }
@@ -174,11 +171,8 @@ bool SDP_ServiceSearchRequest(const RawAddress& bd_addr, tSDP_DISCOVERY_DB* p_db
  ******************************************************************************/
 bool SDP_ServiceSearchAttributeRequest(const RawAddress& bd_addr, tSDP_DISCOVERY_DB* p_db,
                                        tSDP_DISC_CMPL_CB* p_cb) {
-  tCONN_CB* p_ccb;
-
   /* Specific BD address */
-  p_ccb = sdp_conn_originate(bd_addr);
-
+  tCONN_CB* p_ccb = sdp_conn_originate(bd_addr);
   if (!p_ccb) {
     return false;
   }
@@ -208,11 +202,8 @@ bool SDP_ServiceSearchAttributeRequest(const RawAddress& bd_addr, tSDP_DISCOVERY
 bool SDP_ServiceSearchAttributeRequest2(
         const RawAddress& bd_addr, tSDP_DISCOVERY_DB* p_db,
         base::RepeatingCallback<tSDP_DISC_CMPL_CB> complete_callback) {
-  tCONN_CB* p_ccb;
-
   /* Specific BD address */
-  p_ccb = sdp_conn_originate(bd_addr);
-
+  tCONN_CB* p_ccb = sdp_conn_originate(bd_addr);
   if (!p_ccb) {
     return false;
   }
