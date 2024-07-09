a47377c5b6ada72bd58e91679f698fd645653356
Chris Manton <cmanton@google.com>
2024-07-09 15:38:37 -0700

    mock: [7/63] Remove BTM_BleSetPhy
    Use get_btm_client_interface() instead
    
    Bug: 352129816
    Test: m .
    Flag: EXEMPT, Test infrastructure
    
    Change-Id: Ie0d6877999ae445840a4df125ea60aeb683b3678

--- system/bta/test/common/btm_api_mock.cc
+++ system/bta/test/common/btm_api_mock.cc
@@ -33,21 +33,9 @@ static bluetooth::manager::MockBtmInterface* btm_interface = nullptr;
 
 void bluetooth::manager::SetMockBtmInterface(MockBtmInterface* mock_btm_interface) {
   btm_interface = mock_btm_interface;
-  mock_btm_client_interface.peer.BTM_IsPhy2mSupported = [](const RawAddress& remote_bda,
-                                                           tBT_TRANSPORT transport) {
-    return btm_interface->IsPhy2mSupported(remote_bda, transport);
-  };
-  mock_btm_client_interface.peer.BTM_GetHCIConnHandle = [](RawAddress const& bd_addr,
-                                                           tBT_TRANSPORT transport) -> uint16_t {
-    return btm_interface->GetHCIConnHandle(bd_addr, transport);
-  };
-  mock_btm_client_interface.peer.BTM_GetPeerSCA = [](const RawAddress& remote_bda,
-                                                     tBT_TRANSPORT transport) {
-    return btm_interface->GetPeerSCA(remote_bda, transport);
-  };
-  mock_btm_client_interface.peer.BTM_RequestPeerSCA = [](RawAddress const& bd_addr,
-                                                         tBT_TRANSPORT transport) {
-    btm_interface->RequestPeerSCA(bd_addr, transport);
+  mock_btm_client_interface.ble.BTM_BleSetPhy = [](const RawAddress& bd_addr, uint8_t tx_phys,
+                                                   uint8_t rx_phys, uint16_t phy_options) {
+    btm_interface->BleSetPhy(bd_addr, tx_phys, rx_phys, phy_options);
   };
 }
 
@@ -64,12 +52,6 @@ tBTM_STATUS BTM_SetEncryption(const RawAddress& bd_addr, tBT_TRANSPORT transport
                               tBTM_SEC_CALLBACK* p_callback, void* p_ref_data,
                               tBTM_BLE_SEC_ACT sec_act) {
   return btm_interface->SetEncryption(bd_addr, transport, p_callback, p_ref_data, sec_act);
-}
-
-void BTM_BleSetPhy(const RawAddress& bd_addr, uint8_t tx_phys, uint8_t rx_phys,
-                   uint16_t phy_options) {
-  log::assert_that(btm_interface != nullptr, "Mock btm interface not set!");
-  btm_interface->BleSetPhy(bd_addr, tx_phys, rx_phys, phy_options);
 }
 
 bool BTM_SecIsSecurityPending(const RawAddress& bd_addr) {
