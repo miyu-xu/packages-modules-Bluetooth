4edb0822a8c99810d866a34f22f736c6749fa50f
mtk26430 <jinyao.yu@mediatek.com>
2024-07-12 17:04:36 +0800

    Add A2DP src and sink co-exist feature stack layer (4/4)
    [Description]
    Add A2DP src and sink co-exist feature, that we can connect
    both sink and src remote device at the same time while only
    keep 1 streaming.
    
    Bug: 353599680
    Test: mmm packages/modules/Bluetooth
    
    Change-Id: Ic3744d8fa9a3e32cf0fdd25968c25477530cd95a

--- system/btif/include/btif_av.h
+++ system/btif/include/btif_av.h
@@ -296,5 +296,6 @@ bool btif_av_peer_is_connected_sink(const RawAddress& peer_address);
 bool btif_av_peer_is_connected_source(const RawAddress& peer_address);
 bool btif_av_peer_is_sink(const RawAddress& peer_address);
 bool btif_av_peer_is_source(const RawAddress& peer_address);
+bool btif_av_src_is_connected(void);
 
 #endif /* BTIF_AV_H */
