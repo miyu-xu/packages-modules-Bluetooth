8c8f61a400ea6ec04319f720cd265638016caeee
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-07-15 07:47:17 +0000

    bta: vc: handle active members list changes
    This adds handling of active members list changes.
    Once the active members list changes, ensure the absolute volume
    and mute state is applied to every active device in the set.
    
    Bug: 357472821
    Flag: leaudio_unicast_dynamic_coordinated_sets
    Test: atest bluetooth_vc_test
    Test: atest com.android.bluetooth.csip.CsipSetCoordinatorServiceTest
    Test: atest com.android.bluetooth.le_audio.LeAudioServiceTest
    Change-Id: I559eb30ef48665358a3691be03721a9be6a5fdb1

--- android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/le_audio/LeAudioServiceTest.java
@@ -3212,4 +3212,53 @@ public class LeAudioServiceTest {
                 .sendBroadcastAsUser(
                         MockitoHamcrest.argThat(AllOf.allOf(matchers)), any(), any(), any());
     }
+
+    /** Test the group volume is applied to all group active members */
+    @Test
+    public void testUpdateGroupVolumeOnActiveMembersListChange() {
+        mSetFlagsRule.enableFlags(Flags.FLAG_LEAUDIO_GETTING_ACTIVE_STATE_SUPPORT);
+        mSetFlagsRule.enableFlags(Flags.FLAG_LEAUDIO_ALLOWED_CONTEXT_MASK);
+        /* AUDIO_DIRECTION_OUTPUT_BIT = 0x01 */
+        int direction = 1;
+        int snkAudioLocation = 3;
+        int srcAudioLocation = 4;
+        int availableContexts = 5 + BluetoothLeAudio.CONTEXT_TYPE_RINGTONE;
+
+        // Not connected device
+        assertThat(mService.setActiveDevice(mSingleDevice)).isFalse();
+
+        // Connected device
+        doReturn(true).when(mNativeInterface).connectLeAudio(any(BluetoothDevice.class));
+        connectTestDevice(mSingleDevice, testGroupId);
+
+        // Active members changed - group is inactive yet
+        mService.csipActiveMembersChanged(testGroupId);
+
+        InOrder inOrder = inOrder(mVolumeControlService);
+
+        inOrder.verify(mVolumeControlService, times(0)).setGroupVolume(anyInt(), anyInt());
+
+        // Add location support
+        LeAudioStackEvent audioConfChangedEvent =
+                new LeAudioStackEvent(LeAudioStackEvent.EVENT_TYPE_AUDIO_CONF_CHANGED);
+        audioConfChangedEvent.device = mSingleDevice;
+        audioConfChangedEvent.valueInt1 = direction;
+        audioConfChangedEvent.valueInt2 = testGroupId;
+        audioConfChangedEvent.valueInt3 = snkAudioLocation;
+        audioConfChangedEvent.valueInt4 = srcAudioLocation;
+        audioConfChangedEvent.valueInt5 = availableContexts;
+        mService.messageFromNative(audioConfChangedEvent);
+
+        assertThat(mService.setActiveDevice(mSingleDevice)).isTrue();
+        verify(mNativeInterface).groupSetActive(testGroupId);
+
+        // Set group and device as active.
+        injectGroupStatusChange(testGroupId, LeAudioStackEvent.GROUP_STATUS_ACTIVE);
+
+        // Active members changed - group is active at the time
+        mService.csipActiveMembersChanged(testGroupId);
+
+        // Verify the Volume is applied
+        inOrder.verify(mVolumeControlService, times(1)).setGroupVolume(anyInt(), anyInt());
+    }
 }
