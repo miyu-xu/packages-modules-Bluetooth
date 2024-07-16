a96440e9bcb24ae959c192ccb4da97dbf15b8b71
Palash Ahuja <ahujapalash@google.com>
2024-07-16 15:10:27 -0700

    BluetoothMetrics: Function for bluetooth event logging
    Test: m com.android.btservices
    Bug: 345564021
    Change-Id: I4e61d4975567a67e94f5fcebfdc0ed3206c7cea3

---
+++ system/gd/metrics/bluetooth_event.cc
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bluetooth_event.h"
+
+#include <frameworks/proto_logging/stats/enums/bluetooth/enums.pb.h>
+
+#include "os/log.h"
+#include "os/metrics.h"
+
+namespace bluetooth {
+namespace metrics {
+
+using android::bluetooth::EventType;
+using android::bluetooth::State;
+using hci::ErrorCode;
+
+State mapHciReasonToEnum(ErrorCode reason) {
+  // TODO - add a manual mapping from each status code to State
+  switch (reason) {
+    case ErrorCode::SUCCESS:
+      return State::SUCCESS;
+    case ErrorCode::CONNECTION_ALREADY_EXISTS:
+      return State::ALREADY_CONNECTED;
+    case ErrorCode::CONNECTION_TIMEOUT:
+    case ErrorCode::CONNECTION_ACCEPT_TIMEOUT:
+    case ErrorCode::PAGE_TIMEOUT:
+      return State::TIMEOUT;
+    case ErrorCode::REMOTE_USER_TERMINATED_CONNECTION:
+    case ErrorCode::REMOTE_DEVICE_TERMINATED_CONNECTION_LOW_RESOURCES:
+    case ErrorCode::REMOTE_DEVICE_TERMINATED_CONNECTION_POWER_OFF:
+      return State::REMOTE_USER_TERMINATED_CONNECTION;
+    case ErrorCode::MEMORY_CAPACITY_EXCEEDED:
+      return State::MEMORY_EXCEEDED;
+    default:
+      return State::STATE_UNKNOWN;
+  }
+}
+
+void LogAclCompletionEvent(const hci::Address& address, ErrorCode reason,
+                           bool is_locally_initiated) {
+  if (is_locally_initiated) {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_INITIATOR,
+                                           mapHciReasonToEnum(reason));
+  } else {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_RESPONDER,
+                                           mapHciReasonToEnum(reason));
+  }
+}
+
+}  // namespace metrics
+}  // namespace bluetooth
--- system/gd/os/android/metrics.cc
+++ system/gd/os/android/metrics.cc
@@ -29,6 +29,7 @@
 #include "common/strings.h"
 #include "hardware/bt_av.h"
 #include "hci/hci_packets.h"
+#include "metrics/bluetooth_event.h"
 #include "metrics/metrics_state.h"
 #include "os/log.h"
 
@@ -48,6 +49,10 @@ struct formatter<android::bluetooth::DeviceInfoSrcEnum>
 template <>
 struct formatter<android::bluetooth::AddressTypeEnum>
     : enum_formatter<android::bluetooth::AddressTypeEnum> {};
+template <>
+struct formatter<android::bluetooth::EventType> : enum_formatter<android::bluetooth::EventType> {};
+template <>
+struct formatter<android::bluetooth::State> : enum_formatter<android::bluetooth::State> {};
 }  // namespace fmt
 
 namespace bluetooth {
@@ -444,6 +449,18 @@ void LogMetricBluetoothLEConnection(os::LEConnectionSessionOptions session_optio
             session_options.remote_address,
             common::ToHexString(session_options.acl_connection_state),
             common::ToHexString(session_options.origin_type));
+  }
+}
+
+void LogMetricBluetoothEvent(const Address& address, android::bluetooth::EventType event_type,
+                             android::bluetooth::State state) {
+  int metric_id = 0;
+  if (!address.IsEmpty()) {
+    metric_id = MetricIdManager::GetInstance().AllocateId(address);
+  }
+  int ret = stats_write(BLUETOOTH_EVENT_REPORTED, event_type, state, metric_id, 0, 0);
+  if (ret < 0) {
+    log::warn("Failed BluetoothEvent - Address {}, Event_type {}, State {}", address, event_type, state);
   }
 }
 
