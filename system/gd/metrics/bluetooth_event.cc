a94a7b3d8a8e8b78b8ac26db78f084ec70199d45
Palash Ahuja <ahujapalash@google.com>
2024-07-16 15:10:27 -0700

    BluetoothMetrics: Function for bluetooth event logging
    Test: m com.android.btservices and statsd_testdrive
    Bug: 345564021
    Change-Id: I4e61d4975567a67e94f5fcebfdc0ed3206c7cea3

---
+++ system/gd/metrics/bluetooth_event.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bluetooth_event.h"
+
+namespace bluetooth {
+namespace metrics {
+
+using android::bluetooth::EventType;
+using android::bluetooth::State;
+using hci::ErrorCode;
+
+State MapErrorCodeToState(ErrorCode reason) {
+  // TODO - map the error codes to the state enum variants.
+  switch (reason) {
+    case ErrorCode::SUCCESS:
+      return State::SUCCESS;
+    default:
+      return State::STATE_UNKNOWN;
+  }
+}
+
+void LogAclCompletionEvent(const hci::Address& address, ErrorCode reason,
+                           bool is_locally_initiated) {
+  if (is_locally_initiated) {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_INITIATOR,
+                                           MapErrorCodeToState(reason));
+  } else {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_RESPONDER,
+                                           MapErrorCodeToState(reason));
+  }
+}
+
+}  // namespace metrics
+}  // namespace bluetooth
