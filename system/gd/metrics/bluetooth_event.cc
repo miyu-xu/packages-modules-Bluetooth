d1ec1d4f1ed7e6d2d84752aaab7e44862c43de0b
Palash Ahuja <ahujapalash@google.com>
2024-07-18 11:30:53 -0700

    BluetoothMetrics: Map Error Code to State in Bluetooth Event
    Test: m com.google.android.btservices and statsd_testdrive
    Bug: 345564021
    Flag: EXEMPT, metrics related changes
    Change-Id: Ic1fdc83f38e9e415dddd52086ab9d79514d9a51c

--- system/gd/metrics/bluetooth_event.cc
+++ system/gd/metrics/bluetooth_event.cc
@@ -31,6 +31,21 @@ State MapErrorCodeToState(ErrorCode reason) {
   switch (reason) {
     case ErrorCode::SUCCESS:
       return State::SUCCESS;
+    // Timeout related errors
+    case ErrorCode::PAGE_TIMEOUT:
+    case ErrorCode::CONNECTION_TIMEOUT:
+    case ErrorCode::CONNECTION_ACCEPT_TIMEOUT:
+    case ErrorCode::TRANSACTION_RESPONSE_TIMEOUT:
+      return State::TIMEOUT;
+    // Connection exists (should mark it as success?)
+    case ErrorCode::CONNECTION_ALREADY_EXISTS:
+      return State::ALREADY_CONNECTED;
+    case ErrorCode::REPEATED_ATTEMPTS:
+      return State::REPEATED_ATTEMPTS;
+    case ErrorCode::PIN_OR_KEY_MISSING:
+      return State::KEY_MISSING;
+    case ErrorCode::PAIRING_NOT_ALLOWED:
+      return State::PAIRING_NOT_ALLOWED;
     default:
       return State::STATE_UNKNOWN;
   }
