6aff4764f7d7747c77d4abcd8fbc93a5f3c25764
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-07-19 06:40:24 +0000

    Revert "RAS: Ignore duplicate BTA_GATTC_SEARCH_CMPL_EVT"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/27fe2ee8-3eb4-4d31-b780-a20e07486337). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: If15c17565d496885bc01efd143f25c67b5adce6e
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- system/bta/ras/ras_client.cc
+++ system/bta/ras/ras_client.cc
@@ -53,7 +53,6 @@ public:
     uint16_t latest_ranging_counter_ = 0;
     bool handling_on_demand_data_ = false;
     bool is_connected_ = false;
-    bool service_search_complete_ = false;
     std::vector<VendorSpecificCharacteristic> vendor_specific_characteristics_;
     uint8_t writeReplyCounter_ = 0;
     uint8_t writeReplySuccessCounter_ = 0;
@@ -227,15 +226,11 @@ public:
       }
     }
 
-    if (tracker->service_search_complete_) {
-      log::info("Service search already completed, ignore");
-      return;
-    } else if (!service_found) {
+    if (!service_found) {
       log::error("Can't find Ranging Service in the services list");
       return;
     } else {
       log::info("Found Ranging Service");
-      tracker->service_search_complete_ = true;
       ListCharacteristic(tracker);
     }
 
@@ -505,7 +500,6 @@ public:
   }
 
   void ListCharacteristic(std::shared_ptr<RasTracker> tracker) {
-    tracker->vendor_specific_characteristics_.clear();
     for (auto& characteristic : tracker->service_->characteristics) {
       bool vendor_specific = !IsRangingServiceCharacteristic(characteristic.uuid);
       log::info(
