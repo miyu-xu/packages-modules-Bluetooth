3ecf896976a49514fa92d2035d328e4376a8c982
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-07-22 16:58:14 +0000

    Set LE Data length on LE CoC connection
    - Set Max length as Data length for LE CoC sockets
    - Allow privileged clients to decrease the data length in order
      to provide bandwidth for other ACLs (eg: ASHA)
    - Prioritize the privileged client's data length over other requests
    
    Test: mmm packages/modules/Bluetooth
    Flag: com.android.bluetooth.flags.set_max_data_length_for_lecoc
    Bug: 206487778
    Bug: 373726267
    
    Change-Id: Ia4a05b2c74dd7c025fc8e52ee9739dbcfabe340d

--- system/test/mock/mock_stack_btm_interface.cc
+++ system/test/mock/mock_stack_btm_interface.cc
@@ -181,9 +181,8 @@ struct btm_client_interface_t default_btm_client_interface = {
                   return tBTM_STATUS::BTM_SUCCESS;
                 },
                 .BTM_SetBleDataLength = [](const RawAddress& /* bd_addr */,
-                                           uint16_t /* tx_pdu_length */) -> tBTM_STATUS {
-                  return tBTM_STATUS::BTM_SUCCESS;
-                },
+                                           uint16_t /* tx_pdu_length */, bool /* priv_client */)
+                        -> tBTM_STATUS { return tBTM_STATUS::BTM_SUCCESS; },
                 .BTM_BleReadControllerFeatures =
                         [](tBTM_BLE_CTRL_FEATURES_CBACK* /* p_vsc_cback */) {},
                 .BTM_BleSetPhy = [](const RawAddress& /* bd_addr */, uint8_t /* tx_phys */,
