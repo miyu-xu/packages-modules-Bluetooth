c0ae67ee117ec8e346cb60050427bccef275214c
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-07-22 08:26:43 +0000

    bta: le_audio: Avoid creating another CIS for ASE
    Before issuing Establish CIS command, make sure the ASE don't have
    CIS assigned already. It the CIS is in one of the following states:
    CONNECTING, CONNECTED, DISCONNECTING, use the existing CIS.
    This change added guard for calling EstablishCis method with
    empty CIS conn handles.
    
    Change-Id: Ic5827fd8a58fa29fe488bf488021674c51c58f91

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -1590,9 +1590,9 @@ private:
       /* First in ase pair is Sink, second Source */
       auto ases_pair = leAudioDevice->GetAsesByCisConnHdl(ase->cis_conn_hdl);
 
-      /* Already in pending state - bi-directional CIS or seconde CIS to same
-       * device */
-      if (ase->cis_state == CisState::CONNECTING || ase->cis_state == CisState::CONNECTED) {
+      /* Already in pending state - bi-directional CIS */
+      if (ase->cis_state == CisState::CONNECTING || ase->cis_state == CisState::CONNECTED ||
+          ase->cis_state == CisState::DISCONNECTING) {
         continue;
       }
 
@@ -1615,7 +1615,9 @@ private:
             kLogStateMachineTag, leAudioDevice->group_id_, RawAddress::kEmpty,
             kLogCisCreateOp + "#CIS: " + std::to_string(conn_pairs.size()), extra_stream.str());
 
-    IsoManager::GetInstance()->EstablishCis({.conn_pairs = std::move(conn_pairs)});
+    if (!conn_pairs.empty()) {
+      IsoManager::GetInstance()->EstablishCis({.conn_pairs = std::move(conn_pairs)});
+    }
 
     return true;
   }
@@ -1642,7 +1644,8 @@ private:
         auto ases_pair = leAudioDevice->GetAsesByCisConnHdl(ase->cis_conn_hdl);
 
         /* Already in pending state - bi-directional CIS */
-        if (ase->cis_state == CisState::CONNECTING) {
+        if (ase->cis_state == CisState::CONNECTING || ase->cis_state == CisState::CONNECTED ||
+            ase->cis_state == CisState::DISCONNECTING) {
           continue;
         }
 
@@ -1661,7 +1664,9 @@ private:
       } while ((ase = leAudioDevice->GetNextActiveAse(ase)));
     } while ((leAudioDevice = group->GetNextActiveDevice(leAudioDevice)));
 
-    IsoManager::GetInstance()->EstablishCis({.conn_pairs = std::move(conn_pairs)});
+    if (!conn_pairs.empty()) {
+      IsoManager::GetInstance()->EstablishCis({.conn_pairs = std::move(conn_pairs)});
+    }
 
     return true;
   }
