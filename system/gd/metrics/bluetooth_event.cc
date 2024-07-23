fc7431fa0c5567961e0769e2211f047edf9913f4
Palash Ahuja <ahujapalash@google.com>
2024-07-23 14:54:25 -0700

    BluetoothMetrics: Log Remote Name Request Completion
    Test: m com.android.btservices and statsd_testdrive
    Bug: 345564021
    Flag: EXEMPT, metrics related changes
    Change-Id: I581271ae246b216f786266e8777bdca3bfaf42d4

---
+++ system/gd/metrics/bluetooth_event.cc
@@ -0,0 +1,96 @@
+<<<<<<< PATCH SET (7f079b BluetoothMetrics: Log Remote Name Request Completion)
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "bluetooth_event.h"
+
+#include <frameworks/proto_logging/stats/enums/bluetooth/enums.pb.h>
+
+#include "main/shim/helpers.h"
+#include "os/metrics.h"
+#include "stack/include/btm_status.h"
+#include "types/raw_address.h"
+
+namespace bluetooth {
+namespace metrics {
+
+using android::bluetooth::EventType;
+using android::bluetooth::State;
+using hci::ErrorCode;
+
+State MapErrorCodeToState(ErrorCode reason) {
+  // TODO - map the error codes to the state enum variants.
+  switch (reason) {
+    case ErrorCode::SUCCESS:
+      return State::SUCCESS;
+    // Timeout related errors
+    case ErrorCode::PAGE_TIMEOUT:
+    case ErrorCode::CONNECTION_TIMEOUT:
+    case ErrorCode::CONNECTION_ACCEPT_TIMEOUT:
+    case ErrorCode::TRANSACTION_RESPONSE_TIMEOUT:
+      return State::TIMEOUT;
+    // Connection exists (should mark it as success?)
+    case ErrorCode::CONNECTION_ALREADY_EXISTS:
+      return State::ALREADY_CONNECTED;
+    case ErrorCode::REPEATED_ATTEMPTS:
+      return State::REPEATED_ATTEMPTS;
+    case ErrorCode::PIN_OR_KEY_MISSING:
+      return State::KEY_MISSING;
+    case ErrorCode::PAIRING_NOT_ALLOWED:
+      return State::PAIRING_NOT_ALLOWED;
+    case ErrorCode::CONNECTION_REJECTED_LIMITED_RESOURCES:
+      return State::RESOURCES_EXCEEDED;
+    default:
+      return State::STATE_UNKNOWN;
+  }
+}
+
+void LogAclCompletionEvent(const hci::Address& address, ErrorCode reason,
+                           bool is_locally_initiated) {
+  if (is_locally_initiated) {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_INITIATOR,
+                                           MapErrorCodeToState(reason));
+  } else {
+    bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_RESPONDER,
+                                           MapErrorCodeToState(reason));
+  }
+}
+
+void LogRemoteNameRequestCompletion(const RawAddress& raw_address) {
+  hci::Address address = bluetooth::ToGdAddress(raw_address);
+  bluetooth::os::LogMetricBluetoothEvent(address, EventType::REMOTE_NAME_REQUEST, State::SUCCESS);
+}
+
+void LogAclAfterRemoteNameRequest(const RawAddress& raw_address, tBTM_STATUS status) {
+  hci::Address address = bluetooth::ToGdAddress(raw_address);
+
+  switch (status) {
+    case BTM_SUCCESS:
+      bluetooth::os::LogMetricBluetoothEvent(address, EventType::ACL_CONNECTION_INITIATOR,
+                                             State::ALREADY_CONNECTED);
+      break;
+    case BTM_NO_RESOURCES:
+      bluetooth::os::LogMetricBluetoothEvent(
+              address, EventType::ACL_CONNECTION_INITIATOR,
+              MapErrorCodeToState(ErrorCode::CONNECTION_REJECTED_LIMITED_RESOURCES));
+      break;
+    default:
+      break;
+  }
+}
+}  // namespace metrics
+}  // namespace bluetooth
+=======
+>>>>>>> BASE      (b9e1f3 Merge changes Ibe26844b,Ib3cbe9ef,Id0935c0f,I828a96d1,I73d16)
