3a22ef1a8091d4597fd26f7da8c0a1a1ddf31826
Palash Ahuja <ahujapalash@google.com>
2024-07-23 14:54:25 -0700

    BluetoothMetrics: Log Remote Name Request Completion
    Test: m com.android.btservices and statsd_testdrive
    Bug: 345564021
    Flag: EXEMPT, metrics related changes
    Change-Id: I581271ae246b216f786266e8777bdca3bfaf42d4

--- system/gd/metrics/bluetooth_event.h
+++ system/gd/metrics/bluetooth_event.h
@@ -19,6 +19,7 @@
 #include "hci/address.h"
 #include "hci/hci_packets.h"
 #include "stack/include/btm_status.h"
+#include "stack/include/hci_error_code.h"
 #include "types/raw_address.h"
 
 namespace bluetooth {
@@ -27,6 +28,7 @@ namespace metrics {
 void LogAclCompletionEvent(const hci::Address& address, hci::ErrorCode reason,
                            bool is_locally_initiated);
 
+void LogRemoteNameRequestCompletion(const RawAddress& raw_address, tHCI_STATUS hci_status);
 
 void LogAclAfterRemoteNameRequest(const RawAddress& raw_address, tBTM_STATUS status);
 
