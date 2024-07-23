6c35668e1a7c3986021f352ade0cb790b351514b
Ritvik Mahesh <quic_rmahesh@quicinc.com>
2024-07-23 18:49:51 +0530

    Bluetooth consuming more power while perforing OPP TX/RX
    Steps to reproduce:
    
    1.  Turn  on BT  DUT and Ref device
    
    2.  Establish BT connection between devices
    
    3. Start File transfer between DUT and Ref device (opp TX/RX)
    
    4.Disconnect USB cable  on DUT
    
    Observed Results:
    
    BT OPP TX/RX power numbers are high
    
    Expected Results:
    
    BT OPP TX/RX power numbers should be within the known limits
    
    Repro rate : 10/10 times
    
    Root cause: 1)sniff timer start/stop repeatdly
                2)OBEX MTU used is 4K instead of 8K
    
    Fix: 1) Increase obex MTU to 8K
         2) Use 1 idle sec timer and check OPP status after 1 sec timery expiry and start sniff idle timer if required.
    
    Bug: 378616532
    
    Change-Id: Ieaf86de48561549dbfc25e2196d8251c4c08a4da

--- system/bta/jv/bta_jv_int.h
+++ system/bta/jv/bta_jv_int.h
@@ -30,6 +30,7 @@
 #include "bta/include/bta_jv_api.h"
 #include "bta/include/bta_sec_api.h"
 #include "internal_include/bt_target.h"
+#include "osi/include/alarm.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/rfcdefs.h"
 #include "types/bluetooth/uuid.h"
@@ -55,6 +56,8 @@ typedef struct {
   uint8_t state;           /* state: see above enum */
   tBTA_JV_PM_ID app_id;    /* JV app specific id indicating power table to use */
   RawAddress peer_bd_addr; /* Peer BD address */
+  bool cong;               /* TRUE, if congested */
+  alarm_t* idle_timer;     /* intermediate idle timer for paricular scb */
 } tBTA_JV_PM_CB;
 
 enum {
@@ -175,5 +178,6 @@ void bta_jv_rfcomm_write(uint32_t handle, uint32_t req_id, tBTA_JV_RFC_CB* p_cb,
 void bta_jv_set_pm_profile(uint32_t handle, tBTA_JV_PM_ID app_id, tBTA_JV_CONN_STATE init_st);
 
 void bta_jv_l2cap_stop_server_le(uint16_t local_chan);
+extern void bta_jv_idle_timeout_handler(void* tle);
 
 #endif /* BTA_JV_INT_H */
