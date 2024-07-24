db065a0afb0cbb9581db1f9341beb6cb6c7352eb
William Escande <wescande@google.com>
2024-07-23 18:20:50 -0700

    SystemServer: One unique method to reset the Adapter
    Bug: 311772251
    Test: None
    Flag: Exempt refactor
    Change-Id: Iff430e5248eee0fcb3a75a8a4c59f03a0feca367

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1186,7 +1186,7 @@ class BluetoothManagerService {
         Log.d(TAG, "Force sleep 100 ms for propagating Bluetooth app death");
         SystemClock.sleep(100); // required to let the ActivityManager be notified of BT death
 
-        mAdapter = null;
+        mAdapter = null; // Don't call resetAdapter as we already call unbindService
         mHandler.removeMessages(MESSAGE_TIMEOUT_BIND);
     }
 
@@ -1605,12 +1605,10 @@ class BluetoothManagerService {
 
                 case MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED:
                     Log.e(TAG, "MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED");
-                    // if service is unbinded already, do nothing and return
-                    if (mAdapter == null) {
+
+                    if (!resetAdapter()) {
                         break;
                     }
-                    mContext.unbindService(mConnection);
-                    mAdapter = null;
 
                     // log the unexpected crash
                     addCrashLog();
@@ -1650,12 +1648,7 @@ class BluetoothManagerService {
                         ActiveLogs.add(ENABLE_DISABLE_REASON_RESTARTED, true);
                         handleEnable(mQuietEnable);
                     } else {
-                        // if service is unbinded already, do nothing and return
-                        if (mAdapter == null) {
-                            break;
-                        }
-                        mContext.unbindService(mConnection);
-                        mAdapter = null;
+                        resetAdapter();
                         Log.e(TAG, "Reach maximum retry to restart Bluetooth!");
                     }
                     break;
@@ -1865,6 +1858,15 @@ class BluetoothManagerService {
         }
     }
 
+    private boolean resetAdapter() {
+        if (mAdapter == null) {
+            return false;
+        }
+        mAdapter = null;
+        mContext.unbindService(mConnection);
+        return true;
+    }
+
     private void handleEnable(boolean quietMode) {
         mQuietEnable = quietMode;
 
@@ -2071,11 +2073,7 @@ class BluetoothManagerService {
 
         sendBluetoothServiceDownCallback();
 
-        if (mAdapter != null) {
-            mAdapter = null;
-            // Unbind
-            mContext.unbindService(mConnection);
-        }
+        resetAdapter();
 
         mHandler.removeMessages(MESSAGE_BLUETOOTH_STATE_CHANGE);
         mState.set(STATE_OFF);
