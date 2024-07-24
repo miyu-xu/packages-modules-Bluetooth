b3ed24b364e9609aa3473c05f4ba720113179c6a
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-07-24 02:09:27 +0000

    Add null checks for list access
    Ensure that queue is not null before calling list_is_empty(),
    list_append() and list_prepend()
    
    Test: m Bluetooth
    Bug: 344702352
    Bug: 354862416
    Change-Id: Ib3c853c1b58980b36a2b433c866f730788a27c53

--- system/stack/l2cap/l2c_ble.cc
+++ system/stack/l2cap/l2c_ble.cc
@@ -1026,7 +1026,8 @@ void l2c_ble_link_adjust_allocation(void) {
       /* There is a special case where we have readjusted the link quotas and */
       /* this link may have sent anything but some other link sent packets so */
       /* so we may need a timer to kick off this link's transmissions. */
-      if ((p_lcb->link_state == LST_CONNECTED) && (!list_is_empty(p_lcb->link_xmit_data_q)) &&
+      if ((p_lcb->link_state == LST_CONNECTED) &&
+          (p_lcb->link_xmit_data_q && !list_is_empty(p_lcb->link_xmit_data_q)) &&
           (p_lcb->sent_not_acked < p_lcb->link_xmit_quota)) {
         alarm_set_on_mloop(p_lcb->l2c_lcb_timer, L2CAP_LINK_FLOW_CONTROL_TIMEOUT_MS,
                            l2c_lcb_timer_timeout, p_lcb);
