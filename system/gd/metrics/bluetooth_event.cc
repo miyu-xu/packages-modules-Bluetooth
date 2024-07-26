fb48b5fd4aa97004cbac8e64ee3e6c7f4c381c29
Palash Ahuja <ahujapalash@google.com>
2024-07-26 14:20:31 -0700

    BluetoothMetrics: Log Authentication Complete Event
    Test: m com.android.btservices and statsd_testdrive
    Bug: 345564021
    Flag: EXEMPT, metrics related changes
    Change-Id: I1ede6a560ccc1cd6cee49365f1ee46a6e12b920d

--- system/gd/metrics/bluetooth_event.cc
+++ system/gd/metrics/bluetooth_event.cc
@@ -54,6 +54,35 @@ State MapErrorCodeToState(ErrorCode reason) {
   }
 }
 
+State MapHCIStatusToState(tHCI_STATUS status) {
+  // TODO - map the error codes to the state enum variants.
+  switch (status) {
+    case tHCI_STATUS::HCI_SUCCESS:
+      return State::SUCCESS;
+    // Timeout related errors
+    case tHCI_STATUS::HCI_ERR_PAGE_TIMEOUT:
+    case tHCI_STATUS::HCI_ERR_CONNECTION_TOUT:
+    case tHCI_STATUS::HCI_ERR_HOST_TIMEOUT:
+    case tHCI_STATUS::HCI_ERR_LMP_RESPONSE_TIMEOUT:
+      return State::TIMEOUT;
+    case tHCI_STATUS::HCI_ERR_AUTH_FAILURE:
+      return State::AUTH_FAILURE;
+    // Connection Exists (should mark it as success?)
+    case tHCI_STATUS::HCI_ERR_CONNECTION_EXISTS:
+      return State::ALREADY_CONNECTED;
+    case tHCI_STATUS::HCI_ERR_REPEATED_ATTEMPTS:
+      return State::REPEATED_ATTEMPTS;
+    case tHCI_STATUS::HCI_ERR_KEY_MISSING:
+      return State::KEY_MISSING;
+    case tHCI_STATUS::HCI_ERR_PAIRING_NOT_ALLOWED:
+      return State::PAIRING_NOT_ALLOWED;
+    case tHCI_STATUS::HCI_ERR_HOST_REJECT_RESOURCES:
+      return State::RESOURCES_EXCEEDED;
+    default:
+      return State::STATE_UNKNOWN;
+  }
+}
+
 void LogAclCompletionEvent(const hci::Address& address, ErrorCode reason,
                            bool is_locally_initiated) {
   bluetooth::os::LogMetricBluetoothEvent(address,
@@ -90,6 +119,15 @@ void LogAclAfterRemoteNameRequest(const RawAddress& raw_address, tBTM_STATUS sta
 void LogUserConfirmationRequestResponse(const hci::Address& address, bool positive) {
   bluetooth::os::LogMetricBluetoothEvent(address, EventType::USER_CONF_REQUEST,
                                          positive ? State::SUCCESS : State::FAIL);
+}
+
+void LogAuthenticationComplete(const RawAddress& raw_address, tHCI_STATUS hci_status) {
+  hci::Address address = bluetooth::ToGdAddress(raw_address);
+  bluetooth::os::LogMetricBluetoothEvent(address,
+                                         hci_status == tHCI_STATUS::HCI_SUCCESS
+                                                 ? EventType::AUTHENTICATION_COMPLETE
+                                                 : EventType::AUTHENTICATION_COMPLETE_FAIL,
+                                         MapHCIStatusToState(hci_status));
 }
 
 }  // namespace metrics
