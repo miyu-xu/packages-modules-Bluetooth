e7347b5c53d231760141f82bc9fe3bd0fedd37d9
Etienne Ruffieux <eruffieux@google.com>
2024-07-26 12:17:19 +0000

    Add BumbleBluetooth AVRCP tests
    First tests added, other tests will follow as changes are
    merged.
    
    Bug: 346730065
    Test: atest BumbleBluetoothTests:android.bluetooth.AvrcpTest
    Flag: TEST ONLY
    Change-Id: If4df30bf60a81f0999289762b1e09ac9b4c728f1

--- pandora/server/bumble_experimental/avrcp.py
+++ pandora/server/bumble_experimental/avrcp.py
@@ -12,22 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import grpc
+import logging
+
+from bumble.avc import PassThroughFrame
 from bumble.avdtp import Listener as AvdtpListener, MediaCodecCapabilities, AVDTP_AUDIO_MEDIA_TYPE
 from bumble.avrcp import Protocol as AvrcpProtocol, make_target_service_sdp_records, make_controller_service_sdp_records
 from bumble.a2dp import (A2DP_SBC_CODEC_TYPE, SBC_DUAL_CHANNEL_MODE, SBC_JOINT_STEREO_CHANNEL_MODE,
                          SBC_LOUDNESS_ALLOCATION_METHOD, SBC_MONO_CHANNEL_MODE, SBC_SNR_ALLOCATION_METHOD,
                          SBC_STEREO_CHANNEL_MODE, SbcMediaCodecInformation, make_audio_sink_service_sdp_records,
                          make_audio_source_service_sdp_records)
-from bumble.device import Device
+from bumble.device import Device, Connection
+from bumble.pandora import utils
+from google.protobuf.empty_pb2 import Empty
 from pandora_experimental.avrcp_grpc_aio import AVRCPServicer
+from pandora_experimental.avrcp_pb2 import (
+    MediaAttribute,
+    GetElementAttributesRequest,
+    GetElementAttributesResponse,
+)
 
 
 class AvrcpService(AVRCPServicer):
     device: Device
+    connection: Connection
+    avrcp_protocol: AvrcpProtocol
+    avdtp_listener: AvdtpListener
 
     def __init__(self, device: Device) -> None:
         super().__init__()
         self.device = device
+        self.logger = logging.getLogger(__name__)
+        self.connection = None
+        self.avrcp_connected = False
 
         sdp_records = {
             0x00010002: make_audio_source_service_sdp_records(0x00010002),  # A2DP Source
@@ -38,16 +55,64 @@ class AvrcpService(AVRCPServicer):
         self.device.sdp_service_records.update(sdp_records)
 
         # Register AVDTP L2cap
-        avdtp_listener = AvdtpListener.for_device(device)
+        self.avdtp_listener = AvdtpListener.for_device(device)
 
-        def on_avdtp_connection(server) -> None:  # type: ignore
-            server.add_sink(codec_capabilities())  # type: ignore
-
-        avdtp_listener.on('connection', on_avdtp_connection)  # type: ignore
+        self.device.on('connection', self.on_device_connected)  # type: ignore
+        self.avdtp_listener.on('connection', self.on_avdtp_connection)  # type: ignore
 
         # Register AVRCP L2cap
-        avrcp_protocol = AvrcpProtocol(delegate=None)
-        avrcp_protocol.listen(device)
+        self.avrcp_protocol = AvrcpProtocol(delegate=None)
+        self.avrcp_protocol.listen(device)
+
+    def __del__(self) -> None:
+        self.device.remove_listener('connection', self.on_device_connected)  # type: ignore
+        self.avdtp_listener.remove_listener('connection', self.on_avdtp_connection)  # type: ignore
+
+    def on_avdtp_connection(self, server) -> None:  # type: ignore
+        server.add_sink(codec_capabilities())  # type: ignore
+
+    def on_device_connected(self, connection) -> None:
+        self.connection = connection
+
+    @utils.rpc
+    async def SendKeyEventNext(self, request: Empty, context: grpc.ServicerContext) -> Empty:
+        if not self.avrcp_connected:
+            await self.avrcp_protocol.connect(self.connection)
+            self.avrcp_connected = True
+
+        self.logger.info("SendKeyEventNext")
+        await self.avrcp_protocol.send_key_event(PassThroughFrame.OperationId.FORWARD, True)
+        await self.avrcp_protocol.send_key_event(PassThroughFrame.OperationId.FORWARD, False)
+        return Empty()
+
+    @utils.rpc
+    async def MonitorNowPlayingContent(self, request: Empty, context: grpc.ServicerContext) -> Empty:
+        if not self.avrcp_connected:
+            await self.avrcp_protocol.connect(self.connection)
+            self.avrcp_connected = True
+
+        registered = False
+        async for identifier in self.avrcp_protocol.monitor_now_playing_content():
+            if registered is True:
+                self.logger.info("MonitorNowPlayingContent CHANGED)")
+                return Empty()
+            self.logger.info("MonitorNowPlayingContent INTERIM")
+            registered = True
+
+    @utils.rpc
+    async def GetElementAttributes(self, request: GetElementAttributesRequest,
+                                   context: grpc.ServicerContext) -> GetElementAttributesResponse:
+        if not self.avrcp_connected:
+            await self.avrcp_protocol.connect(self.connection)
+            self.avrcp_connected = True
+
+        attributes = []
+        for attribute in await self.avrcp_protocol.get_element_attributes(request.identifier,
+                                                                          request.requested_attributes):
+            attributes.append(
+                MediaAttribute(attribute_id=attribute.attribute_id, attribute_value=attribute.attribute_value))
+
+        return GetElementAttributesResponse(attribute_values=attributes)
 
 
 def codec_capabilities() -> MediaCodecCapabilities:
