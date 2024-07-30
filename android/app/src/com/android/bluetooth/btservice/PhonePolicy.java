14ee1afa8f0c527b4422e7d905e94cd1ea27c617
Jakub Pawlowski <jpawlowski@google.com>
2024-07-30 11:57:40 +0200

    Fix profiles being marked as FORBIDDEN before bonding is attempted.
    Consider folowing scenario:
    
    1. Device connects over LE transport. Service discovery is triggered.
    2. UUIDS discovered event is propagated through stack, profile
       connection is triggered, which can mark some profiles as FORBIDDEN.
    3. Bonding is triggered during same connection.
    4. Service discovery, and DIS read is triggered after bonding.
    5. Profile connection can't happen, because some profiles were already
       marked as FORBIDDEN in steap 2.
    
    Only Battery Service is allowed to connect while not bonded, therefore
    separate profile connection logic to treat it separately, and deal with
    remaining profiles only when BONDING or BONDED.
    
    Bug: 356325069
    Bug: 355109861
    Flag: unbonded_profile_forbid_fix
    Test: perform subsequent FP pairing couple of times
    Change-Id: I5bc5559b8e9007d6f41d313f170b935e50d50bcc

--- android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
+++ android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -312,7 +312,6 @@ public class PhonePolicy implements AdapterService.BluetoothStateCallback {
         VolumeControlService volumeControlService = mFactory.getVolumeControlService();
         HapClientService hapClientService = mFactory.getHapClientService();
         BassClientService bcService = mFactory.getBassClientService();
-        BatteryService batteryService = mFactory.getBatteryService();
 
         final boolean isBypassLeAudioAllowlist =
                 SystemProperties.getBoolean(BYPASS_LE_AUDIO_ALLOWLIST_PROPERTY, false);
@@ -611,6 +610,12 @@ public class PhonePolicy implements AdapterService.BluetoothStateCallback {
             }
         }
 
+        processInitProfilePrioritiesBondNone(device, uuids);
+    }
+
+    private void processInitProfilePrioritiesBondNone(BluetoothDevice device, ParcelUuid[] uuids) {
+        BatteryService batteryService = mFactory.getBatteryService();
+        debugLog("processInitProfilePrioritiesBondNone() - device " + device);
         if ((batteryService != null)
                 && Utils.arrayContains(uuids, BluetoothUuid.BATTERY)
                 && (batteryService.getConnectionPolicy(device)
@@ -1117,17 +1122,38 @@ public class PhonePolicy implements AdapterService.BluetoothStateCallback {
         }
     }
 
+    private String bondState2str(int state) {
+        if (state == BluetoothDevice.BOND_NONE) {
+            return "BOND_NONE";
+        } else if (state == BluetoothDevice.BOND_BONDING) {
+            return "BOND_BONDING";
+        } else if (state == BluetoothDevice.BOND_BONDED) {
+            return "BOND_BONDED";
+        } else return "UNKNOWN(" + state + ")";
+    }
+
     /**
      * Direct call prior to sending out {@link BluetoothDevice#ACTION_UUID}. This indicates that
      * service discovery is complete and passes the UUIDs directly to PhonePolicy.
      *
      * @param device is the remote device whose services have been discovered
      * @param uuids are the services supported by the remote device
+     * @param bondState is the bond state of remote device
      */
-    void onUuidsDiscovered(BluetoothDevice device, ParcelUuid[] uuids) {
-        debugLog("onUuidsDiscovered: discovered services for device " + device);
+    void onUuidsDiscovered(BluetoothDevice device, ParcelUuid[] uuids, int bondState) {
+        debugLog(
+                "onUuidsDiscovered: discovered services for device "
+                        + device
+                        + " ("
+                        + bondState2str(bondState)
+                        + ")");
         if (uuids != null) {
-            processInitProfilePriorities(device, uuids);
+            if (Flags.unbondedProfileForbidFix() && bondState == BluetoothDevice.BOND_NONE) {
+                processInitProfilePrioritiesBondNone(device, uuids);
+            } else {
+                processInitProfilePriorities(device, uuids);
+            }
+
         } else {
             warnLog("onUuidsDiscovered: uuids is null for device " + device);
         }
