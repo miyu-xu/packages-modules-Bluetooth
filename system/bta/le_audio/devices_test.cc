8d24e562ec06bef42ec33dec95906cc31eb72ac7
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-07-31 13:34:10 +0000

    bta: le_audio: Fix possible data path not being removed
    It may happen the ASE in Enabling state is released from the
    Peripheral side.
    If the HCI_LE_Create_CIS has been already issued by the host
    stack, the controller can still report CIS HCI_LE_Connection_Complete
    event with status SUCCESS followed by HCI_Disconnection_Complete
    event (as the host issues the CIS HCI_Disconnect on ASE Release).
    
    The stack once receives CIS HCI_LE_Connection_Complete will set up
    DataPath. At the time, the DataPath setup is complete, the ASE can
    be not active, thus CIS DataPath state is left in CONFIGURING state.
    
    So that the next attempt to CisEstablish will not assign the new
    CIS Handle, because DataPath has not been removed.
    
    Bug: 357472821
    Flag: leaudio_unicast_dynamic_coordinated_sets
    Test: atest bluetooth_le_audio_test
    Change-Id: I60a49e47570defa743198d361e82ce64fc2f58b4

--- system/bta/le_audio/devices_test.cc
+++ system/bta/le_audio/devices_test.cc
@@ -2178,6 +2178,7 @@ TEST_P(LeAudioAseConfigurationTest, test_reconnection_media) {
   group_->cig.AssignCisConnHandles(handles);
   group_->cig.AssignCisIds(left);
   group_->cig.AssignCisIds(right);
+  group_->AssignCisConnHandlesToAses();
 
   TestActiveAses();
   /* Left got disconnected */
