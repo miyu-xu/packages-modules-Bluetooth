3f45d839d58663163553cf06b737673c932ae5ae
Palash Ahuja <ahujapalash@google.com>
2024-07-31 15:28:38 -0700

    BluetoothMetrics: Log SDP Complete Event
    Test: m com.android.btservices and statsd_testdrive
    Bug: 345564021
    Flag: EXEMPT, metrics related changes
    Change-Id: I5917d3d03a1dcc221c27648c8dbfc896e9a82624

--- system/gd/metrics/bluetooth_event.h
+++ system/gd/metrics/bluetooth_event.h
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include "bta/include/bta_sec_api.h"
 #include "hci/address.h"
 #include "hci/hci_packets.h"
 #include "stack/include/btm_status.h"
@@ -34,5 +35,8 @@ void LogAclAfterRemoteNameRequest(const RawAddress& raw_address, tBTM_STATUS sta
 void LogUserConfirmationRequestResponse(const hci::Address& address, bool positive);
 
 void LogAuthenticationComplete(const hci::Address& address, hci::ErrorCode error_code);
+
+void LogSDPComplete(const RawAddress& raw_address, tBTA_STATUS status);
+
 }  // namespace metrics
 }  // namespace bluetooth
