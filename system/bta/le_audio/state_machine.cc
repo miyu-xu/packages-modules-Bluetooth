494ae4d4d67a8fa11d200efb5561dc34580765d3
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2024-07-31 12:57:16 +0000

    bta: le_audio: Change PrepareAndSendCodecConfigure return type
    This changes return type of PrepareAndSendCodecConfigure method
    so that the status is reported to caller.
    If failed, the stream will be stopped.
    Once called from AttachToStream, the clent.cc can later reschedule
    the process of attaching the device.
    
    Bug: 357472821
    Flag: Exempt; mechanical refactor
    Test: atest bluetooth_le_audio_test
    Change-Id: If755fc1b06b02edafa56854b3b1196f411e90eb3

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -189,8 +189,7 @@ public:
       return false;
     }
 
-    PrepareAndSendCodecConfigure(group, leAudioDevice);
-    return true;
+    return PrepareAndSendCodecConfigure(group, leAudioDevice);
   }
 
   bool StartStream(LeAudioDeviceGroup* group, LeAudioContextType context_type,
@@ -1839,12 +1838,15 @@ private:
     }
 
     for (; leAudioDevice; leAudioDevice = group->GetNextActiveDevice(leAudioDevice)) {
-      PrepareAndSendCodecConfigure(group, leAudioDevice);
+      if (!PrepareAndSendCodecConfigure(group, leAudioDevice)) {
+        StopStream(group);
+        return false;
+      };
     }
     return true;
   }
 
-  void PrepareAndSendCodecConfigure(LeAudioDeviceGroup* group, LeAudioDevice* leAudioDevice) {
+  bool PrepareAndSendCodecConfigure(LeAudioDeviceGroup* group, LeAudioDevice* leAudioDevice) {
     struct bluetooth::le_audio::client_parser::ascs::ctp_codec_conf conf;
     std::vector<struct bluetooth::le_audio::client_parser::ascs::ctp_codec_conf> confs;
     struct ase* ase;
@@ -1853,8 +1855,7 @@ private:
 
     if (!group->cig.AssignCisIds(leAudioDevice)) {
       log::error("unable to assign CIS IDs");
-      StopStream(group);
-      return;
+      return false;
     }
 
     if (group->cig.GetState() == CigState::CREATED) {
@@ -1896,6 +1897,7 @@ private:
 
     log_history_->AddLogHistory(kLogControlPointCmd, group->group_id_, leAudioDevice->address_,
                                 msg_stream.str(), extra_stream.str());
+    return true;
   }
 
   void AseStateMachineProcessCodecConfigured(
