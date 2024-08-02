dc39cbc842b4d4718a8516539ba88232fc56090d
William Escande <wescande@google.com>
2024-08-02 15:25:55 -0700

    Hap: Notify java with all preset at once
    Bug: 357144118
    Bug: 350514735
    Test: m Bluetooth
    Flag: com.android.bluetooth.flags.preset_notification_sent_as_one_group
    Change-Id: I6d6a4d9ae7a37f6b528a10e06e198d9305f33da7

--- system/bta/has/has_client.cc
+++ system/bta/has/has_client.cc
@@ -19,6 +19,7 @@
 #include <base/functional/callback.h>
 #include <base/strings/string_number_conversions.h>
 #include <bluetooth/log.h>
+#include <com_android_bluetooth_flags.h>
 #include <hardware/bt_gatt_types.h>
 #include <hardware/bt_has.h>
 
@@ -1154,6 +1155,38 @@ private:
 
   void OnHasPresetReadResponseNotification(HasDevice& device) {
     log::debug("");
+    if (com::android::bluetooth::flags::preset_notification_sent_as_one_group()) {
+      while (device.ctp_notifications_.size() != 0) {
+        auto ntf = device.ctp_notifications_.front();
+        /* Process only read response events */
+        if (ntf.opcode != PresetCtpOpcode::READ_PRESET_RESPONSE) {
+          break;
+        }
+
+        /* Update preset values */
+        if (ntf.preset.has_value()) {
+          device.has_presets.erase(ntf.preset->GetIndex());
+          device.has_presets.insert(ntf.preset.value());
+        }
+
+        /* Journal update */
+        device.has_journal_.Append(HasJournalRecord(ntf));
+        device.ctp_notifications_.pop_front();
+      }
+
+      MarkDeviceValidIfInInitialDiscovery(device);
+      callbacks_->OnPresetInfo(device.addr, PresetInfoReason::ALL_PRESET_INFO,
+                               device.GetAllPresetInfo());
+
+      /* If this was the last validation step then send the currently active
+       * preset as well.
+       */
+      if (device.isGattServiceValid()) {
+        callbacks_->OnActivePresetSelected(device.addr, device.currently_active_preset);
+      }
+
+      return;
+    }
 
     while (device.ctp_notifications_.size() != 0) {
       auto ntf = device.ctp_notifications_.front();
