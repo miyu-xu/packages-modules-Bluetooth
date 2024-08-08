cae17201caf18a173700d179212f99d738fafc60
zhenchao <quic_zhenchao@quicinc.com>
2024-08-08 16:28:24 +0800

    BASS: process source removed information properly
    While remove source command sent to remotes, and mPendingOperation
    is REMOVE_BCAST_SOURCE, the other msg(STOP_SCAN_OFFLOAD) is processed
    by StateMachine and overwrite mPendingOperation to STOP_SCAN_OFFLOAD.
    As a result, while source information is removed in remote and update empty receiverState to DUT, it can not handle it properly and not
    update onSourceRemoved callback to upper layer.
    
    Make fix to handle source removed information properly.
    
    CRs-Fixed: 3880357
    Change-Id: Ie3d6a896593bcc53cbdc86f8fe46dc1afbd08217

--- android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
@@ -168,6 +168,7 @@ class BassClientStateMachine extends StateMachine {
     @VisibleForTesting boolean mAutoTriggered = false;
     private boolean mDefNoPAS = false;
     private boolean mForceSB = false;
+    private boolean mRemoveSourceRequested = false;
     @VisibleForTesting BluetoothLeBroadcastMetadata mPendingSourceToAdd = null;
     private int mBroadcastSourceIdLength = 3;
     @VisibleForTesting byte mNextSourceId = 0;
@@ -352,6 +353,7 @@ class BassClientStateMachine extends StateMachine {
         mPendingOperation = -1;
         mPendingSourceId = -1;
         mPendingMetadata = null;
+        mRemoveSourceRequested = false;
         mPendingSourceToAdd = null;
         mPendingSourceToSwitch = null;
         mCurrentMetadata.clear();
@@ -1020,7 +1022,8 @@ class BassClientStateMachine extends StateMachine {
         if (receiverState.length == 0
                 || isEmpty(Arrays.copyOfRange(receiverState, 1, receiverState.length - 1))) {
             byte[] emptyBluetoothDeviceAddress = Utils.getBytesFromAddress("00:00:00:00:00:00");
-            if (mPendingOperation == REMOVE_BCAST_SOURCE) {
+            log("processBroadcastReceiverState: mRemoveSourceRequested " + mRemoveSourceRequested);
+            if (mRemoveSourceRequested) {
                 recvState =
                         new BluetoothLeBroadcastReceiveState(
                                 mPendingSourceId,
@@ -1038,6 +1041,7 @@ class BassClientStateMachine extends StateMachine {
                                 Arrays.asList(
                                         new BluetoothLeAudioContentMetadata[0]) // subgroupMetadata
                                 );
+                mRemoveSourceRequested = false;
             } else if (receiverState.length == 0) {
                 if (mBluetoothLeBroadcastReceiveStates != null) {
                     mNextSourceId = (byte) mBluetoothLeBroadcastReceiveStates.size();
@@ -1890,6 +1894,7 @@ class BassClientStateMachine extends StateMachine {
         mBassStateReady = false;
         mCurrentMetadata.clear();
         mPendingRemove.clear();
+        mRemoveSourceRequested = false;
     }
 
     @VisibleForTesting
@@ -2601,6 +2606,7 @@ class BassClientStateMachine extends StateMachine {
                         }
 
                         writeBassControlPoint(removeSourceInfo);
+                        mRemoveSourceRequested = true;
                         mPendingOperation = message.what;
                         mPendingSourceId = sid;
                         transitionTo(mConnectedProcessing);
@@ -2814,6 +2820,8 @@ class BassClientStateMachine extends StateMachine {
                     if ((message.arg1 == UPDATE_BCAST_SOURCE)
                             || (message.arg1 == ADD_BCAST_SOURCE)) {
                         mPendingMetadata = null;
+                    } else if (message.arg1 == REMOVE_BCAST_SOURCE) {
+                        mRemoveSourceRequested = false;
                     }
                     transitionTo(mConnected);
                     break;
