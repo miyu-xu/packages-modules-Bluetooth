b5ab779cf9c7d7de0ee1854f5e01c75011e710c2
Chris Manton <cmanton@google.com>
2024-08-10 09:56:00 -0700

    stack::rnr Introduce RNR interface
    Also prioritize RNR data structure
    
    Bug: 359912830
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: Ib571f4ac740d8c9944e201ec004001264e32877c

--- system/bta/test/bta_sec_test.cc
+++ system/bta/test/bta_sec_test.cc
@@ -22,11 +22,13 @@
 #include "bta/dm/bta_dm_sec_int.h"
 #include "bta/test/bta_test_fixtures.h"
 #include "stack/include/btm_status.h"
-#include "test/mock/mock_stack_btm_inq.h"
 #include "test/mock/mock_stack_btm_interface.h"
+#include "test/mock/mock_stack_rnr_interface.h"
 #include "types/raw_address.h"
 
+using ::testing::_;
 using ::testing::ElementsAre;
+using ::testing::Return;
 
 namespace {
 const RawAddress kRawAddress({0x11, 0x22, 0x33, 0x44, 0x55, 0x66});
@@ -50,18 +52,27 @@ tBTM_STATUS bta_dm_sp_cback(tBTM_SP_EVT event, tBTM_SP_EVT_DATA* p_data);
 
 class BtaSecTest : public BtaWithHwOnTest {
 protected:
-  void SetUp() override { BtaWithHwOnTest::SetUp(); }
+  void SetUp() override {
+    BtaWithHwOnTest::SetUp();
+    bluetooth::testing::set_stack_rnr_interface(&mock_stack_rnr_interface_);
+  }
 
-  void TearDown() override { BtaWithHwOnTest::TearDown(); }
+  void TearDown() override {
+    bluetooth::testing::reset_rnr_interface();
+    BtaWithHwOnTest::TearDown();
+  }
+
+  bluetooth::stack::rnr::Mock mock_stack_rnr_interface_;
 };
 
 TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithName) {
   constexpr uint32_t kNumVal = 1234;
   static bool callback_sent = false;
+#if 0
   mock_btm_client_interface.peer.BTM_ReadRemoteDeviceName =
           [](const RawAddress& remote_bda, tBTM_NAME_CMPL_CB* p_cb,
              tBT_TRANSPORT transport) -> tBTM_STATUS { return tBTM_STATUS::BTM_CMD_STARTED; };
-
+#endif
   static tBTA_DM_SP_CFM_REQ cfm_req{};
   bta_dm_sec_enable([](tBTA_DM_SEC_EVT event, tBTA_DM_SEC* p_data) {
     callback_sent = true;
@@ -75,7 +86,7 @@ TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithName) {
                           .bd_addr = kRawAddress,
                           .dev_class = {},
                           .bd_name = {},
-                          .num_val = kNumVal,
+                          ck -.num_val = kNumVal,
                           .just_works = false,
                           .loc_auth_req = BTM_AUTH_SP_YES,
                           .rmt_auth_req = BTM_AUTH_SP_YES,
@@ -107,9 +118,9 @@ TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithoutName_RNRSuccess) {
   constexpr uint32_t kNumVal = 1234;
   static bool callback_sent = false;
   reset_mock_btm_client_interface();
-  mock_btm_client_interface.peer.BTM_ReadRemoteDeviceName =
-          [](const RawAddress& remote_bda, tBTM_NAME_CMPL_CB* p_cb,
-             tBT_TRANSPORT transport) -> tBTM_STATUS { return tBTM_STATUS::BTM_CMD_STARTED; };
+
+  ON_CALL(mock_stack_rnr_interface_, BTM_ReadRemoteDeviceName(_, _, _))
+          .WillByDefault(Return(tBTM_STATUS::BTM_CMD_STARTED));
 
   static tBTA_DM_SP_CFM_REQ cfm_req{};
   bta_dm_sec_enable([](tBTA_DM_SEC_EVT event, tBTA_DM_SEC* p_data) {
@@ -144,9 +155,9 @@ TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithoutName_RNRSuccess) {
 TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithoutName_RNRFail) {
   constexpr uint32_t kNumVal = 1234;
   static bool callback_sent = false;
-  mock_btm_client_interface.peer.BTM_ReadRemoteDeviceName =
-          [](const RawAddress& remote_bda, tBTM_NAME_CMPL_CB* p_cb,
-             tBT_TRANSPORT transport) -> tBTM_STATUS { return tBTM_STATUS::BTM_SUCCESS; };
+
+  ON_CALL(mock_stack_rnr_interface_, BTM_ReadRemoteDeviceName(_, _, _))
+          .WillByDefault(Return(tBTM_STATUS::BTM_SUCCESS));
 
   static tBTA_DM_SP_CFM_REQ cfm_req{};
   bta_dm_sec_enable([](tBTA_DM_SEC_EVT event, tBTA_DM_SEC* p_data) {
@@ -190,9 +201,9 @@ TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_CFM_REQ_EVT_WithoutName_RNRFail) {
 TEST_F(BtaSecTest, bta_dm_sp_cback__BTM_SP_KEY_NOTIF_EVT) {
   constexpr uint32_t kPassKey = 1234;
   static bool callback_sent = false;
-  mock_btm_client_interface.peer.BTM_ReadRemoteDeviceName =
-          [](const RawAddress& remote_bda, tBTM_NAME_CMPL_CB* p_cb,
-             tBT_TRANSPORT transport) -> tBTM_STATUS { return tBTM_STATUS::BTM_CMD_STARTED; };
+
+  ON_CALL(mock_stack_rnr_interface_, BTM_ReadRemoteDeviceName(_, _, _))
+          .WillByDefault(Return(tBTM_STATUS::BTM_CMD_STARTED));
 
   static tBTA_DM_SP_KEY_NOTIF key_notif{};
   bta_dm_sec_enable([](tBTA_DM_SEC_EVT event, tBTA_DM_SEC* p_data) {
