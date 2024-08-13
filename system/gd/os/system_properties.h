e68adeeec27d23a1dba6a127ce814ab611b2a231
Jeremy Wu <jrwu@google.com>
2024-08-13 06:58:34 +0000

    floss: check if BCC is allowed to open SCO
    In HFPv1.9, 4.11.2, Audio Connection Setup by HF:
    
    > When the HF triggers the establishment of the Codec Connection
    it shall send the AT command AT+BCC to the AG. The AG shall respond
    with OK if it will start the Codec Connection procedure, and with ERROR
    if it cannot start the Codec Connection procedure.
    
    In CrOS, SCO initiated from the peer is not a supported use case. In
    this CL we introduce a check to reject the BCC open request.
    
    Bug: 353234804
    Flag: EXEMPT floss-only
    Test: m Bluetooth
    Change-Id: I339c68c537d842fcc6b71ce17e7a35389abbc043

--- system/gd/os/system_properties.h
+++ system/gd/os/system_properties.h
@@ -29,6 +29,9 @@ namespace os {
 static const std::string kIsDualModeAudioEnabledProperty =
         "persist.bluetooth.enable_dual_mode_audio";
 
+/* System Property to indicate if BCC event should not open SCO. */
+static const std::string kIsBccOpenScoDisallowed = "persist.bluetooth.is_bcc_open_sco_disallowed";
+
 // Get |property| keyed system property from supported platform, return std::nullopt if the property
 // does not exist or if the platform does not support system property
 std::optional<std::string> GetSystemProperty(const std::string& property);
