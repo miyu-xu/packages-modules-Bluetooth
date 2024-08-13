7a0cd39b2768d19b856205b82bbe0beb2ef9a5d6
Pragati <quic_pragati@quicinc.com>
2024-08-13 09:46:58 +0530

    Add L2cap APIs to support PTS test cases
    Added below APIs to support PTS test cases
    
     - GetPeerChannelId
    
     - FlowControl
    
     - Ping send peer echo req
    
     bug: 407908182
    
    Test: m Bluetooth
    Flag: EXEMPT, trivial
    
    Change-Id: I524cd9e154198169427e395b47ee258cbc9776c0

--- system/stack/l2cap/l2c_utils.cc
+++ system/stack/l2cap/l2c_utils.cc
@@ -222,6 +222,16 @@ void l2cu_release_lcb(tL2C_LCB* p_lcb) {
 
   p_lcb->suspended.clear();
 
+  /* Check for ping outstanding */
+  if (p_lcb->p_echo_rsp_cb) {
+    tL2CA_ECHO_RSP_CB* p_cb = p_lcb->p_echo_rsp_cb;
+
+    // Clear the callback before invoking to avoid re-entrancy issues
+    p_lcb->p_echo_rsp_cb = NULL;
+
+    (*p_cb)(L2CAP_PING_RESULT_NO_LINK);
+  }
+
   /* Check and release all the LE COC connections waiting for security */
   if (p_lcb->le_sec_pending_q) {
     while (!fixed_queue_is_empty(p_lcb->le_sec_pending_q)) {
@@ -345,7 +355,7 @@ static BT_HDR* l2cu_build_header(tL2C_LCB* p_lcb, uint16_t len, uint8_t cmd, uin
  * Returns          void
  *
  ******************************************************************************/
-static void l2cu_adj_id(tL2C_LCB* p_lcb) {
+void l2cu_adj_id(tL2C_LCB* p_lcb) {
   if (p_lcb->signal_id == 0) {
     p_lcb->signal_id++;
   }
@@ -394,6 +404,42 @@ void l2cu_send_peer_cmd_reject(tL2C_LCB* p_lcb, uint16_t reason, uint8_t rem_id,
 
   if (param_len >= 4) {
     UINT16_TO_STREAM(p, p2);
+  }
+
+  l2c_link_check_send_pkts(p_lcb, 0, p_buf);
+}
+
+/*******************************************************************************
+ *
+ * Function         l2cu_send_peer_echo_req
+ *
+ * Description      Build and send an L2CAP "echo request" message
+ *                  to the peer. Note that we do not currently allow
+ *                  data in the echo request.
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+void l2cu_send_peer_echo_req(tL2C_LCB* p_lcb, uint8_t* p_data,
+                             uint16_t data_len) {
+  BT_HDR* p_buf;
+  uint8_t* p;
+
+  p_lcb->signal_id++;
+  l2cu_adj_id(p_lcb); /* check for wrap to '0' */
+
+  p_buf = l2cu_build_header(p_lcb, (uint16_t)(L2CAP_ECHO_REQ_LEN + data_len),
+                            L2CAP_CMD_ECHO_REQ, p_lcb->signal_id);
+  if (p_buf == NULL) {
+    log::debug("L2CAP - no buffer for echo_req");
+    return;
+  }
+
+  p = (uint8_t*)(p_buf + 1) + L2CAP_SEND_CMD_OFFSET + HCI_DATA_PREAMBLE_SIZE +
+      L2CAP_PKT_OVERHEAD + L2CAP_CMD_OVERHEAD;
+
+  if (data_len) {
+    ARRAY_TO_STREAM(p, p_data, data_len);
   }
 
   l2c_link_check_send_pkts(p_lcb, 0, p_buf);
