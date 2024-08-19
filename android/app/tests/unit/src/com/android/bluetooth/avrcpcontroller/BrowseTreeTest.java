6d3685721c0919f84bbab0a1996d75619e66e7fa
Sal Savage <salsavage@google.com>
2024-08-19 14:14:30 -0700

    Replace flag rule with flag annotations
    Flag rule based calls happen exactly when they're made, and allow for
    setup code to be ran without the properly set flag values. This is
    errorprone and can cause subtle bugs where the flag values don't appear
    to be set properly.
    
    Instead, we should use the new annotations, which are executed _before_
    setup is called.
    
    Bug: 352982293
    Flag: EXEMPT, test only change
    Test: atest BrowseTreeTest.java
    Test: atest AvrcpControllerServiceTest.java
    Test: atest AvrcpControllerStateMachineTest.java
    Change-Id: Iff1c55f96b51269bbc74492d464135e7cc579744

--- android/app/tests/unit/src/com/android/bluetooth/avrcpcontroller/BrowseTreeTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/avrcpcontroller/BrowseTreeTest.java
@@ -20,13 +20,13 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.platform.test.flag.junit.SetFlagsRule;
+import android.platform.test.annotations.DisableFlags;
+import android.platform.test.annotations.EnableFlags;
 
 import com.android.bluetooth.avrcpcontroller.BrowseTree.BrowseNode;
 import com.android.bluetooth.flags.Flags;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.Set;
@@ -35,8 +35,6 @@ public class BrowseTreeTest {
     private static final String ILLEGAL_ID = "illegal_id";
     private static final String TEST_HANDLE = "test_handle";
     private static final String TEST_NODE_ID = "test_node_id";
-
-    @Rule public final SetFlagsRule mSetFlagsRule = new SetFlagsRule();
 
     private final byte[] mTestAddress = new byte[] {01, 01, 01, 01, 01, 01};
     private BluetoothAdapter mAdapter;
@@ -100,8 +98,8 @@ public class BrowseTreeTest {
     }
 
     @Test
+    @EnableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS)
     public void sameDeviceDifferentBrowseTrees_uniqueMediaIds() {
-        mSetFlagsRule.enableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS);
         BrowseTree browseTree1 = new BrowseTree(mTestDevice);
         BrowseTree browseTree2 = new BrowseTree(mTestDevice);
 
@@ -118,16 +116,16 @@ public class BrowseTreeTest {
     }
 
     @Test
+    @DisableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS)
     public void findBrowseNodeByIDForDevice() {
-        mSetFlagsRule.disableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS);
         BrowseTree browseTree = new BrowseTree(mTestDevice);
         final String deviceId = BrowseTree.ROOT + mTestDevice.getAddress().toString();
         assertThat(browseTree.findBrowseNodeByID(deviceId)).isEqualTo(browseTree.mRootNode);
     }
 
     @Test
+    @EnableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS)
     public void findBrowseNodeByIDForDevice_flagEnabled() {
-        mSetFlagsRule.enableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS);
         BrowseTree browseTree = new BrowseTree(mTestDevice);
         final String deviceId = browseTree.mRootNode.getID();
         assertThat(browseTree.findBrowseNodeByID(deviceId)).isEqualTo(browseTree.mRootNode);
@@ -149,8 +147,8 @@ public class BrowseTreeTest {
     }
 
     @Test
+    @EnableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS)
     public void findBrowseNodeByIDForDevice_withRandomDeviceID_nodeIsFound() {
-        mSetFlagsRule.enableFlags(Flags.FLAG_RANDOMIZE_DEVICE_LEVEL_MEDIA_IDS);
         BrowseTree browseTree = new BrowseTree(mTestDevice);
         final String deviceId = browseTree.mRootNode.getID();
         assertThat(browseTree.findBrowseNodeByID(deviceId)).isEqualTo(browseTree.mRootNode);
