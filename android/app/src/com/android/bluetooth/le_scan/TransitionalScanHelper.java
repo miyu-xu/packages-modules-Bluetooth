d6d5a216d0bdc358f791be1567d4621a8e599a66
Kihong Seong <kihongs@google.com>
2024-08-20 05:31:34 +0000

    Add log when callback is null during scan result callback
    Currently even when scan result is delivered to the Java layer, it might not reach the app if callback is null. Adding a log to inform in that case.
    
    Bug: 360991150
    Test: m com.android.btservices
    Flag: EXEMPT, changes to logs only
    Change-Id: Idb511e9fcfc533d212665da321159af256e9cf54

--- android/app/src/com/android/bluetooth/le_scan/TransitionalScanHelper.java
+++ android/app/src/com/android/bluetooth/le_scan/TransitionalScanHelper.java
@@ -451,6 +451,7 @@ public class TransitionalScanHelper {
                 if (app.mCallback != null) {
                     app.mCallback.onScanResult(result);
                 } else {
+                    Log.v(TAG, "Callback is null, sending scan result to pendingIntent");
                     // Send the PendingIntent
                     ArrayList<ScanResult> results = new ArrayList<>();
                     results.add(result);
