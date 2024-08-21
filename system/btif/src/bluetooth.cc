7e9ed03329285ba577d16b3908a7cd9628bdd211
Abdelrahman Daim <adaim@meta.com>
2024-08-21 05:52:12 -0700

    Print ble rpa to BT Dumpsys
    Summary: Currently the BLE address is rotated for privacy reasons.
    For debugging and automation purposes:
    this address is required so we can validate the RSSI is good;
    do some connection tests from cmd line;
    Since the BLE address is not exposed via any existing
    dumpsys or filesystem interfaces, it's hard to get the BLE address.
    
    Bug: 361335194
    
    Test: adb dumpsys shows current ble address
    after changing rotation frequency I can see adress rotations as expected
    Successful build on master branch
    
    Change-Id: Ibdb6d5a4ab4488da4dbf4a6c0d9919d81dc308b1
    Signed-off-by: Abdelrahman Daim <adaim@meta.com>

--- system/btif/src/bluetooth.cc
+++ system/btif/src/bluetooth.cc
@@ -97,6 +97,7 @@
 #include "osi/include/stack_power_telemetry.h"
 #include "osi/include/wakelock.h"
 #include "stack/btm/btm_dev.h"
+#include "stack/btm/btm_ble_int.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
 #include "stack/gatt/connection_manager.h"
 #include "stack/include/a2dp_api.h"
@@ -892,6 +893,7 @@ static void dump(int fd, const char** arguments) {
   VolumeControl::DebugDump(fd);
   connection_manager::dump(fd);
   bluetooth::bqr::DebugDump(fd);
+  btm_ble_dump_random_address(fd);
   PAN_Dumpsys(fd);
   DumpsysHid(fd);
   DumpsysBtaDm(fd);
