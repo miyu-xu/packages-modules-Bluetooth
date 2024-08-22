364ea7383c727c2c4aae796773f41150c46e5318
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-08-22 18:32:29 +0000

    Implement APCF/MSFT detection in ScanManager
    Bug: 360387731
    Test: m
    Change-Id: I7ef0770537d126c1ec1012353317ba968d6c8a1b

--- android/app/src/com/android/bluetooth/le_scan/ScanManager.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanManager.java
@@ -1545,7 +1545,7 @@ public class ScanManager {
             mNativeInterface.gattClientScanFilterEnable(scannerId, true);
             waitForCallback();
 
-            if (shouldUseAllPassFilter(client)) {
+            if (shouldUseAllPassFilter(client) && !mNativeInterface.gattClientIsMsftSupported()) {
                 int filterIndex =
                         (deliveryMode == DELIVERY_MODE_BATCH)
                                 ? ALL_PASS_FILTER_INDEX_BATCH_SCAN
@@ -1555,6 +1555,8 @@ public class ScanManager {
                 configureFilterParameter(
                         scannerId, client, ALL_PASS_FILTER_SELECTION, filterIndex, 0);
                 waitForCallback();
+            } else if (mNativeInterface.gattClientIsMsftSupported()) {
+
             } else {
                 Deque<Integer> clientFilterIndices = new ArrayDeque<Integer>();
                 for (ScanFilter filter : client.filters) {
