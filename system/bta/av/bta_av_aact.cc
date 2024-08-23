358c5df5bf1ee9a5756a49327c5dbba453c72dd3
Yun-Hao Chung <howardchung@google.com>
2024-08-23 08:13:03 +0000

    Fix AVDTP reconfig not setting L2CAP flushable
    We set the L2CAP for AVDTP to flushable when opening AVDTP at the first
    time, but if the reconfiguration happens, the AVDTP could be run on a
    different L2CAP channel which needs to set flushable again.
    
    Bug: 361714491
    Test: mma -j
    Flag: com::android::bluetooth::flags::fix_avdt_rconfig_not_setting_l2cap
    Change-Id: If4e791045a3f9f3010e8402137170dec589e0582

--- system/bta/av/bta_av_aact.cc
+++ system/bta/av/bta_av_aact.cc
@@ -2584,6 +2584,19 @@ void bta_av_rcfg_str_ok(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
     p_scb->p_cos->update_mtu(p_scb->hndl, p_scb->PeerAddress(), p_scb->stream_mtu);
   }
 
+  if (com::android::bluetooth::flags::fix_avdt_rconfig_not_setting_l2cap()) {
+    /* Set the media channel as high priority */
+    if (!L2CA_SetTxPriority(p_scb->l2c_cid, L2CAP_CHNL_PRIORITY_HIGH)) {
+      log::warn("Unable to set L2CAP flush peer:{} cid:{}", p_scb->PeerAddress(), p_scb->l2c_cid);
+    }
+
+    if (!L2CA_SetChnlFlushability(p_scb->l2c_cid, true)) {
+      log::warn("nable to set L2CAP flush peer:{} cid:{}", p_scb->PeerAddress(), p_scb->l2c_cid);
+    }
+
+    L2CA_SetMediaStreamChannel(p_scb->l2c_cid, true);
+  }
+
   /* rc listen */
   bta_av_st_rc_timer(p_scb, NULL);
 
