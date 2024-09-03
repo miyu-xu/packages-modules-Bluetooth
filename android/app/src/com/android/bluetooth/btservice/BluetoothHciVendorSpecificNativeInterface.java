2b1c509208bdc2b244eb97902834c589466275c2
Antoine SOULIER <asoulier@google.com>
2024-09-03 23:11:11 +0000

    JAVA Service implementation for HCI Vendor Spcific Handling
    Bug: 360924438
    Test: mma -j32
    Flag: com.android.bluetooth.flags.hci_vendor_specific_extension
    Change-Id: I19ee6dd1bedd467c7d6f364fa1cedc139ea829d3

---
+++ android/app/src/com/android/bluetooth/btservice/BluetoothHciVendorSpecificNativeInterface.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.btservice;
+
+import android.util.Log;
+
+class BluetoothHciVendorSpecificNativeInterface {
+    private static final String TAG = "BluetoothHciVendorSpecificNativeInterface";
+
+    void init() {
+        initNative();
+    }
+
+    void cleanup() {
+        cleanupNative();
+    }
+
+    void sendCommand(int ocf, byte[] parameters, byte[] cookie) {
+        sendCommandNative(ocf, parameters, cookie);
+    }
+
+    // Callbacks from the native stack
+
+    private void commandStatusDeliver(int ocf, int status, byte[] cookie) {
+        AdapterService adapterService = AdapterService.getAdapterService();
+        if (adapterService == null) {
+            Log.e(TAG, "commandStatusDeliver failed: adapterService is null");
+            return;
+        }
+        adapterService.bluetoothHciVendorSpecificCommandStatus(ocf, status, cookie);
+    }
+
+    private void commandCompleteDeliver(int ocf, byte[] returnParameters, byte[] cookie) {
+        AdapterService adapterService = AdapterService.getAdapterService();
+        if (adapterService == null) {
+            Log.e(TAG, "commandCompleteDeliver failed: adapterService is null");
+            return;
+        }
+        adapterService.bluetoothHciVendorSpecificCommandComplete(ocf, returnParameters, cookie);
+    }
+
+    private void eventDeliver(int code, byte[] data) {
+        AdapterService adapterService = AdapterService.getAdapterService();
+        if (adapterService == null) {
+            Log.e(TAG, "eventDeliver failed: adapterService is null");
+            return;
+        }
+        adapterService.bluetoothHciVendorSpecificEventCallback(code, data);
+    }
+
+    // Native methods that call into the JNI interface
+
+    private native void initNative();
+
+    private native void cleanupNative();
+
+    private native void sendCommandNative(int ocf, byte[] parameters, byte[] cookie);
+}
