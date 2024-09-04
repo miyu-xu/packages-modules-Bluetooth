afcae8a7d45f7c3ae9699b31d674fb4ffd679b4e
Jakub Pawlowski <jpawlowski@google.com>
2024-09-04 15:02:24 +0200

    encryption_change
    encryption_change
    
    Test: mma -j32;
    Bug:
    Flag: EXEMPT, just wiring event through the stack
    Change-Id: I02beb43c7dfd09e101122e072c5096385b0ac1da

--- android/app/src/com/android/bluetooth/btservice/JniCallbacks.java
+++ android/app/src/com/android/bluetooth/btservice/JniCallbacks.java
@@ -91,6 +91,16 @@ class JniCallbacks {
         mRemoteDevices.keyMissingCallback(address);
     }
 
+    void encryptionChangeCallback(
+            byte[] address,
+            int status,
+            boolean encryptionEnable,
+            int transport,
+            boolean secureConnection) {
+        mRemoteDevices.encryptionChangeCallback(
+                address, status, encryptionEnable, transport, secureConnection);
+    }
+
     void stateChangeCallback(int status) {
         mAdapterService.stateChangeCallback(status);
     }
