27e97966a6ea175903ae7966a82d72c3bd79c553
Kyunglyul Hyun <klhyun@google.com>
2024-09-04 16:37:23 +0000

    Limit the number of gatt client per app
    limit the number of GATT clients per app uid
    
    Bug: 366473955
    Test: atest GattServiceTest
    
    Change-Id: I9c600a867d0c2aee4f92d31fdbf3f8a0775a83c1

--- android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceTest.java
@@ -367,6 +367,17 @@ public class GattServiceTest {
     }
 
     @Test
+    public void registerClient_checkLimitPerApp() {
+        doReturn(GattService.LIMIT_GATT_CLIENT_PER_APP).when(mClientMap).countByAppUid(anyInt());
+        UUID uuid = UUID.randomUUID();
+        IBluetoothGattCallback callback = mock(IBluetoothGattCallback.class);
+
+        mService.registerClient(uuid, callback, /* eattSupport= */ true, mAttributionSource);
+        verify(mClientMap, never()).add(any(), any(), any());
+        verify(mNativeInterface, never()).gattClientRegisterApp(anyLong(), anyLong(), anyBoolean());
+    }
+
+    @Test
     public void unregisterClient() {
         int clientIf = 3;
 
