03abc290f413c9f1bef49dab67e2c4254d4d39ef
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-09-04 00:15:15 +0000

    Implement a APCF->MSFT converter utility
    Change-Id: I3d0ee366c42f5aa5d50a1ca36ca6142b402d8fbb

--- android/app/src/com/android/bluetooth/le_scan/MsftAdvMonitor.java
+++ android/app/src/com/android/bluetooth/le_scan/MsftAdvMonitor.java
@@ -16,8 +16,20 @@
 
 package com.android.bluetooth.le_scan;
 
+import android.bluetooth.le.ScanFilter;
+
+import java.util.ArrayList;
+
 /** Helper class used to manage MSFT Advertisement Monitors. */
 /* package */ class MsftAdvMonitor {
+    //private static final int MSFT_CONDITION_TYPE_ALL = 0x00;
+    private static final int MSFT_CONDITION_TYPE_PATTERNS = 0x01;
+    //private static final int MSFT_CONDITION_TYPE_UUID = 0x02;
+    //private static final int MSFT_CONDITION_TYPE_IRK = 0x03;
+    //private static final int MSFT_CONDITION_TYPE_ADDRESS = 0x04;
+
+    private static final int FILTER_PATTERN_START_POSITION = 0;
+
     static class Monitor {
         public byte rssi_threshold_high;
         public byte rssi_threshold_low;
@@ -35,5 +47,47 @@ package com.android.bluetooth.le_scan;
     static class Address {
         byte addr_type;
         String bd_addr;
+    }
+
+    private Monitor mMonitor;
+    private ArrayList<Pattern> mPatterns;
+    private Address mAddress;
+
+    // Constructor that converts an APCF-friendly filter to an MSFT-friendly format
+    public MsftAdvMonitor(ScanFilter filter) {
+        mMonitor = new Monitor();
+        mPatterns = new ArrayList<>();
+        mAddress = new Address();
+
+        mMonitor.rssi_threshold_high = Byte.MIN_VALUE;
+        mMonitor.rssi_threshold_low = Byte.MIN_VALUE;
+        mMonitor.rssi_threshold_low_time_interval = 1; // hard coded to 1 s
+        mMonitor.rssi_sampling_period = 5; // hard coded to 500 ms
+        mMonitor.condition_type = MSFT_CONDITION_TYPE_PATTERNS;
+
+        if (filter.getServiceData() != null && filter.getServiceData().length != 0) {
+          Pattern pattern = new Pattern();
+          pattern.ad_type = (byte) filter.getAdvertisingDataType();
+          pattern.start_byte = FILTER_PATTERN_START_POSITION;
+          pattern.pattern = filter.getServiceData();    // TODO ensure is valid
+          mPatterns.add(pattern);
+        }
+
+        if (filter.getDeviceAddress() != null) {
+          mAddress.addr_type = (byte) filter.getAddressType();
+          mAddress.bd_addr = filter.getDeviceAddress();
+        }
+    }
+
+    public Monitor getMonitor() {
+      return mMonitor;
+    }
+
+    public Pattern[] getPatterns() {
+      return mPatterns.toArray(new Pattern[mPatterns.size()]);
+    }
+
+    public Address getAddress() {
+      return mAddress;
     }
 }
