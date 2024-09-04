9b1a6294f8400da911c14cd8940581b337d8623b
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-09-04 00:15:15 +0000

    Implement a APCF->MSFT converter utility
    Change-Id: I3d0ee366c42f5aa5d50a1ca36ca6142b402d8fbb

--- android/app/src/com/android/bluetooth/le_scan/MsftAdvMonitor.java
+++ android/app/src/com/android/bluetooth/le_scan/MsftAdvMonitor.java
@@ -16,24 +16,74 @@
 
 package com.android.bluetooth.le_scan;
 
+import android.bluetooth.le.ScanFilter;
+
+import java.util.ArrayList;
+
 /** Helper class used to manage MSFT Advertisement Monitors. */
 /* package */ class MsftAdvMonitor {
+    //private static final int MSFT_CONDITION_TYPE_ALL = 0x00;
+    private static final int MSFT_CONDITION_TYPE_PATTERNS = 0x01;
+    //private static final int MSFT_CONDITION_TYPE_UUID = 0x02;
+    //private static final int MSFT_CONDITION_TYPE_IRK = 0x03;
+    //private static final int MSFT_CONDITION_TYPE_ADDRESS = 0x04;
+
+    private static final int FILTER_PATTERN_START_POSITION = 0;
+
     static class Monitor {
-        public byte rssi_threshold_high;
-        public byte rssi_threshold_low;
-        public byte rssi_threshold_low_time_interval;
-        public byte rssi_sampling_period;
-        public byte condition_type;
+        public int rssi_threshold_high;
+        public int rssi_threshold_low;
+        public int rssi_threshold_low_time_interval;
+        public int rssi_sampling_period;
+        public int condition_type;
     }
 
     static class Pattern {
-        public byte ad_type;
-        public byte start_byte;
+        public int ad_type;
+        public int start_byte;
         public byte[] pattern;
     }
 
     static class Address {
-        byte addr_type;
+        int addr_type;
         String bd_addr;
+    }
+
+    private Monitor mMonitor;
+    private ArrayList<Pattern> mPatterns;
+    private Address mAddress;
+
+    // Constructor that converts an APCF-friendly filter to an MSFT-friendly format
+    public MsftAdvMonitor(ScanFilter filter, int scanIntervalMs, int scanWindowMs) {
+        mMonitor = new Monitor();
+        mPatterns = new ArrayList<>();
+        mAddress = new Address();
+
+        mMonitor.rssi_threshold_high = Byte.MIN_VALUE;
+        mMonitor.rssi_threshold_low = Byte.MIN_VALUE;
+        mMonitor.rssi_threshold_low_time_interval = scanIntervalMs * 1000;
+        mMonitor.rssi_sampling_period = scanWindowMs * 1000;
+        mMonitor.condition_type = MSFT_CONDITION_TYPE_PATTERNS;
+
+        Pattern pattern = new Pattern();
+        pattern.ad_type = filter.getAdvertisingDataType();
+        pattern.start_byte = FILTER_PATTERN_START_POSITION;
+        pattern.pattern = filter.getServiceData();
+        mPatterns.add(pattern);
+
+        mAddress.addr_type = filter.getAddressType();
+        mAddress.bd_addr = filter.getDeviceAddress();
+    }
+
+    public Monitor getMonitor() {
+      return mMonitor;
+    }
+
+    public Pattern[] getPatterns() {
+      return mPatterns.toArray(new Pattern[mPatterns.size()]);
+    }
+
+    public Address getAddress() {
+      return mAddress;
     }
 }
