dd2ff4baa03f68b85d2e3d61e8443c9e74336ca9
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-09-04 08:30:05 +0000

    Revert "[MTK][CONVERGENCE][ALPS07830065] Use 1M to response"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/e7b96737-7523-459c-81d7-e608155be5c3). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I1a898a7f9705638aaa9dbc88d1fc5744a7bee381
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- system/stack/btm/btm_ble.cc
+++ system/stack/btm/btm_ble.cc
@@ -167,11 +167,11 @@ void BTM_BleReadPhy(const RawAddress& bd_addr,
     return;
   }
 
-  // The connection PHY is always LE_1M when the controller supports
-  // neither LE_2M nor LE_CODED PHYs.
+  // checking if local controller supports it!
   if (!bluetooth::shim::GetController()->SupportsBle2mPhy() &&
       !bluetooth::shim::GetController()->SupportsBleCodedPhy()) {
-    cb.Run(1, 1, HCI_SUCCESS);
+    log::error("request not supported in local controller!");
+    cb.Run(0, 0, GATT_REQ_NOT_SUPPORTED);
     return;
   }
 
