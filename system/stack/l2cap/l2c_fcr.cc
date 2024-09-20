b509e78a2040a426e110ed73a6d2d47213123904
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-09-20 23:09:28 +0000

    Fix L2cap buffer length check against MPS for the first k-frame
    Test: mmm packages/module/Bluetooth
    bug: 323923764
    flag: Exempt, minor bug
    Change-Id: I4546016ee142aacf16848914688bff1a4cc31351

--- system/stack/l2cap/l2c_fcr.cc
+++ system/stack/l2cap/l2c_fcr.cc
@@ -671,10 +671,17 @@ void l2c_lcc_proc_pdu(tL2C_CCB* p_ccb, BT_HDR* p_buf) {
   uint16_t sdu_length;
   BT_HDR* p_data = NULL;
 
+  uint16_t local_mps = p_ccb->local_conn_cfg.mps;
+  if (p_ccb->is_first_seg) {
+     //for the first k-frame, donot consider sdu_length
+     //as part of the information payload
+     local_mps = p_ccb->local_conn_cfg.mps + sizeof(sdu_length);
+  }
+
   /* Buffer length should not exceed local mps */
-  if (p_buf->len > p_ccb->local_conn_cfg.mps) {
+  if (p_buf->len > local_mps) {
     log::error("buffer length={} exceeds local mps={}. Drop and disconnect.", p_buf->len,
-               p_ccb->local_conn_cfg.mps);
+               local_mps);
 
     /* Discard the buffer and disconnect*/
     osi_free(p_buf);
