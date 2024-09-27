c2934514cc3c676d9c0767ac858290a0a37df3c8
Pomai Ahlo <poahlo@google.com>
2024-09-27 15:27:06 -0700

    Save app_uid
    Refactor code to pass app_uid down to sm_cb
    
    Bug: 361844445
    Test: atest RfcommTest
    Flag: EXEMPT no-op for metrics
    Change-Id: I12b1545fb9d928b320c86a7b121e034531909003

--- system/bta/include/bta_jv_api.h
+++ system/bta/include/bta_jv_api.h
@@ -672,7 +672,7 @@ tBTA_JV_STATUS BTA_JvL2capWrite(uint32_t handle, uint32_t req_id, BT_HDR* msg, u
  ******************************************************************************/
 tBTA_JV_STATUS BTA_JvRfcommConnect(tBTA_SEC sec_mask, uint8_t remote_scn,
                                    const RawAddress& peer_bd_addr, tBTA_JV_RFCOMM_CBACK* p_cback,
-                                   uint32_t rfcomm_slot_id);
+                                   uint32_t rfcomm_slot_id, uint32_t app_uid);
 
 /*******************************************************************************
  *
@@ -702,7 +702,8 @@ tBTA_JV_STATUS BTA_JvRfcommClose(uint32_t handle, uint32_t rfcomm_slot_id);
  *
  ******************************************************************************/
 tBTA_JV_STATUS BTA_JvRfcommStartServer(tBTA_SEC sec_mask, uint8_t local_scn, uint8_t max_session,
-                                       tBTA_JV_RFCOMM_CBACK* p_cback, uint32_t rfcomm_slot_id);
+                                       tBTA_JV_RFCOMM_CBACK* p_cback, uint32_t rfcomm_slot_id,
+                                       uint32_t app_uid);
 
 /*******************************************************************************
  *
