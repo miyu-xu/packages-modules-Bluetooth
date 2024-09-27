e9dd8cc92ab71be66eb72c472a19ffb773b4d5cf
Yuyang Huang <yuyangh@google.com>
2024-09-27 15:40:36 -0700

    match CancelStreamingRequest with ConfirmStreamingRequest about ack on |*StreamingRequest|
    The audio HAL expects a response from the BT stack on start/suspend.
    
    Bug: 367479598
    Bug: 315234036
    Flag: com::android::bluetooth::flags::is_sco_managed_by_audio
    Test: atest HeadsetStateMachineTest
    Change-Id: Ibf51abdbd3c5489b37164a4f548ad30e0e480020

--- system/audio_hal_interface/hfp_client_interface.cc
+++ system/audio_hal_interface/hfp_client_interface.cc
@@ -529,13 +529,13 @@ void HfpClientInterface::Offload::ConfirmStreamingRequest() {
   auto instance = aidl::hfp::HfpEncodingTransport::instance_;
   auto pending_cmd = instance->GetPendingCmd();
   switch (pending_cmd) {
+    case aidl::hfp::HFP_CTRL_CMD_NONE:
+      log::warn("no pending start stream request");
+      [[fallthrough]];
     case aidl::hfp::HFP_CTRL_CMD_START:
       aidl::hfp::HfpEncodingTransport::offloading_hal_interface->StreamStarted(
               aidl::BluetoothAudioCtrlAck::SUCCESS_FINISHED);
       instance->ResetPendingCmd();
-      return;
-    case aidl::hfp::HFP_CTRL_CMD_NONE:
-      log::warn("no pending start stream request");
       return;
     default:
       log::warn("Invalid state, {}", pending_cmd);
@@ -552,7 +552,7 @@ void HfpClientInterface::Offload::CancelStreamingRequest() {
       instance->ResetPendingCmd();
       return;
     case aidl::hfp::HFP_CTRL_CMD_NONE:
-      log::info("no pending start stream request");
+      log::warn("no pending start stream request");
       [[fallthrough]];
     case aidl::hfp::HFP_CTRL_CMD_SUSPEND:
       log::info("suspends");
