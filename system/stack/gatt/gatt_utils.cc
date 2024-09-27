ed1e41ca882bfa1742551f5323b375239f4a6e94
Himanshu Rawat <rwt@google.com>
2024-09-27 16:00:34 +0000

    Skip ATT Read Blob request for name discovery before MTU exchange
    Some LE devices cannot handle the ATT Read Blob request. They expect that ATT MTU should be large enough to avoid ATT Read Blob request. Their companion apps are expected to send their MTU preferences.
    However, LE name discovery may happen earlier than MTU exchange is performed. In such case, ATT Read Blob request is used if the remote device is larger than 20 octets.
    
    This change introduces an intermediary IOP fix for such devices by skipping ATT Read Blob request for LE name discovery if their companion app had request MTU exchange but it was not performed yet.
    
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT IOP fix
    Bug: 369329192
    Change-Id: I59021fc5530bbaeffefa3c70f46218276a759bbf

--- system/stack/gatt/gatt_utils.cc
+++ system/stack/gatt/gatt_utils.cc
@@ -1993,3 +1993,59 @@ tCONN_ID gatt_create_conn_id(tTCB_IDX tcb_idx, tGATT_IF gatt_if) {
 tTCB_IDX gatt_get_tcb_idx(tCONN_ID conn_id) { return (uint8_t)(conn_id >> 8); }
 
 tGATT_IF gatt_get_gatt_if(tCONN_ID conn_id) { return (tGATT_IF)conn_id; }
+
+uint16_t gatt_get_mtu_pref(const tGATT_REG* p_reg, const RawAddress& bda) {
+  for (auto& it : p_reg->mtu_prefs) {
+    if (it.bda == bda) {
+      return it.preferred_mtu;
+    }
+  }
+  return 0;
+}
+
+void gatt_remove_mtu_pref(tGATT_REG* p_reg, const RawAddress& bda) {
+  p_reg->mtu_prefs.remove_if([bda](tGATT_MTU_PREF mtu_pref) { return mtu_pref.bda == bda; });
+}
+
+uint16_t gatt_get_apps_preferred_mtu(const RawAddress& bda) {
+  uint16_t preferred_mtu = 0;
+  if (com::android::bluetooth::flags::gatt_client_dynamic_allocation()) {
+    for (auto& [i, p_reg] : gatt_cb.cl_rcb_map) {
+      if (!p_reg->in_use) {
+        continue;
+      }
+
+      preferred_mtu = std::max(preferred_mtu, gatt_get_mtu_pref(p_reg.get(), bda));
+    }
+  } else {
+    for (auto& reg : gatt_cb.cl_rcb) {
+      if (!reg.in_use) {
+        continue;
+      }
+
+      preferred_mtu = std::max(preferred_mtu, gatt_get_mtu_pref(&reg, bda));
+    }
+  }
+
+  return preferred_mtu;
+}
+
+void gatt_remove_apps_mtu_prefs(const RawAddress& bda) {
+  if (com::android::bluetooth::flags::gatt_client_dynamic_allocation()) {
+    for (auto& [i, p_reg] : gatt_cb.cl_rcb_map) {
+      if (!p_reg->in_use) {
+        continue;
+      }
+
+      gatt_remove_mtu_pref(p_reg.get(), bda);
+    }
+  } else {
+    for (auto& reg : gatt_cb.cl_rcb) {
+      if (!reg.in_use) {
+        continue;
+      }
+
+      gatt_remove_mtu_pref(&reg, bda);
+    }
+  }
+}
