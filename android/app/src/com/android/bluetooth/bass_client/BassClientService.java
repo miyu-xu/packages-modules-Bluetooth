809a9fc06c4296451d1c26f335049c9672119783
zhenchao <quic_zhenchao@quicinc.com>
2024-09-29 16:48:12 +0800

    [le audio] use broadcastId instead of Sid to check for local broadcast
    Advertising sid is likely to be same for local source and external source. As a result, external source is unable to be added while local source is enabled. This fix use broadcastId instead of advertising Sid to check for local broadcast source.
    
    Bug: 370267705
    Test: manual
    Test: atest BassClientServiceTest LeAudioBroadcastServiceTest
    Change-Id: I6e969a4f47d4280019c32195c177654e2f713158

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -2907,7 +2907,7 @@ public class BassClientService extends ProfileService {
         return stateMachine.getMaximumSourceCapacity();
     }
 
-    private boolean isLocalBroadcast(int sourceAdvertisingSid) {
+    private boolean isLocalBroadcast(int broadcastId) {
         LeAudioService leAudioService = mServiceFactory.getLeAudioService();
         if (leAudioService == null) {
             return false;
@@ -2917,7 +2917,7 @@ public class BassClientService extends ProfileService {
                 leAudioService.getAllBroadcastMetadata().stream()
                         .anyMatch(
                                 meta -> {
-                                    return meta.getSourceAdvertisingSid() == sourceAdvertisingSid;
+                                    return meta.getBroadcastId() == broadcastId;
                                 });
         log("isLocalBroadcast=" + wasFound);
         return wasFound;
@@ -2928,7 +2928,7 @@ public class BassClientService extends ProfileService {
             return false;
         }
 
-        return isLocalBroadcast(metaData.getSourceAdvertisingSid());
+        return isLocalBroadcast(metaData.getBroadcastId());
     }
 
     boolean isLocalBroadcast(BluetoothLeBroadcastReceiveState receiveState) {
@@ -2936,7 +2936,7 @@ public class BassClientService extends ProfileService {
             return false;
         }
 
-        return isLocalBroadcast(receiveState.getSourceAdvertisingSid());
+        return isLocalBroadcast(receiveState.getBroadcastId());
     }
 
     static void log(String msg) {
