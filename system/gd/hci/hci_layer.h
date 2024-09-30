6cee06db6db66f69e53a5972f844d55c761b877f
Ritvik Mahesh <quic_rmahesh@quicinc.com>
2024-09-30 14:13:44 +0530

    HCI: Dynamically increase command timeout
    Dynamic command timeout.
    Problem statement:
    Today in AOSP stack command timeout is triggered if response
    is not received in 2 seconds. But if there is a LE scan
    running background, and there is flood of LE advertising
    events received processing this event can be delayed either
    by stack or HAL or UART. Upon reaching 2 second stack triggers
    a command timeout by sending debug info command.
    From our command timeout issue triage, we found a flood of
    LE events are received along with events, so it is a False
    positive command timeout triggered by stack.
    To mitigate these false positive command timeouts issue we are proposing below.
    
    Proposal:
    When the stack sends the command to HAL. It starts monitoring
    incoming data from the HAL, either events or the ACL data
    (regular ACL data or debug info data sent as ACL).
     When an event is received in less than 2 seconds it stops
    monitoring incoming data. If the event is not received, based
    on the incoming data flow from HAL it increases the timeout value,
    for example HAL send 5 LE ADV events and 5 ACL data.
    Then a total of 10 packets are received in 2 seconds from HAL,
    Stack increases the timeout to 1 more seconds
    i.e. 10 packets * 100 msec of each packet.
    If the actual command event is received in new timeout,
    stack stops the command timeout timer and stops monitoring
    incoming data. If command event is not received in new 1 second too,
    upon timeout stack checks if there are any new incoming data in
    new timeout duration i.e. x+2 to x+2+1 (2 is actual command timeout
    and 1 is new command timeout value configured). If the data is received,
    then it increases command timeout timer to new value.
    This increment logic runs until the stack reaches 5 second timeout.
    
    bug: 410686699
    
    Test: m Bluetooth
    Flag: EXEMPT, trivial
    
    Change-Id: Ieda46b9cb335aa297fad3f4bb7680cc8caf5237c

--- system/gd/hci/hci_layer.h
+++ system/gd/hci/hci_layer.h
@@ -42,6 +42,8 @@
 namespace bluetooth {
 namespace hci {
 
+struct monitor_command;
+
 class HciLayer : public Module, public HciInterface {
   // LINT.IfChange
 public:
@@ -137,6 +139,8 @@ public:
   static constexpr std::chrono::milliseconds kHciTimeoutMs = std::chrono::milliseconds(2000);
   static constexpr std::chrono::milliseconds kHciTimeoutRestartMs = std::chrono::milliseconds(5000);
 
+
+
   static const ModuleFactory Factory;
 
 protected:
@@ -162,8 +166,16 @@ private:
   struct hal_callbacks;
   impl* impl_;
   hal_callbacks* hal_callbacks_;
+  struct monitor_command {
+    unsigned int lapsed_timeout;
+    unsigned int no_packets_rx;
+    unsigned int prev_no_packets;
+    bool is_monitor_enabled;
+  };
+  monitor_command cmd_stats;
 
   std::mutex callback_handlers_guard_;
+  std::mutex monitor_cmd_stats;
   void on_connection_request(EventView event_view);
   void on_disconnection_complete(EventView event_view);
   void on_read_remote_version_complete(EventView event_view);
