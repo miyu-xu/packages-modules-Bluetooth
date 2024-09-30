92a34098f370368e388e6e014223db8705213e8c
Jayden Kim <jaydenk@google.com>
2024-09-30 04:28:58 +0000

    Add low power processor offload manager with socket hal shim
    Bug: 342012881
    Test: m com.android.btservices
    Change-Id: Iec194e241bdafe8dce34db23cc65398a0af17be0

--- system/main/test/main_shim_test.cc
+++ system/main/test/main_shim_test.cc
@@ -42,6 +42,7 @@
 #include "hci/distance_measurement_manager_mock.h"
 #include "hci/le_advertising_manager_mock.h"
 #include "hci/le_scanning_manager_mock.h"
+#include "hci/lpp_offload_interface.h"
 #include "include/hardware/ble_scanner.h"
 #include "main/shim/acl.h"
 #include "main/shim/acl_interface.h"
@@ -349,6 +350,8 @@ protected:
             new bluetooth::hci::testing::MockLeAdvertisingManager();
     /* extern */ test::mock_distance_measurement_manager_ =
             new bluetooth::hci::testing::MockDistanceMeasurementManager();
+    /* extern */ test::mock_lpp_offload_interface_ =
+            new bluetooth::hci::testing::MockLppOffloadInterface();
   }
   void TearDown() override {
     delete test::mock_controller_;
@@ -361,6 +364,8 @@ protected:
     test::mock_le_scanning_manager_ = nullptr;
     delete test::mock_distance_measurement_manager_;
     test::mock_distance_measurement_manager_ = nullptr;
+    delete test::mock_lpp_offload_interface_;
+    test::mock_lpp_offload_interface_ = nullptr;
 
     handler_->Clear();
     delete handler_;
