b767aa781b6eca043272c1579984b9fbc6874c90
Jayden Kim <jaydenk@google.com>
2024-09-30 04:28:58 +0000

    Add low power processor offload manager with socket hal shim
    Bug: 342012881
    Test: m com.android.btservices
    Change-Id: Iec194e241bdafe8dce34db23cc65398a0af17be0

---
+++ system/gd/hal/socket_hal_host.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hal/socket_hal.h"
+
+using bluetooth::hci::ConnectionState;
+using bluetooth::hci::SocketContext;
+using bluetooth::hci::SocketHalCallback;
+using bluetooth::hci::SocketProperties;
+
+namespace bluetooth {
+namespace hal {
+
+const std::vector<SocketProperties> kDefaultOffloadSocketProperties = {
+        {.dataPath = hci::SocketDataPath::OFFLOAD_SOFTWARE,
+         .protocolProperties = {{
+                 .protocol = hci::SocketProtocol::LE_COC,
+                 .numOfSocketSupported = 0,
+         }}},
+        {.dataPath = hci::SocketDataPath::OFFLOAD_HARDWARE,
+         .protocolProperties = {{
+                 .protocol = hci::SocketProtocol::LE_COC,
+                 .numOfSocketSupported = 0,
+         }}}};
+
+class SocketHalHost : public SocketHal {
+public:
+  bool IsBound() override { return false; }
+
+protected:
+  void ListDependencies(ModuleList* /*list*/) const {}
+
+  void Start() override {}
+
+  void Stop() override {}
+
+  std::string ToString() const override { return std::string("SocketHalHost"); }
+
+  std::vector<SocketProperties> GetSocketProperties() override {
+    return {kDefaultOffloadSocketProperties};
+  }
+
+  void RegisterCallback(SocketHalCallback* callback) override { socket_hal_cb_ = callback; }
+
+  bool NotifySocketConnectionStateChange(const SocketContext& /*context*/) override { return true; }
+
+private:
+  SocketHalCallback* socket_hal_cb_;
+};
+
+const ModuleFactory SocketHal::Factory = ModuleFactory([]() { return new SocketHalHost(); });
+
+}  // namespace hal
+}  // namespace bluetooth
---
+++ system/gd/hci/lpp_offload_interface.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "lpp_offload_socket_interface.h"
+
+namespace bluetooth {
+namespace hci {
+
+struct LppOffloadFeatures {
+  bool socket_sw_offload_supported;
+  bool socket_hw_offload_supported;
+};
+
+/**
+ * Callbacks to notify upper layers of events or requests received from the offload domain. It can
+ * inherit multiple offload HAL callbacks, allowing higher layers to implement callback handlers in
+ * the same place.
+ */
+class LppOffloadCallbacks : public SocketHalCallback {
+public:
+  virtual ~LppOffloadCallbacks() = default;
+
+  /**
+   * This callback is to allow HAL to send an asynchronous event to host stack.
+   *
+   * @param eventType asynchronous event type
+   */
+  virtual void onReceiveAsyncEvent(AsyncEventType eventType) = 0;
+
+  /**
+   * This callback is to request host stack to close the socket.
+   *
+   * @param socketId socket identifier to be closed
+   * @param reason request reason to close the socket
+   */
+  virtual void onReceiveSocketCloseRequest(const bluetooth::Uuid& socketId,
+                                           RequestReason reason) = 0;
+};
+
+/**
+ * Interface to low-power processors to support LPP offload
+ * features. This allows multiple offload HAL interfaces to be inherited and a single offload
+ * interface to be called from the upper layer to support unified offload function management.
+ */
+class LppOffloadInterface : public SocketHalInterface {
+public:
+  LppOffloadInterface() = default;
+
+  virtual ~LppOffloadInterface() = default;
+
+  LppOffloadInterface(const LppOffloadInterface&) = delete;
+
+  LppOffloadInterface& operator=(const LppOffloadInterface&) = delete;
+
+  /**
+   * Register a callback function to receive events from LPP offload HALs
+   *
+   * @param callback LPP offload hal callback
+   */
+  virtual void RegisterLppOffloadCallbacks(LppOffloadCallbacks* callbacks) = 0;
+
+  /**
+   * Get offload features available from LPP offload HALs
+   *
+   * @param features pointer to get LPP offload features
+   */
+  virtual void GetOffloadFeaturesSupported(LppOffloadFeatures* features) = 0;
+
+  /**
+   * Get the supported socket properties like number of sockets and protocol specs
+   *
+   * @param dataPath socket data path
+   * @param socketPros pointer to get socket properties
+   * @return true if successfully found socket properties for the data path and set it
+   */
+  virtual bool GetSocketProperties(SocketDataPath dataPath, SocketProperties* socketPros) = 0;
+
+  /**
+   * Notify to offload stack and end point when socket connection state is changed
+   *
+   * @param SocketContext channel info, connection state, data path, and end point info
+   * @return true if successfully notified to the offload end point
+   */
+  virtual bool NotifySocketConnectionStateChange(const SocketContext& context) = 0;
+};
+
+}  // namespace hci
+}  // namespace bluetooth
