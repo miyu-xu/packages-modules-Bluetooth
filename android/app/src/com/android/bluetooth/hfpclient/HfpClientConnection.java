de78be1cc08a780aad8eab63024e26ecb6aa6d90
Nitin Jadhav <jadhavnitin@google.com>
2024-10-01 16:25:11 +0000

    Fix a potential null pointer dereference in HfpClientConnection->finishInitializing()
    Call termination sets mCurrentCall to null, this CL adds a condition to check if call is terminated. This is to prevent a potential crash if mCurrentCall is null.
    
    Bug: 369169855
    Test: mmm  packages/modules/Bluetooth
    
    Flag: com.android.bluetooth.flags.check_call_termination_during_init
    Change-Id: I78cb450bc187b50116af9d145f3aa40e5295f643

--- android/app/src/com/android/bluetooth/hfpclient/HfpClientConnection.java
+++ android/app/src/com/android/bluetooth/hfpclient/HfpClientConnection.java
@@ -85,6 +85,9 @@ public class HfpClientConnection extends Connection {
 
     void finishInitializing() {
         setAudioModeIsVoip(false);
+        if (mClosed) {
+            return;
+        }
         Uri number = Uri.fromParts(PhoneAccount.SCHEME_TEL, mCurrentCall.getNumber(), null);
         setAddress(number, TelecomManager.PRESENTATION_ALLOWED);
         setConnectionCapabilities(
