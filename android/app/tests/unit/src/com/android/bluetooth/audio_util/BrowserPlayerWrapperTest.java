2978a7b007231d7189b823ec5d60ef91537faae0
Etienne Ruffieux <eruffieux@google.com>
2024-10-01 11:16:32 +0000

    Add VFS browsing Cover Art support
    Cover art was implemented for browsing, but only when remote requested
    Now Playing browsing. This was due to memory usage required to store images for
    VFS browsing as the media players send an URI instead. We need to implement it,
    and gate it behind a System property to enable as wanted to pass certification
    tests. The implementation is already done in the native side.
    
    Bug: 364831930
    Test: atest BluetoothInstrumentationTests
    Flag: exempt, change is disabled by default
    Change-Id: Ib970e3add951bc7e9581e1e4b38b67d215828bb4

--- android/app/tests/unit/src/com/android/bluetooth/audio_util/BrowserPlayerWrapperTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/audio_util/BrowserPlayerWrapperTest.java
@@ -38,7 +38,6 @@ import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.android.bluetooth.R;
 import com.android.bluetooth.TestUtils;
 
 import org.junit.After;
@@ -74,7 +73,6 @@ public class BrowserPlayerWrapperTest {
     private HandlerThread mThread;
 
     @Mock Context mMockContext;
-    @Mock Resources mMockResources;
     private Context mTargetContext;
     private Resources mTestResources;
     private MockContentResolver mTestContentResolver;
@@ -115,8 +113,7 @@ public class BrowserPlayerWrapperTest {
                 });
 
         when(mMockContext.getContentResolver()).thenReturn(mTestContentResolver);
-        when(mMockResources.getBoolean(R.bool.avrcp_target_cover_art_uri_images)).thenReturn(true);
-        when(mMockContext.getResources()).thenReturn(mMockResources);
+        Util.sUriImagesSupport = true;
 
         // Set up Looper thread for the timeout handler
         mThread = new HandlerThread("MediaPlayerWrapperTestThread");
@@ -137,6 +134,7 @@ public class BrowserPlayerWrapperTest {
         mTestBitmap = null;
         mTestResources = null;
         mTargetContext = null;
+        Util.sUriImagesSupport = false;
     }
 
     private Bitmap loadImage(int resId) {
