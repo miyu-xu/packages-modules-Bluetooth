788ecb151fe35a8ab9c60feaa67a905d13c6802c
Jayden Kim <jaydenk@google.com>
2024-10-02 06:44:57 +0000

    Notify offload socket connection state to socket hal
    Bug: 342012881
    Test: m com.android.btservices
    Change-Id: I48d5ac1e89ef6a93ec0ddfa9df2db9cc56bc872e

--- system/stack/gap/gap_conn.cc
+++ system/stack/gap/gap_conn.cc
@@ -55,9 +55,11 @@ typedef struct {
 #define GAP_CCB_FLAGS_CONN_DONE 0x0E
   uint8_t con_flags;
 
-  uint8_t service_id;     /* Used by BTM */
-  uint16_t gap_handle;    /* GAP handle */
-  uint16_t connection_id; /* L2CAP CID */
+  uint8_t service_id;         /* Used by BTM */
+  uint16_t gap_handle;        /* GAP handle */
+  uint16_t connection_id;     /* Local L2CAP CID */
+  uint16_t rem_connection_id; /* Remote L2CAP CID */
+  uint16_t acl_handle;        /* ACL handle */
   bool rem_addr_specified;
   uint8_t chan_mode_mask; /* Supported channel modes (FCR) */
   RawAddress rem_dev_address;
@@ -579,6 +581,53 @@ uint16_t GAP_ConnGetL2CAPCid(uint16_t gap_handle) {
   }
 
   return p_ccb->connection_id;
+}
+
+/*******************************************************************************
+ *
+ * Function         GAP_GetChannelInfo
+ *
+ * Description      This function is called to get L2CAP channel information
+ *                  by the gap handle.
+ *
+ * Parameters:      handle        - Handle of the port returned in the Open
+ *                  remote_mtu    - OUT remote L2CAP MTU
+ *                  local_mps     - OUT local L2CAP COC MPS
+ *                  remote_mps    - OUT remote L2CAP COC MPS
+ *                  local_credit  - OUT local L2CAP COC credit
+ *                  remote_credit - OUT remote L2CAP COC credit
+ *                  local_cid     - OUT local L2CAP CID
+ *                  remote_cid    - OUT remote L2CAP CID
+ *                  acl_handle    - OUT ACL handle
+ *
+ * Returns          true if request accepted
+ *
+ ******************************************************************************/
+bool GAP_GetChannelInfo(uint16_t gap_handle, uint16_t* remote_mtu, uint16_t* local_mps,
+                        uint16_t* remote_mps, uint16_t* local_credit, uint16_t* remote_credit,
+                        uint16_t* local_cid, uint16_t* remote_cid, uint16_t* acl_handle) {
+  tGAP_CCB* p_ccb;
+
+  p_ccb = gap_find_ccb_by_handle(gap_handle);
+  if (p_ccb == NULL) {
+    return false;
+  }
+
+  uint16_t ahandle;
+  if (!stack::l2cap::get_interface().L2CA_GetAclHandle(p_ccb->connection_id, &ahandle)) {
+    return false;
+  }
+  p_ccb->acl_handle = ahandle;
+
+  *remote_mtu = p_ccb->rem_mtu_size;
+  *local_mps = p_ccb->local_coc_cfg.mps;
+  *remote_mps = p_ccb->peer_coc_cfg.mps;
+  *local_credit = p_ccb->local_coc_cfg.credits;
+  *remote_credit = p_ccb->peer_coc_cfg.credits;
+  *local_cid = p_ccb->connection_id;
+  *remote_cid = p_ccb->rem_connection_id;
+  *acl_handle = p_ccb->acl_handle;
+  return true;
 }
 
 /*******************************************************************************
