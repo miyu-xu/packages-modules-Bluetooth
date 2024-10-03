c0e2322fc6b340bafe50893146b0cc37ce06cc05
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-10-03 16:39:13 +0000

    Implement MSFT extension logic in ScanManager
    Bug: 360387731
    Bug: 360392001
    Bug: 365787977
    Test: m -j
    Change-Id: I7ef0770537d126c1ec1012353317ba968d6c8a1b

--- android/app/src/com/android/bluetooth/le_scan/ScanNativeInterface.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanNativeInterface.java
@@ -101,6 +101,16 @@ public class ScanNativeInterface {
     /************************** MSFT scan related native methods *****************************/
     private native boolean gattClientIsMsftSupportedNative();
 
+    private native void gattClientMsftAdvMonitorAddNative(
+            MsftAdvMonitor.Monitor msft_adv_monitor,
+            MsftAdvMonitor.Pattern[] msft_adv_monitor_patterns,
+            MsftAdvMonitor.Address msft_adv_monitor_address,
+            int filter_index);
+
+    private native void gattClientMsftAdvMonitorRemoveNative(int filter_index, int monitor_handle);
+
+    private native void gattClientMsftAdvMonitorEnableNative(boolean enable);
+
     /************************** Batch related native methods *********************************/
     private native void gattClientConfigBatchScanStorageNative(
             int clientIf,
@@ -175,6 +185,31 @@ public class ScanNativeInterface {
     /** Check if MSFT HCI extension is supported */
     public boolean gattClientIsMsftSupported() {
         return gattClientIsMsftSupportedNative();
+    }
+
+    /** Add a MSFT Advertisement Monitor */
+    public void gattClientMsftAdvMonitorAdd(
+            MsftAdvMonitor.Monitor msft_adv_monitor,
+            MsftAdvMonitor.Pattern[] msft_adv_monitor_patterns,
+            MsftAdvMonitor.Address msft_adv_monitor_address,
+            int filter_index) {
+        gattClientMsftAdvMonitorAddNative(
+                msft_adv_monitor,
+                msft_adv_monitor_patterns,
+                msft_adv_monitor_address,
+                filter_index);
+    }
+
+    /** Remove a MSFT Advertisement Monitor */
+    public void gattClientMsftAdvMonitorRemove(int filter_index) {
+        int monitor_handle = mScanHelper.msftMonitorHandleFromFilterIndex(filter_index);
+        if (monitor_handle < 0) return;
+        gattClientMsftAdvMonitorRemoveNative(filter_index, monitor_handle);
+    }
+
+    /** Enable a MSFT Advertisement Monitor */
+    public void gattClientMsftAdvMonitorEnable(boolean enable) {
+        gattClientMsftAdvMonitorEnableNative(enable);
     }
 
     /** Configure BLE batch scan storage */
@@ -380,9 +415,27 @@ public class ScanNativeInterface {
         mScanHelper.onScanParamSetupCompleted(status, scannerId);
     }
 
-    void onMsftAdvMonitorAdd(int filter_index, int monitor_handle, int status) {}
+    void onMsftAdvMonitorAdd(int filter_index, int monitor_handle, int status) {
+        if (mScanHelper == null) {
+            Log.e(TAG, "Scan helper is null!");
+            return;
+        }
+        mScanHelper.onMsftAdvMonitorAdd(filter_index, monitor_handle, status);
+    }
 
-    void onMsftAdvMonitorRemove(int filter_index, int status) {}
+    void onMsftAdvMonitorRemove(int filter_index, int status) {
+        if (mScanHelper == null) {
+            Log.e(TAG, "Scan helper is null!");
+            return;
+        }
+        mScanHelper.onMsftAdvMonitorRemove(filter_index, status);
+    }
 
-    void onMsftAdvMonitorEnable(int status) {}
+    void onMsftAdvMonitorEnable(int status) {
+        if (mScanHelper == null) {
+            Log.e(TAG, "Scan helper is null!");
+            return;
+        }
+        mScanHelper.onMsftAdvMonitorEnable(status);
+    }
 }
