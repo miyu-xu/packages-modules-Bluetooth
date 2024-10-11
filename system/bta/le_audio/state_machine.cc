f0a1e99c81e3534aae22faec4a6e0907e0754d43
wentjin <quic_wentjin@quicinc.com>
2024-10-11 17:02:29 +0800

    Attach single device to stream when group target is STREAMING
    Use-case:
    1. Connect LEA CSIP headset and play music
    2. Reboot BT
    3. Headset reconnected again but no sounds in headset.
    
    Root cause: Earbud A connected and MM triggered STREAMING, earbud B
    connected during A under streaming transition, and attached B into
    streaming. When B entered QoS state, A had been in ENABLING. B would
    wait for A’s QoS, but A would enter STREAMING later. B would block in
    QoS, and A waited for B’s STREAMING. Finally, timeout happened.
    
    Fixed: If group's target was STREAMING, and CIG had been CREATED. It
    should enable current device.
    
    Bug: 381948003
    Test: m com.android.btservices
    Change-Id: I4bf87c97d7a51edbe0a045928f52416410cfe399

--- system/bta/le_audio/state_machine.cc
+++ system/bta/le_audio/state_machine.cc
@@ -2284,7 +2284,9 @@ private:
           return;
         }
 
-        if (group->GetState() == AseState::BTA_LE_AUDIO_ASE_STATE_STREAMING) {
+        if (group->GetState() == AseState::BTA_LE_AUDIO_ASE_STATE_STREAMING ||
+            (group->GetTargetState() == AseState::BTA_LE_AUDIO_ASE_STATE_STREAMING &&
+                                       group->cig.GetState() == CigState::CREATED)) {
           /* We are here because of the reconnection of the single device. */
           PrepareAndSendEnable(leAudioDevice);
           return;
