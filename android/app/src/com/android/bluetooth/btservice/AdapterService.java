5dd99ecf150ff2a7876da9a13238278df47d7d46
jasonwshsu <jasonwshsu@google.com>
2024-10-12 00:35:34 +0800

    Add new metadata APIs get/set for is_microphone_for_call_enabled
    These API allows application can set/get microphone for call enable status for the bluetooth device.
    Default value is true.
    
    Tag: #feature
    Bug: 372395197
    Bug: 349255906
    Test: atest DatabaseManagerTest
    Change-Id: Idda448b41a1a7ff0c7bf980f072752a80a8fc82f

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -4279,6 +4279,40 @@ public class AdapterService extends Service {
             service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
             return service.mDatabaseManager.getActiveAudioDevicePolicy(device);
         }
+
+        @Override
+        public boolean setMicrophoneForCallEnabled(
+                BluetoothDevice device, boolean enabled, AttributionSource source) {
+            requireNonNull(device);
+            // don't check caller, may be called from system UI
+            AdapterService service = getService();
+            if (service == null
+                    || !BluetoothAdapter.checkBluetoothAddress(device.getAddress())
+                    || !Utils.checkConnectPermissionForDataDelivery(
+                            service, source, "AdapterService setMicrophoneForCallEnabled")) {
+                return false;
+            }
+
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+            return service.mDatabaseManager.setMicrophoneForCallEnabled(device, enabled);
+        }
+
+        @Override
+        public boolean isMicrophoneForCallEnabled(
+                BluetoothDevice device, AttributionSource source) {
+            requireNonNull(device);
+            // don't check caller, may be called from system UI
+            AdapterService service = getService();
+            if (service == null
+                    || !BluetoothAdapter.checkBluetoothAddress(device.getAddress())
+                    || !Utils.checkConnectPermissionForDataDelivery(
+                            service, source, "AdapterService getMicrophoneForCallEnabled")) {
+                return true;
+            }
+
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+            return service.mDatabaseManager.isMicrophoneForCallEnabled(device);
+        }
     }
 
     /**
