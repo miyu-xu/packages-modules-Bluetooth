203183ce6d837a440243435c6af10f68cffa26a6
William Escande <wescande@google.com>
2024-10-11 17:37:03 -0700

    Aics: Add API
    WIP
    
    Bug: 372328699
    Bug: 359916608
    Flag: com.android.bluetooth.flags.aics_api
    Test: atest CtsBluetoothTestCases
    Change-Id: Ie0ff7fb44db852c41794e91388fedb3555d2df4d

--- android/app/src/com/android/bluetooth/a2dp/A2dpNativeInterface.java
+++ android/app/src/com/android/bluetooth/a2dp/A2dpNativeInterface.java
@@ -15,7 +15,7 @@
  */
 
 /*
- * Defines the native inteface that is used by state machine/service to
+ * Defines the native interface that is used by state machine/service to
  * send or receive messages from the native stack. This file is registered
  * for the native methods in the corresponding JNI C++ file.
  */
