74b8609a4f80de612ad92be4ad0e74911839409b
ELIYAZ MOMIN <mohammedeliyaz@google.com>
2024-10-17 15:00:08 +0000

    Revert "Fix crash on SDP results"
    This reverts commit 265b4973889d401a0590b3a751c555a69640e984.
    
    Reason for revert: <Potential culprit for b/374083359  - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.>
    
    Change-Id: I244228d83c47e87abf2f70ceaecd1038dd351d81

--- system/bta/dm/bta_dm_disc.cc
+++ system/bta/dm/bta_dm_disc.cc
@@ -246,11 +246,9 @@ void bta_dm_sdp_finished(RawAddress bda, tBTA_STATUS result, std::vector<bluetoo
 
 /* Callback from sdp with discovery status */
 void bta_dm_sdp_callback(const RawAddress& /* bd_addr */, tSDP_STATUS sdp_status) {
-  bool sdp_pending = bta_dm_discovery_cb.transports & BT_TRANSPORT_BR_EDR;
-  log::info("{}, sdp_pending: {}", bta_dm_state_text(bta_dm_discovery_get_state()), sdp_pending);
+  log::info("{}", bta_dm_state_text(bta_dm_discovery_get_state()));
 
-  if (bta_dm_discovery_get_state() == BTA_DM_DISCOVER_IDLE || !sdp_pending ||
-      !bta_dm_discovery_cb.sdp_state) {
+  if (bta_dm_discovery_get_state() == BTA_DM_DISCOVER_IDLE) {
     return;
   }
 
