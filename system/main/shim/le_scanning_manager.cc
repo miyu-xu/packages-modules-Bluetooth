8be96c98507c65d67388e43d0a4f6b0cc5de5c23
Suqun Peng <quic_suqupeng@quicinc.com>
2024-10-17 15:17:28 +0800

    le_scanning_manager: Prevent race condition between gd_stack_thread and bt_main_thread
    - INQUIRY_RESULT/INQUIRY_RESULT_WITH_RSSI/EXTENDED_INQUIRY_RESULT
    Callbacks register on bt_main_thread
    - ADVERTISING_REPORT/EXTENDED_ADVERTISING_REPORT
    Callbacks register on gd_stack_thread
    - Race condition happened on writing eir_uuids_cache in
    btif_dm_search_devices_evt(BTA_DM_INQ_RES_EVT)
    
    Post task to bt_main_thread
    
    Bug: 374011856
    Test: m com.android.btservices
    Change-Id: Ia2f67f6cca77749592c5e4d5b0a316fd366e4b34

--- system/main/shim/le_scanning_manager.cc
+++ system/main/shim/le_scanning_manager.cc
@@ -38,6 +38,7 @@
 #include "stack/include/bt_dev_class.h"
 #include "stack/include/btm_log_history.h"
 #include "stack/include/btm_status.h"
+#include "stack/include/main_thread.h"
 #include "storage/device.h"
 #include "storage/le_device.h"
 #include "storage/storage_module.h"
@@ -459,27 +460,10 @@ void BleScannerInterfaceImpl::OnScanResult(uint16_t event_type, uint8_t address_
                                            int8_t tx_power, int8_t rssi,
                                            uint16_t periodic_advertising_interval,
                                            std::vector<uint8_t> advertising_data) {
-  RawAddress raw_address = ToRawAddress(address);
-  tBLE_ADDR_TYPE ble_addr_type = to_ble_addr_type(address_type);
-
-  btm_cb.neighbor.le_scan.results++;
-  if (ble_addr_type != BLE_ADDR_ANONYMOUS) {
-    btm_ble_process_adv_addr(raw_address, &ble_addr_type);
-  }
-
-  do_in_jni_thread(base::BindOnce(&BleScannerInterfaceImpl::handle_remote_properties,
-                                  base::Unretained(this), raw_address, ble_addr_type,
-                                  advertising_data));
-
-  do_in_jni_thread(base::BindOnce(
-          &ScanningCallbacks::OnScanResult, base::Unretained(scanning_callbacks_), event_type,
-          static_cast<uint8_t>(address_type), raw_address, primary_phy, secondary_phy,
-          advertising_sid, tx_power, rssi, periodic_advertising_interval, advertising_data));
-
-  // TODO: Remove when StartInquiry in GD part implemented
-  btm_ble_process_adv_pkt_cont_for_inquiry(event_type, ble_addr_type, raw_address, primary_phy,
-                                           secondary_phy, advertising_sid, tx_power, rssi,
-                                           periodic_advertising_interval, advertising_data);
+  do_in_main_thread(base::BindOnce(&BleScannerInterfaceImpl::on_scan_result, base::Unretained(this),
+                                   event_type, address_type, address, primary_phy, secondary_phy,
+                                   advertising_sid, tx_power, rssi, periodic_advertising_interval,
+                                   std::move(advertising_data)));
 }
 
 void BleScannerInterfaceImpl::OnTrackAdvFoundLost(
@@ -725,6 +709,40 @@ void BleScannerInterfaceImpl::handle_remote_properties(RawAddress bd_addr, tBLE_
   bluetooth::storage::LeDevice le_device = device.Le();
   mutation2.Add(le_device.SetAddressType((bluetooth::hci::AddressType)addr_type));
   mutation2.Commit();
+}
+
+void BleScannerInterfaceImpl::on_scan_result(uint16_t event_type, uint8_t address_type,
+                                             bluetooth::hci::Address address, uint8_t primary_phy,
+                                             uint8_t secondary_phy, uint8_t advertising_sid,
+                                             int8_t tx_power, int8_t rssi,
+                                             uint16_t periodic_advertising_interval,
+                                             std::vector<uint8_t> advertising_data) {
+  if (!bluetooth::shim::is_gd_stack_started_up()) {
+    log::warn("Gd stack is stopped, return");
+    return;
+  }
+
+  RawAddress raw_address = ToRawAddress(address);
+  tBLE_ADDR_TYPE ble_addr_type = to_ble_addr_type(address_type);
+
+  btm_cb.neighbor.le_scan.results++;
+  if (ble_addr_type != BLE_ADDR_ANONYMOUS) {
+    btm_ble_process_adv_addr(raw_address, &ble_addr_type);
+  }
+
+  do_in_jni_thread(base::BindOnce(&BleScannerInterfaceImpl::handle_remote_properties,
+                                  base::Unretained(this), raw_address, ble_addr_type,
+                                  advertising_data));
+
+  do_in_jni_thread(base::BindOnce(
+          &ScanningCallbacks::OnScanResult, base::Unretained(scanning_callbacks_), event_type,
+          static_cast<uint8_t>(address_type), raw_address, primary_phy, secondary_phy,
+          advertising_sid, tx_power, rssi, periodic_advertising_interval, advertising_data));
+
+  // TODO: Remove when StartInquiry in GD part implemented
+  btm_ble_process_adv_pkt_cont_for_inquiry(event_type, ble_addr_type, raw_address, primary_phy,
+                                           secondary_phy, advertising_sid, tx_power, rssi,
+                                           periodic_advertising_interval, advertising_data);
 }
 
 void BleScannerInterfaceImpl::AddressCache::add(const RawAddress& p_bda) {
