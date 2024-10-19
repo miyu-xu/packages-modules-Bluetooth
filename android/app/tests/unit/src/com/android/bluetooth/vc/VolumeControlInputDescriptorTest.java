2bc08963edefff16f8bedac838a75fbe9faf9361
William Escande <wescande@google.com>
2024-10-18 17:50:20 -0700

    AICS: Check mute value and update to int type
    Bug: 361263965
    Test: atest vc_test
    Flag: com.android.bluetooth.flags.leaudio_add_aics_support
    Change-Id: I7b0a28408752dcb16d68c36d9ed9693c2c5579e5

--- android/app/tests/unit/src/com/android/bluetooth/vc/VolumeControlInputDescriptorTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/vc/VolumeControlInputDescriptorTest.java
@@ -111,12 +111,12 @@ public class VolumeControlInputDescriptorTest {
     public void setState_withValidIdButIncorrectSettings_valueIsNotUpdated() {
         int newGainValue = 42;
         int newGainMode = 42;
-        boolean isMute = true;
-        mDescriptor.setState(VALID_ID, newGainMode, newGainMode, isMute);
+        int mute = 0;
+        mDescriptor.setState(VALID_ID, newGainMode, newGainMode, mute);
 
         assertThat(mDescriptor.getGain(VALID_ID)).isNotEqualTo(newGainValue);
         // assertThat(mDescriptor.getGainMode(VALID_ID)).isNotEqualTo(newGainMode);
-        assertThat(mDescriptor.isMuted(VALID_ID)).isFalse();
+        assertThat(mDescriptor.isMuted(VALID_ID)).isTrue();
     }
 
     @Test
@@ -128,8 +128,8 @@ public class VolumeControlInputDescriptorTest {
 
         int newGainValue = 42;
         int newGainMode = 42;
-        boolean isMute = true;
-        mDescriptor.setState(VALID_ID, newGainMode, newGainMode, isMute);
+        int mute = 1;
+        mDescriptor.setState(VALID_ID, newGainMode, newGainMode, mute);
 
         assertThat(mDescriptor.getGain(VALID_ID)).isEqualTo(newGainValue);
         // assertThat(mDescriptor.getGainMode(VALID_ID)).isNotEqualTo(newGainMode);
@@ -146,8 +146,8 @@ public class VolumeControlInputDescriptorTest {
 
         int newGainValue = 42;
         int newGainMode = 42;
-        boolean isMute = true;
-        mDescriptor.setState(INVALID_ID, newGainMode, newGainMode, isMute);
+        int mute = 1;
+        mDescriptor.setState(INVALID_ID, newGainMode, newGainMode, mute);
 
         assertThat(mDescriptor.getGain(INVALID_ID)).isNotEqualTo(newGainValue);
         // assertThat(mDescriptor.getGainMode(VALID_ID)).isNotEqualTo(newGainMode);
