068f1f205e52f3beaa752da376b5e3aa6b930d8b
William Escande <wescande@google.com>
2024-10-23 15:08:28 -0700

    Add aics module and use proper definition
    Bug: 372328699
    Flag: com.android.bluetooth.flags.aics_api
    Test: m .
    Change-Id: I964c79c02cbf5b53aad1434b0361be4f18798677

--- system/bta/vc/types.h
+++ system/bta/vc/types.h
@@ -17,6 +17,7 @@
 
 #pragma once
 
+#include <aics/api.h>
 #include <hardware/bt_vc.h>
 
 #include <algorithm>
@@ -28,6 +29,8 @@
 #include "osi/include/alarm.h"
 #include "types/bluetooth/uuid.h"
 #include "types/raw_address.h"
+
+using bluetooth::aics::MuteField;
 
 namespace bluetooth {
 namespace vc {
@@ -136,7 +139,7 @@ struct GainSettings {
 
 struct VolumeAudioInput {
   uint8_t id;
-  uint8_t mute;
+  MuteField mute;
   int8_t gain_value;
   VolumeInputStatus status;
   VolumeInputType type;
@@ -158,7 +161,7 @@ struct VolumeAudioInput {
 
   explicit VolumeAudioInput(uint16_t service_handle)
       : id(0),
-        mute(0x02),  // DISABLED
+        mute(MuteField::DISABLED),
         gain_value(0),
         status(VolumeInputStatus::Inactive),
         type(VolumeInputType::Unspecified),
