1c6739fa1f2670062b5b2723fe0b3107c408d68b
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-10-25 06:24:48 +0000

    Revert "use_unified_connection_manager removal"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/bbe7b1b2-bd0a-4b0f-87e2-6cb0b1973d89). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I539787592c770d50d44b7c84ee78ef46e3db6e6a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- system/main/shim/acl.cc
+++ system/main/shim/acl.cc
@@ -1009,7 +1009,9 @@ struct shim::Acl::impl {
     auto connection = handle_to_le_connection_map_.find(handle);
     if (connection != handle_to_le_connection_map_.end()) {
       auto remote_address_with_type = connection->second->GetRemoteAddressWithType();
-      GetAclManager()->RemoveFromBackgroundList(remote_address_with_type);
+      if (!com::android::bluetooth::flags::unified_connection_manager()) {
+        GetAclManager()->RemoveFromBackgroundList(remote_address_with_type);
+      }
       connection->second->InitiateDisconnect(ToDisconnectReasonFromLegacy(reason));
       log::debug("Disconnection initiated le remote:{} handle:{}", remote_address_with_type,
                  handle);
--- system/rust/src/core/ffi/module.cc
+++ system/rust/src/core/ffi/module.cc
@@ -22,6 +22,7 @@
 #include "btcore/include/module.h"
 #include "os/log.h"
 #ifndef TARGET_FLOSS
+#include "src/connection/ffi/connection_shim.h"
 #include "src/core/ffi.rs.h"
 #include "src/gatt/ffi.rs.h"
 #endif
@@ -64,7 +65,8 @@ future_t* Start() {
     return fut;
   }
   bluetooth::rust_shim::start(
-          std::make_unique<bluetooth::gatt::GattServerCallbacks>(*callbacks->server), *fut);
+          std::make_unique<bluetooth::gatt::GattServerCallbacks>(*callbacks->server),
+          std::make_unique<bluetooth::connection::LeAclManagerShim>(), *fut);
 
   return fut;
 }
