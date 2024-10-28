1d1e855b26222c02eefe71679e8950870fdf3f3c
Gopi Sakshihally Bhuthaiah <bhuthaiah@google.com>
2024-10-28 09:54:01 +0000

    Modified HidHostTest as per new hid rpc call
    Introduced new RPC call to support run time HID and
    HOGP service configuration. Modified the HidHostTest
    accordingly
    
    Bug: 368045103
    Test: atest -v HidHostTest
    Change-Id: Iba8705587180c1c3433e351b2d8e4aa22db133c4

--- framework/tests/bumble/src/android/bluetooth/hid/HidHostTest.java
+++ framework/tests/bumble/src/android/bluetooth/hid/HidHostTest.java
@@ -80,8 +80,10 @@ import org.mockito.hamcrest.MockitoHamcrest;
 import org.mockito.stubbing.Answer;
 
 import pandora.HIDGrpc;
+import pandora.HidProto.HidServiceType;
 import pandora.HidProto.ProtocolModeEvent;
 import pandora.HidProto.ReportEvent;
+import pandora.HidProto.ServiceRequest;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -284,7 +286,10 @@ public class HidHostTest {
                 (BluetoothHeadset) verifyProfileServiceConnected(BluetoothProfile.HEADSET);
 
         mHidBlockingStub = mBumble.hidBlocking();
-
+        mHidBlockingStub.registerService(
+                ServiceRequest.newBuilder()
+                        .setServiceType(HidServiceType.SERVICE_TYPE_HID)
+                        .build());
         mDevice = mBumble.getRemoteDevice();
         // Remove bond if the device is already bonded
         if (mDevice.getBondState() == BluetoothDevice.BOND_BONDED) {
