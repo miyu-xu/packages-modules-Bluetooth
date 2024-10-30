f720114d4691427636ef4b6d994e01eda6cf4a7f
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-10-30 23:10:39 +0000

    Dont make authentication a mandatory when encryption is requirement
    Change-Id: Ia460e22b5cd2a96c1b871bff2ba791642e6285a7

--- system/stack/btm/btm_sec_cb.cc
+++ system/stack/btm/btm_sec_cb.cc
@@ -276,8 +276,10 @@ bool tBTM_SEC_CB::AddService(bool is_originator, const char* p_name, uint8_t ser
     }
 
     /* Make sure the authenticate bit is set, when encrypt bit is set */
-    if (sec_level & BTM_SEC_OUT_ENCRYPT) {
+    if (false && sec_level & BTM_SEC_OUT_ENCRYPT) {
       sec_level |= BTM_SEC_OUT_AUTHENTICATE;
+    } else {
+        log::debug("dont madatate authentication");
     }
 
     /* outgoing connections usually set the security level right before
