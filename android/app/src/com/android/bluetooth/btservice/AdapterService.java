c26d9f87596bea33f0ebcf2d2b2dbc03737d7ffe
Zhengping Jiang <jiangzp@google.com>
2024-10-30 08:58:11 -0700

    bluetooth: configure wake by BT according to device state
    Monitor device state changes and configure wake by BT.
    
    Bug: 371030412
    Bug: 366432079
    Test: m -j, manual test suspend
    Flag: adapter_suspend_mgmt
    Change-Id: I091196103cb09bfae7b146da4e999ed0baf02f26

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -89,7 +89,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.hardware.display.DisplayManager;
+import android.hardware.devicestate.DeviceStateManager;
 import android.os.AsyncTask;
 import android.os.BatteryStatsManager;
 import android.os.Binder;
@@ -733,10 +733,10 @@ public class AdapterService extends Service {
 
         mBluetoothSocketManagerBinder = new BluetoothSocketManagerBinder(this);
 
-        if (Flags.adapterSuspendMgmt()) {
+        if (Flags.adapterSuspendMgmt() && Flags.deviceStatePropertyApi()) {
             mAdapterSuspend =
                     new AdapterSuspend(
-                            mNativeInterface, mLooper, getSystemService(DisplayManager.class));
+                            mNativeInterface, mLooper, getSystemService(DeviceStateManager.class));
         }
 
         invalidateBluetoothCaches();
