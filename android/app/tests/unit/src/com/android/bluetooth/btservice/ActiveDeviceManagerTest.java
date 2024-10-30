d824c57bfa7f743c172020ab06ad26f61dfde731
Eric Shih <shiheric@google.com>
2024-10-30 04:14:04 +0000

    Fix not fallback to LE Audio when HFP removed active device
    If the device connects to an LE audio device and HFP device, when the HFP
    active device is set to null, the ActvieDeviceManager should try to
    fallback to the LE Audio device.
    
    Bug: 359743806
    Test: atest BluetoothInstrumentationTests:ActiveDeviceManagerTest
    Flag: Exempt, trivial and obvious fix.
    Change-Id: I7e42f9788ee53914fd010b09b745ae37c4921db5

--- android/app/tests/unit/src/com/android/bluetooth/btservice/ActiveDeviceManagerTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/ActiveDeviceManagerTest.java
@@ -378,6 +378,19 @@ public class ActiveDeviceManagerTest {
     }
 
     @Test
+    public void headsetRemoveActive_fallbackToLeAudio() {
+        when(mHeadsetService.getFallbackDevice()).thenReturn(mHeadsetDevice);
+        leAudioConnected(mLeAudioDevice);
+        headsetConnected(mHeadsetDevice, false);
+        mTestLooper.dispatchAll();
+        verify(mHeadsetService).setActiveDevice(mHeadsetDevice);
+
+        headsetActiveDeviceChanged(null);
+        mTestLooper.dispatchAll();
+        verify(mLeAudioService, times(2)).setActiveDevice(mLeAudioDevice);
+    }
+
+    @Test
     public void a2dpConnectedButHeadsetNotConnected_setA2dpActive() {
         when(mAudioManager.getMode()).thenReturn(AudioManager.MODE_IN_CALL);
         a2dpConnected(mA2dpHeadsetDevice, true);
