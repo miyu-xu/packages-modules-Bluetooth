67dd7c5fea4d0320f867ca6bd98834d595640cb0
Antoine SOULIER <asoulier@google.com>
2024-10-31 17:31:39 +0000

    WIP
    Change-Id: Id94a9453c176c466cbe12c7161a2dbd63f81229e

--- system/gd/common/contextual_callback.h
+++ system/gd/common/contextual_callback.h
@@ -20,6 +20,8 @@
 #include "callback.h"
 #include "i_postable_context.h"
 
+#include <bluetooth/log.h>
+
 namespace bluetooth {
 namespace common {
 
@@ -31,7 +33,9 @@ template <typename R, typename... Args>
 class ContextualOnceCallback<R(Args...)> {
 public:
   ContextualOnceCallback(common::OnceCallback<R(Args...)>&& callback, IPostableContext* context)
-      : callback_(std::move(callback)), context_(context) {}
+      : callback_(std::move(callback)), context_(context) {
+          log::assert_that(context_ != nullptr, "IPostableContext must not be null");
+      }
 
   constexpr ContextualOnceCallback() = default;
   ContextualOnceCallback(const ContextualOnceCallback&) = delete;
@@ -41,6 +45,7 @@ public:
   ContextualOnceCallback& operator=(ContextualOnceCallback&&) noexcept = default;
 
   void operator()(Args... args) {
+    log::assert_that(context_ != nullptr, "Arf, it's null.....");
     context_->Post(common::BindOnce(std::move(callback_), std::forward<Args>(args)...));
   }
 
