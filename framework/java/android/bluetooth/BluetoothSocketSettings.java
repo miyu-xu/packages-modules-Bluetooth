b3694ce8938cb13c20ff7352d3bdbe9e5d865548
Jayden Kim <jaydenk@google.com>
2024-10-31 19:09:42 +0000

    Add bluetooth offload socket parameters to settings
    Bug: 342012881
    Bug: 367419086
    Test: m -j
    Change-Id: I72a81087aa9b018c111599fe705c710df6a76b0c

--- framework/java/android/bluetooth/BluetoothSocketSettings.java
+++ framework/java/android/bluetooth/BluetoothSocketSettings.java
@@ -19,12 +19,16 @@ package android.bluetooth;
 import static android.bluetooth.BluetoothSocket.SocketType;
 
 import android.annotation.FlaggedApi;
+import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.RequiresNoPermission;
+import android.annotation.SystemApi;
 
 import com.android.bluetooth.flags.Flags;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.UUID;
 
 /**
@@ -40,6 +44,52 @@ import java.util.UUID;
 public final class BluetoothSocketSettings {
 
     private static final int L2CAP_PSM_UNSPECIFIED = -1;
+
+    /**
+     * Annotation to define the data path used for Bluetooth socket communication. This determines
+     * how data flows between the application and the Bluetooth controller.
+     *
+     * @hide
+     */
+    @IntDef(
+            prefix = {"DATA_PATH_"},
+            value = {DATA_PATH_NO_OFFLOAD, DATA_PATH_HW_OFFLOAD})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface SocketDataPath {}
+
+    /**
+     * Non-offload data path where the application's socket data is processed by the main Bluetooth
+     * stack on the host device.
+     */
+    public static final int DATA_PATH_NO_OFFLOAD = 0;
+
+    /**
+     * Hardware offload data path where the application's socket data is processed by a offloaded
+     * application running on the low-power processor.
+     */
+    public static final int DATA_PATH_HW_OFFLOAD = 1;
+
+    /**
+     * Maximum size (in bytes) of a data packet that can be received from the endpoint when using
+     * {@link #DATA_PATH_HW_OFFLOAD}.
+     */
+    public static final int PACKET_MAX_SIZE = 65535;
+
+    /**
+     * Constant representing an invalid hub ID. This value indicates that a hub ID has not been
+     * assigned or is not valid.
+     *
+     * @hide
+     */
+    private static final long INVALID_HUB_ID = 0;
+
+    /**
+     * Constant representing an invalid hub endpoint ID. This value indicates that an endpoint ID
+     * has not been assigned or is not valid.
+     *
+     * @hide
+     */
+    private static final long INVALID_ENDPOINT_ID = 0;
 
     /** Type of the Bluetooth socket */
     @SocketType private int mSocketType;
@@ -58,6 +108,48 @@ public final class BluetoothSocketSettings {
 
     /** RFCOMM service UUID associated with the Bluetooth socket. */
     private UUID mRfcommUuid;
+
+    /**
+     * Specifies the data path used for this socket, influencing how data is transmitted and
+     * processed. Select the appropriate data path based on performance and power consumption
+     * requirements:
+     *
+     * <ul>
+     *   <li>{@link #DATA_PATH_NO_OFFLOAD}: Suitable for applications that require the full
+     *       processing capabilities of the main Bluetooth stack.
+     *   <li>{@link #DATA_PATH_HW_OFFLOAD}: Optimized for lower power consumption by utilizing an
+     *       offloaded application running on a dedicated low-power processor.
+     * </ul>
+     */
+    @SocketDataPath private int mDataPath;
+
+    /**
+     * A user-friendly name for this socket, primarily for debugging and logging. This name should
+     * be descriptive and can help identify the socket during development and troubleshooting.
+     *
+     * <p>When using {@link #DATA_PATH_HW_OFFLOAD}, this name is also passed to the offloaded
+     * application running on the low-power processor. This allows the offloaded application to
+     * identify and manage the socket.
+     */
+    private String mSocketName;
+
+    /**
+     * When using {@link #DATA_PATH_HW_OFFLOAD}, this identifies the Hub to which the endpoint
+     * belongs.
+     */
+    private long mHubId;
+
+    /**
+     * When using {@link #DATA_PATH_HW_OFFLOAD}, this identifies the specific endpoint within the
+     * hub that is associated with this socket.
+     */
+    private long mEndpointId;
+
+    /**
+     * The maximum size (in bytes) of a single data packet that can be received from the endpoint
+     * when using {@link #DATA_PATH_HW_OFFLOAD}.
+     */
+    private int mMaximumPacketSize;
 
     /**
      * Returns the type of the Bluetooth socket.
@@ -119,35 +211,134 @@ public final class BluetoothSocketSettings {
     }
 
     /**
+     * Returns the data path used for this socket. The data path determines how data is routed and
+     * processed for the socket connection.
+     *
+     * <p>Possible values are:
+     *
+     * <ul>
+     *   <li>{@link #DATA_PATH_NO_OFFLOAD}: Data is processed by the main Bluetooth stack on the
+     *       host device.
+     *   <li>{@link #DATA_PATH_HW_OFFLOAD}: Data is processed by an offloaded application running on
+     *       the low-power processor.
+     * </ul>
+     *
+     * <p>Defaults to {@link #DATA_PATH_NO_OFFLOAD}.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+    @RequiresNoPermission
+    public @SocketDataPath int getDataPath() {
+        return mDataPath;
+    }
+
+    /**
+     * Returns the user-friendly name assigned to this socket. This name is primarily used for
+     * debugging and logging purposes.
+     *
+     * <p>When using {@link #DATA_PATH_HW_OFFLOAD}, this name is also passed to the offloaded
+     * application running on the low-power processor.
+     *
+     * <p>Defaults to {@code null} if no name was explicitly set.
+     */
+    @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+    @Nullable
+    @RequiresNoPermission
+    public String getSocketName() {
+        return mSocketName;
+    }
+
+    /**
+     * Returns the ID of the Hub associated with this socket when using {@link
+     * #DATA_PATH_HW_OFFLOAD}.
+     *
+     * <p>If the data path is not set to {@link #DATA_PATH_HW_OFFLOAD}, this method returns {@link
+     * #INVALID_HUB_ID}.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+    @RequiresNoPermission
+    public long getHubId() {
+        if (mDataPath != DATA_PATH_HW_OFFLOAD) {
+            return INVALID_HUB_ID;
+        }
+        return mHubId;
+    }
+
+    /**
+     * Returns the ID of the endpoint within the Hub associated with this socket when using {@link
+     * #DATA_PATH_HW_OFFLOAD}. An endpoint represents a specific point of communication within the
+     * hub.
+     *
+     * <p>If the data path is not set to {@link #DATA_PATH_HW_OFFLOAD}, this method returns {@link
+     * #INVALID_ENDPOINT_ID}.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+    @RequiresNoPermission
+    public long getEndpointId() {
+        if (mDataPath != DATA_PATH_HW_OFFLOAD) {
+            return INVALID_ENDPOINT_ID;
+        }
+        return mEndpointId;
+    }
+
+    /**
+     * Returns the maximum size (in bytes) of a data packet that can be received from the endpoint
+     * associated with this socket when using {@link #DATA_PATH_HW_OFFLOAD}.
+     *
+     * <p>Defaults to {@link #PACKET_MAX_SIZE}.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+    @RequiresNoPermission
+    public int getMaximumPacketSize() {
+        return mMaximumPacketSize;
+    }
+
+    /**
      * Returns a {@link String} that describes each BluetoothSocketSettings parameter current value.
      */
     @Override
     public String toString() {
+        StringBuilder builder = new StringBuilder("BluetoothSocketSettings{");
+        builder.append("mSocketType=");
+        builder.append(mSocketType);
+        builder.append(", mEncryptionRequired=");
+        builder.append(mEncryptionRequired);
+        builder.append(", mAuthenticationRequired=");
+        builder.append(mAuthenticationRequired);
+        builder.append(", mSocketName=");
+        builder.append(mSocketName);
         if (mSocketType == BluetoothSocket.TYPE_RFCOMM) {
-            return "BluetoothSocketSettings{"
-                    + "mSocketType="
-                    + mSocketType
-                    + ", mEncryptionRequired="
-                    + mEncryptionRequired
-                    + ", mAuthenticationRequired="
-                    + mAuthenticationRequired
-                    + ", mRfcommServiceName="
-                    + mRfcommServiceName
-                    + ", mRfcommUuid="
-                    + mRfcommUuid
-                    + "}";
+            builder.append(", mRfcommServiceName=");
+            builder.append(mRfcommServiceName);
+            builder.append(", mRfcommUuid=");
+            builder.append(mRfcommUuid);
         } else {
-            return "BluetoothSocketSettings{"
-                    + "mSocketType="
-                    + mSocketType
-                    + ", mL2capPsm="
-                    + mL2capPsm
-                    + ", mEncryptionRequired="
-                    + mEncryptionRequired
-                    + ", mAuthenticationRequired="
-                    + mAuthenticationRequired
-                    + "}";
+            builder.append(", mL2capPsm=");
+            builder.append(mL2capPsm);
         }
+        if (mDataPath == DATA_PATH_HW_OFFLOAD) {
+            builder.append(", mDataPath=");
+            builder.append(mDataPath);
+            builder.append(", mHubId=");
+            builder.append(mHubId);
+            builder.append(", mEndpointId=");
+            builder.append(mEndpointId);
+            builder.append(", mMaximumPacketSize=");
+            builder.append(mMaximumPacketSize);
+        }
+        builder.append("}");
+        return builder.toString();
     }
 
     private BluetoothSocketSettings(
@@ -156,13 +347,23 @@ public final class BluetoothSocketSettings {
             boolean encryptionRequired,
             boolean authenticationRequired,
             String rfcommServiceName,
-            UUID rfcommUuid) {
+            UUID rfcommUuid,
+            int dataPath,
+            String socketName,
+            long hubId,
+            long endpointId,
+            int maximumPacketSize) {
         mSocketType = socketType;
         mL2capPsm = l2capPsm;
         mEncryptionRequired = encryptionRequired;
         mAuthenticationRequired = authenticationRequired;
         mRfcommUuid = rfcommUuid;
         mRfcommServiceName = rfcommServiceName;
+        mDataPath = dataPath;
+        mSocketName = socketName;
+        mHubId = hubId;
+        mEndpointId = endpointId;
+        mMaximumPacketSize = maximumPacketSize;
     }
 
     /** Builder for {@link BluetoothSocketSettings}. */
@@ -174,6 +375,11 @@ public final class BluetoothSocketSettings {
         private boolean mAuthenticationRequired = false;
         private String mRfcommServiceName = null;
         private UUID mRfcommUuid = null;
+        private int mDataPath = DATA_PATH_NO_OFFLOAD;
+        private String mSocketName = null;
+        private long mHubId = INVALID_HUB_ID;
+        private long mEndpointId = INVALID_ENDPOINT_ID;
+        private int mMaximumPacketSize = PACKET_MAX_SIZE;
 
         public Builder() {}
 
@@ -292,20 +498,157 @@ public final class BluetoothSocketSettings {
         }
 
         /**
+         * Set the socket data path. If used to set data path anything other than {@link
+         * #DATA_PATH_NO_OFFLOAD}, then it will require BLUETOOTH_PRIVILEGED permission and will be
+         * checked at the time of creating socket connection or channel.
+         *
+         * @param dataPath The socket data path
+         * @throws IllegalArgumentException If the {@code dataPath} is invalid.
+         * @hide
+         */
+
+        /**
+         * Sets the data path for this socket. The data path determines how data is routed and
+         * processed for the socket connection.
+         *
+         * <p>Valid data paths are:
+         *
+         * <ul>
+         *   <li>{@link #DATA_PATH_NO_OFFLOAD}: Data is processed by the main Bluetooth stack on the
+         *       host device. This is the default data path.
+         *   <li>{@link #DATA_PATH_HW_OFFLOAD}: Data is processed by a offloaded application on the
+         *       low-power processor. Using this data path requires the {@code BLUETOOTH_PRIVILEGED}
+         *       permission, which will be checked when a socket connection or channel is created.
+         * </ul>
+         *
+         * @param dataPath The desired data path for the socket.
+         * @return This Builder object to allow for method chaining.
+         * @throws IllegalArgumentException If {@code dataPath} is an invalid value.
+         * @hide
+         */
+        @SystemApi
+        @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+        @NonNull
+        @RequiresNoPermission
+        public Builder setDataPath(@SocketDataPath int dataPath) {
+            if (dataPath < DATA_PATH_NO_OFFLOAD || dataPath > DATA_PATH_HW_OFFLOAD) {
+                throw new IllegalArgumentException("invalid dataPath - " + dataPath);
+            }
+            mDataPath = dataPath;
+            return this;
+        }
+
+        /**
+         * Sets a user-friendly name for this socket. This name is primarily used for debugging and
+         * logging purposes.
+         *
+         * <p>When using {@link #DATA_PATH_HW_OFFLOAD}, this name is also passed to the offloaded
+         * application running on low-power processor.
+         *
+         * @param socketName The desired name for the socket. This should be a descriptive name that
+         *     helps identify the socket during development and troubleshooting.
+         * @return This Builder object to allow for method chaining.
+         */
+        @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+        @NonNull
+        @RequiresNoPermission
+        public Builder setSocketName(@NonNull String socketName) {
+            mSocketName = socketName;
+            return this;
+        }
+
+        /**
+         * Sets the ID of the Hub to be associated with this socket when using {@link
+         * #DATA_PATH_HW_OFFLOAD}.
+         *
+         * @param hubId The ID of the Hub.
+         * @return This Builder object to allow for method chaining.
+         * @hide
+         */
+        @SystemApi
+        @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+        @NonNull
+        @RequiresNoPermission
+        public Builder setHubId(long hubId) {
+            mHubId = hubId;
+            return this;
+        }
+
+        /**
+         * Sets the ID of the endpoint within the Hub to be associated with this socket when using
+         * {@link #DATA_PATH_HW_OFFLOAD}. An endpoint represents a specific point of communication
+         * within the hub.
+         *
+         * @param endpointId The ID of the endpoint within the Hub.
+         * @return This Builder object to allow for method chaining.
+         * @hide
+         */
+        @SystemApi
+        @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+        @NonNull
+        @RequiresNoPermission
+        public Builder setEndpointId(long endpointId) {
+            mEndpointId = endpointId;
+            return this;
+        }
+
+        /**
+         * Sets the maximum size (in bytes) of a data packet that can be received from the endpoint
+         * associated with this socket when using {@link #DATA_PATH_HW_OFFLOAD}.
+         *
+         * <p>The Bluetooth host stack may adjust this value based on the actual capabilities
+         * negotiated with the peer device during connection establishment. To get the final
+         * negotiated value, use {@link BluetoothSocket#getMaxReceivePacketSize()} after the socket
+         * is connected.
+         *
+         * @param maximumPacketSize The maximum packet size in bytes.
+         * @return This Builder object to allow for method chaining.
+         * @hide
+         */
+        @SystemApi
+        @FlaggedApi(Flags.FLAG_BT_OFFLOAD_SOCKET_API)
+        @NonNull
+        @RequiresNoPermission
+        public Builder setMaximumPacketSize(int maximumPacketSize) {
+            mMaximumPacketSize = maximumPacketSize;
+            return this;
+        }
+
+        /**
          * Builds a {@link BluetoothSocketSettings} object.
          *
          * @return A new {@link BluetoothSocketSettings} object with the configured parameters.
+         * @throws IllegalArgumentException if the settings are invalid and cannot be built.
          */
         @NonNull
         @RequiresNoPermission
         public BluetoothSocketSettings build() {
+            if (mDataPath == DATA_PATH_HW_OFFLOAD) {
+                if (mHubId == INVALID_HUB_ID || mEndpointId == INVALID_ENDPOINT_ID) {
+                    throw new IllegalArgumentException(
+                            "Hub ID and endpoint ID must be set for hardware data path");
+                }
+                if (mMaximumPacketSize < 0 || mMaximumPacketSize > PACKET_MAX_SIZE) {
+                    throw new IllegalArgumentException("invalid packet size " + mMaximumPacketSize);
+                }
+            } else {
+                if (mHubId != INVALID_HUB_ID || mEndpointId != INVALID_ENDPOINT_ID) {
+                    throw new IllegalArgumentException(
+                            "Hub ID and endpoint ID may not be set for software data path");
+                }
+            }
             return new BluetoothSocketSettings(
                     mSocketType,
                     mL2capPsm,
                     mEncryptionRequired,
                     mAuthenticationRequired,
                     mRfcommServiceName,
-                    mRfcommUuid);
+                    mRfcommUuid,
+                    mDataPath,
+                    mSocketName,
+                    mHubId,
+                    mEndpointId,
+                    mMaximumPacketSize);
         }
     }
 }
