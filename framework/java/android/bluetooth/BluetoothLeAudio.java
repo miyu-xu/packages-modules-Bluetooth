a056bfc0ff9e7fe551960776e041d9a9ed15ebab
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2024-10-31 13:29:01 +0000

    le_audio: Introduce setPrimaryGroup API
    This CL extends BluetoothLeAudio API with possibility to set primary
    group. Contept of primary group represents a fallback group while being
    in Broadcast mode or active group when active.
    
    Bug: 375421718
    Bug: 375422410
    Flag: com.android.bluetooth.flags.leaudio_broadcast_api_manage_primary_group
    Test: m com.android.btservices
    Change-Id: I435c5bcc7044c7142b3f708d3104e84c5f38552f

--- framework/java/android/bluetooth/BluetoothLeAudio.java
+++ framework/java/android/bluetooth/BluetoothLeAudio.java
@@ -1404,4 +1404,38 @@ public final class BluetoothLeAudio implements BluetoothProfile, AutoCloseable {
             }
         }
     }
+
+    /**
+     * Set primary group
+     *
+     * <p>Primary group represents fallback group for broadcast handover case (Unicast is not active
+     * and Broadcast is) and active group when Unicast is active.
+     *
+     * <p>On primary group changed, {@link Callback#onLeAudioPrimaryGroupChanged} will be invoked.
+     *
+     * @param groupId the groupId
+     * @throws IllegalStateException if LE Audio Service is null
+     * @hide
+     */
+    @FlaggedApi(Flags.FLAG_LEAUDIO_BROADCAST_API_MANAGE_PRIMARY_GROUP)
+    @SystemApi
+    @RequiresBluetoothConnectPermission
+    @RequiresPermission(allOf = {BLUETOOTH_CONNECT, BLUETOOTH_PRIVILEGED})
+    public void setPrimaryGroup(int groupId) {
+        if (DBG) Log.d(TAG, "setPrimaryGroup(" + groupId + ")");
+
+        final IBluetoothLeAudio service = getService();
+
+        if (service == null) {
+            Log.w(TAG, "Proxy not attached to service");
+            if (DBG) log(Log.getStackTraceString(new Throwable()));
+            throw new IllegalStateException("Service is unavailable");
+        } else if (mAdapter.isEnabled()) {
+            try {
+                service.setPrimaryGroup(groupId, mAttributionSource);
+            } catch (RemoteException e) {
+                Log.e(TAG, e.toString() + "\n" + Log.getStackTraceString(new Throwable()));
+            }
+        }
+    }
 }
