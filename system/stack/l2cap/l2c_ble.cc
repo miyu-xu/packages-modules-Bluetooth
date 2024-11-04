7b87e2cfbce8e7048403aebe4a9db7c24ba029c0
Hyundo Moon <hdmoon@google.com>
2024-11-04 20:10:53 +0900

    Use aggressive initial connection params
    This CL makes the stack use aggressive initial connection
    parameters when there are less than 2 ongoing ACL connections.
    
    TODO: Why the test build fails with undefined symbol?
    - Why cannot gd unit test find the definitions in stack/?
    TODO: Modify tests with the new threshold system property
    
    Bug: 308483803
    Bug: 378595485
    Flag: com.android.bluetooth.flags.initial_conn_params_p1
    Test: atest LeImplTest
    Test: atest GattClientTest
    Test: Manual, checked aggressive params are used for first connection,
          and relaxed params are used from the third connection.
    Test: Manual, checked that paramters are relaxed after service
          discovery. Also for the case when the service discovery is
          skipped.
    Test: Manual, made GATT connection while music is playing via A2DP/ASHA.
          The music was continously playing while service discovery.
    Test: Manual, had a Wifi traffic and phone call with HFP/ASHA.
          The phone call has no noticable sound errors.
    Change-Id: Idd8cac01f67e4dd797487fad87781fddb5ce73d3

--- system/stack/l2cap/l2c_ble.cc
+++ system/stack/l2cap/l2c_ble.cc
@@ -57,6 +57,7 @@
 #include "stack/include/l2cap_hci_link_interface.h"
 #include "stack/include/l2cap_interface.h"
 #include "stack/include/l2cdefs.h"
+#include "stack/include/le_conn_params.h"
 #include "stack/include/main_thread.h"
 #include "stack/l2cap/l2c_int.h"
 #include "types/raw_address.h"
@@ -177,6 +178,22 @@ bool l2cble_conn_comp(uint16_t handle, tHCI_ROLE role, const RawAddress& bda,
   p_lcb->timeout = conn_timeout;
   p_lcb->latency = conn_latency;
   p_lcb->conn_update_mask = L2C_BLE_NOT_DEFAULT_PARAM;
+  if (com::android::bluetooth::flags::initial_conn_params_p1()) {
+    uint16_t min_conn_interval_aggressive = LeConnectionParameters::GetMinConnIntervalAggressive();
+    uint16_t max_conn_interval_aggressive = LeConnectionParameters::GetMaxConnIntervalAggressive();
+
+    stack::l2cap::get_interface().L2CA_AdjustConnectionIntervals(
+            &min_conn_interval_aggressive, &max_conn_interval_aggressive, BTM_BLE_CONN_INT_MIN);
+
+    bool is_aggressive_initial_param = conn_interval <= max_conn_interval_aggressive;
+    log::info("conn_interval={}, max_conn_interval_aggressive={}, is_aggressive_initial_param={}",
+              conn_interval, max_conn_interval_aggressive, is_aggressive_initial_param);
+
+    if (is_aggressive_initial_param) {
+      p_lcb->conn_update_mask |= L2C_BLE_AGGRESSIVE_INITIAL_PARAM;
+    }
+  }
+
   p_lcb->conn_update_blocked_by_profile_connection = false;
   p_lcb->conn_update_blocked_by_service_discovery = false;
 
@@ -328,6 +345,9 @@ void l2cble_process_sig_cmd(tL2C_LCB* p_lcb, uint8_t* p, uint16_t pkt_len) {
           p_lcb->latency = latency;
           p_lcb->timeout = timeout;
           p_lcb->conn_update_mask |= L2C_BLE_NEW_CONN_PARAM;
+          if (com::android::bluetooth::flags::initial_conn_params_p1()) {
+            p_lcb->conn_update_mask &= ~L2C_BLE_AGGRESSIVE_INITIAL_PARAM;
+          }
 
           l2cble_start_conn_update(p_lcb);
         }
