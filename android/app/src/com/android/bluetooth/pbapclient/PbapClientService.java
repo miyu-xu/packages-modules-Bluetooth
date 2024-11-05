ca828a0512dae2d5d8e99d266cd680ca52fac4ec
Sal Savage <salsavage@google.com>
2024-11-04 16:08:34 -0800

    Storage Refactor: Introduce PbapClientContactsStorage
    This change combines the concepts of our Account Manager and Storage
    interfaces (not currently centralized) into a single storage abstraction
    called PbapClientContactsStorage.
    
    This object owns storage accounts (a wrapper on Account Manager
    accounts), storage interfaces, intialization (clearing of stale accounts
    and data, etc.), and clean up (final bulk deletion of data on stop).
    
    It's to be owned by PbapClientService, who will start and stop it (i.e.
    own the lifecycle). State Machines will use a reference to this object
    to create accounts, get notified when their account is ready, store
    download results, and clean up contacts and call history upon
    disconnect. This will come in a future change with a new State Machine
    
    Bug: 376461939
    Bug: 365626536
    Test: atest com.android.bluetooth.pbapclient
    Change-Id: Ibd14f91ca5f5e85b9ab78dbb61b2ab175f73a692

--- android/app/src/com/android/bluetooth/pbapclient/PbapClientService.java
+++ android/app/src/com/android/bluetooth/pbapclient/PbapClientService.java
@@ -35,6 +35,7 @@ import com.android.bluetooth.BluetoothMethodProxy;
 import com.android.bluetooth.btservice.AdapterService;
 import com.android.bluetooth.btservice.ProfileService;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
+import com.android.bluetooth.flags.Flags;
 import com.android.bluetooth.hfpclient.HfpClientConnectionService;
 import com.android.bluetooth.sdp.SdpManagerNativeInterface;
 import com.android.internal.annotations.VisibleForTesting;
@@ -63,6 +64,7 @@ public class PbapClientService extends ProfileService {
             new ConcurrentHashMap<>();
 
     private static PbapClientService sPbapClientService;
+    private final PbapClientContactsStorage mPbapClientContactsStorage;
     private final PbapClientAccountManager mPbapClientAccountManager;
     private int mSdpHandle = -1;
     private DatabaseManager mDatabaseManager;
@@ -83,14 +85,21 @@ public class PbapClientService extends ProfileService {
 
     public PbapClientService(Context context) {
         super(context);
-        mPbapClientAccountManager =
-                new PbapClientAccountManager(context, new PbapClientAccountManagerCallback());
+        if (Flags.pbapClientStorageRefactor()) {
+            mPbapClientContactsStorage = new PbapClientContactsStorage(context);
+            mPbapClientAccountManager = null;
+        } else {
+            mPbapClientAccountManager =
+                    new PbapClientAccountManager(context, new PbapClientAccountManagerCallback());
+            mPbapClientContactsStorage = null;
+        }
     }
 
     @VisibleForTesting
     PbapClientService(Context context, PbapClientAccountManager accountManager) {
         super(context);
         mPbapClientAccountManager = accountManager;
+        mPbapClientContactsStorage = null;
     }
 
     public static boolean isEnabled() {
@@ -115,7 +124,11 @@ public class PbapClientService extends ProfileService {
 
         mHandler = new Handler(Looper.getMainLooper());
 
-        mPbapClientAccountManager.start();
+        if (Flags.pbapClientStorageRefactor()) {
+            mPbapClientContactsStorage.start();
+        } else {
+            mPbapClientAccountManager.start();
+        }
 
         registerSdpRecord();
         setPbapClientService(this);
@@ -137,8 +150,12 @@ public class PbapClientService extends ProfileService {
             mHandler = null;
         }
 
-        removeUncleanAccounts();
-        mPbapClientAccountManager.stop();
+        if (Flags.pbapClientStorageRefactor()) {
+            mPbapClientContactsStorage.stop();
+        } else {
+            removeUncleanAccounts();
+            mPbapClientAccountManager.stop();
+        }
 
         setComponentAvailable(AUTHENTICATOR_SERVICE, false);
     }
@@ -204,6 +221,11 @@ public class PbapClientService extends ProfileService {
      * separately clean up the call log data associated with a given account too.
      */
     private void removeUncleanAccounts() {
+        if (Flags.pbapClientStorageRefactor()) {
+            Log.i(TAG, "removeUncleanAccounts: this is the responsibility of contacts storage");
+            return;
+        }
+
         List<Account> accounts = mPbapClientAccountManager.getAccounts();
         Log.i(TAG, "removeUncleanAccounts: Found " + accounts.size() + " accounts");
 
@@ -269,6 +291,9 @@ public class PbapClientService extends ProfileService {
      * @return True is account type is ready, false otherwise
      */
     public boolean isAccountTypeReady() {
+        if (Flags.pbapClientStorageRefactor()) {
+            throw new UnsupportedOperationException("This is not needed with contacts storage");
+        }
         return mPbapClientAccountManager.isAccountTypeInitialized();
     }
 
@@ -283,7 +308,12 @@ public class PbapClientService extends ProfileService {
         for (PbapClientStateMachineOld smOld : mPbapClientStateMachineOldMap.values()) {
             smOld.dump(sb);
         }
-        ProfileService.println(sb, mPbapClientAccountManager.dump());
+
+        if (Flags.pbapClientStorageRefactor()) {
+            ProfileService.println(sb, mPbapClientContactsStorage.dump());
+        } else {
+            ProfileService.println(sb, mPbapClientAccountManager.dump());
+        }
     }
 
     // *********************************************************************************************
