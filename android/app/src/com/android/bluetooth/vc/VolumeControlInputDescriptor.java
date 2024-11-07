8ee962d119490905510c0e32330d382cdf5e2403
William Escande <wescande@google.com>
2024-11-06 22:12:14 -0800

    AICS: Gain Setting Properties implementation
    This relate to:
    * gainSettingUnit
    * gainSettingMax
    * gainSettingMin
    
    Bug: 372328699
    Flag: com.android.bluetooth.flags.aics_api
    Test: atest CtsBluetoothTestCases
    Change-Id: I90503b73a8202cfd1dfd46f6d633c26c770af725

--- android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
+++ android/app/src/com/android/bluetooth/vc/VolumeControlInputDescriptor.java
@@ -45,7 +45,7 @@ class VolumeControlInputDescriptor {
         mNativeInterface = requireNonNull(nativeInterface);
         mDevice = requireNonNull(device);
         mVolumeInputs = new Descriptor[numberOfExternalInputs];
-        // Stack delivers us number of audio inputs. ids are countinous from [0;n[
+        // Stack delivers us number of AICSs. ids are countinous from [0;n[
         for (int i = 0; i < numberOfExternalInputs; i++) {
             mVolumeInputs[i] = new Descriptor();
         }
@@ -174,6 +174,21 @@ class VolumeControlInputDescriptor {
         mVolumeInputs[id].mGainSettingsUnits = gainUnit;
         mVolumeInputs[id].mGainSettingsMin = gainMin;
         mVolumeInputs[id].mGainSettingsMax = gainMax;
+    }
+
+    int getGainSettingUnit(int id) {
+        if (!isValidId(id)) return 0;
+        return mVolumeInputs[id].mGainSettingsUnits;
+    }
+
+    int getGainSettingMin(int id) {
+        if (!isValidId(id)) return 0;
+        return mVolumeInputs[id].mGainSettingsMin;
+    }
+
+    int getGainSettingMax(int id) {
+        if (!isValidId(id)) return 0;
+        return mVolumeInputs[id].mGainSettingsMax;
     }
 
     void onStateChanged(int id, int gainSetting, @Mute int mute, @GainMode int gainMode) {
