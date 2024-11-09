fe2fcb1c513ebd31f9b3efe2021150b3880fe550
cmanton@google.com <cmanton@google.com>
2024-11-09 09:16:50 -0800

    pbapclient:: Check SDP record callback status for validity
    Provide additional layer of indirection within statemachine
    when expected SDP completes and proceed accordingly based
    upon SDP status.
    
    Bug: 374811645
    Test: m com.android.btservices
    Flag: EXEMPT, Bug fix
    Change-Id: I03c8aa57f28ddbed4fc5c07d3807739706b3732b

--- android/app/src/com/android/bluetooth/pbapclient/PbapClientConnectionHandler.java
+++ android/app/src/com/android/bluetooth/pbapclient/PbapClientConnectionHandler.java
@@ -198,7 +198,10 @@ class PbapClientConnectionHandler extends Handler {
         Log.d(TAG, "Handling Message = " + msg.what);
         switch (msg.what) {
             case MSG_CONNECT:
-                mPseRec = (SdpPseRecord) msg.obj;
+                setPseRecord((SdpPseRecord) msg.obj);
+                if (mPseRec == null) {
+                    Log.i(TAG, "SDP PBAP record is missing");
+                }
                 /* To establish a connection, first open a socket and then create an OBEX session */
                 if (connectSocket()) {
                     Log.d(TAG, "Socket connected");
