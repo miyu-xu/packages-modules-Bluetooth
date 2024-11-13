a1279743feb20dd2ac7cc96302b1decbf211be4d
Andrew Cheng <chengandrew@google.com>
2024-11-13 15:48:58 -0800

    New config for enabling LE Audio profiles
    Differentiate between "Use Case Profiles" (e.g., TMAP Unicast Media
    Sender) vs "Framework Profiles" (e.g., BAP Unicast Client or CSIP Set
    Coordinator).
    
    Replace "Framework Profile" sysprops with higher level sysprops
    corresponding to "Use Case Profiles". In other words, "Framework
    Profiles" are no longer directly enabled by sysprops, and "Use Case"
    functionality is not enabled by enabling the correct combination of
    "Framework Profiles".
    
    Rather, enablement flows in the other direction: "Use Case Profiles" are
    directly enabled by sysprops, and "Framework Profiles" are enabled as a
    function of the "Use Case Profiles" that have been abled, in accordance
    with the profile dependencies described in the specs.
    
    Bug: 369150497
    Bug: 379178585
    Test: atest LeAudioProfileConfigTest
    Flag: leaudio_config_profile_enabling
    Change-Id: I8c12959be47d4a83c562436fe238e1fbb1e7f027

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -28,6 +28,7 @@ import static com.android.bluetooth.flags.Flags.leaudioBigDependsOnAudioState;
 import static com.android.bluetooth.flags.Flags.leaudioBroadcastApiGetLocalMetadata;
 import static com.android.bluetooth.flags.Flags.leaudioBroadcastPreventResumeInterruption;
 import static com.android.bluetooth.flags.Flags.leaudioBroadcastResyncHelper;
+import static com.android.bluetooth.flags.Flags.leaudioConfigProfileEnabling;
 import static com.android.bluetooth.flags.Flags.leaudioMonitorUnicastSourceWhenManagedByBroadcastDelegator;
 import static com.android.bluetooth.flags.Flags.leaudioSortScansToSyncByFails;
 
@@ -78,6 +79,7 @@ import com.android.bluetooth.btservice.ProfileService;
 import com.android.bluetooth.btservice.ServiceFactory;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
 import com.android.bluetooth.csip.CsipSetCoordinatorService;
+import com.android.bluetooth.le_audio.LeAudioProfileConfig;
 import com.android.bluetooth.le_audio.LeAudioService;
 import com.android.bluetooth.le_scan.ScanController;
 import com.android.internal.annotations.GuardedBy;
@@ -518,7 +520,10 @@ public class BassClientService extends ProfileService {
     }
 
     public static boolean isEnabled() {
-        return BluetoothProperties.isProfileBapBroadcastAssistEnabled().orElse(false);
+        if (!leaudioConfigProfileEnabling()) {
+            return BluetoothProperties.isProfileBapBroadcastAssistEnabled().orElse(false);
+        }
+        return LeAudioProfileConfig.isBapBroadcastAssistantEnabled();
     }
 
     private static class SourceSyncRequest {
