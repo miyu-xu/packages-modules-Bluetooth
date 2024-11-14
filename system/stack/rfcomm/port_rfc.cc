73fae6a10294f595c1fb8177caf756676bd2d835
Pomai Ahlo <poahlo@google.com>
2024-11-14 10:58:05 -0800

    RFCOMM Metrics v2: logging metrics
    Implement the collection of RFCOMM metrics v2
    
    Bug: 374989690
    Test: m com.android.btservices && manual using Atom Tester
    Flag: EXEMPT (metrics)
    Change-Id: I8bd4b639ac81aa5b9ba5cc9b494c40338aded59f

--- system/stack/rfcomm/port_rfc.cc
+++ system/stack/rfcomm/port_rfc.cc
@@ -40,6 +40,7 @@
 #include "osi/include/mutex.h"
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_uuid16.h"
+#include "stack/include/rfc_metrics.h"
 #include "stack/include/stack_metrics_logging.h"
 #include "stack/l2cap/l2c_int.h"
 #include "stack/rfcomm/port_int.h"
@@ -1045,6 +1046,7 @@ void port_rfc_closed(tPORT* p_port, uint8_t res) {
 
   rfc_set_state(RFC_STATE_CLOSED, p_port);
   p_port->rfc.sm_cb.close_reason = static_cast<tPORT_RESULT>(res);
+  port_collect_attempt_metrics(p_port->rfc.sm_cb, p_port->app_uid);
   log::info(
           "RFCOMM connection closed, index={}, state={}, reason={}[{}], "
           "UUID=0x{:x}, bd_addr={}, is_server={}",
