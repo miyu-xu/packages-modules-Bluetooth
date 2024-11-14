1f55968da3ed5e5b8b8a9a0dfc43fee87eb69dc8
Ömer Faruk Yılmaz <ofy@google.com>
2024-11-14 03:33:45 +0000

    Add new API `getIdentityAddressType` to BluetoothDevice.java
    Bug: 377171798
    Bug: 373899888
    Test: m com.android.btservices
    Change-Id: I48ddc08c40350bbd1050d53c0da0a23c978558b0

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -2395,6 +2395,27 @@ public class AdapterService extends Service {
         }
 
         @Override
+        @NonNull
+        public Bundle getIdentityAddressWithType(@NonNull String address) {
+            AdapterService service = getService();
+            if (service == null
+                    || !callerIsSystemOrActiveOrManagedUser(
+                            service, TAG, "getIdentityAddressWithType")
+                    || !Utils.checkConnectPermissionForDataDelivery(
+                            service,
+                            Utils.getCallingAttributionSource(mService),
+                            "AdapterService getIdentityAddressWithType")) {
+                Bundle identityAddressWithType = new Bundle();
+                identityAddressWithType.putInt(
+                        BluetoothDevice.IDENTITY_ADDRESS_TYPE,
+                        BluetoothDevice.ADDRESS_TYPE_UNKNOWN);
+                return identityAddressWithType;
+            }
+            service.enforceCallingOrSelfPermission(BLUETOOTH_PRIVILEGED, null);
+            return service.getIdentityAddressWithType(address);
+        }
+
+        @Override
         public String getName(AttributionSource source) {
             AdapterService service = getService();
             if (service == null
@@ -4900,6 +4921,40 @@ public class AdapterService extends Service {
                 return address;
             }
         }
+    }
+
+    /**
+     * Returns the identity address and identity address type.
+     *
+     * @param address of remote device
+     * @return a Bundle containing identity address and identity address type
+     */
+    @NonNull
+    public Bundle getIdentityAddressWithType(@NonNull String address) {
+        BluetoothDevice device =
+                BluetoothAdapter.getDefaultAdapter().getRemoteDevice(Ascii.toUpperCase(address));
+        DeviceProperties deviceProp = mRemoteDevices.getDeviceProperties(device);
+
+        String identityAddress = null;
+        int identityAddressType = BluetoothDevice.ADDRESS_TYPE_UNKNOWN;
+
+        if (deviceProp != null) {
+            if (deviceProp.getIdentityAddress() != null) {
+                identityAddress = deviceProp.getIdentityAddress();
+            }
+            identityAddressType = deviceProp.getIdentityAddressType();
+        } else {
+            if (Flags.identityAddressNullIfNotKnown()) {
+                identityAddress = null;
+            } else {
+                identityAddress = address;
+            }
+        }
+
+        Bundle identityAddressWithType = new Bundle();
+        identityAddressWithType.putString(BluetoothDevice.IDENTITY_ADDRESS, identityAddress);
+        identityAddressWithType.putInt(BluetoothDevice.IDENTITY_ADDRESS_TYPE, identityAddressType);
+        return identityAddressWithType;
     }
 
     private static class CallerInfo {
