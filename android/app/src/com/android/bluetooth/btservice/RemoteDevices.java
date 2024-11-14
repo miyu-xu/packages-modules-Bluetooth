c0acf62fd058110fd4d8737b66ba47c3281b820d
Ömer Faruk Yılmaz <ofy@google.com>
2024-11-14 03:13:23 +0000

    Add `identity_address_type` to `leAddressAssociateCallback`
    Bug: 377171798
    Bug: 226253021
    Test: m com.android.btservices
    Change-Id: I104bc3db91a97e83f6190a34dafed2881e5d5736

--- android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
+++ android/app/src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -26,6 +26,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothAssignedNumbers;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothDevice.AddressType;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
@@ -322,6 +323,7 @@ public class RemoteDevices {
         private String mName;
         private byte[] mAddress;
         private String mIdentityAddress;
+        @AddressType private int mIdentityAddressType = BluetoothDevice.ADDRESS_TYPE_UNKNOWN;
         private boolean mIsConsolidated = false;
         private int mBluetoothClass = BluetoothClass.Device.Major.UNCATEGORIZED;
         private int mBredrConnectionHandle = BluetoothDevice.ERROR;
@@ -378,6 +380,25 @@ public class RemoteDevices {
         void setIdentityAddress(String identityAddress) {
             synchronized (mObject) {
                 this.mIdentityAddress = identityAddress;
+            }
+        }
+
+        /**
+         * @return the mIdentityAddressType
+         */
+        @AddressType
+        int getIdentityAddressType() {
+            synchronized (mObject) {
+                return mIdentityAddressType;
+            }
+        }
+
+        /**
+         * @param identityAddressType the mIdentityAddressType to set
+         */
+        void setIdentityAddressType(int identityAddressType) {
+            synchronized (mObject) {
+                this.mIdentityAddressType = identityAddressType;
             }
         }
 
@@ -1146,19 +1167,24 @@ public class RemoteDevices {
     }
 
     /**
-     * Callback to associate an LE-only device's RPA with its identity address
+     * Callback to associate an LE-only device's RPA with its identity address and identity address
+     * type
      *
      * @param mainAddress the device's RPA
      * @param secondaryAddress the device's identity address
+     * @param identityAddressType the device's identity address type
      */
-    void leAddressAssociateCallback(byte[] mainAddress, byte[] secondaryAddress) {
+    void leAddressAssociateCallback(
+            byte[] mainAddress, byte[] secondaryAddress, int identityAddressType) {
         BluetoothDevice device = getDevice(mainAddress);
         if (device == null) {
             errorLog(
                     "leAddressAssociateCallback: device is NULL, address="
                             + Utils.getRedactedAddressStringFromByte(mainAddress)
                             + ", secondaryAddress="
-                            + Utils.getRedactedAddressStringFromByte(secondaryAddress));
+                            + Utils.getRedactedAddressStringFromByte(secondaryAddress)
+                            + ", identityAddressType="
+                            + identityAddressType);
             return;
         }
         Log.d(
@@ -1166,10 +1192,13 @@ public class RemoteDevices {
                 "leAddressAssociateCallback device: "
                         + device
                         + ", secondaryAddress:"
-                        + Utils.getRedactedAddressStringFromByte(secondaryAddress));
+                        + Utils.getRedactedAddressStringFromByte(secondaryAddress)
+                        + ", identityAddressType="
+                        + identityAddressType);
 
         DeviceProperties deviceProperties = getDeviceProperties(device);
         deviceProperties.setIdentityAddress(Utils.getAddressStringFromByte(secondaryAddress));
+        deviceProperties.setIdentityAddressType(identityAddressType);
     }
 
     void aclStateChangeCallback(
