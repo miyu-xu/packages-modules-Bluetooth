2ba06d23b3eaa095b52e5a0220ccfc7d40224c83
Jayden Kim <jaydenk@google.com>
2024-11-14 23:32:37 +0000

    Notify offload socket channel connection info to socket hal
    Bug: 342012881
    Bug: 367419086
    Test: m -j
    Change-Id: Iab67fb510becfe97088321c8683a2c61916d5496

---
+++ system/test/mock/mock_btif_sock_l2cap.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+/*
+ * Generated mock file from original source file
+ *   Functions generated:13
+ *
+ *  mockcify.pl ver 0.7.1
+ */
+
+#include <cstdint>
+#include <functional>
+
+// Original included files, if any
+#include <cstdint>
+#include <cstring>
+
+// Original usings
+
+// Mocked compile conditionals, if any
+
+namespace test {
+namespace mock {
+namespace btif_sock_l2cap {
+
+// Shared state between mocked functions and tests
+// Name: on_btsocket_l2cap_close
+// Params: uint64_t connection_id
+// Return: void
+struct on_btsocket_l2cap_close {
+  std::function<void(uint64_t connection_id)> body{[](uint64_t /* connection_id */) {}};
+  void operator()(uint64_t connection_id) { body(connection_id); }
+};
+extern struct on_btsocket_l2cap_close on_btsocket_l2cap_close;
+
+// Name: on_btsocket_l2cap_opened_complete
+// Params: uint64_t connection_id, bool success
+// Return: void
+struct on_btsocket_l2cap_opened_complete {
+  std::function<void(uint64_t connection_id, bool success)> body{
+          [](uint64_t /* connection_id */, bool /* success */) {}};
+  void operator()(uint64_t connection_id, bool success) { body(connection_id, success); }
+};
+extern struct on_btsocket_l2cap_opened_complete on_btsocket_l2cap_opened_complete;
+
+}  // namespace btif_sock_l2cap
+}  // namespace mock
+}  // namespace test
+
+// END mockcify generation
