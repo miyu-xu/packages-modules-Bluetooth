900ee8612862935722cad129a3e1fcbcb7dfd1bd
Krzysztof Kopyściński <kopyscinski@google.com>
2024-11-18 06:53:33 +0000

    PhonePolicy: add HAP to processConnectOtherProfiles
    On reconnection, profile was not always getting connected, and
    workaround had to be used to make profile usable again. This patch fixes
    the issue by calling hapClientService.connect() when other profiles
    connect.
    
    Bug: 376489074
    Bug: 379771539
    Flag: com.android.bluetooth.flags.connect_hap_on_other_profile_connect
    Test: m -j
    Change-Id: I491ed2140df888e3eedd552430a767607643adee

--- android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
+++ android/app/src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -1020,6 +1020,9 @@ public class PhonePolicy implements AdapterService.BluetoothStateCallback {
         BatteryService batteryService = mFactory.getBatteryService();
         HidHostService hidHostService = mFactory.getHidHostService();
         BassClientService bcService = mFactory.getBassClientService();
+        if (Flags.connectHapOnOtherProfileConnect()) {
+            HapClientService hapClientService = mFactory.getHapClientService();
+        }
 
         if (hsService != null) {
             if (!mHeadsetRetrySet.contains(device)
@@ -1118,6 +1121,19 @@ public class PhonePolicy implements AdapterService.BluetoothStateCallback {
                             == BluetoothProfile.STATE_DISCONNECTED)) {
                 debugLog("Retrying connection to BASS with device " + device);
                 bcService.connect(device);
+            }
+        }
+        if (Flags.connectHapOnOtherProfileConnect()) {
+            if (hapClientService != null) {
+                List<BluetoothDevice> connectedDevices = hapClientService.getConnectedDevices();
+                if (!connectedDevices.contains(device)
+                        && (hapClientService.getConnectionPolicy(device)
+                                == BluetoothProfile.CONNECTION_POLICY_ALLOWED)
+                        && (hapClientService.getConnectionState(device)
+                                == BluetoothProfile.STATE_DISCONNECTED)) {
+                    debugLog("Retrying connection to HAS with device " + device);
+                    hapClientService.connect(device);
+                }
             }
         }
     }
