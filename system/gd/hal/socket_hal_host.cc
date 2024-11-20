05b88587734051e158d81aaffbf0aacd1e83ff93
Jayden Kim <jaydenk@google.com>
2024-11-20 04:33:38 +0000

    Add low power processor offload manager with socket hal shim
    Bug: 342012881
    Test: m com.android.btservices
    Change-Id: Iec194e241bdafe8dce34db23cc65398a0af17be0

---
+++ system/gd/hal/socket_hal_host.cc
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hal/socket_hal.h"
+
+namespace bluetooth::hal {
+
+const hal::SocketCapabilities kEmptySocketCapabilities = {};
+class SocketHalHost : public SocketHal {
+public:
+  bool IsBound() override { return false; }
+
+protected:
+  void ListDependencies(ModuleList* /*list*/) const {}
+
+  void Start() override {}
+
+  void Stop() override { socket_hal_cb_ = nullptr; }
+
+  std::string ToString() const override { return std::string("SocketHalHost"); }
+
+  hal::SocketCapabilities GetSocketCapabilities() override { return kEmptySocketCapabilities; }
+
+  bool RegisterCallback(hal::SocketHalCallback* callback) override {
+    socket_hal_cb_ = callback;
+    return true;
+  }
+
+  bool Opened(const hal::SocketContext& /*context*/) override { return false; }
+
+  void Closed(uint64_t /*socket_id*/) override {}
+
+private:
+  hal::SocketHalCallback* socket_hal_cb_;
+};
+
+const ModuleFactory SocketHal::Factory = ModuleFactory([]() { return new SocketHalHost(); });
+
+}  // namespace bluetooth::hal
