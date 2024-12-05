718b4a6e00c55e5a6ba6d03831489d47b9f1411d
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-12-05 21:27:09 +0000

    Queue the security request if either LE and BREDR link state is not Idle
    Also make sure callbacks are pushed to both the tranport for all queued
    security requests
    
    Bug: 380284981
    Test: mmm packages/modules/Bluetooth
    Flag: Exempt, minor change
    
    Change-Id: If86560c859cea52ea39fb2769ee619e1db3398ae

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -1033,7 +1033,7 @@ tBTM_STATUS BTM_SetEncryption(const RawAddress& bd_addr, tBT_TRANSPORT transport
                                                           : p_dev_rec->sec_rec.classic_link;
 
   /* Enqueue security request if security is active */
-  if (p_dev_rec->sec_rec.p_callback || (p_dev_rec->sec_rec.le_link != tSECURITY_STATE::IDLE &&
+  if (p_dev_rec->sec_rec.p_callback || (p_dev_rec->sec_rec.le_link != tSECURITY_STATE::IDLE ||
                                         p_dev_rec->sec_rec.classic_link != tSECURITY_STATE::IDLE)) {
     log::warn("Security Manager: BTM_SetEncryption busy, enqueue request");
     btm_sec_queue_encrypt_request(bd_addr, transport, p_callback, p_ref_data, sec_act);
@@ -4963,12 +4963,14 @@ static void btm_sec_check_pending_enc_req(tBTM_SEC_DEV_REC* p_dev_rec, tBT_TRANS
   for (const list_node_t* node = list_begin(list); node != list_end(list);) {
     tBTM_SEC_QUEUE_ENTRY* p_e = (tBTM_SEC_QUEUE_ENTRY*)list_node(node);
     node = list_next(node);
-
+    log::debug("btm_sec_check_pending_enc_req : sec_act=0x{:x}", p_e->sec_act);
     if (p_e->bd_addr == p_dev_rec->bd_addr && p_e->psm == 0 && p_e->transport == transport) {
-      if (encr_enable == 0 || transport == BT_TRANSPORT_BR_EDR ||
-          p_e->sec_act == BTM_BLE_SEC_ENCRYPT || p_e->sec_act == BTM_BLE_SEC_ENCRYPT_NO_MITM ||
+      log::debug("btm_sec_check_pending_enc_req : matching address ");
+      if (encr_enable == 0 || p_e->sec_act == BTM_BLE_SEC_ENCRYPT ||
+          p_e->sec_act == BTM_BLE_SEC_ENCRYPT_NO_MITM ||
           (p_e->sec_act == BTM_BLE_SEC_ENCRYPT_MITM &&
            p_dev_rec->sec_rec.sec_flags & BTM_SEC_LE_AUTHENTICATED)) {
+        log::debug("btm_sec_check_pending_enc_req : pushing callback ");
         if (p_e->p_callback) {
           (*p_e->p_callback)(p_dev_rec->bd_addr, transport, p_e->p_ref_data, res);
         }
