96964ba3ebc5d902abe48df075518b4e05c854eb
Rajat Soni <quic_rajasoni@quicinc.com>
2024-12-09 11:13:24 +0530

    A2DP: Disallow sending two outgoing suspend to remote
    - This issue is seen when we try to change the frequency from the
      developer options while streaming.
    
    - Two AVDTP suspend commands are being sent back to back which is not
      expected. One as a part of stopping the session then stopping the
      stream, and other as part of reconfigure.
    
    Change-Id: Ia662427e64cb432b8ab6a2cf3a21301fc44253e3

--- system/bta/av/bta_av_aact.cc
+++ system/bta/av/bta_av_aact.cc
@@ -855,6 +855,7 @@ void bta_av_cleanup(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* /* p_data */) {
   /* if de-registering shut everything down */
   msg.hdr.layer_specific = p_scb->hndl;
   p_scb->started = false;
+  p_scb->suspend_local_sent = false;
   p_scb->use_rtp_header_marker_bit = false;
   p_scb->cong = false;
   p_scb->role = role;
@@ -1187,6 +1188,7 @@ void bta_av_str_opened(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   bta_av_conn_chg(reinterpret_cast<tBTA_AV_DATA*>(&msg));
   /* set the congestion flag, so AV would not send media packets by accident */
   p_scb->cong = true;
+  p_scb->suspend_local_sent = false;
   // Don't use AVDTP SUSPEND for restrict listed devices
   btif_storage_get_stored_remote_name(p_scb->PeerAddress(), remote_name);
   if (interop_match_name(INTEROP_DISABLE_AVDTP_SUSPEND, remote_name) ||
@@ -1347,6 +1349,7 @@ void bta_av_do_close(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* /* p_data */) {
 
   /* close stream */
   p_scb->started = false;
+  p_scb->suspend_local_sent = false;
   p_scb->use_rtp_header_marker_bit = false;
 
   /* drop the buffers queued in L2CAP */
@@ -1972,16 +1975,18 @@ void bta_av_str_stopped(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   suspend_rsp.hndl = p_scb->hndl;
 
   if (p_data && p_data->api_stop.suspend) {
-    log::verbose("peer {} suspending: {}, sup:{}", p_scb->PeerAddress(), start, p_scb->suspend_sup);
-    if ((start) && (p_scb->suspend_sup)) {
+    log::verbose("peer {} suspending: {}, sup:{}, suspend_local_sent: {}",
+                  p_scb->PeerAddress(), start, p_scb->suspend_sup, p_scb->suspend_local_sent);
+    if ((start) && (p_scb->suspend_sup) && (!p_scb->suspend_local_sent)) {
       sus_evt = false;
+      p_scb->suspend_local_sent = true;
       p_scb->l2c_bufs = 0;
       AVDT_SuspendReq(&p_scb->avdt_handle, 1);
     }
 
     /* send SUSPEND_EVT event only if not in reconfiguring state and sus_evt is
      * true*/
-    if ((sus_evt) && (p_scb->state != BTA_AV_RCFG_SST)) {
+    if ((sus_evt) && ((p_scb->suspend_local_sent) || (p_scb->state != BTA_AV_RCFG_SST))) {
       suspend_rsp.status = BTA_AV_SUCCESS;
       suspend_rsp.initiator = true;
       tBTA_AV bta_av_data;
@@ -2529,9 +2534,10 @@ void bta_av_clr_cong(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* /* p_data */) {
 void bta_av_suspend_cfm(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   tBTA_AV_SUSPEND suspend_rsp = {};
   uint8_t err_code = p_data->str_msg.msg.hdr.err_code;
+  p_scb->suspend_local_sent = false;
 
-  log::verbose("peer {} bta_handle:0x{:x} audio_open_cnt:{} err_code:{}", p_scb->PeerAddress(),
-               p_scb->hndl, bta_av_cb.audio_open_cnt, err_code);
+  log::verbose("peer {} bta_handle:0x{:x} audio_open_cnt:{} err_code:{} scb_started:{}", p_scb->PeerAddress(),
+               p_scb->hndl, bta_av_cb.audio_open_cnt, err_code, p_scb->started);
 
   if (!p_scb->started) {
     /* handle the condition where there is a collision of SUSPEND req from
@@ -2765,6 +2771,8 @@ void bta_av_suspend_cont(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
 
   p_scb->started = false;
   p_scb->cong = false;
+  p_scb->suspend_local_sent = false;
+
   if (err_code) {
     if (AVDT_ERR_CONNECT == err_code) {
       /* report failure */
