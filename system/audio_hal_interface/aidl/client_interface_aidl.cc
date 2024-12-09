4589b78e8455c3b9e27dfdb9b5625a2e54638060
Rajat Soni <quic_rajasoni@quicinc.com>
2024-12-09 12:18:30 +0530

    Start is failing after audio server restart for hearing aid
    - When audio server is killed, suspend is not happening,
      due to which, in hearing_aid.cc, some flags are not getting
      reset. Now when, OnAudioResume is called when audio server
      is restarted, we find that audio is already playing because
      flags were not cleared.
    - Issue is started when suspend is not happening after killing
      audio server.
    - This fix makes sure that suspend happens when audio server
      is killed.
    
    Bug: 383970349
    Test: mmm packages/modules/Bluetooth
    Test: atest bluetooth-test-audio-hal-interface
    Flag: EXEMPT, trivial
    
    Change-Id: I806e0b9942b696737c5946a12ee6f128b78b4723

--- system/audio_hal_interface/aidl/client_interface_aidl.cc
+++ system/audio_hal_interface/aidl/client_interface_aidl.cc
@@ -17,6 +17,7 @@
 #define LOG_TAG "BTAudioClientAIDL"
 
 #include "aidl/client_interface_aidl.h"
+#include "hearing_aid_software_encoding_aidl.h"
 
 #include <android/binder_manager.h>
 #include <bluetooth/log.h>
@@ -216,7 +217,9 @@ void BluetoothAudioClientInterface::binderDiedCallbackAidl(void* ptr) {
     log::error("null audio HAL died!");
     return;
   }
-  client->RenewAudioProviderAndSession();
+  message_loop_->DoInThread(
+          base::BindOnce(&BluetoothAudioClientInterface::RenewAudioProviderAndSession,
+                         base::Unretained(bluetooth_audio_clientif_)));
 }
 
 bool BluetoothAudioClientInterface::UpdateAudioConfig(const AudioConfiguration& audio_config) {
@@ -531,7 +534,11 @@ void BluetoothAudioClientInterface::RenewAudioProviderAndSession() {
   if (session_started_) {
     log::info("Restart the session while audio HAL recovering");
     session_started_ = false;
-
+    if(transport_->GetSessionType() ==
+                 SessionType::HEARING_AID_SOFTWARE_ENCODING_DATAPATH) {
+      log::info("Session type: HEARING_AID_SOFTWARE_ENCODING_DATAPATH");
+      transport_->StopRequest();
+    }
     StartSession();
   }
 }
