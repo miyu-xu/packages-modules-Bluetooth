fc7b542c098cee8dd8abeace186fe0cb84ccea17
Rajat Soni <quic_rajasoni@quicinc.com>
2024-12-09 12:18:30 +0530

    Start is failing after audio server restart for hearing aid
    - When audio server is killed, suspend is not happening,
      due to which, in hearing_aid.cc, some flags are not getting
      reset. Now when, OnAudioResume is called when audio server
      is restarted, we find that audio is already playing because
      flags were not cleared.
    - Issue is started when suspend is not happening after killing
      audio server.
    - This fix makes sure that suspend happens when audio server
      is killed.
    
    Change-Id: I806e0b9942b696737c5946a12ee6f128b78b4723

--- system/audio_hal_interface/aidl/client_interface_aidl.cc
+++ system/audio_hal_interface/aidl/client_interface_aidl.cc
@@ -17,6 +17,7 @@
 #define LOG_TAG "BTAudioClientAIDL"
 
 #include "aidl/client_interface_aidl.h"
+#include "hearing_aid_software_encoding_aidl.h"
 
 #include <android/binder_manager.h>
 #include <bluetooth/log.h>
@@ -531,7 +532,11 @@ void BluetoothAudioClientInterface::RenewAudioProviderAndSession() {
   if (session_started_) {
     log::info("Restart the session while audio HAL recovering");
     session_started_ = false;
-
+    if(transport_->GetSessionType() ==
+                 SessionType::HEARING_AID_SOFTWARE_ENCODING_DATAPATH) {
+      log::info("Session type: HEARING_AID_SOFTWARE_ENCODING_DATAPATH");
+      bluetooth::audio::aidl::hearing_aid::stop_request();
+    }
     StartSession();
   }
 }
