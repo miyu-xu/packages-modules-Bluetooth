3458cb991a3b136018777b977db8ce08fb38328b
Hyundo Moon <hdmoon@google.com>
2024-12-12 17:07:03 +0900

    Report UUIDs in BLE advertising data
    This CL makes the 16bit/128bit UUIDs in BLE advertising data
    from BLE-only devices be reported to Java layer.
    
    As a result, the UUIDs can be found in:
    - BluetoothDevice.ACTION_UUID intent
    - BluetoothDevice.getUuids()
    
    Bug: 385277135
    Bug: 389598210
    Flag: com.android.bluetooth.flags.report_uuids_from_le_advertising_data
    Test: atest android.bluetooth.DeviceDiscoveryTest#getUuidsInBleAdvertisingData
    Change-Id: Ia4c80fc40d81ffd1b1e849171f08d775f7beb06a

--- framework/tests/bumble/src/android/bluetooth/DeviceDiscoveryTest.java
+++ framework/tests/bumble/src/android/bluetooth/DeviceDiscoveryTest.java
@@ -22,28 +22,42 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.ParcelUuid;
+import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.util.Log;
 
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.android.bluetooth.flags.Flags;
 import com.android.compatibility.common.util.AdoptShellPermissionsRule;
 
 import com.google.common.util.concurrent.SettableFuture;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import pandora.HostProto.AdvertiseRequest;
+import pandora.HostProto.AdvertiseResponse;
+import pandora.HostProto.ConnectabilityMode;
+import pandora.HostProto.DataTypes;
 import pandora.HostProto.DiscoverabilityMode;
+import pandora.HostProto.OwnAddressType;
+import pandora.HostProto.SetConnectabilityModeRequest;
 import pandora.HostProto.SetDiscoverabilityModeRequest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /** Test cases for {@link DeviceDiscoveryManager}. */
-@RunWith(AndroidJUnit4.class)
+@RunWith(TestParameterInjector.class)
 public class DeviceDiscoveryTest {
     private static final String TAG = "DeviceDiscoveryTest";
+    private static final String TEST_16_BIT_SERVICE_UUID_STRING = "1809";
+    private static final String TEST_128_BIT_SERVICE_UUID_STRING =
+            "88400001-e95a-844e-c53f-fbec32ed5e54";
 
     private final Context mContext = ApplicationProvider.getApplicationContext();
     private final BluetoothManager mManager = mContext.getSystemService(BluetoothManager.class);
@@ -58,6 +72,8 @@ public class DeviceDiscoveryTest {
 
     private ArrayList<Intent> mDeviceFoundData;
 
+    private ArrayList<Intent> mUuidData;
+
     private BroadcastReceiver mConnectionStateReceiver =
             new BroadcastReceiver() {
                 @Override
@@ -71,6 +87,8 @@ public class DeviceDiscoveryTest {
                                 BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
                     } else if (BluetoothDevice.ACTION_FOUND.equals(intent.getAction())) {
                         mDeviceFoundData.add(intent);
+                    } else if (BluetoothDevice.ACTION_UUID.equals(intent.getAction())) {
+                        mUuidData.add(intent);
                     }
                 }
             };
@@ -120,14 +138,8 @@ public class DeviceDiscoveryTest {
     public void checkDeviceIsDiscoveredTest() throws Exception {
         mFutureDiscoveryStartedIntent = SettableFuture.create();
         mFutureDiscoveryFinishedIntent = SettableFuture.create();
-        mDeviceFoundData = new ArrayList<Intent>();
-
-        // Ensure remote device is discoverable
-        mBumble.hostBlocking()
-                .setDiscoverabilityMode(
-                        SetDiscoverabilityModeRequest.newBuilder()
-                                .setMode(DiscoverabilityMode.DISCOVERABLE_GENERAL)
-                                .build());
+        mDeviceFoundData = new ArrayList<>();
+        mUuidData = new ArrayList<>();
 
         IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
@@ -147,5 +159,87 @@ public class DeviceDiscoveryTest {
         // Ensure we received at least one inquiry response
         assertThat(!mDeviceFoundData.isEmpty()).isTrue();
         Log.i(TAG, "Found inquiry results count:" + mDeviceFoundData.size());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_REPORT_UUIDS_FROM_LE_ADVERTISING_DATA)
+    public void getUuidsInBleAdvertisingData() throws Exception {
+        mFutureDiscoveryStartedIntent = SettableFuture.create();
+        mFutureDiscoveryFinishedIntent = SettableFuture.create();
+        mDeviceFoundData = new ArrayList<>();
+        mUuidData = new ArrayList<>();
+
+        // Make Bumble BLE-only by setting BR/EDR not discoverable/connectable.
+        // Note: Even with these calls, Bumble does not set the 'BR/EDR not supported' flag as true,
+        //       which makes this test fail when the address type is PUBLIC.
+        //       Therefore, this now only tests the RANDOM address type.
+        mBumble.hostBlocking()
+                .setDiscoverabilityMode(
+                        SetDiscoverabilityModeRequest.newBuilder()
+                                .setMode(DiscoverabilityMode.NOT_DISCOVERABLE)
+                                .build());
+        mBumble.hostBlocking()
+                .setConnectabilityMode(
+                        SetConnectabilityModeRequest.newBuilder()
+                                .build()
+                                .newBuilder()
+                                .setMode(ConnectabilityMode.NOT_CONNECTABLE)
+                                .build());
+
+        AdvertiseRequest.Builder requestBuilder =
+                AdvertiseRequest.newBuilder().setOwnAddressType(OwnAddressType.RANDOM);
+        DataTypes.Builder dataTypeBuilder = DataTypes.newBuilder();
+        dataTypeBuilder.addCompleteServiceClassUuids16(TEST_16_BIT_SERVICE_UUID_STRING);
+        dataTypeBuilder.addCompleteServiceClassUuids128(TEST_128_BIT_SERVICE_UUID_STRING);
+        dataTypeBuilder.setLeDiscoverabilityModeValue(
+                DiscoverabilityMode.DISCOVERABLE_GENERAL_VALUE);
+
+        requestBuilder.setData(dataTypeBuilder.build());
+        requestBuilder.setLegacy(true); // Bumble only supports legacy advertising
+
+        advertiseWithBumble(requestBuilder);
+
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothDevice.ACTION_UUID);
+        mContext.registerReceiver(mConnectionStateReceiver, filter);
+
+        assertThat(mAdapter.startDiscovery()).isTrue();
+        assertThat(mFutureDiscoveryStartedIntent.get())
+                .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+
+        // Wait for device discovery to complete
+        assertThat(mFutureDiscoveryFinishedIntent.get())
+                .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        mContext.unregisterReceiver(mConnectionStateReceiver);
+
+        assertThat(mUuidData).isNotEmpty();
+        Intent intent = mUuidData.get(0);
+        List<ParcelUuid> uuids =
+                Arrays.asList(
+                        intent.getParcelableArrayExtra(
+                                BluetoothDevice.EXTRA_UUID, ParcelUuid.class));
+
+        assertThat(uuids).contains(ParcelUuid.fromString(TEST_128_BIT_SERVICE_UUID_STRING));
+        assertThat(uuids).contains(convert16bitUuidToParcelUuid(TEST_16_BIT_SERVICE_UUID_STRING));
+
+        BluetoothDevice device =
+                intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice.class);
+        uuids = Arrays.asList(device.getUuids());
+        assertThat(uuids).contains(ParcelUuid.fromString(TEST_128_BIT_SERVICE_UUID_STRING));
+        assertThat(uuids).contains(convert16bitUuidToParcelUuid(TEST_16_BIT_SERVICE_UUID_STRING));
+    }
+
+    private ParcelUuid convert16bitUuidToParcelUuid(String uuidString) {
+        return ParcelUuid.fromString("0000" + uuidString + "-0000-1000-8000-00805f9b34fb");
+    }
+
+    private void advertiseWithBumble(AdvertiseRequest.Builder requestBuilder) {
+        // Collect and ignore responses.
+        StreamObserverSpliterator<AdvertiseResponse> responseObserver =
+                new StreamObserverSpliterator<>();
+        mBumble.host().advertise(requestBuilder.build(), responseObserver);
     }
 }
