76d9103dcb156d3e0c02dba8975c5171f579bc29
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2024-12-16 09:52:16 +0000

    broadcaster: Add missing fallback default update handlers
    This change modifies two cases of handling update of fallback Unicast to
    Broadcast group Id. For first case the fallback group should not be
    updated when group is changing state to inactive. Second case is for
    group from which active device (device which is part of active group)
    is disconnecting.
    
    Bug: 384847958
    Bug: 375422795
    Flag: leaudio_broadcast_primary_group_selection
    Test: atest LeAudioBroadcastServiceTest
    Change-Id: Ic18f32caabd6603a6762b9abcdfcfb2b18a6d510

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -3612,7 +3612,8 @@ public class LeAudioService extends ProfileService {
                             }
 
                             if (leaudioBigDependsOnAudioState()) {
-                                if (mAwaitingBroadcastCreateResponse) {
+                                if (mAwaitingBroadcastCreateResponse
+                                        && !Flags.leaudioBroadcastPrimaryGroupSelection()) {
                                     updateFallbackUnicastGroupIdForBroadcast(groupId);
                                 }
                             } else {
@@ -4145,6 +4146,11 @@ public class LeAudioService extends ProfileService {
                             false,
                             hasFallbackDevice,
                             false);
+                    /* Set by default earliest connected device */
+                    if (Flags.leaudioBroadcastPrimaryGroupSelection()
+                            && mUnicastGroupIdDeactivatedForBroadcastTransition == groupId) {
+                        setDefaultBroadcastToUnicastFallbackGroup();
+                    }
                     return;
                 }
 
