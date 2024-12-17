4408c49d6eb399c20de2eb781fb0538e97a576dc
Naresh Kumar Podishetty (xWF) <podishettyk@google.com>
2024-12-17 00:01:01 -0800

    Revert "Configure scan on resume"
    This reverts commit ca5b24a4391a66f5826fc1040a9716ce059f4818.
    
    Reason for revert: <Droidmonitor created revert due to b/384619750. Will be verifying through ABTD before submission.>
    
    Change-Id: Ice4fe6f03f0d9eaf0818ccd49d621c6d3cbb2709

--- system/gd/hci/le_scanning_manager.cc
+++ system/gd/hci/le_scanning_manager.cc
@@ -1577,14 +1577,8 @@ struct LeScanningManager::impl : public LeAddressManagerCallback {
       return;
     }
     paused_ = false;
-    if (scan_on_resume_) {
+    if (scan_on_resume_ == true) {
       scan_on_resume_ = false;
-      if (com::android::bluetooth::flags::configure_scan_on_resume()) {
-        // This is a workaround for b/381010390.
-        // We'll eventually recover scan parameters which could be overridden by
-        // btm_send_hci_set_scan_params.
-        configure_scan();
-      }
       start_scan();
     }
     le_address_manager_->AckResume(this);
