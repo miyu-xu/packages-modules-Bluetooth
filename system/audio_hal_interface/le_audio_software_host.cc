52729bb1b6ca790a7e6c31cb601213487682df23
Jakub Tyszkowski <tyszkowski@google.com>
2024-12-17 09:29:27 +0000

    LeAudio: Support codec extensibility flags for GMAP
    Bug: 313054645
    Bug: 350102910
    Bug: 341935895
    Test: atest bluetooth_le_audio_codec_manager_test
    Flag: leaudio_multicodec_aidl_support
    Change-Id: Id2ac73958b08429da53564c7d68f610480b47037

--- system/audio_hal_interface/le_audio_software_host.cc
+++ system/audio_hal_interface/le_audio_software_host.cc
@@ -570,6 +570,8 @@ LeAudioClientInterface* LeAudioClientInterface::Get() {
 
 void LeAudioClientInterface::SetAllowedDsaModes(DsaModes /*dsa_modes*/) { return; }
 
+static std::optional<bluetooth::le_audio::ProviderInfo> GetCodecConfigProviderInfo(void) const { return; }
+
 }  // namespace le_audio
 }  // namespace audio
 }  // namespace bluetooth
