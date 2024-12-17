5de2f0ce3241ac1405560cc44c2cf6028fea4046
Jakub Tyszkowski <tyszkowski@google.com>
2024-12-17 08:23:00 +0000

    LeAudio: Add persistent storage for GMAP
    For the bonded devices, service discovery is
    skipped and service data needs to be restored from the persistent
    storage for the GMAS client to work. The storage will not be used when
    GMAP feature flag is disabled.
    
    Bug: 383623629
    Bug: 353978074
    Test: atest bluetooth_le_audio_test
    Flag: Exempt; Covered with unit tests
    Change-Id: Ib01c38e7e6eed6103fccb70953483bb0e32abb87

--- system/test/mock/mock_bta_leaudio.cc
+++ system/test/mock/mock_bta_leaudio.cc
@@ -41,14 +41,12 @@ class HalVersionManager {
 }  // namespace audio
 }  // namespace bluetooth
 
-void LeAudioClient::AddFromStorage(const RawAddress& /* addr */, bool /* autoconnect */,
-                                   int /* sink_audio_location */, int /* source_audio_location */,
-                                   int /* sink_supported_context_types */,
-                                   int /* source_supported_context_types */,
-                                   const std::vector<uint8_t>& /* handles */,
-                                   const std::vector<uint8_t>& /* sink_pacs */,
-                                   const std::vector<uint8_t>& /* source_pacs */,
-                                   const std::vector<uint8_t>& /* ases */) {
+void LeAudioClient::AddFromStorage(
+        const RawAddress& /* addr */, bool /* autoconnect */, int /* sink_audio_location */,
+        int /* source_audio_location */, int /* sink_supported_context_types */,
+        int /* source_supported_context_types */, const std::vector<uint8_t>& /* handles */,
+        const std::vector<uint8_t>& /* sink_pacs */, const std::vector<uint8_t>& /* source_pacs */,
+        const std::vector<uint8_t>& /* ases */, const std::vector<uint8_t>& /* gmap */) {
   inc_func_call_count(__func__);
 }
 
