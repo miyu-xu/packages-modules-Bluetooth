0b62c8c27c3ff6f7e23bd1867e8ddf1b83880e0d
wentjin <quic_wentjin@quicinc.com>
2024-12-17 13:15:31 +0800

    Send INTERIM for last playback when changes during notification
    Root cause: In some use-cases, the playback status had been
    changed before it send INTERIM during notification procedure.
    It responded current playback to remotes caused UI of remotes
    shown error status of player.
    
    Fixed: Compare last playback status and current status. If it
    changes, we should send last playback first. Then, send playback
    CHANGED again with new playback status.
    
    Change-Id: Ib3eb3e68241e43bc18df1514ff879df64f72ae79

--- system/profile/avrcp/device.cc
+++ system/profile/avrcp/device.cc
@@ -645,12 +645,36 @@ void Device::PlaybackStatusNotificationResponse(uint8_t label, bool interim, Pla
     return;
   }
 
+  log::verbose("status.state: {}", status.state);
   auto state_to_send = status.state;
   if (!IsActive()) {
     state_to_send = PlayState::PAUSED;
   }
   if (!interim && state_to_send == last_play_status_.state) {
     log::verbose("Not sending notification due to no state update {}", address_);
+    return;
+  }
+
+  log::verbose("last_play_status_.state: {}", last_play_status_.state);
+  if (interim && last_play_status_.state != state_to_send &&
+      (last_play_status_.state == PlayState::PAUSED ||
+       last_play_status_.state == PlayState::PLAYING)) {
+    log::verbose("playback Status has changed from last playstatus response");
+    auto lastresponse =
+       RegisterNotificationResponseBuilder::MakePlaybackStatusBuilder(
+         interim, last_play_status_.state);
+    send_message_cb_.Run(label, false, std::move(lastresponse));
+
+    last_play_status_.state = state_to_send;
+
+    log::verbose("Send new playback Status CHANGED");
+    auto newresponse =
+        RegisterNotificationResponseBuilder::MakePlaybackStatusBuilder(
+            false, IsActive() ? status.state : PlayState::PAUSED);
+    send_message_cb_.Run(label, false, std::move(newresponse));
+
+    active_labels_.erase(label);
+    play_status_changed_ = Notification(false, 0);
     return;
   }
 
