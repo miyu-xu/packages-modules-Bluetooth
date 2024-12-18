6ad231e60f8dde6c8fb486424a00f13cdb4dda6b
Bhakthavatsala Raghavendra <bhaktha@google.com>
2024-12-18 23:55:38 +0000

    Fix controller_le_xmit_window calculation on acl push
    controller_le_xmit_window to be decremented based on number of
    acl packets being pushed to controller rather than l2cap SDU
    
    Bug: 369452158
    Test: mmm packages/modules/Bluetooth
    Flag: Exempt, minor change
    
    Change-Id: Ib14ef1fe84b6aec97140b0fd548c808603e4e1c1

--- system/stack/l2cap/l2c_link.cc
+++ system/stack/l2cap/l2c_link.cc
@@ -31,7 +31,9 @@
 #include <cstdint>
 
 #include "device/include/device_iot_config.h"
+#include "hci/controller_interface.h"
 #include "internal_include/bt_target.h"
+#include "main/shim/entry.h"
 #include "osi/include/allocator.h"
 #include "stack/btm/btm_int_types.h"
 #include "stack/include/acl_api.h"
@@ -1041,7 +1043,12 @@ static void l2c_link_send_to_lower_ble(tL2C_LCB* p_lcb, BT_HDR* p_buf) {
   }
   p_lcb->sent_not_acked++;
   p_buf->layer_specific = 0;
-  l2cb.controller_le_xmit_window--;
+  uint16_t max_le_acl_len =
+          bluetooth::shim::GetController()->GetLeBufferSize().le_data_packet_length_;
+  uint16_t num_of_frags = (p_buf->len / max_le_acl_len) + ((p_buf->len % max_le_acl_len) ? 1 : 0);
+  l2cb.controller_le_xmit_window = l2cb.controller_le_xmit_window - num_of_frags;
+  log::debug("num_of_frags = {}, p_buf->len = {}, max_le_acl_len = {}", num_of_frags, p_buf->len,
+             max_le_acl_len);
 
   acl_send_data_packet_ble(p_lcb->remote_bd_addr, p_buf);
   log::debug("TotalWin={},Hndl=0x{:x},Quota={},Unack={},RRQuota={},RRUnack={}",
