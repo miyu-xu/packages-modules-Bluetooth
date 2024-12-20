f181983b31e8ee11c1534cf111ad249b976714cf
Henri Chataing <henrichataing@google.com>
2024-12-20 01:40:24 +0000

    Move CounterMetrics out of the module registry
    Flag: EXEMPT, no logical change
    Test: m com.android.btservices
    Change-Id: Ic973de67af9f100b418a3ff32bf3a38f1f0f0597

--- system/main/shim/stack.cc
+++ system/main/shim/stack.cc
@@ -67,6 +67,7 @@ namespace shim {
 
 struct Stack::impl {
   Acl* acl_ = nullptr;
+  metrics::CounterMetrics* counter_metrics_ = nullptr;
 };
 
 Stack::Stack() { pimpl_ = std::make_shared<Stack::impl>(); }
@@ -82,6 +83,11 @@ void Stack::StartEverything() {
   log::info("Starting Gd stack");
   ModuleList modules;
 
+  stack_thread_ = new os::Thread("gd_stack_thread", os::Thread::Priority::REAL_TIME);
+  stack_handler_ = new os::Handler(stack_thread_);
+
+  pimpl_->counter_metrics_ = new metrics::CounterMetrics(new Handler(stack_thread_));
+
 #if TARGET_FLOSS
   modules.add<sysprops::SyspropsModule>();
 #else
@@ -89,7 +95,6 @@ void Stack::StartEverything() {
     modules.add<lpp::LppOffloadManager>();
   }
 #endif
-  modules.add<metrics::CounterMetrics>();
   modules.add<hal::HciHal>();
   modules.add<hci::HciLayer>();
   modules.add<storage::StorageModule>();
@@ -103,14 +108,12 @@ void Stack::StartEverything() {
   modules.add<hci::LeScanningManager>();
   modules.add<hci::DistanceMeasurementManager>();
 
-  stack_thread_ = new os::Thread("gd_stack_thread", os::Thread::Priority::REAL_TIME);
   StartUp(&modules, stack_thread_);
-
-  stack_handler_ = new os::Handler(stack_thread_);
 
   log::info("Successfully toggled Gd stack");
 
   is_running_ = true;
+
   // Make sure the leaf modules are started
   log::assert_that(GetInstance<storage::StorageModule>() != nullptr,
                    "assert failed: GetInstance<storage::StorageModule>() != nullptr");
@@ -192,11 +195,17 @@ bool Stack::IsRunning() {
   return is_running_;
 }
 
-Acl* Stack::GetAcl() {
+Acl* Stack::GetAcl() const {
   std::lock_guard<std::recursive_mutex> lock(mutex_);
   log::assert_that(is_running_, "assert failed: is_running_");
   log::assert_that(pimpl_->acl_ != nullptr, "Acl shim layer has not been created");
   return pimpl_->acl_;
+}
+
+metrics::CounterMetrics* Stack::GetCounterMetrics() const {
+  std::lock_guard<std::recursive_mutex> lock(mutex_);
+  log::assert_that(is_running_, "assert failed: is_running_");
+  return pimpl_->counter_metrics_;
 }
 
 os::Handler* Stack::GetHandler() {
@@ -247,6 +256,10 @@ void Stack::StartUp(ModuleList* modules, Thread* stack_thread) {
 }
 
 void Stack::handle_start_up(ModuleList* modules, Thread* stack_thread, std::promise<void> promise) {
+  if (pimpl && pimpl_->counter_metrics_) {
+    pimpl_->counter_metrics_->Start();
+  }
+
   registry_.Start(modules, stack_thread);
   promise.set_value();
 }
