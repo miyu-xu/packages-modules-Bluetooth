1121bff31456ca63f2e6d26f8d7b178f32aa583d
janardhana rao bokka <quic_jbokka@quicinc.com>
2024-12-24 18:00:54 +0530

    SMP: Not updating SMP_state in LE transport if smp_bredr progress
    Not updating SMP_state in LE transport if smp over BREDR progress
    
    Change-Id: I7e502df3b57cea82b080047ab4d4626eb93cbc6e

--- system/stack/smp/smp_l2c.cc
+++ system/stack/smp/smp_l2c.cc
@@ -48,6 +48,7 @@ static void smp_data_received(uint16_t channel, const RawAddress& bd_addr, BT_HD
 static void smp_br_connect_callback(uint16_t channel, const RawAddress& bd_addr, bool connected,
                                     uint16_t reason, tBT_TRANSPORT transport);
 static void smp_br_data_received(uint16_t channel, const RawAddress& bd_addr, BT_HDR* p_buf);
+extern const char* smp_get_br_state_name(tSMP_BR_STATE state);
 
 /*******************************************************************************
  *
@@ -117,11 +118,15 @@ static void smp_connect_callback(uint16_t /* channel */, const RawAddress& bd_ad
         /* initiating connection established */
         p_cb->role = stack::l2cap::get_interface().L2CA_GetBleConnRole(bd_addr);
 
-        /* initialize local i/r key to be default keys */
-        p_cb->local_r_key = p_cb->local_i_key = SMP_SEC_DEFAULT_KEY;
-        p_cb->loc_auth_req = p_cb->peer_auth_req = SMP_DEFAULT_AUTH_REQ;
-        p_cb->cb_evt = SMP_IO_CAP_REQ_EVT;
-        smp_sm_event(p_cb, SMP_L2CAP_CONN_EVT, NULL);
+        if (p_cb->br_state == SMP_BR_STATE_IDLE) {
+          /* initialize local i/r key to be default keys */
+          p_cb->local_r_key = p_cb->local_i_key = SMP_SEC_DEFAULT_KEY;
+          p_cb->loc_auth_req = p_cb->peer_auth_req = SMP_DEFAULT_AUTH_REQ;
+          p_cb->cb_evt = SMP_IO_CAP_REQ_EVT;
+          smp_sm_event(p_cb, SMP_L2CAP_CONN_EVT, NULL);
+        } else {
+          log::warn("SMP over BREDR is in progress state {}", p_cb->br_state);
+        }
       }
     } else {
       /* Disconnected while doing security */
