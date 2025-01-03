4c4c0016723c9005f0b91c9aac0d25dfe78eba85
Henri Chataing <henrichataing@google.com>
2025-01-03 14:55:06 +0100

    Remove PRIVATE_NAME
    not implemented
    
    Bug: 331817295
    Test: m com.android.bt
    Flag: EXEMPT, no logical change
    Change-Id: If765cc42665f64e0c3ddd9218e6045614a5b8606

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -971,8 +971,8 @@ static void btif_dm_pin_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
   }
   BTM_LogHistory(kBtmLogTagCallback, bd_addr, "Pin request",
                  std::format("name:\"{}\" min16:{:c}",
-                             PRIVATE_NAME(reinterpret_cast<char const*>(bd_name.name)),
-                             (p_pin_req->min_16_digit) ? 'T' : 'F'));
+                             reinterpret_cast<char const*>(bd_name.name),
+                             p_pin_req->min_16_digit ? 'T' : 'F'));
   GetInterfaceToProfiles()->events->invoke_pin_request_cb(bd_addr, bd_name, cod,
                                                           p_pin_req->min_16_digit);
 }
@@ -1859,7 +1859,7 @@ static void btif_on_name_read(RawAddress bd_addr, tHCI_ERROR_CODE hci_status, co
   GetInterfaceToProfiles()->events->invoke_remote_device_properties_cb(
           status, bd_addr, properties.size(), properties.data());
   log::info("Callback for read name event addr:{} name:{}", bd_addr,
-            PRIVATE_NAME(reinterpret_cast<char const*>(bd_name)));
+            reinterpret_cast<char const*>(bd_name));
 
   if (!during_device_search) {
     return;
@@ -1873,7 +1873,7 @@ static void btif_on_name_read(RawAddress bd_addr, tHCI_ERROR_CODE hci_status, co
     GetInterfaceToProfiles()->events->invoke_device_found_cb(properties.size(), properties.data());
   } else {
     log::info("Skipping device found callback because cod is zero addr:{} name:{}", bd_addr,
-              PRIVATE_NAME(reinterpret_cast<char const*>(bd_name)));
+              reinterpret_cast<char const*>(bd_name));
   }
 }
 
@@ -3565,7 +3565,7 @@ static void btif_dm_ble_passkey_req_evt(tBTA_DM_PIN_REQ* p_pin_req) {
 
   BTM_LogHistory(
           kBtmLogTagCallback, bd_addr, "PIN request",
-          std::format("name:'{}'", PRIVATE_NAME(reinterpret_cast<char const*>(bd_name.name))));
+          std::format("name:'{}'", reinterpret_cast<char const*>(bd_name.name)));
 
   GetInterfaceToProfiles()->events->invoke_pin_request_cb(bd_addr, bd_name, cod, false);
 }
