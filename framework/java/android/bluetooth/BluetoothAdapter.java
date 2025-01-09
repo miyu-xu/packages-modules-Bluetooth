f4685056da32469dcd096437e4fee9093a4b6791
Edward Shin <shined@google.com>
2025-01-09 15:26:42 -0500

    Set channel for RFCOMM sockets
    This allows for `BluetoothServerSocket` users to retrieve the assigned
    channel ID when creating a RFCOMM socket with only a (SDP) service UUID
    specified. Access to the channel ID is needed for Bluetooth Offload
    usecases.
    
    Bug: 389768053
    Bug: 388874105
    Test: m com.android.btservices
    Test: Connected to RFCOMM socket on-target.
    Flag: com.android.bluetooth.flags.rfcomm_sockets_report_channel_number
    Change-Id: I8a9afe88185a5b68e484001f64765e4784a0433b

--- framework/java/android/bluetooth/BluetoothAdapter.java
+++ framework/java/android/bluetooth/BluetoothAdapter.java
@@ -3073,7 +3073,9 @@ public final class BluetoothAdapter {
                 new BluetoothServerSocket(
                         BluetoothSocket.TYPE_RFCOMM, true, true, channel, mitm, min16DigitPin);
         int errno = socket.mSocket.bindListen();
-        if (channel == SOCKET_CHANNEL_AUTO_STATIC_NO_SDP) {
+        // If the channel was -1 or `SOCKET_CHANNEL_AUTO_STATIC_NO_SDP`,
+        // update the RFCOMM channel automatically assigned to this socket.
+        if (channel < 0) {
             socket.setChannel(socket.mSocket.getPort());
         }
         if (errno != 0) {
@@ -3350,6 +3352,10 @@ public final class BluetoothAdapter {
             // socket.mSocket.throwErrnoNative(errno);
             throw new IOException("Error: " + errno);
         }
+
+        // Update the RFCOMM channel assigned to this socket.
+        socket.setChannel(socket.mSocket.getPort());
+
         return socket;
     }
 
@@ -3368,7 +3374,9 @@ public final class BluetoothAdapter {
         BluetoothServerSocket socket =
                 new BluetoothServerSocket(BluetoothSocket.TYPE_RFCOMM, false, false, port);
         int errno = socket.mSocket.bindListen();
-        if (port == SOCKET_CHANNEL_AUTO_STATIC_NO_SDP) {
+        // If the channel was -1 or `SOCKET_CHANNEL_AUTO_STATIC_NO_SDP`,
+        // update RFCOMM channel automatically assigned to this socket.
+        if (port < 0) {
             socket.setChannel(socket.mSocket.getPort());
         }
         if (errno != 0) {
@@ -4704,6 +4712,16 @@ public final class BluetoothAdapter {
                 Log.d(TAG, "listenUsingSocketSettings: set assigned PSM to " + assignedPsm);
             }
             socket.setChannel(assignedPsm);
+        } else if (type == BluetoothSocket.TYPE_RFCOMM) {
+            int assignedChannel = socket.mSocket.getPort();
+            if (assignedChannel == 0) {
+                throw new IOException("Error: Unable to assign RFCOMM channel value");
+            }
+            if (DBG) {
+                Log.d(TAG, "listenUsingSocketSettings: set assigned RFCOMM channel to "
+                        + assignedChannel);
+            }
+            socket.setChannel(assignedChannel);
         }
 
         return socket;
