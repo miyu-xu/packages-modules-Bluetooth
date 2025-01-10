4b86937e4e52bff2ddf60f0d6970c2a18a940fce
William Escande <wescande@google.com>
2025-01-09 18:51:05 -0800

    Build a file in kotlin
    I took the smallest and simplest file just to check that kotlin works
    
    Flag: Exempt build only
    Bug: 388947773
    Test: m com.android.bt
    Change-Id: Ia1d03ae7910455468626cad6b6aa0f8f90b9cfc8

--- android/app/src/com/android/bluetooth/gatt/GattServiceConfig.kt
+++ android/app/src/com/android/bluetooth/gatt/GattServiceConfig.kt
@@ -13,11 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:JvmName("AirplaneModeListener")
 
-package com.android.bluetooth.gatt;
+package com.android.bluetooth.gatt
 
-/** GattService configuration. */
-public class GattServiceConfig {
-    public static final String TAG_PREFIX = "BtGatt.";
-    public static final boolean DEBUG_ADMIN = false;
+object GattServiceConfig {
+    @JvmField val TAG_PREFIX = "BtGatt."
+    @JvmField val DEBUG_ADMIN = false
 }
