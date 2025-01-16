6d149fc0811094ee9b52856760378d6e9aecfadc
Jakub Rotkiewicz <rotkiewicz@google.com>
2025-01-16 13:43:18 +0000

    ActiveDeviceManager: Remove handling of wired devices
    Instead when the active device is set once again make sure that the audio routing
    will be updated accordingly.
    
    Bug: 202832397
    Flag: com.android.bluetooth.flags.adm_remove_handling_wired
    Test: atest ActiveDeviceManagerTest
    
    Change-Id: I18512d20aa8da6408aa43d145b24589b93d28488

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -2655,7 +2655,14 @@ public class LeAudioService extends ProfileService {
                 return false;
             }
         }
-        return setActiveGroupWithDevice(device, false);
+        boolean success = setActiveGroupWithDevice(device, false);
+
+        if (Flags.admRemoveHandlingWired() && success) {
+            // Force audio route change in case we reactivate the same device.
+            // This handles switching from wired device to currently active BT.
+            mAdapterService.getActiveDeviceManager().setPreferredDeviceForAudioRoute(device);
+        }
+        return success;
     }
 
     /**
