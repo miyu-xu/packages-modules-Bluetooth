5994411785cb7c12ed354162798a49214f38e80b
DC Xu <dcxu@google.com>
2025-01-20 00:05:55 -0800

    Fix enabling profile if already enabled, this change ensure the state in PackageManager has DISABLED to ENABLED to trigger PACKAGE_CHANGED intent
    Change-Id: I46d1f56d222d9e2f52424fc12fb54c68c5c5c74d

--- android/app/src/com/android/bluetooth/btservice/ProfileService.java
+++ android/app/src/com/android/bluetooth/btservice/ProfileService.java
@@ -124,6 +124,20 @@ public abstract class ProfileService extends ContextWrapper {
         if (component == null) {
             return;
         }
+
+        // Ensure the state in PackageManager has DISABLED to ENABLED to trigger PACKAGE_CHANGED
+        Log.e(mName, "setComponentAvailable DCs_fix_1");
+        if (enable
+                && getPackageManager().getComponentEnabledSetting(component)
+                        == PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {
+            getPackageManager()
+                    .setComponentEnabledSetting(
+                            component,
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP | PackageManager.SYNCHRONOUS);
+            Log.e(mName, "setComponentAvailable DCs_fix_2");
+        }
+
         getPackageManager()
                 .setComponentEnabledSetting(
                         component,
