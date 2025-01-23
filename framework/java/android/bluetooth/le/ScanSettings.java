b5ddc4e5fc785d5f7d2d532b86d806fcc1995dd8
Kihong Seong <kihongs@google.com>
2025-01-23 01:28:04 +0000

    Change default number of trackable advertisements for onFound filter
    The current default number of trackable advertisements set for each
    onFound filter is 'MATCH_NUM_MAX_ADVERTISEMENT', which is 'Max hardware allows / 2'. This is exhausting
    resources and causing errors. Therefore, changing the default to
    'MATCH_NUM_FEW_ADVERTISEMENT', and also changing the value of
    'MATCH_NUM_MAX_ADVERTISEMENT' to 'Max / 4'.
    
    Bug: 386727721
    Bug: 389568695
    Bug: 391958027
    Test: atest ScanManagerTest, ScanSettingsTest
    Change-Id: I39cb19fd6e299c9b74c441fc3da35f232cc8ec74

--- framework/java/android/bluetooth/le/ScanSettings.java
+++ framework/java/android/bluetooth/le/ScanSettings.java
@@ -17,9 +17,14 @@
 package android.bluetooth.le;
 
 import android.annotation.SystemApi;
+import android.app.compat.CompatChanges;
 import android.bluetooth.BluetoothDevice;
+import android.compat.annotation.ChangeId;
+import android.compat.annotation.EnabledSince;
 import android.os.Parcel;
 import android.os.Parcelable;
+
+import com.android.bluetooth.flags.Flags;
 
 /**
  * Bluetooth LE scan settings are passed to {@link BluetoothLeScanner#startScan} to define the
@@ -162,6 +167,11 @@ public final class ScanSettings implements Parcelable {
      */
     public static final int PHY_LE_ALL_SUPPORTED = 255;
 
+    // TODO: b/391981111 - Change 36 to VERSION_CODES.BAKLAVA when available.
+    @ChangeId
+    @EnabledSince(targetSdkVersion = 36)
+    static final long CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER = 391958027L;
+
     // Bluetooth LE scan mode.
     private int mScanMode;
 
@@ -293,6 +303,14 @@ public final class ScanSettings implements Parcelable {
         private int mNumOfMatchesPerFilter = MATCH_NUM_MAX_ADVERTISEMENT;
         private boolean mLegacy = true;
         private int mPhy = PHY_LE_ALL_SUPPORTED;
+
+        // Instance initializer for mNumOfMatchesPerFilter
+        {
+            if (Flags.changeDefaultTrackableAdvNumber()
+                    && CompatChanges.isChangeEnabled(CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)) {
+                mNumOfMatchesPerFilter = MATCH_NUM_FEW_ADVERTISEMENT;
+            }
+        }
 
         /**
          * Set scan mode for Bluetooth LE scan.
--- framework/tests/unit/src/android/bluetooth/le/ScanSettingsTest.java
+++ framework/tests/unit/src/android/bluetooth/le/ScanSettingsTest.java
@@ -16,8 +16,21 @@
 
 package android.bluetooth.le;
 
+import static android.bluetooth.le.ScanSettings.CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER;
+
+import static com.google.common.truth.Truth.assertThat;
+
 import static org.junit.Assert.assertThrows;
 
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
+
+import com.android.bluetooth.flags.Flags;
+
+import libcore.junit.util.compat.CoreCompatChangeRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -25,6 +38,8 @@ import org.junit.runners.JUnit4;
 /** Test for Bluetooth LE {@link ScanSettings}. */
 @RunWith(JUnit4.class)
 public class ScanSettingsTest {
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
 
     @Test
     public void testCallbackType() {
@@ -102,5 +117,14 @@ public class ScanSettingsTest {
                         builder.setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES_AUTO_BATCH)
                                 .setReportDelay(0)
                                 .build());
+    }
+
+    @Test
+    @CoreCompatChangeRule.DisableCompatChanges(CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)
+    @RequiresFlagsEnabled(Flags.FLAG_CHANGE_DEFAULT_TRACKABLE_ADV_NUMBER)
+    public void builderInitialize() {
+        ScanSettings settings = new ScanSettings.Builder().build();
+
+        assertThat(settings.getNumOfMatches()).isEqualTo(ScanSettings.MATCH_NUM_FEW_ADVERTISEMENT);
     }
 }
