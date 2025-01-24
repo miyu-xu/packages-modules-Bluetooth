6da17c8852f7b95e7b02807aaeea3c5da9ea8629
Bhakthavatsala Raghavendra <bhaktha@google.com>
2025-01-24 01:16:23 +0000

    Consider internal buffer content in BluetoothSocket#available
    Test: mmm packages/modules/Bluetooth
    flag: Exempt
    Bug: 0000000
    Change-Id: I3ec90d95799398674b4650533e00d18d0ee67c15

--- framework/java/android/bluetooth/BluetoothSocket.java
+++ framework/java/android/bluetooth/BluetoothSocket.java
@@ -954,7 +954,20 @@ public final class BluetoothSocket implements Closeable {
 
     /*package*/ int available() throws IOException {
         if (VDBG) Log.d(TAG, "available: " + mSocketIS);
-        return mSocketIS.available();
+        int available = 0;
+        if (mType == TYPE_L2CAP || mType == TYPE_L2CAP_LE) {
+            if (mL2capBuffer == null) {
+                createL2capRxBuffer();
+            }
+            available = mL2capBuffer.remaining();
+            if (available == 0) {
+                available = mSocketIS.available();
+            }
+        } else {
+            available = mSocketIS.available();
+        }
+        Log.d(TAG, "available returns: " + available);
+        return available;
     }
 
     /*package*/ int read(byte[] b, int offset, int length) throws IOException {
