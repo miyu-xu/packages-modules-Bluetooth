592ae8bf3af311285ee8615e3a65101ff57dc222
Bhakthavatsala Raghavendra <bhaktha@google.com>
2025-01-27 18:40:47 +0000

    Add testcase for BluetoothSocket#available
    Add test to ensure BluetoothSocket#available,
    - returns proper value after reading few bytes
    - returns proper value after socket is closed
    
    Test: mmm packages/modules/Bluetooth
    flag: Exempt
    Bug: 391623333
    
    Change-Id: I82b9e2fec1ef4934c6f842fd31e3305b9e68474c

--- framework/tests/bumble/src/android/bluetooth/DckL2capTest.kt
+++ framework/tests/bumble/src/android/bluetooth/DckL2capTest.kt
@@ -271,6 +271,54 @@ public class DckL2capTest() : Closeable {
         Log.d(TAG, "testReadReturnOnRemoteSocketDisconnect: done")
     }
 
+    @Test
+    @VirtualOnly
+    fun testBluetoothSocketAvailable() {
+        Log.d(TAG, "testBluetoothSocketAvailable: Connect L2CAP")
+        var bluetoothSocket: BluetoothSocket?
+        val l2capServer = bluetoothAdapter.listenUsingInsecureL2capChannel()
+        val socketFlow = flow { emit(l2capServer.accept()) }
+        val connectResponse = createAndConnectL2capChannelWithBumble(l2capServer.psm)
+        runBlocking {
+            bluetoothSocket = socketFlow.first()
+            assertThat(connectResponse.hasChannel()).isTrue()
+        }
+
+        val inputStream = bluetoothSocket!!.inputStream
+        val sampleData: ByteString = ByteString.copyFromUtf8("cafe-place")
+        val buffer = ByteArray(sampleData.size())
+
+        val sendRequest =
+            SendRequest.newBuilder().setChannel(connectResponse.channel).setData(sampleData).build()
+        Log.d(TAG, "testBluetoothSocketAvailable: Send data from Bumble to Android")
+        mBumble.l2capBlocking().send(sendRequest)
+
+        // delay ensures data is arrived at the server side
+        Thread.sleep(1000 * 5)
+        // Ensure that data len available is equal to what is written from client
+        assertThat(inputStream.available()).isEqualTo(sampleData.size())
+
+        Log.d(TAG, "testBluetoothSocketAvailable: Receive data on Android")
+        // read 1 byte
+        val read = inputStream.read()
+
+        // Ensure that data len available is equal to 1 less as 1 byte read in previous call
+        assertThat(inputStream.available()).isEqualTo(sampleData.size() - 1)
+
+        Log.d(TAG, "testBluetoothSocketAvailable: disconnect")
+        val disconnectRequest =
+            DisconnectRequest.newBuilder().setChannel(connectResponse.channel).build()
+        val disconnectResponse = mBumble.l2capBlocking().disconnect(disconnectRequest)
+        assertThat(disconnectResponse.hasSuccess()).isTrue()
+
+        Thread.sleep(1000 * 20)
+        inputStream.close()
+        bluetoothSocket?.close()
+        assertThat(inputStream.available()).isEqualTo(0)
+        l2capServer.close()
+        Log.d(TAG, "testReceive: done")
+    }
+
     private fun createAndConnectL2capChannelWithBumble(psm: Int): ConnectResponse {
         Log.d(TAG, "createAndConnectL2capChannelWithBumble")
         val remoteDevice =
