57b54dfd5f8ced273d3adbe75225f5d77ce77acb
Jakub Tyszkowski <tyszkowski@google.com>
2025-01-28 14:02:18 +0000

    LeAudio: Improve detecting offload capabilities of GMAP
    Bug: 371450112
    Test: TBD
    Flag: EXEMPT; day to day fix
    Change-Id: Ibf0598dae512dd35efc792d77ad7f0cf68382432

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -6876,6 +6876,11 @@ void LeAudioClient::Initialize(
                 bluetooth::gmap::UGGFeatureBitMask::NinetySixKbpsSourceFeatureSupport);
         break;
       }
+      if (capa.channel_count > bluetooth::le_audio::LE_AUDIO_CHANNEL_COUNT_INDEX_1) {
+        UGG_feature |=
+                static_cast<uint8_t>(bluetooth::gmap::UGGFeatureBitMask::MultiplexFeatureSupport);
+        break;
+      }
     }
     GmapServer::Initialize(UGG_feature);
   }
