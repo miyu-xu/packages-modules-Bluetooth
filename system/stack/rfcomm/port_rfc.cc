eb13fd163232976af5843419c97af5dd19fa23f7
Jayden Kim <jaydenk@google.com>
2025-01-29 06:35:51 +0000

    Indicate RFCOMM connection complete when modem signal is confirmed
    Bug: 342012881
    Test: m com.android.btservices
    Change-Id: I3ec8652cb19a7e4dc1bd1f6e63d807374d68e503

--- system/stack/rfcomm/port_rfc.cc
+++ system/stack/rfcomm/port_rfc.cc
@@ -452,10 +452,12 @@ void PORT_DlcEstablishInd(tRFC_MCB* p_mcb, uint8_t dlci, uint16_t mtu) {
     (p_port->p_callback)(PORT_EV_CONNECTED, p_port->handle);
   }
 
-  if (p_port->p_mgmt_callback) {
-    p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
-    log_counter_metrics(android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_IND,
-                        1);
+  if (!p_port->rfc_cfg_info.hardware_data_path) {
+    if (p_port->p_mgmt_callback) {
+      p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
+      log_counter_metrics(android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_IND,
+                          1);
+    }
   }
 
   p_port->state = PORT_CONNECTION_STATE_OPENED;
@@ -466,9 +468,8 @@ void PORT_DlcEstablishInd(tRFC_MCB* p_mcb, uint8_t dlci, uint16_t mtu) {
  * Function         PORT_DlcEstablishCnf
  *
  * Description      This function is called from the RFCOMM layer when peer
- *                  acknowledges establish procedure (SABME/UA).  Send reply
- *                  to the user and set state to OPENED if result was
- *                  successful.
+ *                  acknowledges establish procedure (SABME/UA).  Set state to
+ *                  OPENED if result was successful.
  *
  ******************************************************************************/
 void PORT_DlcEstablishCnf(tRFC_MCB* p_mcb, uint8_t dlci, uint16_t mtu, uint16_t result) {
@@ -499,11 +500,14 @@ void PORT_DlcEstablishCnf(tRFC_MCB* p_mcb, uint8_t dlci, uint16_t mtu, uint16_t 
     (p_port->p_callback)(PORT_EV_CONNECTED, p_port->handle);
   }
 
-  if (p_port->p_mgmt_callback) {
-    p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
-    log_counter_metrics(android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_CNF,
-                        1);
+  if (!p_port->rfc_cfg_info.hardware_data_path) {
+    if (p_port->p_mgmt_callback) {
+      p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
+      log_counter_metrics(android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_CNF,
+                          1);
+    }
   }
+
   p_port->state = PORT_CONNECTION_STATE_OPENED;
 
   /* RPN is required only if we want to tell DTE how the port should be opened
@@ -636,6 +640,17 @@ void PORT_ControlInd(tRFC_MCB* p_mcb, uint8_t dlci, tPORT_CTRL* p_pars) {
                (p_port->peer_ctrl.modem_signal & MODEM_SIGNAL_RTSCTS) ? 1 : 0,
                (p_port->peer_ctrl.modem_signal & MODEM_SIGNAL_RI) ? 1 : 0,
                (p_port->peer_ctrl.modem_signal & MODEM_SIGNAL_DCD) ? 1 : 0);
+
+  if (p_port->rfc_cfg_info.hardware_data_path) {
+    log::verbose("port_ctrl: 0x{:x}", p_port->port_ctrl);
+    if (p_port->port_ctrl == PORT_CTRL_SETUP_COMPLETED) {
+      if (p_port->p_mgmt_callback) {
+        p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
+        log_counter_metrics(
+                android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_IND, 1);
+      }
+    }
+  }
 }
 
 /*******************************************************************************
@@ -672,6 +687,17 @@ void PORT_ControlCnf(tRFC_MCB* p_mcb, uint8_t dlci, tPORT_CTRL* /* p_pars */) {
    */
   if (event && p_port->p_callback) {
     (p_port->p_callback)(event, p_port->handle);
+  }
+
+  if (p_port->rfc_cfg_info.hardware_data_path) {
+    log::verbose("port_ctrl: 0x{:x}", p_port->port_ctrl);
+    if (p_port->port_ctrl == PORT_CTRL_SETUP_COMPLETED) {
+      if (p_port->p_mgmt_callback) {
+        p_port->p_mgmt_callback(PORT_SUCCESS, p_port->handle);
+        log_counter_metrics(
+                android::bluetooth::CodePathCounterKeyEnum::RFCOMM_CONNECTION_SUCCESS_CNF, 1);
+      }
+    }
   }
 }
 
