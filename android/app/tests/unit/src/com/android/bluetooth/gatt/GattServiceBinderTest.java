8d8fa655082eae35b34db2af22b5bbf494ce7f0b
Kevin Lockwood <klockwood@google.com>
2025-02-01 15:57:31 -0800

    Updated GattServiceTest and GattServiceBinderTest for BluetoothGatt#requestSubrateMode
    Test: atest GattServiceTest, atest GattServiceBinderTest
    Bug: 384113880
    Flag: com.android.bluetooth.flags.le_subrate_api
    
    Change-Id: I337762ac086a1ac74b051a3c44ad828c021500de

--- android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceBinderTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/gatt/GattServiceBinderTest.java
@@ -22,6 +22,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
@@ -50,6 +51,8 @@ public class GattServiceBinderTest {
     @Rule public final MockitoRule mMockitoRule = new MockitoRule();
 
     @Mock private GattService mService;
+
+    private final BluetoothAdapter mAdapter = BluetoothAdapter.getDefaultAdapter();
 
     private AttributionSource mAttributionSource;
 
@@ -380,6 +383,17 @@ public class GattServiceBinderTest {
                         minConnectionEventLen,
                         maxConnectionEventLen,
                         mAttributionSource);
+    }
+
+    @Test
+    public void subrateModeRequest() throws Exception {
+        int clientIf = 1;
+        BluetoothDevice testDevice = mAdapter.getRemoteDevice("00:01:02:03:04:05");
+        int subrateMode = 0;
+
+        mBinder.subrateModeRequest(clientIf, testDevice, subrateMode, mAttributionSource);
+
+        verify(mService).subrateModeRequest(clientIf, testDevice, subrateMode);
     }
 
     @Test
