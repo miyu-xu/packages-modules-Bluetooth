f2f71562008a67f8ca0b39e8f9fa524532289de4
Michal Belusiak <belusiak@google.com>
2025-02-03 10:11:59 +0000

    Bass: Remove sink broadcast metadata in source added failed
    Follow up on "Add support for multiple sources per sink device"
    
    Sink metadata must be removed as it is no longer valid.
    If not removed, the sink may attempt to synchronize with it
    during resume (for another broadcast) or reconnection.
    
    Bug: 385389212
    Flag: EXEMPT, day-to-day bugfix covered by unit tests
    Test: atest BassClientServiceTest
    Change-Id: I136029cc1767bedbda26d7ddc7de9157d64d85cb

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -1238,6 +1238,11 @@ public class BassClientService extends ProfileService {
         }
     }
 
+    private void localNotifySourceAddFailed(
+            BluetoothDevice sink, BluetoothLeBroadcastMetadata source) {
+        removeSinkMetadata(sink, source.getBroadcastId());
+    }
+
     private void setSourceGroupManaged(BluetoothDevice sink, int sourceId, boolean isGroupOp) {
         log("setSourceGroupManaged device: " + sink);
         if (isGroupOp) {
@@ -4582,6 +4587,7 @@ public class BassClientService extends ProfileService {
         void notifySourceAddFailed(
                 BluetoothDevice sink, BluetoothLeBroadcastMetadata source, int reason) {
             sService.checkAndResetGroupAllowedContextMask();
+            sService.localNotifySourceAddFailed(sink, source);
 
             sEventLogger.loge(
                     TAG,
