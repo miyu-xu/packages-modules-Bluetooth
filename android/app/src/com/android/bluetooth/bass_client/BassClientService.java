be8ad976ac132cea46dfdfc650d971451ac53979
Michal Belusiak <belusiak@google.com>
2025-02-03 10:54:26 +0000

    Bass: Store sink metadata per each sink in modify source
    Follow up on "Add support for multiple sources per sink device"
    
    Bug: 385389212
    Flag: EXEMPT, day-to-day bugfix covered by unit tests
    Test: atest BassClientServiceTest
    Change-Id: I29df4ba66fe1725368717c99e3563ebe432be75e

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -3498,9 +3498,6 @@ public class BassClientService extends ProfileService {
             return;
         }
 
-        /* Update metadata for sink device */
-        storeSinkMetadata(sink, updatedMetadata.getBroadcastId(), updatedMetadata);
-
         byte[] code = updatedMetadata.getBroadcastCode();
         for (Map.Entry<BluetoothDevice, Integer> deviceSourceIdPair : devices.entrySet()) {
             BluetoothDevice device = deviceSourceIdPair.getKey();
@@ -3524,6 +3521,9 @@ public class BassClientService extends ProfileService {
                         device, sourceId, BluetoothStatusCodes.ERROR_ALREADY_IN_TARGET_STATE);
                 continue;
             }
+
+            /* Update metadata for sink device */
+            storeSinkMetadata(device, updatedMetadata.getBroadcastId(), updatedMetadata);
 
             sEventLogger.logd(
                     TAG,
