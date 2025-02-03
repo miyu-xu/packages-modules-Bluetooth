99906357a744ee5c21029fbf6e0dfdb3ec9a3d18
Michal Belusiak <belusiak@google.com>
2025-02-03 10:40:45 +0000

    Bass: Remove sink broadcast metadata in source switch during suspend
    Follow up on "Add support for multiple sources per sink device"
    
    Sink metadata must be removed as it is no longer valid.
    If not removed, the sink may attempt to synchronize with it
    during resume (for another broadcast) or reconnection.
    
    Even if there is a room for broadcast, it could happen that all
    broadcasts were suspended via removing source. In that case, we have
    to found such broadcast and remove it from metadata.
    
    checkAndStopBigMonitoring will not remove HOST_INTENTIONAL pause type
    if there are any paused sinks to give possibility of check it.
    
    Bug: 385389212
    Flag: EXEMPT, day-to-day bugfix covered by unit tests
    Test: atest BassClientServiceTest
    Change-Id: If4a4cb584d1d0b990d68f5a5c912ff68e484257a

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -3197,6 +3197,30 @@ public class BassClientService extends ProfileService {
         }
 
         mBroadcastMetadataMap.remove(device);
+
+    private void checkIfBroadcastIsSuspendedBySourceRemovalAndClearData(
+            BluetoothDevice device, BassClientStateMachine stateMachine) {
+        int broadcastIdToRemove = BassConstants.INVALID_BROADCAST_ID;
+        if (mPausedBroadcastSinks.contains(device)) {
+            Map<Integer, BluetoothLeBroadcastMetadata> entry = mBroadcastMetadataMap.get(device);
+            if (entry != null) {
+                if (entry.keySet().size() >= stateMachine.getMaximumSourceCapacity()) {
+                    for (Integer broadcastId : entry.keySet()) {
+                        // Found broadcastId which is paused by host but not synced
+                        if (!getAllSources(device).stream()
+                                        .anyMatch(rs -> (rs.getBroadcastId() == broadcastId))
+                                && isHostPauseType(broadcastId)) {
+                            broadcastIdToRemove = broadcastId;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (broadcastIdToRemove != BassConstants.INVALID_BROADCAST_ID) {
+                stopBigMonitoring(broadcastIdToRemove, false);
+                removeSinkMetadata(device, broadcastIdToRemove);
+            }
+        }
     }
 
     /**
@@ -3403,6 +3427,11 @@ public class BassClientService extends ProfileService {
                     continue;
                 }
             }
+
+            // Even if there is a room for broadcast, it could happen that all broadcasts were
+            // suspended via removing source. In that case, we have to found such broadcast and
+            // remove it from metadata.
+            checkIfBroadcastIsSuspendedBySourceRemovalAndClearData(device, stateMachine);
 
             /* Store metadata for sink device */
             storeSinkMetadata(device, sourceMetadata.getBroadcastId(), sourceMetadata);
@@ -4117,7 +4146,12 @@ public class BassClientService extends ProfileService {
             if (!isAnyReceiverSyncedToBroadcast(pausedBroadcastId)) {
                 mTimeoutHandler.stop(pausedBroadcastId, MESSAGE_BIG_MONITOR_TIMEOUT);
                 mTimeoutHandler.stop(pausedBroadcastId, MESSAGE_BROADCAST_MONITOR_TIMEOUT);
-                iterator.remove();
+
+                if (isSinkUnintentionalPauseType(pausedBroadcastId)
+                        || (isHostPauseType(pausedBroadcastId)
+                                && mPausedBroadcastSinks.isEmpty())) {
+                    iterator.remove();
+                }
                 synchronized (mSearchScanCallbackLock) {
                     // when searching is stopped then stop active sync
                     if (!isSearchInProgress()) {
