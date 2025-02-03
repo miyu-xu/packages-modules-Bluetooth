33965fef2fe51a27df52f7ec17c7daf761f76b16
Pomai Ahlo <poahlo@google.com>
2025-02-03 15:09:59 -0800

    RFCOMM metrics V2: Add metrics functions
    - Collect metrics when sdp fails for HFP and RFCOMM
    - Collect metrics when port creation fails for HFP and RFCOMM
    - Collect metrics when port closes
    
    Bug: 374989690
    Test: m com.android.bt
    Flag: EXEMPT metrics no-op
    Change-Id: Ie2de6f0305afad58ab7d2cd7fdcf988c21649a28

--- system/test/mock/mock_main_shim_metrics_api.cc
+++ system/test/mock/mock_main_shim_metrics_api.cc
@@ -58,6 +58,7 @@ struct LogMetricManufacturerInfo LogMetricManufacturerInfo;
 struct LogMetricLeConnectionStatus LogMetricLeConnectionStatus;
 struct LogMetricLeDeviceInAcceptList LogMetricLeDeviceInAcceptList;
 struct LogMetricLeConnectionLifecycle LogMetricLeConnectionLifecycle;
+struct LogMetricRfcommConnectionAtClose LogMetricRfcommConnectionAtClose;
 
 }  // namespace main_shim_metrics_api
 }  // namespace mock
@@ -207,4 +208,17 @@ void bluetooth::shim::LogMetricLeConnectionLifecycle(bluetooth::hci::Address add
   inc_func_call_count(__func__);
   test::mock::main_shim_metrics_api::LogMetricLeConnectionLifecycle(address, is_connect, is_direct);
 }
+void bluetooth::shim::LogMetricRfcommConnectionAtClose(
+        const RawAddress& raw_address, android::bluetooth::rfcomm::PortResult close_reason,
+        android::bluetooth::rfcomm::SocketConnectionSecurity security,
+        android::bluetooth::rfcomm::RfcommPortEvent last_event,
+        android::bluetooth::rfcomm::RfcommPortState previous_state, int32_t open_duration_ms,
+        int32_t uid, android::bluetooth::BtaStatus sdp_status, bool is_server, bool sdp_initiated,
+        int32_t sdp_duration_ms) {
+  inc_func_call_count(__func__);
+  test::mock::main_shim_metrics_api::LogMetricRfcommConnectionAtClose(
+          raw_address, close_reason, security, last_event, previous_state, open_duration_ms, uid,
+          sdp_status, is_server, sdp_initiated, sdp_duration_ms);
+}
+
 // END mockcify generation
