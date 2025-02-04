f1f129eddcb1a0f0f8f3c83c94cb8e28d19282b8
Himanshu Rawat <rwt@google.com>
2025-02-04 11:46:42 -0800

    Add IsSerializable test for UUID.
    Ensure that Uuid objects can be directly serialized as UUID128Bit objects
    
    Change-Id: Iee83e4380585df4bbfd38b41eedbaf7f58c2e449

--- system/types/test/bluetooth/uuid_unittest.cc
+++ system/types/test/bluetooth/uuid_unittest.cc
@@ -19,6 +19,7 @@
 #include "types/bluetooth/uuid.h"
 
 #include <gtest/gtest.h>
+#include <cstdint>
 
 using bluetooth::Uuid;
 
@@ -37,6 +38,18 @@ constexpr Uuid kBase =
 TEST(UuidTest, IsEmpty) {
   EXPECT_TRUE(Uuid::kEmpty.IsEmpty());
   EXPECT_FALSE(kBase.IsEmpty());
+}
+
+TEST(UuidTest, IsSerializable) {
+  EXPECT_TRUE(std::is_pod<Uuid>());
+  EXPECT_TRUE(std::is_pod<Uuid::UUID128Bit>());
+  EXPECT_EQ(sizeof(Uuid), Uuid::kNumBytes128);
+  EXPECT_EQ(sizeof(Uuid::UUID128Bit), Uuid::kNumBytes128);
+  const Uuid::UUID128Bit& ones_bytes = ONES.To128BitBE();
+  uint8_t* p_ONES = (uint8_t*)&ONES;
+  uint8_t* p_ones_bytes = (uint8_t*)&ones_bytes;
+  EXPECT_EQ(p_ONES, p_ones_bytes);
+  EXPECT_EQ(0, memcmp(p_ONES, p_ones_bytes, sizeof(Uuid)));
 }
 
 TEST(UuidTest, GetShortestRepresentationSize) {
