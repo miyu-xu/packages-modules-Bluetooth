0c656ebf2586b17611d42e6157ecca7a8eee7e00
Kyunglyul Hyun <klhyun@google.com>
2025-02-06 10:22:41 +0000

    Fix potential NPE
    mRemoteDevice can be null if the service
    is closed just before broadcast is received.
    
    Add null check to ignore that intent.
    
    Bug: 394246502
    Test: atest BluetoothInstrumentationTests
    
    Change-Id: If2112023ebe7819994be30ff2fc6d70799c08386

--- android/app/src/com/android/bluetooth/map/BluetoothMapService.java
+++ android/app/src/com/android/bluetooth/map/BluetoothMapService.java
@@ -1107,7 +1107,7 @@ public class BluetoothMapService extends ProfileService {
                 Log.d(TAG, "USER_CONFIRM_TIMEOUT ACTION Received.");
                 sendConnectTimeoutMessage();
             } else if (action.equals(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY)) {
-
+                BluetoothDevice remoteDevice = mRemoteDevice;
                 int requestType =
                         intent.getIntExtra(
                                 BluetoothDevice.EXTRA_ACCESS_REQUEST_TYPE,
@@ -1121,6 +1121,9 @@ public class BluetoothMapService extends ProfileService {
                                 + mIsWaitingAuthorization);
                 if ((!mIsWaitingAuthorization)
                         || (requestType != BluetoothDevice.REQUEST_TYPE_MESSAGE_ACCESS)) {
+                    return;
+                }
+                if (remoteDevice == null) {
                     return;
                 }
 
@@ -1139,13 +1142,13 @@ public class BluetoothMapService extends ProfileService {
                     mPermission = BluetoothDevice.ACCESS_ALLOWED;
                     if (intent.getBooleanExtra(BluetoothDevice.EXTRA_ALWAYS_ALLOWED, false)) {
                         boolean result =
-                                mRemoteDevice.setMessageAccessPermission(
+                                remoteDevice.setMessageAccessPermission(
                                         BluetoothDevice.ACCESS_ALLOWED);
                         Log.d(TAG, "setMessageAccessPermission(ACCESS_ALLOWED) result=" + result);
                     }
 
                     mAdapterService.sdpSearch(
-                            mRemoteDevice, BluetoothMnsObexClient.BLUETOOTH_UUID_OBEX_MNS);
+                            remoteDevice, BluetoothMnsObexClient.BLUETOOTH_UUID_OBEX_MNS);
                     mSdpSearchInitiated = true;
                 } else {
                     // Auth. declined by user, serverSession should not be running, but
@@ -1153,7 +1156,7 @@ public class BluetoothMapService extends ProfileService {
                     mPermission = BluetoothDevice.ACCESS_REJECTED;
                     if (intent.getBooleanExtra(BluetoothDevice.EXTRA_ALWAYS_ALLOWED, false)) {
                         boolean result =
-                                mRemoteDevice.setMessageAccessPermission(
+                                remoteDevice.setMessageAccessPermission(
                                         BluetoothDevice.ACCESS_REJECTED);
                         Log.d(TAG, "setMessageAccessPermission(ACCESS_REJECTED) result=" + result);
                     }
