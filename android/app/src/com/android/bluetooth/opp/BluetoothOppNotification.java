bea49e7cb3986034910f591da368b2f9f536c93e
Hyundo Moon <hdmoon@google.com>
2025-02-10 18:20:07 +0900

    Opp: Fix that group summary notification does not disappear
    Bug: 393984953
    Bug: 393984953
    Test: m -j;
    Test: Manual, send/receive files and open the files,
          the notification group summary should disappear
    Change-Id: I4585bf4dbfc93ba23e20526ea0ae6339d90e3f88

--- android/app/src/com/android/bluetooth/opp/BluetoothOppNotification.java
+++ android/app/src/com/android/bluetooth/opp/BluetoothOppNotification.java
@@ -51,6 +51,7 @@ import android.util.Log;
 import com.android.bluetooth.BluetoothMethodProxy;
 import com.android.bluetooth.R;
 import com.android.bluetooth.Utils;
+import com.android.bluetooth.flags.Flags;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -630,6 +631,10 @@ class BluetoothOppNotification {
                             .setLocalOnly(true);
 
             mNotificationMgr.notify(NOTIFICATION_ID_COMPLETE_SUMMARY, b.build());
+        }
+
+        if (Flags.oppRemoveEmptyGroupNotification() && inboundNum == 0 && outboundNum == 0) {
+            mNotificationMgr.cancel(NOTIFICATION_ID_COMPLETE_SUMMARY);
         }
     }
 
