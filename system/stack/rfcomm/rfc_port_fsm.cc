5c4bcd8aa2ea7b1b4460392067afa525fe746fe0
Miwei Shi <shimiwei@google.com>
2025-02-10 19:57:35 -0800

    Set RFC is_disc_initiator to false after DM.
    This should only be set when an initiator connection
    is established and disc command is sent. After receiving DM on SABM, we do not sent disc command and shall not set this flag.
    
    Bug:386716769
    Change-Id: Ic015f45e887a43917118ca22af3985848d2ec960

--- system/stack/rfcomm/rfc_port_fsm.cc
+++ system/stack/rfcomm/rfc_port_fsm.cc
@@ -245,7 +245,7 @@ void rfc_port_sm_sabme_wait_ua(tPORT* p_port, tRFC_PORT_EVENT event, void* p_dat
 
     case RFC_PORT_EVENT_DM:
       log::warn("RFC_EVENT_DM, handle:{}", p_port->handle);
-      p_port->rfc.p_mcb->is_disc_initiator = true;
+      p_port->rfc.p_mcb->is_disc_initiator = false;
       PORT_DlcEstablishCnf(p_port->rfc.p_mcb, p_port->dlci, p_port->rfc.p_mcb->peer_l2cap_mtu,
                            RFCOMM_ERROR);
       rfc_port_closed(p_port);
