7b5eee077b062503ffcaf5ce3e11557002e9a663
DC Xu <dcxu@google.com>
2025-02-11 02:02:24 -0800

    Disable components of bluetooth package in PackageManager on HAL crash
    This change addresses the issue where telephony fails to re-bind Bluetooth after recovering from a BT HAL crash during an active call. A PACKAGE_CHANGED intent from PackageManager will now trigger re-binding.
    
    Bug: 389540919
    Bug: 391084450
    Test: atest & tester verification
    Flag: com.android.bluetooth.flags.set_component_available_fix
    
    Change-Id: I46d1f56d222d9e2f52424fc12fb54c68c5c5c74d

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1344,7 +1344,6 @@ class BluetoothManagerService {
                 Log.e(TAG, "Unknown service disconnected: " + name);
                 return;
             }
-            mHandler.sendEmptyMessage(MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED);
         }
     }
 
@@ -1576,6 +1575,13 @@ class BluetoothManagerService {
 
                 case MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED:
                     Log.e(TAG, "MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED");
+
+                    String packageName = (String) msg.obj;
+                    if (packageName != null && !packageName.isEmpty()) {
+                        disableBluetoothComponents(packageName);
+                    } else {
+                        Log.i(TAG, "Invalid package name received: " + packageName);
+                    }
 
                     if (!resetAdapter()) {
                         break;
@@ -2451,5 +2457,42 @@ class BluetoothManagerService {
         PackageManager pm = context.getPackageManager();
         return pm.hasSystemFeature(PackageManager.FEATURE_TELEVISION)
                 || pm.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
+    }
+
+    private void disableBluetoothComponents(String packageName) {
+        PackageManager pm = mContext.getPackageManager();
+
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SERVICES |
+                            PackageManager.MATCH_DISABLED_COMPONENTS);
+
+            if (packageInfo == null) {
+                Log.i(TAG, "Failed to get package info for " + packageName);
+                return;
+            }
+
+            // Disable services
+            if (packageInfo.services != null) {
+                for (android.content.pm.ServiceInfo serviceInfo : packageInfo.services) {
+                    ComponentName componentName = new ComponentName(
+                            packageName, serviceInfo.name);
+                    if (pm.getComponentEnabledSetting(componentName)
+                            != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                            !serviceInfo.isEnabled()) {
+                        pm.setComponentEnabledSetting(
+                            componentName,
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
+                        Log.i(TAG, "Disabled component: " + serviceInfo.name);
+                    }
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Package not found: " + packageName, e);
+        } catch (SecurityException e) {
+            Log.e(TAG, "disableBluetoothComponents failed." + e);
+        }
     }
 }
