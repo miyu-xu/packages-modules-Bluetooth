b0983ec44eca8ab2936c750ef35f6ee932bea5e7
Henri Chataing <henrichataing@google.com>
2025-02-11 11:10:37 -0800

    Move SnoopLogger out of the module registry
    Bug: 333555245
    Test: m com.android.bt
    Flag: EXEMPT, no logical change
    Change-Id: I77d3b062ccdc1f508856dd616af64c043277f0e7

--- system/gd/hal/snoop_logger.h
+++ system/gd/hal/snoop_logger.h
@@ -193,6 +193,8 @@ public:
     uint16_t remote_cid;
   };
 
+  SnoopLogger(os::Handler* handler);
+
   // Returns the maximum number of packets per file
   // Changes to this value is only effective after restarting Bluetooth
   static size_t GetMaxPacketsPerFile();
@@ -223,6 +225,8 @@ public:
     OUTGOING,
   };
 
+  void Start() override;
+  void Stop() override;
   void Capture(const HciPacket& packet, Direction direction, PacketType type);
 
   // Set a L2CAP channel as acceptlisted, allowing packets with that L2CAP CID
@@ -267,6 +271,8 @@ public:
   // Dump the contents of the snooz buffer to a file.
   void DumpSnoozLogToFile();
 
+  SnoopLoggerSocketThread const* GetSocketThread() { return snoop_logger_socket_thread_.get(); }
+
 protected:
   // Packet type length
   static const size_t PACKET_TYPE_LENGTH;
@@ -276,16 +282,16 @@ protected:
   // Max packet data size when headersfiltered option enabled
   static const size_t MAX_HCI_ACL_LEN;
 
-  void ListDependencies(ModuleList* list) const override;
-  void Start() override;
-  void Stop() override;
+  void ListDependencies(ModuleList* /*list*/) const override {}
   std::string ToString() const override { return std::string("SnoopLogger"); }
 
-  SnoopLogger(std::string snoop_log_path, std::string snooz_log_path, size_t max_packets_per_file,
-              size_t max_packets_per_buffer, const std::string& btsnoop_mode,
-              bool qualcomm_debug_log_enabled, const std::chrono::milliseconds snooz_log_life_time,
+  SnoopLogger(os::Handler* handler, std::string snoop_log_path, std::string snooz_log_path,
+              size_t max_packets_per_file, size_t max_packets_per_buffer,
+              const std::string& btsnoop_mode, bool qualcomm_debug_log_enabled,
+              const std::chrono::milliseconds snooz_log_life_time,
               const std::chrono::milliseconds snooz_log_delete_alarm_interval,
               bool snoop_log_persists);
+
   void CloseCurrentSnoopLogFile();
   void OpenNextSnoopLogFile();
   // Enable filters according to their sysprops
@@ -339,6 +345,8 @@ private:
   SnoopLoggerSocketInterface* socket_;
   SyscallWrapperImpl syscall_if;
   bool snoop_log_persists = false;
+
+  friend class SnoopLoggerTest;
 };
 
 }  // namespace hal
