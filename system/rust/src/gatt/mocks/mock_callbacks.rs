41ae045322adbd51f73ce4742a204e92704695c3
Chris Suter <csuter@google.com>
2025-02-12 15:18:53 +1100

    system/rust: Remove static globals from the Rust GATT server
    The use of global variables is prone to error and was arguably the root
    cause of http://b/356462170.  This issue is compounded by the Rust
    module being a module, which meant the dependencies were tricky to keep
    track of.
    
    This CL refactors the initialisation and destruction of the private Rust
    GATT server so that it's done by the code that actually uses the private
    Rust GATT server.  With this change, there is no need for a Rust module.
    
    Other notable changes:
    
    1. GattCallbacksImpl is moved into src/gatt/callbacks.rs
    
    2. After fixing some visibility attributes, it seems execute was unused, so
       I've removed that for now.  We can add it back later if necessary.
    
    3. I've made registering the Arbiter with AclArbiter thread-safe.
    
    Bug: 356462170
    Test: m com.android.btservices
    Flag: EXEMPT refactor only
    Change-Id: I46b5d9858646e65e435d3a091683d866a003969d

--- system/rust/src/gatt/mocks/mock_callbacks.rs
+++ system/rust/src/gatt/mocks/mock_callbacks.rs
@@ -1,14 +1,17 @@
 //! Mocked implementation of GattCallbacks for use in test
 
-use crate::gatt::callbacks::{GattWriteType, TransactionDecision};
+// NOTE: This module is currently unused.
+#![allow(dead_code)]
+
+use crate::gatt::callbacks::{GattCallbacks, GattWriteType, TransactionDecision};
 use crate::gatt::ffi::AttributeBackingType;
 use crate::gatt::ids::{AttHandle, ConnectionId, TransactionId};
 use crate::gatt::server::IndicationError;
-use crate::gatt::GattCallbacks;
 use tokio::sync::mpsc::{self, unbounded_channel, UnboundedReceiver};
 
 /// Routes calls to GattCallbacks into a channel of MockCallbackEvents
-pub struct MockCallbacks(mpsc::UnboundedSender<MockCallbackEvents>);
+/* pub */
+struct MockCallbacks(mpsc::UnboundedSender<MockCallbackEvents>);
 
 impl MockCallbacks {
     /// Constructor. Returns self and the RX side of the associated channel.
@@ -20,6 +23,7 @@ impl MockCallbacks {
 
 /// Events representing calls to GattCallbacks
 #[derive(Debug)]
+#[allow(clippy::enum_variant_names)]
 pub enum MockCallbackEvents {
     /// GattCallbacks#on_server_read_characteristic invoked
     OnServerRead(ConnectionId, TransactionId, AttHandle, AttributeBackingType, u32),
@@ -79,14 +83,5 @@ impl GattCallbacks for MockCallbacks {
         result: Result<(), IndicationError>,
     ) {
         self.0.send(MockCallbackEvents::OnIndicationSentConfirmation(conn_id, result)).unwrap();
-    }
-
-    fn on_execute(
-        &self,
-        conn_id: ConnectionId,
-        trans_id: TransactionId,
-        decision: TransactionDecision,
-    ) {
-        self.0.send(MockCallbackEvents::OnExecute(conn_id, trans_id, decision)).unwrap()
     }
 }
