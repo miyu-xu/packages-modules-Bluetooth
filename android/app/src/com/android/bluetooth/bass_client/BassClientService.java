76707d7709822859f824d81bc6af45fee948aa31
Michal Belusiak <belusiak@google.com>
2025-02-19 11:50:45 +0000

    Bass: Prevent adding already synced broadcast in addSelectSourceRequest
    This could occur when an unintentional paused sink is triggered by a
    music pause while the remote unsyncs from BIS and PA. Subsequently,
    addSelectSourceRequest is called on every onScanResult, even if the
    source is already synced.
    
    It was checked in handleSelectSourceRequest, but there was still
    unnecessary adding and retrieval from the queue.
    
    Check directly for the existence of a syncHandle in
    getActiveSyncedSources instead of creating a new list if it's not
    modified or used multiple times
    
    Bug: 390189678
    Flag: EXEMPT, day-to-day bugfix covered by unit tests
    Test: atest BassClientServiceTest
    Change-Id: I8056a9ae048e52a87b3bd15e3ef60799d7edcea4

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -405,10 +405,10 @@ public class BassClientService extends ProfileService {
                                         case MESSAGE_BROADCAST_MONITOR_TIMEOUT:
                                             {
                                                 log("MESSAGE_BROADCAST_MONITOR_TIMEOUT");
-                                                List<Integer> activeSyncedSrc =
-                                                        new ArrayList<>(getActiveSyncedSources());
-                                                if (activeSyncedSrc.contains(
-                                                        getSyncHandleForBroadcastId(broadcastId))) {
+                                                if (getActiveSyncedSources()
+                                                        .contains(
+                                                                getSyncHandleForBroadcastId(
+                                                                        broadcastId))) {
                                                     break;
                                                 }
                                                 // Clear from cache to make possible sync again
@@ -2963,6 +2963,11 @@ public class BassClientService extends ProfileService {
                         + ", hasPriority: "
                         + hasPriority);
 
+        if (getActiveSyncedSources().contains(getSyncHandleForBroadcastId(broadcastId))) {
+            log("addSelectSourceRequest: Already synced");
+            return;
+        }
+
         if (isAddedToSelectSourceRequest(broadcastId, hasPriority)) {
             log("addSelectSourceRequest: Already added");
             return;
@@ -3400,11 +3405,12 @@ public class BassClientService extends ProfileService {
 
         for (BluetoothDevice device : devices) {
             if (leaudioBroadcastExtractPeriodicScannerFromStateMachine()) {
-                List<Integer> activeSyncedSrc = getActiveSyncedSources();
                 BluetoothDevice sourceDevice = sourceMetadata.getSourceDevice();
                 if (!isLocalBroadcast(sourceMetadata)
-                        && (!activeSyncedSrc.contains(
-                                getSyncHandleForBroadcastId(sourceMetadata.getBroadcastId())))) {
+                        && (!getActiveSyncedSources()
+                                .contains(
+                                        getSyncHandleForBroadcastId(
+                                                sourceMetadata.getBroadcastId())))) {
                     log("Adding inactive source: " + sourceDevice);
                     int broadcastId = sourceMetadata.getBroadcastId();
                     if (broadcastId != BassConstants.INVALID_BROADCAST_ID) {
@@ -4394,14 +4400,13 @@ public class BassClientService extends ProfileService {
                         continue;
                     }
 
-                    List<Integer> activeSyncedSrc = getActiveSyncedSources();
-
                     if (receiveState.isPresent()
                             && (!leaudioBroadcastResyncHelper()
                                     || isLocalBroadcast(metadata)
-                                    || activeSyncedSrc.contains(
-                                            getSyncHandleForBroadcastId(
-                                                    metadata.getBroadcastId())))) {
+                                    || getActiveSyncedSources()
+                                            .contains(
+                                                    getSyncHandleForBroadcastId(
+                                                            metadata.getBroadcastId())))) {
                         int sourceId = receiveState.get().getSourceId();
                         updateSourceToResumeBroadcast(sink, sourceId, metadata);
                     } else {
