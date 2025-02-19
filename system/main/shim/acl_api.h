cd6ca3798aa23afb2314f2b6e08a4fec8a67b851
Jakub Pawlowski <jpawlowski@google.com>
2025-02-19 13:25:30 +0100

    Get rid of LE Acl Manager shim
    connection_manager.cc is only user of LE Acl Manager API, so it's safe
    to skip the shim
    
    Bug: 372202918
    Test: atest net_test_conn_multiplexing
    Flag: exempt, well unit-tested
    Change-Id: Id9bd060f55581bcd044c857128df9119eb7d5a3b

--- system/main/shim/acl_api.h
+++ system/main/shim/acl_api.h
@@ -29,8 +29,6 @@ namespace shim {
 
 void ACL_CreateClassicConnection(const RawAddress& raw_address);
 void ACL_CancelClassicConnection(const RawAddress& raw_address);
-void ACL_AcceptLeConnectionFrom(const tBLE_BD_ADDR& legacy_address_with_type, bool is_direct);
-void ACL_IgnoreLeConnectionFrom(const tBLE_BD_ADDR& legacy_address_with_type);
 
 void ACL_Disconnect(uint16_t handle, bool is_classic, tHCI_STATUS reason, std::string comment);
 void ACL_WriteData(uint16_t handle, BT_HDR* p_buf);
