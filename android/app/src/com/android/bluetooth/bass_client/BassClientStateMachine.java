a4b26ce6d475470408db81192fd2b432b2f1fccc
William Escande <wescande@google.com>
2025-02-21 15:48:00 -0800

    Errorprone fix & enforce FieldCanBeFinal
    Bug: 344658662
    Test: m BluetoothInstrumentationTests
    Test: m .
    Flag: Exempt refactor
    Change-Id: Iea773640f276794f32020685feb886b53485a0c1

--- android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientStateMachine.java
@@ -107,8 +107,7 @@ class BassClientStateMachine extends StateMachine {
     static final int CANCEL_PENDING_SOURCE_OPERATION = 15;
     static final int INITIATE_PA_SYNC_TRANSFER = 16;
 
-    // NOTE: the value is not "final" - it is modified in the unit tests
-    @VisibleForTesting private int mConnectTimeoutMs;
+    private final int mConnectTimeoutMs;
 
     // Type of argument for set broadcast code operation
     static final int ARGTYPE_METADATA = 1;
@@ -152,7 +151,7 @@ class BassClientStateMachine extends StateMachine {
     private final Map<Integer, Boolean> mPendingRemove = new HashMap();
     private boolean mDefNoPAS = false;
     private boolean mForceSB = false;
-    private int mBroadcastSourceIdLength = 3;
+    private static final int BROADCAST_SOURCE_ID_LENGTH = 3;
     @VisibleForTesting byte mNextSourceId = 0;
     private boolean mAllowReconnect = false;
     @VisibleForTesting BluetoothGattTestableWrapper mBluetoothGatt = null;
@@ -198,10 +197,10 @@ class BassClientStateMachine extends StateMachine {
     }
 
     private static class LeAudioBroadcastSyncStats {
-        private BluetoothDevice mDevice;
-        private boolean mIsLocalBroadcast;
-        private int mBroadcastId;
-        private long mSourceAddTime;
+        private final BluetoothDevice mDevice;
+        private final boolean mIsLocalBroadcast;
+        private final int mBroadcastId;
+        private final long mSourceAddTime;
         private long mSourcePaSyncedTime;
         private long mSourceBisSyncedTime;
         private int mSyncStatus;
@@ -745,13 +744,13 @@ class BassClientStateMachine extends StateMachine {
                     metadataList.add(BluetoothLeAudioContentMetadata.fromRawBytes(new byte[0]));
                 }
             }
-            byte[] broadcastIdBytes = new byte[mBroadcastSourceIdLength];
+            byte[] broadcastIdBytes = new byte[BROADCAST_SOURCE_ID_LENGTH];
             System.arraycopy(
                     receiverState,
                     BassConstants.BCAST_RCVR_STATE_SRC_BCAST_ID_START_IDX,
                     broadcastIdBytes,
                     0,
-                    mBroadcastSourceIdLength);
+                    BROADCAST_SOURCE_ID_LENGTH);
             int broadcastId = BassUtils.parseBroadcastId(broadcastIdBytes);
             byte[] sourceAddress = new byte[BassConstants.BCAST_RCVR_STATE_SRC_ADDR_SIZE];
             System.arraycopy(
@@ -966,13 +965,13 @@ class BassClientStateMachine extends StateMachine {
                     metadataList.add(BluetoothLeAudioContentMetadata.fromRawBytes(new byte[0]));
                 }
             }
-            byte[] broadcastIdBytes = new byte[mBroadcastSourceIdLength];
+            byte[] broadcastIdBytes = new byte[BROADCAST_SOURCE_ID_LENGTH];
             System.arraycopy(
                     receiverState,
                     BassConstants.BCAST_RCVR_STATE_SRC_BCAST_ID_START_IDX,
                     broadcastIdBytes,
                     0,
-                    mBroadcastSourceIdLength);
+                    BROADCAST_SOURCE_ID_LENGTH);
             int broadcastId = BassUtils.parseBroadcastId(broadcastIdBytes);
             byte[] sourceAddress = new byte[BassConstants.BCAST_RCVR_STATE_SRC_ADDR_SIZE];
             System.arraycopy(
--- android/app/src/com/android/bluetooth/le_scan/ScanController.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanController.java
@@ -1551,7 +1551,7 @@ public class ScanController {
      */
     class ScannerDeathRecipient implements IBinder.DeathRecipient {
         int mScannerId;
-        private String mPackageName;
+        private final String mPackageName;
 
         ScannerDeathRecipient(int scannerId, String packageName) {
             mScannerId = scannerId;
