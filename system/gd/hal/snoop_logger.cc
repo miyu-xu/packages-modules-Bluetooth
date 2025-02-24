a9ece7820e3dbebb906061d0ce7334d3289afa1e
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-02-24 10:04:31 -0800

    Revert "Add custom tracing to bluetooth using Tracing SDK"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/545fd3c7-ea99-4201-b64a-3dc0f50bb5a2). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I652982ee55bb7e82201cd165eeb6de7ac2b2f00c
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- system/gd/hal/snoop_logger.cc
+++ system/gd/hal/snoop_logger.cc
@@ -35,9 +35,6 @@
 #include "common/circular_buffer.h"
 #include "common/strings.h"
 #include "hal/snoop_logger_common.h"
-#ifdef __ANDROID__
-#include "hal/snoop_logger_tracing.h"
-#endif  // __ANDROID__
 #include "hci/hci_packets.h"
 #include "os/files.h"
 #include "os/parameter_provider.h"
@@ -1159,15 +1156,15 @@ void SnoopLogger::Capture(const HciPacket& immutable_packet, Direction direction
   HciPacket& packet = mutable_packet;
   //////////////////////////////////////////////////////////////////////////
 
-  uint64_t timestamp_us = std::chrono::duration_cast<std::chrono::microseconds>(
-                                  std::chrono::system_clock::now().time_since_epoch())
-                                  .count();
-#ifdef __ANDROID__
+  #ifdef __ANDROID__
   if (com::android::bluetooth::flags::snoop_logger_tracing()) {
-    LogTracePoint(timestamp_us, packet, direction, type);
+    LogTracePoint(packet, direction, type);
   }
 #endif  // __ANDROID__
 
+  uint64_t timestamp_us = std::chrono::duration_cast<std::chrono::microseconds>(
+                                  std::chrono::system_clock::now().time_since_epoch())
+                                  .count();
   std::bitset<32> flags = 0;
   switch (type) {
     case PacketType::CMD:
@@ -1318,11 +1315,6 @@ void SnoopLogger::Start() {
       snoop_logger_socket_thread_ = nullptr;
     }
   }
-
-#ifdef __ANDROID__
-  SnoopLoggerTracing::InitializePerfetto();
-#endif  // __ANDROID__
-
   alarm_ = std::make_unique<os::RepeatingAlarm>(GetHandler());
   alarm_->Schedule(common::Bind(&delete_old_btsnooz_files, snooz_log_path_, snooz_log_life_time_),
                    snooz_log_delete_alarm_interval_);
@@ -1435,8 +1427,7 @@ const ModuleFactory SnoopLogger::Factory = ModuleFactory([]() {
 });
 
 #ifdef __ANDROID__
-void SnoopLogger::LogTracePoint(uint64_t timestamp_us, const HciPacket& packet, Direction direction,
-                                PacketType type) {
+void SnoopLogger::LogTracePoint(const HciPacket& packet, Direction direction, PacketType type) {
   switch (type) {
     case PacketType::EVT: {
       uint8_t evt_code = packet[0];
@@ -1482,8 +1473,6 @@ void SnoopLogger::LogTracePoint(uint64_t timestamp_us, const HciPacket& packet, 
       ATRACE_INSTANT_FOR_TRACK(LOG_TAG, message.c_str());
     } break;
   }
-
-  SnoopLoggerTracing::TracePacket(timestamp_us, packet, direction, type);
 }
 #endif  // __ANDROID__
 
