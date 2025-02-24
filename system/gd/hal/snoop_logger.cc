7a43ef65531088cbbdb65d16d66391893542d9f5
Henri Chataing <henrichataing@google.com>
2025-02-24 12:14:15 -0800

    Disable the snoop logger socket on user builds
    See b/ for additional context. Enabling the socket on user
    builds poses a security risk. The socket shoudl be
    reenabled after a security review of the implementation
    is performed.
    
    Bug: 375056207
    Test: m com.android.bt
    Flag: EXEMPT, minor change
    Change-Id: Ia195f0590b42e7765365b4b2551039baa3149dcb

--- system/gd/hal/snoop_logger.cc
+++ system/gd/hal/snoop_logger.cc
@@ -1305,17 +1305,21 @@ void SnoopLogger::Start() {
       EnableFilters();
     }
 
-    auto snoop_logger_socket = std::make_unique<SnoopLoggerSocket>(&syscall_if);
-    snoop_logger_socket_thread_ =
-            std::make_unique<SnoopLoggerSocketThread>(std::move(snoop_logger_socket));
-    auto thread_started_future = snoop_logger_socket_thread_->Start();
-    thread_started_future.wait();
-    if (thread_started_future.get()) {
-      RegisterSocket(snoop_logger_socket_thread_.get());
-    } else {
-      snoop_logger_socket_thread_->Stop();
-      snoop_logger_socket_thread_.reset();
-      snoop_logger_socket_thread_ = nullptr;
+    if (os::GetSystemPropertyBool(kIsDebuggableProperty, false)) {
+      // TODO(b/375056207): Enable the snoop logger socket in user mode
+      // after running a security review of the implementation.
+      auto snoop_logger_socket = std::make_unique<SnoopLoggerSocket>(&syscall_if);
+      snoop_logger_socket_thread_ =
+              std::make_unique<SnoopLoggerSocketThread>(std::move(snoop_logger_socket));
+      auto thread_started_future = snoop_logger_socket_thread_->Start();
+      thread_started_future.wait();
+      if (thread_started_future.get()) {
+        RegisterSocket(snoop_logger_socket_thread_.get());
+      } else {
+        snoop_logger_socket_thread_->Stop();
+        snoop_logger_socket_thread_.reset();
+        snoop_logger_socket_thread_ = nullptr;
+      }
     }
   }
 
