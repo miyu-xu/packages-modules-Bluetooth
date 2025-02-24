27b6a85f40d00ff37ea3e43dfa0336f457ee3b96
Nitin Shivpure <quic_nshivpur@quicinc.com>
2025-02-24 16:38:44 +0530

    Gatt: Enable encryption in main thread
    Enable encryption in main thread.
    
    Change-Id: I8d8468295b57d961f0898808b6fe347bb4a61622

--- system/btif/src/btif_gatt_util.cc
+++ system/btif/src/btif_gatt_util.cc
@@ -20,6 +20,7 @@
 
 #include "btif_gatt_util.h"
 
+#include <base/functional/bind.h>
 #include <bluetooth/log.h>
 #include <hardware/bluetooth.h>
 #include <hardware/bt_gatt.h>
@@ -34,6 +35,7 @@
 #include "os/system_properties.h"
 #include "stack/btm/btm_sec.h"
 #include "stack/include/acl_api.h"
+#include "stack/include/main_thread.h"
 #include "types/ble_address_with_type.h"
 #include "types/bluetooth/uuid.h"
 #include "types/bt_transport.h"
@@ -90,6 +92,7 @@ void btif_gatt_check_encrypted_link(RawAddress bd_addr, tBT_TRANSPORT transport_
       !btif_gatt_is_link_encrypted(bd_addr)) {
     log::debug("Checking gatt link peer:{} transport:{}", bd_addr,
                bt_transport_text(transport_link));
-    BTA_DmSetEncryption(bd_addr, transport_link, &btif_gatt_set_encryption_cb, BTM_BLE_SEC_ENCRYPT);
+    do_in_main_thread(base::BindOnce(BTA_DmSetEncryption, bd_addr, transport_link,
+                                     &btif_gatt_set_encryption_cb, BTM_BLE_SEC_ENCRYPT));
   }
 }
