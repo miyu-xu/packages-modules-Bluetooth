1293d5f74407c52b8aad26eb159eee54ae4ff8ac
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-02-24 18:50:02 -0800

    Revert "leaudio: Inactivate device on autonomous stream release"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/fd7722e7-135f-48ec-8285-696ac9cf10c1). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I3dfcfce5b7b0fbbd4786a66714fa8e32142386c6
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- system/bta/le_audio/state_machine_test.cc
+++ system/bta/le_audio/state_machine_test.cc
@@ -4147,12 +4147,8 @@ TEST_F(StateMachineTest, testAutonomousReleaseMultiple) {
 
   // Validate GroupStreamStatus
   EXPECT_CALL(mock_callbacks_,
-              StatusReportCb(leaudio_group_id,
-                             bluetooth::le_audio::GroupStreamStatus::RELEASING_AUTONOMOUS))
-          .Times(1);
-  EXPECT_CALL(mock_callbacks_,
               StatusReportCb(leaudio_group_id, bluetooth::le_audio::GroupStreamStatus::RELEASING))
-          .Times(0);
+          .Times(1);
   EXPECT_CALL(mock_callbacks_,
               StatusReportCb(leaudio_group_id, bluetooth::le_audio::GroupStreamStatus::IDLE))
           .Times(1);
