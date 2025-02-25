764859613d991ee2bc707fc3216a1f53d1fe2e52
penghua <quic_penghua@quicinc.com>
2025-02-25 10:31:01 +0800

    Report failure if connection collision happens during SDP discovery
    Steps:
    1.  BT ON
    2.  watch APP on DUT initiates SPP connection to remote watch
    
    [Expected result]
    SPP connection failing due ACL collision and APP not able to try any further connection
    
    [Actual result]
    After ACL collision, APP SPP connetcion should work
    
    Repro rate : 5/10 times if we follow above steps
    
    Root cause: When SPP SDP collided, DUT no updating SDP failure
                to APP and hence next RFCOMM connection failing
    
    Fix: Report failure directlly if connection collision happens
    during SDP discovery, because there is no collision callback for SDP
    
    bug :412891895
    
    Test: m Bluetooth
    Flag: EXEMPT, trivial
    
    Change-Id: I8839d789b3e71ea6252ec16e7ac2d4e419c50126

--- system/stack/l2cap/l2c_csm.cc
+++ system/stack/l2cap/l2c_csm.cc
@@ -43,7 +43,7 @@
 #include "stack/include/bt_types.h"
 #include "stack/include/l2cdefs.h"
 #include "stack/l2cap/l2c_int.h"
-
+#include "stack/include/bt_psm_types.h"
 using namespace bluetooth;
 
 /******************************************************************************/
@@ -307,7 +307,7 @@ static void l2c_csm_closed(tL2C_CCB* p_ccb, tL2CEVT event, void* p_data) {
       break;
 
     case L2CEVT_LP_CONNECT_CFM_NEG: /* Link failed          */
-      if (p_ci->hci_status == HCI_ERR_CONNECTION_EXISTS) {
+      if ((p_ci->hci_status == HCI_ERR_CONNECTION_EXISTS) && (!(p_ccb->p_rcb && p_ccb->p_rcb->psm == BT_PSM_SDP))) {
         btm_acl_notif_conn_collision(p_ccb->p_lcb->remote_bd_addr);
       } else {
         l2cu_release_ccb(p_ccb);
