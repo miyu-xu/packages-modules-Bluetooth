5bd92e7b81b34338eb560f3f5fac60cc1406d87a
Jakub Tyszkowski <tyszkowski@google.com>
2025-02-25 13:32:54 +0000

    LeAudio: Improve handling remote device audio locations
    With this change we correctly distinguish devices with Mono audio location (0),
    from devices which do not support any audio location in a paritcular direction.
    This is important for devices which are one direction only - speakers
    only or microphone only.
    
    Bug: 388499616
    Test: atest bluetooth_le_audio_test bluetooth_le_audio_client_test
    Flag: Exempt: day to day fix
    Change-Id: Ia1e81140b5fe1998532d0d979c3287a79c76cde9

--- system/btif/src/btif_le_audio.cc
+++ system/btif/src/btif_le_audio.cc
@@ -71,8 +71,10 @@ class LeAudioClientInterfaceImpl : public LeAudioClientInterface, public LeAudio
                           group_id, node_status));
   }
 
-  void OnAudioConf(uint8_t direction, int group_id, uint32_t snk_audio_location,
-                   uint32_t src_audio_location, uint16_t avail_cont) override {
+  void OnAudioConf(uint8_t direction, int group_id,
+                   std::optional<std::bitset<32>> snk_audio_location,
+                   std::optional<std::bitset<32>> src_audio_location,
+                   uint16_t avail_cont) override {
     do_in_jni_thread(Bind(&LeAudioClientCallbacks::OnAudioConf, Unretained(callbacks), direction,
                           group_id, snk_audio_location, src_audio_location, avail_cont));
   }
