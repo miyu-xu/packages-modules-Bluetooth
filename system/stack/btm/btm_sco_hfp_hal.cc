331176c3ff0d92d5ee6d086144fd66656ec88a2f
Hsin-chen Chuang <chharry@google.com>
2025-02-26 12:06:08 +0800

    HFP HAL: Add software data path implementaion
    Bug: 387424290
    Test: mmm packages/modules/Bluetooth
    Test: Use HFP with the kernel patch in b/387425332
    Flag: EXEMPT, behind sysprop only used by desktop
    Change-Id: Id9d0cbe9e526d38fca61a4926126dd1f737ffb6f

--- system/stack/btm/btm_sco_hfp_hal.cc
+++ system/stack/btm/btm_sco_hfp_hal.cc
@@ -23,29 +23,46 @@
 
 namespace hfp_hal_interface {
 namespace {
-bool offload_supported = true;
-bool offload_enabled = true;
+bool offload_supported;
+bool offload_enabled;
 std::vector<bt_codec> cached_codecs;
 }  // namespace
 
-// Android implementation only has consts. Initialize CVSD and MSBC to PCM
-// offloaded defaults.
 void init() {
-  bt_codec cvsd = {
+  cached_codecs.clear();
+
+  cached_codecs.push_back({
           .codec = codec::CVSD,
           .data_path = ESCO_DATA_PATH_PCM,
           .pkt_size = kDefaultPacketSize,
-  };
+  });
 
-  bt_codec msbc = {
-          .codec = codec::MSBC,
-          .data_path = ESCO_DATA_PATH_PCM,
-          .pkt_size = kDefaultPacketSize,
-  };
+  // We assert WBS support on either SW path (!offload_enabled) or HW path (offload_enabled).
+  // offload_enabled is defined by sysprop and never changes during runtime.
+  if (osi_property_get_bool("bluetooth.hfp.software_datapath.enabled", false)) {
+    offload_supported = false;
+    offload_enabled = false;
+    cached_codecs.push_back({
+            .codec = codec::MSBC_TRANSPARENT,
+            .data_path = ESCO_DATA_PATH_HCI,
+            // TODO(b/387424290): Query the USB cap and select the size here. Currently all devices
+            // support alt 6 so leave it 60 for now.
+            .pkt_size = 60,
+    });
+  } else {
+    offload_supported = true;
+    offload_enabled = true;
+    cached_codecs.push_back({
+            .codec = codec::MSBC,
+            .data_path = ESCO_DATA_PATH_PCM,
+            .pkt_size = kDefaultPacketSize,
+    });
+  }
 
-  cached_codecs.clear();
-  cached_codecs.emplace_back(cvsd);
-  cached_codecs.emplace_back(msbc);
+  for (const auto& c : cached_codecs) {
+    bluetooth::log::info("Caching HFP codec {}, data path {}, pkt_size {}", (uint64_t)c.codec,
+                         c.data_path, c.pkt_size);
+  }
 }
 
 // This is not used in Android.
@@ -54,7 +71,10 @@ bool is_coding_format_supported(esco_coding_format_t /* coding_format */) { retu
 // Android statically compiles WBS support.
 bool get_wbs_supported() { return true; }
 
-bool get_swb_supported() { return osi_property_get_bool("bluetooth.hfp.swb.supported", false); }
+// Software path (!offload_supported) implies support of SWB.
+bool get_swb_supported() {
+  return !offload_supported || osi_property_get_bool("bluetooth.hfp.swb.supported", false);
+}
 
 // Checks the supported codecs
 bt_codecs get_codec_capabilities(uint64_t codecs) {
@@ -75,20 +95,21 @@ bool get_offload_supported() { return offload_supported; }
 // Check if hardware offload is enabled
 bool get_offload_enabled() { return offload_supported && offload_enabled; }
 
-// Set offload enable/disable
-bool enable_offload(bool enable) {
-  if (!offload_supported) {
-    return false;
-  }
-  offload_enabled = enable;
-  return true;
-}
+// This is not used in Android.
+bool enable_offload(bool /* enable */) { return true; }
 
-// On Android, this is a no-op because the settings default to offloaded case.
+// On Android, this is a no-op because the settings default to work and offload mode won't change.
 void set_codec_datapath(tBTA_AG_UUID_CODEC /* codec_uuid */) {}
 
-// No packet size limits on Android since it will be offloaded.
-size_t get_packet_size(int /* codec */) { return kDefaultPacketSize; }
+size_t get_packet_size(int codec) {
+  for (const bt_codec& c : cached_codecs) {
+    if (c.codec == static_cast<uint64_t>(codec)) {
+      return c.pkt_size;
+    }
+  }
+
+  return kDefaultPacketSize;
+}
 
 void notify_sco_connection_change(RawAddress /* device */, bool /* is_connected */,
                                   int /* codec */) {
