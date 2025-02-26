9edc8eee08d122273701d5a21d5ed8535a88dece
Rongxuan Liu <rongxuan@google.com>
2025-02-26 01:08:51 +0000

    [le audio broadcast] Handle late big/iso event in stopped state
    Bug: 399038589
    
    Flag: EXEMPT, trivial fix covered by unit tests
    Test: atest bluetooth_test_broadcaster_state_machine --host --no-bazel-mode
    Change-Id: I9e34369802a59ccc04142cdd126fdaf23514f637

--- system/bta/le_audio/broadcaster/broadcaster.cc
+++ system/bta/le_audio/broadcaster/broadcaster.cc
@@ -898,15 +898,24 @@ public:
 
   void OnSetupIsoDataPath(uint8_t status, uint16_t conn_handle, uint8_t big_handle) override {
     auto broadcast_id = BroadcastIdFromBigHandle(big_handle);
-    log::assert_that(broadcasts_.count(broadcast_id) != 0,
-                     "assert failed: broadcasts_.count(broadcast_id) != 0");
-    broadcasts_[broadcast_id]->OnSetupIsoDataPath(status, conn_handle);
+    if (broadcasts_.count(broadcast_id) == 0) {
+      log::warn("broadcasts_.count(broadcast_id) == 0");
+      /* ISO setup after broadcast destroyed, remove ISO */
+      IsoManager::GetInstance()->RemoveIsoDataPath(
+              conn_handle, bluetooth::hci::iso_manager::kRemoveIsoDataPathDirectionInput);
+    } else {
+      broadcasts_[broadcast_id]->OnSetupIsoDataPath(status, conn_handle);
+    }
   }
 
   void OnRemoveIsoDataPath(uint8_t status, uint16_t conn_handle, uint8_t big_handle) override {
     auto broadcast_id = BroadcastIdFromBigHandle(big_handle);
-    log::assert_that(broadcasts_.count(broadcast_id) != 0,
-                     "assert failed: broadcasts_.count(broadcast_id) != 0");
+    if (broadcasts_.count(broadcast_id) == 0) {
+      log::warn("broadcasts_.count(broadcast_id) == 0");
+      /* No valid broadcast, tearing down the BIG */
+      IsoManager::GetInstance()->TerminateBig(big_handle, 0x13);
+      return;
+    }
     broadcasts_[broadcast_id]->OnRemoveIsoDataPath(status, conn_handle);
   }
 
@@ -915,15 +924,21 @@ public:
       case bluetooth::hci::iso_manager::kIsoEventBigOnCreateCmpl: {
         auto* evt = static_cast<big_create_cmpl_evt*>(data);
         auto broadcast_id = BroadcastIdFromBigHandle(evt->big_id);
-        log::assert_that(broadcasts_.count(broadcast_id) != 0,
-                         "assert failed: broadcasts_.count(broadcast_id) != 0");
-        broadcasts_[broadcast_id]->HandleHciEvent(HCI_BLE_CREATE_BIG_CPL_EVT, evt);
+        if (broadcasts_.count(broadcast_id) == 0) {
+          log::warn("kIsoEventBigOnCreateCmpl, broadcasts_.count(broadcast_id) == 0");
+          // BIG created after broadcast destroyed, remove BIG
+          IsoManager::GetInstance()->TerminateBig(evt->big_id, 0x13);
+        } else {
+          broadcasts_[broadcast_id]->HandleHciEvent(HCI_BLE_CREATE_BIG_CPL_EVT, evt);
+        }
       } break;
       case bluetooth::hci::iso_manager::kIsoEventBigOnTerminateCmpl: {
         auto* evt = static_cast<big_terminate_cmpl_evt*>(data);
         auto broadcast_id = BroadcastIdFromBigHandle(evt->big_id);
-        log::assert_that(broadcasts_.count(broadcast_id) != 0,
-                         "assert failed: broadcasts_.count(broadcast_id) != 0");
+        if (broadcasts_.count(broadcast_id) == 0) {
+          log::warn("kIsoEventBigOnTerminateCmpl, broadcasts_.count(broadcast_id) == 0");
+          return;
+        }
         broadcasts_[broadcast_id]->HandleHciEvent(HCI_BLE_TERM_BIG_CPL_EVT, evt);
         if (!com::android::bluetooth::flags::leaudio_big_depends_on_audio_state()) {
           auto result = CodecManager::GetInstance()->UpdateActiveBroadcastAudioHalClient(
