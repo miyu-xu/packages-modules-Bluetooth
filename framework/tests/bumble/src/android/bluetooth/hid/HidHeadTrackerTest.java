2435f9503ffd28845f0bc537f6bdbe5b472332fc
Gopi Sakshihally Bhuthaiah <bhuthaiah@google.com>
2025-02-27 08:40:25 +0000

    Removed HID Disconnecting state verification when transport switch
    handleMessageSetPreferredTransport() is modified to update
    the connection state to disconnected immediately
    
    Bug: 399286751
    Test: atest -v HidHeadTrackerTest
    Test: atest -v HidHostDualModeTest
    Flag: TEST_ONLY
    Change-Id: Icc6d7cd3e436cfc2ccbdcfd15e8f151f19b13725

--- framework/tests/bumble/src/android/bluetooth/hid/HidHeadTrackerTest.java
+++ framework/tests/bumble/src/android/bluetooth/hid/HidHeadTrackerTest.java
@@ -64,6 +64,7 @@ import android.util.Log;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.android.bluetooth.flags.Flags;
 import com.android.compatibility.common.util.AdoptShellPermissionsRule;
 
 import org.hamcrest.CustomTypeSafeMatcher;
@@ -492,8 +493,10 @@ public class HidHeadTrackerTest {
      */
     private void verifyTransportSwitch(BluetoothDevice device, int fromTransport, int toTransport) {
         assertThat(fromTransport).isNotEqualTo(toTransport);
-        verifyConnectionState(mBumbleDevice, equalTo(fromTransport), equalTo(STATE_DISCONNECTING));
-
+        if (!Flags.ignoreUnselectedHidTransportStates()) {
+            verifyConnectionState(
+                    mBumbleDevice, equalTo(fromTransport), equalTo(STATE_DISCONNECTING));
+        }
         // Capture the next intent with filter
         // Filter is necessary as otherwise it will corrupt all other unordered verifications
         final Intent[] savedIntent = {null};
