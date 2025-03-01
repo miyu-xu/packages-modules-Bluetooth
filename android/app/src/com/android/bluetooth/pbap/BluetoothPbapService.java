7a9e0fe8bf4e3e7ac1fa52dc730535183c151b3f
William Escande <wescande@google.com>
2025-02-28 16:16:32 -0800

    Remove guava Uninterruptibles dependency
    Toward removing guava dependency entirely
    
    This is just a while loop. No need for an external dependency for this
    
    Bug: 311772251
    Test: m .
    Flag: Exempt refactor no-op
    Change-Id: I385635b045f7a347e86e0225797e2b8a6b73245d

--- android/app/src/com/android/bluetooth/pbap/BluetoothPbapService.java
+++ android/app/src/com/android/bluetooth/pbap/BluetoothPbapService.java
@@ -24,6 +24,8 @@ import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED;
 import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_FORBIDDEN;
 import static android.bluetooth.BluetoothProfile.STATE_DISCONNECTED;
 
+import static com.android.bluetooth.Utils.joinUninterruptibly;
+
 import static java.util.Objects.requireNonNull;
 
 import android.annotation.RequiresPermission;
@@ -70,8 +72,6 @@ import com.android.bluetooth.content_profiles.ContentProfileErrorReportUtils;
 import com.android.bluetooth.sdp.SdpManagerNativeInterface;
 import com.android.bluetooth.util.DevicePolicyUtils;
 import com.android.internal.annotations.VisibleForTesting;
-
-import com.google.common.util.concurrent.Uninterruptibles;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -740,7 +740,7 @@ public class BluetoothPbapService extends ProfileService implements IObexConnect
         setBluetoothPbapService(null);
         mSessionStatusHandler.sendEmptyMessage(SHUTDOWN);
         mHandlerThread.quitSafely();
-        Uninterruptibles.joinUninterruptibly(mHandlerThread);
+        joinUninterruptibly(mHandlerThread);
         mContactsLoaded = false;
         unregisterReceiver(mPbapReceiver);
         mAdapterService.getContentResolver().unregisterContentObserver(mContactChangeObserver);
