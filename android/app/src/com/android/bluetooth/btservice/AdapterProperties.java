cbe15f549f5d905af7f4e45fd7b4383f01277c78
Mingbo Zhang <quic_mingboz@quicinc.com>
2025-03-04 18:17:28 +0800

    BQR: adjust vendor part offset based on BQR version to support BQRv5 format
    Determine the common part length based on the BQR version,
    and then adjust the vendor part offset to ensure that vendor
    parameters can be parsed correctly.
    
    Bug: 384649925
    Test: executed cts.BluetoothQualityReportTest tests.
          manually tested with a BT controller supporting BQRV5.
    
    Change-Id: I3ce20d33f5b710d2150f30e7fbdd1b47a2f9816f

--- android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -1113,6 +1113,13 @@ class AdapterProperties {
         return options.toBundle();
     }
 
+    /**
+     * @return the mVersSupported
+     */
+    int getVendorCapVersion() {
+        return mVersSupported;
+    }
+
     protected void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
         writer.println(TAG);
         writer.println("  " + "Name: " + getName());
--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -6693,6 +6693,10 @@ public class AdapterService extends Service {
         return mRemoteDevices;
     }
 
+    public int getVendorCapVersion() {
+        return mAdapterProperties.getVendorCapVersion();
+    }
+
     private static String dumpScanMode(int scanMode) {
         switch (scanMode) {
             case SCAN_MODE_NONE:
