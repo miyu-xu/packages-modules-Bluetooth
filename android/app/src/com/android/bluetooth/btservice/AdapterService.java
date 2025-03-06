12e4a2eea31718cb9ff99d703b5cb423ae5c7478
Srini S <ssrinivaz@google.com>
2025-03-06 14:39:00 -0800

    Annotate createBondOutOfBand API with BLUETOOTH_PRIVILEGED permission
    createBondOutOfBand API is annotated with only BLUETOOTH_CONNECT permission although BLUETOOTH_PRIVILEGED permission is also enforced internally.
    This change fixes the annotation.
    
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT
    Bug: 372295458
    
    Change-Id: I6fae8e550b76d122e01a2963d3e8cc05e2b7079a

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -2605,6 +2605,29 @@ public class AdapterService extends Service {
         public boolean createBond(
                 BluetoothDevice device,
                 int transport,
+                AttributionSource source) {
+            AdapterService service = getService();
+            if (service == null
+                    || !callerIsSystemOrActiveOrManagedUser(service, TAG, "createBond")
+                    || !Utils.checkConnectPermissionForDataDelivery(
+                            service, source, "AdapterService createBond")) {
+                return false;
+            }
+
+            Log.i(
+                    TAG,
+                    "createBond:"
+                            + (" device=" + device)
+                            + (" transport=" + transport)
+                            + (" from " + Utils.getUidPidString()));
+            return service.createBond(
+                    device, transport, remoteP192Data, remoteP256Data, source.getPackageName());
+        }
+
+        @Override
+        public boolean createBondOutOfBand(
+                BluetoothDevice device,
+                int transport,
                 OobData remoteP192Data,
                 OobData remoteP256Data,
                 AttributionSource source) {
@@ -2623,7 +2646,7 @@ public class AdapterService extends Service {
 
             Log.i(
                     TAG,
-                    "createBond:"
+                    "createBondOutOfBand:"
                             + (" device=" + device)
                             + (" transport=" + transport)
                             + (" from " + Utils.getUidPidString()));
