eb389e408db61bba5090f16d194a47b9eadcdfa0
William Escande <wescande@google.com>
2025-03-05 16:03:16 -0800

    Unify usage of RemoteExceptionIgnoringConsumer
    Bug: 311772251
    Flag: Exempt refactor
    Test: m .
    Change-Id: I5910ce5aff3a35448496d1b7fc139c4f85fb1f08

--- android/app/src/com/android/bluetooth/Utils.java
+++ android/app/src/com/android/bluetooth/Utils.java
@@ -63,7 +63,6 @@ import android.os.Build;
 import android.os.ParcelUuid;
 import android.os.PowerExemptionManager;
 import android.os.Process;
-import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -96,7 +95,6 @@ import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 public final class Utils {
     public static final String TAG_PREFIX_BLUETOOTH = "Bluetooth";
@@ -1243,23 +1241,6 @@ public final class Utils {
         PackageManager pm = context.getPackageManager();
         return pm.hasSystemFeature(PackageManager.FEATURE_TELEVISION)
                 || pm.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
-    }
-
-    /** A {@link Consumer} that automatically ignores any {@link RemoteException}s. */
-    @FunctionalInterface
-    @SuppressWarnings("FunctionalInterfaceMethodChanged")
-    public interface RemoteExceptionIgnoringConsumer<T> extends Consumer<T> {
-        /** Called by {@code accept}. */
-        void acceptOrThrow(T t) throws RemoteException;
-
-        @Override
-        default void accept(T t) {
-            try {
-                acceptOrThrow(t);
-            } catch (RemoteException ex) {
-                // Ignore RemoteException
-            }
-        }
     }
 
     /**
