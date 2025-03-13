46bd3ebdb7b2646ae5a845c30983f62e20eddd0e
Rajat Soni <quic_rajasoni@quicinc.com>
2025-03-13 11:55:11 +0530

    AVRCP: Set AVRCP Controller version to v1.6 as default
    - Set AVRCP Controller version to v1.6 as default that
    earlier was v1.4 for PTS.
    
    - Correctly Map Avrcp Features to AVRCP profile version.
    
    Change-Id: Iecbc903f5679a4be325d03167a5081f649f7338d

--- system/stack/avrc/avrc_api.cc
+++ system/stack/avrc/avrc_api.cc
@@ -985,7 +985,8 @@ uint16_t AVRC_GetControlProfileVersion() {
   uint16_t profile_version = AVRC_REV_1_3;
   char avrcp_version[PROPERTY_VALUE_MAX] = {0};
   osi_property_get(AVRC_CONTROL_VERSION_PROPERTY, avrcp_version,
-                   strncmp(volume_disabled, "true", 4) == 0 ? AVRC_1_3_STRING : AVRC_1_4_STRING);
+                   strncmp(volume_disabled, "true", 4) == 0 ? AVRC_1_3_STRING
+                                                            : AVRC_1_6_STRING);
 
   if (!strncmp(AVRC_1_6_STRING, avrcp_version, sizeof(AVRC_1_6_STRING))) {
     profile_version = AVRC_REV_1_6;
