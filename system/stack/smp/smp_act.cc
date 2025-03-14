09d7d2eb7dcee172e3d15ac7692fb14323c390e7
penghua <quic_penghua@quicinc.com>
2025-03-14 11:54:28 +0800

    Process SMP complete in one corner condition
    Issue:
    1.DUT is peripheral role and starts security request after LE link
    is established.
    2. remote starts encryption.
    3. DUT received authentication complete before security request is sent
    out.  DUT moved to SMP IDLE but not reset SMP control block.
    4.Security request is sent out, but has no chance to reset SMP control
    block as SMP is in IDLE state.
    
    Fix:
    Process SMP complete event if role is peripheral and started
    flag is true
    
    Change-Id: Id2351999bb8081003ba8b5bba8a2405bb99b0d4a

--- system/stack/smp/smp_act.cc
+++ system/stack/smp/smp_act.cc
@@ -1592,8 +1592,14 @@ void smp_br_send_pair_response(tSMP_CB* p_cb, tSMP_INT_DATA* /* p_data */) {
  *                  callback and remove the connection if needed.
  ******************************************************************************/
 void smp_pairing_cmpl(tSMP_CB* p_cb, tSMP_INT_DATA* /* p_data */) {
+  log::warn("smp_pairing_cmpl, tx unacked: {}", p_cb->total_tx_unacked);
   if (p_cb->total_tx_unacked == 0) {
     /* process the pairing complete */
+    smp_proc_pairing_cmpl(p_cb);
+  } else if ((p_cb->flags & SMP_PAIR_FLAGS_WE_STARTED_DD) && (p_cb->role == HCI_ROLE_PERIPHERAL) &&
+             (p_cb->state == SMP_STATE_IDLE)) {
+    log::warn(
+            "smp_pairing_cmpl tx unacked is not zero and role is peripheral, proc pairing comple");
     smp_proc_pairing_cmpl(p_cb);
   }
 }
