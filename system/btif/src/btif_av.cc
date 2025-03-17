6bdce7b9b6c9ba6acd5fd6ff3f339d8464b8a420
Rajat Soni <quic_rajasoni@quicinc.com>
2025-03-17 13:02:14 +0530

    A2DP: Disallow offload start call for already streaming
    Disallow calling btif_a2dp_on_started if stream was
    already intended to suspend later.
    
    Change-Id: I7fa1565290f354df1423003f4756883261f19e16

--- system/btif/src/btif_av.cc
+++ system/btif/src/btif_av.cc
@@ -2298,9 +2298,11 @@ bool BtifAvStateMachine::StateOpened::ProcessEvent(uint32_t event, void* p_data)
         }
 
         // Invoke the started handler only when initiator.
+        log::info("Peer should suspend {}", should_suspend ? "true" : "false");
         if (!com::android::bluetooth::flags::a2dp_ignore_started_when_responder() ||
             peer_.CheckFlags(BtifAvPeer::kFlagPendingStart)) {
-          if (btif_a2dp_on_started(peer_.PeerAddress(), &p_av->start, A2dpType::kSource)) {
+          if (!should_suspend &&
+                   btif_a2dp_on_started(peer_.PeerAddress(), &p_av->start, A2dpType::kSource)) {
             // Only clear pending flag after acknowledgement
             peer_.ClearFlags(BtifAvPeer::kFlagPendingStart);
           }
