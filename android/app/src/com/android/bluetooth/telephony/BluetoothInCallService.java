602cc6fb00148e7087d5e4799360f5049b46e50c
sarvsaur <quic_sarvsaur@quicinc.com>
2025-03-25 17:01:06 +0530

    Add support to hold conference call
    Added support to hold a conference call.
    
    Change-Id: I3798a3a0e40fc8c690e09e464539640ada87fbe9

--- android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
+++ android/app/src/com/android/bluetooth/telephony/BluetoothInCallService.java
@@ -1209,6 +1209,15 @@ public class BluetoothInCallService extends InCallService {
                     && activeCall.can(Connection.CAPABILITY_HOLD)) {
                 activeCall.hold();
                 return true;
+            } else if (!mCallInfo.isNullCall(activeCall)) {
+                BluetoothCall conferenceCall = getBluetoothCallById(activeCall.getParentId());
+                if (!mCallInfo.isNullCall(conferenceCall)
+                    && (conferenceCall.can(Connection.CAPABILITY_HOLD) ||
+                        conferenceCall.can(Connection.CAPABILITY_SUPPORT_HOLD))) {
+                    Log.i(TAG, "Hold conference call");
+                    conferenceCall.hold();
+                    return true;
+                }
             }
         } else if (chld == CHLD_TYPE_ADDHELDTOCONF) {
             if (!mCallInfo.isNullCall(activeCall)) {
