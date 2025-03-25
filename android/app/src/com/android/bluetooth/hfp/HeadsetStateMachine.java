4967a05fdd403f1b05a769220426d749deca1184
sarvsaur <quic_sarvsaur@quicinc.com>
2025-03-25 17:34:40 +0530

    HFP: Suspend A2DP/LE audio if call is present during HFP connection.
    Suspend A2DP/LE audio if call is present during HFP connection.
    
    Bug: 406173322
    Change-Id: Ibdad4f842c9357231fb5bfd0dc1115e5c5640989

--- android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -1298,6 +1298,13 @@ class HeadsetStateMachine extends StateMachine {
                 processNoiseReductionEvent(true);
                 // Query phone state for initial setup
                 mSystemInterface.queryPhoneState(mHeadsetService);
+                if (mSystemInterface.isInCall() || mSystemInterface.isRinging()) {
+                    Log.w(TAG, "call is in ringing/present, suspending a2dp/le audio");
+                    mSystemInterface.getAudioManager().setA2dpSuspended(true);
+                    if (isAtLeastU()) {
+                        mSystemInterface.getAudioManager().setLeAudioSuspended(true);
+                    }
+                }
                 // Remove pending connection attempts that were deferred during the pending
                 // state. This is to prevent auto connect attempts from disconnecting
                 // devices that previously successfully connected.
