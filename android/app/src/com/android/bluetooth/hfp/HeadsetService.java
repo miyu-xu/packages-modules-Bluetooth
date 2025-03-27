39336ad652c78ebeec3638ac874f7571c6b09da7
sarvsaur <quic_sarvsaur@quicinc.com>
2025-03-27 18:05:20 +0530

    HFP: Delay audio connection
    Delay audio connection for 100ms by default.
    It is configurable by property persist.vendor.bluetooth.audioconnect.delay.
    During HFP SHO with different codecs, if SCO switches too fast, it is likely that
    previous voice data stream to next SCO channel, results in short-term speech noise.
    
    Few remotes expect SCO immediately after SLC if there is an outgoing call on the phone. Set connDelay to 0 for them.
    
    Bug: 406708501
    Change-Id: I8240dffaa3f810d40f5c784cde01ab7b74367a0e

--- android/app/src/com/android/bluetooth/hfp/HeadsetService.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetService.java
@@ -173,6 +173,7 @@ public class HeadsetService extends ProfileService {
     public HeadsetService(AdapterService adapterService) {
         this(adapterService, HeadsetNativeInterface.getInstance(), null);
     }
+    private static final int AUDIO_CONNECTION_DELAY_DEFAULT = 100;
 
     @VisibleForTesting
     HeadsetService(AdapterService adapterService, HeadsetNativeInterface nativeInterface) {
@@ -1557,6 +1558,8 @@ public class HeadsetService extends ProfileService {
     }
 
     int connectAudio(BluetoothDevice device) {
+        int connDelay = SystemProperties.getInt("persist.vendor.bluetooth.audioconnect.delay",
+                AUDIO_CONNECTION_DELAY_DEFAULT);
         Log.i(TAG, "connectAudio: device=" + device + ", " + Utils.getUidPidString());
         synchronized (mStateMachines) {
             final HeadsetStateMachine stateMachine = mStateMachines.get(device);
@@ -1580,6 +1583,23 @@ public class HeadsetService extends ProfileService {
                         BluetoothStatsLog
                                 .BLUETOOTH_CROSS_LAYER_EVENT_REPORTED__STATE__SCO_CONNECT_AUDIO_START,
                         Binder.getCallingUid());
+                /**
+                 * add for case that device disconnecting audio has been set active again,
+                 * then send CONNECT_AUDIO if not contained in queue and should persist audio
+                 */
+                if (mActiveDevice != null && mActiveDevice.equals(device) &&
+                        stateMachine.getAudioState() == BluetoothHeadset.STATE_AUDIO_DISCONNECTING
+                        && !stateMachine.hasMessagesInQueue(HeadsetStateMachine.CONNECT_AUDIO) &&
+                        !stateMachine.hasDeferredMessagesInQueue(HeadsetStateMachine.CONNECT_AUDIO)
+                        && shouldPersistAudio()) {
+                    if (stateMachine.getIfDeviceDenylistedForSCOAfterSLC() == true)
+                        connDelay = 0;
+
+                    Log.i(TAG, "connectAudio: active again and connect audio after "
+                            + connDelay + " ms");
+                    stateMachine.sendMessageDelayed(HeadsetStateMachine.CONNECT_AUDIO,
+                            device, connDelay);
+                }
                 return BluetoothStatusCodes.SUCCESS;
             }
             if (isAudioOn()) {
@@ -1589,7 +1609,12 @@ public class HeadsetService extends ProfileService {
                                 + Arrays.toString(getNonIdleAudioDevices().toArray()));
                 return BluetoothStatusCodes.ERROR_AUDIO_DEVICE_ALREADY_CONNECTED;
             }
-            stateMachine.sendMessage(HeadsetStateMachine.CONNECT_AUDIO, device);
+
+            if (stateMachine.getIfDeviceDenylistedForSCOAfterSLC() == true)
+                connDelay = 0;
+
+            Log.i(TAG, "connectAudio: connect audio after " + connDelay + " ms");
+            stateMachine.sendMessageDelayed(HeadsetStateMachine.CONNECT_AUDIO, device, connDelay);
             logScoSessionMetric(
                     device,
                     BluetoothStatsLog
