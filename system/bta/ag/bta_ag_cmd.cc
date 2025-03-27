df441bfacd720857d84eb02e78924bae63a77c36
sarvsaur <quic_sarvsaur@quicinc.com>
2025-03-27 15:44:00 +0530

    HFP IOP: Disable codec negotiation for some devices
    Some remotes are taking longer than codec negotiation
    time-out to respond to +BCS during codec negotiation.
    When codec negotiation times out, we abort SCO connection.
    For such remotes, disable codec negotiation and directly
    initiate SCO connection.
    
    Dynamic denylisting is added for INTEROP_DISABLE_CODEC_NEGOTIATION.
    When codec negotiation fails within 3 seconds, adding the device
    to dynamic denylist.
    
    Change-Id: If369575df08e4ebf6451df77146ea5f7d876e546

--- system/bta/ag/bta_ag_cmd.cc
+++ system/bta/ag/bta_ag_cmd.cc
@@ -50,6 +50,7 @@
 #include "main/shim/helpers.h"
 #include "main/shim/metrics_api.h"
 #include "osi/include/compat.h"
+#include "btif/include/btif_storage.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
 #include "stack/include/port_api.h"
 
@@ -1138,6 +1139,7 @@ void bta_ag_at_hfp_cback(tBTA_AG_SCB* p_scb, uint16_t cmd, uint8_t arg_type, cha
       /* store peer features */
       p_scb->peer_features = (uint16_t)int_arg;
 
+      tBTA_AG_FEAT features = p_scb->features & BTA_AG_BRSF_FEAT_SPEC;
       if (p_scb->peer_version < HFP_VERSION_1_7) {
         if (!(com::android::bluetooth::flags::check_peer_hf_indicator() &&
               p_scb->peer_version == HFP_HSP_VERSION_UNKNOWN &&
@@ -1149,6 +1151,12 @@ void bta_ag_at_hfp_cback(tBTA_AG_SCB* p_scb, uint16_t cmd, uint8_t arg_type, cha
       LogMetricHfpAgVersion(ToGdAddress(p_scb->peer_addr), p_scb->peer_version);
       log::verbose("BRSF HF: 0x{:x}, phone: 0x{:x}", p_scb->peer_features, p_scb->masked_features);
 
+      if(interop_match_addr_or_name(INTEROP_DISABLE_CODEC_NEGOTIATION,
+          &p_scb->peer_addr, &btif_storage_get_remote_device_property)) {
+          log::verbose("disable codec negotiation for phone, remote for denylisted device");
+          features = features & ~(BTA_AG_FEAT_CODEC);
+          p_scb->peer_features = p_scb->peer_features & ~(BTA_AG_PEER_FEAT_CODEC);
+      }
       /* send BRSF, send OK */
       bta_ag_send_result(p_scb, BTA_AG_LOCAL_RES_BRSF, nullptr, (int16_t)p_scb->masked_features);
       bta_ag_send_ok(p_scb);
