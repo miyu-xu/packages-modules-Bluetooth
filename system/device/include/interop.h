67025c0a0803debc74008c31eac67ae26899f258
Rajat Soni <quic_rajasoni@quicinc.com>
2025-04-23 12:40:34 +0530

    BTA: Delay Sniff Subrating till VS_AP_STOP_MEDIA is sent.
    - Delay updation of Sniff Subrating as in some remote's
      timing issues were observed when VS_A2DP_STOP was sent
      after Suspend Request was sent by the Host.
    
    Change-Id: I1735e0374f4777bfbb35f177b749d84a8984ed98

--- system/device/include/interop.h
+++ system/device/include/interop.h
@@ -368,6 +368,8 @@ typedef enum {
   // Some devices claim to support HFP in EIR but does not actually support it.
   INTEROP_DISABLE_HF_PROFILE,
 
+  INTEROP_A2DP_DELAY_SNIFF_SUBRATING,
+
   END_OF_INTEROP_LIST
 } interop_feature_t;
 
