4972d5dc0b0672589c5edcc6f0cd0b920dfb7541
Wonkyoung Jang <wonder7.jang@samsung.com>
2025-05-19 03:04:36 -0700

    [2025][CodeSync][Bug][HFP] change sequence between btm_sco_chk_pend_unpark and btm_pm_proc_mode_change
    Change-Id: I1e4cab88dee308ab15c370644e038ef3b85462d9

--- system/stack/btu/btu_hcif.cc
+++ system/stack/btu/btu_hcif.cc
@@ -1172,9 +1172,9 @@ static void btu_hcif_mode_change_evt(uint8_t* p) {
   STREAM_TO_UINT16(handle, p);
   STREAM_TO_UINT8(current_mode, p);
   STREAM_TO_UINT16(interval, p);
-  btm_sco_chk_pend_unpark(static_cast<tHCI_STATUS>(status), handle);
   btm_pm_proc_mode_change(static_cast<tHCI_STATUS>(status), handle,
                           static_cast<tHCI_MODE>(current_mode), interval);
+  btm_sco_chk_pend_unpark(static_cast<tHCI_STATUS>(status), handle);
 
 #if (HID_DEV_INCLUDED == TRUE && HID_DEV_PM_INCLUDED == TRUE)
   hidd_pm_proc_mode_change(status, current_mode, interval);
