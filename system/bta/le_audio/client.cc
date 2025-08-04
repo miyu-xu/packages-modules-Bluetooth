35507106152b9a25833fad46b21cc28571b0e786
sarvsaur <quic_sarvsaur@quicinc.com>
2025-08-04 16:12:52 +0530

    Le-Audio: Handle race condition between BT HAL and BT App req.
    - Group-1 is active and MM HAL sent start request, and BT stack
      sent codecconfig request to remote by setting target state
      as streaming as part of start req processing.
    
    - Meantime SetActive for Group-2 sent by BT App, on which BT stack
      sent GroupStop for previous group. This lead to target state
      reset to idle for Group-1.
    
    - Finally, there is no ack from BT stack to BT HAL till 4.5sec,
      will lead to ANR.
    
    - Fixed by, cancelling stream request in this corner case for the
      start request of Group-1, when inbetween start process has been
      aborted.
    
    Change-Id: Icb000003040a29861ef7249c55245c3eac461142

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -1302,6 +1302,19 @@ public:
       return;
     }
 
+    if (group->GetState() == AseState::BTA_LE_AUDIO_ASE_STATE_IDLE) {
+      if (group->GetTargetState() != AseState::BTA_LE_AUDIO_ASE_STATE_IDLE) {
+        log::warn("group {} was about to stream, but got canceled: {}",
+                  group_id, ToString(group->GetTargetState()));
+        group->SetTargetState(AseState::BTA_LE_AUDIO_ASE_STATE_IDLE);
+        CancelStreamingRequest();
+      } else {
+        log::warn(", group {} already stopped: {}", group_id,
+                  ToString(group->GetState()));
+      }
+      return;
+    }
+
     groupStateMachine_->StopStream(group);
   }
 
