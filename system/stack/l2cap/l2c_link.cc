55eb2ebae1ddc4b3e57431947b1f7aca376777c4
janardhana rao bokka <quic_jbokka@quicinc.com>
2025-10-06 06:43:17 +0530

    L2CAP: Send disconnectInd to upper module if LE link disconnected
    Issue:
    L2CAP is not sending to disconnectInd to upper layers and instead
    reusing it, due to that some apps, not receiving the disconnect
    indication
    
    Fix:
    Send disconnectInd to upper module if LE link disconnected
    
    Bug: 449582012
    
    Change-Id: Ibd97f1bc5fb006a501f94d63290966fc2d91c8b2

--- system/stack/l2cap/l2c_link.cc
+++ system/stack/l2cap/l2c_link.cc
@@ -340,6 +340,20 @@ bool l2c_link_hci_disc_comp(uint16_t handle, tHCI_REASON reason) {
     /* for LE link, always drop and re-open to ensure to get LE remote feature
      */
     if (p_lcb->transport == BT_TRANSPORT_LE) {
+      /* If we are going to re-use the LCB without dropping it, release all
+       * fixed channels here */
+      int xx;
+      for (xx = 0; xx < L2CAP_NUM_FIXED_CHNLS; xx++) {
+        if (p_lcb->p_fixed_ccbs[xx] &&
+            p_lcb->p_fixed_ccbs[xx] != p_lcb->p_pending_ccb) {
+          l2cu_release_ccb(p_lcb->p_fixed_ccbs[xx]);
+
+          p_lcb->p_fixed_ccbs[xx] = NULL;
+          (*l2cb.fixed_reg[xx].pL2CA_FixedConn_Cb)(
+              xx + L2CAP_FIRST_FIXED_CHNL, p_lcb->remote_bd_addr, false,
+              p_lcb->DisconnectReason(), p_lcb->transport);
+        }
+      }
       btm_acl_removed(handle);
       if (com::android::bluetooth::flags::invalidate_hci_handle_on_acl_removal()) {
         p_lcb->InvalidateHandle();
