64c95feefbabe0138eba11e02f6c2605320d3c31
Zhangxu Hu <zhangxu.hu@gm.com>
2026-01-29 15:46:02 -0500

    Cancel bluetooth connection
    Test:test bluetooth connection can be canceled during pairing
    
    BUG: 463931117
    Change-Id: Ib66b33e433e566d1c083819b0b250b04369db37f

--- android/app/jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ android/app/jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -42,6 +42,8 @@
 #include "hardware/bluetooth.h"
 #include "hardware/bt_sock.h"
 
+#include "main/shim/acl_api.h"
+
 using bluetooth::Uuid;
 extern bt_interface_t bluetoothInterface;
 
@@ -1478,6 +1480,26 @@ static jboolean cancelBondNative(JNIEnv* env, jobject /* obj */, jbyteArray addr
   return (ret == BT_STATUS_SUCCESS) ? JNI_TRUE : JNI_FALSE;
 }
 
+static jboolean cancelConnectionNative(JNIEnv* env, jobject /* obj */, jbyteArray address) {
+  log::verbose("");
+
+  if (!sBluetoothInterface) {
+    return JNI_FALSE;
+  }
+
+  jbyte* addr = env->GetByteArrayElements(address, NULL);
+  if (addr == NULL) {
+    jniThrowIOException(env, EINVAL);
+    return JNI_FALSE;
+  }
+
+  RawAddress legacy_raw{};
+  memcpy(&legacy_raw, addr, sizeof(legacy_raw));
+  env->ReleaseByteArrayElements(address, addr, JNI_ABORT);
+  const bool ok = bluetooth::shim::ACL_CancelConnection(legacy_raw);
+  return ok ? JNI_TRUE : JNI_FALSE;
+}
+
 static jboolean pairingIsBusyNative(JNIEnv* /*env*/, jobject /* obj */) {
   log::verbose("");
 
@@ -2215,6 +2237,7 @@ static int register_com_android_bluetooth_btservice_AdapterService(JNIEnv* env) 
            reinterpret_cast<void*>(createBondOutOfBandNative)},
           {"removeBondNative", "([B)Z", reinterpret_cast<void*>(removeBondNative)},
           {"cancelBondNative", "([B)Z", reinterpret_cast<void*>(cancelBondNative)},
+          {"cancelConnectionNative", "([B)Z", reinterpret_cast<void*>(cancelConnectionNative)},
           {"pairingIsBusyNative", "()Z", reinterpret_cast<void*>(pairingIsBusyNative)},
           {"generateLocalOobDataNative", "(I)V",
            reinterpret_cast<void*>(generateLocalOobDataNative)},
--- framework/java/android/bluetooth/BluetoothAdapter.java
+++ framework/java/android/bluetooth/BluetoothAdapter.java
@@ -48,6 +48,7 @@ import android.annotation.SdkConstant;
 import android.annotation.SdkConstant.SdkConstantType;
 import android.annotation.SuppressLint;
 import android.annotation.SystemApi;
+import android.annotation.FlaggedApi;
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothDevice.AddressType;
 import android.bluetooth.BluetoothDevice.Transport;
@@ -2543,6 +2544,32 @@ public final class BluetoothAdapter {
                                 s.getMostRecentlyConnectedDevices(mAttributionSource),
                                 mAttributionSource),
                 Collections.emptyList());
+    }
+
+    /**
+     * Cancel Bluetooth Connection
+     *
+     * @hide
+     */
+    @SystemApi
+    @RequiresBluetoothConnectPermission
+    @RequiresPermission(BLUETOOTH_CONNECT)
+    @FlaggedApi("com.android.bluetooth.flags.cancel_connection_api")
+    public boolean cancelConnection(@Nullable BluetoothDevice device) {
+        if (getState() != STATE_ON) {
+            return false;
+        }
+        mServiceLock.readLock().lock();
+        try {
+            if (mService != null) {
+                return mService.cancelConnection(device, mAttributionSource);
+            }
+        } catch (RemoteException e) {
+            logRemoteException(TAG, e);
+        } finally {
+            mServiceLock.readLock().unlock();
+        }
+        return false;
     }
 
     /**
